562311d803b1559835d96d796f03bf7ea84ab7cd
Kirk Wylie <kirk@opengamma.com>
2009-08-27 18:14:10 +0100

    Initial empty repository for OpenGamma Engine.

---
+++ src/com/opengamma/Dummy.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma;
+
+/**
+ * Just a dummy class for repository setup.
+ *
+ * @author kirk
+ */
+public class Dummy {
+
+}
