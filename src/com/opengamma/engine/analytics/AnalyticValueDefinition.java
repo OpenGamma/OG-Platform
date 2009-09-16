1236d04186ee5a370bb8bd10000bfbafe4b71b47
Kirk Wylie <kirk@opengamma.com>
2009-09-16 09:40:50 +0100

    Just better docs on what an AnalyticValueDefinition could represent.

--- src/com/opengamma/engine/analytics/AnalyticValueDefinition.java
+++ src/com/opengamma/engine/analytics/AnalyticValueDefinition.java
@@ -5,16 +5,24 @@
  */
 package com.opengamma.engine.analytics;
 
+import java.io.Serializable;
+
+
 /**
  * Allows specification of the types of data that an {@link AnalyticFunction} can
  * produce.
+ * Examples of concrete {@code AnalyticValueDefinition} instances might include:
+ * <ul>
+ *   <li>The last price for CSCO on the Nasdaq exchange.</li>
+ *   <li>The per-unit delta for a particular option on CSCO.</li>
+ *   <li>The yield curve for USD.</li>
+ *   <li>The yield curve for USD using a specified set of strips using a particular
+ *       bootstrapping algorithm and a particular interpolation algorithm.</li>
+ * </ul>
  *
  * @author kirk
  */
-public interface AnalyticValueDefinition {
-
-  // REVIEW kirk 2009-09-03 -- Need units?
-  
+public interface AnalyticValueDefinition extends Serializable {
   // NOTE kirk 2009-09-03 -- Plan is for this to be a key/value pair set, and thus
   // exactly match up with a FudgeMsg.
 }
