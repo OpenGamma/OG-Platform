cf28e4d3ee986ae76ea4aa34ae7a3b3569c896f1
Elaine McLeod <elaine@opengamma.com>
2009-09-17 17:45:04 +0100

    Added calculation mode.

---
+++ src/com/opengamma/util/CalculationMode.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.util;
+
+/**
+ * 
+ * @author emcleod
+ * 
+ */
+public enum CalculationMode {
+  LENIENT, STRICT
+}
