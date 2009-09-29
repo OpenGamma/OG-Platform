63264cac48f1cc49fb37ab6ed76602c2d1920c16
Kirk Wylie <kirk@opengamma.com>
2009-09-29 16:49:44 +0100

    Created DomainSpecificIdentifiers & Impl.
    This is the merger of the logic from SecurityKeyImpl and LiveDataSpecificationImpl,
    because there was way too much duplicated code, and I think we can get some
    benefits from collapsing down where appropriate to common routines.

---
+++ src/com/opengamma/DomainSpecificIdentifiers.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma;
+
+import java.util.Collection;
+
+/**
+ * 
+ *
+ * @author kirk
+ */
+public interface DomainSpecificIdentifiers {
+  
+  /**
+   * Obtain any security identifiers desired as part of resolving based on this key.
+   * The order in which the results are identified in the collection
+   * <em>may</em> be meaningful, depending on the implementation of
+   * the {@link SecurityMaster} used to look up securities based on this key.
+   * This method <em>must not</em> return {@code null}, but should
+   * return an empty {@link Collection} where there are no identifiers
+   * for this key.
+   * 
+   * @return All identifiers for this key.
+   */
+  Collection<DomainSpecificIdentifier> getIdentifiers();
+
+}
---
+++ src/com/opengamma/DomainSpecificIdentifiersImpl.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang.ObjectUtils;
+
+/**
+ * 
+ *
+ * @author kirk
+ */
+public class DomainSpecificIdentifiersImpl implements Serializable, DomainSpecificIdentifiers {
+  private final List<DomainSpecificIdentifier> _identifiers;
+  private final int _hashCode;
+  
+  public DomainSpecificIdentifiersImpl(DomainSpecificIdentifier... identifiers) {
+    if((identifiers == null) || (identifiers.length == 0)) {
+      _identifiers = Collections.emptyList();
+    } else {
+      _identifiers = new ArrayList<DomainSpecificIdentifier>(identifiers.length);
+      for(DomainSpecificIdentifier secId : identifiers) {
+        _identifiers.add(secId);
+      }
+    }
+    _hashCode = calcHashCode();
+  }
+  
+  public DomainSpecificIdentifiersImpl(Collection<? extends DomainSpecificIdentifier> identifiers) {
+    if(identifiers == null) {
+      _identifiers = Collections.emptyList();
+    } else {
+      _identifiers = new ArrayList<DomainSpecificIdentifier>(identifiers);
+    }
+    _hashCode = calcHashCode();
+  }
+  
+  public DomainSpecificIdentifiersImpl(DomainSpecificIdentifier identifier) {
+    if(identifier == null) {
+      _identifiers = Collections.emptyList();
+    } else {
+      _identifiers = new ArrayList<DomainSpecificIdentifier>();
+      _identifiers.add(identifier);
+    }
+    _hashCode = calcHashCode();
+  }
+
+  @Override
+  public Collection<DomainSpecificIdentifier> getIdentifiers() {
+    return _identifiers;
+  }
+  
+  protected int calcHashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result
+        + ((_identifiers == null) ? 0 : _identifiers.hashCode());
+    return result;
+  }
+  
+  @Override
+  public int hashCode() {
+    return _hashCode;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    DomainSpecificIdentifiersImpl other = (DomainSpecificIdentifiersImpl) obj;
+    if(!ObjectUtils.equals(_identifiers, other._identifiers)) {
+      return false;
+    }
+    return true;
+  }
+
+
+}
