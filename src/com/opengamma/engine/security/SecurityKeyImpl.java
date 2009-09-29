08f3fe069f1b204812d12a481699e4f33f684c2c
Kirk Wylie <kirk@opengamma.com>
2009-09-29 16:51:08 +0100

    Changed SecurityKey to be based on DomainSpecificIdentifiers.

--- src/com/opengamma/engine/security/SecurityKeyImpl.java
+++ src/com/opengamma/engine/security/SecurityKeyImpl.java
@@ -5,13 +5,10 @@
  */
 package com.opengamma.engine.security;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 import com.opengamma.DomainSpecificIdentifier;
+import com.opengamma.DomainSpecificIdentifiersImpl;
 
 // REVIEW kirk 2009-09-01 -- I'm not particularly happy with the name of this class.
 
@@ -20,68 +17,19 @@ import com.opengamma.DomainSpecificIdentifier;
  *
  * @author kirk
  */
-public class SecurityKeyImpl implements SecurityKey, Serializable {
-  
-  private final List<DomainSpecificIdentifier> _identifiers;
-  
+public class SecurityKeyImpl
+extends DomainSpecificIdentifiersImpl
+implements SecurityKey {
   public SecurityKeyImpl(DomainSpecificIdentifier... identifiers) {
-    if((identifiers == null) || (identifiers.length == 0)) {
-      _identifiers = Collections.emptyList();
-    } else {
-      _identifiers = new ArrayList<DomainSpecificIdentifier>(identifiers.length);
-      for(DomainSpecificIdentifier secId : identifiers) {
-        _identifiers.add(secId);
-      }
-    }
+    super(identifiers);
   }
   
   public SecurityKeyImpl(Collection<? extends DomainSpecificIdentifier> identifiers) {
-    if(identifiers == null) {
-      _identifiers = Collections.emptyList();
-    } else {
-      _identifiers = new ArrayList<DomainSpecificIdentifier>(identifiers);
-    }
+    super(identifiers);
   }
   
   public SecurityKeyImpl(DomainSpecificIdentifier secIdentifier) {
-    if(secIdentifier == null) {
-      _identifiers = Collections.emptyList();
-    } else {
-      _identifiers = new ArrayList<DomainSpecificIdentifier>();
-      _identifiers.add(secIdentifier);
-    }
+    super(secIdentifier);
   }
-
-  @Override
-  public Collection<DomainSpecificIdentifier> getIdentifiers() {
-    return _identifiers;
-  }
-  
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result
-        + ((_identifiers == null) ? 0 : _identifiers.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    SecurityKeyImpl other = (SecurityKeyImpl) obj;
-    if (_identifiers == null) {
-      if (other._identifiers != null)
-        return false;
-    } else if (!_identifiers.equals(other._identifiers))
-      return false;
-    return true;
-  }
-
 
 }
