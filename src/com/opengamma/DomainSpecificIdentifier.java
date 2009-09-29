5bfd618c2c7768ab6373ad336e9b90e57ac02509
Kirk Wylie <kirk@opengamma.com>
2009-09-29 11:40:58 +0100

    Moved SecurityIdentifier to OG-Util and renamed DomainSpecificIdentifier.

---
+++ src/com/opengamma/DomainSpecificIdentifier.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * A particular identifier, within a particular domain, which can be used
+ * to identify an entity.
+ * Real-world examples of {@code DomainSpecificIdentifier} would include:
+ * <ul>
+ *   <li>Cusip</li>
+ *   <li>Isin</li>
+ *   <li>RIC code</li>
+ *   <li>Bloomberg ID</li>
+ *   <li>Bloomberg Ticker</li>
+ *   <li>Trading system OTC trade ID</li>
+ * </ul>
+ *
+ * @author kirk
+ */
+public final class DomainSpecificIdentifier implements Serializable, Cloneable {
+  private final IdentificationDomain _domain;
+  private final String _value;
+
+  public DomainSpecificIdentifier(IdentificationDomain domain, String value) {
+    if(domain == null) {
+      throw new NullPointerException("Must provide a valid domain.");
+    }
+    if(value == null) {
+      throw new NullPointerException("Must provide a valid value.");
+    }
+    _domain = domain;
+    _value = value;
+  }
+  
+  /**
+   * @return the domain
+   */
+  public IdentificationDomain getDomain() {
+    return _domain;
+  }
+
+  /**
+   * @return the value
+   */
+  public String getValue() {
+    return _value;
+  }
+
+  @Override
+  protected DomainSpecificIdentifier clone() {
+    try {
+      return (DomainSpecificIdentifier) super.clone();
+    } catch (CloneNotSupportedException e) {
+      throw new AssertionError("Cloning is definitely supported.");
+    }
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return EqualsBuilder.reflectionEquals(this, obj);
+  }
+
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+
+}
---
+++ tests/unit/com/opengamma/DomainSpecificIdentifierTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.opengamma.IdentificationDomain;
+import com.opengamma.DomainSpecificIdentifier;
+
+/**
+ * A pure unit test for {@link DomainSpecificIdentifier}. 
+ *
+ * @author kirk
+ */
+public class DomainSpecificIdentifierTest {
+  
+  @Test(expected=NullPointerException.class)
+  public void noDomainConstruction() {
+    new DomainSpecificIdentifier(null, "foo");
+  }
+
+  @Test(expected=NullPointerException.class)
+  public void noValueConstruction() {
+    new DomainSpecificIdentifier(new IdentificationDomain("Bloomberg"), null);
+  }
+  
+  @Test
+  public void equality() {
+    IdentificationDomain d1 = new IdentificationDomain("d1");
+    IdentificationDomain d2 = new IdentificationDomain("d2");
+    
+    assertTrue(new DomainSpecificIdentifier(d1, "v1").equals(new DomainSpecificIdentifier(d1, "v1")));
+    assertFalse(new DomainSpecificIdentifier(d1, "v1").equals(new DomainSpecificIdentifier(d1, "v2")));
+    assertFalse(new DomainSpecificIdentifier(d1, "v1").equals(new DomainSpecificIdentifier(d2, "v1")));
+  }
+  
+  @Test
+  public void hashing() {
+    IdentificationDomain d1 = new IdentificationDomain("d1");
+    IdentificationDomain d2 = new IdentificationDomain("d2");
+    
+    assertTrue(new DomainSpecificIdentifier(d1, "v1").hashCode() == new DomainSpecificIdentifier(d1, "v1").hashCode());
+    assertFalse(new DomainSpecificIdentifier(d1, "v1").hashCode() == new DomainSpecificIdentifier(d1, "v2").hashCode());
+    assertFalse(new DomainSpecificIdentifier(d1, "v1").hashCode() == new DomainSpecificIdentifier(d2, "v1").hashCode());
+  }
+  
+  @Test
+  public void cloning() {
+    DomainSpecificIdentifier id = new DomainSpecificIdentifier(new IdentificationDomain("domain"), "value");
+    assertEquals(id, id.clone());
+    assertNotSame(id, id.clone());
+  }
+
+}
