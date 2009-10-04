6b1064b7c22fd598405b8ee3e7069c052da103af
Kirk Wylie <kirk@opengamma.com>
2009-10-04 16:21:04 +0100

    Another REVIEW comment.

--- src/com/opengamma/livedata/client/LiveDataEntitlementChecker.java
+++ src/com/opengamma/livedata/client/LiveDataEntitlementChecker.java
@@ -10,6 +10,10 @@ import com.opengamma.livedata.LiveDataSpecification;
 // REVIEW kirk 2009-10-04 -- While this is fine initially, eventually this needs to be changed
 // so that it can be fully asynchronous so that we're not blocking threads at all in a synchronous
 // state waiting for distributed responses.
+
+// REVIEW kirk 2009-10-04 -- The true/false nature here is too simplistic. Need some way
+// to provide additional feedback to the user as to WHY so that they can have IT staff
+// fix it easily. Otherwise all failures will result in expensive debugging and log-checking.
 /**
  * 
  *
