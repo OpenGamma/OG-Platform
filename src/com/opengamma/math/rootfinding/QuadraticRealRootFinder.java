09deae10ccd1e0233775d3b7212063d942aca6ff
Elaine McLeod <emcleod@emcleodthinkpad.(none)>
2009-10-23 12:27:33 +0100

    Finished testing root-finding.

--- src/com/opengamma/math/rootfinding/QuadraticRealRootFinder.java
+++ src/com/opengamma/math/rootfinding/QuadraticRealRootFinder.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.math.rootfinding;
 
 import com.opengamma.math.function.PolynomialFunction1D;
@@ -8,19 +13,21 @@ import com.opengamma.math.function.PolynomialFunction1D;
  * 
  */
 
-public class QuadraticRealRootFinder implements Polynomial1DRootFinder {
+public class QuadraticRealRootFinder implements Polynomial1DRootFinder<Double> {
 
-  public Double[] getRoot(PolynomialFunction1D f) {
-    Double[] coefficients = f.getCoefficients();
+  public Double[] getRoots(final PolynomialFunction1D function) {
+    if (function == null)
+      throw new IllegalArgumentException("Function was null");
+    final Double[] coefficients = function.getCoefficients();
     if (coefficients.length != 3)
       throw new IllegalArgumentException("Function is not a quadratic");
-    double c = coefficients[0];
-    double b = coefficients[1];
-    double a = coefficients[2];
-    double discriminant = b * b - 4 * a * c;
+    final double c = coefficients[0];
+    final double b = coefficients[1];
+    final double a = coefficients[2];
+    final double discriminant = b * b - 4 * a * c;
     if (discriminant < 0)
       throw new RootNotFoundException("No real roots for quadratic");
-    double q = -0.5 * (b + Math.signum(b) * discriminant);
+    final double q = -0.5 * (b + Math.signum(b) * discriminant);
     return new Double[] { q / a, c / q };
   }
 }
--- src/com/opengamma/math/rootfinding/RidderSingleRootFinder.java
+++ src/com/opengamma/math/rootfinding/RidderSingleRootFinder.java
@@ -1,62 +1,71 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.math.rootfinding;
 
-import com.opengamma.math.ConvergenceException;
 import com.opengamma.math.MathException;
-import com.opengamma.math.function.Function;
+import com.opengamma.math.function.Function1D;
 
 /**
  * 
  * @author emcleod
  * 
  */
-public class RidderSingleRootFinder implements DoubleSingleRootFinder {
+public class RidderSingleRootFinder extends RealSingleRootFinder {
+  private final double _accuracy;
+  private static final int MAX_ITER = 10000;
+  private static final double ZERO = 1e-16;
+
+  public RidderSingleRootFinder() {
+    _accuracy = 1e-9;
+  }
+
+  public RidderSingleRootFinder(final double accuracy) {
+    _accuracy = Math.abs(accuracy);
+  }
 
   @Override
-  public Double getRoot(Function<Double, Double> function, Double xLow, Double xHigh, Double accuracy) {
-    if (accuracy == null)
-      throw new IllegalArgumentException("Accuracy was null");
+  public Double getRoot(final Function1D<Double, Double> function, final Double xLow, final Double xHigh) {
+    checkInputs(function, xLow, xHigh);
     double x1 = xLow;
     double x2 = xHigh;
     double y1 = function.evaluate(x1);
     double y2 = function.evaluate(x2);
-    if (Math.abs(y1) < accuracy)
+    if (Math.abs(y1) < _accuracy)
       return xHigh;
-    if (Math.abs(y2) < accuracy)
-      return xLow;
+    if (Math.abs(y2) < _accuracy)
+      return x1;
     if (y1 * y2 >= 0)
-      throw new MathException(xLow + " and " + xHigh + " do not bracket a root");
+      throw new RootNotFoundException(x1 + " and " + xHigh + " do not bracket a root");
     double xMid, yMid, denom, xNew, yNew;
-    for (int i = 0; i < MAX_ATTEMPTS; i++) {
+    for (int i = 0; i < MAX_ITER; i++) {
       xMid = (x1 + x2) / 2;
       yMid = function.evaluate(xMid);
       denom = Math.sqrt(yMid * yMid - y1 * y2);
       if (Math.abs(denom) < ZERO)
-        throw new MathException("Denominator of updating formula was zero");
-      xNew = xMid + (xMid - x1) * ((y1 >= y2 ? 1 : -1) * yMid / denom);
+        throw new RootNotFoundException("Denominator of updating formula was zero");
+      xNew = xMid + (xMid - x1) * (y1 >= y2 ? 1 : -1) * yMid / denom;
       yNew = function.evaluate(xNew);
       if (Math.abs(yNew) < ZERO)
         return xNew;
-      if (Math.abs(adjustSign(yMid, yNew) - yMid) > ZERO) {
+      if (Math.abs(Math.copySign(yMid, yNew) - yMid) > ZERO) {
         x1 = xMid;
         y1 = yMid;
         x2 = xNew;
         y2 = yNew;
-      } else if (Math.abs(adjustSign(y1, yNew) - y1) > ZERO) {
+      } else if (Math.abs(Math.copySign(y1, yNew) - y1) > ZERO) {
         x2 = xNew;
         y2 = yNew;
-      } else if (Math.abs(adjustSign(y2, yNew) - y2) > ZERO) {
+      } else if (Math.abs(Math.copySign(y2, yNew) - y2) > ZERO) {
         x1 = xNew;
         y1 = yNew;
-      } else {
+      } else
         throw new MathException("Should never reach here");
-      }
-      if (Math.abs(x2 - x1) < accuracy)
+      if (Math.abs(x2 - x1) < _accuracy)
         return xNew;
     }
-    throw new ConvergenceException(CONVERGENCE_STRING);
-  }
-
-  private double adjustSign(double x, double y) {
-    return Math.abs(x) * Math.signum(y);
+    throw new RootNotFoundException("Could not find root in " + MAX_ITER + " attempts");
   }
 }
