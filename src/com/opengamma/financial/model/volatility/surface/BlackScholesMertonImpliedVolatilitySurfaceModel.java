91b78b1c614c56e1071cbbccc36bc876d8bcec80
Elaine McLeod <emcleod@emcleodthinkpad.(none)>
2009-10-28 13:12:25 +0000

    Tested implying volatility from option price in a BSM universe.

--- src/com/opengamma/financial/model/volatility/surface/BlackScholesMertonImpliedVolatilitySurfaceModel.java
+++ src/com/opengamma/financial/model/volatility/surface/BlackScholesMertonImpliedVolatilitySurfaceModel.java
@@ -7,6 +7,9 @@ package com.opengamma.financial.model.volatility.surface;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.opengamma.financial.model.option.definition.EuropeanVanillaOptionDefinition;
 import com.opengamma.financial.model.option.definition.StandardOptionDataBundle;
 import com.opengamma.financial.model.option.pricing.OptionPricingException;
@@ -16,11 +19,21 @@ import com.opengamma.math.function.Function1D;
 import com.opengamma.math.rootfinding.SingleRootFinder;
 
 public class BlackScholesMertonImpliedVolatilitySurfaceModel implements VolatilitySurfaceModel<EuropeanVanillaOptionDefinition, StandardOptionDataBundle> {
+  private static final Logger s_Log = LoggerFactory.getLogger(BlackScholesMertonImpliedVolatilitySurfaceModel.class);
   private final AnalyticOptionModel<EuropeanVanillaOptionDefinition, StandardOptionDataBundle> _bsm = new BlackScholesMertonModel();
   private SingleRootFinder<StandardOptionDataBundle, Double, Double> _rootFinder;
 
   @Override
   public VolatilitySurface getSurface(final Map<EuropeanVanillaOptionDefinition, Double> optionPrices, final StandardOptionDataBundle optionDataBundle) {
+    if (optionPrices == null)
+      throw new IllegalArgumentException("Option price map was null");
+    if (optionPrices.isEmpty())
+      throw new IllegalArgumentException("Option price map was empty");
+    if (optionDataBundle == null)
+      throw new IllegalArgumentException("Data bundle was null");
+    if (optionPrices.size() > 1) {
+      s_Log.info("Option price map had more than one entry: using the first pair to imply volatility");
+    }
     final Map.Entry<EuropeanVanillaOptionDefinition, Double> entry = optionPrices.entrySet().iterator().next();
     final Double price = entry.getValue();
     final Function1D<StandardOptionDataBundle, Double> pricingFunction = _bsm.getPricingFunction(entry.getKey());
@@ -50,9 +63,9 @@ public class BlackScholesMertonImpliedVolatilitySurfaceModel implements Volatili
   private class MyBisectionSingleRootFinder implements SingleRootFinder<StandardOptionDataBundle, Double, Double> {
     private final MyMutableStandardOptionDataBundle _data;
     private final double _price;
-    private static final double _accuracy = 1e-9;
+    private static final double _accuracy = 1e-12;
     private static final double ZERO = 1e-16;
-    private static final int MAX_ATTEMPTS = 1000;
+    private static final int MAX_ATTEMPTS = 10000;
 
     public MyBisectionSingleRootFinder(final StandardOptionDataBundle data, final double price) {
       _data = new MyMutableStandardOptionDataBundle(data);
@@ -81,7 +94,7 @@ public class BlackScholesMertonImpliedVolatilitySurfaceModel implements Volatili
         dVol *= 0.5;
         midVol = rootVol + dVol;
         _data.setVolatility(midVol);
-        highPrice = function.evaluate(_data);
+        highPrice = function.evaluate(_data) - _price;
         if (highPrice <= 0) {
           rootVol = midVol;
         }
--- tests/unit/com/opengamma/financial/model/volatility/surface/BlackScholesMertonImpliedVolatilitySurfaceModelTest.java
+++ tests/unit/com/opengamma/financial/model/volatility/surface/BlackScholesMertonImpliedVolatilitySurfaceModelTest.java
@@ -5,10 +5,77 @@
  */
 package com.opengamma.financial.model.volatility.surface;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Collections;
+
+import javax.time.calendar.ZonedDateTime;
+
+import org.junit.Test;
+
+import com.opengamma.financial.model.interestrate.curve.ConstantInterestRateDiscountCurve;
+import com.opengamma.financial.model.interestrate.curve.DiscountCurve;
+import com.opengamma.financial.model.option.definition.EuropeanVanillaOptionDefinition;
+import com.opengamma.financial.model.option.definition.StandardOptionDataBundle;
+import com.opengamma.financial.model.option.pricing.analytic.AnalyticOptionModel;
+import com.opengamma.financial.model.option.pricing.analytic.BlackScholesMertonModel;
+import com.opengamma.util.time.DateUtil;
+import com.opengamma.util.time.Expiry;
+
 /**
- *
+ * 
  * @author emcleod
  */
 public class BlackScholesMertonImpliedVolatilitySurfaceModelTest {
+  private static final VolatilitySurfaceModel<EuropeanVanillaOptionDefinition, StandardOptionDataBundle> MODEL = new BlackScholesMertonImpliedVolatilitySurfaceModel();
+  private static final AnalyticOptionModel<EuropeanVanillaOptionDefinition, StandardOptionDataBundle> BSM = new BlackScholesMertonModel();
+  private static final ZonedDateTime DATE = DateUtil.getUTCDate(2009, 1, 1);
+  private static final double EPS = 1e-3;
 
+  @Test
+  public void testInputs() {
+    try {
+      MODEL.getSurface(null, null);
+      fail();
+    } catch (final IllegalArgumentException e) {
+      // Expected
+    }
+    try {
+      MODEL.getSurface(Collections.<EuropeanVanillaOptionDefinition, Double> emptyMap(), null);
+      fail();
+    } catch (final IllegalArgumentException e) {
+      // Expected
+    }
+    try {
+      MODEL.getSurface(Collections.singletonMap(new EuropeanVanillaOptionDefinition(Math.random(), new Expiry(DATE), true), 2.3), null);
+      fail();
+    } catch (final IllegalArgumentException e) {
+      // Expected
+    }
+  }
+
+  @Test
+  public void test() {
+    boolean isCall;
+    double sigma, spot, strike, b, price;
+    Expiry expiry;
+    DiscountCurve curve;
+    EuropeanVanillaOptionDefinition definition;
+    StandardOptionDataBundle initialData, data;
+    for (int i = 0; i < 100; i++) {
+      expiry = new Expiry(DateUtil.getDateOffsetWithYearFraction(DATE, Math.random() * 2));
+      sigma = Math.random();
+      spot = 2 * Math.random() + 10;
+      strike = 2 * Math.random() + 10;
+      curve = new ConstantInterestRateDiscountCurve(Math.random() / 10);
+      b = Math.random() / 20;
+      isCall = Math.random() < 0.5 ? true : false;
+      definition = new EuropeanVanillaOptionDefinition(strike, expiry, isCall);
+      initialData = new StandardOptionDataBundle(curve, b, null, spot, DATE);
+      data = new StandardOptionDataBundle(curve, b, new ConstantVolatilitySurface(sigma), spot, DATE);
+      price = BSM.getPricingFunction(definition).evaluate(data);
+      assertEquals(sigma, MODEL.getSurface(Collections.singletonMap(definition, price), initialData).getVolatility(0., 0.), EPS);
+    }
+  }
 }
