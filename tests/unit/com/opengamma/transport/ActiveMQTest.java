d1de237dedde1a4701b82195a8b0eab477a5947d
Kirk Wylie <kirk@opengamma.com>
2009-10-29 15:47:12 +0000

    Made ivy file depend on ActiveMQ, and wrote a very simple test for the integration.
    This test class will ultimately be removed, but it was useful if nothing else
    as an example of how to setup ActiveMQ for a simple unit test.

---
+++ tests/unit/com/opengamma/transport/ActiveMQTest.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.transport;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.Topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Test;
+
+/**
+ * This test is only here to ensure that the Ivy configuration is sufficient
+ * to launch an in-memory ActiveMQ connection.
+ *
+ * @author kirk
+ */
+public class ActiveMQTest {
+
+  @Test
+  public void testVMBroker() throws Exception {
+    ConnectionFactory cf = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+    Connection conn = cf.createConnection();
+    Session session = conn.createSession(false, Session.DUPS_OK_ACKNOWLEDGE);
+    Topic t = session.createTopic("fibble");
+    MessageProducer producer = session.createProducer(t);
+    Message message = session.createMessage();
+    producer.send(message);
+    conn.close();
+  }
+}
