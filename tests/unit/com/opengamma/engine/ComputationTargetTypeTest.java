01c672abe4baa0fa92284c01206b95815a1a77cc
Kirk Wylie <kirk@opengamma.com>
2009-11-02 15:39:50 +0000

    Added unit test for the static methods on ComputationTargetType.

---
+++ tests/unit/com/opengamma/engine/ComputationTargetTypeTest.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.engine;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+
+import org.junit.Test;
+
+import com.opengamma.engine.position.Position;
+import com.opengamma.engine.position.PositionBean;
+import com.opengamma.engine.security.DefaultSecurity;
+import com.opengamma.engine.security.Security;
+import com.opengamma.engine.security.SecurityKeyImpl;
+
+/**
+ * 
+ *
+ * @author kirk
+ */
+public class ComputationTargetTypeTest {
+  private static final Position POSITION = new PositionBean(new BigDecimal(1), new SecurityKeyImpl());
+  private static final Security SECURITY = new DefaultSecurity();
+
+  @Test
+  public void determine() {
+    assertEquals(ComputationTargetType.POSITION, ComputationTargetType.determineFromTarget(POSITION));
+    assertEquals(ComputationTargetType.SECURITY, ComputationTargetType.determineFromTarget(SECURITY));
+    assertEquals(ComputationTargetType.PRIMITIVE, ComputationTargetType.determineFromTarget(null));
+    assertEquals(ComputationTargetType.MULTIPLE_POSITIONS, ComputationTargetType.determineFromTarget(Collections.EMPTY_LIST));
+    assertNull(ComputationTargetType.determineFromTarget("Kirk Wylie"));
+  }
+  
+  @Test
+  public void compatible() {
+    assertTrue(ComputationTargetType.isCompatible(ComputationTargetType.POSITION, POSITION));
+    assertTrue(ComputationTargetType.isCompatible(ComputationTargetType.SECURITY, SECURITY));
+    assertTrue(ComputationTargetType.isCompatible(ComputationTargetType.PRIMITIVE, null));
+    assertTrue(ComputationTargetType.isCompatible(ComputationTargetType.MULTIPLE_POSITIONS, Collections.EMPTY_LIST));
+  }
+  
+  @Test
+  public void notCompatible() {
+    assertFalse(ComputationTargetType.isCompatible(ComputationTargetType.POSITION, SECURITY));
+    assertFalse(ComputationTargetType.isCompatible(ComputationTargetType.SECURITY, POSITION));
+    assertFalse(ComputationTargetType.isCompatible(ComputationTargetType.PRIMITIVE, SECURITY));
+    assertFalse(ComputationTargetType.isCompatible(ComputationTargetType.MULTIPLE_POSITIONS, POSITION));
+  }
+}
