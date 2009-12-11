bba79e3c744ce2507ee0adb8c2d0d574c2fa4cdc
Elaine McLeod <emcleod@emcleodthinkpad.(none)>
2009-12-11 12:40:38 +0000

    A class to identify the order of a MA(q) series.
    ACF(n) is statistically equal to zero for n > q.

--- tests/unit/com/opengamma/financial/timeseries/analysis/MovingAverageTimeSeriesOrderIdentifierTest.java
+++ tests/unit/com/opengamma/financial/timeseries/analysis/MovingAverageTimeSeriesOrderIdentifierTest.java
@@ -17,7 +17,7 @@ import javax.time.calendar.ZonedDateTime;
 
 import org.junit.Test;
 
-import com.opengamma.financial.timeseries.model.AutoregressiveTimeSeriesModel;
+import com.opengamma.financial.timeseries.model.MovingAverageTimeSeriesModel;
 import com.opengamma.math.statistics.distribution.NormalProbabilityDistribution;
 import com.opengamma.math.statistics.distribution.ProbabilityDistribution;
 import com.opengamma.timeseries.ArrayDoubleTimeSeries;
@@ -27,17 +27,16 @@ import com.opengamma.timeseries.DoubleTimeSeries;
  * 
  * @author emcleod
  */
-public class AutoregressiveTimeSeriesOrderIdentifierTest {
-  private static final AutoregressiveTimeSeriesPACFOrderIdentifier PACF_IDENTIFIER = new AutoregressiveTimeSeriesPACFOrderIdentifier(10, 0.01);
-  private static final AutoregressiveTimeSeriesModel AR_MODEL = new AutoregressiveTimeSeriesModel(new NormalProbabilityDistribution(0, 1));
+public class MovingAverageTimeSeriesOrderIdentifierTest {
+  private static final MovingAverageTimeSeriesOrderIdentifier MA_IDENTIFIER = new MovingAverageTimeSeriesOrderIdentifier(10, 0.01);
+  private static final MovingAverageTimeSeriesModel MA_MODEL = new MovingAverageTimeSeriesModel(new NormalProbabilityDistribution(0, 1));
   private static final DoubleTimeSeries RANDOM;
-  private static final DoubleTimeSeries AR3;
-  private static final DoubleTimeSeries AR5;
+  private static final DoubleTimeSeries MA3;
 
   static {
     final List<ZonedDateTime> dates = new ArrayList<ZonedDateTime>();
     final List<Double> random = new ArrayList<Double>();
-    final ProbabilityDistribution<Double> normal = new NormalProbabilityDistribution(2, 1);
+    final ProbabilityDistribution<Double> normal = new NormalProbabilityDistribution(0, 1);
     final int n = 20000;
     for (int i = 0; i < n; i++) {
       dates.add(ZonedDateTime.fromInstant(Instant.instant(i), TimeZone.UTC));
@@ -49,50 +48,48 @@ public class AutoregressiveTimeSeriesOrderIdentifierTest {
     for (int i = 1; i <= order; i++) {
       coeffs[i] = 1. / (i + 5);
     }
-    AR3 = AR_MODEL.getSeries(coeffs, order, dates);
-    AR5 = AR_MODEL.getSeries(new Double[] { coeffs[0], coeffs[1], coeffs[2], 0., 0., 0.1 }, 5, dates);
+    MA3 = MA_MODEL.getSeries(coeffs, order, dates);
     RANDOM = new ArrayDoubleTimeSeries(dates, random);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testBadOrder() {
-    new AutoregressiveTimeSeriesPACFOrderIdentifier(-10, 0.05);
+    new MovingAverageTimeSeriesOrderIdentifier(-10, 0.05);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testNegativeLevel() {
-    new AutoregressiveTimeSeriesPACFOrderIdentifier(20, -0.1);
+    new MovingAverageTimeSeriesOrderIdentifier(20, -0.1);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testHighLevel() {
-    new AutoregressiveTimeSeriesPACFOrderIdentifier(20, 1.2);
+    new MovingAverageTimeSeriesOrderIdentifier(20, 1.2);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testNullTS() {
-    PACF_IDENTIFIER.getOrder(null);
+    MA_IDENTIFIER.getOrder(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testEmptyTS() {
-    PACF_IDENTIFIER.getOrder(ArrayDoubleTimeSeries.EMPTY_SERIES);
+    MA_IDENTIFIER.getOrder(ArrayDoubleTimeSeries.EMPTY_SERIES);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testInsufficientData() {
-    PACF_IDENTIFIER.getOrder(new ArrayDoubleTimeSeries(Arrays.asList(ZonedDateTime.fromInstant(Instant.instant(1), TimeZone.UTC), ZonedDateTime.fromInstant(Instant.instant(2),
+    MA_IDENTIFIER.getOrder(new ArrayDoubleTimeSeries(Arrays.asList(ZonedDateTime.fromInstant(Instant.instant(1), TimeZone.UTC), ZonedDateTime.fromInstant(Instant.instant(2),
         TimeZone.UTC)), Arrays.asList(new Double[] { 0.1, 0.2 })));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testRandomTS() {
-    PACF_IDENTIFIER.getOrder(RANDOM);
+    MA_IDENTIFIER.getOrder(RANDOM);
   }
 
   @Test
   public void test() {
-    assertEquals(PACF_IDENTIFIER.getOrder(AR3), 3);
-    assertEquals(PACF_IDENTIFIER.getOrder(AR5), 5);
+    assertEquals(MA_IDENTIFIER.getOrder(MA3), 3);
   }
 }
