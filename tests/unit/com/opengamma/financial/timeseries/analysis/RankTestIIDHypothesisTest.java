5a7e0b79548469a4f6c0053246600af48798d926
Elaine McLeod <emcleod@emcleodthinkpad.(none)>
2009-12-11 11:16:37 +0000

    Added a rank test for IID time series.
    Useful for detecting linear trends in data, but isn't the most powerful test in other situations.

---
+++ tests/unit/com/opengamma/financial/timeseries/analysis/RankTestIIDHypothesisTest.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.timeseries.analysis;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author emcleod
+ */
+public class RankTestIIDHypothesisTest extends IIDHypothesisTest {
+  private static final IIDHypothesis RANK_TEST = new RankTestIIDHypothesis(0.05);
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNegativeLevel() {
+    new RankTestIIDHypothesis(-0.1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testHighLevel() {
+    new RankTestIIDHypothesis(1.5);
+  }
+
+  @Test
+  public void test() {
+    super.testNullTS(RANK_TEST);
+    super.testEmptyTS(RANK_TEST);
+    assertTrue(RANK_TEST.evaluate(RANDOM));
+    assertTrue(RANK_TEST.evaluate(SIGNAL));
+    assertFalse(RANK_TEST.evaluate(INCREASING));
+  }
+}
