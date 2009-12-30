68a9e88e10b426fbee935fe79df3e96659192a3a
Kirk Wylie <kirk@opengamma.com>
2009-12-30 12:17:45 +0000

    Added support for NewFunction* to HardCodedBSMEquityOptionVolatilitySurfaceFunction.

--- src/com/opengamma/financial/analytics/HardCodedBSMEquityOptionVolatilitySurfaceFunction.java
+++ src/com/opengamma/financial/analytics/HardCodedBSMEquityOptionVolatilitySurfaceFunction.java
@@ -9,8 +9,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.time.calendar.Clock;
 import javax.time.calendar.TimeZone;
@@ -21,9 +23,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.opengamma.OpenGammaRuntimeException;
+import com.opengamma.engine.ComputationTarget;
+import com.opengamma.engine.ComputationTargetType;
 import com.opengamma.engine.function.AbstractSecurityFunction;
 import com.opengamma.engine.function.FunctionExecutionContext;
 import com.opengamma.engine.function.FunctionInputs;
+import com.opengamma.engine.function.NewFunctionDefinition;
+import com.opengamma.engine.function.NewFunctionInputs;
+import com.opengamma.engine.function.NewFunctionInvoker;
 import com.opengamma.engine.function.SecurityFunctionDefinition;
 import com.opengamma.engine.function.SecurityFunctionInvoker;
 import com.opengamma.engine.security.Security;
@@ -31,6 +38,10 @@ import com.opengamma.engine.value.AnalyticValueDefinition;
 import com.opengamma.engine.value.ComputedValue;
 import com.opengamma.engine.value.MarketDataAnalyticValueDefinitionFactory;
 import com.opengamma.engine.value.MarketDataComputedValue;
+import com.opengamma.engine.value.NewComputedValue;
+import com.opengamma.engine.value.ValueRequirement;
+import com.opengamma.engine.value.ValueRequirementNames;
+import com.opengamma.engine.value.ValueSpecification;
 import com.opengamma.financial.model.interestrate.curve.DiscountCurve;
 import com.opengamma.financial.model.option.definition.EuropeanVanillaOptionDefinition;
 import com.opengamma.financial.model.option.definition.OptionDefinition;
@@ -56,7 +67,8 @@ import com.opengamma.util.time.Expiry;
  */
 public class HardCodedBSMEquityOptionVolatilitySurfaceFunction
 extends AbstractSecurityFunction
-implements SecurityFunctionDefinition, SecurityFunctionInvoker {
+implements SecurityFunctionDefinition, SecurityFunctionInvoker,
+NewFunctionDefinition, NewFunctionInvoker {
   private static final Logger s_logger = LoggerFactory.getLogger(HardCodedBSMEquityOptionVolatilitySurfaceFunction.class);
   public static final String PRICE_FIELD_NAME = "PRICE";
   
@@ -182,4 +194,103 @@ implements SecurityFunctionDefinition, SecurityFunctionInvoker {
   public boolean isApplicableTo(String securityType) {
     return true;
   }
+
+  // NEW METHODS:
+  @Override
+  public boolean canApplyTo(ComputationTarget target) {
+    if(target.getType() != ComputationTargetType.SECURITY) {
+      return false;
+    }
+    if(target.getSecurity() instanceof EquityOptionSecurity) {
+      return true;
+    }
+    EquityOptionSecurity equityOptionSec = (EquityOptionSecurity) target.getSecurity();
+    Boolean canApply = equityOptionSec.accept(new OptionVisitor<Boolean>() {
+      @Override
+      public Boolean visitAmericanVanillaOption(AmericanVanillaOption option) {
+        return true;
+      }
+
+      @Override
+      public Boolean visitEuropeanVanillaOption(EuropeanVanillaOption option) {
+        return true;
+      }
+
+      @Override
+      public Boolean visitPoweredOption(PoweredOption option) {
+        return true;
+      }
+      
+    });
+    return canApply;
+  }
+
+  @Override
+  public Set<ValueRequirement> getRequirements(ComputationTarget target) {
+    if(!canApplyTo(target)) {
+      return null;
+    }
+    final EquityOptionSecurity equityOptionSec = (EquityOptionSecurity)target.getSecurity();
+    ValueRequirement equityOptionMarketDataReq = new ValueRequirement(ValueRequirementNames.MARKET_DATA_HEADER, ComputationTargetType.SECURITY, equityOptionSec.getIdentityKey());
+    ValueRequirement underlyingMarketDataReq = new ValueRequirement(ValueRequirementNames.MARKET_DATA_HEADER, ComputationTargetType.SECURITY, equityOptionSec.getUnderlyingIdentityKey());
+    ValueRequirement discountCurveReq = new ValueRequirement(ValueRequirementNames.DISCOUNT_CURVE, ComputationTargetType.PRIMITIVE, equityOptionSec.getCurrency().getISOCode());
+    final Set<ValueRequirement> optionRequirements = new HashSet<ValueRequirement>();
+    optionRequirements.add(equityOptionMarketDataReq);
+    optionRequirements.add(underlyingMarketDataReq);
+    optionRequirements.add(discountCurveReq);
+    // Since we check the state in canApply, no need to have a visitor. 
+    return optionRequirements;
+  }
+  
+  @Override
+  public Set<ValueSpecification> getResults(ComputationTarget target) {
+    if(!canApplyTo(target)) {
+      return null;
+    }
+    return Collections.singleton(createResultSpecification(target.getSecurity()));
+  }
+
+  @Override
+  public Set<NewComputedValue> execute(
+      FunctionExecutionContext executionContext,
+      NewFunctionInputs inputs,
+      ComputationTarget target) {
+    final ZonedDateTime today = Clock.system(TimeZone.UTC).zonedDateTime();
+    final EquityOptionSecurity equityOptionSec = (EquityOptionSecurity)target.getSecurity();
+    
+    // Get inputs:
+    ValueRequirement equityOptionMarketDataReq = new ValueRequirement(ValueRequirementNames.MARKET_DATA_HEADER, ComputationTargetType.SECURITY, equityOptionSec.getIdentityKey());
+    ValueRequirement underlyingMarketDataReq = new ValueRequirement(ValueRequirementNames.MARKET_DATA_HEADER, ComputationTargetType.SECURITY, equityOptionSec.getUnderlyingIdentityKey());
+    ValueRequirement discountCurveReq = new ValueRequirement(ValueRequirementNames.DISCOUNT_CURVE, ComputationTargetType.PRIMITIVE, equityOptionSec.getCurrency().getISOCode());
+    
+    FudgeFieldContainer equityOptionMarketData = (FudgeFieldContainer)inputs.getValue(equityOptionMarketDataReq);
+    FudgeFieldContainer underlyingMarketData = (FudgeFieldContainer)inputs.getValue(underlyingMarketDataReq);
+    final DiscountCurve discountCurve = (DiscountCurve)inputs.getValue(discountCurveReq);
+    
+    final double optionPrice = equityOptionMarketData.getDouble(MarketDataComputedValue.INDICATIVE_VALUE_NAME);
+    final double spotPrice = underlyingMarketData.getDouble(MarketDataComputedValue.INDICATIVE_VALUE_NAME);
+    
+    // No need for a visitor since we check it in canApply.
+    
+    // Perform the calculation:
+    Expiry expiry = equityOptionSec.getExpiry();
+    double years = DateUtil.getDifferenceInYears(today, expiry.getExpiry().toInstant());
+    final double b = discountCurve.getInterestRate(years);
+    OptionDefinition europeanVanillaOptionDefinition = new EuropeanVanillaOptionDefinition(equityOptionSec.getStrike(), expiry, (equityOptionSec.getOptionType() == OptionType.CALL));
+    Map<OptionDefinition, Double> prices = new HashMap<OptionDefinition, Double>();
+    prices.put(europeanVanillaOptionDefinition, optionPrice);
+    VolatilitySurface volatilitySurface = _volatilitySurfaceModel.getSurface(prices, new StandardOptionDataBundle(discountCurve, b, null, spotPrice, today));
+    
+    // Package the result
+    ValueSpecification resultSpec = createResultSpecification(equityOptionSec);
+    NewComputedValue resultValue = new NewComputedValue(resultSpec, volatilitySurface);
+    return Collections.singleton(resultValue);
+  }
+  
+  protected static ValueSpecification createResultSpecification(Security security) {
+    ValueRequirement resultRequirement = new ValueRequirement(ValueRequirementNames.VOLATILITY_SURFACE, ComputationTargetType.SECURITY, security.getIdentityKey());
+    ValueSpecification resultSpec = new ValueSpecification(resultRequirement);
+    return resultSpec;
+  }
+
 }
