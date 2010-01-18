7e876e5261c8bde39c6a13ece41de991da852b9f
Elaine McLeod <emcleod@emcleodthinkpad.(none)>
2010-01-18 17:32:34 +0000

    Added a step interpolator.

---
+++ src/com/opengamma/math/interpolation/StepInterpolator1D.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.math.interpolation;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.opengamma.util.CompareUtils;
+
+/**
+ * @author emcleod
+ * 
+ */
+public class StepInterpolator1D extends Interpolator1D {
+  private final double _eps;
+
+  public StepInterpolator1D() {
+    _eps = 1e-12;
+  }
+
+  public StepInterpolator1D(final double eps) {
+    _eps = Math.abs(eps);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.opengamma.math.interpolation.Interpolator1D#interpolate(java.util.Map,
+   * java.lang.Double)
+   */
+  @Override
+  public InterpolationResult<Double> interpolate(final Map<Double, Double> data, final Double value) {
+    if (value == null)
+      throw new IllegalArgumentException("x value to be interpolated was null");
+    final TreeMap<Double, Double> sorted = initData(data);
+    if (value < sorted.firstKey() || CompareUtils.closeEquals(sorted.firstKey(), value, _eps))
+      return new InterpolationResult<Double>(sorted.firstEntry().getValue(), 0.);
+    if (value > sorted.lastKey() || CompareUtils.closeEquals(value, sorted.lastKey(), _eps))
+      return new InterpolationResult<Double>(sorted.lastEntry().getValue(), 0.);
+    if (CompareUtils.closeEquals(sorted.higherKey(value), value, _eps))
+      return new InterpolationResult<Double>(sorted.higherEntry(value).getValue(), 0.);
+    return new InterpolationResult<Double>(sorted.lowerEntry(value).getValue(), 0.);
+  }
+}
---
+++ tests/unit/com/opengamma/math/interpolation/StepInterpolator1DTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.math.interpolation;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * @author emcleod
+ * 
+ */
+public class StepInterpolator1DTest {
+  private static final Interpolator1D INTERPOLATOR = new StepInterpolator1D();
+  private static final Map<Double, Double> DATA = new HashMap<Double, Double>();
+  private static final double EPS = 1e-13;
+
+  static {
+    DATA.put(1., 4.5);
+    DATA.put(2., 4.3);
+    DATA.put(3., 6.7);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullData() {
+    INTERPOLATOR.interpolate(null, 2.);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullValue() {
+    INTERPOLATOR.interpolate(DATA, null);
+  }
+
+  @Test
+  public void test() {
+    double value = -1;
+    test(INTERPOLATOR.interpolate(DATA, value), 4.5);
+    value = 10;
+    test(INTERPOLATOR.interpolate(DATA, value), 6.7);
+    value = 1;
+    test(INTERPOLATOR.interpolate(DATA, value), 4.5);
+    value = 1.1;
+    test(INTERPOLATOR.interpolate(DATA, value), 4.5);
+    value = 2 - EPS * 10;
+    test(INTERPOLATOR.interpolate(DATA, value), 4.5);
+    value = 2 - EPS / 10;
+    test(INTERPOLATOR.interpolate(DATA, value), 4.3);
+  }
+
+  private void test(final InterpolationResult<Double> result, final double expected) {
+    assertEquals(result.getResult(), expected, EPS);
+  }
+}
