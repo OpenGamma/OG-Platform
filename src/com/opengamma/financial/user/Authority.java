9eeec3e63ed1791f91042be3919a308347eff2c7
Pietari Laurila <pietari@opengamma.com>
2010-01-19 16:34:56 +0000

    First commit

---
+++ src/com/opengamma/financial/user/Authority.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.user;
+
+/**
+ * An <code>Authority</code> represents a permission granted to users.
+ * <p>
+ * Permissions can include viewing a portfolio or a class of portfolios, modifying a portfolio,
+ * viewing market data, etc.
+ * <p> 
+ * Permissions can be regular expressions, for example <code>/MarketData/Bloomberg/&#42;/View</code>, which would
+ * grant its holders the right to view any Bloomberg market data. See {@link PathMatcher}.
+ * <p>
+ * Note that <code>Authorities</code> are technically granted to {@link UserGroup}. Each 
+ * {@link User} belongs to a number of <code>UserGroups</code>. This reduces the need to modify individual
+ * permissions when users move within a company.   
+ * 
+ * @author pietari
+ */
+public class Authority {
+
+  private Long _id;
+  
+  /** A regular expression in Ant format **/
+  private String _authority;
+
+  protected Authority(String authority) {
+    _authority = authority;
+  }
+  
+  protected Authority() {
+  }
+
+  public Long getId() {
+    return _id;
+  }
+
+  public void setId(Long id) {
+    _id = id;
+  }
+
+  public String getAuthority() {
+    return _authority;
+  }
+
+  public void setAuthority(String authority) {
+    this._authority = authority;
+  }
+  
+  /**
+   * Returns whether this <code>Authority</code> can be used to grant the requested permission.
+   * @param requestedPermission The requested permission, for example /MarketData/Bloomberg/AAPL/View  
+   * @return Whether the regular expression pattern stored in this <code>Authority</code>, 
+   * for example <code>/MarketData/Bloomberg/&#42;/View</code>, matches the requested permission
+   */
+  public boolean matches(String requestedPermission) {
+    return PathMatcher.matches(requestedPermission, _authority);
+  }  
+  
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result
+        + ((_authority == null) ? 0 : _authority.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Authority other = (Authority) obj;
+    if (_authority == null) {
+      if (other._authority != null)
+        return false;
+    } else if (!_authority.equals(other._authority))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return _authority;
+  }
+
+}
---
+++ src/com/opengamma/financial/user/UserGroup.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.user;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * 
+ * @author pietari
+ */
+public class UserGroup {
+
+  private Long _id;
+  private String _name;
+  private Set<User> _users = new HashSet<User>();
+  private Set<Authority> _authorities = new HashSet<Authority>();
+
+  protected UserGroup() {
+  }
+  
+  public Long getId() {
+    return _id;
+  }
+
+  public void setId(Long id) {
+    _id = id;
+  }
+
+  public String getName() {
+    return _name;
+  }
+
+  public void setName(String name) {
+    this._name = name;
+  }
+
+  public Set<User> getUsers() {
+    return _users;
+  }
+
+  public void setUsers(Set<User> users) {
+    this._users = users;
+  }
+
+  public Set<Authority> getAuthorities() {
+    return _authorities;
+  }
+
+  public void setAuthorities(Set<Authority> authorities) {
+    this._authorities = authorities;
+  }
+  
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((_name == null) ? 0 : _name.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    UserGroup other = (UserGroup) obj;
+    if (_name == null) {
+      if (other._name != null)
+        return false;
+    } else if (!_name.equals(other._name))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return _name;
+  }
+
+}
