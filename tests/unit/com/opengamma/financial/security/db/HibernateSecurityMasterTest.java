ee0b6464db40df26732366a3ad34667b21c06ec1
Pietari Laurila <pietari@opengamma.com>
2010-01-26 13:13:24 +0000

    Support for PostgreSQL

--- tests/unit/com/opengamma/financial/security/db/HibernateSecurityMasterTest.java
+++ tests/unit/com/opengamma/financial/security/db/HibernateSecurityMasterTest.java
@@ -1,30 +1,20 @@
 package com.opengamma.financial.security.db;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Properties;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.commons.lang.builder.ToStringBuilder;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.orm.hibernate3.HibernateCallback;
 
 import com.opengamma.engine.security.Security;
