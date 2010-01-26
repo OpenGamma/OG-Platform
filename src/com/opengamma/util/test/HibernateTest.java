adb92309658c56243c17a115aba44e3c298473b9
Pietari Laurila <pietari@opengamma.com>
2010-01-26 14:39:56 +0000

    Need for Spring context file removed

--- src/com/opengamma/util/test/HibernateTest.java
+++ src/com/opengamma/util/test/HibernateTest.java
@@ -1,6 +1,5 @@
 package com.opengamma.util.test;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Connection;
@@ -9,6 +8,9 @@ import java.util.Properties;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.lang.builder.ToStringBuilder;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.slf4j.Logger;
@@ -17,14 +19,22 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public abstract class HibernateTest {
-  private static final Logger s_logger = LoggerFactory.getLogger(HibernateTest.class);
+  private static final Logger s_logger = LoggerFactory
+      .getLogger(HibernateTest.class);
   private static final String PROPS_FILE_NAME = "tests.properties";
-  protected ApplicationContext _context;
+  protected SessionFactory _sessionFactory;
   private static Properties _props;
-  private static String _jdbcDriverClass;
   private static boolean _isDerby;
   private static int testCount = 0;
   
+  public SessionFactory getSessionFactory() {
+    return _sessionFactory;
+  }
+
+  public void setSessionFactory(SessionFactory sessionFactory) {
+    _sessionFactory = sessionFactory;
+  }
+
   private static void recursiveDelete(File file) {
     if (file.isDirectory()) {
       File[] list = file.listFiles();
@@ -33,84 +43,91 @@ public abstract class HibernateTest {
           recursiveDelete(entry);
         }
         if (!entry.delete()) {
-          s_logger.warn("Could not delete file:"+file.getAbsolutePath());
-          //throw new OpenGammaRuntimeException("Could not delete file:"+entry.getAbsolutePath());
+          s_logger.warn("Could not delete file:" + file.getAbsolutePath());
+          // throw new
+          // OpenGammaRuntimeException("Could not delete file:"+entry.getAbsolutePath());
         } else {
-          System.err.println("Deleted "+entry.getAbsolutePath());
+          System.err.println("Deleted " + entry.getAbsolutePath());
         }
       }
     }
     if (!file.delete()) {
-      s_logger.warn("Could not delete file:"+file.getAbsolutePath());   
+      s_logger.warn("Could not delete file:" + file.getAbsolutePath());
     } else {
-      System.err.println("Deleted "+file.getAbsolutePath());
+      System.err.println("Deleted " + file.getAbsolutePath());
     }
-    
+
   }
-  
+
   private String getDBUrl(boolean createFrom) {
     if (_isDerby) {
       File blankDbDir = new File("blank");
-      String core = "/"+this.getClass().getSimpleName()+"-test-"+testCount;
+      String core = "/" + this.getClass().getSimpleName() + "-test-"
+          + testCount;
       if (createFrom) {
-        return core + ";createFrom="+blankDbDir.getAbsolutePath();
+        return core + ";createFrom=" + blankDbDir.getAbsolutePath();
       } else {
         return core;
       }
-    
+
     } else {
-      return "/OpenGammaTests"; 
+      return "/OpenGammaTests";
     }
   }
-  
+
   @BeforeClass
   public static void setUpClass() throws Exception {
     Properties props = new Properties();
     File file = new File(PROPS_FILE_NAME);
     System.err.println(file.getAbsoluteFile());
-    props.load(new FileInputStream(file)); 
+    props.load(new FileInputStream(file));
     _props = props;
-    
-    _jdbcDriverClass = ((String) _props.get("jdbc.driver.classname")).trim();
-    _isDerby = _jdbcDriverClass.equals("org.apache.derby.jdbc.EmbeddedDriver");
-    
+
+    String jdbcDriverClass = ((String) _props.get("jdbc.driver.classname")).trim();
+    _isDerby = jdbcDriverClass.equals("org.apache.derby.jdbc.EmbeddedDriver");
+
     if (_isDerby) {
       recursiveDelete(new File("derby-db"));
     }
-    
-    Class.forName(_jdbcDriverClass).newInstance(); // load driver.
+
+    Class.forName(jdbcDriverClass).newInstance(); // load driver.
   }
-  
-  public abstract String getConfigLocation();
-  
+
+  public abstract Class<?>[] getHibernateMappingClasses();
+
   @Before
   public void setUp() throws Exception {
     String createFromUrl = _props.getProperty("jdbc.url") + getDBUrl(true);
-    System.err.println("Connecting with data source URL "+createFromUrl);
-    
+    System.err.println("Connecting with data source URL " + createFromUrl);
+
     String user = _props.getProperty("jdbc.username");
-    String password =  _props.getProperty("jdbc.password");
+    String password = _props.getProperty("jdbc.password");
 
     if (_isDerby) {
-      Connection conn = DriverManager.getConnection(createFromUrl, user, password);
+      Connection conn = DriverManager.getConnection(createFromUrl, user,
+          password);
       // this will create a copy of the blank database, using
-      conn.close(); // that should do the copy...  we do it like this because I'm unsure if we can be sure the App Context will release the resources if we used that.
+      conn.close(); // that should do the copy... we do it like this because I'm
+                    // unsure if we can be sure the App Context will release the
+                    // resources if we used that.
       System.err.println("closed connection, starting App Context");
     }
-    
-    System.setProperty("jdbc.url", _props.getProperty("jdbc.url") + getDBUrl(false)); 
-    System.setProperty("jdbc.driver.classname", _props.getProperty("jdbc.driver.classname"));
-    System.setProperty("jdbc.username", _props.getProperty("jdbc.username"));
-    System.setProperty("jdbc.password", _props.getProperty("jdbc.password"));
-    System.setProperty("hibernate.dialect", _props.getProperty("hibernate.dialect"));
-    
-    // the idea is that this SYSTEM property (the others above are not system props) is picked up by the PropertyPlaceholderConfigurer during startup
-    // and injected into the 
-    _context = new ClassPathXmlApplicationContext(getConfigLocation());
 
-    BasicDataSource dataSource = (BasicDataSource) _context.getBean("myDataSource");
-    System.err.println(ToStringBuilder.reflectionToString(dataSource));
+    Configuration configuration = new Configuration();
+    configuration.setProperty(Environment.DRIVER, _props.getProperty("jdbc.driver.classname"));
+    configuration.setProperty(Environment.URL, _props.getProperty("jdbc.url") + getDBUrl(false));
+    configuration.setProperty(Environment.USER, _props.getProperty("jdbc.username"));
+    configuration.setProperty(Environment.DIALECT, _props.getProperty("hibernate.dialect"));
+    configuration.setProperty(Environment.SHOW_SQL, "true");
+    configuration.setProperty(Environment.HBM2DDL_AUTO, "create-drop");
     
+    for (Class<?> clazz : getHibernateMappingClasses()) {
+      configuration.addClass(clazz);
+    }
+
+    SessionFactory sessionFactory = configuration.buildSessionFactory();
+    setSessionFactory(sessionFactory);
+
     testCount++;
   }
 }
