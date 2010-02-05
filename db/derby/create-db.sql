192768bbcbf59202292a3dcfd5a174cf2b7addf4
Pietari Laurila <pietari@opengamma.com>
2010-02-05 17:15:40 +0000

    Improvements to SQL creation scripts

--- db/derby/create-db.sql
+++ db/derby/create-db.sql
@@ -6,13 +6,20 @@
     );
 
     create table domain_specific_identifier_associations (
-        domain_specific_indentifier_id bigint not null,
+        domain_specific_identifier_id bigint not null,
         security_discriminator varchar(255),
         security_id bigint,
         domain varchar(255) not null,
         identifier varchar(255) not null,
-        primary key (domain_specific_indentifier_id),
+        primary key (domain_specific_identifier_id),
         unique (domain, identifier)
+    );
+
+    create table exchanges (
+        exchange_id bigint not null,
+        name varchar(255) not null unique,
+        description varchar(255),
+        primary key (exchange_id)
     );
 
     create table equities (
@@ -26,25 +33,10 @@
         exchange_id bigint not null,
         companyName varchar(255) not null,
         currency_id bigint not null,
-        primary key (equities_id)
+        primary key (equities_id),
+	constraint fk_equities2currencies foreign key (currency_id) references currencies(currency_id),
+	constraint fk_equities2exchanges foreign key (exchange_id) references exchanges(exchange_id)
     );
-
-    create table exchanges (
-        exchange_id bigint not null,
-        name varchar(255) not null unique,
-        description varchar(255),
-        primary key (exchange_id)
-    );
-
-    alter table equities 
-        add constraint FK231BD223C42C694F 
-        foreign key (currency_id) 
-        references currencies;
-
-    alter table equities 
-        add constraint FK231BD22351E67F0F 
-        foreign key (exchange_id) 
-        references exchanges;
 
     create table hibernate_sequence (
          next_val bigint 
