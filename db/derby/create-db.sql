55db635aa96ac2fa09407eaf14f465cc90dc4dac
Pietari Laurila <pietari@opengamma.com>
2010-02-08 10:47:16 +0000

    Table names changed to singular

--- db/derby/create-db.sql
+++ db/derby/create-db.sql
@@ -11,27 +11,27 @@
         primary key (id)
     );
 
-    create table authorities (
+    create table authority (
         id bigint not null,
         authority varchar(255) not null unique,
         primary key (id)
     );
 
-    create table user_groups (
+    create table user_group (
         id bigint not null,
         name varchar(255) not null unique,
         primary key (id)
     );
 
-    create table user_groups_to_authorities (
+    create table user_group_to_authority (
         group_id bigint not null,
         authority_id bigint not null,
         primary key (group_id, authority_id),
-        constraint fk_user_groups_to_authorities2user_groups foreign key (group_id) references user_groups(id) ON DELETE CASCADE,
-        constraint fk_user_groups_to_authorities2authorities foreign key (authority_id) references authorities(id) ON DELETE CASCADE
+        constraint fk_user_group_to_authority2user_group foreign key (group_id) references user_group(id) ON DELETE CASCADE,
+        constraint fk_user_group_to_authority2authority foreign key (authority_id) references authority(id) ON DELETE CASCADE
     );
 
-    create table users (
+    create table og_user (
         id bigint not null,
         username varchar(255) not null unique,
         password varchar(255) not null,
@@ -39,12 +39,12 @@
         primary key (id)
     );
 
-    create table users_to_user_groups (
+    create table user_to_user_group (
         user_group_id bigint not null,
         user_id bigint not null,
         primary key (user_group_id, user_id),
-        constraint fk_users_to_user_groups2users foreign key (user_id) references users(id) ON DELETE CASCADE,
-        constraint fk_users_to_user_groups2user_groups foreign key (user_group_id) references user_groups(id) ON DELETE CASCADE
+        constraint fk_user_to_user_group2og_user foreign key (user_id) references og_user(id) ON DELETE CASCADE,
+        constraint fk_user_to_user_group2user_group foreign key (user_group_id) references user_group(id) ON DELETE CASCADE
     );
 
 
