3ac7359d260c8643cf058b0a37a7938719f42432
Kirk Wylie <kirk@opengamma.com>
2010-03-02 15:17:44 +0000

    [ENG-34] Wrote first drafts of ViewClient and ViewProcessorClient.

---
+++ src/com/opengamma/engine/view/client/ViewProcessorClient.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.engine.view.client;
+
+import java.util.Set;
+
+/**
+ * 
+ *
+ * @author kirk
+ */
+public interface ViewProcessorClient {
+  
+  /**
+   * True iff this view processor is capable of supporting live computation of views.
+   * 
+   * @return
+   */
+  boolean isLiveComputationSupported();
+  
+  /**
+   * True iff this view processor allows one-off view computation jobs to be provided.
+   * 
+   * @return
+   */
+  boolean isOneOffComputationSupported();
+  
+  Set<String> getAvailableViewNames();
+  
+  Set<String> getLiveComputingViewNames();
+  
+  ViewClient getView(String viewName);
+  
+}
