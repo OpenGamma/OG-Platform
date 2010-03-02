fabdefcd129c910d51e0ff9149393eeaa160d0df
Andrew Griffin <andrew@opengamma.com>
2010-03-02 09:59:15 +0000

    Try to fix bamboo error by repeating the JMS test on failure.

--- tests/unit/com/opengamma/transport/jms/JmsByteArrayTransportTest.java
+++ tests/unit/com/opengamma/transport/jms/JmsByteArrayTransportTest.java
@@ -18,16 +18,18 @@ import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 import com.opengamma.transport.ByteArrayRequestReceiver;
 import com.opengamma.transport.CollectingByteArrayMessageReceiver;
+import com.opengamma.util.test.ProbabilisticTestCase;
 
 /**
  * 
  *
  * @author kirk
  */
-public class JmsByteArrayTransportTest {
+public class JmsByteArrayTransportTest extends ProbabilisticTestCase {
 
   @Test
   public void topicConduit() throws Exception {
+    if (retry (2)) return;
     String topicName = "JmsByteArrayTransportTest-topicConduit-" + System.getProperty("user.name") + "-" + System.currentTimeMillis();
     ConnectionFactory cf = ActiveMQTestUtil.createTestConnectionFactory();
     JmsTemplate jmsTemplate = new JmsTemplate();
@@ -75,6 +77,7 @@ public class JmsByteArrayTransportTest {
   
   @Test
   public void requestConduit() throws Exception {
+    if (retry (2)) return;
     String topicName = "JmsByteArrayTransportTest-requestConduit-" + System.getProperty("user.name") + "-" + System.currentTimeMillis();
     ConnectionFactory cf = ActiveMQTestUtil.createTestConnectionFactory();
     JmsTemplate jmsTemplate = new JmsTemplate();
