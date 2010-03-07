e5e13db3a630f77fef71a0a45ecdc8b412b4dacb
Kirk Wylie <kirk@opengamma.com>
2010-03-07 15:28:51 +0000

    [ENG-43] Wrote a new form of ArgumentChecker specifically for injected inputs.

--- src/com/opengamma/util/ArgumentChecker.java
+++ src/com/opengamma/util/ArgumentChecker.java
@@ -19,5 +19,11 @@ public final class ArgumentChecker {
       throw new NullPointerException("Input parameter " + name + " must not be null.");
     }
   }
+  
+  public static void checkNotNullInjected(Object parameter, String name) throws NullPointerException {
+    if(parameter == null) {
+      throw new NullPointerException("Injected input parameter " + name + " must not be null.");
+    }
+  }
 
 }
