628aa50fdd34a10b88891f2f8809d0c264a5120c
Kirk Wylie <kirk@opengamma.com>
2010-03-07 12:10:32 +0000

    Argument checking and header comment. No code changes otherwise.

--- src/com/opengamma/financial/greeks/MultipleGreekResult.java
+++ src/com/opengamma/financial/greeks/MultipleGreekResult.java
@@ -1,14 +1,24 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
 package com.opengamma.financial.greeks;
 
 import java.util.Map;
 
+import com.opengamma.util.ArgumentChecker;
+
 public class MultipleGreekResult implements GreekResult<Map<String, Double>> {
 
-  private Map<String, Double> _result;
+  private final Map<String, Double> _result;
 
-  public MultipleGreekResult(Map<String, Double> result) {
+  public MultipleGreekResult(final Map<String, Double> result) {
+    ArgumentChecker.checkNotNull(result, "Result map");
+    // REVIEW kirk 2010-03-07 -- Is it okay to not take a copy of the result?
     _result = result;
   }
+
   @Override
   public Map<String, Double> getResult() {
     return _result;
