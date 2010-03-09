a7044c0a39128cfc25adbf8de2435fc7d61f7326
Jim Moores <jim.moores@gmail.com>
2010-03-09 13:47:39 +0000

    Changed constructors to UTC

--- tests/unit/com/opengamma/util/timeseries/zoneddatetime/ArrayDoubleTimeSeriesTest.java
+++ tests/unit/com/opengamma/util/timeseries/zoneddatetime/ArrayDoubleTimeSeriesTest.java
@@ -3,6 +3,7 @@
 
 import java.util.List;
 
+import javax.time.calendar.TimeZone;
 import javax.time.calendar.ZonedDateTime;
 
 public class ArrayDoubleTimeSeriesTest extends ZonedDateTimeDoubleTimeSeriesTest {
@@ -14,16 +15,16 @@ public class ArrayDoubleTimeSeriesTest extends ZonedDateTimeDoubleTimeSeriesTest
 
   @Override
   public ZonedDateTimeDoubleTimeSeries createTimeSeries(ZonedDateTime[] times, double[] values) {
-    return new ArrayZonedDateTimeDoubleTimeSeries(times, values);
+    return new ArrayZonedDateTimeDoubleTimeSeries(TimeZone.UTC, times, values);
   }
 
   @Override
   public ZonedDateTimeDoubleTimeSeries createTimeSeries(List<ZonedDateTime> times, List<Double> values) {
-    return new ArrayZonedDateTimeDoubleTimeSeries(times, values);
+    return new ArrayZonedDateTimeDoubleTimeSeries(TimeZone.UTC, times, values);
   }
 
   @Override
   public ZonedDateTimeDoubleTimeSeries createTimeSeries(ZonedDateTimeDoubleTimeSeries dts) {
-    return new ArrayZonedDateTimeDoubleTimeSeries(dts);
+    return new ArrayZonedDateTimeDoubleTimeSeries(TimeZone.UTC, dts);
   }
 }
