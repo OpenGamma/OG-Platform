96286dbf2988cf47c620e6d49f86d0d8a5480203
Elaine McLeod <elaine@opengamma.com>
2010-03-15 12:06:09 +0000

    Forgot to add this to last commit.

---
+++ src/com/opengamma/financial/pnl/Underlying.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.pnl;
+
+/**
+ * @author emcleod
+ * 
+ */
+public enum Underlying {
+  SPOT_PRICE, SPOT_VOLATILITY, INTEREST_RATE, COST_OF_CARRY, STRIKE, TIME, SPOT_VARIANCE
+}
