dbadae81109172b6212a24668b82c533b727b42e
Elaine McLeod <elaine@opengamma.com>
2010-03-15 12:08:35 +0000

    And these.

--- src/com/opengamma/financial/sensitivity/Sensitivity.java
+++ src/com/opengamma/financial/sensitivity/Sensitivity.java
@@ -1,5 +1,9 @@
 package com.opengamma.financial.sensitivity;
 
+import com.opengamma.financial.greeks.FirstOrder;
+import com.opengamma.financial.greeks.Order;
+import com.opengamma.financial.greeks.SecondOrder;
+import com.opengamma.financial.pnl.Underlying;
 
 public enum Sensitivity {
   VALUE_DELTA {
@@ -8,12 +12,22 @@ public enum Sensitivity {
     public <T> T accept(final SensitivityVisitor<T> visitor) {
       return visitor.visitValueDelta();
     }
+
+    @Override
+    public Order getOrder() {
+      return new FirstOrder(Underlying.SPOT_PRICE);
+    }
   },
   VALUE_GAMMA {
 
     @Override
     public <T> T accept(final SensitivityVisitor<T> visitor) {
       return visitor.visitValueGamma();
+    }
+
+    @Override
+    public Order getOrder() {
+      return new SecondOrder(Underlying.SPOT_PRICE);
     }
 
   },
@@ -24,12 +38,37 @@ public enum Sensitivity {
       return visitor.visitValueVega();
     }
 
+    @Override
+    public Order getOrder() {
+      return new FirstOrder(Underlying.SPOT_VOLATILITY);
+    }
+
   },
   VALUE_THETA {
 
     @Override
     public <T> T accept(final SensitivityVisitor<T> visitor) {
       return visitor.visitValueTheta();
+    }
+
+    @Override
+    public Order getOrder() {
+      return new FirstOrder(Underlying.TIME);
+    }
+
+  },
+  DV01 {
+
+    @Override
+    public <T> T accept(final SensitivityVisitor<T> visitor) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public Order getOrder() {
+      // TODO Auto-generated method stub
+      return null;
     }
 
   },
@@ -40,6 +79,12 @@ public enum Sensitivity {
       return visitor.visitPV01();
     }
 
+    @Override
+    public Order getOrder() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
   },
   CONVEXITY {
 
@@ -47,8 +92,16 @@ public enum Sensitivity {
     public <T> T accept(final SensitivityVisitor<T> visitor) {
       return visitor.visitConvexity();
     }
+
+    @Override
+    public Order getOrder() {
+      // TODO Auto-generated method stub
+      return null;
+    }
   };
 
   public abstract <T> T accept(SensitivityVisitor<T> visitor);
+
+  public abstract Order getOrder();
 
 }
