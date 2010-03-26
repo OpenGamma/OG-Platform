6ed0310687af3fb0a3bd068007aa294a2bd4b834
Elaine McLeod <elaine@opengamma.com>
2010-03-26 14:58:39 +0000

    Added put and call currencies.

--- src/com/opengamma/financial/security/option/FXOptionSecurity.java
+++ src/com/opengamma/financial/security/option/FXOptionSecurity.java
@@ -17,11 +17,23 @@ import com.opengamma.util.time.Expiry;
  */
 public class FXOptionSecurity extends OTCOptionSecurity {
   public static final String FX_OPTION_TYPE = "FX_OPTION";
+  private final Currency _putCurrency;
+  private final Currency _callCurrency;
 
-  public FXOptionSecurity(final OptionType optionType, final double strike, final Expiry expiry, final DomainSpecificIdentifier underlyingIdentityKey, final Currency currency,
-      final String counterparty) {
-    super(optionType, strike, expiry, underlyingIdentityKey, currency, counterparty);
+  public FXOptionSecurity(final OptionType optionType, final double strike, final Expiry expiry, final DomainSpecificIdentifier underlyingIdentityKey,
+      final Currency domesticCurrency, final String counterparty, final Currency putCurrency, final Currency callCurrency) {
+    super(optionType, strike, expiry, underlyingIdentityKey, domesticCurrency, counterparty);
     setSecurityType(FX_OPTION_TYPE);
+    _putCurrency = putCurrency;
+    _callCurrency = callCurrency;
+  }
+
+  public Currency getPutCurrency() {
+    return _putCurrency;
+  }
+
+  public Currency getCallCurrency() {
+    return _callCurrency;
   }
 
   @Override
