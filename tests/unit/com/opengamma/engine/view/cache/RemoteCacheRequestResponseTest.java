e35569f5ada91b63489fc59e6753f7814d625a42
Kirk Wylie <kirk@opengamma.com>
2010-03-30 16:16:22 +0100

    [ENG-22] Much more comprehensive request/response tests for ValueSpecification ID lookups.

--- tests/unit/com/opengamma/engine/view/cache/RemoteCacheRequestResponseTest.java
+++ tests/unit/com/opengamma/engine/view/cache/RemoteCacheRequestResponseTest.java
@@ -5,12 +5,23 @@
  */
 package com.opengamma.engine.view.cache;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.opengamma.engine.ComputationTargetSpecification;
 import com.opengamma.engine.ComputationTargetType;
@@ -27,6 +38,7 @@ import com.opengamma.transport.InMemoryRequestConduit;
  * @author kirk
  */
 public class RemoteCacheRequestResponseTest {
+  private static final Logger s_logger = LoggerFactory.getLogger(RemoteCacheRequestResponseTest.class);
   
   @Test(timeout=10000l)
   public void singleThreadSpecLookupDifferentIdentifierValues() {
@@ -43,5 +55,110 @@ public class RemoteCacheRequestResponseTest {
       seenIds.set((int) id);
     }
   }
+  
+  @Test(timeout=10000l)
+  public void singleThreadLookupDifferentIdentifierValuesRepeated() {
+    RemoteCacheServer server = new RemoteCacheServer();
+    FudgeRequestSender conduit = InMemoryRequestConduit.create(server);
+    RemoteCacheClient client = new RemoteCacheClient(conduit);
 
+    Map<String, Long> _idsByValueName = new HashMap<String, Long>();
+    for(int i = 0; i < 10; i++) {
+      String valueName = "Value" + i;
+      ValueSpecification valueSpec = new ValueSpecification(new ValueRequirement("Test Value", new ComputationTargetSpecification(ComputationTargetType.PRIMITIVE, new DomainSpecificIdentifier(new IdentificationDomain("Kirk"), valueName))));
+      long id = client.getValueSpecificationId(valueSpec);
+      _idsByValueName.put(valueName, id);
+    }
+
+    for(int i = 0; i < 10; i++) {
+      String valueName = "Value" + i;
+      ValueSpecification valueSpec = new ValueSpecification(new ValueRequirement("Test Value", new ComputationTargetSpecification(ComputationTargetType.PRIMITIVE, new DomainSpecificIdentifier(new IdentificationDomain("Kirk"), valueName))));
+      long id = client.getValueSpecificationId(valueSpec);
+      assertEquals(_idsByValueName.get(valueName), new Long(id));
+    }
+  }
+  
+  @Test(timeout=30000l)
+  public void multiThreadLookupDifferentIdentifierValuesRepeatedSharedClient() throws InterruptedException {
+    RemoteCacheServer server = new RemoteCacheServer();
+    FudgeRequestSender conduit = InMemoryRequestConduit.create(server);
+    final RemoteCacheClient client = new RemoteCacheClient(conduit);
+
+    final ConcurrentMap<String, Long> _idsByValueName = new ConcurrentHashMap<String, Long>();
+    final Random rand = new Random();
+    final AtomicBoolean failed = new AtomicBoolean(false);
+    List<Thread> threads = new ArrayList<Thread>();
+    for(int i = 0; i < 10; i++) {
+      Thread t = new Thread(new Runnable() {
+        @Override
+        public void run() {
+          try {
+            for(int j = 0; j < 1000; j++) {
+              int randomValue = rand.nextInt(100);
+              String valueName = "Value" + randomValue;
+              ValueSpecification valueSpec = new ValueSpecification(new ValueRequirement("Test Value", new ComputationTargetSpecification(ComputationTargetType.PRIMITIVE, new DomainSpecificIdentifier(new IdentificationDomain("Kirk"), valueName))));
+              long id = client.getValueSpecificationId(valueSpec);
+              Long previousValue = _idsByValueName.putIfAbsent(valueName, id);
+              if(previousValue != null) {
+                assertEquals(previousValue, new Long(id));
+              }
+            }
+          } catch (Exception e) {
+            s_logger.error("Failed", e);
+            failed.set(true);
+          }
+        }
+      });
+      threads.add(t);
+    }
+    for(Thread t : threads) {
+      t.start();
+    }
+    for(Thread t : threads) {
+      t.join();
+    }
+    assertFalse("One thread failed. Check logs.", failed.get());
+  }
+
+  @Test(timeout=30000l)
+  public void multiThreadLookupDifferentIdentifierValuesRepeatedDifferentClient() throws InterruptedException {
+    RemoteCacheServer server = new RemoteCacheServer();
+    final FudgeRequestSender conduit = InMemoryRequestConduit.create(server);
+
+    final ConcurrentMap<String, Long> _idsByValueName = new ConcurrentHashMap<String, Long>();
+    final Random rand = new Random();
+    final AtomicBoolean failed = new AtomicBoolean(false);
+    List<Thread> threads = new ArrayList<Thread>();
+    for(int i = 0; i < 10; i++) {
+      Thread t = new Thread(new Runnable() {
+        @Override
+        public void run() {
+          RemoteCacheClient client = new RemoteCacheClient(conduit);
+          try {
+            for(int j = 0; j < 1000; j++) {
+              int randomValue = rand.nextInt(100);
+              String valueName = "Value" + randomValue;
+              ValueSpecification valueSpec = new ValueSpecification(new ValueRequirement("Test Value", new ComputationTargetSpecification(ComputationTargetType.PRIMITIVE, new DomainSpecificIdentifier(new IdentificationDomain("Kirk"), valueName))));
+              long id = client.getValueSpecificationId(valueSpec);
+              Long previousValue = _idsByValueName.putIfAbsent(valueName, id);
+              if(previousValue != null) {
+                assertEquals(previousValue, new Long(id));
+              }
+            }
+          } catch (Exception e) {
+            s_logger.error("Failed", e);
+            failed.set(true);
+          }
+        }
+      });
+      threads.add(t);
+    }
+    for(Thread t : threads) {
+      t.start();
+    }
+    for(Thread t : threads) {
+      t.join();
+    }
+    assertFalse("One thread failed. Check logs.", failed.get());
+  }
 }
