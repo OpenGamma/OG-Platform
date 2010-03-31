0be0b49463dd127f58ed8b3319090d2f6914ece4
Elaine McLeod <elaine@opengamma.com>
2010-03-31 17:53:55 +0100

    Added equity pricing (price = share price from market data provider).

---
+++ src/com/opengamma/financial/analytics/model/equity/StandardEquityModelFunction.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.model.equity;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.fudgemsg.FudgeFieldContainer;
+
+import com.opengamma.engine.ComputationTarget;
+import com.opengamma.engine.ComputationTargetType;
+import com.opengamma.engine.function.AbstractFunction;
+import com.opengamma.engine.function.FunctionCompilationContext;
+import com.opengamma.engine.function.FunctionExecutionContext;
+import com.opengamma.engine.function.FunctionInputs;
+import com.opengamma.engine.function.FunctionInvoker;
+import com.opengamma.engine.value.ComputedValue;
+import com.opengamma.engine.value.MarketDataFieldNames;
+import com.opengamma.engine.value.ValueRequirement;
+import com.opengamma.engine.value.ValueRequirementNames;
+import com.opengamma.engine.value.ValueSpecification;
+import com.opengamma.financial.security.EquitySecurity;
+
+/**
+ * 
+ *
+ * @author emcleod
+ */
+public class StandardEquityModelFunction extends AbstractFunction implements FunctionInvoker {
+
+  @Override
+  public Set<ComputedValue> execute(final FunctionExecutionContext executionContext, final FunctionInputs inputs, final ComputationTarget target,
+      final Set<ValueRequirement> desiredValues) {
+    final EquitySecurity equity = (EquitySecurity) target.getSecurity();
+    final double price = (((FudgeFieldContainer) inputs.getValue(new ValueRequirement(ValueRequirementNames.MARKET_DATA_HEADER, ComputationTargetType.SECURITY, equity
+        .getIdentityKey())))).getDouble(MarketDataFieldNames.INDICATIVE_VALUE_NAME);
+    return Collections.<ComputedValue> singleton(new ComputedValue(new ValueSpecification(new ValueRequirement(ValueRequirementNames.FAIR_VALUE, ComputationTargetType.SECURITY,
+        equity.getIdentityKey())), price));
+  }
+
+  @Override
+  public boolean canApplyTo(final FunctionCompilationContext context, final ComputationTarget target) {
+    if (target.getType() != ComputationTargetType.SECURITY)
+      return false;
+    if (target.getSecurity() instanceof EquitySecurity)
+      return true;
+    return false;
+  }
+
+  @Override
+  public Set<ValueRequirement> getRequirements(final FunctionCompilationContext context, final ComputationTarget target) {
+    if (canApplyTo(context, target)) {
+      final EquitySecurity equity = (EquitySecurity) target.getSecurity();
+      final Set<ValueRequirement> requirements = new HashSet<ValueRequirement>();
+      requirements.add(new ValueRequirement(ValueRequirementNames.MARKET_DATA_HEADER, ComputationTargetType.SECURITY, equity.getIdentityKey()));
+      // TODO need to consider fx here?
+      return requirements;
+    }
+    return null;
+  }
+
+  @Override
+  public Set<ValueSpecification> getResults(final FunctionCompilationContext context, final ComputationTarget target) {
+    if (canApplyTo(context, target)) {
+      final EquitySecurity equity = (EquitySecurity) target.getSecurity();
+      return Collections.<ValueSpecification> singleton(new ValueSpecification(new ValueRequirement(ValueRequirementNames.FAIR_VALUE, ComputationTargetType.SECURITY, equity
+          .getIdentityKey())));
+    }
+    return null;
+  }
+
+  @Override
+  public String getShortName() {
+    return "StandardEquityModel";
+  }
+
+  @Override
+  public ComputationTargetType getTargetType() {
+    return ComputationTargetType.SECURITY;
+  }
+
+}
