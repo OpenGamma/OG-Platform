df58f9bb4012be013c5b8033471fee88d62c4ca3
Elaine McLeod <elaine@opengamma.com>
2010-04-06 18:10:46 +0100

    Added an effective convexity calculator.

---
+++ src/com/opengamma/financial/model/cashflow/EffectiveConvexityCalculator.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.model.cashflow;
+
+import com.opengamma.financial.model.bond.BondYieldCalculator;
+import com.opengamma.financial.model.interestrate.InterestRateModel;
+import com.opengamma.financial.model.interestrate.curve.ConstantInterestRateModel;
+import com.opengamma.util.timeseries.DoubleTimeSeries;
+
+/**
+ * @author emcleod
+ *
+ */
+public class EffectiveConvexityCalculator {
+  private final BondYieldCalculator _yield = new BondYieldCalculator();
+  private final double _eps = 1e-3;
+
+  public double calculate(final DoubleTimeSeries<Long> cashFlows, final double price, final Long date, final PresentValueCalculator pvCalculator) {
+    if (cashFlows == null)
+      throw new IllegalArgumentException("Cash flow time series was null");
+    if (cashFlows.isEmpty())
+      throw new IllegalArgumentException("Cash flow time series was empty");
+    if (price <= 0)
+      throw new IllegalArgumentException("Price must be positive");
+    if (date == null)
+      throw new IllegalArgumentException("Date was null");
+    if (pvCalculator == null)
+      throw new IllegalArgumentException("Present value calculator was null");
+    final double yield = _yield.calculate(cashFlows, price, date, pvCalculator);
+    final InterestRateModel<Double> yCurve = new ConstantInterestRateModel(yield);
+    final InterestRateModel<Double> yCurveUp = new ConstantInterestRateModel(yield + _eps);
+    final InterestRateModel<Double> yCurveDown = new ConstantInterestRateModel(yield - _eps);
+    final double pv = pvCalculator.calculate(cashFlows, yCurve, date);
+    return (pvCalculator.calculate(cashFlows, yCurveDown, date) + pvCalculator.calculate(cashFlows, yCurveUp, date) - 2 * pv) / (2 * pv * _eps * _eps);
+  }
+
+}
