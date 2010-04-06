c56c4f47e0fa69ffd9e02f11b31ec6d719616eba
Elaine McLeod <elaine@opengamma.com>
2010-04-06 12:38:51 +0100

    Added underlying identity keys to energy and metal futures. I did not add anything to the bean, so FutureSecurityBeanOperation inputs null for these values at the moment. Similarly, the bean test in HibernateSecurityMasterTest has null as the inputs.

--- src/com/opengamma/financial/security/EnergyFutureSecurity.java
+++ src/com/opengamma/financial/security/EnergyFutureSecurity.java
@@ -5,6 +5,7 @@
  */
 package com.opengamma.financial.security;
 
+import com.opengamma.id.DomainSpecificIdentifier;
 import com.opengamma.util.time.Expiry;
 
 /**
@@ -13,18 +14,25 @@ import com.opengamma.util.time.Expiry;
  * @author Andrew
  */
 public class EnergyFutureSecurity extends CommodityFutureSecurity {
+  private final DomainSpecificIdentifier _underlyingIdentifier;
   
-  public EnergyFutureSecurity (final Expiry expiry, final String tradingExchange, final String settlementExchange, final String type, final Double unitNumber, final String unitName) {
+  public EnergyFutureSecurity (final Expiry expiry, final String tradingExchange, final String settlementExchange, final String type, final Double unitNumber, final String unitName, DomainSpecificIdentifier underlyingIdentifier) {
     super (expiry, tradingExchange, settlementExchange, type, unitNumber, unitName);
+    _underlyingIdentifier = underlyingIdentifier;
   }
   
-  public EnergyFutureSecurity (final Expiry expiry, final String tradingExchange, final String settlementExchange, final String type) {
+  public EnergyFutureSecurity (final Expiry expiry, final String tradingExchange, final String settlementExchange, final String type, DomainSpecificIdentifier underlyingIdentifier) {    
     super (expiry, tradingExchange, settlementExchange, type);
+    _underlyingIdentifier = underlyingIdentifier;
   }
 
   @Override
   public <T> T accept(FutureSecurityVisitor<T> visitor) {
     return visitor.visitEnergyFutureSecurity (this);
+  }
+  
+  public DomainSpecificIdentifier getUnderlyingIdentityKey() {
+    return _underlyingIdentifier;
   }
   
 }
