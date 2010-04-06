8ce7bdfe5eb9ae41ac155299d38ff3396b77cdc1
Elaine McLeod <elaine@opengamma.com>
2010-04-06 12:54:39 +0100

    Added a little bit more functionality to the cost of carry model. It still can't calculate anything, but it is now able to get the underlying spot price.

--- src/com/opengamma/financial/analytics/model/future/CostOfCarryFutureAsForwardModelFunction.java
+++ src/com/opengamma/financial/analytics/model/future/CostOfCarryFutureAsForwardModelFunction.java
@@ -14,6 +14,8 @@ import javax.time.calendar.Clock;
 import javax.time.calendar.TimeZone;
 import javax.time.calendar.ZonedDateTime;
 
+import org.fudgemsg.FudgeFieldContainer;
+
 import com.opengamma.engine.ComputationTarget;
 import com.opengamma.engine.ComputationTargetType;
 import com.opengamma.engine.function.AbstractFunction;
@@ -22,6 +24,7 @@ import com.opengamma.engine.function.FunctionExecutionContext;
 import com.opengamma.engine.function.FunctionInputs;
 import com.opengamma.engine.function.FunctionInvoker;
 import com.opengamma.engine.value.ComputedValue;
+import com.opengamma.engine.value.MarketDataFieldNames;
 import com.opengamma.engine.value.ValueRequirement;
 import com.opengamma.engine.value.ValueRequirementNames;
 import com.opengamma.engine.value.ValueSpecification;
@@ -32,10 +35,17 @@ import com.opengamma.financial.model.future.definition.FutureDefinition;
 import com.opengamma.financial.model.future.definition.StandardFutureDataBundle;
 import com.opengamma.financial.model.future.pricing.CostOfCarryFutureAsForwardModel;
 import com.opengamma.financial.model.future.pricing.FutureModel;
+import com.opengamma.financial.security.AgricultureFutureSecurity;
+import com.opengamma.financial.security.BondFutureSecurity;
+import com.opengamma.financial.security.EnergyFutureSecurity;
+import com.opengamma.financial.security.FXFutureSecurity;
 import com.opengamma.financial.security.FutureSecurity;
+import com.opengamma.financial.security.FutureSecurityVisitor;
 import com.opengamma.financial.security.IndexFutureSecurity;
+import com.opengamma.financial.security.InterestRateFutureSecurity;
 import com.opengamma.financial.security.MetalFutureSecurity;
 import com.opengamma.financial.security.StockFutureSecurity;
+import com.opengamma.id.DomainSpecificIdentifier;
 
 /**
  * 
@@ -44,6 +54,7 @@ import com.opengamma.financial.security.StockFutureSecurity;
  */
 public class CostOfCarryFutureAsForwardModelFunction extends AbstractFunction implements FunctionInvoker {
   private final FutureModel<StandardFutureDataBundle> _model = new CostOfCarryFutureAsForwardModel();
+  private final FutureSecurityVisitor<DomainSpecificIdentifier> _visitor = new UnderlyingFutureSecurityVisitor();
   private static final Set<Class<? extends FutureSecurity>> SUPPORTED_FUTURES = new HashSet<Class<? extends FutureSecurity>>();
   private static final Map<String, Greek> AVAILABLE_GREEKS;
 
@@ -60,8 +71,11 @@ public class CostOfCarryFutureAsForwardModelFunction extends AbstractFunction im
   public Set<ComputedValue> execute(final FunctionExecutionContext executionContext, final FunctionInputs inputs, final ComputationTarget target,
       final Set<ValueRequirement> desiredValues) {
     final FutureSecurity future = (FutureSecurity) target.getSecurity();
+    final DomainSpecificIdentifier underlying = future.accept(_visitor);
+    if (underlying == null)
+      throw new IllegalArgumentException("Could not get underlying identity key for " + future);
     final ZonedDateTime now = Clock.system(TimeZone.UTC).zonedDateTime();
-    // final double spot = ((FudgeFieldContainer) inputs.getValue(getUnderlyingMarketDataRequirement()));
+    final double spot = ((FudgeFieldContainer) inputs.getValue(getUnderlyingMarketDataRequirement(underlying))).getDouble(MarketDataFieldNames.INDICATIVE_VALUE_NAME);
     // final double yield = getYield();
     // final DiscountCurve discountCurve = (DiscountCurve) inputs.getValue(getDiscountCurveMarketDataRequirement());
     // final double storageCost = getStorageCost();
@@ -102,8 +116,11 @@ public class CostOfCarryFutureAsForwardModelFunction extends AbstractFunction im
   public Set<ValueRequirement> getRequirements(final FunctionCompilationContext context, final ComputationTarget target) {
     if (canApplyTo(context, target)) {
       final FutureSecurity future = (FutureSecurity) target.getSecurity();
+      final DomainSpecificIdentifier underlying = future.accept(_visitor);
+      if (underlying == null)
+        throw new IllegalArgumentException("Could not get underlying identity key for " + future);
       final Set<ValueRequirement> requirements = new HashSet<ValueRequirement>();
-      // requirements.add(getUnderlyingMarketDataRequirement(future));
+      requirements.add(getUnderlyingMarketDataRequirement(underlying));
       // requirements.add(getDiscountCurveMarketDataRequirement(future.getCurrency()));
       // requirements.add(getYield());
       // requirements.add(getStorageCost());
@@ -133,5 +150,53 @@ public class CostOfCarryFutureAsForwardModelFunction extends AbstractFunction im
   @Override
   public ComputationTargetType getTargetType() {
     return ComputationTargetType.SECURITY;
+  }
+
+  private ValueRequirement getUnderlyingMarketDataRequirement(final DomainSpecificIdentifier id) {
+    return new ValueRequirement(ValueRequirementNames.MARKET_DATA_HEADER, id);
+  }
+
+  private class UnderlyingFutureSecurityVisitor implements FutureSecurityVisitor<DomainSpecificIdentifier> {
+
+    @Override
+    public DomainSpecificIdentifier visitAgricultureFutureSecurity(final AgricultureFutureSecurity security) {
+      return null;
+    }
+
+    @Override
+    public DomainSpecificIdentifier visitBondFutureSecurity(final BondFutureSecurity security) {
+      return null;
+    }
+
+    @Override
+    public DomainSpecificIdentifier visitEnergyFutureSecurity(final EnergyFutureSecurity security) {
+      return null;
+    }
+
+    @Override
+    public DomainSpecificIdentifier visitFXFutureSecurity(final FXFutureSecurity security) {
+      return null;
+    }
+
+    @Override
+    public DomainSpecificIdentifier visitIndexFutureSecurity(final IndexFutureSecurity security) {
+      return security.getUnderlyingIdentityKey();
+    }
+
+    @Override
+    public DomainSpecificIdentifier visitInterestRateFutureSecurity(final InterestRateFutureSecurity security) {
+      return null;
+    }
+
+    @Override
+    public DomainSpecificIdentifier visitMetalFutureSecurity(final MetalFutureSecurity security) {
+      return security.getUnderlyingIdentityKey();
+    }
+
+    @Override
+    public DomainSpecificIdentifier visitStockFutureSecurity(final StockFutureSecurity security) {
+      return security.getUnderlyingIdentityKey();
+    }
+
   }
 }
