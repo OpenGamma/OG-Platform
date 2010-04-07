46fa282fa316033e962752cbe5f4ba1ef20ec757
Elaine McLeod <elaine@opengamma.com>
2010-04-07 16:22:48 +0100

    Changed interpolated discount curves to allow more than one interpolator to be used on the curve. Also renamed a file.

--- src/com/opengamma/financial/model/interestrate/curve/InterpolatedDiscountCurve.java
+++ src/com/opengamma/financial/model/interestrate/curve/InterpolatedDiscountCurve.java
@@ -34,7 +34,7 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
   private static final Logger s_Log = LoggerFactory.getLogger(InterpolatedDiscountCurve.class);
   private final SortedMap<Double, Double> _rateData;
   private final SortedMap<Double, Double> _dfData;
-  private final Interpolator1D _interpolator;
+  private final SortedMap<Double, Interpolator1D> _interpolators;
 
   /**
    * 
@@ -43,18 +43,41 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
    *          in percent (e.g. 3% = 0.03)
    * @param interpolator
    *          An interpolator to get interest rates / discount factors for
-   *          maturities that fall in between nodes. This can be null.
+   *          maturities that fall in between nodes. This cannot be null.
    * @throws IllegalArgumentException
    *           Thrown if the data map is null or empty, or if it contains a
    *           negative time to maturity.
    */
   public InterpolatedDiscountCurve(final Map<Double, Double> data, final Interpolator1D interpolator) {
+    this(data, Collections.<Double, Interpolator1D> singletonMap(Double.POSITIVE_INFINITY, interpolator));
+  }
+
+  /**
+   * 
+   * @param data
+   *          A map containing pairs of maturities in years and interest rates
+   *          in percent (e.g. 3% = 0.03)
+   * @param interpolators
+   *          A map of times and interpolators. This allows different interpolators 
+   *          to be used for different regions of the curve. The time value is the 
+   *          maximum time in years for which an interpolator is valid.
+   * @throws IllegalArgumentException
+   *           Thrown if the data map is null or empty, or if it contains a
+   *           negative time to maturity.
+   */
+  public InterpolatedDiscountCurve(final Map<Double, Double> data, final Map<Double, Interpolator1D> interpolators) {
     if (data == null)
       throw new IllegalArgumentException("Data map was null");
-    if (interpolator == null)
+    if (interpolators == null)
       throw new IllegalArgumentException("Interpolator was null");
+    if (interpolators.size() == 0)
+      throw new IllegalArgumentException("Interpolator map did not contain values");
     if (data.size() < 2)
       throw new IllegalArgumentException("Need to have at least two data points for an interpolated curve");
+    for (final Map.Entry<Double, Interpolator1D> entry : interpolators.entrySet()) {
+      if (entry.getValue() == null)
+        throw new IllegalArgumentException("Interpolator for time " + entry.getKey() + " was null");
+    }
     final SortedMap<Double, Double> sortedRates = new TreeMap<Double, Double>();
     final SortedMap<Double, Double> sortedDF = new TreeMap<Double, Double>();
     for (final Map.Entry<Double, Double> entry : data.entrySet()) {
@@ -65,13 +88,21 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
     }
     _rateData = Collections.<Double, Double> unmodifiableSortedMap(sortedRates);
     _dfData = Collections.<Double, Double> unmodifiableSortedMap(sortedDF);
-    _interpolator = interpolator;
+    _interpolators = Collections.<Double, Interpolator1D> unmodifiableSortedMap(new TreeMap<Double, Interpolator1D>(interpolators));
   }
 
   protected InterpolatedDiscountCurve(final SortedMap<Double, Double> sortedRates, final SortedMap<Double, Double> sortedDF, final Interpolator1D interpolator) {
     _rateData = Collections.<Double, Double> unmodifiableSortedMap(sortedRates);
     _dfData = Collections.<Double, Double> unmodifiableSortedMap(sortedDF);
-    _interpolator = interpolator;
+    final SortedMap<Double, Interpolator1D> sorted = new TreeMap<Double, Interpolator1D>();
+    sorted.put(Double.POSITIVE_INFINITY, interpolator);
+    _interpolators = Collections.<Double, Interpolator1D> unmodifiableSortedMap(sorted);
+  }
+
+  protected InterpolatedDiscountCurve(final SortedMap<Double, Double> sortedRates, final SortedMap<Double, Double> sortedDF, final SortedMap<Double, Interpolator1D> interpolators) {
+    _rateData = Collections.<Double, Double> unmodifiableSortedMap(sortedRates);
+    _dfData = Collections.<Double, Double> unmodifiableSortedMap(sortedDF);
+    _interpolators = Collections.<Double, Interpolator1D> unmodifiableSortedMap(interpolators);
   }
 
   /**
@@ -87,8 +118,8 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
    * 
    * @return The interpolator for this curve.
    */
-  public Interpolator1D getInterpolator() {
-    return _interpolator;
+  public Map<Double, Interpolator1D> getInterpolators() {
+    return _interpolators;
   }
 
   /**
@@ -116,7 +147,8 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
       throw new IllegalArgumentException("t was null");
     if (t < 0)
       throw new IllegalArgumentException("Cannot have a negative time in a DiscountCurve: provided " + t);
-    return _interpolator.interpolate(_dfData, t).getResult();
+    final Double key = _interpolators.headMap(t).lastKey();
+    return _interpolators.get(key).interpolate(_dfData, t).getResult();
   }
 
   @Override
@@ -132,7 +164,7 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
     for (final Map.Entry<Double, Double> entry : _rateData.entrySet()) {
       map.put(entry.getKey(), entry.getValue() + shift);
     }
-    return new InterpolatedDiscountCurve(map, getInterpolator());
+    return new InterpolatedDiscountCurve(map, getInterpolators());
   }
 
   @Override
@@ -147,10 +179,10 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
     final Map<Double, Double> map = new HashMap<Double, Double>(data);
     if (data.containsKey(t)) {
       map.put(t, data.get(t) + shift);
-      return new InterpolatedDiscountCurve(map, getInterpolator());
+      return new InterpolatedDiscountCurve(map, getInterpolators());
     }
     map.put(t, getInterestRate(t) + shift);
-    return new InterpolatedDiscountCurve(map, getInterpolator());
+    return new InterpolatedDiscountCurve(map, getInterpolators());
   }
 
   @Override
@@ -159,7 +191,7 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
       throw new IllegalArgumentException("Shift map was null");
     if (shifts.isEmpty()) {
       s_Log.info("Shift map was empty; returning identical curve");
-      return new InterpolatedDiscountCurve(getData(), getInterpolator());
+      return new InterpolatedDiscountCurve(getData(), getInterpolators());
     }
     final Map<Double, Double> data = getData();
     final Map<Double, Double> map = new HashMap<Double, Double>(data);
@@ -174,7 +206,7 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
         map.put(entry.getKey(), getInterestRate(entry.getKey()) + entry.getValue());
       }
     }
-    return new InterpolatedDiscountCurve(map, getInterpolator());
+    return new InterpolatedDiscountCurve(map, getInterpolators());
   }
 
   @Override
@@ -182,7 +214,7 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
     final int prime = 31;
     int result = 1;
     result = prime * result + (_rateData == null ? 0 : _rateData.hashCode());
-    result = prime * result + (_interpolator == null ? 0 : _interpolator.hashCode());
+    result = prime * result + (_interpolators == null ? 0 : _interpolators.hashCode());
     return result;
   }
 
@@ -200,10 +232,10 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
         return false;
     } else if (!_rateData.equals(other._rateData))
       return false;
-    if (_interpolator == null) {
-      if (other._interpolator != null)
+    if (_interpolators == null) {
+      if (other._interpolators != null)
         return false;
-    } else if (!_interpolator.equals(other._interpolator))
+    } else if (!_interpolators.equals(other._interpolators))
       return false;
     return true;
   }
@@ -212,8 +244,11 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
   public String toString() {
     final StringBuilder sb = new StringBuilder();
     sb.append("InterpolatedDiscountCurve[");
-    sb.append("interpolator=").append(Interpolator1DFactory.getInterpolatorName(getInterpolator())).append(',');
-    sb.append("rate_data={");
+    sb.append("interpolators={");// .append(Interpolator1DFactory.getInterpolatorName(getInterpolators())).append(',');
+    for (final Map.Entry<Double, Interpolator1D> e : _interpolators.entrySet()) {
+      sb.append(e.getKey()).append('=').append(Interpolator1DFactory.getInterpolatorName(e.getValue())).append(',');
+    }
+    sb.append("},rate_data={");
     for (final Map.Entry<Double, Double> e : _rateData.entrySet()) {
       sb.append(e.getKey()).append('=').append(e.getValue()).append(',');
     }
@@ -228,17 +263,17 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
   public FudgeFieldContainer toFudgeMsg(final FudgeSerializationContext context) {
     final MutableFudgeFieldContainer message = context.newMessage();
     message.add(null, 0, getClass().getName());
-    message.add(INTERPOLATOR_FIELD_NAME, Interpolator1DFactory.getInterpolatorName(getInterpolator()));
+    message.add(INTERPOLATOR_FIELD_NAME, encodeDoubleInterpolator1DMap(context, _interpolators));
     message.add(RATE_DATA_FIELD_NAME, encodeDoubleDoubleMap(context, _rateData));
     message.add(DF_DATA_FIELD_NAME, encodeDoubleDoubleMap(context, _dfData));
     return message;
   }
 
   public static InterpolatedDiscountCurve fromFudgeMsg(final FudgeFieldContainer message) {
-    final Interpolator1D interpolator = Interpolator1DFactory.getInterpolator(message.getString(INTERPOLATOR_FIELD_NAME));
     final SortedMap<Double, Double> rateData = decodeSortedDoubleDoubleMap(message.getMessage(RATE_DATA_FIELD_NAME));
     final SortedMap<Double, Double> dfData = decodeSortedDoubleDoubleMap(message.getMessage(DF_DATA_FIELD_NAME));
-    return new InterpolatedDiscountCurve(rateData, dfData, interpolator);
+    final SortedMap<Double, Interpolator1D> interpolators = decodeSortedDoubleInterpolator1DMap(message.getMessage(INTERPOLATOR_FIELD_NAME));
+    return new InterpolatedDiscountCurve(rateData, dfData, interpolators);
   }
 
   // REVIEW kirk 2010-03-31 -- These probably belong in a utility class
@@ -250,6 +285,15 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
     for (final Map.Entry<Double, Double> entry : data.entrySet()) {
       message.add("key", entry.getKey());
       message.add("value", entry.getValue());
+    }
+    return message;
+  }
+
+  public static MutableFudgeFieldContainer encodeDoubleInterpolator1DMap(final FudgeSerializationContext context, final Map<Double, Interpolator1D> data) {
+    final MutableFudgeFieldContainer message = context.newMessage();
+    for (final Map.Entry<Double, Interpolator1D> entry : data.entrySet()) {
+      message.add("key", entry.getKey());
+      message.add("value", Interpolator1DFactory.getInterpolatorName(entry.getValue()));
     }
     return message;
   }
@@ -267,4 +311,17 @@ public class InterpolatedDiscountCurve extends DiscountCurve {
     return result;
   }
 
+  public static SortedMap<Double, Interpolator1D> decodeSortedDoubleInterpolator1DMap(final FudgeFieldContainer msg) {
+    final SortedMap<Double, Interpolator1D> result = new TreeMap<Double, Interpolator1D>();
+    final Iterator<FudgeField> keyIter = msg.getAllByName("key").iterator();
+    final Iterator<FudgeField> valueIter = msg.getAllByName("value").iterator();
+    FudgeField keyField, valueField;
+    while (keyIter.hasNext()) {
+      assert valueIter.hasNext();
+      keyField = keyIter.next();
+      valueField = valueIter.next();
+      result.put((Double) keyField.getValue(), Interpolator1DFactory.getInterpolator((String) valueField.getValue()));
+    }
+    return result;
+  }
 }
---
+++ tests/unit/com/opengamma/financial/model/interestrate/curve/ParRateBootstrapDiscountCurveModelTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.model.interestrate.curve;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.time.calendar.ZonedDateTime;
+
+import org.junit.Test;
+
+import com.opengamma.financial.model.interestrate.definition.FixedInterestRateInstrumentDefinition;
+import com.opengamma.financial.model.interestrate.definition.ParBondInstrumentDefinition;
+import com.opengamma.math.interpolation.LinearInterpolator1D;
+import com.opengamma.util.time.DateUtil;
+import com.opengamma.util.time.Expiry;
+
+/**
+ * 
+ * @author emcleod
+ */
+public class ParRateBootstrapDiscountCurveModelTest {
+
+  @Test
+  public void test() {
+    final ZonedDateTime date = DateUtil.getUTCDate(2009, 1, 1);
+    final Set<FixedInterestRateInstrumentDefinition> data = new HashSet<FixedInterestRateInstrumentDefinition>();
+    data.add(new ParBondInstrumentDefinition(new Expiry(DateUtil.getDateOffsetWithYearFraction(date, 1)), 0.06));
+    data.add(new ParBondInstrumentDefinition(new Expiry(DateUtil.getDateOffsetWithYearFraction(date, 2)), 0.07));
+    data.add(new ParBondInstrumentDefinition(new Expiry(DateUtil.getDateOffsetWithYearFraction(date, 3)), 0.075));
+    data.add(new ParBondInstrumentDefinition(new Expiry(DateUtil.getDateOffsetWithYearFraction(date, 4)), 0.08));
+    data.add(new ParBondInstrumentDefinition(new Expiry(DateUtil.getDateOffsetWithYearFraction(date, 5)), 0.085));
+    final InterpolatedDiscountCurve curve = new ParRateBootstrapDiscountCurveModel(new LinearInterpolator1D()).getCurve(data, date);
+    System.out.println(curve.getData());
+  }
+}
