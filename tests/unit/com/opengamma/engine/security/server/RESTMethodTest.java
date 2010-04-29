18dc7b813cdc72a968cc1401756fcbc7648bb737
Andrew Griffin <andrew@opengamma.com>
2010-04-29 16:51:49 +0100

    [ENG-2] RESTful implementation of a SecurityMaster distribution

---
+++ tests/unit/com/opengamma/engine/security/server/RESTMethodTest.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.engine.security.server;
+
+import static com.opengamma.engine.security.server.SecurityMasterServiceNames.DEFAULT_SECURITYMASTER_NAME;
+import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SECURITYMASTER_ALLSECURITYTYPES;
+import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SECURITYMASTER_SECURITIES;
+import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SECURITYMASTER_SECURITY;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.fudgemsg.FudgeField;
+import org.fudgemsg.FudgeFieldContainer;
+import org.fudgemsg.FudgeMsgEnvelope;
+import org.fudgemsg.FudgeMsgFormatter;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.opengamma.engine.security.DefaultSecurity;
+import com.opengamma.engine.security.InMemorySecurityMaster;
+import com.opengamma.engine.security.Security;
+import com.opengamma.id.IdentificationScheme;
+import com.opengamma.id.Identifier;
+
+public class RESTMethodTest {
+  
+  private final SecurityMasterService _securityMasterService = new SecurityMasterService ();
+  
+  protected SecurityMasterService getSecurityMasterService () {
+    return _securityMasterService;
+  }
+  
+  protected SecurityMasterResource getSecurityMasterResource () {
+    return getSecurityMasterService ().findSecurityMaster (DEFAULT_SECURITYMASTER_NAME);
+  }
+  
+  @Before
+  public void configureService () {
+    InMemorySecurityMaster secMaster = new InMemorySecurityMaster();
+    Identifier secId1 = new Identifier(new IdentificationScheme("d1"), "v1");
+    Identifier secId2 = new Identifier(new IdentificationScheme("d2"), "v2");
+    DefaultSecurity sec1 = new DefaultSecurity("t1", Collections.singleton(secId1));
+    secMaster.add(sec1);
+    DefaultSecurity sec2 = new DefaultSecurity("t2", Collections.singleton(secId2));
+    secMaster.add(sec2);
+    getSecurityMasterService ().setSecurityMaster (secMaster);
+  }
+  
+  @Test
+  public void testFindSecurityMaster () {
+    assertNull (getSecurityMasterService ().findSecurityMaster ("woot"));
+    assertNotNull (getSecurityMasterResource ());
+  }
+  
+  private <T> List<T> assertIsList (final Class<T> clazz, final FudgeFieldContainer msg) {
+    if (msg.getNumFields () == 0) {
+      return Collections.emptyList ();
+    }
+    final List<T> list = new ArrayList<T> (msg.getNumFields ());
+    for (FudgeField f : msg) {
+      assertEquals (null, f.getName ());
+      assertTrue ((f.getOrdinal () == null) || (f.getOrdinal () == 1));
+      T value = msg.getFieldValue (clazz, f);
+      assertNotNull (value);
+      list.add (value);
+    }
+    return list;
+  }
+  
+  @Test
+  public void testGetAllSecurityTypes () {
+    final FudgeMsgEnvelope fme = getSecurityMasterResource ().getAllSecurityTypes ();
+    assertNotNull (fme);
+    final FudgeFieldContainer msg = fme.getMessage ();
+    assertNotNull (msg);
+    FudgeMsgFormatter.outputToSystemOut (msg);
+    List<String> types = assertIsList (String.class, msg.getFieldValue (FudgeFieldContainer.class, msg.getByName (SECURITYMASTER_ALLSECURITYTYPES)));
+    assertEquals (2, types.size ());
+    assertTrue (types.contains ("t1"));
+    assertTrue (types.contains ("t2"));
+  }
+  
+  @Test
+  public void testGetSecurityByIdentifier () {
+    IdentifierSelectResource req = getSecurityMasterResource ().security ();
+    req = req.addIdentifier (Security.SECURITY_IDENTITY_KEY_DOMAIN.getName (), "1");
+    final FudgeMsgEnvelope fme = req.get ();
+    assertNotNull (fme);
+    final FudgeFieldContainer msg = fme.getMessage ();
+    assertNotNull (msg);
+    FudgeMsgFormatter.outputToSystemOut (msg);
+    final FudgeFieldContainer security = msg.getFieldValue (FudgeFieldContainer.class, msg.getByName (SECURITYMASTER_SECURITY));
+    assertNotNull (security);
+  }
+  
+  @Test
+  public void testGetSecurityByBundle () {
+    IdentifierSelectResource req = getSecurityMasterResource ().security ();
+    req = req.addIdentifier ("d1", "v1");
+    req = req.addIdentifier ("d2", "v2");
+    final FudgeMsgEnvelope fme = req.get ();
+    assertNotNull (fme);
+    final FudgeFieldContainer msg = fme.getMessage ();
+    assertNotNull (msg);
+    FudgeMsgFormatter.outputToSystemOut (msg);
+    final FudgeFieldContainer security = msg.getFieldValue (FudgeFieldContainer.class, msg.getByName (SECURITYMASTER_SECURITY));
+    assertNotNull (security);
+  }
+  
+  @Test
+  public void testGetSecurities () {
+    IdentifierSelectResource req = getSecurityMasterResource ().securities ();
+    req = req.addIdentifier ("d1", "v1");
+    req = req.addIdentifier ("d2", "v2");
+    final FudgeMsgEnvelope fme = req.get ();
+    assertNotNull (fme);
+    final FudgeFieldContainer msg = fme.getMessage ();
+    assertNotNull (msg);
+    FudgeMsgFormatter.outputToSystemOut (msg);
+    final FudgeFieldContainer securities = msg.getFieldValue (FudgeFieldContainer.class, msg.getByName (SECURITYMASTER_SECURITIES));
+    assertNotNull (securities);
+    assertEquals (2, securities.getNumFields ());
+  }
+  
+}
