49fdca84cccf2f1142f13a81d5d557a262e3a4f5
Elaine McLeod <elaine@opengamma.com>
2010-04-30 17:09:56 +0100

    Added more tests.

---
+++ tests/unit/com/opengamma/financial/covariance/CovarianceCalculatorTest.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.covariance;
+
+import org.junit.Test;
+
+import com.opengamma.util.timeseries.DoubleTimeSeries;
+import com.opengamma.util.timeseries.TimeSeriesException;
+import com.opengamma.util.timeseries.fast.DateTimeNumericEncoding;
+import com.opengamma.util.timeseries.fast.integer.FastArrayIntDoubleTimeSeries;
+
+public class CovarianceCalculatorTest {
+  private static final CovarianceCalculator CALCULATOR = new CovarianceCalculator() {
+
+    @Override
+    public Double evaluate(final DoubleTimeSeries<?> x1, final DoubleTimeSeries<?> x2) {
+      return null;
+    }
+
+  };
+  private static final DoubleTimeSeries<?> TS1 = new FastArrayIntDoubleTimeSeries(DateTimeNumericEncoding.TIME_EPOCH_SECONDS, new int[] { 2 }, new double[] { 1 });
+  private static final DoubleTimeSeries<?> TS2 = new FastArrayIntDoubleTimeSeries(DateTimeNumericEncoding.TIME_EPOCH_SECONDS, new int[] { 2, 3 }, new double[] { 1, 2 });
+  private static final DoubleTimeSeries<?> TS3 = new FastArrayIntDoubleTimeSeries(DateTimeNumericEncoding.TIME_EPOCH_SECONDS, new int[] { 4, 5 }, new double[] { 1, 2 });
+  private static final DoubleTimeSeries<?> TS4 = new FastArrayIntDoubleTimeSeries(DateTimeNumericEncoding.TIME_EPOCH_SECONDS, new int[] { 4, 5, 6 }, new double[] { 1, 2, 3 });
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullTS1() {
+    CALCULATOR.testTimeSeries(null, TS1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullTS2() {
+    CALCULATOR.testTimeSeries(TS2, null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testEmptyTS1() {
+    CALCULATOR.testTimeSeries(FastArrayIntDoubleTimeSeries.EMPTY_SERIES, TS1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testEmptyTS2() {
+    CALCULATOR.testTimeSeries(TS2, FastArrayIntDoubleTimeSeries.EMPTY_SERIES);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testSmallTS1() {
+    CALCULATOR.testTimeSeries(TS1, TS2);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testSmallTS2() {
+    CALCULATOR.testTimeSeries(TS2, TS1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDifferentLength() {
+    CALCULATOR.testTimeSeries(TS2, TS4);
+  }
+
+  @Test(expected = TimeSeriesException.class)
+  public void testDifferentDates() {
+    CALCULATOR.testTimeSeries(TS2, TS3);
+  }
+}
