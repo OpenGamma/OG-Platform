5ac292ebc03f759df757c00797181778844b981d
Kirk Wylie <kirk@opengamma.com>
2010-04-30 15:02:10 +0100

    An incredibly braindead test for GUIDGenerator, as well as properly synchronizing GUIDGenerator.

---
+++ tests/unit/com/opengamma/util/GUIDGeneratorTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.util;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.UUID;
+
+import org.junit.Test;
+
+/**
+ * 
+ *
+ * @author kirk
+ */
+public class GUIDGeneratorTest {
+  
+  @Test
+  public void generatorAlwaysGeneratesSomething() {
+    UUID uuid = GUIDGenerator.generate();
+    assertNotNull(uuid);
+    System.out.println("Generated UUID " + uuid);
+  }
+  
+}
