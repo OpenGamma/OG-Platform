10b368b066aeaa6c543807fcbeca08f46f3864d3
Jim Moores <jim.moores@gmail.com>
2010-05-04 10:32:17 +0100

    Deleted some obsolete VaR code (at Elaine's command) and removed reference to old time series class from RenderVisitor.

--- src/com/opengamma/financial/render/RenderVisitor.java
+++ src/com/opengamma/financial/render/RenderVisitor.java
@@ -3,11 +3,9 @@ package com.opengamma.financial.render;
 import com.opengamma.financial.greeks.GreekResultCollection;
 import com.opengamma.financial.model.interestrate.curve.InterpolatedDiscountCurve;
 import com.opengamma.financial.model.volatility.surface.VolatilitySurface;
-import com.opengamma.timeseries.DoubleTimeSeries;
 
 public interface RenderVisitor<T> {
   public T visitDiscountCurve(InterpolatedDiscountCurve discountCurve);
   public T visitVolatilitySurface(VolatilitySurface volatilitySurface);
-  public T visitDoubleTimeSeries(DoubleTimeSeries doubleTimeSeries);
   public T visitGreekResultCollection(GreekResultCollection greekResultCollection);
 }
