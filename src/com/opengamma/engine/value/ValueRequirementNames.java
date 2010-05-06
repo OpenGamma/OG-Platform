c5ea01f08cea6faa1bf816f80c12d0d9bdebf16a
Elaine McLeod <elaine@opengamma.com>
2010-05-06 12:01:24 +0100

    Didn't realise this file lived in OG-Engine, so forgot to commit.

--- src/com/opengamma/engine/value/ValueRequirementNames.java
+++ src/com/opengamma/engine/value/ValueRequirementNames.java
@@ -22,6 +22,8 @@ public interface ValueRequirementNames {
   
   //
   public static final String FAIR_VALUE = "FairValue";
+  public static final String POSITION_FAIR_VALUE = "PositionFairValue";
+  public static final String VALUE_FAIR_VALUE = "ValueFairValue";
   
   // Greeks Names:
   public static final String DELTA = "Delta";
@@ -69,9 +71,98 @@ public interface ValueRequirementNames {
   public static final String VOMMA_P = "VommaP";
   public static final String VARIANCE_VOMMA = "VarianceVomma";
 
-  // Modified Greeks:
-  public static final String DOLLAR_DELTA = "DollarDelta";
+  // Greeks Names:
+  public static final String POSITION_DELTA = "PositionDelta";
+  public static final String POSITION_DELTA_BLEED = "PositionDeltaBleed";
+  public static final String POSITION_STRIKE_DELTA = "PositionStrikeDelta";
+  public static final String POSITION_DRIFTLESS_DELTA = "PositionDriftlessTheta";
   
+  public static final String POSITION_GAMMA = "PositionGamma";
+  public static final String POSITION_GAMMA_P = "PositionGammaP";
+  public static final String POSITION_STRIKE_GAMMA = "PositionStrikeGamma";
+  public static final String POSITION_GAMMA_BLEED = "PositionGammaBleed";
+  public static final String POSITION_GAMMA_P_BLEED = "PositionGammaPBleed";
+  
+  public static final String POSITION_VEGA = "PositionVega";
+  public static final String POSITION_VEGA_P = "PositionVegaP";
+  public static final String POSITION_VARIANCE_VEGA = "PositionVarianceVega";
+  public static final String POSITION_VEGA_BLEED = "PositionVegaBleed";
+  
+  public static final String POSITION_THETA = "PositionTheta";
+  
+  public static final String POSITION_RHO = "PositionRho";
+  public static final String POSITION_CARRY_RHO = "PositionCarryRho";
+  
+  public static final String POSITION_ZETA = "PositionZeta";
+  public static final String POSITION_ZETA_BLEED = "PositionZetaBleed";
+  public static final String POSITION_DZETA_DVOL = "PositiondZeta_dVol";
+  
+  public static final String POSITION_ELASTICITY = "PositionElasticity";
+  public static final String POSITION_PHI = "PositionPhi";
+  
+  public static final String POSITION_ZOMMA = "PositionZomma";
+  public static final String POSITION_ZOMMA_P = "PositionZommaP";
+  
+  public static final String POSITION_ULTIMA = "PositionUltima";
+  public static final String POSITION_VARIANCE_ULTIMA = "PositionVarianceUltima";
+  
+  public static final String POSITION_SPEED = "PositionSpeed";
+  public static final String POSITION_SPEED_P = "PositionSpeedP";
+  
+  public static final String POSITION_VANNA = "PositionVanna";
+  public static final String POSITION_VARIANCE_VANNA = "PositionVarianceVanna";
+  public static final String POSITION_DVANNA_DVOL = "PositiondVanna_dVol";
+  
+  public static final String POSITION_VOMMA = "PositionVomma";
+  public static final String POSITION_VOMMA_P = "PositionVommaP";
+  public static final String POSITION_VARIANCE_VOMMA = "PositionVarianceVomma";
+
+  // Greeks Names:
+  public static final String VALUE_DELTA = "ValueDelta";
+  public static final String VALUE_DELTA_BLEED = "ValueDeltaBleed";
+  public static final String VALUE_STRIKE_DELTA = "ValueStrikeDelta";
+  public static final String VALUE_DRIFTLESS_DELTA = "ValueDriftlessTheta";
+  
+  public static final String VALUE_GAMMA = "ValueGamma";
+  public static final String VALUE_GAMMA_P = "ValueGammaP";
+  public static final String VALUE_STRIKE_GAMMA = "ValueStrikeGamma";
+  public static final String VALUE_GAMMA_BLEED = "ValueGammaBleed";
+  public static final String VALUE_GAMMA_P_BLEED = "ValueGammaPBleed";
+  
+  public static final String VALUE_VEGA = "ValueVega";
+  public static final String VALUE_VEGA_P = "ValueVegaP";
+  public static final String VALUE_VARIANCE_VEGA = "ValueVarianceVega";
+  public static final String VALUE_VEGA_BLEED = "ValueVegaBleed";
+  
+  public static final String VALUE_THETA = "ValueTheta";
+  
+  public static final String VALUE_RHO = "ValueRho";
+  public static final String VALUE_CARRY_RHO = "ValueCarryRho";
+  
+  public static final String VALUE_ZETA = "ValueZeta";
+  public static final String VALUE_ZETA_BLEED = "ValueZetaBleed";
+  public static final String VALUE_DZETA_DVOL = "ValuedZeta_dVol";
+  
+  public static final String VALUE_ELASTICITY = "ValueElasticity";
+  public static final String VALUE_PHI = "ValuePhi";
+  
+  public static final String VALUE_ZOMMA = "ValueZomma";
+  public static final String VALUE_ZOMMA_P = "ValueZommaP";
+  
+  public static final String VALUE_ULTIMA = "ValueUltima";
+  public static final String VALUE_VARIANCE_ULTIMA = "ValueVarianceUltima";
+  
+  public static final String VALUE_SPEED = "ValueSpeed";
+  public static final String VALUE_SPEED_P = "ValueSpeedP";
+  
+  public static final String VALUE_VANNA = "ValueVanna";
+  public static final String VALUE_VARIANCE_VANNA = "ValueVarianceVanna";
+  public static final String VALUE_DVANNA_DVOL = "ValuedVanna_dVol";
+  
+  public static final String VALUE_VOMMA = "ValueVomma";
+  public static final String VALUE_VOMMA_P = "ValueVommaP";
+  public static final String VALUE_VARIANCE_VOMMA = "ValueVarianceVomma";
+
   // Generic Aggregates:
   public static final String SUM = "Sum";
   public static final String MEDIAN = "Median";
