ef67a9f75292bb54ad2d6cc80e6d3d66c2be8c5f
Stephen Colebourne <stephen@opengamma.com>
2010-05-11 19:02:23 +0100

    [ENG-85] Refactor position/security master
    Replace Identifier by UniqueIdentifier

--- src/com/opengamma/financial/analytics/ircurve/FixedIncomeStrip.java
+++ src/com/opengamma/financial/analytics/ircurve/FixedIncomeStrip.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2009 - 2009 by OpenGamma Inc.
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
  *
  * Please see distribution for license.
  */
@@ -13,22 +13,25 @@ import org.apache.commons.lang.builder.ToStringStyle;
 
 import com.opengamma.engine.ComputationTargetSpecification;
 import com.opengamma.engine.ComputationTargetType;
-import com.opengamma.id.Identifier;
+import com.opengamma.id.UniqueIdentifier;
 import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.CompareUtils;
 
 /**
- * 
- *
- * @author kirk
+ * A fixed income strip.
  */
-public class FixedIncomeStrip implements Serializable, Comparable<FixedIncomeStrip> {
-  
+public class FixedIncomeStrip implements Comparable<FixedIncomeStrip>, Serializable {
+
   private final double _numYears;
-  private final Identifier _marketDataKey;
-  
-  public FixedIncomeStrip(double numYears, Identifier marketDataKey) {
-    if(numYears < 0) {
+  private final UniqueIdentifier _marketDataKey;
+
+  /**
+   * Creates the strip.
+   * @param numYears  the number of years, not negative
+   * @param marketDataKey  the market data key, not null
+   */
+  public FixedIncomeStrip(double numYears, UniqueIdentifier marketDataKey) {
+    if (numYears < 0) {
       throw new IllegalArgumentException("Fixed income strips cannot be in the future.");
     }
     ArgumentChecker.notNull(marketDataKey, "Market data key");
@@ -36,54 +39,54 @@ public class FixedIncomeStrip implements Serializable, Comparable<FixedIncomeStr
     _marketDataKey = marketDataKey;
   }
 
+  //-------------------------------------------------------------------------
   /**
-   * @return the numYears
+   * Gets the number of years.
+   * @return the number of years
    */
   public double getNumYears() {
     return _numYears;
   }
 
   /**
-   * @return the marketDataKey
+   * Gets the market data identifier.
+   * @return the market data key, not null
    */
-  public Identifier getMarketDataKey() {
+  public UniqueIdentifier getMarketDataKey() {
     return _marketDataKey;
   }
-  
+
+  /**
+   * Gets the specification for the market data key.
+   * @return the specification, not null
+   */
   public ComputationTargetSpecification getMarketDataSpecification() {
     // REVIEW kirk 2009-12-30 -- We might want to cache this on construction if it's called a lot.
     return new ComputationTargetSpecification(ComputationTargetType.PRIMITIVE, getMarketDataKey());
   }
 
+  //-------------------------------------------------------------------------
   @Override
-  public int compareTo(FixedIncomeStrip o) {
-    if(getNumYears() < o.getNumYears()) {
+  public int compareTo(FixedIncomeStrip other) {
+    if (getNumYears() < other.getNumYears()) {
       return -1;
-    } else if (getNumYears() > o.getNumYears()) {
+    } else if (getNumYears() > other.getNumYears()) {
       return 1;
     }
-    return getMarketDataKey().compareTo(o.getMarketDataKey());
+    return getMarketDataKey().compareTo(other.getMarketDataKey());
   }
 
   @Override
   public boolean equals(Object obj) {
-    if(this == obj) {
+    if (this == obj) {
       return true;
     }
-    if(obj == null) {
-      return false;
+    if(obj instanceof FixedIncomeStrip) {
+      FixedIncomeStrip other = (FixedIncomeStrip) obj;
+      return CompareUtils.closeEquals(_numYears, other._numYears) &&
+        ObjectUtils.equals(_marketDataKey, other._marketDataKey);
     }
-    if(!(obj instanceof FixedIncomeStrip)) {
-      return false;
-    }
-    FixedIncomeStrip other = (FixedIncomeStrip) obj;
-    if(!CompareUtils.closeEquals(_numYears, other._numYears)) {
-      return false;
-    }
-    if(!ObjectUtils.equals(_marketDataKey, other._marketDataKey)) {
-      return false;
-    }
-    return true;
+    return false;
   }
 
   @Override
