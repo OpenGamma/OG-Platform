f3bad919967c68a9983bcd364cb421044db0c19b
Stephen Colebourne <stephen@opengamma.com>
2010-05-11 13:40:18 +0100

    [ENG-85] Refactor position/security master
    Fix RESTful security master

--- src/com/opengamma/engine/security/RemoteSecurityMaster.java
+++ src/com/opengamma/engine/security/RemoteSecurityMaster.java
@@ -5,8 +5,6 @@
  */
 package com.opengamma.engine.security;
 
-import static com.opengamma.engine.security.server.SecurityMasterServiceNames.PATH_IDENTIFIER;
-import static com.opengamma.engine.security.server.SecurityMasterServiceNames.PATH_VALUE;
 import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SECURITYMASTER_ALLSECURITYTYPES;
 import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SECURITYMASTER_SECURITIES;
 import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SECURITYMASTER_SECURITY;
@@ -17,7 +15,6 @@ import java.util.Set;
 
 import org.fudgemsg.FudgeContext;
 
-import com.opengamma.id.Identifier;
 import com.opengamma.id.IdentifierBundle;
 import com.opengamma.id.UniqueIdentifier;
 import com.opengamma.transport.jaxrs.RestClient;
@@ -29,57 +26,42 @@ import com.opengamma.transport.jaxrs.RestTarget;
 public class RemoteSecurityMaster implements SecurityMaster {
 
   private final RestClient _restClient;
-  private final RestTarget _targetSecurities;
-  private final RestTarget _targetSecurity;
-  private final RestTarget _targetAllSecurityTypes;
+  private final RestTarget _targetBase;
 
   public RemoteSecurityMaster(final FudgeContext fudgeContext, final RestTarget baseTarget) {
     _restClient = RestClient.getInstance(fudgeContext, null);
-    _targetSecurities = baseTarget.resolveBase(SECURITYMASTER_SECURITIES);
-    _targetSecurity = baseTarget.resolveBase(SECURITYMASTER_SECURITY);
-    _targetAllSecurityTypes = baseTarget.resolve(SECURITYMASTER_ALLSECURITYTYPES);
+    _targetBase = baseTarget;
   }
 
   protected RestClient getRestClient () {
     return _restClient;
   }
 
-  protected RestTarget resolveIdentifierBase(final RestTarget base, final UniqueIdentifier identifier) {
-    return base.resolveBase(PATH_IDENTIFIER).resolveBase(identifier.getScheme()).resolveBase(identifier.getValue());
-  }
-
-  protected RestTarget resolveIdentifierBundleBase(final RestTarget base, final IdentifierBundle bundle) {
-    RestTarget target = base;
-    for (Identifier identifier : bundle.getIdentifiers()) {
-      target = target.resolveBase(PATH_IDENTIFIER).resolveBase(identifier.getScheme().getName()).resolveBase(identifier.getValue());
-    }
-    return target;
-  }
-
   //-------------------------------------------------------------------------
   @Override
   public Security getSecurity(UniqueIdentifier uid) {
-    return getRestClient().getSingleValueNotNull(Security.class,
-        resolveIdentifierBase(_targetSecurity, uid).resolve(PATH_VALUE), SECURITYMASTER_SECURITY);
+    final RestTarget target = _targetBase.resolveBase("securities").resolveBase("security").resolve(uid.toString());
+    return getRestClient().getSingleValueNotNull(Security.class, target, SECURITYMASTER_SECURITY);
+  }
+
+  @Override
+  public Security getSecurity(IdentifierBundle secKey) {
+    final RestTarget target = _targetBase.resolveBase("securities").resolve("security").resolveQuery("id", secKey.toStringList());
+    return getRestClient().getSingleValueNotNull(Security.class, target, SECURITYMASTER_SECURITY);
   }
 
   @SuppressWarnings("unchecked")
   @Override
   public Collection<Security> getSecurities(IdentifierBundle secKey) {
-    return getRestClient().getSingleValueNotNull(List.class,
-        resolveIdentifierBundleBase(_targetSecurities, secKey).resolve(PATH_VALUE), SECURITYMASTER_SECURITIES);
-  }
-
-  @Override
-  public Security getSecurity(IdentifierBundle secKey) {
-    return getRestClient().getSingleValueNotNull(Security.class,
-        resolveIdentifierBundleBase(_targetSecurity, secKey).resolve(PATH_VALUE), SECURITYMASTER_SECURITY);
+    final RestTarget target = _targetBase.resolveBase("securities").resolveQuery("id", secKey.toStringList());
+    return getRestClient().getSingleValueNotNull(List.class, target, SECURITYMASTER_SECURITIES);
   }
 
   @SuppressWarnings("unchecked")
   @Override
   public Set<String> getAllSecurityTypes() {
-    return getRestClient().getSingleValueNotNull(Set.class, _targetAllSecurityTypes, SECURITYMASTER_ALLSECURITYTYPES);
+    final RestTarget target = _targetBase.resolveBase("securities").resolve("types");
+    return getRestClient().getSingleValueNotNull(Set.class, target, SECURITYMASTER_ALLSECURITYTYPES);
   }
 
 }
--- src/com/opengamma/engine/security/server/SecurityMasterResource.java
+++ src/com/opengamma/engine/security/server/SecurityMasterResource.java
@@ -9,97 +9,122 @@ import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SE
 import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SECURITYMASTER_SECURITIES;
 import static com.opengamma.engine.security.server.SecurityMasterServiceNames.SECURITYMASTER_SECURITY;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 import org.fudgemsg.FudgeContext;
 import org.fudgemsg.FudgeMsgEnvelope;
 import org.fudgemsg.MutableFudgeFieldContainer;
 import org.fudgemsg.mapping.FudgeSerializationContext;
 
-import com.opengamma.engine.security.Security;
 import com.opengamma.engine.security.SecurityMaster;
+import com.opengamma.id.Identifier;
 import com.opengamma.id.IdentifierBundle;
 import com.opengamma.id.UniqueIdentifier;
+import com.opengamma.util.ArgumentChecker;
 
 /**
  * REST resource wrapper for a {@link SecurityMaster}.
  */
 public class SecurityMasterResource {
-  
+
+  /**
+   * The Fudge context being used.
+   */
   private final FudgeContext _fudgeContext;
+  /**
+   * The underlying security master.
+   */
   private final SecurityMaster _securityMaster;
-  
+
+  /**
+   * Creates a resource to expose a security master over REST.
+   * @param fudgeContext  the context, not null
+   * @param securityMaster  the security master, not null
+   */
   public SecurityMasterResource (final FudgeContext fudgeContext, final SecurityMaster securityMaster) {
+    ArgumentChecker.notNull(fudgeContext, "fudge context");
+    ArgumentChecker.notNull(securityMaster, "security master");
     _fudgeContext = fudgeContext;
     _securityMaster = securityMaster;
   }
-  
+
+  //-------------------------------------------------------------------------
+  /**
+   * Gets the Fudge context.
+   * @return the context, not null
+   */
   protected FudgeContext getFudgeContext () {
     return _fudgeContext;
   }
-  
+
+  /**
+   * Gets the security master.
+   * @return the security master, not null
+   */
   protected SecurityMaster getSecurityMaster () {
     return _securityMaster;
   }
-  
-  protected FudgeSerializationContext getFudgeSerializationContext () {
-    return new FudgeSerializationContext (getFudgeContext ());
+
+  //-------------------------------------------------------------------------
+  /**
+   * Gets the serialization context derived from the main Fudge context.
+   * @return the context, not null
+   */
+  protected FudgeSerializationContext getFudgeSerializationContext() {
+    return new FudgeSerializationContext(getFudgeContext());
   }
-  
+
+  //-------------------------------------------------------------------------
   @GET
-  @Path ("allSecurityTypes")
-  public FudgeMsgEnvelope getAllSecurityTypes () {
-    final FudgeSerializationContext context = getFudgeSerializationContext ();
-    final MutableFudgeFieldContainer msg = context.newMessage ();
-    context.objectToFudgeMsg (msg, SECURITYMASTER_ALLSECURITYTYPES, null, getSecurityMaster ().getAllSecurityTypes ());
-    return new FudgeMsgEnvelope (msg);
+  @Path("securities/security/{uid}")
+  public FudgeMsgEnvelope getSecurity(@PathParam("uid") String uidStr) {
+    final UniqueIdentifier uid = UniqueIdentifier.parse(uidStr);
+    final FudgeSerializationContext context = getFudgeSerializationContext();
+    final MutableFudgeFieldContainer msg = context.newMessage();
+    context.objectToFudgeMsg(msg, SECURITYMASTER_SECURITY, null, getSecurityMaster().getSecurity(uid));
+    return new FudgeMsgEnvelope(msg);
   }
-  
-  @Path ("security")
-  public IdentifierSelectResource security () {
-    return new IdentifierSelectResource (new IdentifierSelectResource.Callback () {
-      
-      private FudgeMsgEnvelope returnSecurity (final Security security) {
-        if (security != null) {
-          final FudgeSerializationContext context = getFudgeSerializationContext ();
-          final MutableFudgeFieldContainer msg = context.newMessage ();
-          context.objectToFudgeMsg (msg, SECURITYMASTER_SECURITY, null, security);
-          return new FudgeMsgEnvelope (msg);
-        } else {
-          return null;
-        }
-      }
-      
-      @Override
-      protected FudgeMsgEnvelope getByIdentifier (final UniqueIdentifier identifier) {
-        System.out.println ("security.getByIdentifier: " + identifier);
-        return returnSecurity (getSecurityMaster ().getSecurity (identifier));
-      }
-      
-      @Override
-      protected FudgeMsgEnvelope getByBundle (final IdentifierBundle bundle) {
-        System.out.println ("security.getByBundle: " + bundle);
-        return returnSecurity (getSecurityMaster ().getSecurity (bundle));
-      }
-      
-    });
+
+  @GET
+  @Path("securities/security")
+  public FudgeMsgEnvelope getSecurity(@QueryParam("id") List<String> idStrs) {
+    ArgumentChecker.notEmpty(idStrs, "identifiers");
+    IdentifierBundle bundle = new IdentifierBundle();
+    for (String idStr : idStrs) {
+      bundle = bundle.withIdentifier(Identifier.parse(idStr));
+    }
+    final FudgeSerializationContext context = getFudgeSerializationContext();
+    final MutableFudgeFieldContainer msg = context.newMessage();
+    context.objectToFudgeMsg(msg, SECURITYMASTER_SECURITY, null, getSecurityMaster().getSecurity(bundle));
+    return new FudgeMsgEnvelope(msg);
   }
-  
-  @Path ("securities")
-  public IdentifierSelectResource securities () {
-    return new IdentifierSelectResource (new IdentifierSelectResource.Callback () {
-      
-      @Override
-      protected FudgeMsgEnvelope getByBundle (final IdentifierBundle bundle) {
-        System.out.println ("securities.getByBundle: " + bundle);
-        final FudgeSerializationContext context = getFudgeSerializationContext ();
-        final MutableFudgeFieldContainer msg = context.newMessage ();
-        context.objectToFudgeMsg (msg, SECURITYMASTER_SECURITIES, null, getSecurityMaster ().getSecurities (bundle));
-        return new FudgeMsgEnvelope (msg);
-      }
-      
-    });
+
+  @GET
+  @Path("securities")
+  public FudgeMsgEnvelope getSecurities(@QueryParam("id") List<String> idStrs) {
+    ArgumentChecker.notEmpty(idStrs, "identifiers");
+    IdentifierBundle bundle = new IdentifierBundle();
+    for (String idStr : idStrs) {
+      bundle = bundle.withIdentifier(Identifier.parse(idStr));
+    }
+    final FudgeSerializationContext context = getFudgeSerializationContext();
+    final MutableFudgeFieldContainer msg = context.newMessage();
+    context.objectToFudgeMsg(msg, SECURITYMASTER_SECURITIES, null, getSecurityMaster().getSecurities(bundle));
+    return new FudgeMsgEnvelope(msg);
   }
-  
+
+  @GET
+  @Path("securities/types")
+  public FudgeMsgEnvelope getAllSecurityTypes() {
+    final FudgeSerializationContext context = getFudgeSerializationContext();
+    final MutableFudgeFieldContainer msg = context.newMessage();
+    context.objectToFudgeMsg(msg, SECURITYMASTER_ALLSECURITYTYPES, null, getSecurityMaster().getAllSecurityTypes());
+    return new FudgeMsgEnvelope(msg);
+  }
+
 }
