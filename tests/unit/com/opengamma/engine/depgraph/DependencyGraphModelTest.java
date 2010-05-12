1fc046ff65505f580a31ecde0be60ed6afef279b
Kirk Wylie <kirk@opengamma.com>
2010-05-12 12:30:07 +0100

    More testing, including checking removing unnecessary inputs and unsatisfiability.

--- tests/unit/com/opengamma/engine/depgraph/DependencyGraphModelTest.java
+++ tests/unit/com/opengamma/engine/depgraph/DependencyGraphModelTest.java
@@ -6,6 +6,7 @@
 package com.opengamma.engine.depgraph;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -63,6 +64,10 @@ public class DependencyGraphModelTest {
     assertEquals(1, graphs.size());
     DependencyGraph graph = graphs.iterator().next();
     assertNotNull(graph);
+    assertEquals(target, graph.getComputationTarget());
+    assertTrue(graph.getOutputValues().contains(spec1));
+    assertTrue(graph.getOutputValues().contains(spec2));
+    
     Collection<DependencyNode> nodes = graph.getDependencyNodes();
     assertNotNull(nodes);
     assertEquals(1, nodes.size());
@@ -70,6 +75,20 @@ public class DependencyGraphModelTest {
     assertEquals(fn1, node.getFunctionDefinition());
     assertTrue(node.getOutputValues().contains(spec1));
     assertTrue(node.getOutputValues().contains(spec2));
+    assertTrue(node.getInputNodes().isEmpty());
+    
+    model.removeUnnecessaryOutputs();
+    graphs = model.getDependencyGraphs(ComputationTargetType.PRIMITIVE);
+    graph = graphs.iterator().next();
+    assertTrue(graph.getOutputValues().contains(spec1));
+    assertFalse(graph.getOutputValues().contains(spec2));
+    nodes = graph.getDependencyNodes();
+    assertNotNull(nodes);
+    assertEquals(1, nodes.size());
+    node = nodes.iterator().next();
+    assertEquals(fn1, node.getFunctionDefinition());
+    assertTrue(node.getOutputValues().contains(spec1));
+    assertFalse(node.getOutputValues().contains(spec2));
     assertTrue(node.getInputNodes().isEmpty());
   }
   
@@ -117,5 +136,35 @@ public class DependencyGraphModelTest {
     assertTrue(node.getOutputValues().contains(spec1));
     assertTrue(node.getOutputValues().contains(spec2));
     assertTrue(node.getInputNodes().isEmpty());
+  }
+  
+  @Test(expected=UnsatisfiableDependencyGraphException.class)
+  public void unsatisfiableDependency() {
+    InMemoryFunctionRepository functionRepo = new InMemoryFunctionRepository();
+    UniqueIdentifier targetId = UniqueIdentifier.of("Scheme", "Value");
+    ComputationTarget target = new ComputationTarget(targetId);
+    ValueRequirement req1 = new ValueRequirement("Req-1", targetId);
+    ValueSpecification spec1 = new ValueSpecification(req1);
+    ComputedValue value1 = new ComputedValue(spec1, 14.2);
+    
+    ValueRequirement req2 = new ValueRequirement("Req-2", targetId);
+    ValueSpecification spec2 = new ValueSpecification(req2);
+    ComputedValue value2 = new ComputedValue(spec2, 15.5);
+    
+    ValueRequirement req3 = new ValueRequirement("Req-3", targetId);
+    
+    MockFunction fn1 = new MockFunction(target,
+        Collections.<ValueRequirement>emptySet(),
+        Sets.newHashSet(value1, value2));
+    functionRepo.addFunction(fn1, fn1);
+
+    DependencyGraphModel model = new DependencyGraphModel();
+    model.setFunctionRepository(functionRepo);
+    model.setLiveDataAvailabilityProvider(new FixedLiveDataAvailabilityProvider());
+    model.setFunctionResolver(new DefaultFunctionResolver(functionRepo));
+    model.setTargetResolver(new MapComputationTargetResolver());
+
+    model.addTarget(target, Sets.newHashSet(req1));
+    model.addTarget(target, Collections.singleton(req3));
   }
 }
