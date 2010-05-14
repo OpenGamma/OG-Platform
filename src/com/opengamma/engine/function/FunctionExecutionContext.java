97843cdf6e81f22508f00458306a2f94cd2e9d74
Jim Moores <jim.moores@gmail.com>
2010-05-14 17:34:52 +0100

    Changes for VaR

--- src/com/opengamma/engine/function/FunctionExecutionContext.java
+++ src/com/opengamma/engine/function/FunctionExecutionContext.java
@@ -8,6 +8,7 @@ package com.opengamma.engine.function;
 import javax.time.calendar.Clock;
 
 import com.opengamma.engine.historicaldata.HistoricalDataProvider;
+import com.opengamma.engine.security.SecurityMaster;
 import com.opengamma.engine.view.calcnode.ViewProcessorQuery;
 
 /**
@@ -16,6 +17,7 @@ import com.opengamma.engine.view.calcnode.ViewProcessorQuery;
  */
 public class FunctionExecutionContext extends AbstractFunctionContext {
   public static final String HISTORICAL_DATA_PROVIDER_NAME = "historicalDataProvider";
+  public static final String SECURITY_MASTER_NAME = "securityMaster";
   public static final String VIEW_PROCESSOR_QUERY_NAME = "viewProcessorQuery";
   public static final String SNAPSHOT_EPOCH_TIME_NAME = "snapshotEpochTime";
   public static final String SNAPSHOT_CLOCK_NAME = "snapshotClock";
@@ -26,6 +28,14 @@ public class FunctionExecutionContext extends AbstractFunctionContext {
   
   public void setHistoricalDataProvider(HistoricalDataProvider historicalDataProvider) {
     put(HISTORICAL_DATA_PROVIDER_NAME, historicalDataProvider);
+  }
+  
+  public SecurityMaster getSecurityMaster() {
+    return (SecurityMaster) get(SECURITY_MASTER_NAME);
+  }
+  
+  public void setSecurityMaster(SecurityMaster secMaster) {
+    put(SECURITY_MASTER_NAME, secMaster);
   }
   
   public ViewProcessorQuery getViewProcessorQuery() {
