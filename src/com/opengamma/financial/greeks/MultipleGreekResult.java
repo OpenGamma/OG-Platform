e152f8969a5bdf6ab2f222522c3ca4b50fe89797
Kirk Wylie <kirk@opengamma.com>
2010-05-21 09:53:19 +0100

    [ANA-71] Numerous changes and unit tests of MultipleGreekResult
    - Added review comments
    - Changed from IllegalArgumentException to ArgumentChecker.notNull() on null inputs
    - Added hashCode() and tested
    - Added equals() and tested
    - Added toString() and tested

--- src/com/opengamma/financial/greeks/MultipleGreekResult.java
+++ src/com/opengamma/financial/greeks/MultipleGreekResult.java
@@ -5,16 +5,24 @@
  */
 package com.opengamma.financial.greeks;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
+
+import com.opengamma.util.ArgumentChecker;
+
 public class MultipleGreekResult implements GreekResult<Map<String, Double>> {
+  // REVIEW kirk 2010-05-21 -- Something in Fastutil instead?
   private final Map<String, Double> _result;
 
   public MultipleGreekResult(final Map<String, Double> result) {
-    if (result == null)
-      throw new IllegalArgumentException("Result was null");
+    ArgumentChecker.notNull(result, "Results");
     if (result.isEmpty())
       throw new IllegalArgumentException("Result was empty");
+    // REVIEW kirk 2010-05-21 -- Why not take a copy and make it unmodifiable?
     _result = result;
   }
 
@@ -26,6 +34,38 @@ public class MultipleGreekResult implements GreekResult<Map<String, Double>> {
   @Override
   public boolean isMultiValued() {
     return true;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!(obj instanceof MultipleGreekResult)) {
+      return false;
+    }
+    final MultipleGreekResult other = (MultipleGreekResult) obj;
+    return ObjectUtils.equals(_result, other._result);
+  }
+
+  @Override
+  public int hashCode() {
+    return _result.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    sb.append("MultipleGreekResult[");
+    final List<String> elements = new LinkedList<String>();
+    for (final Map.Entry<String, Double> entry : _result.entrySet()) {
+      final StringBuilder sbElem = new StringBuilder();
+      sbElem.append(entry.getKey()).append("=").append(entry.getValue());
+      elements.add(sbElem.toString());
+    }
+    sb.append(StringUtils.join(elements, ", "));
+    sb.append("]");
+    return sb.toString();
   }
 
 }
--- tests/unit/com/opengamma/financial/greeks/MultipleGreekResultTest.java
+++ tests/unit/com/opengamma/financial/greeks/MultipleGreekResultTest.java
@@ -6,6 +6,8 @@
 package com.opengamma.financial.greeks;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Collections;
@@ -16,7 +18,7 @@ import org.junit.Test;
 
 public class MultipleGreekResultTest {
 
-  @Test(expected = IllegalArgumentException.class)
+  @Test(expected = NullPointerException.class)
   public void testNull() {
     new MultipleGreekResult(null);
   }
@@ -26,6 +28,7 @@ public class MultipleGreekResultTest {
     new MultipleGreekResult(Collections.<String, Double> emptyMap());
   }
 
+  @Test
   public void test() {
     final Map<String, Double> map = new HashMap<String, Double>();
     map.put("A", 1.);
@@ -34,4 +37,40 @@ public class MultipleGreekResultTest {
     assertTrue(result.isMultiValued());
     assertEquals(map, result.getResult());
   }
+
+  @Test
+  public void testHashCode() {
+    final Map<String, Double> map1 = new HashMap<String, Double>();
+    map1.put("A", 1.);
+    map1.put("B", 2.);
+    final MultipleGreekResult result1 = new MultipleGreekResult(map1);
+    MultipleGreekResult result2 = new MultipleGreekResult(map1);
+    assertTrue(result1.hashCode() == result2.hashCode());
+
+    Map<String, Double> map2 = new HashMap<String, Double>();
+    map2.put("A", 2.);
+    map2.put("B", 2.);
+    result2 = new MultipleGreekResult(map2);
+    assertFalse(result1.hashCode() == result2.hashCode());
+
+    map2 = new HashMap<String, Double>();
+    map2.put("A", 1.);
+    map2.put("C", 2.);
+    result2 = new MultipleGreekResult(map2);
+    assertFalse(result1.hashCode() == result2.hashCode());
+  }
+
+  @Test
+  public void testToString() {
+    final Map<String, Double> map1 = new HashMap<String, Double>();
+    map1.put("Apple", 1.);
+    map1.put("Banana", 2.);
+    final MultipleGreekResult result1 = new MultipleGreekResult(map1);
+    final String toString = result1.toString();
+    assertNotNull(toString);
+    assertTrue(toString.indexOf("MultipleGreekResult") > -1);
+    assertTrue(toString.indexOf("Apple") > -1);
+    assertTrue(toString.indexOf("Banana") > -1);
+  }
+
 }
