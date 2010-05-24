cc570214a88aa2f29d3fc19510b03be7bd5443b9
Jonathan Senior <jonathan@opengamma.com>
2010-05-24 13:04:11 +0100

    Adding more tests for CSVPositionMaster and fixing null unique identifier for root node

--- tests/unit/com/opengamma/engine/position/csv/CSVPositionMasterTest.java
+++ tests/unit/com/opengamma/engine/position/csv/CSVPositionMasterTest.java
@@ -9,10 +9,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
+import com.opengamma.engine.position.Portfolio;
+import com.opengamma.engine.position.PortfolioNode;
 import com.opengamma.engine.position.Position;
 import com.opengamma.id.Identifier;
 import com.opengamma.id.UniqueIdentifier;
@@ -73,4 +80,71 @@ public class CSVPositionMasterTest {
     }
   }
 
+  private String createTempTestPortfolioDirectory() throws IOException {
+    String tempDirName = getTempPortfolioDirectory();
+    File tempDir = new File(tempDirName);
+    if (!tempDir.mkdir()) {
+      throw new IOException("Could not create temporary directory for portfolio files at '" + tempDirName + "'");
+    }
+    
+    Collection<String> testPortfolio1 = new ArrayList<String>();
+    testPortfolio1.add("1000,BbgId,APVJS.X Equity");
+    testPortfolio1.add("1000,BbgId,APVJS.X Equity");
+    testPortfolio1.add("2000,BbgId,APVJN.X Equity");
+    testPortfolio1.add("5000,BbgId,AJLJV.X Equity");
+    testPortfolio1.add("3500,BbgId,IBMJE.X Equity");
+    testPortfolio1.add("450,BbgId,IBMJF.X Equity");
+    FileUtils.writeLines(new File(tempDir, "testPortfolio1.csv"), testPortfolio1);
+    
+    Collection<String> testPortfolio2 = new ArrayList<String>();
+    testPortfolio2.add("120,Domain1,Id1");
+    testPortfolio2.add("400,Domain1,Id2");
+    testPortfolio2.add("750,Domain2,Id1");
+    testPortfolio2.add("1000,Domain3,Id1");
+    FileUtils.writeLines(new File(tempDir, "testPortfolio2.port"), testPortfolio2);
+    
+    return tempDirName;
+  }
+
+  private String getTempPortfolioDirectory() {
+    return System.getProperty("java.io.tmpdir") + "/csvPositionMasterTest-" + System.currentTimeMillis();
+  }
+  
+  private void cleanUpTestPortfolios(String portfolioDirName) throws IOException {
+    FileUtils.deleteDirectory(new File(portfolioDirName));
+  }
+  
+  @Test
+  public void testLoadPortfolios() throws IOException {
+    String portfolioDirName = createTempTestPortfolioDirectory();
+    CSVPositionMaster pm = new CSVPositionMaster(portfolioDirName);
+    
+    assertEquals(2, pm.getPortfolioIds().size());
+    
+    // Loaded correctly
+    UniqueIdentifier[] portIds = pm.getPortfolioIds().toArray(new UniqueIdentifier[0]);
+    Portfolio port1 = pm.getPortfolio(portIds[0]);
+    assertEquals(6, port1.getRootNode().getPositions().size());   
+    Portfolio port2 = pm.getPortfolio(portIds[1]);
+    assertEquals(4, port2.getRootNode().getPositions().size());
+    
+    // Unknown portfolio should return null
+    Portfolio unknownPort = pm.getPortfolio(UniqueIdentifier.of("Wrong scheme", "Irrelevant value"));
+    assertNull(unknownPort);
+    
+    // Retrieval by root node
+    PortfolioNode rootNode1 = pm.getPortfolioNode(port1.getRootNode().getUniqueIdentifier());
+    assertEquals(6, rootNode1.getPositions().size());
+    
+    // Retrieval by position
+    Position port1Pos0 = rootNode1.getPositions().get(0);
+    assertEquals(port1Pos0, pm.getPosition(port1Pos0.getUniqueIdentifier()));
+    
+    cleanUpTestPortfolios(portfolioDirName);
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testNonexistentPortfolioDirectory() {
+    new CSVPositionMaster(new File(getTempPortfolioDirectory()));
+  }
 }
