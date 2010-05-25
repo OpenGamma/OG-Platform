4efbef5bdd17c13b5eef26d6b25adc7ef1a86ff8
Elaine McLeod <elaine@opengamma.com>
2010-05-25 13:57:45 +0100

    Added a test and switched to using ArgumentChecker.

--- src/com/opengamma/financial/covariance/CovarianceCalculator.java
+++ src/com/opengamma/financial/covariance/CovarianceCalculator.java
@@ -8,16 +8,15 @@ package com.opengamma.financial.covariance;
 import java.util.List;
 
 import com.opengamma.math.function.Function2D;
+import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.timeseries.DoubleTimeSeries;
 import com.opengamma.util.timeseries.TimeSeriesException;
 
 public abstract class CovarianceCalculator extends Function2D<DoubleTimeSeries<?>, Double> {
 
   protected void testTimeSeries(final DoubleTimeSeries<?> ts1, final DoubleTimeSeries<?> ts2) {
-    if (ts1 == null)
-      throw new IllegalArgumentException("First time series was null");
-    if (ts2 == null)
-      throw new IllegalArgumentException("Second time series was null");
+    ArgumentChecker.notNull(ts1, "ts1");
+    ArgumentChecker.notNull(ts2, "ts2");
     if (ts1.isEmpty())
       throw new IllegalArgumentException("First time series was empty");
     if (ts2.isEmpty())
---
+++ tests/unit/com/opengamma/financial/covariance/HistoricalCovarianceCalculatorTest.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.covariance;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.opengamma.financial.timeseries.returns.TimeSeriesReturnCalculator;
+import com.opengamma.util.CalculationMode;
+import com.opengamma.util.timeseries.DoubleTimeSeries;
+import com.opengamma.util.timeseries.fast.DateTimeNumericEncoding;
+import com.opengamma.util.timeseries.fast.integer.FastArrayIntDoubleTimeSeries;
+
+/**
+ * 
+ */
+public class HistoricalCovarianceCalculatorTest {
+  private static final DoubleTimeSeries<?> TS1 = new FastArrayIntDoubleTimeSeries(DateTimeNumericEncoding.DATE_EPOCH_DAYS, new int[] { 1, 2, 3, 4, 5 }, new double[] { 1, 1, 1, 1, 1 });
+  private static final TimeSeriesReturnCalculator RETURNS = new TimeSeriesReturnCalculator(CalculationMode.STRICT) {
+
+    @Override
+    public DoubleTimeSeries<?> evaluate(final DoubleTimeSeries<?>... x) {
+      return x[0];
+    }
+  };
+  private static final CovarianceCalculator CALCULATOR = new HistoricalCovarianceCalculator(RETURNS);
+
+  @Test(expected = NullPointerException.class)
+  public void testNull() {
+    new HistoricalCovarianceCalculator(null);
+  }
+
+  @Test
+  public void test() {
+    final double n = TS1.size();
+    final double covariance = CALCULATOR.evaluate(TS1, TS1);
+    assertEquals(covariance, n / (n - 1) - 1, 1e-9);
+  }
+}
