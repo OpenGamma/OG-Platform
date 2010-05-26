84ba25dc40cd04c8c947ca0166615a04daa46a8e
Elaine McLeod <elaine@opengamma.com>
2010-05-26 17:21:07 +0100

    Added argument checking and test for American vanilla option definitions.

---
+++ tests/unit/com/opengamma/financial/model/option/definition/AmericanVanillaOptionDefinitionTest.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.model.option.definition;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import javax.time.calendar.ZonedDateTime;
+
+import org.junit.Test;
+
+import com.opengamma.financial.model.interestrate.curve.ConstantInterestRateDiscountCurve;
+import com.opengamma.financial.model.volatility.surface.ConstantVolatilitySurface;
+import com.opengamma.util.time.DateUtil;
+import com.opengamma.util.time.Expiry;
+
+/**
+ * 
+ */
+public class AmericanVanillaOptionDefinitionTest {
+  private static final double DIFF = 10;
+  private static final double STRIKE = 100;
+  private static final double LOW_PRICE = 5;
+  private static final double HIGH_PRICE = LOW_PRICE + DIFF;
+  private static final ZonedDateTime DATE = DateUtil.getUTCDate(2010, 5, 1);
+  private static final Expiry EXPIRY = new Expiry(DateUtil.getDateOffsetWithYearFraction(DATE, 1));
+  private static final OptionDefinition CALL = new AmericanVanillaOptionDefinition(STRIKE, EXPIRY, true);
+  private static final OptionDefinition PUT = new AmericanVanillaOptionDefinition(STRIKE, EXPIRY, false);
+  private static final StandardOptionDataBundle DATA = new StandardOptionDataBundle(new ConstantInterestRateDiscountCurve(0.05), 0., new ConstantVolatilitySurface(0.2), STRIKE, DATE);
+  private static final double EPS = 1e-15;
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullDataInPayoff() {
+    CALL.getPayoffFunction().getPayoff(null, LOW_PRICE);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullOptionPriceInPayoff() {
+    CALL.getPayoffFunction().getPayoff(DATA, null);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNegativeOptionPriceInPayoff() {
+    CALL.getPayoffFunction().getPayoff(DATA, -LOW_PRICE);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullDataInExercise() {
+    CALL.getExerciseFunction().shouldExercise(null, LOW_PRICE);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullOptionPriceInExercise() {
+    CALL.getExerciseFunction().shouldExercise(DATA, -LOW_PRICE);
+  }
+
+  @Test
+  public void testExerciseFunction() {
+    OptionExerciseFunction<StandardOptionDataBundle> exercise = CALL.getExerciseFunction();
+    assertTrue(exercise.shouldExercise(DATA.withSpot(STRIKE + DIFF), LOW_PRICE));
+    assertFalse(exercise.shouldExercise(DATA.withSpot(STRIKE - DIFF), LOW_PRICE));
+    assertFalse(exercise.shouldExercise(DATA.withSpot(STRIKE + DIFF), HIGH_PRICE));
+    assertFalse(exercise.shouldExercise(DATA.withSpot(STRIKE - DIFF), HIGH_PRICE));
+    exercise = PUT.getExerciseFunction();
+    assertFalse(exercise.shouldExercise(DATA.withSpot(STRIKE + DIFF), LOW_PRICE));
+    assertTrue(exercise.shouldExercise(DATA.withSpot(STRIKE - DIFF), LOW_PRICE));
+    assertFalse(exercise.shouldExercise(DATA.withSpot(STRIKE + DIFF), HIGH_PRICE));
+    assertFalse(exercise.shouldExercise(DATA.withSpot(STRIKE - DIFF), HIGH_PRICE));
+  }
+
+  @Test
+  public void testPayoffFunction() {
+    OptionPayoffFunction<StandardOptionDataBundle> payoff = CALL.getPayoffFunction();
+    assertEquals(payoff.getPayoff(DATA.withSpot(STRIKE + DIFF), LOW_PRICE), DIFF, EPS);
+    assertEquals(payoff.getPayoff(DATA.withSpot(STRIKE - DIFF), LOW_PRICE), LOW_PRICE, EPS);
+    assertEquals(payoff.getPayoff(DATA.withSpot(STRIKE + DIFF), HIGH_PRICE), HIGH_PRICE, EPS);
+    assertEquals(payoff.getPayoff(DATA.withSpot(STRIKE - DIFF), HIGH_PRICE), HIGH_PRICE, EPS);
+    payoff = PUT.getPayoffFunction();
+    assertEquals(payoff.getPayoff(DATA.withSpot(STRIKE + DIFF), LOW_PRICE), LOW_PRICE, EPS);
+    assertEquals(payoff.getPayoff(DATA.withSpot(STRIKE - DIFF), LOW_PRICE), DIFF, EPS);
+    assertEquals(payoff.getPayoff(DATA.withSpot(STRIKE + DIFF), HIGH_PRICE), HIGH_PRICE, EPS);
+    assertEquals(payoff.getPayoff(DATA.withSpot(STRIKE - DIFF), HIGH_PRICE), HIGH_PRICE, EPS);
+  }
+}
