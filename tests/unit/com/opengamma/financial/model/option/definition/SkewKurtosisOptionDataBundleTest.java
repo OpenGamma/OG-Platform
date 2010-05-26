7cbf1ffd69aad1b15c465fde77373d36627637fe
Elaine McLeod <elaine@opengamma.com>
2010-05-26 17:44:58 +0100

    Tests and argument checking for SkewKurtosisOptionDataBundle

---
+++ tests/unit/com/opengamma/financial/model/option/definition/SkewKurtosisOptionDataBundleTest.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.model.option.definition;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import javax.time.calendar.ZonedDateTime;
+
+import org.junit.Test;
+
+import com.opengamma.financial.model.interestrate.curve.ConstantInterestRateDiscountCurve;
+import com.opengamma.financial.model.interestrate.curve.DiscountCurve;
+import com.opengamma.financial.model.volatility.surface.ConstantVolatilitySurface;
+import com.opengamma.financial.model.volatility.surface.VolatilitySurface;
+import com.opengamma.util.time.DateUtil;
+
+/**
+ * 
+ */
+public class SkewKurtosisOptionDataBundleTest {
+  private static final double R = 0.03;
+  private static final double SIGMA = 0.25;
+  private static final DiscountCurve CURVE = new ConstantInterestRateDiscountCurve(R);
+  private static final double B = 0.03;
+  private static final VolatilitySurface SURFACE = new ConstantVolatilitySurface(SIGMA);
+  private static final double SPOT = 100;
+  private static final ZonedDateTime DATE = DateUtil.getUTCDate(2010, 5, 1);
+  private static final double SKEW = 1.2;
+  private static final double KURTOSIS = 4.5;
+  private static final DiscountCurve OTHER_CURVE = new ConstantInterestRateDiscountCurve(R + 1);
+  private static final double OTHER_B = B + 1;
+  private static final VolatilitySurface OTHER_SURFACE = new ConstantVolatilitySurface(SIGMA + 1);
+  private static final double OTHER_SPOT = SPOT + 1;
+  private static final ZonedDateTime OTHER_DATE = DateUtil.getDateOffsetWithYearFraction(DATE, 1);
+  private static final double OTHER_SKEW = 0.1;
+  private static final double OTHER_KURTOSIS = 3;
+  private static final SkewKurtosisOptionDataBundle DATA = new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, SPOT, DATE, SKEW, KURTOSIS);
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullBundle() {
+    new SkewKurtosisOptionDataBundle(null);
+  }
+
+  @Test
+  public void testGetters() {
+    assertEquals(DATA.getDiscountCurve(), CURVE);
+    assertEquals(DATA.getCostOfCarry(), B, 0);
+    assertEquals(DATA.getDate(), DATE);
+    assertEquals(DATA.getSpot(), SPOT, 0);
+    assertEquals(DATA.getVolatilitySurface(), SURFACE);
+  }
+
+  @Test
+  public void testGetInterestRate() {
+    for (int i = 0; i < 10; i++) {
+      assertEquals(DATA.getInterestRate(Math.random()), R, 1e-15);
+    }
+  }
+
+  @Test
+  public void testGetVolatility() {
+    for (int i = 0; i < 10; i++) {
+      assertEquals(DATA.getVolatility(Math.random(), Math.random()), SIGMA, 1e-15);
+    }
+  }
+
+  @Test
+  public void testEqualsAndHashCode() {
+    final SkewKurtosisOptionDataBundle other = new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, SPOT, DATE, SKEW, KURTOSIS);
+    assertEquals(DATA, other);
+    assertEquals(DATA.hashCode(), other.hashCode());
+    assertFalse(DATA.equals(new SkewKurtosisOptionDataBundle(OTHER_CURVE, B, SURFACE, SPOT, DATE, SKEW, KURTOSIS)));
+    assertFalse(DATA.equals(new SkewKurtosisOptionDataBundle(CURVE, OTHER_B, SURFACE, SPOT, DATE, SKEW, KURTOSIS)));
+    assertFalse(DATA.equals(new SkewKurtosisOptionDataBundle(CURVE, B, OTHER_SURFACE, SPOT, DATE, SKEW, KURTOSIS)));
+    assertFalse(DATA.equals(new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, OTHER_SPOT, DATE, SKEW, KURTOSIS)));
+    assertFalse(DATA.equals(new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, SPOT, OTHER_DATE, SKEW, KURTOSIS)));
+    assertFalse(DATA.equals(new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, SPOT, DATE, OTHER_SKEW, KURTOSIS)));
+    assertFalse(DATA.equals(new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, SPOT, DATE, SKEW, OTHER_KURTOSIS)));
+  }
+
+  @Test
+  public void testBuilders() {
+    assertEquals(new SkewKurtosisOptionDataBundle(OTHER_CURVE, B, SURFACE, SPOT, DATE, SKEW, KURTOSIS), DATA.withDiscountCurve(OTHER_CURVE));
+    assertEquals(new SkewKurtosisOptionDataBundle(CURVE, OTHER_B, SURFACE, SPOT, DATE, SKEW, KURTOSIS), DATA.withCostOfCarry(OTHER_B));
+    assertEquals(new SkewKurtosisOptionDataBundle(CURVE, B, OTHER_SURFACE, SPOT, DATE, SKEW, KURTOSIS), DATA.withVolatilitySurface(OTHER_SURFACE));
+    assertEquals(new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, OTHER_SPOT, DATE, SKEW, KURTOSIS), DATA.withSpot(OTHER_SPOT));
+    assertEquals(new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, SPOT, OTHER_DATE, SKEW, KURTOSIS), DATA.withDate(OTHER_DATE));
+    assertEquals(new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, SPOT, DATE, OTHER_SKEW, KURTOSIS), DATA.withSkew(OTHER_SKEW));
+    assertEquals(new SkewKurtosisOptionDataBundle(CURVE, B, SURFACE, SPOT, DATE, SKEW, OTHER_KURTOSIS), DATA.withKurtosis(OTHER_KURTOSIS));
+  }
+}
