d642e8490e900e0df25b38fb6a3c5907c6840ec2
Stephen Colebourne <stephen@opengamma.com>
2010-05-27 17:39:11 +0100

    [FIN-82] Rewrite position master database
    Remove logging to debug stack overflow error

--- src/com/opengamma/engine/DefaultComputationTargetResolver.java
+++ src/com/opengamma/engine/DefaultComputationTargetResolver.java
@@ -151,7 +151,6 @@ public class DefaultComputationTargetResolver implements ComputationTargetResolv
   }
   
   private PortfolioNodeImpl resolvePortfolioNode(final UniqueIdentifier uid, final PortfolioNode node) {
-    System.out.println("Resolving: " + uid + " " + node);
     final PortfolioNodeImpl newNode = new PortfolioNodeImpl(node.getUniqueIdentifier(), node.getName());
     for (PortfolioNode child : node.getChildNodes()) {
       final ComputationTarget resolvedChild = getRecursiveResolver().resolve(new ComputationTargetSpecification(ComputationTargetType.MULTIPLE_POSITIONS, child.getUniqueIdentifier()));
