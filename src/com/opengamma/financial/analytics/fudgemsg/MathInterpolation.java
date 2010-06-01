a6780fcae9d077a462af8b0e87dc4ff257c3edb6
Stephen Colebourne <stephen@opengamma.com>
2010-06-01 12:49:17 +0100

    [DVI-34] Checkstyle
    Also changed scopes of builders as they weren't used elsewhere.

--- src/com/opengamma/financial/analytics/fudgemsg/MathInterpolation.java
+++ src/com/opengamma/financial/analytics/fudgemsg/MathInterpolation.java
@@ -17,44 +17,56 @@ import com.opengamma.math.interpolation.Interpolator1D;
 import com.opengamma.math.interpolation.Interpolator1DFactory;
 
 /**
- * 
- * 
+ * Holds Fudge builders for the interpolation model.
  */
-public class MathInterpolation {
-  
+/* package */ final class MathInterpolation {
+
   private static final FudgeBuilder<GridInterpolator2D> GRID_INTERPOLATOR_2D = new GridInterpolator2DBuilder();
   private static final FudgeBuilder<Interpolator1D> INTERPOLATOR_1D = new Interpolator1DBuilder();
-  
+
+  /**
+   * Restricted constructor.
+   */
   private MathInterpolation() {
   }
-  
-  public static class GridInterpolator2DBuilder extends FudgeBuilderBase<GridInterpolator2D> {
-    
+
+  /* package */ static void addBuilders(final FudgeObjectDictionary dictionary) {
+    dictionary.addBuilder(GridInterpolator2D.class, GRID_INTERPOLATOR_2D);
+    dictionary.getDefaultBuilderFactory().addGenericBuilder(Interpolator1D.class, INTERPOLATOR_1D);
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Fudge builder for {@code GridInterpolator2D}.
+   */
+  private static final class GridInterpolator2DBuilder extends FudgeBuilderBase<GridInterpolator2D> {
     private static final String X_FIELD_NAME = "x";
     private static final String Y_FIELD_NAME = "y";
-    
+
     private GridInterpolator2DBuilder() {
     }
-    
+
     @Override
     public void buildMessage(final FudgeSerializationContext context, final MutableFudgeFieldContainer message, final GridInterpolator2D object) {
-      context.objectToFudgeMsg(message, X_FIELD_NAME, null, object.getXInterpolator ());
-      context.objectToFudgeMsg(message, Y_FIELD_NAME, null, object.getYInterpolator ());
+      context.objectToFudgeMsg(message, X_FIELD_NAME, null, object.getXInterpolator());
+      context.objectToFudgeMsg(message, Y_FIELD_NAME, null, object.getYInterpolator());
     }
-    
+
     @Override
     public GridInterpolator2D buildObject(final FudgeDeserializationContext context, final FudgeFieldContainer message) {
       return new GridInterpolator2D(
           context.fieldValueToObject(Interpolator1D.class, message.getByName(X_FIELD_NAME)),
           context.fieldValueToObject(Interpolator1D.class, message.getByName(Y_FIELD_NAME)));
     }
-    
   }
-  
-  public static class Interpolator1DBuilder implements FudgeBuilder<Interpolator1D> {
-    
+
+  //-------------------------------------------------------------------------
+  /**
+   * Fudge builder for {@code Interpolator1D}.
+   */
+  private static final class Interpolator1DBuilder implements FudgeBuilder<Interpolator1D> {
     private static final String TYPE_FIELD_NAME = "type";
-    
+
     private Interpolator1DBuilder() {
     }
 
@@ -70,12 +82,6 @@ public class MathInterpolation {
     public Interpolator1D buildObject(FudgeDeserializationContext context, FudgeFieldContainer message) {
       return Interpolator1DFactory.getInterpolator(message.getFieldValue(String.class, message.getByName(TYPE_FIELD_NAME)));
     }
-    
   }
-  
-  /* package */ static void addBuilders(final FudgeObjectDictionary dictionary) {
-    dictionary.addBuilder(GridInterpolator2D.class, GRID_INTERPOLATOR_2D);
-    dictionary.getDefaultBuilderFactory().addGenericBuilder(Interpolator1D.class, INTERPOLATOR_1D);
-  }
-  
+
 }
