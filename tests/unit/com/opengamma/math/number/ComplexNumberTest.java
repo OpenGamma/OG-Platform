a97cc6660278e14d522276e8a21e97c86467c81f
Elaine McLeod <elaine@opengamma.com>
2010-06-03 16:25:11 +0100

    [ANA-76] Tidying up complex number test and added more equals() and hashCode() tests

--- tests/unit/com/opengamma/math/number/ComplexNumberTest.java
+++ tests/unit/com/opengamma/math/number/ComplexNumberTest.java
@@ -6,7 +6,7 @@
 package com.opengamma.math.number;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -14,46 +14,48 @@ import org.junit.Test;
  * 
  */
 public class ComplexNumberTest {
+  private static final ComplexNumber Z1 = new ComplexNumber(1, 2);
+  private static final ComplexNumber Z2 = new ComplexNumber(1, 2);
+  private static final ComplexNumber Z3 = new ComplexNumber(1, 3);
+  private static final ComplexNumber Z4 = new ComplexNumber(2, 2);
+  private static final ComplexNumber Z5 = new ComplexNumber(2, 3);
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testByteValue() {
+    Z1.byteValue();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testIntValue() {
+    Z1.intValue();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testLongValue() {
+    Z1.longValue();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testFloatValue() {
+    Z1.floatValue();
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testDoubleValue() {
+    Z1.doubleValue();
+  }
 
   @Test
   public void test() {
-    final ComplexNumber z1 = new ComplexNumber(1, 2);
-    final ComplexNumber z2 = new ComplexNumber(1, 2);
-    assertEquals(Double.valueOf(1), Double.valueOf(z1.getReal()));
-    assertEquals(Double.valueOf(2), Double.valueOf(z1.getImaginary()));
-    assertEquals(z1, z2);
-    try {
-      z1.byteValue();
-      fail();
-    } catch (final UnsupportedOperationException e) {
-      // Expected
-    }
-    try {
-      z1.intValue();
-      fail();
-    } catch (final UnsupportedOperationException e) {
-      // Expected
-    }
-    try {
-      z1.longValue();
-      fail();
-    } catch (final UnsupportedOperationException e) {
-      // Expected
-    }
-    try {
-      z1.floatValue();
-      fail();
-    } catch (final UnsupportedOperationException e) {
-      // Expected
-    }
-    try {
-      z1.doubleValue();
-      fail();
-    } catch (final UnsupportedOperationException e) {
-      // Expected
-    }
-    assertEquals("1.0 + 2.0i", z1.toString());
+    assertEquals(Double.valueOf(1), Double.valueOf(Z1.getReal()));
+    assertEquals(Double.valueOf(2), Double.valueOf(Z1.getImaginary()));
+    assertEquals(Z1, Z2);
+    assertEquals(Z1.hashCode(), Z2.hashCode());
+    assertEquals("1.0 + 2.0i", Z1.toString());
     assertEquals("1.0", new ComplexNumber(1, 0).toString());
     assertEquals("2.3i", new ComplexNumber(0, 2.3).toString());
+    assertFalse(Z1.equals(Z3));
+    assertFalse(Z1.equals(Z4));
+    assertFalse(Z1.equals(Z5));
   }
 }
