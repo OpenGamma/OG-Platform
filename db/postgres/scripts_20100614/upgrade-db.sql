c864b509862f07f3dbb3b1bd0a292df4e943c5cd
Stephen Colebourne <stephen@opengamma.com>
2010-06-16 14:09:41 +0100

    [FIN-91] Enhance database position master to allow for website management
    Fix database upgrade scripts

--- db/postgres/scripts_20100614/upgrade-db.sql
+++ db/postgres/scripts_20100614/upgrade-db.sql
@@ -33,7 +33,7 @@ create table pos_node (
     end_version bigint not null,
     name varchar(255),
     primary key (oid, start_version),
-    foreign key (portfolio_oid, start_version) references pos_portfolio(oid, version)
+    constraint pos_fk_node2portfolio foreign key (portfolio_oid, start_version) references pos_portfolio (oid, version)
 );
 
 create table pos_nodetree (
@@ -45,7 +45,7 @@ create table pos_nodetree (
     left_id bigint not null,
     right_id bigint not null,
     primary key (node_oid, start_version),
-    foreign key (portfolio_oid, start_version) references pos_portfolio(oid, version)
+    constraint pos_fk_nodetree2portfolio foreign key (portfolio_oid, start_version) references pos_portfolio (oid, version)
 );
 -- portfolio_oid is an optimization
 -- parent_node_oid is an optimization (left_id/right_id hold all the tree structure)
@@ -58,7 +58,7 @@ create table pos_position (
     end_version bigint not null,
     quantity decimal not null,
     primary key (oid, start_version),
-    foreign key (portfolio_oid, start_version) references pos_portfolio(oid, version)
+    constraint pos_fk_position2portfolio foreign key (portfolio_oid, start_version) references pos_portfolio (oid, version)
 );
 -- portfolio_oid is an optimization
 
@@ -68,7 +68,7 @@ create table pos_securitykey (
     id_scheme varchar(255) not null,
     id_value varchar(255) not null,
     primary key (position_oid, position_version, id_scheme, id_value),
-    foreign key (position_oid, position_version) references pos_position(oid, start_version)
+    constraint pos_fk_securitykey2position foreign key (position_oid, position_version) references pos_position (oid, start_version)
 );
 -- pos_securitykey is fully dependent of pos_position
 -- pos_securitykey.position_version = pos_position.start_version
