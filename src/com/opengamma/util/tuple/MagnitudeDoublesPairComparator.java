bad57cae32d7990f881c97ca92d338c3fdde5eb6
Stephen Colebourne <stephen@opengamma.com>
2010-07-05 12:26:47 +0100

    [UTL-51] Enhance pair classes
    Add comparators dedicated to DoublesPair

---
+++ src/com/opengamma/util/tuple/MagnitudeDoublesPairComparator.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.util.tuple;
+
+import java.util.Comparator;
+
+/**
+ * A comparator for a {@code DoublesPair} that evaluates the magnitude of the pairs.
+ * <p>
+ * This comparator first calculates the magnitude by adding the square of the first
+ * element in the pair to the square of the second element in the pair.
+ * If the magnitudes differ then the result is returned based on the magnitude.
+ * If the magnitude is equal then the {@link QuadrantPairComparator} is used.
+ * <p>
+ * This comparator does not support {@code null} elements in the pair.
+ */
+public final class MagnitudeDoublesPairComparator implements Comparator<DoublesPair> {
+
+  /**
+   * Singleton instance.
+   */
+  public static final MagnitudeDoublesPairComparator INSTANCE = new MagnitudeDoublesPairComparator();
+
+  /**
+   * Restrictive constructor.
+   */
+  private MagnitudeDoublesPairComparator() {
+  }
+
+  @Override
+  public int compare(DoublesPair p1, DoublesPair p2) {
+    double x1 = p1.first;
+    double y1 = p1.second;
+    double x2 = p2.first;
+    double y2 = p2.second;
+    double z1 = x1 * x1 + y1 * y1;
+    double z2 = x2 * x2 + y2 * y2;
+    if (z1 == z2) {
+      return QuadrantPairComparator.INSTANCE.compare(p1, p2);
+    }
+    return z1 < z2 ? -1 : 1;
+  }
+
+}
---
+++ src/com/opengamma/util/tuple/QuadrantDoublesPairComparator.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.util.tuple;
+
+import java.util.Comparator;
+
+/**
+ * A comparator for a {@code DoublesPair} that evaluates which quadrant the elements are in.
+ * <p>
+ * This comparator first calculates the quadrant from 1 to 4 for each pair treating
+ * the first element as x and the second element as y.
+ * If the quadrants differ then the result is returned based on the quadrant value.
+ * If the quadrants are equal, {@link FirstThenSecondDoublesPairComparator} is used.
+ * <p>
+ * This comparator does not support {@code null} elements in the pair.
+ */
+public final class QuadrantDoublesPairComparator implements Comparator<DoublesPair> {
+
+  /**
+   * Singleton instance.
+   */
+  public static final QuadrantDoublesPairComparator INSTANCE = new QuadrantDoublesPairComparator();
+
+  /**
+   * Restrictive constructor.
+   */
+  private QuadrantDoublesPairComparator() {
+  }
+
+  @Override
+  public int compare(DoublesPair p1, DoublesPair p2) {
+    if (p1.equals(p2)) {
+      return 0;
+    }
+    int firstQuadrant = getQuadrant(p1);
+    int secondQuadrant = getQuadrant(p2);
+    if (firstQuadrant == secondQuadrant) {
+      return FirstThenSecondDoublesPairComparator.INSTANCE.compare(p1, p2);
+    }
+    return firstQuadrant < secondQuadrant ? -1 : 1;
+  }
+
+  /**
+   * Find the correct quadrant using standard numbering.
+   * This returns 1 for x and y GE zero, 2 if only x is negative,
+   * 3 if both are negative, and 4 if only y is negative.
+   * @param pair  the pair to extract from, not null
+   * @return the quadrant from 1 to 4
+   */
+  private int getQuadrant(DoublesPair pair) {
+    double x = pair.first;
+    double y = pair.second;
+    if (x >= 0) {
+      return (y >= 0 ? 1 : 4);
+    } else {
+      return (y >= 0 ? 2 : 3);
+    }
+  }
+
+}
