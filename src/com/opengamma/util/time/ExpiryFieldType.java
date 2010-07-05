b9d1dc065d2c73f6abf857116b9bf58c50334054
Andrew Griffin <andrew@opengamma.com>
2010-07-05 11:52:11 +0100

    Encode Expiry as a Fudge secondary type to override the build in DateProvider behaviour which was taking precedence over the toFudgeMsg/fromFudgeMsg methods.

---
+++ src/com/opengamma/util/time/ExpiryFieldType.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.util.time;
+
+import javax.time.calendar.LocalDateTime;
+import javax.time.calendar.TimeZone;
+import javax.time.calendar.ZonedDateTime;
+
+import org.fudgemsg.types.DateFieldType;
+import org.fudgemsg.types.FudgeDate;
+import org.fudgemsg.types.SecondaryFieldType;
+
+/**
+ * Secondary type for Expiry conversion to/from Fudge date type.
+ *
+ * @author Andrew Griffin
+ */
+public final class ExpiryFieldType extends SecondaryFieldType<Expiry, FudgeDate> {
+
+  /**
+   * Singleton instance of the type.
+   */
+  public static final ExpiryFieldType INSTANCE = new ExpiryFieldType();
+
+  private ExpiryFieldType() {
+    super(DateFieldType.INSTANCE, Expiry.class);
+  }
+
+  /**
+   * 
+   * @param object the Expiry
+   * @return the FudgeDate representation of the Expiry
+   */
+  @Override
+  public FudgeDate secondaryToPrimary(final Expiry object) {
+    switch (object.getAccuracy()) {
+      case DAY_MONTH_YEAR:
+        return new FudgeDate(object.getExpiry().getYear(), object.getExpiry().getMonthOfYear().getValue(), object
+            .getExpiry().getDayOfMonth());
+      case MONTH_YEAR:
+        return new FudgeDate(object.getExpiry().getYear(), object.getExpiry().getMonthOfYear().getValue());
+      case YEAR:
+        return new FudgeDate(object.getExpiry().getYear());
+      default:
+        throw new IllegalArgumentException("Invalid accuracy value on " + object);
+    }
+  }
+
+  /**
+   * 
+   * @param data the FudgeDate representation
+   * @return the Expiry
+   */
+  @Override
+  public Expiry primaryToSecondary(final FudgeDate data) {
+    final ZonedDateTime zdt = ZonedDateTime.of(LocalDateTime.ofMidnight(data), TimeZone.UTC);
+    switch (data.getAccuracy()) {
+      case DAY:
+        return new Expiry(zdt, ExpiryAccuracy.DAY_MONTH_YEAR);
+      case MONTH:
+        return new Expiry(zdt, ExpiryAccuracy.MONTH_YEAR);
+      case YEAR:
+        return new Expiry(zdt, ExpiryAccuracy.YEAR);
+      default:
+        throw new IllegalArgumentException("Invalid accuracy value on " + data);
+    }
+  }
+
+}
--- tests/unit/com/opengamma/util/time/ExpiryTest.java
+++ tests/unit/com/opengamma/util/time/ExpiryTest.java
@@ -7,18 +7,40 @@ package com.opengamma.util.time;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.time.calendar.LocalDateTime;
+import javax.time.calendar.TimeZone;
 import javax.time.calendar.ZonedDateTime;
 
 import org.fudgemsg.FudgeContext;
-import org.fudgemsg.FudgeMsgEnvelope;
+import org.fudgemsg.FudgeFieldContainer;
+import org.fudgemsg.MutableFudgeFieldContainer;
+import org.fudgemsg.mapping.FudgeDeserializationContext;
+import org.fudgemsg.mapping.FudgeSerializationContext;
 import org.junit.Test;
 public class ExpiryTest {
 
+  private static final FudgeContext s_fudgeContext = new FudgeContext();
+
+  static {
+    s_fudgeContext.getTypeDictionary().addType(ExpiryFieldType.INSTANCE);
+  }
+
+  private static FudgeFieldContainer cycleMessage(final FudgeFieldContainer message) {
+    final byte[] encoded = s_fudgeContext.toByteArray(message);
+    return s_fudgeContext.deserialize(encoded).getMessage();
+  }
+
   @Test
   public void testFudgeMessage () {
-    final Expiry expiry = new Expiry (ZonedDateTime.nowSystemClock (), ExpiryAccuracy.MONTH_YEAR);
-    final FudgeMsgEnvelope env = FudgeContext.GLOBAL_DEFAULT.toFudgeMsg(expiry);
-    final Expiry result = FudgeContext.GLOBAL_DEFAULT.fromFudgeMsg (Expiry.class, env.getMessage ());
+    final Expiry expiry = new Expiry(ZonedDateTime.of(LocalDateTime.ofMidnight(2010, 7, 1), TimeZone.UTC),
+        ExpiryAccuracy.MONTH_YEAR);
+    final FudgeSerializationContext serContext = new FudgeSerializationContext(s_fudgeContext);
+    final MutableFudgeFieldContainer messageIn = serContext.newMessage();
+    serContext.objectToFudgeMsg(messageIn, "test", null, expiry);
+    final FudgeFieldContainer messageOut = cycleMessage(messageIn);
+    final FudgeDeserializationContext dsrContext = new FudgeDeserializationContext(s_fudgeContext);
+    final Expiry result = dsrContext.fieldValueToObject(Expiry.class, messageOut.getByName("test"));
     assertEquals(expiry, result);
   }
+
 }
