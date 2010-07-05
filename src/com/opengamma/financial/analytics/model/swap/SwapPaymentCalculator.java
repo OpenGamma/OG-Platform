e668dd424660a4c4f396f54cef73a1b2a54059ba
Elaine McLeod <elaine@opengamma.com>
2010-07-05 14:06:16 +0100

    Added a calculator and function to perform basic present value calculations on vanilla swaps

---
+++ src/com/opengamma/financial/analytics/model/swap/SwapPaymentCalculator.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.model.swap;
+
+import org.apache.commons.lang.Validate;
+
+import com.opengamma.financial.security.swap.FixedInterestRateLeg;
+import com.opengamma.financial.security.swap.FloatingInterestRateLeg;
+import com.opengamma.financial.security.swap.InterestRateNotional;
+
+/**
+ * 
+ */
+public class SwapPaymentCalculator {
+
+  public static double[] getFixedPayments(final FixedInterestRateLeg leg, final double[] paymentTimes, final boolean payFixed) {
+    Validate.notNull(leg);
+    Validate.notNull(paymentTimes);
+    if (paymentTimes.length == 0) {
+      throw new IllegalArgumentException("Must have at least one payment");
+    }
+    final int n = paymentTimes.length;
+    final double[] result = new double[n];
+    final double amount = (payFixed ? -1 : 1) * leg.getRate() * ((InterestRateNotional) leg.getNotional()).getAmount();
+    result[0] = paymentTimes[0] * amount;
+    for (int i = 1; i < paymentTimes.length; i++) {
+      result[i] = amount * (paymentTimes[i] - paymentTimes[i - 1]); //TODO this might not work for some of the crazier daycount conventions
+    }
+    return result;
+  }
+
+  public static double getFirstFloatPayment(final FloatingInterestRateLeg leg, final double[] paymentTimes, final boolean payFixed) {
+    Validate.notNull(leg);
+    Validate.notNull(paymentTimes);
+    if (paymentTimes.length == 0) {
+      throw new IllegalArgumentException("Must have at least one payment");
+    }
+    final double amount = (payFixed ? 1 : -1) * leg.getInitialFloatingRate() * ((InterestRateNotional) leg.getNotional()).getAmount();
+    return paymentTimes[0] * amount;
+  }
+}
