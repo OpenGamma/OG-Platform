8724db093d5c232ee3ea5d4aaeeb4dd12f63d090
Pietari Laurila <pietari@opengamma.com>
2010-07-06 15:30:19 +0100

    Added test exercising Mongo when running a batch

--- tests/unit/com/opengamma/engine/batch/BatchJobTest.java
+++ tests/unit/com/opengamma/engine/batch/BatchJobTest.java
@@ -7,6 +7,13 @@ package com.opengamma.engine.batch;
 
 import org.junit.Test;
 
+import com.opengamma.config.db.MongoDBConfigurationRepo;
+import com.opengamma.engine.view.View;
+import com.opengamma.engine.view.ViewDefinition;
+import com.opengamma.engine.view.ViewTest;
+import com.opengamma.util.MongoDBConnectionSettings;
+import com.opengamma.util.test.MongoDBTestUtils;
+
 /**
  * 
  */
@@ -16,6 +23,25 @@ public class BatchJobTest {
   public void minimumCommandLine() {
     BatchJob job = new BatchJob();
     job.parse("-view TestPortfolio".split(" "));
+  }
+  
+  @Test
+  public void initViewFromMongo() {
+    View testView = ViewTest.getMockView();
+    
+    MongoDBConnectionSettings settings = MongoDBTestUtils.makeTestSettings(null, false);
+    MongoDBConfigurationRepo<ViewDefinition> configRepo = new MongoDBConfigurationRepo<ViewDefinition>(ViewDefinition.class, settings, null);
+    configRepo.insertNewItem("MyView", testView.getDefinition());
+
+    BatchJob job = new BatchJob();
+    job.setBatchDbManager(new DummyBatchDbManager());
+    job.setPositionMaster(testView.getProcessingContext().getPositionMaster());
+    job.setSecurityMaster(testView.getProcessingContext().getSecurityMaster());
+    job.setFunctionRepository(testView.getProcessingContext().getFunctionRepository());
+    job.setViewName("MyView");
+    job.setConfigDbConnectionSettings(settings);
+
+    job.init();
   }
   
 }
