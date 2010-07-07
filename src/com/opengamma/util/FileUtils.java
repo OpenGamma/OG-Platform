589219359b70dc0fea0c9b216945a92d848285e6
Jim Moores <jim.moores@gmail.com>
2010-07-07 14:58:41 +0100

    Added temporary FileUtils class containing method to optionally generate drive letters.  This should be eventually removed along with all path references from the codebase.

---
+++ src/com/opengamma/util/FileUtils.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.util;
+
+/**
+ * Collection of file utils
+ * REVIEW: jim 7-July-2009 - there's almost certainly something that I can do this, this shouldn't be in here long term anyway. 
+ *                           File paths shouldn't be embedded in the code anyway.
+ */
+public class FileUtils {
+  public static String getSharedDrivePrefix() {
+    if (System.getProperty("os.name").contains("Windows")) {
+      return "O:";
+    } else {
+      return "";
+    }
+  }
+}
