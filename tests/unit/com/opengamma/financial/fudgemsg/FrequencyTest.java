86e7a8302b1b3f488992ead937dddf3b90695387
Andrew Griffin <andrew@opengamma.com>
2010-07-07 12:32:33 +0100

    Added tests for the OG-Financial Fudge type converters

---
+++ tests/unit/com/opengamma/financial/fudgemsg/FrequencyTest.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.fudgemsg;
+
+import static org.junit.Assert.assertEquals;
+
+import org.fudgemsg.FudgeMsgField;
+import org.fudgemsg.types.StringFieldType;
+import org.junit.Test;
+
+import com.opengamma.financial.convention.frequency.Frequency;
+import com.opengamma.financial.convention.frequency.SimpleFrequency;
+
+public class FrequencyTest extends FinancialTestBase {
+
+  private static final Frequency s_ref = SimpleFrequency.BIMONTHLY;
+
+  @Test
+  public void testCycle() {
+    assertEquals(s_ref, cycleObject(Frequency.class, s_ref));
+  }
+
+  @Test
+  public void testFromString() {
+    assertEquals(s_ref, getFudgeContext().getFieldValue(Frequency.class,
+        new FudgeMsgField(StringFieldType.INSTANCE, s_ref.getConventionName(), null, null)));
+  }
+}
