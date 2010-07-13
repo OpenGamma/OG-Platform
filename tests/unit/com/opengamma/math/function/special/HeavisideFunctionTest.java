e17586412a8227369b96c320bd6a4bc54bb97085
Elaine McLeod <elaine@opengamma.com>
2010-07-13 14:45:32 +0100

    [ANA-125] Added Heaviside function

---
+++ tests/unit/com/opengamma/math/function/special/HeavisideFunctionTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.math.function.special;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.opengamma.math.function.Function1D;
+
+/**
+ * 
+ */
+public class HeavisideFunctionTest {
+  private static final Function1D<Double, Double> F = new HeavisideFunction();
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNull() {
+    F.evaluate((Double) null);
+  }
+
+  @Test
+  public void test() {
+    assertEquals(F.evaluate(-2.), 0, 0);
+    assertEquals(F.evaluate(-1e-15), 0, 0);
+    assertEquals(F.evaluate(0.), 1, 0);
+    assertEquals(F.evaluate(1e-15), 1, 0);
+    assertEquals(F.evaluate(2.), 1, 0);
+  }
+}
