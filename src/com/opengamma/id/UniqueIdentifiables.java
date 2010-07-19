76d94e70e2d922da0ba4839d945ea0598e194d29
Stephen Colebourne <stephen@opengamma.com>
2010-07-19 16:25:11 +0100

    [FIN-111] Refactor SecurityMaster
    Add UniqueIdentifiables utility class to wrap setting of MutableUniqueIdentifier

---
+++ src/com/opengamma/id/UniqueIdentifiables.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.id;
+
+/**
+ * Provides uniform access to objects that support having their unique identifier
+ * updated after construction.
+ * <p>
+ * For example, code in the database layer will need to update the unique identifier
+ * when the object is stored.
+ */
+public final class UniqueIdentifiables {
+
+  /**
+   * Restricted constructor.
+   */
+  private UniqueIdentifiables() {
+  }
+
+  /**
+   * Sets the unique identifier of an object if it implements {@code MutableUniqueIdentifiable}.
+   * @param object  the object to set into
+   * @param uid  the unique identifier to set
+   */
+  public static void setInto(Object object, UniqueIdentifier uid) {
+    if (object instanceof MutableUniqueIdentifiable) {
+      ((MutableUniqueIdentifiable) object).setUniqueIdentifier(uid);
+    }
+  }
+
+}
