71597c4c28bff0a4491dda5d8d6baa515b053f87
Elaine McLeod <elaine@opengamma.com>
2010-07-20 11:13:55 +0100

    [ANA-147] Added cash-or-nothing binary option definitions.

---
+++ src/com/opengamma/financial/model/option/definition/CashOrNothingOptionDefinition.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.model.option.definition;
+
+import com.opengamma.util.ArgumentChecker;
+import com.opengamma.util.time.Expiry;
+
+/**
+ * 
+ */
+public class CashOrNothingOptionDefinition extends OptionDefinition {
+  private final OptionExerciseFunction<StandardOptionDataBundle> _exerciseFunction = new EuropeanExerciseFunction<StandardOptionDataBundle>();
+  private final OptionPayoffFunction<StandardOptionDataBundle> _payoffFunction = new OptionPayoffFunction<StandardOptionDataBundle>() {
+
+    @SuppressWarnings("synthetic-access")
+    @Override
+    public double getPayoff(final StandardOptionDataBundle data, final Double optionPrice) {
+      final double s = data.getSpot();
+      final double k = getStrike();
+      return isCall() ? (s < k ? _payment : 0) : s > k ? _payment : 0;
+    }
+
+  };
+  private final double _payment;
+
+  public CashOrNothingOptionDefinition(final double strike, final Expiry expiry, final boolean isCall, final double payment) {
+    super(strike, expiry, isCall);
+    ArgumentChecker.notNegative(payment, "payment");
+    _payment = payment;
+  }
+
+  @Override
+  public OptionExerciseFunction<StandardOptionDataBundle> getExerciseFunction() {
+    return _exerciseFunction;
+  }
+
+  @Override
+  public OptionPayoffFunction<StandardOptionDataBundle> getPayoffFunction() {
+    return _payoffFunction;
+  }
+
+  public double getPayment() {
+    return _payment;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    long temp;
+    temp = Double.doubleToLongBits(_payment);
+    result = prime * result + (int) (temp ^ (temp >>> 32));
+    return result;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!super.equals(obj)) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final CashOrNothingOptionDefinition other = (CashOrNothingOptionDefinition) obj;
+    if (Double.doubleToLongBits(_payment) != Double.doubleToLongBits(other._payment)) {
+      return false;
+    }
+    return true;
+  }
+
+}
