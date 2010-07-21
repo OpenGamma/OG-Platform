5c98da9546c9833a80c4c25160ffae49852c5bea
Pietari Laurila <pietari@opengamma.com>
2010-07-21 10:04:07 +0100

    [LIV-28] Reduce number of threads created by JmsLiveDataClient

--- src/com/opengamma/engine/livedata/LiveDataSnapshotListener.java
+++ src/com/opengamma/engine/livedata/LiveDataSnapshotListener.java
@@ -31,6 +31,17 @@ public interface LiveDataSnapshotListener {
   void subscriptionStopped(ValueRequirement requirement);
   
   /**
+   * Important implementation note to listener implementors:
+   * THIS METHOD MUST EXECUTE QUICKLY. The way the Live Data Client works,
+   * several market data lines may be handled by the same Live Data Client
+   * thread. If the execution of this method is slow, for example
+   * because it uses external resources such as files or the network
+   * in some way, or executes a complicated algorithm, then 
+   * market data updates for OTHER market data lines will not received
+   * until this method returns. Thus, if you need to execute a
+   * non-trivial operation when you receive new market data, 
+   * do it in a new thread.      
+
    * @param requirement Not null
    */
   void valueChanged(ValueRequirement requirement);
