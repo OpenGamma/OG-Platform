e504ab483d5856d8049944293aae5d299ccf2e6a
Elaine McLeod <elaine@opengamma.com>
2010-07-22 15:10:16 +0100

    [ANA-141] Parameter estimation using moment matching for the generalized Pareto distribution.

---
+++ tests/unit/com/opengamma/math/statistics/estimation/GeneralizedParetoDistributionMomentEstimatorTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.math.statistics.estimation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import cern.jet.random.engine.MersenneTwister64;
+
+import com.opengamma.math.statistics.distribution.GeneralizedParetoDistribution;
+import com.opengamma.math.statistics.distribution.ProbabilityDistribution;
+
+/**
+ * 
+ */
+public class GeneralizedParetoDistributionMomentEstimatorTest {
+  private static final double MU = 6;
+  private static final double SIGMA = 0.5;
+  private static final double KSI = 0.1;
+  private static final ProbabilityDistribution<Double> GPD = new GeneralizedParetoDistribution(MU, SIGMA, KSI, new MersenneTwister64(MersenneTwister64.DEFAULT_SEED));
+  private static final int N = 100000;
+  private static final double[] X = new double[N];
+  private static final DistributionParameterEstimator<Double> ESTIMATOR = new GeneralizedParetoDistributionMomentEstimator();
+  static {
+    for (int i = 0; i < N; i++) {
+      X[i] = GPD.nextRandom();
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNullArray() {
+    ESTIMATOR.evaluate((double[]) null);
+  }
+
+  @Test
+  public void test() {
+    final double eps = 0.05;
+    final GeneralizedParetoDistribution result = (GeneralizedParetoDistribution) ESTIMATOR.evaluate(X);
+    assertEquals(MU, result.getMu(), MU * eps);
+    assertEquals(SIGMA, result.getSigma(), SIGMA * eps);
+    assertEquals(KSI, result.getKsi(), KSI * eps);
+  }
+}
