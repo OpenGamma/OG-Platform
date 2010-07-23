439c0655fa256ab332bf23baeed54d77317885fa
Elaine McLeod <elaine@opengamma.com>
2010-07-23 13:10:30 +0100

    [FIN-116] Added a statistics calculator factory for use in functions in OG-Financial

---
+++ tests/unit/com/opengamma/math/statistics/descriptive/StatisticsCalculatorFactoryTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.math.statistics.descriptive;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class StatisticsCalculatorFactoryTest {
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testBadName() {
+    StatisticsCalculatorFactory.getCalculator("x");
+  }
+
+  @Test
+  public void testNullCalculator() {
+    assertNull(StatisticsCalculatorFactory.getCalculatorName(null));
+  }
+
+  @Test
+  public void test() {
+    assertEquals(StatisticsCalculatorFactory.MEAN, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory.getCalculator(StatisticsCalculatorFactory.MEAN)));
+    assertEquals(StatisticsCalculatorFactory.MEDIAN, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory.getCalculator(StatisticsCalculatorFactory.MEDIAN)));
+    assertEquals(StatisticsCalculatorFactory.MODE, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory.getCalculator(StatisticsCalculatorFactory.MODE)));
+    assertEquals(StatisticsCalculatorFactory.PEARSON_FIRST_SKEWNESS, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory
+        .getCalculator(StatisticsCalculatorFactory.PEARSON_FIRST_SKEWNESS)));
+    assertEquals(StatisticsCalculatorFactory.PEARSON_SECOND_SKEWNESS, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory
+        .getCalculator(StatisticsCalculatorFactory.PEARSON_SECOND_SKEWNESS)));
+    assertEquals(StatisticsCalculatorFactory.POPULATION_STANDARD_DEVIATION, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory
+        .getCalculator(StatisticsCalculatorFactory.POPULATION_STANDARD_DEVIATION)));
+    assertEquals(StatisticsCalculatorFactory.POPULATION_VARIANCE, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory
+        .getCalculator(StatisticsCalculatorFactory.POPULATION_VARIANCE)));
+    assertEquals(StatisticsCalculatorFactory.QUARTILE_SKEWNESS, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory.getCalculator(StatisticsCalculatorFactory.QUARTILE_SKEWNESS)));
+    assertEquals(StatisticsCalculatorFactory.SAMPLE_FISHER_KURTOSIS, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory
+        .getCalculator(StatisticsCalculatorFactory.SAMPLE_FISHER_KURTOSIS)));
+    assertEquals(StatisticsCalculatorFactory.SAMPLE_PEARSON_KURTOSIS, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory
+        .getCalculator(StatisticsCalculatorFactory.SAMPLE_PEARSON_KURTOSIS)));
+    assertEquals(StatisticsCalculatorFactory.SAMPLE_SKEWNESS, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory.getCalculator(StatisticsCalculatorFactory.SAMPLE_SKEWNESS)));
+    assertEquals(StatisticsCalculatorFactory.SAMPLE_STANDARD_DEVIATION, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory
+        .getCalculator(StatisticsCalculatorFactory.SAMPLE_STANDARD_DEVIATION)));
+    assertEquals(StatisticsCalculatorFactory.SAMPLE_VARIANCE, StatisticsCalculatorFactory.getCalculatorName(StatisticsCalculatorFactory.getCalculator(StatisticsCalculatorFactory.SAMPLE_VARIANCE)));
+
+  }
+}
