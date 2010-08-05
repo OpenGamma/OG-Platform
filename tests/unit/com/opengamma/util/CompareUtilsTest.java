2abd6575ad82537fb037f76162fdd18c1703286c
Stephen Colebourne <stephen@opengamma.com>
2010-08-05 12:44:19 +0100

    [FIN-112] Create new PositionMaster API
    Enhance compare utils with type safe min/max via Comparable

--- tests/unit/com/opengamma/util/CompareUtilsTest.java
+++ tests/unit/com/opengamma/util/CompareUtilsTest.java
@@ -15,6 +15,28 @@ import org.junit.Test;
 public class CompareUtilsTest {
 
   @Test
+  public void test_max() {
+    assertEquals(null, CompareUtils.max(null, null));
+    assertEquals("A", CompareUtils.max(null, "A"));
+    assertEquals("A", CompareUtils.max("A", null));
+    Integer a = new Integer(1); // need to use new, not autoboxing
+    Integer b = new Integer(1); // need to use new, not autoboxing
+    assertSame(a, CompareUtils.max(a, b));  // as we test for same here
+    assertEquals("B", CompareUtils.max("A", "B"));
+  }
+
+  @Test
+  public void test_min() {
+    assertEquals(null, CompareUtils.min(null, null));
+    assertEquals("A", CompareUtils.min(null, "A"));
+    assertEquals("A", CompareUtils.min("A", null));
+    Integer a = new Integer(1); // need to use new, not autoboxing
+    Integer b = new Integer(1); // need to use new, not autoboxing
+    assertSame(a, CompareUtils.min(a, b));  // as we test for same here
+    assertEquals("A", CompareUtils.min("A", "B"));
+  }
+
+  @Test
   public void testCompareWithNull() {
     assertTrue(CompareUtils.compareWithNull(null, null) == 0);
     assertTrue(CompareUtils.compareWithNull(null, "Test") < 0);
