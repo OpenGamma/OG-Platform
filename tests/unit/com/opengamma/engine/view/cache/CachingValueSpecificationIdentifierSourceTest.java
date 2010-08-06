22baaec4f873bb7ea8d42312409c6d60814eeeff
Kirk Wylie <kirk@opengamma.com>
2010-08-06 16:38:54 +0100

    [ENG-156] Wrote CachingValueSpecificationIdentifierSource and unit test.

---
+++ tests/unit/com/opengamma/engine/view/cache/CachingValueSpecificationIdentifierSourceTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.engine.view.cache;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.opengamma.engine.ComputationTargetSpecification;
+import com.opengamma.engine.ComputationTargetType;
+import com.opengamma.engine.value.ValueRequirement;
+import com.opengamma.engine.value.ValueSpecification;
+import com.opengamma.id.UniqueIdentifier;
+
+/**
+ * 
+ */
+public class CachingValueSpecificationIdentifierSourceTest {
+  
+  @Test
+  public void simpleOperation() {
+    final AtomicBoolean shouldFail = new AtomicBoolean(false);
+    
+    ValueSpecificationIdentifierSource underlying = new ValueSpecificationIdentifierSource() {
+      @Override
+      public long getIdentifier(ValueSpecification spec) {
+        if (shouldFail.get()) {
+          Assert.fail("Should not have called underlying.");
+        }
+        return 99L;
+      }
+    };
+    
+    CachingValueSpecificationIdentifierSource cachingSource = new CachingValueSpecificationIdentifierSource(underlying);
+    
+    ValueSpecification valueSpec = new ValueSpecification(new ValueRequirement("value", new ComputationTargetSpecification(ComputationTargetType.PRIMITIVE, UniqueIdentifier.of("scheme", "fibble"))));
+    assertEquals(99L, cachingSource.getIdentifier(valueSpec));
+    
+    shouldFail.set(true);
+    assertEquals(99L, cachingSource.getIdentifier(valueSpec));
+    assertEquals(99L, cachingSource.getIdentifier(valueSpec));
+    assertEquals(99L, cachingSource.getIdentifier(valueSpec));
+    assertEquals(99L, cachingSource.getIdentifier(valueSpec));
+    assertEquals(99L, cachingSource.getIdentifier(valueSpec));
+  }
+
+}
