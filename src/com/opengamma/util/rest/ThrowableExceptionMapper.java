425cbfed9d1559d5a5e099aa6e87e60ee38ff454
Stephen Colebourne <stephen@opengamma.com>
2010-08-20 17:40:03 +0100

    [UTL-68] REST enhancements
    Add new REST package with exception mappers, transparent exceptions and a new Jersey based client

---
+++ src/com/opengamma/util/rest/ThrowableExceptionMapper.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.util.rest;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * A JAX-RS provider to convert {@code Throwable} to a RESTful 500.
+ */
+@Provider
+public class ThrowableExceptionMapper
+    extends AbstractExceptionMapper
+    implements ExceptionMapper<Throwable> {
+
+  /**
+   * Creates the mapper.
+   */
+  public ThrowableExceptionMapper() {
+    super(Status.INTERNAL_SERVER_ERROR);
+  }
+
+  //-------------------------------------------------------------------------
+  @Override
+  public Response toResponse(final Throwable exception) {
+    return createResponse(exception);
+  }
+
+}
