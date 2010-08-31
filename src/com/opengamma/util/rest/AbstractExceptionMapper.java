88f259615a5dd4e8668d0a96d04d58778d809a9c
Stephen Colebourne <stephen@opengamma.com>
2010-08-31 17:29:19 +0100

    [IGN-55] RESTful exceptions are hidden
    Log RESTful exceptions using info for Fudge and warn for non-Fudge

--- src/com/opengamma/util/rest/AbstractExceptionMapper.java
+++ src/com/opengamma/util/rest/AbstractExceptionMapper.java
@@ -11,12 +11,18 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.opengamma.transport.jaxrs.FudgeRest;
 
 /**
  * An abstract class to assist with writing JAX-RS exception mappers.
  */
 public class AbstractExceptionMapper {
+
+  /** Logger. */
+  private static final Logger s_logger = LoggerFactory.getLogger(AbstractExceptionMapper.class);
 
   /**
    * The status to use.
@@ -39,6 +45,7 @@ public class AbstractExceptionMapper {
   //-------------------------------------------------------------------------
   public Response createResponse(final Throwable exception) {
     if (_headers.getAcceptableMediaTypes().contains(FudgeRest.MEDIA_TYPE)) {
+      s_logger.info("RESTful web-service exception caught and tunnelled to client", exception);
       // perform transparent exception tunneling for Fudge messages
       return Response.status(_status)
         .header(ExceptionThrowingClientFilter.EXCEPTION_TYPE, exception.getClass().getName())
@@ -47,6 +54,7 @@ public class AbstractExceptionMapper {
         .entity("Status: " + _status.getStatusCode() + " " + _status.getReasonPhrase() + "; Message: " + exception.getMessage())
         .build();
     } else {
+      s_logger.warn("RESTful website exception caught", exception);
       // output error page
       // TODO: error page
       return Response.status(_status).build();
