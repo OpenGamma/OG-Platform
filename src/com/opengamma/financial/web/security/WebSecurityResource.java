8ff945f35d181ff4866cec7ed58ae9f496872e3d
Stephen Colebourne <stephen@opengamma.com>
2010-08-31 18:16:59 +0100

    [FIN-135] HTML view of security master
    Initial checkin

---
+++ src/com/opengamma/financial/web/security/WebSecurityResource.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.web.security;
+
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.lang.StringUtils;
+import org.joda.beans.impl.flexi.FlexiBean;
+
+import com.opengamma.engine.security.DefaultSecurity;
+import com.opengamma.financial.security.SecurityDocument;
+import com.opengamma.id.Identifier;
+import com.opengamma.id.IdentifierBundle;
+import com.opengamma.id.UniqueIdentifier;
+
+/**
+ * RESTful resource for a portfolio.
+ */
+@Path("/securities/{securityId}")
+public class WebSecurityResource extends AbstractWebSecurityResource {
+
+  /**
+   * Creates the resource.
+   * @param parent  the parent resource, not null
+   */
+  public WebSecurityResource(final AbstractWebSecurityResource parent) {
+    super(parent);
+  }
+
+  //-------------------------------------------------------------------------
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public String get() {
+    FlexiBean out = createRootData();
+    return getFreemarker().build("securities/security.ftl", out);
+  }
+
+  @PUT
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  public Response put(
+      @FormParam("name") String name,
+      @FormParam("scheme") String scheme,
+      @FormParam("schemevalue") String schemeValue) {
+    name = StringUtils.trimToNull(name);
+    scheme = StringUtils.trimToNull(scheme);
+    schemeValue = StringUtils.trimToNull(schemeValue);
+    if (name == null || scheme == null || schemeValue == null) {
+      FlexiBean out = createRootData();
+      if (name == null) {
+        out.put("err_nameMissing", true);
+      }
+      if (scheme == null) {
+        out.put("err_schemeMissing", true);
+      }
+      if (schemeValue == null) {
+        out.put("err_schemevalueMissing", true);
+      }
+      String html = getFreemarker().build("securities/security-update.ftl", out);
+      return Response.ok(html).build();
+    }
+    SecurityDocument doc = data().getSecurity();
+    if (doc.getSecurity() instanceof DefaultSecurity == false) {
+      throw new IllegalArgumentException("Unable to update as not an instance of DefaultSecurity");
+    }
+    DefaultSecurity ds = (DefaultSecurity) doc.getSecurity();
+    ds.setName(name);
+    ds.setIdentifiers(IdentifierBundle.of(Identifier.of(scheme, schemeValue)));
+    doc = data().getSecurityMaster().update(doc);
+    data().setSecurity(doc);
+    URI uri = WebSecurityResource.uri(data());
+    return Response.seeOther(uri).build();
+  }
+
+  @DELETE
+  public Response delete() {
+    SecurityDocument doc = data().getSecurity();
+    data().getSecurityMaster().remove(doc.getUniqueIdentifier());
+    URI uri = WebSecuritiesResource.uri(data());
+    return Response.seeOther(uri).build();
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Creates the output root data.
+   * @return the output root data, not null
+   */
+  public FlexiBean createRootData() {
+    SecurityDocument doc = data().getSecurity();
+    FlexiBean out = getFreemarker().createRootData();
+    out.put("securityDoc", doc);
+    out.put("security", doc.getSecurity());
+    out.put("uris", new WebSecuritiesUris(data()));
+    return out;
+  }
+
+  //-------------------------------------------------------------------------
+  @Path("versions")
+  public WebSecurityVersionsResource findVersions() {
+    return new WebSecurityVersionsResource(this);
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Builds a URI for this resource.
+   * @param data  the data, not null
+   * @return the URI, not null
+   */
+  public static URI uri(final WebSecuritiesData data) {
+    return uri(data, null);
+  }
+
+  /**
+   * Builds a URI for this resource.
+   * @param data  the data, not null
+   * @param overrideSecurityId  the override security id, null uses information from data
+   * @return the URI, not null
+   */
+  public static URI uri(final WebSecuritiesData data, final UniqueIdentifier overrideSecurityId) {
+    String portfolioId = data.getBestSecurityUriId(overrideSecurityId);
+    return data.getUriInfo().getBaseUriBuilder().path(WebSecurityResource.class).build(portfolioId);
+  }
+
+}
