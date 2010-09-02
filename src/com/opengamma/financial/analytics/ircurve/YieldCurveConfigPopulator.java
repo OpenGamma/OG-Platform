9ac3a85c74b017f6198ab2850dd76e669899818f
Jim Moores <jim.moores@gmail.com>
2010-09-02 10:24:11 +0100

    Added futures and extended curve definitions to be more realistic (more points).

--- src/com/opengamma/financial/analytics/ircurve/YieldCurveConfigPopulator.java
+++ src/com/opengamma/financial/analytics/ircurve/YieldCurveConfigPopulator.java
@@ -5,12 +5,15 @@
  */
 package com.opengamma.financial.analytics.ircurve;
 
+import javax.time.calendar.LocalDate;
+
 import com.opengamma.config.ConfigMaster;
 import com.opengamma.config.DefaultConfigDocument;
 import com.opengamma.engine.config.MongoDBMasterConfigSource;
 import com.opengamma.financial.analytics.ircurve.CurveDefinitionAndSpecifications;
 import com.opengamma.financial.analytics.ircurve.CurveSpecificationBuilderConfiguration;
 import com.opengamma.financial.analytics.ircurve.YieldCurveDefinition;
+import com.opengamma.util.timeseries.localdate.LocalDateDoubleTimeSeries;
 
 /**
  * 
