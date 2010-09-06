24c6ef0b81835b5203e3871efebd6c4625e8083e
Stephen Colebourne <stephen@opengamma.com>
2010-09-06 18:25:57 +0100

    [FIN-136] Complete database security master
    Remove duplicate security tests. Move to correct folder.

--- tests/unit/com/opengamma/financial/security/master/FudgeSecurityEncodingTest.java
+++ tests/unit/com/opengamma/financial/security/master/FudgeSecurityEncodingTest.java
@@ -3,7 +3,7 @@
  *
  * Please see distribution for license.
  */
-package com.opengamma.financial.security;
+package com.opengamma.financial.security.master;
 
 import static org.junit.Assert.fail;
 
