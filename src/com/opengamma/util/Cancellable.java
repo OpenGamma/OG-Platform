82bf4458653de664bfad6114b8b927c456cb27a2
Andrew Griffin <andrew@opengamma.com>
2010-09-15 16:10:59 +0100

    [ENG-225] Generic Cancellable interface - a subset of the Future interface

---
+++ src/com/opengamma/util/Cancellable.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.util;
+
+/**
+ * If implemented allows something to be canceled.
+ */
+public interface Cancellable {
+
+  /**
+   * Attempts to cancel the operation.
+   * 
+   * @param mayInterruptIfRunning {@code true} if the thread can be interrupted, {@code false} if tasks should be allowed to complete naturally
+   * @return {@code true} if the operation was canceled, {@code false} otherwise
+   */
+  boolean cancel(boolean mayInterruptIfRunning);
+
+}
