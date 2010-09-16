c3a1c3eb8a3950357ec608e08a8da49c9d555e51
Andrew Griffin <andrew@opengamma.com>
2010-09-16 19:17:18 +0100

    [ENG-178] Unit test for isAlive

--- tests/unit/com/opengamma/engine/view/calcnode/RemoteNodeClientTest.java
+++ tests/unit/com/opengamma/engine/view/calcnode/RemoteNodeClientTest.java
@@ -7,7 +7,10 @@ package com.opengamma.engine.view.calcnode;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
 
 import org.fudgemsg.FudgeContext;
 import org.fudgemsg.FudgeMsgEnvelope;
@@ -25,6 +28,7 @@ import com.opengamma.engine.view.cache.IdentifierMap;
 import com.opengamma.engine.view.cache.InMemoryIdentifierMap;
 import com.opengamma.engine.view.calcnode.msg.Execute;
 import com.opengamma.engine.view.calcnode.msg.Failure;
+import com.opengamma.engine.view.calcnode.msg.IsAlive;
 import com.opengamma.engine.view.calcnode.msg.Ready;
 import com.opengamma.engine.view.calcnode.msg.RemoteCalcNodeMessage;
 import com.opengamma.engine.view.calcnode.msg.Result;
@@ -76,6 +80,36 @@ public class RemoteNodeClientTest {
     assertTrue(resultMessage instanceof Result);
     final Result result = (Result) resultMessage;
     assertEquals(job.getSpecification(), result.getResult().getSpecification());
+  }
+  
+  @Test
+  public void isAlive () {
+    final IdentifierMap identifierMap = new InMemoryIdentifierMap ();
+    final DirectFudgeConnection conduit = new DirectFudgeConnection(s_fudgeContext);
+    final CollectingFudgeMessageReceiver messages = new CollectingFudgeMessageReceiver();
+    conduit.getEnd2().setFudgeMessageReceiver(messages);
+    final RemoteNodeClient client = new RemoteNodeClient(conduit.getEnd1(), new FunctionCompilationService (new InMemoryFunctionRepository (), new FunctionCompilationContext ()), new InMemoryIdentifierMap (), new FunctionInvocationStatisticsSender ());
+    client.start();
+    assertEquals(1, messages.getMessages().size());
+    final FudgeMsgEnvelope readyMsgEnvelope = messages.getMessages().get(0);
+    messages.clear();
+    final FudgeDeserializationContext dcontext = new FudgeDeserializationContext(s_fudgeContext);
+    final FudgeSerializationContext scontext = new FudgeSerializationContext(s_fudgeContext);
+    final RemoteCalcNodeMessage readyMessage = dcontext.fudgeMsgToObject(RemoteCalcNodeMessage.class, readyMsgEnvelope.getMessage());
+    assertTrue(readyMessage instanceof Ready);
+    final CalculationJob job1 = createTestCalculationJob();
+    final CalculationJob job2 = createTestCalculationJob();
+    job1.convertInputs(identifierMap);
+    conduit.getEnd2().getFudgeMessageSender().send(FudgeSerializationContext.addClassHeader(scontext.objectToFudgeMsg(new Execute(job1)), Execute.class, RemoteCalcNodeMessage.class));
+    conduit.getEnd2().getFudgeMessageSender().send(FudgeSerializationContext.addClassHeader(scontext.objectToFudgeMsg(new IsAlive(Arrays.asList (job1.getSpecification (), job2.getSpecification ()))), IsAlive.class, RemoteCalcNodeMessage.class));
+    final FudgeMsgEnvelope resultMsgEnvelope = messages.waitForMessage(TIMEOUT);
+    assertNotNull(resultMsgEnvelope);
+    final RemoteCalcNodeMessage failureMessage = dcontext.fudgeMsgToObject(RemoteCalcNodeMessage.class, resultMsgEnvelope.getMessage());
+    assertTrue(failureMessage instanceof Failure);
+    final Failure failure = (Failure) failureMessage;
+    assertEquals(job2.getSpecification(), failure.getJob());
+    // No more messages - job1 is alive
+    assertNull (messages.waitForMessage(TIMEOUT));
   }
 
   @Test
