df912a6469dabbfe0d3f9ecc8afc9f8e369697f2
Pietari Laurila <pietari@opengamma.com>
2010-09-16 16:47:44 +0100

    Got a NoSuchMethodError and this fixed it

--- src/com/opengamma/engine/view/ViewTargetResultModelImpl.java
+++ src/com/opengamma/engine/view/ViewTargetResultModelImpl.java
@@ -6,6 +6,9 @@
 package com.opengamma.engine.view;
 
 import java.util.Collection;
+import java.util.Map;
+
+import com.opengamma.engine.value.ComputedValue;
 
 /**
  * A simple implementation of the per-target calculation result model.
@@ -15,6 +18,11 @@ public class ViewTargetResultModelImpl extends AbstractResultModel<String> imple
   @Override
   public Collection<String> getCalculationConfigurationNames() {
     return getKeys();
+  }
+  
+  @Override
+  public Map<String, ComputedValue> getValues(final String target) {
+    return super.getValues(target);
   }
 
 }
