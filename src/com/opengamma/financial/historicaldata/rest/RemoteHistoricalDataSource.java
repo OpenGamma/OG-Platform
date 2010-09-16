347053557718fbe98fb367e8fac24a273b2e0301
Yomi Ayodele <yomi@opengamma.com>
2010-09-16 12:25:02 +0100

    [ENG-232] implements extra getHistorical methods with flag to include/exclude start and end date

--- src/com/opengamma/financial/historicaldata/rest/RemoteHistoricalDataSource.java
+++ src/com/opengamma/financial/historicaldata/rest/RemoteHistoricalDataSource.java
@@ -154,17 +154,26 @@ public class RemoteHistoricalDataSource implements HistoricalDataSource {
   @Override
   public Pair<UniqueIdentifier, LocalDateDoubleTimeSeries> getHistoricalData(IdentifierBundle identifiers, String dataSource, String dataProvider, String field, LocalDate start,
       boolean inclusiveStart, LocalDate end, boolean exclusiveEnd) {
-    return null;
+    Pair<UniqueIdentifier, LocalDateDoubleTimeSeries> pair = getHistoricalData(identifiers, dataSource, dataProvider, field, start, end);
+    LocalDateDoubleTimeSeries timeSeries = pair.getValue();
+    timeSeries.subSeries(start, inclusiveStart, end, exclusiveEnd);
+    pair.setValue(timeSeries);
+    return pair;
   }
 
   @Override
   public Pair<UniqueIdentifier, LocalDateDoubleTimeSeries> getHistoricalData(IdentifierBundle identifiers, LocalDate start, boolean inclusiveStart, LocalDate end, boolean exclusiveEnd) {
-    return null;
+    Pair<UniqueIdentifier, LocalDateDoubleTimeSeries> pair = getHistoricalData(identifiers, start, end);
+    LocalDateDoubleTimeSeries timeSeries = pair.getValue();
+    timeSeries.subSeries(start, inclusiveStart, end, exclusiveEnd);
+    pair.setValue(timeSeries);
+    return pair;
   }
 
   @Override
   public LocalDateDoubleTimeSeries getHistoricalData(UniqueIdentifier uid, LocalDate start, boolean inclusiveStart, LocalDate end, boolean exclusiveEnd) {
-    return null;
+    LocalDateDoubleTimeSeries timeseries = getHistoricalData(uid, start, end);
+    return (LocalDateDoubleTimeSeries) timeseries.subSeries(start, inclusiveStart, end, exclusiveEnd);
   }
 
 }
