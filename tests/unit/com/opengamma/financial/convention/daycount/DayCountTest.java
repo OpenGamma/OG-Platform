f0a26dcc045c9bcceecec43f2aad7ba219d73361
Andrew Griffin <andrew@opengamma.com>
2010-09-17 15:35:49 +0100

    Correct Act/Act day convention to handle spanning leap years

--- tests/unit/com/opengamma/financial/convention/daycount/DayCountTest.java
+++ tests/unit/com/opengamma/financial/convention/daycount/DayCountTest.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2009 - 2010 by OpenGamma Inc.
- *
+ * 
  * Please see distribution for license.
  */
 package com.opengamma.financial.convention.daycount;
@@ -35,7 +35,7 @@ public class DayCountTest {
 
   @Test
   public void testThirtyThreeSixty() {
-    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("30/360");  // new ThirtyThreeSixtyDayCount();
+    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("30/360"); // new ThirtyThreeSixtyDayCount();
     final double basis = convention.getBasis(null);
     testOneYearNoLeapYears(convention);
     testOneYearOneLeapYear(convention);
@@ -48,7 +48,7 @@ public class DayCountTest {
 
   @Test
   public void testThirtyEThreeSixty() {
-    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("30E/360");  // new ThirtyEThreeSixtyDayCount();
+    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("30E/360"); // new ThirtyEThreeSixtyDayCount();
     final double basis = convention.getBasis(null);
     testOneYearNoLeapYears(convention);
     testOneYearOneLeapYear(convention);
@@ -61,7 +61,7 @@ public class DayCountTest {
 
   @Test
   public void testThirtyEThreeSixtyISDA() {
-    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("30E/360 (ISDA)");  // new ThirtyEThreeSixtyISDADayCount();
+    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("30E/360 (ISDA)"); // new ThirtyEThreeSixtyISDADayCount();
     final double basis = convention.getBasis(null);
     testOneYearNoLeapYears(convention);
     testOneYearOneLeapYear(convention);
@@ -74,7 +74,7 @@ public class DayCountTest {
 
   @Test
   public void testActualThreeSixtyFiveFixed() {
-    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("A/365F");  // new ActualThreeSixtyFiveFixedDayCount();
+    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("A/365F"); // new ActualThreeSixtyFiveFixedDayCount();
     final double basis = convention.getBasis(null);
     testOneYearNoLeapYears(convention);
     testOneYearOneLeapYear(convention, 1. / basis);
@@ -87,7 +87,7 @@ public class DayCountTest {
 
   @Test
   public void testActualThreeSixty() {
-    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("A/360");  // new ActualThreeSixtyDayCount();
+    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("A/360"); // new ActualThreeSixtyDayCount();
     final double basis = convention.getBasis(null);
     testOneYearNoLeapYears(convention, 5. / basis);
     testOneYearOneLeapYear(convention, 6. / basis);
@@ -100,7 +100,7 @@ public class DayCountTest {
 
   @Test
   public void testActualActual() {
-    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("Act/Act (ISDA)");  // new ActualActualISDADayCount();
+    final DayCount convention = DayCountFactory.INSTANCE.getDayCount("Act/Act (ISDA)"); // new ActualActualISDADayCount();
     final ZonedDateTime d1 = DateUtil.getUTCDate(2007, 12, 1);
     final ZonedDateTime d2 = DateUtil.getUTCDate(2008, 2, 1);
     final double basis = convention.getBasis(d1);
@@ -112,12 +112,18 @@ public class DayCountTest {
     testOneDayLeapYear(convention, 1. / leapYearBasis);
     testFirstDayThirtyDayAdjustment(convention, 31. / basis);
     testSecondDayThirtyDayAdjustment(convention, 33. / basis);
+    // overlap start of leap year
     assertFractionEquals(convention, d1, d2, 31. / basis + 31. / leapYearBasis);
+    // overlap end of leap year
+    assertFractionEquals(convention, DateUtil.getUTCDate(2008, 12, 1), DateUtil.getUTCDate(2009, 2, 1), 31. / leapYearBasis + 31. / basis);
+    // overlap leap year completely
+    assertFractionEquals(convention, d1, DateUtil.getUTCDate(2009, 2, 1), 1. + 62. / basis);
+    // overlap multiple leap years
+    assertFractionEquals(convention, DateUtil.getUTCDate(1997, 12, 1), DateUtil.getUTCDate(2009, 2, 1), 31. / basis + 11 + 31. / basis);
   }
 
-  private void assertFractionEquals(final DayCount convention, final ZonedDateTime d1, final ZonedDateTime d2,
-      final double frac) {
-    assertEquals(convention.getDayCountFraction(d1, d2), frac, EPS);
+  private void assertFractionEquals(final DayCount convention, final ZonedDateTime d1, final ZonedDateTime d2, final double frac) {
+    assertEquals(frac, convention.getDayCountFraction(d1, d2), EPS);
   }
 
   private void testOneYearNoLeapYears(final DayCount convention) {
