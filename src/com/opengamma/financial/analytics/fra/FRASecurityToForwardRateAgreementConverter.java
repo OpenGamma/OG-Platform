a8168b8fa9f3b83e7a41eea56d069db9a684635f
Elaine McLeod <elaine@opengamma.com>
2010-09-23 11:48:32 +0100

    Corrected the spelling of the class

--- src/com/opengamma/financial/analytics/fra/FRASecurityToForwardRateAgreementConverter.java
+++ src/com/opengamma/financial/analytics/fra/FRASecurityToForwardRateAgreementConverter.java
@@ -22,38 +22,38 @@ import com.opengamma.id.Identifier;
 /**
  * 
  */
-public class FRASecuityToForwardRateAgreementConverter {
-  
-  private HolidaySource _holidaySource;
-  private ConventionBundleSource _conventionSource;
+public class FRASecurityToForwardRateAgreementConverter {
 
-  public FRASecuityToForwardRateAgreementConverter(HolidaySource holidaySource, ConventionBundleSource conventionSource) {
+  private final HolidaySource _holidaySource;
+  private final ConventionBundleSource _conventionSource;
+
+  public FRASecurityToForwardRateAgreementConverter(final HolidaySource holidaySource, final ConventionBundleSource conventionSource) {
     _holidaySource = holidaySource;
     _conventionSource = conventionSource;
   }
 
-  public ForwardRateAgreement getFRA(FRASecurity security, String fundingCurveName, String indexCurveName, double marketRate, ZonedDateTime now){
-    String currency = security.getCurrency().getISOCode();
-    ConventionBundle conventions = _conventionSource.getConventionBundle(Identifier.of(InMemoryConventionBundleMaster.SIMPLE_NAME_SCHEME, currency + "_FRA"));
-    Calendar calendar = new HolidaySourceCalendarAdapter(_holidaySource, security.getCurrency()); // TODO: check we've got the right holiday calendar.
-    
-    BusinessDayConvention businessDayConvention = conventions.getBusinessDayConvention();
-    ZonedDateTime fixingDate = businessDayConvention.adjustDate(calendar, security.getStartDate().toZonedDateTime()); // just in case
-    
-    ZonedDateTime settlementDate = businessDayConvention.adjustDate(calendar, security.getStartDate().toZonedDateTime().plusDays(conventions.getSettlementDays()));
-    ZonedDateTime maturityDate = businessDayConvention.adjustDate(calendar, security.getEndDate().toZonedDateTime()); // just in case
-    
-    DayCount dayCount = conventions.getDayCount();
-    
+  public ForwardRateAgreement getFRA(final FRASecurity security, final String fundingCurveName, final String indexCurveName, final double marketRate, final ZonedDateTime now){
+    final String currency = security.getCurrency().getISOCode();
+    final ConventionBundle conventions = _conventionSource.getConventionBundle(Identifier.of(InMemoryConventionBundleMaster.SIMPLE_NAME_SCHEME, currency + "_FRA"));
+    final Calendar calendar = new HolidaySourceCalendarAdapter(_holidaySource, security.getCurrency()); // TODO: check we've got the right holiday calendar.
+
+    final BusinessDayConvention businessDayConvention = conventions.getBusinessDayConvention();
+    final ZonedDateTime fixingDate = businessDayConvention.adjustDate(calendar, security.getStartDate().toZonedDateTime()); // just in case
+
+    final ZonedDateTime settlementDate = businessDayConvention.adjustDate(calendar, security.getStartDate().toZonedDateTime().plusDays(conventions.getSettlementDays()));
+    final ZonedDateTime maturityDate = businessDayConvention.adjustDate(calendar, security.getEndDate().toZonedDateTime()); // just in case
+
+    final DayCount dayCount = conventions.getDayCount();
+
     // all times on discount/yield/forward curves are measured ACT/ACT
-    double fixingTime = DayCountFactory.INSTANCE.getDayCount("Actual/Actual").getDayCountFraction(now, fixingDate);
-    double settlementTime = DayCountFactory.INSTANCE.getDayCount("Actual/Actual").getDayCountFraction(now, settlementDate);
-    double maturityTime = DayCountFactory.INSTANCE.getDayCount("Actual/Actual").getDayCountFraction(now, maturityDate);
-    
-    double forwardYearFraction = dayCount.getDayCountFraction(fixingDate, maturityDate);
-    double discountingYearFraction = dayCount.getDayCountFraction(settlementDate, maturityDate);
-    
+    final double fixingTime = DayCountFactory.INSTANCE.getDayCount("Actual/Actual").getDayCountFraction(now, fixingDate);
+    final double settlementTime = DayCountFactory.INSTANCE.getDayCount("Actual/Actual").getDayCountFraction(now, settlementDate);
+    final double maturityTime = DayCountFactory.INSTANCE.getDayCount("Actual/Actual").getDayCountFraction(now, maturityDate);
+
+    final double forwardYearFraction = dayCount.getDayCountFraction(fixingDate, maturityDate);
+    final double discountingYearFraction = dayCount.getDayCountFraction(settlementDate, maturityDate);
+
     return new ForwardRateAgreement(settlementTime, maturityTime, fixingTime, forwardYearFraction, discountingYearFraction, marketRate, fundingCurveName, indexCurveName);
   }
-  
+
 }
