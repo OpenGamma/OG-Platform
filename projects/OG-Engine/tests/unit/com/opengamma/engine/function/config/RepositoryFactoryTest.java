c35a5cd22e5c29e918d20e2936d3cea8f0b3ec19
Andrew Griffin <andrew@opengamma.com>
2010-10-11 18:11:52 +0100

    Removed unused imports

--- projects/OG-Engine/tests/unit/com/opengamma/engine/function/config/RepositoryFactoryTest.java
+++ projects/OG-Engine/tests/unit/com/opengamma/engine/function/config/RepositoryFactoryTest.java
@@ -7,9 +7,7 @@ package com.opengamma.engine.function.config;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
