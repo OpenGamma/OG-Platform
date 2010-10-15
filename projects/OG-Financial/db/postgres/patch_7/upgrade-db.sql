9af055717ce8a6a1e4da5d3afb39d744c497ef9b
Jim Moores <jim.moores@gmail.com>
2010-10-15 16:02:08 +0100

    Support for bond loading and various structural changes to bond storage etc.

---
+++ projects/OG-Financial/db/postgres/patch_7/upgrade-db.sql
@@ -0,0 +1,4 @@
+alter table sec_bond alter column announcement_date drop not null;
+alter table sec_bond alter column announcement_zone drop not null;
+alter table sec_bond alter column businessdayconvention_id drop not null;
+alter table sec_bond alter column guaranteetype_id drop not null;
