73a11d2ab33a5f3bb166e358a865c8cafc28be66
Pietari Laurila <pietari@opengamma.com>
2010-10-15 16:00:49 +0100

    [ENG-243] Start running regular batch jobs overnight

---
+++ projects/OG-Financial/src/com/opengamma/financial/batch/db/HibernateBatchDbFiles.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.batch.db;
+
+import com.opengamma.util.db.HibernateMappingFiles;
+
+/**
+ * BatchDbManagerImpl configuration. 
+ */
+public class HibernateBatchDbFiles implements HibernateMappingFiles {
+
+  @Override
+  public Class<?>[] getHibernateMappingFiles() {
+    return new Class[] {
+      CalculationConfiguration.class,
+      ComputeHost.class,
+      ComputeNode.class,
+      LiveDataField.class,
+      LiveDataSnapshot.class,
+      LiveDataSnapshotEntry.class,
+      ObservationDateTime.class,
+      ObservationTime.class,
+      OpenGammaVersion.class,
+      RiskRun.class,
+      RiskValueName.class,
+      ComputationTarget.class,
+      RiskValue.class
+    };
+  }
+}
