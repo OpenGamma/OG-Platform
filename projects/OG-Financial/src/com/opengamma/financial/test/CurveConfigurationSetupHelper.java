2fcb5a1edeaea6c783519f2bd3fa8558fff2b852
Stephen Colebourne <stephen@opengamma.com>
2010-10-19 17:00:31 +0100

    [CFG-7] Complete implementation of config master
    Fix test by using in-memory rather than database

--- projects/OG-Financial/src/com/opengamma/financial/test/CurveConfigurationSetupHelper.java
+++ projects/OG-Financial/src/com/opengamma/financial/test/CurveConfigurationSetupHelper.java
@@ -10,7 +10,7 @@ import java.io.File;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.opengamma.engine.config.ConfigSource;
-import com.opengamma.engine.config.DbMasterConfigSource;
+import com.opengamma.engine.config.InMemoryMasterConfigSource;
 import com.opengamma.engine.security.SecuritySource;
 import com.opengamma.financial.analytics.ircurve.CurveSpecificationBuilderConfiguration;
 import com.opengamma.financial.analytics.ircurve.YieldCurveConfigPopulator;
@@ -36,10 +36,10 @@ public class CurveConfigurationSetupHelper {
     PlatformConfigUtils.configureSystemProperties(PlatformConfigUtils.RunMode.SHAREDDEV);
     _applicationContext = new ClassPathXmlApplicationContext("demoFinancialMasters.xml");
     
-    DbMasterConfigSource configSource = _applicationContext.getBean("sharedConfigSource", DbMasterConfigSource.class);
-    YieldCurveConfigPopulator.populateCurveDefinitionConfigMaster(configSource.getMaster(YieldCurveDefinition.class));
-    YieldCurveConfigPopulator.populateCurveSpecificationBuilderConfigMaster(configSource.getMaster(CurveSpecificationBuilderConfiguration.class));
-    _configSource = configSource;
+    InMemoryMasterConfigSource cfgSource = new InMemoryMasterConfigSource();
+    YieldCurveConfigPopulator.populateCurveDefinitionConfigMaster(cfgSource.getMaster(YieldCurveDefinition.class));
+    YieldCurveConfigPopulator.populateCurveSpecificationBuilderConfigMaster(cfgSource.getMaster(CurveSpecificationBuilderConfiguration.class));
+    _configSource = cfgSource;
     
     RegionMaster regionMaster = new InMemoryRegionMaster();
     RegionFileReader.populateMaster(regionMaster, new File(RegionFileReader.REGIONS_FILE_PATH));
@@ -50,7 +50,6 @@ public class CurveConfigurationSetupHelper {
   }
 
   public void tearDown() {
-    // may need to delete config table data here
     _applicationContext.close();
   }
 
