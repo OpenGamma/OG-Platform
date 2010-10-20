32d5959b96c3d14fbee8d1249918178e20c829b6
Stephen Colebourne <stephen@opengamma.com>
2010-10-20 14:10:14 +0100

    Fix test

--- projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/ircurve/InterpolatedYieldAndDiscountCurveFunctionTest.java
+++ projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/ircurve/InterpolatedYieldAndDiscountCurveFunctionTest.java
@@ -46,6 +46,8 @@ public class InterpolatedYieldAndDiscountCurveFunctionTest {
 
   /** Logger. */
   private final Logger s_logger = LoggerFactory.getLogger(this.getClass());
+  /** The expected result size. */
+  private static final int EXPECTED_SIZE = 12;
 
   private CurveConfigurationSetupHelper _configHelper;
 
@@ -81,7 +83,7 @@ public class InterpolatedYieldAndDiscountCurveFunctionTest {
     requirements = compiledFunction.getRequirements(context, new ComputationTarget(ComputationTargetType.PRIMITIVE, Currency.getInstance("USD")));
     s_logger.info(requirements.toString());
     assertNotNull(requirements);
-    assertEquals(18, requirements.size());
+    assertEquals(EXPECTED_SIZE, requirements.size());
     Set<Identifier> foundKeys = new TreeSet<Identifier>();
     for (ValueRequirement requirement : requirements) {
       assertNotNull(requirement);
@@ -90,7 +92,7 @@ public class InterpolatedYieldAndDiscountCurveFunctionTest {
       assertEquals(ComputationTargetType.PRIMITIVE, requirement.getTargetSpecification().getType());
       foundKeys.add(requirement.getTargetSpecification().getIdentifier());
     }
-    assertEquals(18, foundKeys.size());
+    assertEquals(EXPECTED_SIZE, foundKeys.size());
     
     ConfigDBInterpolatedYieldCurveDefinitionSource curveDefinitionSource = new ConfigDBInterpolatedYieldCurveDefinitionSource(_configHelper.getConfigSource());
     YieldCurveDefinition curveDefinition = curveDefinitionSource.getDefinition(Currency.getInstance("USD"),curveName);
@@ -121,10 +123,9 @@ public class InterpolatedYieldAndDiscountCurveFunctionTest {
     function.init(context);
     CompiledFunctionDefinition compiledFunction = function.compile(context, curveDate.atStartOfDayInZone(TimeZone.UTC));
 
-    requirements = compiledFunction.getRequirements(context, new ComputationTarget(ComputationTargetType.PRIMITIVE, Currency
-        .getInstance("USD")));
+    requirements = compiledFunction.getRequirements(context, new ComputationTarget(ComputationTargetType.PRIMITIVE, Currency.getInstance("USD")));
     assertNotNull(requirements);
-    assertEquals(18, requirements.size());
+    assertEquals(EXPECTED_SIZE, requirements.size());
     Set<Identifier> foundKeys = new TreeSet<Identifier>();
     for (ValueRequirement requirement : requirements) {
       assertNotNull(requirement);
@@ -133,7 +134,7 @@ public class InterpolatedYieldAndDiscountCurveFunctionTest {
       assertEquals(ComputationTargetType.PRIMITIVE, requirement.getTargetSpecification().getType());
       foundKeys.add(requirement.getTargetSpecification().getIdentifier());
     }
-    assertEquals(18, foundKeys.size());
+    assertEquals(EXPECTED_SIZE, foundKeys.size());
 
     ConfigDBInterpolatedYieldCurveDefinitionSource curveDefinitionSource = new ConfigDBInterpolatedYieldCurveDefinitionSource(_configHelper.getConfigSource());
     YieldCurveDefinition curveDefinition = curveDefinitionSource.getDefinition(Currency.getInstance("USD"),curveName);
