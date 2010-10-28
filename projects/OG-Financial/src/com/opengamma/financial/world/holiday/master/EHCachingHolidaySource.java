74f9c9c787f6d21d04b74f0f413ee691e15024df
Stephen Colebourne <stephen@opengamma.com>
2010-10-28 11:36:29 +0100

    [FIN-187] Database store for Holiday master
    Caching holiday source

--- projects/OG-Financial/src/com/opengamma/financial/world/holiday/master/EHCachingHolidaySource.java
+++ projects/OG-Financial/src/com/opengamma/financial/world/holiday/master/EHCachingHolidaySource.java
@@ -15,62 +15,58 @@ import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.ehcache.EHCacheUtils;
 
 /**
- * 
+ * A cache to optimize the results of {@code MasterHolidaySource}.
  */
 public class EHCachingHolidaySource extends MasterHolidaySource {
-  
+
   /**
    * Cache key for holidays.
    */
   private static final String HOLIDAY_CACHE = "holiday";
-  
+
   /**
    * The cache manager.
    */
   private final CacheManager _cacheManager;
-  
   /**
    * The result cache.
    */
-  private final Cache _holiday;
-  
+  private final Cache _holidayCache;
+
   /**
    * Creates the cache around an underlying holiday source.
+   * 
    * @param underlying  the underlying data, not null
    * @param cacheManager  the cache manager, not null
    */
   public EHCachingHolidaySource(final HolidayMaster underlying, final CacheManager cacheManager) {
     super(underlying);
-
     ArgumentChecker.notNull(cacheManager, "cacheManager");
     _cacheManager = cacheManager;
     EHCacheUtils.addCache(cacheManager, HOLIDAY_CACHE);
-    _holiday = EHCacheUtils.getCacheFromManager(cacheManager, HOLIDAY_CACHE);
+    _holidayCache = EHCacheUtils.getCacheFromManager(cacheManager, HOLIDAY_CACHE);
   }
 
-  // -------------------------------------------------------------------------
-  
+  //-------------------------------------------------------------------------
   public CacheManager getCacheManager() {
     return _cacheManager;
   }
-  
-  // -------------------------------------------------------------------------
-  
+
+  //-------------------------------------------------------------------------
   @Override
   protected boolean isHoliday(final HolidaySearchRequest request, final LocalDate dateToCheck) {
     if (isWeekend(dateToCheck)) {
       return true;
     }
-    
-    Element e = _holiday.get(request);
+    Element e = _holidayCache.get(request);
     if (e != null) {
       HolidayDocument doc = (HolidayDocument) e.getValue();
       return isHoliday(doc, dateToCheck);
     } else {
       HolidayDocument doc = getHolidayMaster().search(request).getFirstDocument();
-      _holiday.put(new Element(request, doc));
+      _holidayCache.put(new Element(request, doc));
       return isHoliday(doc, dateToCheck);
     }
   }
-  
+
 }
