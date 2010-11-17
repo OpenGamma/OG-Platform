d0d73dc30e91a341efe6255ea748e32992b53ca6
Andrew Griffin <andrew@opengamma.com>
2010-11-17 15:32:25 +0000

    [ENG-269] Cache the execution plan for dependency graphs.

--- projects/OG-Engine/src/com/opengamma/engine/view/calc/MultipleNodeExecutorFactory.java
+++ projects/OG-Engine/src/com/opengamma/engine/view/calc/MultipleNodeExecutorFactory.java
@@ -5,23 +5,30 @@
  */
 package com.opengamma.engine.view.calc;
 
+import org.springframework.beans.factory.InitializingBean;
+
 import com.opengamma.engine.view.calcnode.stats.FunctionCost;
 import com.opengamma.util.ArgumentChecker;
+import com.opengamma.util.ehcache.EHCacheUtils;
 
 /**
  * 
  */
-public class MultipleNodeExecutorFactory implements DependencyGraphExecutorFactory<Object> {
+public class MultipleNodeExecutorFactory implements DependencyGraphExecutorFactory<Object>, InitializingBean {
+  
+  private static final int DEFAULT_EXECUTION_PLAN_CACHE = 100;
 
+  private ExecutionPlanCache _executionPlanCache;
   private int _minimumJobItems = 1;
   private int _maximumJobItems = Integer.MAX_VALUE;
   private long _minimumJobCost = 1;
   private long _maximumJobCost = Long.MAX_VALUE;
   private int _maximumConcurrency = Integer.MAX_VALUE;
-  private FunctionCost _functionCost = new FunctionCost();
+  private FunctionCost _functionCost;
 
   public void setMinimumJobItems(final int minimumJobItems) {
     _minimumJobItems = minimumJobItems;
+    invalidateExecutionPlanCache();
   }
 
   public int getMinimumJobItems() {
@@ -30,6 +37,7 @@ public class MultipleNodeExecutorFactory implements DependencyGraphExecutorFacto
 
   public void setMaximumJobItems(final int maximumJobItems) {
     _maximumJobItems = maximumJobItems;
+    invalidateExecutionPlanCache();
   }
 
   public int getMaximumJobItems() {
@@ -38,6 +46,7 @@ public class MultipleNodeExecutorFactory implements DependencyGraphExecutorFacto
 
   public void setMinimumJobCost(final long minimumJobCost) {
     _minimumJobCost = minimumJobCost;
+    invalidateExecutionPlanCache();
   }
 
   public long getMinimumJobCost() {
@@ -46,14 +55,21 @@ public class MultipleNodeExecutorFactory implements DependencyGraphExecutorFacto
 
   public void setMaximumJobCost(final long maximumJobCost) {
     _maximumJobCost = maximumJobCost;
+    invalidateExecutionPlanCache();
   }
 
   public long getMaximumJobCost() {
     return _maximumJobCost;
   }
 
+  /**
+   * Sets the concurrency limit for job tails that are streamed to a single node host.
+   * 
+   * @param maximumConcurrency the number of job tails that are expected to be executing in parallel
+   */
   public void setMaximumConcurrency(final int maximumConcurrency) {
     _maximumConcurrency = maximumConcurrency;
+    invalidateExecutionPlanCache();
   }
 
   public int getMaximumConcurrency() {
@@ -63,21 +79,42 @@ public class MultipleNodeExecutorFactory implements DependencyGraphExecutorFacto
   public void setFunctionCost(final FunctionCost functionCost) {
     ArgumentChecker.notNull(functionCost, "functionCost");
     _functionCost = functionCost;
+    invalidateExecutionPlanCache();
   }
 
   public FunctionCost getFunctionCost() {
     return _functionCost;
   }
 
+  public void setCacheSize(final int size) {
+    _executionPlanCache = new ExecutionPlanCache(EHCacheUtils.createCacheManager(), size);
+  }
+
   @Override
   public MultipleNodeExecutor createExecutor(final SingleComputationCycle cycle) {
     ArgumentChecker.notNull(cycle, "cycle");
-    return new MultipleNodeExecutor(cycle, getMinimumJobItems(), getMaximumJobItems(), getMinimumJobCost(), getMaximumJobCost(), getMaximumConcurrency(), getFunctionCost());
+    return new MultipleNodeExecutor(cycle, getMinimumJobItems(), getMaximumJobItems(), getMinimumJobCost(), getMaximumJobCost(), getMaximumConcurrency(), getFunctionCost(), _executionPlanCache);
   }
 
   @Override
   public String toString() {
     return getClass().getSimpleName();
+  }
+
+  protected void invalidateExecutionPlanCache() {
+    if (_executionPlanCache != null) {
+      _executionPlanCache.clear();
+    }
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    if (getFunctionCost() == null) {
+      setFunctionCost(new FunctionCost());
+    }
+    if (_executionPlanCache == null) {
+      setCacheSize(DEFAULT_EXECUTION_PLAN_CACHE);
+    }
   }
 
 }
