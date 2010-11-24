62c3ab7a089a3d8b835496f29a100accd9b268ab
Stephen Colebourne <stephen@opengamma.com>
2010-11-24 15:00:48 +0000

    Javadoc

---
+++ projects/OG-Master/src/com/opengamma/master/position/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Position master API providing the ability to query and update portfolios and positions.
+ */
+package com.opengamma.master.position;
