b8f85bfa9a8cca956ac89030242108c7f092f1fc
Stephen Colebourne <stephen@opengamma.com>
2010-11-24 15:38:50 +0000

    Javadoc

--- projects/OG-Financial/src/com/opengamma/financial/timeseries/IdentifierProvider.java
+++ projects/OG-Financial/src/com/opengamma/financial/timeseries/IdentifierProvider.java
@@ -12,13 +12,16 @@ import com.opengamma.id.Identifier;
 import com.opengamma.id.IdentifierBundleWithDates;
 
 /**
- * Provider for loading all available identifiers with date ranges if available
+ * Provider for loading all available identifiers with date ranges if available.
  */
 public interface IdentifierProvider {
+
   /**
-   * Get all available identifiers with dates if available
-   * @param unresolvedIdentifiers the identifiers that need to be resolved, not empty
+   * Get all available identifiers with dates if available.
+   * 
+   * @param unresolvedIdentifiers  the identifiers that need to be resolved, not empty
    * @return a map of requested identifier to IdentifierBundleWithDates if available
    */
   Map<Identifier, IdentifierBundleWithDates> getIdentifiers(Set<Identifier> unresolvedIdentifiers);
+
 }
