b7e7e5b19633477c8410272ccf687acf81de8523
Stephen Colebourne <stephen@opengamma.com>
2010-11-29 17:26:04 +0000

    [CFG-8] Move config to OG Core/Master/MasterDB
    Add temp classes for Bamboo

---
+++ projects/OG-Config/tests/unit/com/opengamma/config/ConfigDocumentTest.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.config;
+
+/**
+ * A configuration document type.
+ */
+public class ConfigDocumentTest {
+
+}
