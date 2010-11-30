d6c18e98dd385bbe1872f061148839e425766055
Andrew Griffin <andrew@opengamma.com>
2010-11-30 17:00:57 +0000

    Hibernate sequence needed for the security master database.

--- projects/OG-MasterDB/install/db/hsqldb/patch_1/create-db-common.sql
+++ projects/OG-MasterDB/install/db/hsqldb/patch_1/create-db-common.sql
@@ -1,1 +1,1 @@
---    create sequence hibernate_sequence start with 1 increment by 1;
+    create sequence hibernate_sequence start with 1 increment by 1;
--- projects/OG-MasterDB/install/db/postgres/patch_1/create-db-common.sql
+++ projects/OG-MasterDB/install/db/postgres/patch_1/create-db-common.sql
@@ -1,1 +1,1 @@
---    create sequence hibernate_sequence start 1 increment 1;
+    create sequence hibernate_sequence start 1 increment 1;
