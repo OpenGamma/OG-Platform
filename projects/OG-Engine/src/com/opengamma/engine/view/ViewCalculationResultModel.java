6e3ad6951eeb7de7da5d574c8f5eaff5e46811a4
Andrew Griffin <andrew@opengamma.com>
2011-01-12 16:53:37 +0000

    Corrected JavaDoc.

--- projects/OG-Engine/src/com/opengamma/engine/view/ViewCalculationResultModel.java
+++ projects/OG-Engine/src/com/opengamma/engine/view/ViewCalculationResultModel.java
@@ -26,7 +26,7 @@ public interface ViewCalculationResultModel {
   Collection<ComputationTargetSpecification> getAllTargets();
 
   /**
-   * Returns the computed values for a given target. The values are returned as a map of configuration names to {@link ComputedValue}s.
+   * Returns the computed values for a given target. The values are returned as a map of value names to {@link ComputedValue}s.
    * 
    * @param target the target to search for, not {@code null}
    * @return the computed values for each configuration, or {@code null} if the target does not exist in the view results
