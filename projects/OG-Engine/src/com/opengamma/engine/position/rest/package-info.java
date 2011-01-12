42ecd250c27d2a4eb8d77904f57a851feddba2cb
Stephen Colebourne <stephen@opengamma.com>
2011-01-12 15:57:59 +0000

    [PLAT-412] Create RemotePositionSource
    Create remote position source and integrate

---
+++ projects/OG-Engine/src/com/opengamma/engine/position/rest/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Remote RESTful access to the position source.
+ */
+package com.opengamma.engine.position.rest;
