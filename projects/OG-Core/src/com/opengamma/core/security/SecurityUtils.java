48c32283b0863eb2a553c5fb88fd687a9c59eb9c
Stephen Colebourne <stephen@opengamma.com>
2011-01-27 14:24:00 +0000

    Checkstyle and test

--- projects/OG-Core/src/com/opengamma/core/security/SecurityUtils.java
+++ projects/OG-Core/src/com/opengamma/core/security/SecurityUtils.java
@@ -9,6 +9,7 @@ import javax.time.CalendricalException;
 import javax.time.calendar.LocalDate;
 import javax.time.calendar.format.DateTimeFormatters;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -164,9 +165,12 @@ public class SecurityUtils {
    * Creates a Bloomberg ticker coupon maturity identifier.
    * <p>
    * This is the ticker combined with a coupon and a maturity supplied by Bloomberg.
-   * An examples might be {@code T 4.75 15/08/43 Govt}.
+   * Example might be {@code T 4.75 15/08/43 Govt}.
    * 
-   * @param ticker  the Bloomberg ticker, not null
+   * @param tickerWithoutSector  the Bloomberg ticker without the sector, not null
+   * @param coupon  the coupon, not null
+   * @param maturity  the maturity date, not null
+   * @param marketSector  the sector, not null
    * @return the security identifier, not null
    */
   public static Identifier bloombergTCMSecurityId(final String tickerWithoutSector, final String coupon, final String maturity, final String marketSector) {
@@ -174,33 +178,34 @@ public class SecurityUtils {
     ArgumentChecker.notNull(coupon, "coupon");
     ArgumentChecker.notNull(maturity, "maturity");
     ArgumentChecker.notNull(marketSector, "marketSector");
-    if (tickerWithoutSector.length() == 0) {
-      throw new IllegalArgumentException("Ticker (without sector) is empty string");
+    if (StringUtils.isEmpty(tickerWithoutSector)) {
+      throw new IllegalArgumentException("Ticker (without sector) must not be empty");
     }
-    if (coupon.length() == 0) {
-      throw new IllegalArgumentException("Coupon is empty string.  ticker = " + tickerWithoutSector);
+    if (StringUtils.isEmpty(coupon)) {
+      throw new IllegalArgumentException("Coupon must not be empty, ticker = " + tickerWithoutSector);
     }
-    if (maturity.length() == 0) {
-      throw new IllegalArgumentException("Maturity is empty string.  ticker = " + tickerWithoutSector + ", coupon = " + coupon);
+    if (StringUtils.isEmpty(maturity)) {
+      throw new IllegalArgumentException("Maturity must not be empty, ticker = " + tickerWithoutSector + ", coupon = " + coupon);
     }
-    if (marketSector.length() == 0) {
-      throw new IllegalArgumentException("Market sector is empty string.  ticker = " + tickerWithoutSector + ", coupon = " + coupon + ", maturity = " + maturity);
+    if (StringUtils.isEmpty(marketSector)) {
+      throw new IllegalArgumentException("Market sector must not be empty, ticker = " + tickerWithoutSector + ", coupon = " + coupon + ", maturity = " + maturity);
     }
+    Double couponDbl;
     try {
-      Double couponDbl = Double.parseDouble(coupon);
-      if (s_logger.isDebugEnabled()) {
-    	try {
-    	  LocalDate.parse(maturity, DateTimeFormatters.pattern("MM/dd/YY"));
-        } catch (UnsupportedOperationException uoe) {
-          s_logger.warn("Problem parsing maturity " + maturity + " ticker=" + tickerWithoutSector + ", coupon=" + coupon);
-        } catch (CalendricalException ce) {
-          s_logger.warn("Problem parsing maturity " + maturity + " ticker=" + tickerWithoutSector + ", coupon=" + coupon);
-        }  
-      }
-      return Identifier.of(BLOOMBERG_TCM, tickerWithoutSector + " " + couponDbl + " " + maturity + " " + marketSector);
-    } catch (NumberFormatException nfe) {
-      throw new IllegalArgumentException("Invalid coupon " + coupon + " couldn't be parsed into double. ticker=" + tickerWithoutSector + ", coupon=" + coupon, nfe);
+      couponDbl = Double.parseDouble(coupon);
+    } catch (NumberFormatException ex) {
+      throw new IllegalArgumentException("Coupon must be a valid double, ticker=" + tickerWithoutSector + ", coupon=" + coupon, ex);
     }
+    if (s_logger.isDebugEnabled()) {
+      try {
+        LocalDate.parse(maturity, DateTimeFormatters.pattern("MM/dd/YY"));
+      } catch (UnsupportedOperationException uoe) {
+        s_logger.warn("Problem parsing maturity " + maturity + " ticker=" + tickerWithoutSector + ", coupon=" + coupon);
+      } catch (CalendricalException ce) {
+        s_logger.warn("Problem parsing maturity " + maturity + " ticker=" + tickerWithoutSector + ", coupon=" + coupon);
+      }  
+    }
+    return Identifier.of(BLOOMBERG_TCM, tickerWithoutSector + " " + couponDbl + " " + maturity + " " + marketSector);
   }
 
   /**
