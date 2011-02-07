eb50cb8de16264ff32d08107c52d2b35ecbc4c46
Elaine McLeod <elaine@opengamma.com>
2011-02-07 20:17:32 +0000

    Improving tests

--- projects/OG-Analytics/src/com/opengamma/math/minimization/NullTransform.java
+++ projects/OG-Analytics/src/com/opengamma/math/minimization/NullTransform.java
@@ -11,23 +11,39 @@ package com.opengamma.math.minimization;
 public class NullTransform implements ParameterLimitsTransform {
 
   @Override
-  public double inverseTransform(double y) {
+  public double inverseTransform(final double y) {
     return y;
   }
 
   @Override
-  public double inverseTransformGradient(double y) {
+  public double inverseTransformGradient(final double y) {
     return 1;
   }
 
   @Override
-  public double transform(double x) {
+  public double transform(final double x) {
     return x;
   }
 
   @Override
-  public double transformGradient(double x) {
+  public double transformGradient(final double x) {
     return 1;
+  }
+
+  @Override
+  public int hashCode() {
+    return 37;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    return getClass() == obj.getClass();
   }
 
 }
--- projects/OG-Analytics/src/com/opengamma/math/minimization/SingleRangeLimitTransform.java
+++ projects/OG-Analytics/src/com/opengamma/math/minimization/SingleRangeLimitTransform.java
@@ -71,4 +71,33 @@ public class SingleRangeLimitTransform implements ParameterLimitsTransform {
     return _sign * temp / (temp - 1);
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    long temp;
+    temp = Double.doubleToLongBits(_limit);
+    result = prime * result + (int) (temp ^ (temp >>> 32));
+    result = prime * result + _sign;
+    return result;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final SingleRangeLimitTransform other = (SingleRangeLimitTransform) obj;
+    if (Double.doubleToLongBits(_limit) != Double.doubleToLongBits(other._limit)) {
+      return false;
+    }
+    return _sign == other._sign;
+  }
+
 }
