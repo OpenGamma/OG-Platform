a2cd7f47b445fe194efd49248e75a1fccd5d0f81
Yomi Ayodele <yomi@opengamma.com>
2011-02-28 10:18:27 +0000

    [PLAT-1002] added dev/prod Bundle and bundle freemarker resource and the script and style tag to render the bundles

---
+++ projects/OG-Web/src/com/opengamma/web/bundle/WebDevBundleResource.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) 2009 - 2011 by OpenGamma Inc.
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.web.bundle;
+
+import java.net.URI;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * RESTful resource for a bundle in development mode
+ */
+@Path("/bundles/dev/{bundleId}")
+public class WebDevBundleResource extends AbstractWebBundleResource {
+  
+  @SuppressWarnings("unused")
+  private static final Logger s_logger = LoggerFactory.getLogger(WebDevBundleResource.class);
+  
+  /**
+   * Creates the resource.
+   * @param parent  the parent resource, not null
+   */
+  public WebDevBundleResource(final AbstractWebBundleResource parent) {
+    super(parent);
+  }
+  
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public Response get(@PathParam("bundleId") String idStr) {
+    return Response.ok(getCssImports(idStr)).build();
+  }
+
+  private String getCssImports(String bundleId) {
+    BundleManager bundleManager = data().getBundleManager();
+    Bundle bundle = bundleManager.getBundle(bundleId);
+    String basePath = bundleManager.getBaseDir().getName();
+    return BundleImportUriUtils.buildImports(bundle, new WebBundlesUris(data()), basePath);
+  }
+
+  /**
+   * Builds a URI for this resource.
+   * @param data  the data, not null
+   * @param bundleId  the bundleId, not null
+   * @return the URI, not null
+   */
+  public static URI uri(final WebBundlesData data, String bundleId) {
+    return data.getUriInfo().getBaseUriBuilder().path(WebDevBundleResource.class).build(bundleId);
+  }
+  
+}
