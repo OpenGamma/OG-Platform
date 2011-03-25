fccda8f75466955b6fd2f0099f8eeeb4bce4e3fe
Stephen Colebourne <stephen@opengamma.com>
2011-03-25 11:28:51 +0000

    [DVI-164] Move to TestNG, Fix assertArrayEquals import

--- projects/OG-Analytics/tests/unit/com/opengamma/financial/interestrate/NelsonSiegelBondCurveModelTest.java
+++ projects/OG-Analytics/tests/unit/com/opengamma/financial/interestrate/NelsonSiegelBondCurveModelTest.java
@@ -5,7 +5,7 @@
  */
 package com.opengamma.financial.interestrate;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.testng.AssertJUnit.assertArrayEquals;
 import static org.testng.AssertJUnit.assertEquals;
 import org.testng.annotations.Test;
 import java.util.Arrays;
