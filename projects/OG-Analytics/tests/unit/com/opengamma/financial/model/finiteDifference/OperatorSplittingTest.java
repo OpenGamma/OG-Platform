06616c49f2e96001746647213c8433c8de8f4352
Richard White <richard@opengamma.com>
2011-04-05 13:46:47 +0100

    Disable tests

--- projects/OG-Analytics/tests/unit/com/opengamma/financial/model/finiteDifference/OperatorSplittingTest.java
+++ projects/OG-Analytics/tests/unit/com/opengamma/financial/model/finiteDifference/OperatorSplittingTest.java
@@ -5,6 +5,7 @@
  */
 package com.opengamma.financial.model.finiteDifference;
 
+import org.testng.annotations.Test;
 
 /**
  * NOT WORKING 
@@ -15,7 +16,7 @@ public class OperatorSplittingTest {
   private static final SpreadOptionPDETestCase SPREAD_OPTION_TESTER = new SpreadOptionPDETestCase();
   private static final ConvectionDiffusionPDESolver2D SOLVER = new ImplicitFiniteDifference2D();
 
-  // @Test
+  @Test(enabled = false)
   public void testSpreadOption() {
 
     int timeSteps = 100;
@@ -25,7 +26,7 @@ public class OperatorSplittingTest {
     SPREAD_OPTION_TESTER.testAgaintBSPrice(SOLVER, timeSteps, xSteps, ySteps);
   }
 
-  // @Test
+  @Test(enabled = false)
   public void testHeston() {
 
     int timeSteps = 100;
