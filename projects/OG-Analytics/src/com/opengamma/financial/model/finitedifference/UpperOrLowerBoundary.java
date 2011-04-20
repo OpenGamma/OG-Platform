d16e13c58c1fb3eddc1894d71a2dfc5142e9fbaa
Richard White <richard@opengamma.com>
2011-04-20 15:29:23 +0100

    Working Crank-Nicolson 2D PDE solver

---
+++ projects/OG-Analytics/src/com/opengamma/financial/model/finitedifference/UpperOrLowerBoundary.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2009 - 2011 by OpenGamma Inc.
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.model.finitedifference;
+
+/**
+ * 
+ */
+public enum UpperOrLowerBoundary {
+  Upper, Lower
+}
