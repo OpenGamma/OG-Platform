f76c657067bc030a9d2d376befd22cf1d61b4c0c
Yomi Ayodele <yomi@opengamma.com>
2011-05-05 20:43:16 +0100

    [PLAT-1246] jetty.port in build.xml not actually used to start jetty, cleaned it up now

--- projects/OG-Examples/scripts/start-jetty.bat
+++ projects/OG-Examples/scripts/start-jetty.bat
@@ -9,7 +9,6 @@ if "%RUN_MODE%" == "" set RUN_MODE=shareddev
 "%JAVACMD%" -jar lib\org.eclipse-jetty-jetty-start-7.0.1.v20091125.jar ^
   -Xms1024m -Xmx3072m -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC ^
   -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing ^
-  -Djetty.port=8080 ^
   -Dlogback.configurationFile=jetty-logback.xml ^
   -Dopengamma.platform.runmode=%RUN_MODE% ^
   -Dopengamma.platform.marketdatasource=direct ^
