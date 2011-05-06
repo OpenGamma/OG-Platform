2515c8c64cc9225e239060e059bb8285e649ef8b
Stephen Colebourne <stephen@opengamma.com>
2011-05-06 18:21:19 +0100

    [PLAT-1082] Allow DELETE of a position in a portfolio by JSON

--- projects/OG-Web/src/com/opengamma/web/portfolio/WebPortfolioNodePositionsResource.java
+++ projects/OG-Web/src/com/opengamma/web/portfolio/WebPortfolioNodePositionsResource.java
@@ -11,6 +11,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -102,6 +103,13 @@ public class WebPortfolioNodePositionsResource extends AbstractWebPortfolioResou
       data().setPortfolio(doc);
     }
     return uri;
+  }
+
+  //-------------------------------------------------------------------------
+  @Path("{positionId}")
+  public WebPortfolioNodePositionResource findNode(@PathParam("positionId") String idStr) {
+    data().setUriPositionId(idStr);
+    return new WebPortfolioNodePositionResource(this);
   }
 
   //-------------------------------------------------------------------------
