108c0819b0042371b2919cf01a46007c4c23565c
Alan Ayoub <alan@opengamma.com>
2011-05-13 12:22:46 +0100

    fixed problems with the event to support the jquery ui popup calendar

--- projects/OG-Web/web-engine/prototype/scripts/og/common/search_results/og.common.search.filter.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/search_results/og.common.search.filter.js
@@ -12,13 +12,14 @@ $.register_module({
         var module = this;
         return function (obj) { // obj holds a selector with the location of the filters container
             var select = ['type'], // identify select form elements so we can handle these differently
+                calendar = ['ob_date'], // calendar fields
                 fields = ['name', 'type', 'quantity', 'data_source', 'identifier', 'data_provider', 'data_field',
                     'ob_time', 'ob_date', 'status', 'observation_time'];
-            //og.api.rest.configs.get({handler: console.log, meta: true})
             fields.forEach(function (filter) {
-                var event_type = !!select.indexOf(filter) ? 'keyup change' : 'change',
+                var event_type = ~select.indexOf(filter) || ~calendar.indexOf(filter) ? 'change' : 'keyup',
                     $selector = $(obj.location + ' .og-js-' + filter + '-filter');
                 if (!$selector.length) return;
+                !!~calendar.indexOf(filter) && $selector.datepicker({firstDay: 1, dateFormat: 'yy-mm-dd'});
                 $selector.unbind(event_type).bind(event_type, function () {
                     var routes = og.common.routes, view = og.views[routes.last().page.substring(1)], hash, obj;
                     obj = {}, obj[filter] = $(this).val(), obj.filter = true;
