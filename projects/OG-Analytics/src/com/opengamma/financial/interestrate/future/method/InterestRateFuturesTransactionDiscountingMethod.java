de0975035fbf2be8cfcc02ced838e4c6cc7871a9
Marc Henrard <marc@opengamma.com>
2011-05-13 12:09:11 +0100

    Added a presentValueSensitivity method for interest rate future method.

--- projects/OG-Analytics/src/com/opengamma/financial/interestrate/future/method/InterestRateFuturesTransactionDiscountingMethod.java
+++ projects/OG-Analytics/src/com/opengamma/financial/interestrate/future/method/InterestRateFuturesTransactionDiscountingMethod.java
@@ -5,11 +5,18 @@
  */
 package com.opengamma.financial.interestrate.future.method;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.lang.Validate;
 
+import com.opengamma.financial.interestrate.PresentValueSensitivity;
 import com.opengamma.financial.interestrate.YieldCurveBundle;
 import com.opengamma.financial.interestrate.future.InterestRateFutureTransaction;
 import com.opengamma.financial.model.interestrate.curve.YieldAndDiscountCurve;
+import com.opengamma.util.tuple.DoublesPair;
 
 /**
  * Method to compute the present value and its sensitivities for an interest rate future with discounting (like a forward). 
@@ -35,14 +42,40 @@ public class InterestRateFuturesTransactionDiscountingMethod {
    * @return The present value.
    */
   public double presentValueFromCurve(final InterestRateFutureTransaction future, final YieldCurveBundle curves) {
-    Validate.notNull(future, "FRA");
+    Validate.notNull(future, "Future");
     Validate.notNull(curves, "Curves");
     final YieldAndDiscountCurve forwardCurve = curves.getCurve(future.getUnderlyingFuture().getForwardCurveName());
-    double forward = (forwardCurve.getDiscountFactor(future.getUnderlyingFuture().getFixingPeriodStartTime()) / 
-        forwardCurve.getDiscountFactor(future.getUnderlyingFuture().getFixingPeriodEndTime()) - 1)
+    double forward = (forwardCurve.getDiscountFactor(future.getUnderlyingFuture().getFixingPeriodStartTime()) / forwardCurve.getDiscountFactor(future.getUnderlyingFuture().getFixingPeriodEndTime()) - 1)
         / future.getUnderlyingFuture().getFixingPeriodAccrualFactor();
     double pv = (1 - forward - future.getReferencePrice()) * future.getUnderlyingFuture().getPaymentAccrualFactor() * future.getUnderlyingFuture().getNotional() * future.getQuantity();
     return pv;
+  }
+
+  /**
+   * Compute the present value sensitivity to rates of a interest rate future by discounting.
+   * @param future The future.
+   * @param curves The yield curves. Should contain the forward curve associated. 
+   * @return The present value rate sensitivity.
+   */
+  public PresentValueSensitivity presentValueCurveSensitivity(final InterestRateFutureTransaction future, final YieldCurveBundle curves) {
+    Validate.notNull(future, "Future");
+    Validate.notNull(curves, "Curves");
+    final YieldAndDiscountCurve forwardCurve = curves.getCurve(future.getUnderlyingFuture().getForwardCurveName());
+    double dfForwardStart = forwardCurve.getDiscountFactor(future.getUnderlyingFuture().getFixingPeriodStartTime());
+    double dfForwardEnd = forwardCurve.getDiscountFactor(future.getUnderlyingFuture().getFixingPeriodEndTime());
+    double forward = (dfForwardStart / dfForwardEnd - 1.0) / future.getUnderlyingFuture().getFixingPeriodAccrualFactor();
+    // Backward sweep
+    double pvBar = 1.0;
+    double forwardBar = -future.getUnderlyingFuture().getPaymentAccrualFactor() * future.getUnderlyingFuture().getNotional() * future.getQuantity() * pvBar;
+    double dfForwardEndBar = -dfForwardStart / (dfForwardEnd * dfForwardEnd) / future.getUnderlyingFuture().getFixingPeriodAccrualFactor() * forwardBar;
+    double dfForwardStartBar = 1.0 / (future.getUnderlyingFuture().getFixingPeriodAccrualFactor() * dfForwardEnd) * forwardBar;
+    Map<String, List<DoublesPair>> resultMap = new HashMap<String, List<DoublesPair>>();
+    List<DoublesPair> listForward = new ArrayList<DoublesPair>();
+    listForward.add(new DoublesPair(future.getUnderlyingFuture().getFixingPeriodStartTime(), -future.getUnderlyingFuture().getFixingPeriodStartTime() * dfForwardStart * dfForwardStartBar));
+    listForward.add(new DoublesPair(future.getUnderlyingFuture().getFixingPeriodEndTime(), -future.getUnderlyingFuture().getFixingPeriodEndTime() * dfForwardEnd * dfForwardEndBar));
+    resultMap.put(future.getUnderlyingFuture().getForwardCurveName(), listForward);
+    PresentValueSensitivity result = new PresentValueSensitivity(resultMap);
+    return result;
   }
 
 }
--- projects/OG-Analytics/tests/unit/com/opengamma/financial/interestrate/future/definition/InterestRateFutureTransactionDiscountingMethodTest.java
+++ projects/OG-Analytics/tests/unit/com/opengamma/financial/interestrate/future/definition/InterestRateFutureTransactionDiscountingMethodTest.java
@@ -7,6 +7,8 @@ package com.opengamma.financial.interestrate.future.definition;
 
 import static org.testng.AssertJUnit.assertEquals;
 
+import java.util.List;
+
 import javax.time.calendar.LocalDate;
 import javax.time.calendar.LocalDateTime;
 import javax.time.calendar.Period;
@@ -22,15 +24,20 @@ import com.opengamma.financial.convention.calendar.MondayToFridayCalendar;
 import com.opengamma.financial.convention.daycount.DayCount;
 import com.opengamma.financial.convention.daycount.DayCountFactory;
 import com.opengamma.financial.instrument.index.IborIndex;
+import com.opengamma.financial.interestrate.PresentValueSensitivity;
 import com.opengamma.financial.interestrate.TestsDataSets;
 import com.opengamma.financial.interestrate.YieldCurveBundle;
 import com.opengamma.financial.interestrate.future.InterestRateFutureSecurity;
 import com.opengamma.financial.interestrate.future.InterestRateFutureTransaction;
 import com.opengamma.financial.interestrate.future.method.InterestRateFuturesTransactionDiscountingMethod;
 import com.opengamma.financial.model.interestrate.curve.YieldAndDiscountCurve;
+import com.opengamma.financial.model.interestrate.curve.YieldCurve;
 import com.opengamma.financial.schedule.ScheduleCalculator;
+import com.opengamma.math.curve.InterpolatedDoublesCurve;
+import com.opengamma.math.interpolation.LinearInterpolator1D;
 import com.opengamma.util.money.Currency;
 import com.opengamma.util.time.DateUtil;
+import com.opengamma.util.tuple.DoublesPair;
 
 public class InterestRateFutureTransactionDiscountingMethodTest {
   //EURIBOR 3M Index
@@ -57,16 +64,18 @@ public class InterestRateFutureTransactionDiscountingMethodTest {
   private static final double FIXING_START_TIME = ACT_ACT.getDayCountFraction(REFERENCE_DATE_ZONED, SPOT_LAST_TRADING_DATE);
   private static final double FIXING_END_TIME = ACT_ACT.getDayCountFraction(REFERENCE_DATE_ZONED, FIXING_END_DATE);
   private static final double FIXING_ACCRUAL = DAY_COUNT_INDEX.getDayCountFraction(SPOT_LAST_TRADING_DATE, FIXING_END_DATE);
+  private static final String FUNDING_CURVE_NAME = "Funding";
   private static final String FORWARD_CURVE_NAME = "Forward";
+  private static final String[] CURVES = {FUNDING_CURVE_NAME, FORWARD_CURVE_NAME};
   private static final InterestRateFutureSecurity ERU2 = new InterestRateFutureSecurity(LAST_TRADING_TIME, IBOR_INDEX, FIXING_START_TIME, FIXING_END_TIME, FIXING_ACCRUAL, NOTIONAL, FUTURE_FACTOR,
       FORWARD_CURVE_NAME, NAME);
   // Transaction
   private static final int QUANTITY = -123;
   //  private static final ZonedDateTime TRADE_DATE = DateUtil.getUTCDate(2011, 5, 12);
   private static final double TRADE_PRICE = 0.985;
-  InterestRateFutureTransaction FUTURE_TRANSACTION = new InterestRateFutureTransaction(ERU2, QUANTITY, TRADE_PRICE);
+  private static final InterestRateFutureTransaction FUTURE_TRANSACTION = new InterestRateFutureTransaction(ERU2, QUANTITY, TRADE_PRICE);
   // Method
-  InterestRateFuturesTransactionDiscountingMethod METHOD = new InterestRateFuturesTransactionDiscountingMethod();
+  private static final InterestRateFuturesTransactionDiscountingMethod METHOD = new InterestRateFuturesTransactionDiscountingMethod();
 
   @Test
   public void presentValueFromPrice() {
@@ -77,6 +86,9 @@ public class InterestRateFutureTransactionDiscountingMethodTest {
   }
 
   @Test
+  /**
+   * Test the present value computed from the curves
+   */
   public void presentValueFromCurve() {
     YieldCurveBundle curves = TestsDataSets.createCurves1();
     double pv = METHOD.presentValueFromCurve(FUTURE_TRANSACTION, curves);
@@ -84,6 +96,52 @@ public class InterestRateFutureTransactionDiscountingMethodTest {
     double forward = (forwardCurve.getDiscountFactor(FIXING_START_TIME) / forwardCurve.getDiscountFactor(FIXING_END_TIME) - 1) / FIXING_ACCRUAL;
     double expectedPv = (1 - forward - TRADE_PRICE) * FUTURE_FACTOR * NOTIONAL * QUANTITY;
     assertEquals("Present value from quoted price", expectedPv, pv);
+  }
 
+  @Test
+  /**
+   * Test the present value curves sensitivity computed from the curves
+   */
+  public void presentValueCurveSensitivity() {
+    YieldCurveBundle curves = TestsDataSets.createCurves1();
+    // Par rate sensitivity
+    PresentValueSensitivity pvsFuture = METHOD.presentValueCurveSensitivity(FUTURE_TRANSACTION, curves);
+    pvsFuture.clean();
+    double deltaTolerancePrice = 1.0E+2;
+    //Testing note: Sensitivity is for a movement of 1. 1E+2 = 1 cent for a 1 bp move. Tolerance increased to cope with numerical imprecision of finite difference.
+    final double deltaShift = 1.0E-8;
+    double pv = METHOD.presentValueFromCurve(FUTURE_TRANSACTION, curves);
+    // 1. Forward curve sensitivity
+    String bumpedCurveName = "Bumped Curve";
+    //    String[] bumpedCurvesForwardName = {FUNDING_CURVE_NAME, bumpedCurveName};
+    InterestRateFutureSecurity futureSecutiryBumpedForward = new InterestRateFutureSecurity(LAST_TRADING_TIME, IBOR_INDEX, FIXING_START_TIME, FIXING_END_TIME, FIXING_ACCRUAL, NOTIONAL, FUTURE_FACTOR,
+        bumpedCurveName, NAME);
+    InterestRateFutureTransaction futureTransactionBumpedForward = new InterestRateFutureTransaction(futureSecutiryBumpedForward, QUANTITY, TRADE_PRICE);
+    final YieldAndDiscountCurve curveForward = curves.getCurve(FORWARD_CURVE_NAME);
+    double[] timeForward = new double[2];
+    timeForward[0] = ERU2.getFixingPeriodStartTime();
+    timeForward[1] = ERU2.getFixingPeriodEndTime();
+    int nbForwardDate = timeForward.length;
+    final double[] yieldsForward = new double[nbForwardDate + 1];
+    double[] nodeTimesForward = new double[nbForwardDate + 1];
+    yieldsForward[0] = curveForward.getInterestRate(0.0);
+    for (int i = 0; i < nbForwardDate; i++) {
+      nodeTimesForward[i + 1] = timeForward[i];
+      yieldsForward[i + 1] = curveForward.getInterestRate(nodeTimesForward[i + 1]);
+    }
+    final YieldAndDiscountCurve tempCurveForward = new YieldCurve(InterpolatedDoublesCurve.fromSorted(nodeTimesForward, yieldsForward, new LinearInterpolator1D()));
+    List<DoublesPair> sensiPvForward = pvsFuture.getSensitivity().get(FORWARD_CURVE_NAME);
+    double[] sensiPvForwardFD = new double[nbForwardDate];
+    for (int i = 0; i < nbForwardDate; i++) {
+      final YieldAndDiscountCurve bumpedCurveForward = tempCurveForward.withSingleShift(nodeTimesForward[i + 1], deltaShift);
+      final YieldCurveBundle curvesBumpedForward = new YieldCurveBundle();
+      curvesBumpedForward.addAll(curves);
+      curvesBumpedForward.setCurve("Bumped Curve", bumpedCurveForward);
+      final double bumpedPv = METHOD.presentValueFromCurve(futureTransactionBumpedForward, curvesBumpedForward);
+      sensiPvForwardFD[i] = (bumpedPv - pv) / deltaShift;
+      final DoublesPair pairPv = sensiPvForward.get(i);
+      assertEquals("Sensitivity future pv to forward curve: Node " + i, nodeTimesForward[i + 1], pairPv.getFirst(), 1E-8);
+      assertEquals("Sensitivity future pv to forward curve: Node " + i, sensiPvForwardFD[i], pairPv.getSecond(), deltaTolerancePrice);
+    }
   }
 }
