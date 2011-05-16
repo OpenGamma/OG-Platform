071b1f074e6ea031036100428c1e9d91217a463e
Andrew Griffin <andrew@opengamma.com>
2011-05-16 16:55:33 +0100

    [PLAT-1281] Add "compare and exchange" to the atomic wrapper, correct sub-second semaphore timeouts, and replace the call slot logic with a state graph.

--- projects/OG-Language/ConnectorTest/SynchronousCallsTest.cpp
+++ projects/OG-Language/ConnectorTest/SynchronousCallsTest.cpp
@@ -108,6 +108,8 @@ static void PostAndWait () {
 	ASSERT (poSlot && (poSlot->GetIdentifier () == nIdentifier));
 	fudge_i32 nHandle2 = poSlot->GetHandle ();
 	ASSERT (nHandle1 != nHandle2);
+	msg = poSlot->GetMessage (TIMEOUT_MESSAGE);
+	ASSERT (!msg);
 	CThread *poSender = new CPostMessageThread (msgA, nHandle2, msgB, nHandle1, &oCalls);
 	msg = poSlot->GetMessage (TIMEOUT_MESSAGE);
 	ASSERT (msg);
@@ -129,7 +131,141 @@ static void PostAndWait () {
 	FudgeMsg_release (msgB);
 }
 
+class CRapidPostThread : public CThread {
+private:
+	FudgeMsg m_msg;
+	CSynchronousCallSlot *m_poSlot;
+	CSynchronousCalls *m_poCalls;
+	volatile bool m_bPoison;
+public:
+	CRapidPostThread (FudgeMsg msg, CSynchronousCallSlot *poSlot, CSynchronousCalls *poCalls)
+	: CThread () {
+		FudgeMsg_retain (msg);
+		m_msg = msg;
+		m_poSlot = poSlot;
+		m_poCalls = poCalls;
+		ASSERT (Start ());
+	}
+	~CRapidPostThread () {
+		FudgeMsg_release (m_msg);
+	}
+	void Run () {
+		while (!m_bPoison) {
+			int nHandle = m_poSlot->GetHandle ();
+			CThread::Sleep (TIMEOUT_MESSAGE / 10);
+			LOGINFO (TEXT ("Posting handle ") << nHandle);
+			FudgeMsg_retain (m_msg);
+			m_poCalls->PostAndRelease (nHandle, m_msg);
+		}
+	}
+	void Stop () {
+		m_bPoison = true;
+	}
+};
+
+static void RapidCalls () {
+	CSynchronousCalls oCalls;
+	CSynchronousCallSlot *poSlot = oCalls.Acquire ();
+	ASSERT (poSlot);
+	FudgeMsg msg;
+	ASSERT (FudgeMsg_create (&msg) == FUDGE_OK);
+	CRapidPostThread *poPoster = new CRapidPostThread (msg, poSlot, &oCalls);
+	int i, nGot = 0;
+	for (i = 0; i < 30; i++) {
+		FudgeMsg msg2 = poSlot->GetMessage ((TIMEOUT_MESSAGE / 100) * i);
+		if (msg2) {
+			LOGINFO (TEXT ("Got message ") << (i + 1));
+			nGot++;
+		} else {
+			LOGINFO (TEXT ("No message ") << (i + 1));
+		}
+		poSlot->Release ();
+		poSlot = oCalls.Acquire ();
+	}
+	poPoster->Stop ();
+	poSlot->Release ();
+	ASSERT (CThread::WaitAndRelease (poPoster, TIMEOUT_THREAD));
+	FudgeMsg_release (msg);
+	LOGINFO (TEXT ("Got ") << nGot << TEXT (" of 30"));
+	/* Assuming 1000ms for TIMEOUT_MESSAGE; possible run is
+	 *
+	 *  Time	Get			Send
+	 *     0	Fail  1
+	 * 	  10	Fail  2
+	 * 	  30	Fail  3
+	 * 	  60	Fail  4 
+	 * 	 100    Fail  5		Fail 1
+	 * 	 150	Fail  6
+	 * 	 200				Fail 5 or 6
+	 * 	 210	Fail  7
+	 * 	 280	Fail  8
+	 * 	 300				Fail 7
+	 * 	 360	Fail  9
+	 * 	 400				Fail 9
+	 * 	 450	Fail 10
+	 * 	 500	Fail 11		Fail 10
+	 * 	 600				Poss 11,12
+	 * 	 610	Poss 12
+	 * 	 700				Fail 12
+	 * 	 730	Fail 13
+	 * 	 800				Fail 13
+	 * 	 900				Done 14
+	 * 	 860	Get 14
+	 * 	1000	Fail 15		Fail 14
+	 * 	1150	Fail 16
+	 * 	1200				Fail 15 or 16
+	 * 	1300				Done 17
+	 *  1310	Get 17
+	 *  1400				Fail 17
+	 *  1480	Fail 18
+	 *  1500				Fail 18
+	 *  1600				Done 19
+	 *  1660	Get 19
+	 *  1700				Fail 19
+	 *  1800				Done 20
+	 *  1850	Get 20
+	 *  1900				Fail 20
+	 *  2000				Done 21
+	 *  2050	Get 21
+	 *  2100				Fail 21
+	 *  2200				Done 22
+	 *  2260	Get 22
+	 *  2300				Fail 22
+	 *  2400				Done 23
+	 *  2480	Get 23
+	 *  2500				Fail 23
+	 *  2600				Done 24
+	 *  2700				Fail 24
+	 *  2710	Get 24
+	 *  2800				Fail 24
+	 *  2900				Done 25
+	 *  2950	Get 25
+	 *  3000				Fail 25
+	 *  3100				Done 26
+	 *  3200	Get 26		Fail 26
+	 *  3300				Done 27
+	 *  3400				Fail 27
+	 *  3460	Get 27
+	 *  3500				Fail 27
+	 *  3600				Done 28
+	 *  3700				Fail 28
+	 *  3730	Get 28
+	 *  3800				Fail 28
+	 *  3900				Fail 29
+	 *  4000				Fail 29
+	 *  4010	Get 29
+	 *  4100				Fail 29
+	 *  4200				Done 30
+	 *  4300	Get 30		Fail 30
+	 */
+	// Expect to miss all of the first ten, but get all of the last ten. Those in
+	// the middle are a bit hit or miss. Perhaps we might expect around 14 (e.g. above)
+	// but it's all dependent on scheduling.
+	ASSERT (nGot > 10);
+}
+
 BEGIN_TESTS (SynchronousCallsTest)
 	TEST (AllocateAndRelease)
 	TEST (PostAndWait)
+	TEST (RapidCalls)
 END_TESTS
