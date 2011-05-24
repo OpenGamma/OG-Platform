05faf13f2f4fb427f93ccd16651d4c12740f1483
Stephen Colebourne <stephen@opengamma.com>
2011-05-24 10:40:23 +0100

    Add package info

---
+++ projects/OG-Web/src/com/opengamma/web/region/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Web resources for regions.
+ */
+package com.opengamma.web.region;
