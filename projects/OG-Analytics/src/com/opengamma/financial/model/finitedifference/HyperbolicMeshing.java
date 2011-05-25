5a130ac321e99689e0aadf393876324b008a6ef7
Richard White <richard@opengamma.com>
2011-05-25 17:14:48 +0100

    [PLAT-1300] ,[PLAT-1302]

--- projects/OG-Analytics/src/com/opengamma/financial/model/finitedifference/HyperbolicMeshing.java
+++ projects/OG-Analytics/src/com/opengamma/financial/model/finitedifference/HyperbolicMeshing.java
@@ -26,15 +26,15 @@ public class HyperbolicMeshing extends MeshingFunction {
    * @param upperBound The value of x_N  
    * @param concentration The value where the concentration of points is highest (<b>Note</b> there is no guarantee the a point will correspond exactly 
    * with this value)
+   * @param nPoints Number of Points (equal to N+1 in the above formula)
    * @param bunching Bunching parameter. A value great than zero. Very small values gives a very high density of points around the specified point, with the
    * density quickly falling away in both directions (the total number of points is fixed), while the distribution tends to uniform for large values. Value 
    * greater than 1 are fairly uniform
-   * @param nPoints Number of Points (equal to N+1 in the above formula)
    */
-  public HyperbolicMeshing(final double lowerBound, final double upperBound, final double concentration, final double bunching, final int nPoints) {
+  public HyperbolicMeshing(final double lowerBound, final double upperBound, final double concentration, final int nPoints, final double bunching) {
+    super(nPoints);
     Validate.isTrue(upperBound > lowerBound, "need upperBound>lowerBound");
     Validate.isTrue(upperBound >= concentration && concentration >= lowerBound, "need concentration between upper and lower bounds");
-    Validate.isTrue(nPoints > 1);
     Validate.isTrue(bunching > 0, "need bunching > 0");
 
     _alpha = concentration;
