67e73d4b87a9ee747fe661c96a99fed3956043ca
Afshin Darian <afshin@opengamma.com>
2011-06-06 19:39:36 +0100

    changing some formatting stuff

--- projects/OG-Web/web-engine/prototype/scripts/og/common/og.common.core.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/og.common.core.js
@@ -7,8 +7,7 @@
  * register_module is a function that helps manage large javascripts codebases.
  * All scripts should be registered with this function and supply a namespaced name and an obj function.
  * The function can optionally return an init method that will automatically get initialized by this script.
- * This script also allows you to set default methods and properties for all scripts. For example
- * the imageUrl property stores the path or the images folder.
+ * This script also allows you to set default methods and properties for all scripts.
  */
 
 (function ($, window, document, undefined) {
@@ -20,8 +19,7 @@
                 console.log.apply(console, Array.prototype.slice.call(arguments));
         },
         top_level = window, default_module = {
-            imageUrl: '/images', liveDataRoot: '/jax/', htmlRoot: '/prototype/modules/', appRoot: '/prototype/',
-            obj: function () {return default_obj;}
+            live_data_root: '/jax/', html_root: '/prototype/modules/', obj: function () {return default_obj;}
         };
     $.extend(default_obj, {warn: output.partial('[warning]'), log: output});
     /** @private */
