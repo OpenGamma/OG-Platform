e408a246011858115d82922439dc8c00419f2a12
Elaine McLeod <elaine@opengamma.com>
2011-06-09 01:30:42 +0100

    [PLAT-1350] First pass at converters for interest rate futures.

---
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/interestratefuture/InterestRateFutureTradeConverter.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.interestratefuture;
+
+import javax.time.calendar.TimeZone;
+import javax.time.calendar.ZonedDateTime;
+
+import org.apache.commons.lang.Validate;
+
+import com.opengamma.core.holiday.HolidaySource;
+import com.opengamma.core.position.impl.TradeImpl;
+import com.opengamma.core.region.RegionSource;
+import com.opengamma.financial.convention.ConventionBundleSource;
+import com.opengamma.financial.instrument.future.InterestRateFutureSecurityDefinition;
+import com.opengamma.financial.instrument.future.InterestRateFutureTransactionDefinition;
+import com.opengamma.financial.security.future.InterestRateFutureSecurity;
+
+/**
+ * 
+ */
+public class InterestRateFutureTradeConverter {
+  private final InterestRateFutureSecurityConverter _securityConverter;
+
+  public InterestRateFutureTradeConverter(final HolidaySource holidaySource,
+      final ConventionBundleSource conventionSource, final RegionSource regionSource) {
+    _securityConverter = new InterestRateFutureSecurityConverter(holidaySource, conventionSource, regionSource);
+  }
+
+  public InterestRateFutureTransactionDefinition convert(final TradeImpl trade) {
+    Validate.notNull(trade, "trade");
+    Validate.isTrue(trade.getSecurity() instanceof InterestRateFutureSecurity,
+        "Can only handle trades with security type InterestRateFutureSecurity");
+    final InterestRateFutureSecurityDefinition securityDefinition = _securityConverter
+        .convert((InterestRateFutureSecurity) trade.getSecurity());
+    final int quantity = (int) trade.getQuantity().doubleValue();
+    final ZonedDateTime tradeDate = ZonedDateTime.of(trade.getTradeDate().atMidnight(), TimeZone.UTC); //TODO need the closing time of the exchange
+    final double tradePrice = trade.getPremium();
+    return new InterestRateFutureTransactionDefinition(securityDefinition, quantity, tradeDate, tradePrice);
+  }
+}
