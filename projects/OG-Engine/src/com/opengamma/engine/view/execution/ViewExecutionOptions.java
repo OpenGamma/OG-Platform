32931e117beac72d31faedf19b79f30d9d7c15db
Jonathan Senior <jonathan@opengamma.com>
2011-06-20 16:38:58 +0100

    [PLAT-1382] Adding flag for elapsed time trigger and pulling out flags into ExecutionFlags to tidy up the API

--- projects/OG-Engine/src/com/opengamma/engine/view/execution/ViewExecutionOptions.java
+++ projects/OG-Engine/src/com/opengamma/engine/view/execution/ViewExecutionOptions.java
@@ -5,8 +5,8 @@
  */
 package com.opengamma.engine.view.execution;
 
-import com.opengamma.engine.livedata.LiveDataSnapshotProvider;
-import com.opengamma.engine.view.ViewDefinition;
+import java.util.EnumSet;
+
 import com.opengamma.id.UniqueIdentifier;
 
 /**
@@ -27,24 +27,11 @@ public interface ViewExecutionOptions {
   ViewCycleExecutionSequence getExecutionSequence();
   
   /**
-   * Indicates whether the view should run as fast as possible, perhaps faster than any constraints specified in the
-   * {@link ViewDefinition}, and perhaps running multiple computation cycles concurrently.
-   * <p>
-   * This might make sense for batch jobs where the evaluation times are independent of the current time.
+   * Gets the set of flags defining aspects of the execution behaviour in relation to the execution options.
    * 
-   * @return {@code true} if the view should run as fast as possible, {@code false} otherwise.
+   * @return the set of flags, not {@code null}
    */
-  boolean isRunAsFastAsPossible();
-  
-  /**
-   * Gets whether live data ticks can trigger computation cycles. This requires support for subscriptions from the
-   * {@link LiveDataSnapshotProvider}.
-   * <p>
-   * This setting may be ignored if other settings would cause computation cycles to run even faster.
-   * 
-   * @return {@code true} if live data ticks can trigger computation cycles, {@code false} otherwise.
-   */
-  boolean isLiveDataTriggerEnabled();
+  EnumSet<ViewExecutionFlags> getFlags();
   
   /**
    * Gets the maximum number of delta cycles following a full computation cycles.
@@ -52,17 +39,7 @@ public interface ViewExecutionOptions {
    * @return the maximum number of delta cycles following a full computation cycle, or {@code null} for no limit
    */
   Integer getMaxSuccessiveDeltaCycles();
-  
-  /**
-   * Gets whether execution involves merely compiling the view definition for each valuation time in the execution
-   * sequence.
-   * 
-   * @return {@code true} to only compile the view definition for each valuation time in the execution sequence,
-   *         {@code false} for normal execution
-   */
-  boolean isCompileOnly();
-  
-  
+    
   /**
    * Gets the snapshot ID used to provide live data for this cycle, or null not to use one
    * @return the snapshot id to use, or null
