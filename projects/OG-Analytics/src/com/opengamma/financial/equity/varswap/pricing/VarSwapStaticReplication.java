054793f125f1bcf0ef26515327c75c143c14dfe2
Casey Clements <casey@opengamma.com>
2011-06-21 17:23:25 +0100

    Right before I change ShiftedLognormal fit objective from two prices to the price at the cutoff and the instaneous slope there.

--- projects/OG-Analytics/src/com/opengamma/financial/equity/varswap/pricing/VarSwapStaticReplication.java
+++ projects/OG-Analytics/src/com/opengamma/financial/equity/varswap/pricing/VarSwapStaticReplication.java
@@ -10,6 +10,8 @@ import com.opengamma.financial.model.volatility.surface.VolatilitySurface;
 import com.opengamma.math.function.Function1D;
 import com.opengamma.math.integration.Integrator1D;
 import com.opengamma.math.integration.RungeKuttaIntegrator1D;
+import com.opengamma.math.matrix.DoubleMatrix1D;
+import com.opengamma.math.rootfinding.newton.BroydenVectorRootFinder;
 import com.opengamma.math.statistics.distribution.NormalDistribution;
 import com.opengamma.math.statistics.distribution.ProbabilityDistribution;
 import com.opengamma.util.tuple.DoublesPair;
@@ -19,10 +21,51 @@ import org.apache.commons.lang.Validate;
 /**
  * We construct a model independent method to price variance as a static replication
  * of an (in)finite sum of call and put option prices on the underlying.
- * Here, we assume the existence of a smooth function of these option prices / Implied volatilities.
- * Construction of this function is done exogenously.
+ * We assume the existence of a smooth function of these option prices / Implied volatilities.
+ * That said, this method is very sensitive to strike near zero as the portfolio weighting is 1/k^2,
+ * so we allow the caller to override the Volatilities below a cutoff point (defined as a fraction of the forward rate).
+ * We fit a ShiftedLognormal model to the price of the linear (call) and digital (call spread) at the cutoff. 
  */
 public class VarSwapStaticReplication {
+
+  // Vol Extrapolation 
+  private final Double _strikeCutoff; // Lowest interpolated strike. ShiftedLognormal hits Put(_strikeCutoff)
+  private final Double _strikeSpread; // Match derivative near cutoff by also fitting to Put(_strikeCutoff + _strikeSpread)
+  private final boolean _cutoffProvided; // False if both the above are null 
+
+  // Integration parameters
+  private final double _lowerBound; // Integrate over strikes in 'moneyness', defined as strike/forward. Start very close to zero
+  private final double _upperBound; // Upper bound in 'moneyness' (K/F). This represents 'large'
+  private final Integrator1D<Double, Double> _integrator;
+
+  /**
+   * Default constructor with sensible inputs.
+   */
+  public VarSwapStaticReplication() {
+    _lowerBound = 1e-9; // almost zero
+    _upperBound = 10.0; // multiple of the atm forward
+    _integrator = new RungeKuttaIntegrator1D();
+    _strikeCutoff = 0.25; // TODO Choose how caller tells impliedVariance not to use ShiftedLognormal..
+    _strikeSpread = 0.01;
+    _cutoffProvided = true;
+  }
+
+  public VarSwapStaticReplication(final double lowerBound, final double upperBound, final Integrator1D<Double, Double> integrator, Double strikeCutoff, Double strikeSpread) {
+    _lowerBound = lowerBound;
+    _upperBound = upperBound;
+    _integrator = integrator;
+
+    _strikeCutoff = strikeCutoff;
+    _strikeSpread = strikeSpread;
+    if (_strikeCutoff == null || _strikeSpread == null) {
+      Validate.isTrue(_strikeCutoff == null && _strikeSpread == null, "Both a cutoff moneyness and a spread must be provided to specify where to tie ShiftedLognormal.");
+      _cutoffProvided = false;
+    } else {
+      Validate.isTrue(strikeCutoff < 1, "strikeCutoff should be less than the forward, i.e. less than one. Note its defined as moneyness: strike / fwd.");
+      _cutoffProvided = true;
+    }
+
+  }
 
   // ************************** Black *****************
   // * TODO Review where a Black(fwd,strike,variance,isCall) function might live
@@ -30,7 +73,7 @@ public class VarSwapStaticReplication {
   // * where my usage here is simply to create a Function1D:  k -> price. 
   private static final ProbabilityDistribution<Double> NORMAL = new NormalDistribution(0, 1);
 
-  private static final double Black(double fwd, double strike, double variance, boolean isCall) {
+  private static final double black(double fwd, double strike, double variance, boolean isCall) {
 
     final double small = 1.0E-16;
 
@@ -51,47 +94,112 @@ public class VarSwapStaticReplication {
     return sign * (fwd * NORMAL.getCDF(sign * d1) - strike * NORMAL.getCDF(sign * d2));
   }
 
+  private static final double blackStrikeSensitivity(double fwd, double strike, double variance, boolean isCall) {
+
+    final double small = 1.0E-16;
+
+    if (strike < small) {
+      Validate.isTrue(false); //return isCall ? fwd : 0.0;
+    }
+    final int sign = isCall ? 1 : -1;
+    final double sigmaRootT = Math.sqrt(variance);
+    if (Math.abs(fwd - strike) < small) {
+      return -sign * NORMAL.getCDF(sign * 0.5 * sigmaRootT);
+    }
+    if (sigmaRootT < small) {
+      return sign;
+    }
+    final double d2 = (Math.log(fwd / strike) - 0.5 * variance) / sigmaRootT;
+    return -sign * NORMAL.getCDF(sign * d2);
+  }
+
   /**
    * @param deriv VarianceSwap derivative to be priced
    * @param market VarianceSwapDataBundle containing volatility surface, spot underlying, and funding curve
-   * @return presentValue of the variance *remaining* in the swap. 
+   * @return presentValue of the *remaining* variance in the swap. 
    */
-  public static double impliedVariance(final VarianceSwap deriv, final VarianceSwapDataBundle market) {
+  public double impliedVariance(final VarianceSwap deriv, final VarianceSwapDataBundle market) {
     Validate.notNull(deriv, "VarianceSwap deriv");
     Validate.notNull(market, "VarianceSwapDataBundle market");
 
-    // 1. Compute the forward
+    // 1. Unpack market data
     // TODO Review whether fwd=spot/df is sufficient, or whether the fwd itself should be in the VarianceSwapDataBundle
-    final double expiry = deriv.getTimeToSettlement();
+    final double expiry = deriv.getTimeToObsEnd(); // TODO Confirm treatmen t of which date should be used, obsEnd or settlement
     final double df = market.getDiscountCurve().getDiscountFactor(expiry);
     final double spot = market.getSpotUnderlying();
     final double fwd = spot / df;
+    final VolatilitySurface vsurf = market.getVolatilitySurface();
 
+    // *********************************************************
+    // 1B. Fit the leftExtrapolator
+    // TODO Cover the case in which a spread point isn't given
+    // TODO Test what happens when a cutoff IS given but the vol is ConstantDoublesSurface
+
+    final DoubleMatrix1D shiftedLnParams; // Two entries, Lognormal Variance and Forward Shift parameters, respectively
+
+    // Targets
+    if (_cutoffProvided) {
+
+      final DoublesPair cutoffCoords = DoublesPair.of(expiry, _strikeCutoff * fwd + 0.0); // TODO Review
+      final double cutoffVol = vsurf.getVolatility(cutoffCoords);
+      final double cutoffVar = cutoffVol * cutoffVol * expiry;
+      final double cutoffPut = black(fwd, _strikeCutoff * fwd, cutoffVar, false);
+
+      final DoublesPair spreadCoords = DoublesPair.of(expiry, (_strikeCutoff + _strikeSpread) * fwd);
+      final double spreadVol = vsurf.getVolatility(spreadCoords);
+      double spreadVar = spreadVol * spreadVol * expiry;
+      final double spreadPut = black(fwd, (_strikeCutoff + _strikeSpread) * fwd, spreadVar, false);
+
+      // Function
+      final Function1D<DoubleMatrix1D, DoubleMatrix1D> fitShiftedLognormal = new Function1D<DoubleMatrix1D, DoubleMatrix1D>() {
+        @Override
+        public DoubleMatrix1D evaluate(final DoubleMatrix1D varianceShiftPair) {
+          double[] diffs = new double[] {100, 100 };
+          double variance = Math.max(1e-9, varianceShiftPair.getEntry(0));
+          double shift = varianceShiftPair.getEntry(1);
+
+          diffs[0] = cutoffPut - black(fwd + shift, _strikeCutoff * fwd + shift, variance, false);
+          diffs[1] = spreadPut - black(fwd + shift, (_strikeCutoff + _strikeSpread) * fwd + shift, variance, false);
+
+          return new DoubleMatrix1D(diffs);
+        }
+      };
+      // 
+      // final NewtonDefaultVectorRootFinder solver = new NewtonDefaultVectorRootFinder(); // TODO Review whether we'd like to add control params
+      final BroydenVectorRootFinder solver = new BroydenVectorRootFinder();
+      DoubleMatrix1D guess = new DoubleMatrix1D(new double[] {cutoffVar, 0.0 }); // Start with the Lognormal that hits the cutoff price
+      shiftedLnParams = solver.getRoot(fitShiftedLognormal, guess);
+    } else {
+      shiftedLnParams = null;
+    }
+
+    // *********************************************************    
     // 2. Define integrand, the position to hold in each otmOption(k) = 2 / strike^2, where otmOption is a call if k>fwd and a put otherwise
-    //    Note:  strike space is parameterized wrt the forward, moneyness := strike/fwd i.e. atm moneyness=1
+    //    Note:  strike space is parameterised wrt the forward, moneyness := strike/fwd i.e. atm moneyness=1
     final Function1D<Double, Double> otmOptionAndWeight = new Function1D<Double, Double>() {
       @Override
       public Double evaluate(final Double moneyness) {
 
         double strike = moneyness * fwd;
-        VolatilitySurface vsurf = market.getVolatilitySurface();
-        DoublesPair coord = DoublesPair.of(expiry, strike);
-        double vol = vsurf.getVolatility(coord);
 
-        boolean isCall = moneyness > 1; // if strike > fwd, the call is out of the money..
-        double otmPrice = Black(fwd, moneyness * fwd, vol * vol * expiry, isCall);
         double weight = 2 / (fwd * moneyness * moneyness);
+        double otmPrice;
+
+        if (_cutoffProvided && moneyness < _strikeCutoff) { // Extrapolate with ShiftedLognormal
+          otmPrice = black(fwd + shiftedLnParams.getEntry(1), moneyness * fwd + shiftedLnParams.getEntry(1), shiftedLnParams.getEntry(0), false);
+        } else {
+          DoublesPair coord = DoublesPair.of(expiry, strike);
+          double vol = vsurf.getVolatility(coord);
+          boolean isCall = moneyness > 1; // if strike > fwd, the call is out of the money..
+          otmPrice = black(fwd, moneyness * fwd, vol * vol * expiry, isCall);
+        }
+
         return otmPrice * weight;
       }
     };
 
     // 3. Compute variance hedge by integrating positions over all strikes
-    // TODO How do I expose control params of pricing methods to user?
-    final double lowerBound = 1e-9; // almost zero
-    final double upperBound = 10.0; // multiple of the atm forward
-    final Integrator1D<Double, Double> integrator = new RungeKuttaIntegrator1D();
-
-    double variance = integrator.integrate(otmOptionAndWeight, lowerBound, upperBound);
+    double variance = _integrator.integrate(otmOptionAndWeight, _lowerBound, _upperBound);
     return variance;
   }
 }
--- projects/OG-Analytics/tests/unit/com/opengamma/financial/equity/varswap/VarSwapStaticReplicationTest.java
+++ projects/OG-Analytics/tests/unit/com/opengamma/financial/equity/varswap/VarSwapStaticReplicationTest.java
@@ -15,6 +15,7 @@ import com.opengamma.financial.interestrate.TestsDataSets;
 import com.opengamma.financial.interestrate.YieldCurveBundle;
 import com.opengamma.financial.model.interestrate.curve.YieldAndDiscountCurve;
 import com.opengamma.financial.model.volatility.surface.VolatilitySurface;
+import com.opengamma.math.integration.RungeKuttaIntegrator1D;
 import com.opengamma.math.interpolation.CombinedInterpolatorExtrapolator;
 import com.opengamma.math.interpolation.GridInterpolator2D;
 import com.opengamma.math.interpolation.Interpolator1D;
@@ -33,57 +34,173 @@ import org.testng.annotations.Test;
  */
 public class VarSwapStaticReplicationTest {
 
-  private static final double[] EXPIRIES = new double[] {0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 5.0, 5.0, 5.0, 10.0, 10.0, 10.0 };
-  private static final double[] STRIKES = new double[] {80, 100, 120, 80, 100, 120, 80, 100, 120, 80, 100, 120 };
-  private static final double[] VOLS = new double[] {0.2563287801311072, 0.2563287801311072, 0.2563287801311072,
-                                                     0.25, 0.25, 0.25,
-                                                     0.20468268826949546, 0.20468268826949546, 0.20468268826949546,
-                                                     0.1594070379054433, 0.1594070379054433, 0.1594070379054433 };
-
-  private static final CombinedInterpolatorExtrapolator<? extends Interpolator1DDataBundle> INTERPOLATOR_1D = getInterpolator(Interpolator1DFactory.DOUBLE_QUADRATIC,
-                                                                                        Interpolator1DFactory.FLAT_EXTRAPOLATOR,
-                                                                                        Interpolator1DFactory.LINEAR_EXTRAPOLATOR);
-
-  private static final Interpolator2D INTERPOLATOR_2D = new GridInterpolator2D(new LinearInterpolator1D(), (Interpolator1D<Interpolator1DDataBundle>) INTERPOLATOR_1D);
-  private static final InterpolatedDoublesSurface SURFACE = new InterpolatedDoublesSurface(EXPIRIES, STRIKES, VOLS, INTERPOLATOR_2D);
-
+  // The derivative
   final double varStrike = 0.05;
   final double varNotional = 3150;
-  final double expiry = 1;
+  final double expiry1 = 1;
+  final double expiry2 = 2;
+  final double expiry5 = 5;
 
-  final VarianceSwap swap = new VarianceSwap(0, expiry, expiry, 750, 750, 0, Currency.EUR, varStrike, varNotional);
+  final VarianceSwap swap1 = new VarianceSwap(0, expiry1, expiry1, 750, 750, 0, Currency.EUR, varStrike, varNotional);
+  final VarianceSwap swap2 = new VarianceSwap(0, expiry2, expiry2, 750, 750, 0, Currency.EUR, varStrike, varNotional);
+  final VarianceSwap swap5 = new VarianceSwap(0, expiry5, expiry5, 750, 750, 0, Currency.EUR, varStrike, varNotional);
 
+  // The pricing method
+  final VarSwapStaticReplication pricer = new VarSwapStaticReplication();
+
+  // Market data
   double spot = 80;
   final YieldCurveBundle curves = TestsDataSets.createCurves1();
   final YieldAndDiscountCurve curveDiscount = curves.getCurve("Funding");
 
+  private static final double[] EXPIRIES = new double[] {0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 5.0, 5.0, 5.0, 10.0, 10.0, 10.0 };
+  private static final double[] STRIKES = new double[] {80, 100, 120, 80, 100, 120, 80, 100, 120, 80, 100, 120 };
+  private static final double[] VOLS = new double[] {0.2563287801311072, 0.2563287801311072, 0.2563287801311072,
+                                                     0.25, 0.25, 0.25,
+                                                     0.24968268826949546, 0.23968268826949546, 0.23968268826949546,
+                                                     0.1594070379054433, 0.1594070379054433, 0.1594070379054433 };
+
+  private static final CombinedInterpolatorExtrapolator<? extends Interpolator1DDataBundle> INTERPOLATOR_1D = getInterpolator(Interpolator1DFactory.DOUBLE_QUADRATIC,
+      Interpolator1DFactory.LINEAR_EXTRAPOLATOR, Interpolator1DFactory.LINEAR_EXTRAPOLATOR);
+  @SuppressWarnings("unchecked")
+  private static final Interpolator2D INTERPOLATOR_2D = new GridInterpolator2D(new LinearInterpolator1D(), (Interpolator1D<Interpolator1DDataBundle>) INTERPOLATOR_1D);
+  private static final InterpolatedDoublesSurface SURFACE = new InterpolatedDoublesSurface(EXPIRIES, STRIKES, VOLS, INTERPOLATOR_2D);
+
+  // Tests ------------------------------------------
+  /**
+   * Test of VolatilitySurface type
+   */
   @Test
   public void testConstantDoublesSurface() {
 
     double vol = 0.25;
-    double targetVar = expiry * vol * vol;
+    double targetVar = swap1.getTimeToObsEnd() * vol * vol;
 
     final ConstantDoublesSurface constSurf = ConstantDoublesSurface.from(0.25);
     final VolatilitySurface vols = new VolatilitySurface(constSurf);
     final VarianceSwapDataBundle market = new VarianceSwapDataBundle(vols, curveDiscount, spot);
 
-    double testVar = VarSwapStaticReplication.impliedVariance(swap, market);
+    double testVar = pricer.impliedVariance(swap1, market);
     System.out.println(testVar);
 
     assertEquals(testVar, targetVar, 1e-9);
   }
 
+  /**
+   * Test of VolatilitySurface type
+   */
   @Test
-  public void testInterpolatedDoublesSurface() {
+  public void testInterpolatedDoublesSurfaceFlat() {
 
     double vol = 0.25;
-    double targetVar = expiry * vol * vol;
+    double targetVar = expiry1 * vol * vol;
 
     final VarianceSwapDataBundle market = new VarianceSwapDataBundle(new VolatilitySurface(SURFACE), curveDiscount, spot);
 
-    double testVar = VarSwapStaticReplication.impliedVariance(swap, market);
+    double testVar = pricer.impliedVariance(swap1, market);
     System.out.println(testVar);
 
     assertEquals(testVar, targetVar, 1e-9);
+  }
+
+  /**
+   * Confirm all is well when null values are passed for the cutoff, 
+   * at least when the surface can handle low strikes itself
+   */
+  @Test
+  public void testInterpolatedDoublesSurfaceWithoutCutoff() {
+
+    VarSwapStaticReplication pricerNoCutoff = new VarSwapStaticReplication(1e-16, 10, new RungeKuttaIntegrator1D(), null, null);
+
+    double vol = 0.25;
+    double targetVar = expiry1 * vol * vol;
+
+    final VarianceSwapDataBundle market = new VarianceSwapDataBundle(new VolatilitySurface(SURFACE), curveDiscount, spot);
+
+    double testVar = pricerNoCutoff.impliedVariance(swap1, market);
+    System.out.println(testVar);
+
+    assertEquals(testVar, targetVar, 1e-9);
+  }
+
+  /** 
+   * Confirm all is well when null values are passed for the cutoff, 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testInterpolatedDoublesSurfaceWithPoorCutoffDescription() {
+
+    VarSwapStaticReplication pricerBadCutoff = new VarSwapStaticReplication(1e-16, 10, new RungeKuttaIntegrator1D(), null, 0.05);
+
+    double vol = 0.25;
+    double targetVar = expiry1 * vol * vol;
+
+    final VarianceSwapDataBundle market = new VarianceSwapDataBundle(new VolatilitySurface(SURFACE), curveDiscount, spot);
+
+    double testVar = pricerBadCutoff.impliedVariance(swap1, market);
+    System.out.println(testVar);
+
+    assertEquals(testVar, targetVar, 1e-9);
+  }
+
+  /**
+   * Although all we get from impliedVariance(swap,market) is the variance, it is clear that the fit is the Lognormal with zero shift,
+   * as the variance produced is not affected.
+   */
+  @Test
+  public void testShiftedLognormalFitOnFlatSurface() {
+
+    VarSwapStaticReplication pricerHighCutoff = new VarSwapStaticReplication(1e-16, 10, new RungeKuttaIntegrator1D(), 0.95, 0.05);
+
+    double vol = 0.25;
+    double targetVar = expiry1 * vol * vol;
+
+    final VarianceSwapDataBundle market = new VarianceSwapDataBundle(new VolatilitySurface(SURFACE), curveDiscount, spot);
+
+    double testVar = pricerHighCutoff.impliedVariance(swap1, market);
+    System.out.println(testVar);
+
+    assertEquals(testVar, targetVar, 1e-9);
+  }
+
+  /**
+   * Confirm that we can split an integral over [a,c] into [a,b]+[b,c]
+   */
+  @Test
+  public void testIntegrationBounds() {
+
+    VarSwapStaticReplication pricerPuts = new VarSwapStaticReplication(1e-16, 1.0, new RungeKuttaIntegrator1D(), 0.9, 0.01);
+    VarSwapStaticReplication pricerCalls = new VarSwapStaticReplication(1.0, 10, new RungeKuttaIntegrator1D(), null, null);
+
+    double vol = 0.25;
+    double targetVar = expiry1 * vol * vol;
+
+    final VarianceSwapDataBundle market = new VarianceSwapDataBundle(new VolatilitySurface(SURFACE), curveDiscount, spot);
+
+    double varFromPuts = pricerPuts.impliedVariance(swap1, market);
+    double varFromCalls = pricerCalls.impliedVariance(swap1, market);
+    double testVar = varFromPuts + varFromCalls;
+    System.out.println(varFromPuts);
+    System.out.println(varFromCalls);
+    System.out.println(testVar);
+
+    assertEquals(testVar, targetVar, 1e-9);
+  }
+
+  /**
+   * Test of VolatilitySurface with Smile and Skew
+   */
+  @Test
+  public void testInterpolatedDoublesSurfaceV() {
+
+    double vol = 0.25;
+    double targetVar = swap5.getTimeToObsEnd() * vol * vol;
+
+    final VarianceSwapDataBundle market = new VarianceSwapDataBundle(new VolatilitySurface(SURFACE), curveDiscount, spot);
+    VarSwapStaticReplication pricerNoCutoff = new VarSwapStaticReplication(1e-16, 10, new RungeKuttaIntegrator1D(), 0.25, 0.01);
+
+    double testVar = pricerNoCutoff.impliedVariance(swap5, market);
+    System.out.println(testVar);
+
+    //assertEquals(testVar, targetVar, 1e-9);
   }
 }
