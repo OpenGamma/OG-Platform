5c172a3883364cabefa6446d3c173ba09bbcc0b2
Simon Jackson <simon@opengamma.com>
2011-07-01 14:01:15 +0100

    CombinedLiveDataServer supports ExpirationManager

--- projects/OG-LiveData/src/com/opengamma/livedata/server/CombiningLiveDataServer.java
+++ projects/OG-LiveData/src/com/opengamma/livedata/server/CombiningLiveDataServer.java
@@ -8,6 +8,7 @@ package com.opengamma.livedata.server;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -16,23 +17,24 @@ import java.util.Set;
 import org.fudgemsg.FudgeMsg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.internal.annotations.Sets;
 
+import com.google.common.collect.Sets;
 import com.opengamma.OpenGammaRuntimeException;
 import com.opengamma.id.IdentificationScheme;
 import com.opengamma.livedata.LiveDataSpecification;
 import com.opengamma.livedata.msg.LiveDataSubscriptionRequest;
 import com.opengamma.livedata.msg.LiveDataSubscriptionResponse;
 import com.opengamma.livedata.msg.LiveDataSubscriptionResponseMsg;
+import com.opengamma.livedata.server.distribution.MarketDataDistributor;
 import com.opengamma.livedata.server.distribution.MarketDataSenderFactory;
 
 /**
  * A {@link AbstractLiveDataServer} which delegates all the work to a set of {@link AbstractLiveDataServer} 
  * NOTE: this is only really a partial implementation of AbstractLiveDataServer
- *        e.g. ExpirationManagers and will need to point at the underlyings 
+ *        e.g. Entitlement checking will have to be set up on thi client as wekk as on the underlyings 
  */
 public abstract class CombiningLiveDataServer extends AbstractLiveDataServer {
-  //TODO: things include Entitlement checking, heartbeats
+  //TODO: things include Entitlement checking
   
   private static final Logger s_logger = LoggerFactory.getLogger(CombiningLiveDataServer.class);
 
@@ -79,13 +81,46 @@ public abstract class CombiningLiveDataServer extends AbstractLiveDataServer {
   }
 
   protected abstract Map<AbstractLiveDataServer, Collection<LiveDataSpecification>> groupByServer(
-      List<LiveDataSpecification> specs);
+      Collection<LiveDataSpecification> specs);
 
+  private AbstractLiveDataServer getServer(LiveDataSpecification spec) {
+    Map<AbstractLiveDataServer, Collection<LiveDataSpecification>> grouped = groupByServer(Sets.newHashSet(spec));
+    for (Entry<AbstractLiveDataServer, Collection<LiveDataSpecification>> entry : grouped.entrySet()) {
+      if (entry.getValue().size() > 0) {
+        return entry.getKey();
+      }
+    }
+    throw new OpenGammaRuntimeException("Couldn't find server for " + spec);
+  }
+  
+  // For expiration manager
   @Override
   public void addSubscriptionListener(SubscriptionListener subscriptionListener) {
     for (AbstractLiveDataServer server : _underlyings) {
       server.addSubscriptionListener(subscriptionListener);
     }
+  }
+
+  
+  @Override
+  public Set<Subscription> getSubscriptions() {
+    Set<Subscription> ret = new HashSet<Subscription>();
+    for (AbstractLiveDataServer server : _underlyings) {
+      ret.addAll(server.getSubscriptions());
+    }
+    return ret;
+  }
+
+  @Override
+  public MarketDataDistributor getMarketDataDistributor(LiveDataSpecification fullyQualifiedSpec) {
+    return getServer(fullyQualifiedSpec).getMarketDataDistributor(fullyQualifiedSpec);
+  }
+  
+  
+
+  @Override
+  public boolean stopDistributor(MarketDataDistributor distributor) {
+    return getServer(distributor.getFullyQualifiedLiveDataSpecification()).stopDistributor(distributor);    
   }
 
   @Override
