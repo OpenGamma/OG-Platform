b32da65fad72b6ffdd7d8a05a5111b6d97d8ebbe
Stephen Colebourne <stephen@opengamma.com>
2011-07-13 16:31:17 +0100

    [PLAT-1296] Refactor time-series master; New database, stage 10
    Post merge fixes

--- projects/OG-MasterDB/install/db/hsqldb/patch_20/upgrade-db.sql
+++ projects/OG-MasterDB/install/db/hsqldb/patch_20/upgrade-db.sql
@@ -1,78 +1,106 @@
-CREATE TABLE sec_fx (
-    id bigint not null,
-    security_id bigint not null,
-    pay_currency_id bigint not null,
-    receive_currency_id bigint not null,
-    region_scheme varchar(255) not null,
-    region_identifier varchar(255) not null,
-    pay_amount double precision not null,
-    receive_amount double precision not null,
-    primary key (id),
-    constraint sec_fk_fx2sec foreign key (security_id) references sec_security (id),
-    constraint sec_fk_fxpay2currency foreign key (pay_currency_id) references sec_currency (id),
-    constraint sec_fk_fxreceive2currency foreign key (receive_currency_id) references sec_currency (id)
-);
+CREATE SEQUENCE hts_master_seq as bigint
+    START WITH 1000 INCREMENT BY 1 NO CYCLE;
+CREATE SEQUENCE hts_idkey_seq as bigint
+    start with 1000 increment by 1 no cycle;
+CREATE SEQUENCE hts_dimension_seq as bigint
+    START WITH 1000 INCREMENT BY 1 NO CYCLE;
+-- "as bigint" required by Derby, not accepted by Postgresql
 
-CREATE TABLE sec_fxforward (
-  id bigint not null,
-  security_id bigint not null,
-  region_scheme varchar(255) not null,
-  region_identifier varchar(255) not null,
-  underlying_scheme varchar(255) not null,
-  underlying_identifier varchar(255) not null,
-  forward_date timestamp not null,
-  forward_zone varchar(50) not null,
-  primary key (id),
-  constraint sec_fk_fxforward2sec foreign key (security_id) references sec_security (id)
+CREATE TABLE hts_name (
+    id bigint NOT NULL,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX ix_hts_name_name ON hts_name(name);
 
-CREATE TABLE sec_capfloor (
-  id bigint not null,
-  security_id bigint not null,
-  currency_id bigint not null,
-  daycountconvention_id bigint not null,
-  frequency_id bigint not null,
-  is_cap boolean not null,
-  is_ibor boolean not null,
-  is_payer boolean not null,
-  maturity_date timestamp not null,
-  maturity_zone varchar(50) not null,
-  notional double precision not null,
-  start_date timestamp not null,
-  start_zone varchar(50) not null,
-  strike double precision not null,
-  underlying_scheme varchar(255) not null,
-  underlying_identifier varchar(255) not null,
-  
-  primary key (id),
-  constraint sec_fk_capfloor2sec foreign key (security_id) references sec_security (id),
-  constraint sec_fk_capfloor2currency foreign key (currency_id) references sec_currency(id),
-  constraint sec_fk_capfloor2daycount foreign key (daycountconvention_id) references sec_daycount (id),
-  constraint sec_fk_capfloor2frequency foreign key (frequency_id) references sec_frequency (id)
+CREATE TABLE hts_data_field (
+    id bigint NOT NULL,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX ix_hts_data_field_name ON hts_data_field(name);
 
-CREATE TABLE  sec_capfloorcmsspread (
-  id bigint not null,
-  security_id bigint not null,
-  currency_id bigint not null,
-  daycountconvention_id bigint not null,
-  frequency_id bigint not null,
-  is_cap boolean not null,
-  is_payer boolean not null,
-  long_scheme varchar(255) not null,
-  long_identifier varchar(255) not null,
-  maturity_date timestamp not null,
-  maturity_zone varchar(50) not null,
-  notional double precision not null,
-  short_scheme varchar(255) not null,
-  short_identifier varchar(255) not null,
-  start_date timestamp not null,
-  start_zone varchar(50) not null,
-  strike double precision not null,
-  
-  primary key (id),
-  constraint sec_fk_capfloorcmsspread2sec foreign key (security_id) references sec_security (id),
-  constraint sec_fk_capfloorcmsspread2currency foreign key (currency_id) references sec_currency(id),
-  constraint sec_fk_capfloorcmsspread2daycount foreign key (daycountconvention_id) references sec_daycount (id),
-  constraint sec_fk_capfloorcmsspread2frequency foreign key (frequency_id) references sec_frequency (id)
+CREATE TABLE hts_data_source (
+    id bigint NOT NULL,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX ix_hts_data_source_name ON hts_data_source(name);
+
+CREATE TABLE hts_data_provider (
+    id bigint NOT NULL,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX ix_hts_data_provider_name ON hts_data_provider(name);
+
+CREATE TABLE hts_observation_time (
+    id bigint NOT NULL,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX ix_hts_observation_time_name ON hts_observation_time(name);
+
+CREATE TABLE hts_document (
+    id bigint NOT NULL,
+    oid bigint NOT NULL,
+    ver_from_instant timestamp NOT NULL,
+    ver_to_instant timestamp NOT NULL,
+    corr_from_instant timestamp NOT NULL,
+    corr_to_instant timestamp NOT NULL,
+    name_id bigint NOT NULL,
+    data_field_id bigint NOT NULL,
+    data_source_id bigint NOT NULL,
+    data_provider_id bigint NOT NULL,
+    observation_time_id bigint NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT hts_fk_doc2doc FOREIGN KEY (oid) REFERENCES hts_document (id),
+    CONSTRAINT hts_chk_doc_ver_order CHECK (ver_from_instant <= ver_to_instant),
+    CONSTRAINT hts_chk_doc_corr_order CHECK (corr_from_instant <= corr_to_instant),
+    CONSTRAINT hts_fk_doc2name FOREIGN KEY (name_id) REFERENCES hts_name (id),
+    CONSTRAINT hts_fk_doc2data_field FOREIGN KEY (data_field_id) REFERENCES hts_data_field (id),
+    CONSTRAINT hts_fk_doc2data_source FOREIGN KEY (data_source_id) REFERENCES hts_data_source (id),
+    CONSTRAINT hts_fk_doc2data_provider FOREIGN KEY (data_provider_id) REFERENCES hts_data_provider (id),
+    CONSTRAINT hts_fk_doc2observation_time FOREIGN KEY (observation_time_id) REFERENCES hts_observation_time (id)
+);
+CREATE INDEX ix_hts_hts_oid ON hts_document(oid);
+CREATE INDEX ix_hts_hts_ver_from_instant ON hts_document(ver_from_instant);
+CREATE INDEX ix_hts_hts_ver_to_instant ON hts_document(ver_to_instant);
+CREATE INDEX ix_hts_hts_corr_from_instant ON hts_document(corr_from_instant);
+CREATE INDEX ix_hts_hts_corr_to_instant ON hts_document(corr_to_instant);
+CREATE INDEX ix_hts_hts_name_id ON hts_document(name_id);
+CREATE INDEX ix_hts_hts_data_field ON hts_document(data_field_id);
+CREATE INDEX ix_hts_hts_data_source ON hts_document(data_source_id);
+CREATE INDEX ix_hts_hts_data_provider ON hts_document(data_provider_id);
+CREATE INDEX ix_hts_hts_observation_time ON hts_document(observation_time_id);
+
+CREATE TABLE hts_idkey (
+    id bigint NOT NULL,
+    key_scheme varchar(255) NOT NULL,
+    key_value varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT hts_chk_idkey UNIQUE (key_scheme, key_value)
+);
+CREATE INDEX ix_hts_key_scheme ON hts_idkey(key_scheme);
+CREATE INDEX ix_hts_key_value ON hts_idkey(key_value);
+
+CREATE TABLE hts_doc2idkey (
+    doc_id bigint NOT NULL,
+    idkey_id bigint NOT NULL,
+    valid_from date NOT NULL,
+    valid_to date NOT NULL,
+    PRIMARY KEY (doc_id, idkey_id, valid_from, valid_to),
+    CONSTRAINT hts_fk_htsidkey2doc FOREIGN KEY (doc_id) REFERENCES hts_document (id),
+    CONSTRAINT hts_fk_htsidkey2idkey FOREIGN KEY (idkey_id) REFERENCES hts_idkey (id)
+);
+-- hts_doc2idkey is fully dependent of hts_document
+
+CREATE TABLE hts_point (
+    doc_oid bigint NOT NULL,
+    point_date date NOT NULL,
+    ver_instant timestamp NOT NULL,
+    corr_instant timestamp NOT NULL,
+    point_value double precision,
+    PRIMARY KEY (doc_oid, point_date, ver_instant, corr_instant)
+);
+-- null value used to indicate point was deleted
