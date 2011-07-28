79c0fcfb3d0693d941e3111b566ce3af92fdba79
Andrew Griffin <andrew@opengamma.com>
2011-07-28 12:55:46 +0100

    Fix test for DoubleLabelledMatrix3D and create Fudge builder.

--- projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/DoubleLabelledMatrix3DTest.java
+++ projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/DoubleLabelledMatrix3DTest.java
@@ -27,15 +27,12 @@ public class DoubleLabelledMatrix3DTest {
   private static final Object[] SORTED_Y_LABELS2 = new String[] {"A", "B", "C", "D" };
   private static final Object[] SORTED_Z_LABELS2 = new String[] {"A", "B", "C" };
   private static final double[][][] SORTED_VALUES1 = new double[][][] {
-      { {27., 26., 28., 30., 29. }, {31., 31., 33., 35., 34. }, {22., 21., 34., 25., 24. }, {37., 36., 38., 40., 39. } },
-      { {7., 6., 8., 10., 9. }, {12., 11., 13., 15., 14. }, {2., 1., 3., 5., 4. }, {17., 16., 18., 20., 19. } },
-      { {47., 46., 48., 50., 49. }, {52., 51., 53., 55., 54. }, {42., 41., 43., 45., 44. }, {57., 56., 58., 60., 59. } } };
+      { {32., 31., 33., 35., 34. }, {22., 21., 23., 25., 24. }, {27., 26., 28., 30., 29. }, {37., 36., 38., 40., 39. } },
+      { {12., 11., 13., 15., 14. }, {2., 1., 3., 5., 4. }, {7., 6., 8., 10., 9. }, {17., 16., 18., 20., 19. } },
+      { {52., 51., 53., 55., 54. }, {42., 41., 43., 45., 44. }, {47., 46., 48., 50., 49. }, {57., 56., 58., 60., 59. } } };
   private static final Double[] X_KEYS1 = new Double[] {2., 1., 3., 5., 4. };
   private static final Double[] Y_KEYS1 = new Double[] {2., 3., 1., 4. };
   private static final Double[] Z_KEYS1 = new Double[] {2., 1., 3. };
-  private static final Double[] X_KEYS1T = new Double[] {2., 1., 3., 5., 4. };
-  private static final Double[] Y_KEYS1T = new Double[] {2., 3., 1., 4. };
-  private static final Double[] Z_KEYS1T = new Double[] {2., 1., 3. };
   private static final Double[] X_KEYS2 = new Double[] {3., 6. };
   private static final Double[] Y_KEYS2 = new Double[] {4., 7. };
   private static final Double[] Z_KEYS2 = new Double[] {1., 8. };
@@ -44,7 +41,10 @@ public class DoubleLabelledMatrix3DTest {
   private static final Double[] Z_KEYS3 = new Double[] {1.1, 8. };
   private static final Object[] X_LABELS2 = new Object[] {"B", "A", "C", "E", "D" };
   private static final Object[] Y_LABELS2 = new Object[] {"B", "C", "A", "D" };
-  private static final Object[] Z_LABELS2 = new Object[] {"B", "C", "A" };
+  private static final Object[] Z_LABELS2 = new Object[] {"B", "A", "C" };
+  private static final Object[] X_LABELS3 = new Object[] {"C", "F" };
+  private static final Object[] Y_LABELS3 = new Object[] {"D", "G" };
+  private static final Object[] Z_LABELS3 = new Object[] {"A", "H" };
   private static final double[][][] VALUES1 = new double[][][] { { {1., 2., 3., 4., 5. }, {6., 7., 8., 9., 10. }, {11., 12., 13., 14., 15. }, {16., 17., 18., 19., 20., } },
       { {21., 22., 23., 24., 25. }, {26., 27., 28., 29., 30. }, {31., 32., 33., 34., 35. }, {36., 37., 38., 39., 40., } },
       { {41., 42., 43., 44., 45. }, {46., 47., 48., 49., 50. }, {51., 52., 53., 54., 55. }, {56., 57., 58., 59., 60., } } };
@@ -84,8 +84,8 @@ public class DoubleLabelledMatrix3DTest {
 
   @Test
   public void testAddWithTolerance() {
-    final DoubleLabelledMatrix3D a = new DoubleLabelledMatrix3D(X_KEYS1T, Y_KEYS1T, Z_KEYS1T, VALUES1);
-    final DoubleLabelledMatrix3D b = new DoubleLabelledMatrix3D(X_KEYS3, Y_KEYS3, Z_KEYS3, VALUES2);
+    final DoubleLabelledMatrix3D a = new DoubleLabelledMatrix3D(X_KEYS1, X_LABELS2, Y_KEYS1, Y_LABELS2, Z_KEYS1, Z_LABELS2, VALUES1);
+    final DoubleLabelledMatrix3D b = new DoubleLabelledMatrix3D(X_KEYS3, X_LABELS3, Y_KEYS3, Y_LABELS3, Z_KEYS3, Z_LABELS3, VALUES2);
     final DoubleLabelledMatrix3D result = a.add(b, 0.5, 0.5, 0.5);
     testAddResult(result);
   }
@@ -118,7 +118,13 @@ public class DoubleLabelledMatrix3DTest {
     assertEquals(zKeysResult, SORTED_Z_KEYS1);
     final Object[] zLabelsResult = result.getZLabels();
     assertEquals(zLabelsResult, zLabels);
-    assertTrue(Arrays.deepEquals(result.getValues(), SORTED_VALUES1));
+    final double[][][] values = result.getValues();
+    //print(values);
+    for (int i = 0; i < values.length; i++) {
+      for (int j = 0; j < values[i].length; j++) {
+        assertTrue(Arrays.equals(values[i][j], SORTED_VALUES1[i][j]));
+      }
+    }
   }
 
   private void testAddResult(final DoubleLabelledMatrix3D result) {
@@ -134,6 +140,27 @@ public class DoubleLabelledMatrix3DTest {
     assertEquals(values[3][3][5], 6., 0.5);
     assertEquals(values[3][4][2], 7., 0.5);
     assertEquals(values[3][4][5], 8., 0.5);
+  }
+
+  @SuppressWarnings("unused")
+  private static void print(final double[][][] v) {
+    for (double[][] slice : v) {
+      print(slice);
+      System.out.println();
+    }
+  }
+
+  private static void print(final double[][] v) {
+    for (double[] row : v) {
+      print(row);
+      System.out.println();
+    }
+  }
+
+  private static void print(final double[] v) {
+    for (double d : v) {
+      System.out.print(" " + d);
+    }
   }
 
 }
