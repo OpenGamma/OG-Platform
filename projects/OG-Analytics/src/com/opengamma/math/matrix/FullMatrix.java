ffe500266941121cef33925ef52469acc4402b77
Stuart Archibald <stuart@opengamma.com>
2011-07-29 15:12:52 +0100

    Adding the start of matrix primitives to try and unify matrix types and allow future extensions. Linearising all data will also aid pushing linalg to GPU/FPGA.

---
+++ projects/OG-Analytics/src/com/opengamma/math/matrix/FullMatrix.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.math.matrix;
+
+import org.apache.commons.lang.NotImplementedException;
+
+/**
+ * The FullMatrix class provides access to the typically understood notion of a matrix, i.e. A Fully populated array.
+ */
+public class FullMatrix implements MatrixPrimitiveInterface {
+  private double[] _data;
+  private int _rows;
+  private int _cols;
+  private int[] _rowPtr;
+  /**
+   * Constructors
+   */
+  // construct from array
+  /**
+   * Constructs from an array of arrays representation
+   * @param aMatrix is an n columns x m rows matrix stored as a row major array of arrays
+   */
+  public FullMatrix(double[][] aMatrix) {
+    // test if ragged
+    if (MatrixPrimitiveUtils.isRagged(aMatrix)) {
+      throw new NotImplementedException("Construction from ragged array not implemented");
+    }
+
+    _rows = aMatrix.length;
+    // test if square
+    if (MatrixPrimitiveUtils.isSquare(aMatrix)) {
+      _cols = _rows;
+    } else {
+      _cols = aMatrix[0].length;
+    }
+
+    //malloc
+    _data = new double[MatrixPrimitiveUtils.getNumberOfElementsInArray(aMatrix)];
+    _rowPtr = new int[aMatrix.length];
+
+    int ptr = 0;
+    // flatten the matrix
+    for (int i = 0; i < aMatrix.length; i++) {
+      _rowPtr[i] = ptr;
+      for (int j = 0; j < aMatrix[i].length; j++) {
+        _data[ptr] = aMatrix[i][j];
+        ptr++;
+      }
+    }
+  }
+
+  // construct from DoubleMatrix2D
+  public FullMatrix(DoubleMatrix2D aMatrix) {
+    this(aMatrix.toArray());
+  }
+
+/**
+ * Methods
+ */
+
+  /**
+   * Gets the number of elements in the matrix (full population assumed).
+   * @return the number of elements in the matrix
+   */
+  @Override
+  public int getNumberOfElements() {
+    return _data.length;
+  }
+
+  /**
+   * @param indices The index of the entry within the matrix to be returned.
+   * If a single index is given, it assumes ind2sub behaviour (index = i*rows+j) and returns that index
+   * If a pair of indices are given, it assumes standard lookup behaviour and returns the index at the given matrix "coordinate".
+   * @return the entry at index specified
+   *    */
+  @Override
+  public Double getEntry(int... indices) {
+    if (indices.length > 2) {
+      throw new IndexOutOfBoundsException("Trying to access a 2D array representation with tuple>2 is forbidden!");
+    } else if (indices.length == 2) {
+      return _data[_rowPtr[indices[0]] + indices[2]];
+    } else {
+      return _data[indices[0]];
+    }
+  }
+
+  @Override
+  public double[] getFullRow(int index) {
+    double[] tmp = new double[_cols];
+    for (int i = 0; i < _cols; i++) {
+      tmp[i] = _data[_rowPtr[index] + i];
+    }
+    return tmp;
+  }
+
+  @Override
+  public double[] getFullColumn(int index) {
+    double[] tmp = new double[_rows];
+    for (int i = 0; i < _rows; i++) {
+      tmp[i] = _data[_rowPtr[index] + i * _cols];
+    }
+    return tmp;
+  }
+
+  @Override
+  public double[] getRowElements(int index) {
+    return this.getFullRow(index);
+  }
+
+  @Override
+  public double[] getColumnElements(int index) {
+    return this.getFullColumn(index);
+  }
+
+  @Override
+  public int getNumberOfNonZeroElements() {
+    return MatrixPrimitiveUtils.numberOfNonZeroElementsInVector(_data);
+  }
+
+  @Override
+  public double[][] toArray() {
+    double[][] tmp = new double[_rows][_cols];
+    for (int i = 0; i < _rows; i++) {
+      for (int j = 0; j < _cols; j++) {
+        tmp[i][j] = _data[_rowPtr[i] + j];
+      }
+    }
+    return tmp;
+  }
+}
---
+++ projects/OG-Analytics/src/com/opengamma/math/matrix/MatrixPrimitiveUtils.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.math.matrix;
+
+/**
+ *
+ */
+public class MatrixPrimitiveUtils {
+
+  /**
+   * Tests if an array is ragged
+   * @param aMatrix an array of arrays
+   * @return true if array is ragged, false if array is not!
+   */
+  public static boolean isRagged(double[][] aMatrix) {
+    int rows = aMatrix.length;
+    int cols = aMatrix[0].length;
+    for (int i = 0; i < rows; i++) {
+      if (aMatrix[i].length != cols) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Tests if an array is square
+   * @param aMatrix an array of arrays
+   * @return true if array is "square" (length of all rows == number of rows), false if array is not!
+   */
+  public static boolean isSquare(double[][] aMatrix) {
+    int rows = aMatrix.length;
+    for (int i = 0; i < rows; i++) {
+      if (aMatrix[i].length != rows) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Computes the number of elements in an array of arrays
+   * @param aMatrix the array of arrays who's elements are going to be counted
+   * @return els the number of elements in the array of arrays
+   */
+  public static int getNumberOfElementsInArray(double[][] aMatrix) {
+    int els = 0;
+    for (int i = 0; i < aMatrix.length; i++) {
+      els += aMatrix[i].length;
+    }
+    return els;
+  }
+
+  /**
+   * Counts number of *true* nonzero elements in a vector
+   * @param aVector which is the vector (array of doubles) being tested
+   * @return tmp the number of nonzero elements in the vector
+   */
+  public static int numberOfNonZeroElementsInVector(double[] aVector) {
+    int tmp = 0;
+    for (int i = 0; i < aVector.length; i++) {
+      if (Double.doubleToLongBits(aVector[i]) != 0L) {
+        tmp++;
+      }
+    }
+    return tmp;
+  }
+
+}
