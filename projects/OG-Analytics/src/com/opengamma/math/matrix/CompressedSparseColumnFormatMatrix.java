fc2b8546b97f9d29c5975e1617a1835728678dc8
Stuart Archibald <stuart@opengamma.com>
2011-08-02 16:16:27 +0100

    Added compressed sparse column format matrix based on code from compressed sparse row. Need to fix wonky unit tests caused by incorrect indexing due to direct code copy. It appears that the constructors are now behaving.
    TODO: Fix unit tests on get() methods in CSC format.

---
+++ projects/OG-Analytics/src/com/opengamma/math/matrix/CompressedSparseColumnFormatMatrix.java
@@ -0,0 +1,311 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.math.matrix;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang.Validate;
+
+/**
+ * Converts, or instantiates, a matrix to Compressed Sparse Row format (CSR). CSR is a near optimal method of storing sparse matrix data.
+ * Only the non-zero components of the matrix are stored (note: there is no tolerance for testing zero to machine precision, zero is solely tested bitwise).
+ * The CSR format requires three vectors:
+ *  colIdx - contains column indexes of elements
+ *  rowPtr - contains the row pointer information used to reassemble the colIdx references in the correct place within the matrix
+ *  values - contains the nonzero values of the sparse matrix in row-major order
+ *
+ *  For an example call and output see the unit test.
+ *
+ *  A "normal" path to CSR form is via the {@link SparseCoordinateFormatMatrix} this formatting path is taken to allow access to the
+ *  Compressed Sparse Column (CSC) format with equal ease.
+ *  The final format chosen (CSR or CSC) should reflect the cache optimal access pattern of the calling function data.
+ */
+public class CompressedSparseColumnFormatMatrix extends SparseMatrixType  {
+
+  private double[] _values;
+  private int[] _colPtr;
+  private int[] _rowIdx;
+  private int _els;
+  private int _rows;
+  private int _cols;
+
+  /* constructors */
+
+  /**
+   * Construct from DoubleMatrix2D type
+   * @param m is a DoubleMatrix2D
+   */
+  public CompressedSparseColumnFormatMatrix(DoubleMatrix2D m) {
+    Validate.notNull(m);
+
+    //get number of elements
+    _els = m.getNumberOfElements();
+
+    // tmp arrays, in case we get in a fully populated matrix, intelligent design upstream should ensure that this is overkill!
+    double[] dataTmp = new double[_els];
+    int[] colPtrTmp = new int[_els];
+    int[] rowIdxTmp = new int[_els];
+
+    // we need unwind the array m into coordinate form
+    int ptr = 0;
+    int i;
+    for (i = 0; i < m.getNumberOfColumns(); i++) {
+      colPtrTmp[i] = ptr;
+      for (int j = 0; j < m.getNumberOfRows(); j++) {
+        if (Double.doubleToLongBits(m.getEntry(j, i)) != 0L) {
+          rowIdxTmp[ptr] = j;
+          dataTmp[ptr] = m.getEntry(j, i);
+          ptr++;
+        }
+      }
+    }
+    colPtrTmp[i] = ptr;
+
+    // return correct 0 to correct length of the vector buffers
+    _values = Arrays.copyOfRange(dataTmp, 0, ptr);
+    _colPtr = Arrays.copyOfRange(colPtrTmp, 0, i + 1); // yes, the +1 is correct, it allows the computation of the number of elements in the final row!
+    _rowIdx = Arrays.copyOfRange(rowIdxTmp, 0, ptr);
+    _rows = m.getNumberOfRows();
+    _cols = m.getNumberOfColumns();
+  }
+
+  /**
+   * Construct from SparseCoordinateFormatMatrix type
+   * @param m is a SparseCoordinateFormatMatrix
+   */
+  public CompressedSparseColumnFormatMatrix(SparseCoordinateFormatMatrix m) {
+    Validate.notNull(m);
+    _els = m.getNumberOfElements();
+    int[] colPtrTmp = new int[_els];
+    int[] rowIdxTmp = new int[_els];
+    double[] valuesTmp = new double[_els];
+    double val;
+    int ptr = 0, i;
+    for (i = 0; i < m.getNumberOfColumns(); i++) {
+      colPtrTmp[i] = ptr;
+      for (int j = 0; j < m.getNumberOfRows(); j++) {
+        val = m.getEntry(j, i);
+        if (Double.doubleToLongBits(val) != 0L) {
+          valuesTmp[ptr] = val;
+          rowIdxTmp[ptr] = j;
+          ptr++;
+        }
+      }
+    }
+    colPtrTmp[i] = ptr; // adds in last entry to close access fields
+
+    _values = Arrays.copyOfRange(valuesTmp, 0, ptr);
+    _rowIdx = Arrays.copyOfRange(rowIdxTmp, 0, ptr);
+    _colPtr = Arrays.copyOfRange(colPtrTmp, 0, i + 1); // yes, the +1 is correct!
+    _rows = m.getNumberOfRows();
+    _cols = m.getNumberOfColumns();
+  }
+
+  /**
+   * Construct from a (double) array of arrays
+   * @param m is a (double) array of arrays
+   */
+  public CompressedSparseColumnFormatMatrix(double[][] m) {
+    this(new DoubleMatrix2D(m));
+  }
+
+  /* methods */
+
+  /**
+   *  Gets the row indexes that can be looked up by the column pointer
+   *  @return _rowIdx, the row indexes
+   */
+  public int[] getRowIndex() {
+    return _rowIdx;
+  }
+
+  /**
+   * Gets the column pointer (not actually a pointer, but would be in C)
+   * @return _colPtr, the col pointer
+   */
+  public int[] getColumnPtr() {
+    return _colPtr;
+  }
+
+  /**
+   * Gets the non-zero values in the matrix, i.e. the values that are worth storing
+   * @return _values, the non-zero values
+   */
+  public double[] getNonZeroElements() {
+    return _values;
+  }
+
+  /**
+   * Gets the number of rows in the matrix (is not equal to count(unique(rowPtr)) as matrix could be singular/have row of zeros))
+   * @return _rows, the number of rows corresponding to a full matrix representation of the compressed matrix
+   */
+  public int getNumberOfRows() {
+    return _rows;
+  }
+
+  /**
+   * Gets the number of columns in the matrix (is not equal to count(unique(colIdx)) as matrix could be singular/have column of zeros))
+   * @return _cols, the number of columns corresponding to a full matrix representation of the compressed matrix
+   */
+  public int getNumberOfColumns() {
+    return _cols;
+  }
+
+ /**
+  * Gets the number of non-zero elements in the matrix
+  * @return _values.length, the number of non-zero elements in the matrix
+  */
+  public int getNumberOfNonzeroElements() {
+    return _values.length;
+  }
+
+/**
+ * Converts matrix to a Full Matrix representation (undoes the sparse compression)
+ * @return tmp, a DoubleMatrix2D
+ */
+  public DoubleMatrix2D toFullMatrix() {
+    return new DoubleMatrix2D(this.toArray());
+  }
+
+  /**
+   * Converts matrix to a Full Matrix representation (undoes the sparse compression)
+   * @return tmp, an array of arrays
+   */
+  @Override
+  public double[][] toArray() {
+    double[][] tmp = new double[_rows][_cols];
+    for (int ir = 0; ir < _rows; ir++) {
+        //translate an index and see if it exists, if it doesn't then return 0
+      for (int i = _colPtr[ir]; i <= _colPtr[ir + 1] - 1; i++) { // loops through elements of correct row
+        tmp[ir][_rowIdx[i]] = _values[i];
+      }
+    }
+    return tmp;
+  }
+
+  @Override
+  public int getNumberOfElements() {
+    return _els;
+  }
+
+
+  @Override
+  public double[] getFullRow(int index) {
+    double[] tmp = new double[_cols];
+    for (int i = _colPtr[index]; i <= _colPtr[index + 1] - 1; i++) { // loops through elements of correct row
+      tmp[_rowIdx[i]] = _values[i];
+    }
+    return tmp;
+  }
+
+  @Override // column slicing CSR is a nightmare, implemented for completeness, but really not worth actually using unless desperate. Use COO or CSC instead.
+  public double[] getFullColumn(int index) {
+    double[] tmp = new double[_rows];
+    for (int i = 0; i < _rows; i++) {
+      tmp[i] = this.getEntry(i, index);
+    }
+    return tmp;
+  }
+
+  @Override
+  public double[] getRowElements(int index) {
+    double[] tmp = new double[_cols];
+    int ptr = 0;
+    for (int i = _colPtr[index]; i <= _colPtr[index + 1] - 1; i++) { // loops through elements of correct row
+      tmp[ptr] = _values[i];
+      ptr++;
+    }
+    return Arrays.copyOfRange(tmp, 0, ptr);
+  }
+
+  @Override // again, column slicing CSR is a bad idea and essentially requires multiple brute forces. Store the matrix differently if you are thinking about doing this a lot
+  public double[] getColumnElements(int index) {
+    double[] tmp = new double[_cols];
+    double val;
+    int ptr = 0;
+    for (int i = 0; i < _cols; i++) {
+      val = this.getEntry(i, index);
+      if (Double.doubleToLongBits(val) != 0) {
+        tmp[ptr] = val;
+        ptr++;
+      }
+
+    }
+    return Arrays.copyOfRange(tmp, 0, ptr);
+  }
+
+  @Override
+  public int getNumberOfNonZeroElements() {
+    return _values.length;
+  }
+
+
+  @Override
+  public Double getEntry(int... indices) {
+    //translate an index and see if it exists, if it doesn't then return 0
+    for (int i = _colPtr[indices[0]]; i <= _colPtr[indices[0] + 1] - 1; i++) { // loops through elements of correct row
+      // looks for col index
+      if (_rowIdx[i] == indices[1]) {
+        return _values[i];
+      }
+    }
+    return 0.0;
+  }
+
+  @Override
+  public String toString() {
+    return "\nvalues=" + Arrays.toString(_values) +
+        "\nrowInd=" + Arrays.toString(_rowIdx) +
+        "\ncolPtr=" + Arrays.toString(_colPtr) +
+        "\nels=" + _els;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + Arrays.hashCode(_rowIdx);
+    result = prime * result + _cols;
+    result = prime * result + _els;
+    result = prime * result + Arrays.hashCode(_colPtr);
+    result = prime * result + _rows;
+    result = prime * result + Arrays.hashCode(_values);
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    CompressedSparseColumnFormatMatrix other = (CompressedSparseColumnFormatMatrix) obj;
+    if (_cols != other._cols) {
+      return false;
+    }
+    if (_rows != other._rows) {
+      return false;
+    }
+    if (!Arrays.equals(_colPtr, other._colPtr)) {
+      return false;
+    }
+    if (!Arrays.equals(_rowIdx, other._rowIdx)) {
+      return false;
+    }
+    if (!Arrays.equals(_values, other._values)) {
+      return false;
+    }
+    return true;
+  }
+
+}
+
+
