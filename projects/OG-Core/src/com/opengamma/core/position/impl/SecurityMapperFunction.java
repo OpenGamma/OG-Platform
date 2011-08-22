04d58a84c76bef0cb611f9d49b830bb424047bfb
Jonathan Senior <jonathan@opengamma.com>
2011-08-22 16:44:59 +0100

    Moving portfolio mapper to OG-Core where it can be more widely used

---
+++ projects/OG-Core/src/com/opengamma/core/position/impl/SecurityMapperFunction.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.core.position.impl;
+
+import com.opengamma.core.position.PortfolioNode;
+import com.opengamma.core.position.Position;
+import com.opengamma.core.security.Security;
+
+/**
+ * Implements a mapper function that returns the security referenced by a position.
+ */
+public class SecurityMapperFunction implements PortfolioMapperFunction<Security> {
+
+  @Override
+  public Security apply(PortfolioNode node) {
+    return null;
+  }
+
+  @Override
+  public Security apply(Position position) {
+    return position.getSecurity();
+  }
+  
+}
---
+++ projects/OG-Core/src/com/opengamma/core/position/impl/SecurityTypeMapperFunction.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.core.position.impl;
+
+import com.opengamma.core.position.PortfolioNode;
+import com.opengamma.core.position.Position;
+import com.opengamma.core.security.Security;
+
+/**
+ * Implements a mapper function that returns the type string of the security referenced by a position.
+ */
+public class SecurityTypeMapperFunction implements PortfolioMapperFunction<String> {
+
+  @Override
+  public String apply(PortfolioNode node) {
+    return null;
+  }
+
+  @Override
+  public String apply(Position position) {
+    Security security = position.getSecurity();
+    return security != null ? security.getSecurityType() : null;
+  }
+  
+}
