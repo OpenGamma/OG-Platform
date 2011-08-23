15c7e1bbdec344e006dbc24c9da8ee1e157647da
Stuart Archibald <stuart@opengamma.com>
2011-08-23 09:44:33 +0100

    Added the Toeplitz matrix type on request/suggestion from Richard.

---
+++ projects/OG-Analytics/src/com/opengamma/math/matrix/ToeplitzMatrix.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.math.matrix;
+
+/**
+ * A Toeplitz (or diagonal constant matrix) is a matrix which has the form:
+ *
+ * |a b c d e|
+ * |f a b c d|
+ * |g f a b c|
+ * |h g f a b|
+ * |i h g f a|
+ *
+ * such that the diagonals of the matrix have constant values.
+ *
+ * If it is know a priori that a Full Matrix will have entries of the Toeplitz matrix form and the Toeplitz form
+ * is immutable throughout the computation, then the matrix should be expressed using the VandermondeMatrix class. This definition
+ * allows a number of special algorithms to be used for linear algebra operations that have considerably lower Landau order
+ * than their Full Matrix counterparts.
+ *
+ */
+public class ToeplitzMatrix extends FullMatrix {
+  public ToeplitzMatrix(double[][] aMatrix) {
+    super(aMatrix);
+  }
+
+  public ToeplitzMatrix(DoubleMatrix2D aMatrix) {
+    super(aMatrix);
+  }
+
+  public ToeplitzMatrix(FullMatrix aMatrix) {
+    super(aMatrix.toArray());
+  }
+}
---
+++ projects/OG-Analytics/tests/unit/com/opengamma/math/matrix/ToeplitzMatrixTest.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.math.matrix;
+
+import org.testng.annotations.Test;
+
+/**
+ * Tests the Vandermonde Matrix type
+ */
+public class ToeplitzMatrixTest {
+  double[][] _data = {{1,2,3,4},{5,1,2,3},{6,5,1,2},{7,6,5,1}};
+
+/**
+ * there is no reason why these tests explicitly would fail, they rely on FullMatrix supers
+ * and so if these tests fail something is broken upstream.
+ */
+
+@Test
+public void testConstructorFromArrayOfArrays() {
+  new ToeplitzMatrix(_data);
+}
+
+@Test
+public void testConstructorFromDoubleMatrix2D() {
+  new ToeplitzMatrix(new DoubleMatrix2D(_data));
+}
+
+@Test
+public void testConstructorFromFullMatrix() {
+  new ToeplitzMatrix(new FullMatrix(_data));
+}
+
+
+}
