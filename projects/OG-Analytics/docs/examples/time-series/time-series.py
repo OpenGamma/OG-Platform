f792e6e0101ee7ed0afae54e546f74d0618b4fa8
Ana Nelson <ana@opengamma.com>
2011-08-24 19:30:39 +0100

    Add time series examples. Change version from dev to 0.9.1

--- projects/OG-Analytics/docs/examples/time-series/time-series.py
+++ projects/OG-Analytics/docs/examples/time-series/time-series.py
@@ -1,9 +1,3 @@
-#java.util.Date  com.opengamma.util.timeseries.date  ArrayDateDoubleTimeSeries   ListDateDoubleTimeSeries
-#java.sql.Date   com.opengamma.util.timeseries.sqldate   ArraySQLDateDoubleTimeSeries    ListSQLDateDoubleTimeSeries
-#javax.time.calendar.ZonedDateTime   com.opengamma.util.timeseries.zoneddatetime     ArrayZonedDateTimeDoubleTimeSeries  ListZonedDateTimeDoubleTimeSeries
-#java.util.Date  com.opengamma.util.timeseries.datetime  ArrayDateTimeDoubleTimeSeries   ListDateTimeDoubleTimeSeries
-#java.lang.Double    com.opengamma.util.timeseries.yearoffset    ArrayYearOffsetDoubleTimeSeries     ListYearOffsetDoubleTimeSeries 
-
 ### @export "local-date-imports"
 from javax.time.calendar import LocalDate
 from com.opengamma.util.timeseries.localdate import ArrayLocalDateDoubleTimeSeries
@@ -14,9 +8,11 @@ ts1 = ListLocalDateDoubleTimeSeries()
 ts1.putDataPoint(LocalDate.of(2010, 1, 1), 2.1)
 ts1.putDataPoint(LocalDate.of(2010, 1, 2), 2.2)
 ts1.putDataPoint(LocalDate.of(2010, 1, 3), 2.3)
+print ts1
 
 ### @export "ts-from-existing"
 ts2 = ArrayLocalDateDoubleTimeSeries(ts1)
+print ts2
 
 ### @export "ts-from-arrays"
 date_array = [
@@ -24,22 +20,63 @@ date_array = [
         LocalDate.of(2010, 1, 2),
         LocalDate.of(2010, 1, 3)
     ]
-ts3 = ArrayLocalDateDoubleTimeSeries(date_array, [2.1, 2.2, 2.3])
-
-print ts1
-print ts2
+ts3 = ArrayLocalDateDoubleTimeSeries(date_array, [1.4, -1.5, 1.6])
 print ts3
 
-### @export "ts-methods"
-time_series = ts3
-for c in dir(time_series):
+### @export "sub-series"
+ts2
+ts4 = ts2.subSeries(LocalDate.of(2010,1,2), LocalDate.of(2010,1,3))
+ts4
+ts2.tail(1)
+ts2.head(1)
+
+### @export "unary-ops"
+ts3
+ts3.abs()
+ts3.log()
+ts3.log10()
+ts3.negate()
+ts3.reciprocal()
+
+### @export "summaries"
+ts3.maxValue()
+ts3.minValue()
+
+### @export "info"
+ts3.times()
+ts3.values()
+ts3.getEarliestValue()
+ts3.getEarliestTime()
+ts3.getLatestValue()
+ts3.getLatestTime()
+
+### @export "binary-ops-scalar"
+ts3
+ts3.multiply(2)
+ts3.power(2)
+
+### @export "binary-ops"
+ts2
+ts3
+ts3.add(ts2)
+ts3.average(ts2)
+ts3.divide(ts2)
+ts3.maximum(ts2)
+ts3.minimum(ts2)
+ts3.multiply(ts2)
+ts3.power(ts2)
+ts3.subtract(ts2)
+
+### @export "union"
+ts2
+ts3
+ts4
+ts2.unionAdd(ts3)
+ts2.unionAdd(ts4)
+ts2.unionAverage(ts3)
+ts2.unionAverage(ts4)
+
+### @export "methods-list"
+for c in dir(ts3):
     print c
-
-print time_series.times()
-print time_series.values()
-print time_series.maxValue()
-print time_series.minValue()
-
-print time_series.negate()
-print time_series.reciprocal()
 
