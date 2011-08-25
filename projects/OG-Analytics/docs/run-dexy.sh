de5a7a1f7352c7b0d6f2d13cd56c6ee8e7fd3598
Ana Nelson <ana@opengamma.com>
2011-08-25 15:38:31 +0100

    Remove memory graph.

--- projects/OG-Analytics/docs/run-dexy.sh
+++ projects/OG-Analytics/docs/run-dexy.sh
@@ -4,5 +4,4 @@ dexy --global OG_VERSION=$VERSION
 mkdir -p $VERSION/java
 cp -r ../../../build/docs/javadoc-all/ $VERSION/java/javadocs
 cp -r output $VERSION/analytics
-R CMD BATCH graph-memory.R
 sudo python -m SimpleHTTPServer 80
