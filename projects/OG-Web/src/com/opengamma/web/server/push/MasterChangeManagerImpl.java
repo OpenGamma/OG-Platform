facd0760382a932fb7d07771a26bceff62a393f6
Chris Kent <chris@opengamma.com>
2011-09-02 11:26:52 +0100

    [PLAT-1515] Integrated master subscriptions into engine config

--- projects/OG-Web/src/com/opengamma/web/server/push/MasterChangeManagerImpl.java
+++ projects/OG-Web/src/com/opengamma/web/server/push/MasterChangeManagerImpl.java
@@ -3,6 +3,7 @@ package com.opengamma.web.server.push;
 import com.opengamma.core.change.ChangeEvent;
 import com.opengamma.core.change.ChangeListener;
 import com.opengamma.core.change.ChangeManager;
+import com.opengamma.core.change.ChangeProvider;
 import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.tuple.Pair;
 
@@ -17,11 +18,11 @@ public class MasterChangeManagerImpl implements MasterChangeManager {
 
   private final Set<MasterChangeListener> _listeners = new CopyOnWriteArraySet<MasterChangeListener>();
 
-  public MasterChangeManagerImpl(List<Pair<ChangeManager, MasterType>> changeManagers) {
-    for (Pair<ChangeManager, MasterType> changeManagerAndType : changeManagers) {
-      ChangeManager changeManager = changeManagerAndType.getFirst();
+  public MasterChangeManagerImpl(List<Pair<ChangeProvider, MasterType>> changeManagers) {
+    for (Pair<ChangeProvider, MasterType> changeManagerAndType : changeManagers) {
+      ChangeProvider changeProvider = changeManagerAndType.getFirst();
       final MasterType masterType = changeManagerAndType.getSecond();
-      changeManager.addChangeListener(new ChangeListener() {
+      changeProvider.changeManager().addChangeListener(new ChangeListener() {
         @Override
         public void entityChanged(ChangeEvent event) {
           MasterChangeManagerImpl.this.entityChanged(masterType);
