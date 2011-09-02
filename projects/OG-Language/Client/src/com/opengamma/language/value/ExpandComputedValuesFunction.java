33d712f36df3ee8f9ccb21e50ae6664fadbf74ec
Andrew Griffin <andrew@opengamma.com>
2011-09-02 15:33:05 +0100

    Added PositionSource and SecuritySource to the GlobalContext.

--- projects/OG-Language/Client/src/com/opengamma/language/value/ExpandComputedValuesFunction.java
+++ projects/OG-Language/Client/src/com/opengamma/language/value/ExpandComputedValuesFunction.java
@@ -9,6 +9,15 @@ package com.opengamma.language.value;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.opengamma.core.position.PortfolioNode;
+import com.opengamma.core.position.Position;
+import com.opengamma.core.position.PositionSource;
+import com.opengamma.core.position.Trade;
+import com.opengamma.core.security.Security;
+import com.opengamma.core.security.SecuritySource;
 import com.opengamma.engine.ComputationTargetSpecification;
 import com.opengamma.engine.value.ComputedValue;
 import com.opengamma.id.ExternalId;
@@ -25,6 +34,8 @@ import com.opengamma.language.function.PublishedFunction;
  * Expands a Fudge representation of a {@link List} of {@link ComputedValue} objects into a 2D structure.
  */
 public class ExpandComputedValuesFunction extends AbstractFunctionInvoker implements PublishedFunction {
+
+  private static final Logger s_logger = LoggerFactory.getLogger(ExpandComputedValuesFunction.class);
 
   private final MetaFunction _meta;
 
@@ -53,20 +64,68 @@ public class ExpandComputedValuesFunction extends AbstractFunctionInvoker implem
 
   private static String getName(final SessionContext sessionContext, final ComputationTargetSpecification targetSpec) {
     switch (targetSpec.getType()) {
-      case PORTFOLIO_NODE:
-        // TODO: fetch the portfolio node
+      case PORTFOLIO_NODE: {
+        final PositionSource positions = sessionContext.getGlobalContext().getPositionSource();
+        if (positions != null) {
+          final PortfolioNode node = positions.getPortfolioNode(targetSpec.getUniqueId());
+          if (node != null) {
+            return node.getName();
+          } else {
+            s_logger.warn("Node {} not found in position source", targetSpec);
+          }
+        }
         return "Node " + targetSpec.getUniqueId();
-      case POSITION:
-        // TODO: fetch the position
+      }
+      case POSITION: {
+        final PositionSource positions = sessionContext.getGlobalContext().getPositionSource();
+        final SecuritySource securities = sessionContext.getGlobalContext().getSecuritySource();
+        if ((positions != null) && (securities != null)) {
+          final Position position = positions.getPosition(targetSpec.getUniqueId());
+          if (position != null) {
+            final Security security = position.getSecurityLink().resolve(securities);
+            if (security != null) {
+              return position.getQuantity() + " x " + security.getName();
+            } else {
+              s_logger.warn("Security {} not found in security source for position {}", position.getSecurityLink(), targetSpec);
+            }
+          } else {
+            s_logger.warn("Position {} not found in position source", targetSpec);
+          }
+        }
         return "Position " + targetSpec.getUniqueId();
-      case SECURITY:
-        // TODO: fetch the security
+      }
+      case SECURITY: {
+        final SecuritySource securities = sessionContext.getGlobalContext().getSecuritySource();
+        if (securities != null) {
+          final Security security = securities.getSecurity(targetSpec.getUniqueId());
+          if (security != null) {
+            return security.getName();
+          } else {
+            s_logger.warn("Security {} not found in security source", targetSpec);
+          }
+        }
         return "Security " + targetSpec.getUniqueId();
+      }
       case PRIMITIVE:
         return "PRIMITIVE";
-      case TRADE:
-        // TODO: fetch the trade
+      case TRADE: {
+        final PositionSource positions = sessionContext.getGlobalContext().getPositionSource();
+        final SecuritySource securities = sessionContext.getGlobalContext().getSecuritySource();
+        if ((positions != null) && (securities != null)) {
+          final Trade trade = positions.getTrade(targetSpec.getUniqueId());
+          if (trade != null) {
+            final Security security = trade.getSecurityLink().resolve(securities);
+            if (security != null) {
+              return trade.getQuantity() + " x " + security.getName() + " (" + trade.getTradeDate() + ")";
+            } else {
+              s_logger.warn("Security {} not found in security source for trade {}", trade.getSecurityLink(), targetSpec);
+            }
+          } else {
+            s_logger.warn("Trade {} not found in position source", targetSpec);
+          }
+        }
         return "Trade " + targetSpec.getUniqueId();
+      }
       default:
         throw new IllegalStateException();
     }
