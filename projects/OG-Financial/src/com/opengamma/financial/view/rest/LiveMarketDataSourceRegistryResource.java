52f24cdbf028691575006ddb910e8225649b0821
Simon Jackson <simon@opengamma.com>
2011-09-02 15:25:53 +0100

    Move the LiveMarketDataSourceRepository to the viewprocessor and expose it remotely

---
+++ projects/OG-Financial/src/com/opengamma/financial/view/rest/LiveMarketDataSourceRegistryResource.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.view.rest;
+
+import java.util.Collection;
+
+import javax.ws.rs.GET;
+
+import com.opengamma.engine.marketdata.LiveMarketDataSourceRegistry;
+
+/**
+ * RESTful resource for {@link LiveMarketDataSourceRegistry}
+ */
+public class LiveMarketDataSourceRegistryResource {
+
+  private final LiveMarketDataSourceRegistry _liveMarketDataSourceRegistry;
+
+  public LiveMarketDataSourceRegistryResource(LiveMarketDataSourceRegistry liveMarketDataSourceRegistry) {
+    _liveMarketDataSourceRegistry = liveMarketDataSourceRegistry;
+  }
+
+  @GET
+  public Collection<String> getDataSources() {
+    return _liveMarketDataSourceRegistry.getDataSources();
+  }
+}
