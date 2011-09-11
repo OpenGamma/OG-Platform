31aa810970b40c13ce7c9b06db08609f4bc13f67
Jim Moores <jim.moores@gmail.com>
2011-09-11 15:45:14 +0100

    Changes current market cap agg function to use units of millions, changed number of threads in backend language client to 32.

--- projects/OG-Financial/src/com/opengamma/financial/aggregation/CurrentMarketCapAggregationFunction.java
+++ projects/OG-Financial/src/com/opengamma/financial/aggregation/CurrentMarketCapAggregationFunction.java
@@ -34,11 +34,11 @@ public class CurrentMarketCapAggregationFunction implements AggregationFunction<
   private static final String RESOLUTION_KEY = "DEFAULT_TSS_CONFIG";
   private static final String NO_CUR_MKT_CAP = "N/A";
   
-  private static final double NANO_CAP_UPPER_THRESHOLD = 10E6;
-  private static final double MICRO_CAP_UPPER_THRESHOLD = 100E6;
-  private static final double SMALL_CAP_UPPER_THRESHOLD = 1000E6;
-  private static final double MID_CAP_UPPER_THRESHOLD = 10E9;
-  private static final double LARGE_CAP_UPPER_THRESHOLD = 100E9;
+  private static final double NANO_CAP_UPPER_THRESHOLD = 10;
+  private static final double MICRO_CAP_UPPER_THRESHOLD = 100;
+  private static final double SMALL_CAP_UPPER_THRESHOLD = 1000;
+  private static final double MID_CAP_UPPER_THRESHOLD = 10E3;
+  private static final double LARGE_CAP_UPPER_THRESHOLD = 100E3;
   
   private static final String NANO_CAP = "E) Nano Cap";
   private static final String MICRO_CAP = "D) Micro Cap";
