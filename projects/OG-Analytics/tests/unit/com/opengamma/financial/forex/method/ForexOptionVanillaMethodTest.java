b4a75777982dcc7cb4f1b2b5b0e62c5cdd2c4840
Marc Henrard <marc@opengamma.com>
2011-09-26 16:04:29 +0100

    [PLAT-1556] [PLATY-1562] Added LMM method for cap floor Ibor. Created Monte Carlo method for Libor Market Model (LiborMarketModelMonteCarlo) and the related calculator of price from the Libor simulation (MonteCarloIborRateCalculator). Implemented the calculator for cap/floor Ibor, cash swaption and physical swaptions. Implemented the pricing method of physical swaptions in G2++ model and the related calibration.

--- projects/OG-Analytics/tests/unit/com/opengamma/financial/forex/method/ForexOptionVanillaMethodTest.java
+++ projects/OG-Analytics/tests/unit/com/opengamma/financial/forex/method/ForexOptionVanillaMethodTest.java
@@ -60,7 +60,7 @@ public class ForexOptionVanillaMethodTest {
   // Smile data
   private static final Currency CUR_1 = Currency.EUR;
   private static final Currency CUR_2 = Currency.USD;
-  private static final Period[] EXPIRY_PERIOD = new Period[] {Period.ofMonths(3), Period.ofMonths(6), Period.ofYears(1), Period.ofYears(2)};
+  private static final Period[] EXPIRY_PERIOD = new Period[] {Period.ofMonths(3), Period.ofMonths(6), Period.ofYears(1), Period.ofYears(2), Period.ofYears(5)};
   private static final int NB_EXP = EXPIRY_PERIOD.length;
   private static final ZonedDateTime REFERENCE_DATE = DateUtils.getUTCDate(2011, 6, 13);
   private static final ZonedDateTime REFERENCE_SPOT = ScheduleCalculator.getAdjustedDate(REFERENCE_DATE, CALENDAR, SETTLEMENT_DAYS);
@@ -76,10 +76,10 @@ public class ForexOptionVanillaMethodTest {
     }
   }
   private static final double SPOT = 1.40;
-  private static final double[] ATM = {0.175, 0.185, 0.18, 0.17, 0.16};
+  private static final double[] ATM = {0.175, 0.185, 0.18, 0.17, 0.16, 0.16};
   private static final double[] DELTA = new double[] {0.10, 0.25};
-  private static final double[][] RISK_REVERSAL = new double[][] { {-0.010, -0.0050}, {-0.011, -0.0060}, {-0.012, -0.0070}, {-0.013, -0.0080}, {-0.014, -0.0090}};
-  private static final double[][] STRANGLE = new double[][] { {0.0300, 0.0100}, {0.0310, 0.0110}, {0.0320, 0.0120}, {0.0330, 0.0130}, {0.0340, 0.0140}};
+  private static final double[][] RISK_REVERSAL = new double[][] { {-0.010, -0.0050}, {-0.011, -0.0060}, {-0.012, -0.0070}, {-0.013, -0.0080}, {-0.014, -0.0090}, {-0.014, -0.0090}};
+  private static final double[][] STRANGLE = new double[][] { {0.0300, 0.0100}, {0.0310, 0.0110}, {0.0320, 0.0120}, {0.0330, 0.0130}, {0.0340, 0.0140}, {0.0340, 0.0140}};
   private static final int NB_STRIKE = 2 * DELTA.length + 1;
   private static final SmileDeltaTermStructureParameter SMILE_TERM = new SmileDeltaTermStructureParameter(TIME_TO_EXPIRY, DELTA, ATM, RISK_REVERSAL, STRANGLE);
   // Methods and curves
