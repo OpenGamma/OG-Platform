9810821e3b3ce2d8fff758c8cbe32b0628d76ca0
Yomi Ayodele <yomi@opengamma.com>
2011-09-27 17:16:17 +0100

    print more info in toString in debug mode for ViewDefinition

--- projects/OG-Engine/src/com/opengamma/engine/view/ViewDefinition.java
+++ projects/OG-Engine/src/com/opengamma/engine/view/ViewDefinition.java
@@ -17,6 +17,8 @@ import java.util.TreeSet;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.opengamma.engine.value.ValueProperties;
 import com.opengamma.id.MutableUniqueIdentifiable;
@@ -36,6 +38,8 @@ import com.opengamma.util.tuple.Pair;
 @PublicAPI
 public class ViewDefinition implements Serializable, UniqueIdentifiable, MutableUniqueIdentifiable {
 
+  private static final Logger s_logger = LoggerFactory.getLogger(ViewDefinition.class);
+  
   private static final long serialVersionUID = 1L;
   
   private UniqueId _uniqueIdentifier;
@@ -473,8 +477,11 @@ public class ViewDefinition implements Serializable, UniqueIdentifiable, Mutable
 
   @Override
   public String toString() {
-    //    return "ViewDefinition[" + getName() + "]";
-    return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE, false);
+    if (s_logger.isDebugEnabled()) {
+      return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE, false);
+    } else {
+      return "ViewDefinition[" + getName() + "]";
+    }
   }
 
 }
