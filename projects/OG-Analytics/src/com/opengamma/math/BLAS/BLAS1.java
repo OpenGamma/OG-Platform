a75f41ccc2521ffaa982aa954b547daf46663ab0
Stuart Archibald <stuart@opengamma.com>
2011-10-03 12:19:00 +0100

    Added some more to BLAS1, finishing statefull DAXPY at present.

--- projects/OG-Analytics/src/com/opengamma/math/BLAS/BLAS1.java
+++ projects/OG-Analytics/src/com/opengamma/math/BLAS/BLAS1.java
@@ -29,31 +29,31 @@ public class BLAS1 {
    *
    */
 
+  /** Stateless versions */
+
   /**
    * Ensures that the inputs to DAXPY routines are sane when DAXPY is function(Vector,Vector).
-   * @param v1 double[] is the vector to be tested (y)
-   * @param v2 double[] is the vector to be tested (x)
+   * @param v1 double[] is the vector to be tested (x)
+   * @param v2 double[] is the vector to be tested (y)
    */
   public static void daxpyInputSanityChecker(double[] v1, double[] v2) {
-    final int xlen = v1.length;
-    final int ylen = v2.length;
     assertNotNull(v1); // check not null
     assertNotNull(v2); // check not null
+    final int xlen = v1.length;
+    final int ylen = v2.length;
     assertEquals(xlen, ylen); // going to struggle singleton addition unless the vectors are the same length
   }
 
   /**
    * DAXPY simplified: returns:=x+y
-   * @param y a double[] vector
    * @param x a double[] vector
+   * @param y a double[] vector
    * @return tmp double[] vector
    */
-  public double[] daxpy(double[] y, double[] x) {
+  public double[] daxpy(double[] x, double[] y) {
+    daxpyInputSanityChecker(x, y);
     final int n = y.length;
     double[] tmp = new double[n];
-    for (int i = 0; i < n; i++) {
-      tmp[i] = y[i] + x[i];
-    }
     final int extra = n - n % 16;
     final int ub = ((n / 16) * 16) - 1;
     // the induction (variable + loop unwind) common subexpression is actually spotted by the JIT and so
@@ -84,35 +84,242 @@ public class BLAS1 {
 
   /**
    * DAXPY simplified: returns:=x+y
-   * @param y a DoubleMatrix1D vector
    * @param x a double[] vector
-   * @return tmp double[] vector
-   */
-  public double[] daxpy(DoubleMatrix1D y, double[] x) {
-    return daxpy(y.getData(), x);
-  }
-
-  /**
-   * DAXPY simplified: returns:=x+y
-   * @param y a double[] vector
-   * @param x a DoubleMatrix1D vector
-   * @return tmp double[] vector
-   */
-  public double[] daxpy(double[] y, DoubleMatrix1D x) {
-    return daxpy(y, x.getData());
-  }
-
-  /**
-   * DAXPY simplified: returns:=x+y
    * @param y a DoubleMatrix1D vector
-   * @param x a DoubleMatrix1D vector
    * @return tmp double[] vector
    */
-  public double[] daxpy(DoubleMatrix1D y, DoubleMatrix1D x) {
-    return daxpy(y.getData(), x.getData());
+  public double[] daxpy(double[] x, DoubleMatrix1D y) {
+    return daxpy(x, y.getData());
   }
 
+  /**
+   * DAXPY simplified: returns:=x+y
+   * @param x a DoubleMatrix1D vector
+   * @param y a double[] vector
+   * @return tmp double[] vector
+   */
+  public double[] daxpy(DoubleMatrix1D x, double[] y) {
+    return daxpy(x.getData(), y);
+  }
 
+  /**
+   * DAXPY simplified: returns:=x+y
+   * @param x a DoubleMatrix1D vector
+   * @param y a DoubleMatrix1D vector
+   * @return tmp double[] vector
+   */
+  public double[] daxpy(DoubleMatrix1D x, DoubleMatrix1D y) {
+    return daxpy(x.getData(), y.getData());
+  }
+
+  /**
+   * DAXPY returns:=alpha*x+y
+   * @param alpha double
+   * @param x a double[] vector
+   * @param y a double[] vector
+   * @return tmp double[] vector
+   */
+  public double[] daxpy(double alpha, double[] x, double[] y) {
+    daxpyInputSanityChecker(x, y);
+    final int n = y.length;
+    double[] tmp = new double[n];
+    if (Double.doubleToLongBits(alpha) == 0) { // short cut if alpha = 0, insanely stupid thing to do but might occur if coming from generated results
+      System.arraycopy(y, 0, tmp, 0, n);
+      return tmp;
+    }
+    final int extra = n - n % 16;
+    final int ub = ((n / 16) * 16) - 1;
+    // the induction (variable + loop unwind) common subexpression is actually spotted by the JIT and so
+    // doesn't need to be spelled out which is a nice change
+    for (int i = 0; i < ub; i += 16) {
+      tmp[i] = alpha * x[i] + y[i];
+      tmp[i + 1] = alpha * x[i + 1] + y[i + 1];
+      tmp[i + 2] = alpha * x[i + 2] + y[i + 2];
+      tmp[i + 3] = alpha * x[i + 3] + y[i + 3];
+      tmp[i + 4] = alpha * x[i + 4] + y[i + 4];
+      tmp[i + 5] = alpha * x[i + 5] + y[i + 5];
+      tmp[i + 6] = alpha * x[i + 6] + y[i + 6];
+      tmp[i + 7] = alpha * x[i + 7] + y[i + 7];
+      tmp[i + 8] = alpha * x[i + 8] + y[i + 8];
+      tmp[i + 9] = alpha * x[i + 9] + y[i + 9];
+      tmp[i + 10] = alpha * x[i + 10] + y[i + 10];
+      tmp[i + 11] = alpha * x[i + 11] + y[i + 11];
+      tmp[i + 12] = alpha * x[i + 12] + y[i + 12];
+      tmp[i + 13] = alpha * x[i + 13] + y[i + 13];
+      tmp[i + 14] = alpha * x[i + 14] + y[i + 14];
+      tmp[i + 15] = alpha * x[i + 15] + y[i + 15];
+    }
+    for (int i = extra; i < n; i++) {
+      tmp[i] = alpha * x[i] + y[i];
+    }
+    return tmp;
+  }
+
+  /**
+   * DAXPY returns:=x+y
+   * @param alpha double
+   * @param x a double[] vector
+   * @param y a DoubleMatrix1D vector
+   * @return tmp double[] vector
+   */
+  public double[] daxpy(double alpha, double[] x, DoubleMatrix1D y) {
+    return daxpy(alpha, x, y.getData());
+  }
+
+  /**
+   * DAXPY returns:=x+y
+   * @param alpha double
+   * @param x a DoubleMatrix1D vector
+   * @param y a double[] vector
+   * @return tmp double[] vector
+   */
+  public double[] daxpy(double alpha, DoubleMatrix1D x, double[] y) {
+    return daxpy(alpha, x.getData(), y);
+  }
+
+  /**
+   * DAXPY returns:=x+y
+   * @param alpha double
+   * @param x a DoubleMatrix1D vector
+   * @param y a DoubleMatrix1D vector
+   * @return tmp double[] vector
+   */
+  public double[] daxpy(double alpha, DoubleMatrix1D x, DoubleMatrix1D y) {
+    return daxpy(alpha, x.getData(), y.getData());
+  }
+
+  /** Stateful versions - mangle memory! */
+
+  /**
+   * DAXPY simplified: y:=x+y
+   * @param x a double[] vector
+   * @param y a double[] vector
+   */
+  public void daxpyInplace(double[] x, double[] y) {
+    daxpyInputSanityChecker(x, y);
+    final int n = y.length;
+    final int extra = n - n % 16;
+    final int ub = ((n / 16) * 16) - 1;
+    // the induction (variable + loop unwind) common subexpression is actually spotted by the JIT and so
+    // doesn't need to be spelled out which is a nice change
+    for (int i = 0; i < ub; i += 16) {
+      y[i] += x[i];
+      y[i + 1] += x[i + 1];
+      y[i + 2] += x[i + 2];
+      y[i + 3] += x[i + 3];
+      y[i + 4] += x[i + 4];
+      y[i + 5] += x[i + 5];
+      y[i + 6] += x[i + 6];
+      y[i + 7] += x[i + 7];
+      y[i + 8] += x[i + 8];
+      y[i + 9] += x[i + 9];
+      y[i + 10] += x[i + 10];
+      y[i + 11] += x[i + 11];
+      y[i + 12] += x[i + 12];
+      y[i + 13] += x[i + 13];
+      y[i + 14] += x[i + 14];
+      y[i + 15] += x[i + 15];
+    }
+    for (int i = extra; i < n; i++) {
+      y[i] = +x[i];
+    }
+  }
+
+  /**
+   * DAXPY simplified: returns:=x+y
+   * @param x a double[] vector
+   * @param y a DoubleMatrix1D vector
+   */
+  public void daxpyInplace(double[] x, DoubleMatrix1D y) {
+    daxpyInplace(x, y.getData());
+  }
+
+  /**
+   * DAXPY simplified: returns:=x+y
+   * @param x a DoubleMatrix1D vector
+   * @param y a double[] vector
+   */
+  public void daxpyInplace(DoubleMatrix1D x, double[] y) {
+    daxpyInplace(x.getData(), y);
+  }
+
+  /**
+   * DAXPY simplified: returns:=x+y
+   * @param x a DoubleMatrix1D vector
+   * @param y a DoubleMatrix1D vector
+   */
+  public void daxpyInplace(DoubleMatrix1D x, DoubleMatrix1D y) {
+    daxpyInplace(x.getData(), y.getData());
+  }
+
+  /**
+   * DAXPY: y:=alpha*x+y
+   * @param alpha double
+   * @param x a double[] vector
+   * @param y a double[] vector
+   */
+  public void daxpyInplace(double alpha, double[] x, double[] y) {
+    daxpyInputSanityChecker(x, y);
+    final int n = y.length;
+    if (Double.doubleToLongBits(alpha) == 0) { // short cut if alpha = 0, insanely stupid thing to do but might occur if coming from generated results
+      return;
+    }
+    final int extra = n - n % 16;
+    final int ub = ((n / 16) * 16) - 1;
+    // the induction (variable + loop unwind) common subexpression is actually spotted by the JIT and so
+    // doesn't need to be spelled out which is a nice change
+    for (int i = 0; i < ub; i += 16) {
+      y[i] += alpha * x[i];
+      y[i + 1] += alpha * x[i + 1];
+      y[i + 2] += alpha * x[i + 2];
+      y[i + 3] += alpha * x[i + 3];
+      y[i + 4] += alpha * x[i + 4];
+      y[i + 5] += alpha * x[i + 5];
+      y[i + 6] += alpha * x[i + 6];
+      y[i + 7] += alpha * x[i + 7];
+      y[i + 8] += alpha * x[i + 8];
+      y[i + 9] += alpha * x[i + 9];
+      y[i + 10] += alpha * x[i + 10];
+      y[i + 11] += alpha * x[i + 11];
+      y[i + 12] += alpha * x[i + 12];
+      y[i + 13] += alpha * x[i + 13];
+      y[i + 14] += alpha * x[i + 14];
+      y[i + 15] += alpha * x[i + 15];
+    }
+    for (int i = extra; i < n; i++) {
+      y[i] = +alpha * x[i];
+    }
+  }
+
+  /**
+   * DAXPY: y:=alpha*x+y
+   * @param alpha double
+   * @param x a double[] vector
+   * @param y a DoubleMatrix1D vector
+   */
+  public void daxpyInplace(double alpha, double[] x, DoubleMatrix1D y) {
+    daxpyInplace(x, y.getData());
+  }
+
+  /**
+   * DAXPY: y:=alpha*x+y
+   * @param alpha double
+   * @param x a DoubleMatrix1D vector
+   * @param y a double[] vector
+   */
+  public void daxpyInplace(double alpha, DoubleMatrix1D x, double[] y) {
+    daxpyInplace(x.getData(), y);
+  }
+
+  /**
+   * DAXPY: y:=alpha*x+y
+   * @param alpha double
+   * @param x a DoubleMatrix1D vector
+   * @param y a DoubleMatrix1D vector
+   */
+  public void daxpyInplace(double alpha, DoubleMatrix1D x, DoubleMatrix1D y) {
+    daxpyInplace(x.getData(), y.getData());
+  }
 
 } // class end
 
--- projects/OG-Analytics/tests/unit/com/opengamma/math/BLAS/BLAS1Test.java
+++ projects/OG-Analytics/tests/unit/com/opengamma/math/BLAS/BLAS1Test.java
@@ -5,9 +5,13 @@
  */
 package com.opengamma.math.BLAS;
 
+import static org.testng.AssertJUnit.assertTrue;
+
 import java.util.Arrays;
 
 import org.testng.annotations.Test;
+
+import com.opengamma.math.matrix.DoubleMatrix1D;
 
 
 /**
@@ -17,43 +21,233 @@ public class BLAS1Test {
   // tests 1element short vector
   double[] x1 = {1};
   double[] y1 = {10};
-  double[] x1_times_y1={10};
+  double[] x1_plus_y1={11};
+  double[] alpha_times_x1_plus_y1={17};
 
   // hits loop unwind max
   double[] x16 = range(1,16);
   double[] y16 = range(10,160,10);
-  double[] x16_times_y16 = {10,40,90,160,250,360,490,640,810,1000,1210,1440,1690,1960,2250,2560};
+  double[] x16_plus_y16 = {11,22,33,44,55,66,77,88,99,110,121,132,143,154,165,176};
+  double[] alpha_times_x16_plus_y16 = {17,34,51,68,85,102,119,136,153,170,187,204,221,238,255,272};
 
   // trips loop unwinds to hit clean up code
-  double[] x37 = {1};
-  double[] y37 = {10};
-  double[] x37_times_y37 = {10,40,90,160,250,360,490,640,810,1000,1210,1440,1690,1960,2250,2560,2890,3240,3610,4000,4410,4840,5290,5760,6250,6760,7290,7840,8410,9000,9610,10240,10890,11560,12250,12960,13690};
+  double[] x37 = range(1,37);
+  double[] y37 = range(10,370,10);
+  double[] x37_plus_y37 = {11,22,33,44,55,66,77,88,99,110,121,132,143,154,165,176,187,198,209,220,231,242,253,264,275,286,297,308,319,330,341,352,363,374,385,396,407};
+  double[] alpha_times_x37_plus_y37 = {17,34,51,68,85,102,119,136,153,170,187,204,221,238,255,272,289,306,323,340,357,374,391,408,425,442,459,476,493,510,527,544,561,578,595,612,629};
+
+  // scalar
+  double alpha = 7;
 
   BLAS1 blas1 = new BLAS1();
 
+/** test the sanity checker */
+@Test(expectedExceptions = AssertionError.class)
+public void testDAXPYsanityCheckerArg1() {
+  double[] tmp1 = null;
+  double[] tmp2 = {1};
+  blas1.daxpy(tmp1, tmp2);
+}
+
+@Test(expectedExceptions = AssertionError.class)
+public void testDAXPYsanityCheckerArg2() {
+  double[] tmp1 = {1};
+  double[] tmp2 = null;
+  blas1.daxpy(tmp1, tmp2);
+}
+
+@Test(expectedExceptions = AssertionError.class)
+public void testDAXPYsanityCheckerLengths() {
+  double[] tmp1 = {1,2};
+  double[] tmp2 = {3,4,5};
+  blas1.daxpy(tmp1, tmp2);
+}
+
+/** y:=x+y */
+// Test DAXPY double[] double[] interface
+
 @Test
-public void testDAXPY_ans_eq_x1_times_y1() {
-  double[] tmp = blas1.daxpy(y1, x1);
-  Arrays.equals(x1_times_y1, tmp);
+public void testDAXPY_ans_eq_x1_plus_y1() {
+  double[] tmp = blas1.daxpy(x1, y1);
+  assertTrue(Arrays.equals(x1_plus_y1, tmp));
 }
 
 @Test
-public void testDAXPY_ans_eq_x16_times_y16() {
-  double[] tmp = blas1.daxpy(y16, x16);
-  Arrays.equals(x16_times_y16, tmp);
+public void testDAXPY_ans_eq_x16_plus_y16() {
+  double[] tmp = blas1.daxpy(x16, y16);
+  assertTrue(Arrays.equals(x16_plus_y16, tmp));
 }
 
 @Test
-public void testDAXPY_ans_eq_x37_times_y37() {
-  double[] tmp = blas1.daxpy(y37, x37);
-  Arrays.equals(x37_times_y37, tmp);
+public void testDAXPY_ans_eq_x37_plus_y37() {
+  double[] tmp = blas1.daxpy(x37, y37);
+  assertTrue(Arrays.equals(x37_plus_y37, tmp));
+}
+
+//Test DAXPY double[] DoubleMatrix1D interface
+@Test
+public void testDAXPY_ans_eq_x1_plus_D1D_y1() {
+  double[] tmp = blas1.daxpy(x1, new DoubleMatrix1D(y1));
+  assertTrue(Arrays.equals(x1_plus_y1, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_x16_plus_D1D_y16() {
+  double[] tmp = blas1.daxpy(x16, new DoubleMatrix1D(y16));
+  assertTrue(Arrays.equals(x16_plus_y16, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_x37_plus_D1D_y37() {
+  double[] tmp = blas1.daxpy(x37, new DoubleMatrix1D(y37));
+  assertTrue(Arrays.equals(x37_plus_y37, tmp));
 }
 
 
-/* helper functions to generate number ranges */
+//Test DAXPY DoubleMatrix1D double[] interface
+@Test
+public void testDAXPY_ans_eq_D1D_x1_plus_y1() {
+  double[] tmp = blas1.daxpy(new DoubleMatrix1D(x1), y1);
+  assertTrue(Arrays.equals(x1_plus_y1, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_D1D_x16_plus_y16() {
+  double[] tmp = blas1.daxpy(new DoubleMatrix1D(x16), y16);
+  assertTrue(Arrays.equals(x16_plus_y16, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_D1D_x37_plus_y37() {
+  double[] tmp = blas1.daxpy(new DoubleMatrix1D(x37), y37);
+  assertTrue(Arrays.equals(x37_plus_y37, tmp));
+}
+
+//Test DAXPY DoubleMatrix1D DoubleMatrix1D interface
+@Test
+public void testDAXPY_ans_eq_D1D_x1_plus_D1D_y1() {
+  double[] tmp = blas1.daxpy(new DoubleMatrix1D(x1), new DoubleMatrix1D(y1));
+  assertTrue(Arrays.equals(x1_plus_y1, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_D1D_x16_plus_D1D_y16() {
+  double[] tmp = blas1.daxpy(new DoubleMatrix1D(x16), new DoubleMatrix1D(y16));
+  assertTrue(Arrays.equals(x16_plus_y16, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_D1D_x37_plus_D1D_y37() {
+  double[] tmp = blas1.daxpy(new DoubleMatrix1D(x37), new DoubleMatrix1D(y37));
+  assertTrue(Arrays.equals(x37_plus_y37, tmp));
+}
+
+
+/** y:=alpha*x + y */
+//Test DAXPY double[] double[] interface
+@Test
+public void testDAXPY_ans_eq_alpha_x1_plus_y1() {
+  double[] tmp = blas1.daxpy(alpha, x1, y1);
+  assertTrue(Arrays.equals(alpha_times_x1_plus_y1, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alpha_x16_plus_y16() {
+  double[] tmp = blas1.daxpy(alpha, x16, y16);
+  System.out.println(new DoubleMatrix1D(tmp).toString());
+  assertTrue(Arrays.equals(alpha_times_x16_plus_y16, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alpha_x37_plus_y37() {
+  double[] tmp = blas1.daxpy(alpha, x37, y37);
+  assertTrue(Arrays.equals(alpha_times_x37_plus_y37, tmp));
+}
+
+//Test DAXPY double[] DoubleMatrix1D interface
+@Test
+public void testDAXPY_ans_eq_alpha_x1_plus_D1D_y1() {
+  double[] tmp = blas1.daxpy(alpha, x1, new DoubleMatrix1D(y1));
+  assertTrue(Arrays.equals(alpha_times_x1_plus_y1, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alpha_x16_plus_D1D_y16() {
+  double[] tmp = blas1.daxpy(alpha, x16, new DoubleMatrix1D(y16));
+  assertTrue(Arrays.equals(alpha_times_x16_plus_y16, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alpha_x37_plus_D1D_y37() {
+  double[] tmp = blas1.daxpy(alpha, x37, new DoubleMatrix1D(y37));
+  assertTrue(Arrays.equals(alpha_times_x37_plus_y37, tmp));
+}
+
+
+//Test DAXPY DoubleMatrix1D double[] interface
+@Test
+public void testDAXPY_ans_eq_alpha_D1D_x1_plus_y1() {
+  double[] tmp = blas1.daxpy(alpha, new DoubleMatrix1D(x1), y1);
+  assertTrue(Arrays.equals(alpha_times_x1_plus_y1, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alpha_D1D_x16_plus_y16() {
+  double[] tmp = blas1.daxpy(alpha, new DoubleMatrix1D(x16), y16);
+  assertTrue(Arrays.equals(alpha_times_x16_plus_y16, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alpha_D1D_x37_plus_y37() {
+  double[] tmp = blas1.daxpy(alpha, new DoubleMatrix1D(x37), y37);
+  assertTrue(Arrays.equals(alpha_times_x37_plus_y37, tmp));
+}
+
+//Test DAXPY DoubleMatrix1D DoubleMatrix1D interface
+@Test
+public void testDAXPY_ans_eq_alpha_D1D_x1_plus_D1D_y1() {
+  double[] tmp = blas1.daxpy(alpha, new DoubleMatrix1D(x1), new DoubleMatrix1D(y1));
+  assertTrue(Arrays.equals(alpha_times_x1_plus_y1, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alpha_D1D_x16_plus_D1D_y16() {
+  double[] tmp = blas1.daxpy(alpha, new DoubleMatrix1D(x16), new DoubleMatrix1D(y16));
+  assertTrue(Arrays.equals(alpha_times_x16_plus_y16, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alpha_D1D_x37_plus_D1D_y37() {
+  double[] tmp = blas1.daxpy(alpha, new DoubleMatrix1D(x37), new DoubleMatrix1D(y37));
+  assertTrue(Arrays.equals(alpha_times_x37_plus_y37, tmp));
+}
+
+//test fall through if(alpha==0)
+@Test
+public void testDAXPY_ans_eq_alphaZERO_x1_plus_y1() {
+  double[] tmp = blas1.daxpy(0, x1, y1);
+  assertTrue(Arrays.equals(y1, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alphaZERO_x16_plus_y16() {
+  double[] tmp = blas1.daxpy(0, x16, y16);
+  assertTrue(Arrays.equals(y16, tmp));
+}
+
+@Test
+public void testDAXPY_ans_eq_alphaZERO_x37_plus_y37() {
+  double[] tmp = blas1.daxpy(0, x37, y37);
+  assertTrue(Arrays.equals(y37, tmp));
+}
+
+/** Test the inplaces */
+
+
+/** helper functions to generate number ranges */
   private double[] range(int low, int high) {
     assert(high>=low);
-    final int lim = (high - low) > 0 ? (high - low) : 1;
+    final int lim = (high - low) > 0 ? (high - low) + 1 : 1;
     double [] tmp = new double[lim];
     for ( int i = 0; i < lim; i++) {
       tmp[i] = low + i;
@@ -64,8 +258,8 @@ public void testDAXPY_ans_eq_x37_times_y37() {
   private double[] range(int low, int high, int step) {
     assert(high>=low);
     assert(step>0);
-    final int t = (high - low) > 0 ? (high - low) : 1;
-    final int lim = t / step;
+    final int t = (high - low) > 0 ? (high - low)  + step : 1;
+    final int lim = t/step;
     double [] tmp = new double[lim];
     for ( int i = 0; i < lim; i++) {
       tmp[i] = low + i*step;
