3cf9944a3709c2f7516a6cfea1d2faf62b34ad36
Andrew Griffin <andrew@opengamma.com>
2011-10-19 16:18:00 +0100

    Added FetchSnapshotFunction to OG-Language. (Partial) remote implementation of MarketDataSnapshotSource.

---
+++ projects/OG-Language/Client/src/com/opengamma/language/snapshot/Loader.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+package com.opengamma.language.snapshot;
+
+import org.fudgemsg.FudgeContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.opengamma.financial.marketdatasnapshot.rest.RemoteMarketDataSnapshotSource;
+import com.opengamma.language.config.Configuration;
+import com.opengamma.language.context.ContextInitializationBean;
+import com.opengamma.language.context.MutableGlobalContext;
+import com.opengamma.language.function.FunctionProviderBean;
+import com.opengamma.transport.jaxrs.RestTarget;
+import com.opengamma.util.ArgumentChecker;
+
+/**
+ * Extends the global context with market data snapshot support (if available).
+ */
+public class Loader extends ContextInitializationBean {
+
+  private static final Logger s_logger = LoggerFactory.getLogger(Loader.class);
+
+  private Configuration _configuration;
+  private String _configurationEntry = "marketDataSnapshotSource";
+  private FudgeContext _fudgeContext = FudgeContext.GLOBAL_DEFAULT;
+
+  public void setConfiguration(final Configuration configuration) {
+    ArgumentChecker.notNull(configuration, "configuration");
+    _configuration = configuration;
+  }
+
+  public Configuration getConfiguration() {
+    return _configuration;
+  }
+
+  public void setConfigurationEntry(final String configurationEntry) {
+    ArgumentChecker.notNull(configurationEntry, "configurationEntry");
+    _configurationEntry = configurationEntry;
+  }
+
+  public String getConfigurationEntry() {
+    return _configurationEntry;
+  }
+
+  public void setFudgeContext(final FudgeContext fudgeContext) {
+    ArgumentChecker.notNull(fudgeContext, "fudgeContext");
+    _fudgeContext = fudgeContext;
+  }
+
+  public FudgeContext getFudgeContext() {
+    return _fudgeContext;
+  }
+
+  // ContextInitializationBean
+
+  @Override
+  protected void assertPropertiesSet() {
+    ArgumentChecker.notNull(getConfiguration(), "configuration");
+    ArgumentChecker.notNull(getGlobalContextFactory(), "globalContextFactory");
+  }
+
+  @Override
+  protected void initContext(final MutableGlobalContext globalContext) {
+    final RestTarget restTarget = getConfiguration().getRestTargetConfiguration(getConfigurationEntry());
+    if (restTarget == null) {
+      s_logger.warn("Snapshot support not available");
+      return;
+    }
+    s_logger.info("Configuring snapshot support");
+    globalContext.setMarketDataSnapshotSource(new RemoteMarketDataSnapshotSource(getConfiguration().getFudgeContext(), restTarget));
+    globalContext.getFunctionProvider().addProvider(new FunctionProviderBean(
+        FetchSnapshotFunction.INSTANCE));
+    // TODO: type converters
+  }
+
+}
