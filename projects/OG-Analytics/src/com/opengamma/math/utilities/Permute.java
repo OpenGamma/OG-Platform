9fa22ff468c76b43e788cbc0f8fadccb40db67c7
Stuart Archibald <stuart@opengamma.com>
2011-10-28 11:05:24 +0100

    Added ewec to Permute.

--- projects/OG-Analytics/src/com/opengamma/math/utilities/Permute.java
+++ projects/OG-Analytics/src/com/opengamma/math/utilities/Permute.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
- * 
+ *
  * Please see distribution for license.
  */
 package com.opengamma.math.utilities;
@@ -25,7 +25,7 @@ public class Permute {
     System.arraycopy(tmp, 0, v, 0, v.length);
     return;
   }
-  
+
   /**
    * Applies permutation p statelessly to vector v
    * @param v the vector to permute
@@ -52,6 +52,138 @@ public class Permute {
   private static int[] permuter(int[] v, int[] p) {
     final int n = v.length;
     int[] tmp = new int[n];
+    for (int i = 0; i < n; i++) {
+      tmp[i] = v[p[i]];
+    }
+    return tmp;
+  }
+
+  /**
+   * Applies permutation p inplace to vector v.
+   * @param v the vector to permute
+   * @param p the permutation
+   */
+  public static void inplace(long[] v, int[] p) {
+    inputValidator(v, p);
+    final long[] tmp = permuter(v, p);
+    System.arraycopy(tmp, 0, v, 0, v.length);
+    return;
+  }
+
+  /**
+   * Applies permutation p statelessly to vector v
+   * @param v the vector to permute
+   * @param p the permutation
+   * @return tmp, a permuted vector v[p].
+   */
+  public static long[] stateless(long[] v, int[] p) {
+    inputValidator(v, p);
+    return permuter(v, p);
+  }
+
+  /* checks the input */
+  private static void inputValidator(long[] v, int[] p) {
+    Validate.notNull(v);
+    Validate.notNull(p);
+    assertTrue("Permutation is of length: " + p.length + " whereas vector is of length " + v.length + ". Permutation is therefore impossible.",
+        v.length == p.length); // shortcut costly parse tests if vectors are not the same length
+    assertTrue("Permutation contains indices with impossible range (too large)", Max.value(p) < v.length); // make sure the permutation won't go out of range.
+    assertTrue("Permutation doesn't contain index 0", Min.value(p) == 0); // make sure the permutation won't go out of range, also catches -ve indices
+    assertTrue("Permutation is nonunique (some indices are repeated)", Unique.bitwise(p).length == p.length); // make sure the permutation is a valid permutation.
+  }
+
+  /* actually does the permutation, can proceed unchecked as validity is performed in the callers */
+  private static long[] permuter(long[] v, int[] p) {
+    final int n = v.length;
+    long[] tmp = new long[n];
+    for (int i = 0; i < n; i++) {
+      tmp[i] = v[p[i]];
+    }
+    return tmp;
+  }
+
+  /**
+   * Applies permutation p inplace to vector v.
+   * @param v the vector to permute
+   * @param p the permutation
+   */
+  public static void inplace(float[] v, int[] p) {
+    inputValidator(v, p);
+    final float[] tmp = permuter(v, p);
+    System.arraycopy(tmp, 0, v, 0, v.length);
+    return;
+  }
+
+  /**
+   * Applies permutation p statelessly to vector v
+   * @param v the vector to permute
+   * @param p the permutation
+   * @return tmp, a permuted vector v[p].
+   */
+  public static float[] stateless(float[] v, int[] p) {
+    inputValidator(v, p);
+    return permuter(v, p);
+  }
+
+  /* checks the input */
+  private static void inputValidator(float[] v, int[] p) {
+    Validate.notNull(v);
+    Validate.notNull(p);
+    assertTrue("Permutation is of length: " + p.length + " whereas vector is of length " + v.length + ". Permutation is therefore impossible.",
+        v.length == p.length); // shortcut costly parse tests if vectors are not the same length
+    assertTrue("Permutation contains indices with impossible range (too large)", Max.value(p) < v.length); // make sure the permutation won't go out of range.
+    assertTrue("Permutation doesn't contain index 0", Min.value(p) == 0); // make sure the permutation won't go out of range, also catches -ve indices
+    assertTrue("Permutation is nonunique (some indices are repeated)", Unique.bitwise(p).length == p.length); // make sure the permutation is a valid permutation.
+  }
+
+  /* actually does the permutation, can proceed unchecked as validity is performed in the callers */
+  private static float[] permuter(float[] v, int[] p) {
+    final int n = v.length;
+    float[] tmp = new float[n];
+    for (int i = 0; i < n; i++) {
+      tmp[i] = v[p[i]];
+    }
+    return tmp;
+  }
+
+  /**
+   * Applies permutation p inplace to vector v.
+   * @param v the vector to permute
+   * @param p the permutation
+   */
+  public static void inplace(double[] v, int[] p) {
+    inputValidator(v, p);
+    final double[] tmp = permuter(v, p);
+    System.arraycopy(tmp, 0, v, 0, v.length);
+    return;
+  }
+
+  /**
+   * Applies permutation p statelessly to vector v
+   * @param v the vector to permute
+   * @param p the permutation
+   * @return tmp, a permuted vector v[p].
+   */
+  public static double[] stateless(double[] v, int[] p) {
+    inputValidator(v, p);
+    return permuter(v, p);
+  }
+
+  /* checks the input */
+  private static void inputValidator(double[] v, int[] p) {
+    Validate.notNull(v);
+    Validate.notNull(p);
+    assertTrue("Permutation is of length: " + p.length + " whereas vector is of length " + v.length + ". Permutation is therefore impossible.",
+        v.length == p.length); // shortcut costly parse tests if vectors are not the same length
+    assertTrue("Permutation contains indices with impossible range (too large)", Max.value(p) < v.length); // make sure the permutation won't go out of range.
+    assertTrue("Permutation doesn't contain index 0", Min.value(p) == 0); // make sure the permutation won't go out of range, also catches -ve indices
+    assertTrue("Permutation is nonunique (some indices are repeated)", Unique.bitwise(p).length == p.length); // make sure the permutation is a valid permutation.
+  }
+
+  /* actually does the permutation, can proceed unchecked as validity is performed in the callers */
+  private static double[] permuter(double[] v, int[] p) {
+    final int n = v.length;
+    double[] tmp = new double[n];
     for (int i = 0; i < n; i++) {
       tmp[i] = v[p[i]];
     }
--- projects/OG-Analytics/tests/unit/com/opengamma/math/utilities/PermuteTest.java
+++ projects/OG-Analytics/tests/unit/com/opengamma/math/utilities/PermuteTest.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
- * 
+ *
  * Please see distribution for license.
  */
 package com.opengamma.math.utilities;
@@ -21,11 +21,40 @@ public class PermuteTest {
   int[] _singledataI = {1 };
   int[] _smalldataI = {1, 2, 3, 4, 5 };
   int[] _meddataI = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
   /* int answers */
   int[] _singlePermedI = {1 };
   int[] _smallPermedI = {1, 3, 5, 2, 4 };
   int[] _medPermedI = {2, 4, 6, 8, 10, 1, 3, 5, 7, 9 };
+
+  /* long data */
+  long[] _nulldataL = null;
+  long[] _singledataL = {1 };
+  long[] _smalldataL = {1, 2, 3, 4, 5 };
+  long[] _meddataL = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  /* long answers */
+  long[] _singlePermedL = {1 };
+  long[] _smallPermedL = {1, 3, 5, 2, 4 };
+  long[] _medPermedL = {2, 4, 6, 8, 10, 1, 3, 5, 7, 9 };
+
+  /* float data */
+  float[] _nulldataF = null;
+  float[] _singledataF = {1 };
+  float[] _smalldataF = {1, 2, 3, 4, 5 };
+  float[] _meddataF = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  /* float answers */
+  float[] _singlePermedF = {1 };
+  float[] _smallPermedF = {1, 3, 5, 2, 4 };
+  float[] _medPermedF = {2, 4, 6, 8, 10, 1, 3, 5, 7, 9 };
+
+  /* double data */
+  double[] _nulldataD = null;
+  double[] _singledataD = {1 };
+  double[] _smalldataD = {1, 2, 3, 4, 5 };
+  double[] _meddataD = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  /* double answers */
+  double[] _singlePermedD = {1 };
+  double[] _smallPermedD = {1, 3, 5, 2, 4 };
+  double[] _medPermedD = {2, 4, 6, 8, 10, 1, 3, 5, 7, 9 };
 
   /* permutations */
   int[] _nullPerm = null;
@@ -36,7 +65,7 @@ public class PermuteTest {
   int[] _badPermPos = {0, 1, 2, 3, 19 };
   int[] _badPermUniq = {0, 1, 2, 2, 3 };
 
-  /** try and break the function */
+  /* try and break the on Int function */
   @Test(expectedExceptions = IllegalArgumentException.class)
   public void testNullDataI() {
     Permute.inplace(_nulldataI, _singlePerm);
@@ -93,4 +122,178 @@ public class PermuteTest {
     Permute.inplace(tmp, _medPerm);
     assertTrue(Arrays.equals(_medPermedI, tmp));
   }
+
+
+  /* try and break the on Long function */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testNullDataL() {
+    Permute.inplace(_nulldataL, _singlePerm);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testNullPermutationL() {
+    Permute.inplace(_singledataL, _nullPerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testShortPermutationL() {
+    Permute.inplace(_smalldataL, _singlePerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testShortVectorL() {
+    Permute.inplace(_singledataL, _smallPerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermNegL() {
+    Permute.inplace(_smalldataL, _badPermNeg);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermPosL() {
+    Permute.inplace(_smalldataL, _badPermPos);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermUniqL() {
+    Permute.inplace(_smalldataL, _badPermUniq);
+  }
+  /* test permutations on longs */
+  @Test
+  public void testSinglePermInplaceL() {
+    long[] tmp = Arrays.copyOf(_singledataL, _singledataL.length);
+    Permute.inplace(tmp, _singlePerm);
+    assertTrue(Arrays.equals(_singlePermedL, tmp));
+  }
+
+  @Test
+  public void testSmallPermInplaceL() {
+    long[] tmp = Arrays.copyOf(_smalldataL, _smalldataL.length);
+    Permute.inplace(tmp, _smallPerm);
+    assertTrue(Arrays.equals(_smallPermedL, tmp));
+  }
+
+  @Test
+  public void testMedPermInplaceL() {
+    long[] tmp = Arrays.copyOf(_meddataL, _meddataL.length);
+    Permute.inplace(tmp, _medPerm);
+    assertTrue(Arrays.equals(_medPermedL, tmp));
+  }
+
+
+  /* try and break the on float function */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testNullDataF() {
+    Permute.inplace(_nulldataF, _singlePerm);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testNullPermutationF() {
+    Permute.inplace(_singledataF, _nullPerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testShortPermutationF() {
+    Permute.inplace(_smalldataF, _singlePerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testShortVectorF() {
+    Permute.inplace(_singledataF, _smallPerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermNegF() {
+    Permute.inplace(_smalldataF, _badPermNeg);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermPosF() {
+    Permute.inplace(_smalldataF, _badPermPos);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermUniqF() {
+    Permute.inplace(_smalldataF, _badPermUniq);
+  }
+  /* test permutations on floats */
+  @Test
+  public void testSinglePermInplaceF() {
+    float[] tmp = Arrays.copyOf(_singledataF, _singledataF.length);
+    Permute.inplace(tmp, _singlePerm);
+    assertTrue(Arrays.equals(_singlePermedF, tmp));
+  }
+
+  @Test
+  public void testSmallPermInplaceF() {
+    float[] tmp = Arrays.copyOf(_smalldataF, _smalldataF.length);
+    Permute.inplace(tmp, _smallPerm);
+    assertTrue(Arrays.equals(_smallPermedF, tmp));
+  }
+
+  @Test
+  public void testMedPermInplaceF() {
+    float[] tmp = Arrays.copyOf(_meddataF, _meddataF.length);
+    Permute.inplace(tmp, _medPerm);
+    assertTrue(Arrays.equals(_medPermedF, tmp));
+  }
+
+  /* try and break the on doubles function */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testNullDataD() {
+    Permute.inplace(_nulldataD, _singlePerm);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testNullPermutationD() {
+    Permute.inplace(_singledataD, _nullPerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testShortPermutationD() {
+    Permute.inplace(_smalldataD, _singlePerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testShortVectorD() {
+    Permute.inplace(_singledataD, _smallPerm);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermNegD() {
+    Permute.inplace(_smalldataD, _badPermNeg);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermPosD() {
+    Permute.inplace(_smalldataD, _badPermPos);
+  }
+
+  @Test(expectedExceptions = AssertionError.class)
+  public void testbadPermUniqD() {
+    Permute.inplace(_smalldataD, _badPermUniq);
+  }
+  /* test permutations on doubles */
+  @Test
+  public void testSinglePermInplaceD() {
+    double[] tmp = Arrays.copyOf(_singledataD, _singledataD.length);
+    Permute.inplace(tmp, _singlePerm);
+    assertTrue(Arrays.equals(_singlePermedD, tmp));
+  }
+
+  @Test
+  public void testSmallPermInplaceD() {
+    double[] tmp = Arrays.copyOf(_smalldataD, _smalldataD.length);
+    Permute.inplace(tmp, _smallPerm);
+    assertTrue(Arrays.equals(_smallPermedD, tmp));
+  }
+
+  @Test
+  public void testMedPermInplaceD() {
+    double[] tmp = Arrays.copyOf(_meddataD, _meddataD.length);
+    Permute.inplace(tmp, _medPerm);
+    assertTrue(Arrays.equals(_medPermedD, tmp));
+  }
+
 }
