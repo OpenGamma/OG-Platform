7979966eda8037238cc2e5a9230fbb6e0e387e15
Afshin Darian <afshin@opengamma.com>
2011-11-03 12:27:51 +0000

    incremental check-in for sync stuff

--- projects/OG-Web/web-engine/prototype/scripts/og/views/og.views.portfolios_sync.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/og.views.portfolios_sync.js
@@ -5,21 +5,34 @@
 $.register_module({
     name: 'og.views.portfolios_sync',
     dependencies: [
+        'og.api.rest',
         'og.common.util.ui.message',
         'og.views.common.layout',
         'og.common.util.ui.Form'
     ],
     obj: function () {
         var PANEL = '.ui-layout-inner-south', CONTENT = PANEL + ' .ui-layout-content',
-            Form = og.common.util.ui.Form, sync;
+            ui = og.common.util.ui, Form = og.common.util.ui.Form, sync;
         return sync = {
             load: function (args) {
-                var form;
-                sync.setup();
-                sync.clear();
-                og.api.rest.sync.get({trades: args.id, handler: console.log});
-                form = new Form({module: 'og.views.portfolios_sync.main', selector: PANEL});
-                form.dom();
+                og.api.rest.sync.get({
+                    trades: args.id,
+                    handler: function (result) {
+                        var form;
+                        ui.message({location: '.ui-layout-inner-south', destroy: true});
+                        sync.setup();
+                        sync.clear();
+                        form = new Form({module: 'og.views.portfolios_sync.main', selector: PANEL});
+                        form.dom();
+                    },
+                    loading: function () {
+                        ui.message({
+                            location: '.ui-layout-inner-south',
+                            css: {left: 0},
+                            message: {0: 'loading...', 3000: 'still loading...'}
+                        });
+                    }
+                });
             },
             clear: function () {$(CONTENT).empty();},
             setup: function () {
