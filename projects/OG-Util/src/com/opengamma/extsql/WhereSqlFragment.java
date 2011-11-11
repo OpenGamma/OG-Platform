229def9e237ee0ec2c7b928fe716f1971ea83310
Stephen Colebourne <stephen@opengamma.com>
2011-11-11 10:45:39 +0000

    [PLAT-1698] Extsql library

---
+++ projects/OG-Util/src/com/opengamma/extsql/WhereSqlFragment.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.extsql;
+
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+/**
+ * Representation of WHERE.
+ * <p>
+ * This outputs a WHERE clause if at least one child was output.
+ */
+public final class WhereSqlFragment extends ContainerSqlFragment {
+
+  /**
+   * Creates an instance.
+   */
+  public WhereSqlFragment() {
+  }
+
+  //-------------------------------------------------------------------------
+  @Override
+  protected void toSQL(StringBuilder buf, ExtSqlBundle bundle, SqlParameterSource paramSource) {
+    int oldLen = buf.length();
+    buf.append("WHERE ");
+    int newLen = buf.length();
+    super.toSQL(buf, bundle, paramSource);
+    if (buf.length() == newLen) {
+      buf.setLength(oldLen);
+    }
+  }
+
+  //-------------------------------------------------------------------------
+  @Override
+  public String toString() {
+    return getClass().getSimpleName() + " " + getFragments();
+  }
+
+}
