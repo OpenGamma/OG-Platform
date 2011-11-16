af1b8cdce96932498a5816c9bd8ff758e6916ab9
Elaine McLeod <elaine@opengamma.com>
2011-11-16 14:17:49 +0000

    Adding tests for splitting volatility surface data into strips of x-values

--- projects/OG-Core/tests/unit/com/opengamma/core/marketdatasnapshot/VolatilitySurfaceDataTest.java
+++ projects/OG-Core/tests/unit/com/opengamma/core/marketdatasnapshot/VolatilitySurfaceDataTest.java
@@ -5,6 +5,7 @@
  */
 package com.opengamma.core.marketdatasnapshot;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,7 +47,7 @@ public class VolatilitySurfaceDataTest {
     final VolatilitySurfaceData<Double, Double> data = new VolatilitySurfaceData<Double, Double>(name, name, target, xs, ys, values);
     AssertJUnit.assertArrayEquals(xs, data.getXs());
     AssertJUnit.assertArrayEquals(ys, data.getYs());
-    AssertJUnit.assertArrayEquals(xValues, data.getUniqueXValues().toArray(new Double[0]));
+    AssertJUnit.assertArrayEquals(xValues, data.getUniqueXValues().toArray(new Double[xLength]));
     int i = 0;
     for (Double x : data.getUniqueXValues()) {
       List<ObjectsPair<Double, Double>> strips = data.getYValuesForX(x);
@@ -60,4 +61,24 @@ public class VolatilitySurfaceDataTest {
     }
   }
   
+  @Test
+  public void testRagged() {
+    final Double[] xs = new Double[] {1., 1., 1., 2., 2., 3., 3., 3., 4.};
+    final Double[] ys = new Double[] {4., 5., 6., 4., 5., 5., 6., 7., 8.};
+    final double[] vols = new double[] {10, 11, 12, 13, 14, 15, 16, 17, 18};    
+    final Map<Pair<Double, Double>, Double> values = new HashMap<Pair<Double, Double>, Double>();
+    for (int i = 0; i < xs.length; i++) {
+      values.put(DoublesPair.of(xs[i], ys[i]), vols[i]);
+    }
+    final String name = "test";
+    final UniqueIdentifiable target = Currency.USD;
+    final VolatilitySurfaceData<Double, Double> data = new VolatilitySurfaceData<Double, Double>(name, name, target, xs, ys, values);
+    AssertJUnit.assertArrayEquals(xs, data.getXs());
+    AssertJUnit.assertArrayEquals(ys, data.getYs());
+    AssertJUnit.assertArrayEquals(new Double[]{1., 2., 3., 4.}, data.getUniqueXValues().toArray(new Double[3]));
+    AssertJUnit.assertEquals(Arrays.asList(Pair.of(4., 10.), Pair.of(5., 11.), Pair.of(6., 12.)), data.getYValuesForX(1.));
+    AssertJUnit.assertEquals(Arrays.asList(Pair.of(4., 13.), Pair.of(5., 14.)), data.getYValuesForX(2.));
+    AssertJUnit.assertEquals(Arrays.asList(Pair.of(5., 15.), Pair.of(6., 16.), Pair.of(7., 17.)), data.getYValuesForX(3.));
+    AssertJUnit.assertEquals(Arrays.asList(Pair.of(8., 18.)), data.getYValuesForX(4.));
+  }
 }
