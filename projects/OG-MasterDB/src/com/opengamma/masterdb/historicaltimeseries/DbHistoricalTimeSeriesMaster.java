OpenGamma/OG-Platform

commit 4561a2fede3f507a66b26dd23d371525db9653e9

Avoid unnecesary query when empty time series is requested

projects/OG-MasterDB/src/com/opengamma/masterdb/historicaltimeseries/DbHistoricalTimeSeriesMaster.java
@@ -51,9 +51,9 @@ import com.opengamma.master.historicaltimeseries.ManageableHistoricalTimeSeries;
 import com.opengamma.master.historicaltimeseries.ManageableHistoricalTimeSeriesInfo;
 import com.opengamma.masterdb.AbstractDocumentDbMaster;
 import com.opengamma.util.ArgumentChecker;
+import com.opengamma.util.db.DbConnector;
 import com.opengamma.util.db.DbDateUtils;
 import com.opengamma.util.db.DbMapSqlParameterSource;
-import com.opengamma.util.db.DbConnector;
 import com.opengamma.util.paging.Paging;
 import com.opengamma.util.timeseries.localdate.ArrayLocalDateDoubleTimeSeries;
 import com.opengamma.util.timeseries.localdate.LocalDateDoubleTimeSeries;
@@ -611,8 +611,13 @@ public class DbHistoricalTimeSeriesMaster extends AbstractDocumentDbMaster<Histo
     if (result == null) {
       throw new DataNotFoundException("Unable to find time-series: " + objectId);
     }
-    LocalDateDoubleTimeSeries series = namedJdbc.query(sqlSelectDataPoints(), args, new DataPointsExtractor());
-    result.setTimeSeries(series);
+    if (!toDateInclusive.isBefore(fromDateInclusive)) {
+      LocalDateDoubleTimeSeries series = namedJdbc.query(sqlSelectDataPoints(), args, new DataPointsExtractor());
+      result.setTimeSeries(series);
+    } else {
+      //TODO: this is a hack, most of the places that call with this condition want some kind of metadata, which it would be cheaper for us to expose specifically
+      result.setTimeSeries(new ArrayLocalDateDoubleTimeSeries());
+    }
     return result;
   }
 

commit ff7c90e3711e8ccaa59908b938dde64d6dfb1754

Handle infinite time series ranges again (broken in 4561a2fede3f507a66b26dd23d371525db9653e9)

projects/OG-MasterDB/src/com/opengamma/masterdb/historicaltimeseries/DbHistoricalTimeSeriesMaster.java
@@ -611,7 +611,7 @@ public class DbHistoricalTimeSeriesMaster extends AbstractDocumentDbMaster<Histo
     if (result == null) {
       throw new DataNotFoundException("Unable to find time-series: " + objectId);
     }
-    if (!toDateInclusive.isBefore(fromDateInclusive)) {
+    if (toDateInclusive == null || fromDateInclusive == null || !toDateInclusive.isBefore(fromDateInclusive)) {
       LocalDateDoubleTimeSeries series = namedJdbc.query(sqlSelectDataPoints(), args, new DataPointsExtractor());
       result.setTimeSeries(series);
     } else {