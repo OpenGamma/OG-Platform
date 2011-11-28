a3a97c572c84092b7cca212c5de30e641fdb9a53
Alan Ayoub <alan@opengamma.com>
2011-11-28 15:33:36 +0000

    added message for items with no content

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.positions.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.positions.js
@@ -8,15 +8,24 @@ $.register_module({
     obj: function () {
         var common = og.common, views = og.views, routes = common.routes, api = og.api;
         return function (config) {
+            /* timeseries */
+            var timeseries = function (obj) {
+                return; // TODO: PLAT-1772
+                api.rest.timeseries.get({
+                    handler: function (timeseries_data) {
+                        console.log(timeseries_data);
+                    },
+                    id: ''
+                });
+            };
             api.rest.positions.get({
                 handler: function (result) {
-                    console.log(config);
                     if (result.error) return alert(result.message);
                     api.text({module: 'og.views.gadgets.positions', handler: function (template) {
-                        $(config.selector).html($.tmpl(template, $.extend(result.data, {editable: config.editable})));
                         var args = routes.current();
-                        if (!args.version || args.version === '*') {
-                            if (!config.editable) return;
+                        $(config.selector).html($.tmpl(template, $.extend(result.data, {editable: config.editable})));
+                        timeseries(result);
+                        if ((!args.version || args.version === '*') && config.editable) {
                             common.util.ui.content_editable({
                                 attribute: 'data-og-editable',
                                 handler: function () {
