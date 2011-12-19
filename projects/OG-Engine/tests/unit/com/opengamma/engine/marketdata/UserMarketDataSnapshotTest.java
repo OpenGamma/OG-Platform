1cb73152b2f0bfed8298a7f7f8aa473304aaae36
Jonathan Senior <jonathan@opengamma.com>
2011-12-19 12:58:56 +0000

    [PLAT-1776] Testing new logic

--- projects/OG-Engine/tests/unit/com/opengamma/engine/marketdata/UserMarketDataSnapshotTest.java
+++ projects/OG-Engine/tests/unit/com/opengamma/engine/marketdata/UserMarketDataSnapshotTest.java
@@ -5,9 +5,10 @@
  */
 package com.opengamma.engine.marketdata;
 
+import static org.testng.AssertJUnit.assertNull;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.testng.AssertJUnit.assertEquals;
 
 import java.util.Map;
 
@@ -16,6 +17,8 @@ import org.testng.annotations.Test;
 import com.google.common.collect.ImmutableMap;
 import com.opengamma.core.marketdatasnapshot.MarketDataSnapshotSource;
 import com.opengamma.core.marketdatasnapshot.MarketDataValueSpecification;
+import com.opengamma.core.marketdatasnapshot.MarketDataValueType;
+import com.opengamma.core.marketdatasnapshot.SnapshotDataBundle;
 import com.opengamma.core.marketdatasnapshot.StructuredMarketDataSnapshot;
 import com.opengamma.core.marketdatasnapshot.UnstructuredMarketDataSnapshot;
 import com.opengamma.core.marketdatasnapshot.ValueSnapshot;
@@ -26,6 +29,7 @@ import com.opengamma.engine.value.ValuePropertyNames;
 import com.opengamma.engine.value.ValueRequirement;
 import com.opengamma.engine.value.ValueRequirementNames;
 import com.opengamma.id.UniqueId;
+import com.opengamma.livedata.normalization.MarketDataRequirementNames;
 import com.opengamma.util.money.Currency;
 
 /**
@@ -41,11 +45,18 @@ public class UserMarketDataSnapshotTest {
     UniqueId snapshotId = UniqueId.of("TestSnapshot", "1");
     when(snapshotSource.getSnapshot(snapshotId)).thenReturn(snapshot);
     
+    UniqueId testValueId = UniqueId.of("TestScheme", "Value1");
+
+    UnstructuredMarketDataSnapshot globalValues = mock(UnstructuredMarketDataSnapshot.class); 
+    when(globalValues.getValues()).thenReturn(generateGlobalValuesMap(testValueId, 234d));
+    when(snapshot.getGlobalValues()).thenReturn(globalValues);
+    
     UserMarketDataSnapshot userSnapshot = new UserMarketDataSnapshot(snapshotSource, snapshotId);
     userSnapshot.init();
     
     UnstructuredMarketDataSnapshot yieldCurveValues = mock(UnstructuredMarketDataSnapshot.class);
-    when(yieldCurveValues.getValues()).thenReturn(ImmutableMap.<MarketDataValueSpecification, Map<String, ValueSnapshot>>of());
+    
+    when(yieldCurveValues.getValues()).thenReturn(generateGlobalValuesMap(testValueId, 123d));
     YieldCurveSnapshot yieldCurveSnapshot = mock(YieldCurveSnapshot.class);
     when(yieldCurveSnapshot.getValues()).thenReturn(yieldCurveValues);
 
@@ -54,8 +65,21 @@ public class UserMarketDataSnapshotTest {
     
     ValueProperties yieldCurveConstraints = ValueProperties.with(ValuePropertyNames.CURVE, "testCurve").get();
     ValueRequirement yieldCurveRequirement = new ValueRequirement(ValueRequirementNames.YIELD_CURVE_MARKET_DATA, Currency.USD, yieldCurveConstraints);
-    userSnapshot.query(yieldCurveRequirement);
-    verify(yieldCurveSnapshot.getValues());
+    SnapshotDataBundle yieldCurveBundle = (SnapshotDataBundle) userSnapshot.query(yieldCurveRequirement);
+    assertEquals(123d, yieldCurveBundle.getDataPoints().get(testValueId));
+    
+    ValueProperties unsatisfiableYieldCurveConstraints = ValueProperties
+        .with(ValuePropertyNames.CURVE, "testCurve")
+        .with("SomethingElse", "Value").get();
+    ValueRequirement unsatisfiableYieldCurveRequirement = new ValueRequirement(ValueRequirementNames.YIELD_CURVE_MARKET_DATA, Currency.USD, unsatisfiableYieldCurveConstraints);
+    SnapshotDataBundle unsatisfiableYieldCurveBundle = (SnapshotDataBundle) userSnapshot.query(unsatisfiableYieldCurveRequirement);
+    assertNull(unsatisfiableYieldCurveBundle);
   }
 
+  private Map<MarketDataValueSpecification, Map<String, ValueSnapshot>> generateGlobalValuesMap(UniqueId testValueId, Double marketValue) {
+    MarketDataValueSpecification valueSpec = new MarketDataValueSpecification(MarketDataValueType.PRIMITIVE, testValueId);
+    Map<String, ValueSnapshot> values = ImmutableMap.of(MarketDataRequirementNames.MARKET_VALUE, new ValueSnapshot(marketValue));
+    return ImmutableMap.<MarketDataValueSpecification, Map<String, ValueSnapshot>>of(valueSpec, values);
+  }
+  
 }
