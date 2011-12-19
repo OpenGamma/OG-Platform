3799c397b0ac82429d827c1b0112a5041724ca1f
Chris Kent <chris@opengamma.com>
2011-12-19 11:15:24 +0000

    Fixed package-info Javadoc

--- projects/OG-Web/src/com/opengamma/web/server/push/package-info.java
+++ projects/OG-Web/src/com/opengamma/web/server/push/package-info.java
@@ -1,8 +1,3 @@
-/**
- * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
- *
- * Please see distribution for license.
- */
 package com.opengamma.web.server.push;
 
 /**
