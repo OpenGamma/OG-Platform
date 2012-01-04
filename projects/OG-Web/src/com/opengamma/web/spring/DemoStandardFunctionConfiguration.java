0d9189ce0c5db2964bebce78914e552ed10edc5e
Jim Moores <jim.moores@gmail.com>
2012-01-04 14:04:17 +0000

    Commented out duplicate summing function

--- projects/OG-Web/src/com/opengamma/web/spring/DemoStandardFunctionConfiguration.java
+++ projects/OG-Web/src/com/opengamma/web/spring/DemoStandardFunctionConfiguration.java
@@ -447,7 +447,7 @@ public class DemoStandardFunctionConfiguration extends SingletonFactoryBean<Repo
     addSummingFunction(functionConfigs, ValueRequirementNames.FX_VOLATILITY_SENSITIVITIES);
 
     addScalingFunction(functionConfigs, ValueRequirementNames.VALUE_DELTA);
-    addSummingFunction(functionConfigs, ValueRequirementNames.VALUE_DELTA);
+    //addSummingFunction(functionConfigs, ValueRequirementNames.VALUE_DELTA);
     addScalingFunction(functionConfigs, ValueRequirementNames.VALUE_RHO);
     addSummingFunction(functionConfigs, ValueRequirementNames.VALUE_RHO);
 
