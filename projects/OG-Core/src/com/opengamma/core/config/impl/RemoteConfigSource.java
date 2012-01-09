9a5bcdc1acb89b395fb58697940348025ee18632
Stephen Colebourne <stephen@opengamma.com>
2012-01-09 14:25:20 +0000

    [PLAT-1658] Add RESTful config source

---
+++ projects/OG-Core/src/com/opengamma/core/config/impl/RemoteConfigSource.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.core.config.impl;
+
+import java.net.URI;
+import java.util.Collection;
+
+import javax.time.Instant;
+
+import com.opengamma.core.config.ConfigSource;
+import com.opengamma.id.ObjectId;
+import com.opengamma.id.UniqueId;
+import com.opengamma.id.VersionCorrection;
+import com.opengamma.util.ArgumentChecker;
+import com.opengamma.util.fudgemsg.FudgeListWrapper;
+import com.opengamma.util.rest.AbstractRemoteClient;
+
+/**
+ * Provides remote access to a {@link ConfigSource}.
+ */
+public class RemoteConfigSource extends AbstractRemoteClient implements ConfigSource {
+
+  /**
+   * Creates an instance.
+   * 
+   * @param baseUri  the base target URI for all RESTful web services, not null
+   */
+  public RemoteConfigSource(final URI baseUri) {
+    super(baseUri);
+  }
+
+  //-------------------------------------------------------------------------
+  @Override
+  public <T> T getConfig(Class<T> clazz, UniqueId uniqueId) {
+    ArgumentChecker.notNull(clazz, "clazz");
+    ArgumentChecker.notNull(uniqueId, "uniqueId");
+    
+    URI uri = DataConfigSourceResource.uriGet(getBaseUri(), uniqueId, clazz);
+    return accessRemote(uri).get(clazz);
+  }
+
+  @Override
+  public <T> T getConfig(Class<T> clazz, ObjectId objectId, VersionCorrection versionCorrection) {
+    ArgumentChecker.notNull(clazz, "clazz");
+    ArgumentChecker.notNull(objectId, "objectId");
+    ArgumentChecker.notNull(versionCorrection, "versionCorrection");
+    
+    URI uri = DataConfigSourceResource.uriGet(getBaseUri(), objectId, versionCorrection, clazz);
+    return accessRemote(uri).get(clazz);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <T> Collection<? extends T> getConfigs(Class<T> clazz, String configName, VersionCorrection versionCorrection) {
+    ArgumentChecker.notNull(clazz, "clazz");
+    ArgumentChecker.notNull(configName, "configName");
+    ArgumentChecker.notNull(versionCorrection, "versionCorrection");
+    
+    URI uri = DataConfigSourceResource.uriSearch(getBaseUri(), versionCorrection, configName, clazz);
+    return accessRemote(uri).get(FudgeListWrapper.class).getList();
+  }
+
+  //-------------------------------------------------------------------------
+  @Override
+  public <T> T getLatestByName(Class<T> clazz, String name) {
+    ArgumentChecker.notNull(clazz, "clazz");
+    ArgumentChecker.notNull(name, "name");
+    
+    URI uri = DataConfigSourceResource.uriSearchSingle(getBaseUri(), name, null, clazz);
+    return accessRemote(uri).get(clazz);
+  }
+
+  @Override
+  public <T> T getByName(Class<T> clazz, String name, Instant versionAsOf) {
+    ArgumentChecker.notNull(clazz, "clazz");
+    ArgumentChecker.notNull(name, "name");
+    
+    URI uri = DataConfigSourceResource.uriSearchSingle(getBaseUri(), name, versionAsOf, clazz);
+    return accessRemote(uri).get(clazz);
+  }
+
+}
