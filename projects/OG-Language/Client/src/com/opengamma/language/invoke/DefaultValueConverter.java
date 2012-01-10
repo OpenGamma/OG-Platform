9fdafaffa9119d8b1b601ce12b7001af39b67e69
Jonathan Senior <jonathan@opengamma.com>
2012-01-10 12:26:40 +0000

    Revert earlier change; doesn't seem to be that simple as this causes some converter sub-chains to loop

--- projects/OG-Language/Client/src/com/opengamma/language/invoke/DefaultValueConverter.java
+++ projects/OG-Language/Client/src/com/opengamma/language/invoke/DefaultValueConverter.java
@@ -71,6 +71,17 @@ public class DefaultValueConverter extends ValueConverter {
     }
 
     // TODO: build a set to speed up the visit lookups
+    
+    public boolean visited(final TypeConverter converter) {
+      State s = this;
+      do {
+        if (converter == s._nextStateConverter) {
+          return true;
+        }
+        s = s._nextState;
+      } while (s != null);
+      return false;
+    }
 
     public boolean visited(final JavaTypeInfo<?> type) {
       State s = this;
@@ -264,24 +275,26 @@ public class DefaultValueConverter extends ValueConverter {
     do {
       consideredConverters.clear();
       for (TypeConverter converter : getConvertersTo(conversionContext, explore.getTargetType())) {
-        final Map<JavaTypeInfo<?>, Integer> alternativeTypes = converter.getConversionsTo(explore.getTargetType());
-        if ((alternativeTypes != null) && !alternativeTypes.isEmpty()) {
-          for (Map.Entry<JavaTypeInfo<?>, Integer> alternativeType : alternativeTypes.entrySet()) {
-            // Only try types not already used on the chain, and only use the first converter (most recently added) for a type
-            if (!explore.visited(alternativeType.getKey()) && consideredConverters.add(alternativeType.getKey())) {
-              final State nextState = new State(alternativeType.getKey(), converter, explore, alternativeType.getValue());
-              final Integer key = (Integer) nextState.getCost();
-              if (key > 100) {
-                // Ignore expensive chains 
-                continue;
+        if (!explore.visited(converter)) {
+          final Map<JavaTypeInfo<?>, Integer> alternativeTypes = converter.getConversionsTo(explore.getTargetType());
+          if ((alternativeTypes != null) && !alternativeTypes.isEmpty()) {
+            for (Map.Entry<JavaTypeInfo<?>, Integer> alternativeType : alternativeTypes.entrySet()) {
+              // Only try types not already used on the chain, and only use the first converter (most recently added) for a type
+              if (!explore.visited(alternativeType.getKey()) && consideredConverters.add(alternativeType.getKey())) {
+                final State nextState = new State(alternativeType.getKey(), converter, explore, alternativeType.getValue());
+                final Integer key = (Integer) nextState.getCost();
+                if (key > 100) {
+                  // Ignore expensive chains 
+                  continue;
+                }
+                Queue<State> states = searchStates.get(key);
+                if (states == null) {
+                  states = new LinkedList<State>();
+                  searchStates.put(key, states);
+                }
+                states.add(nextState);
+                statesStored++;
               }
-              Queue<State> states = searchStates.get(key);
-              if (states == null) {
-                states = new LinkedList<State>();
-                searchStates.put(key, states);
-              }
-              states.add(nextState);
-              statesStored++;
             }
           }
         }
