f01fe10811ae51f238e832d5eac0b0eea9cb3665
Alan Ayoub <alan@opengamma.com>
2012-01-17 10:23:19 +0000

    changed custom hander on search to use slickgrid click handler

--- projects/OG-Web/web-engine/prototype/scripts/og/common/search_results/og.common.search_results.core.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/search_results/og.common.search_results.core.js
@@ -25,6 +25,7 @@ $.register_module({
                         buffer: 17
                     });
                     grid = new Slick.Grid(obj.selector, slick_manager.data, obj.columns, options);
+                    grid.setSelectionModel(new Slick.RowSelectionModel());
                     window.onresize = function () {
                         setTimeout(function () {
                             grid.resizeCanvas();
@@ -34,15 +35,15 @@ $.register_module({
                     filters_obj = obj.url;
                     // Setup filter inputs
                     og.common.search.filter({location: obj.selector});
-                    // Handle click
-                    $(obj.selector).undelegate().delegate('[row]', 'click', function (e) {
+
+                    grid.onClick.subscribe(function (e, dd) {
                         var current = routes.current().args, args = obj.url,
                             params = {
-                                id: slick_manager.data[$(e.currentTarget).attr('row')].id,
+                                id: slick_manager.data[dd.row].id,
                                 name: current.name || '',
                                 quantity: current.quantity || '',
                                 type: current.type || '',
-                                filter: slick_manager.data[$(e.currentTarget).attr('row')].filter,
+                                filter: slick_manager.data[dd.row].filter,
                                 version: '',
                                 sync: ''
                             };
