43673f1fd3159a006d643f3d24cd4dd3afd269a8
Jonathan Senior <jonathan@opengamma.com>
2012-01-17 14:26:15 +0000

    [XLS-400] Factoring out value requirement string parsing; adding first cut of function to pull a primitive value from a ViewComputationResultModel

--- projects/OG-Language/Client/src/com/opengamma/language/view/ViewDefinitionFunction.java
+++ projects/OG-Language/Client/src/com/opengamma/language/view/ViewDefinitionFunction.java
@@ -7,17 +7,10 @@
 package com.opengamma.language.view;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.apache.commons.lang.StringUtils;
-
-import com.opengamma.OpenGammaRuntimeException;
 import com.opengamma.core.position.impl.PortfolioMapper;
 import com.opengamma.core.position.impl.SecurityTypeMapperFunction;
 import com.opengamma.engine.value.ValueProperties;
@@ -35,9 +28,7 @@ import com.opengamma.language.function.AbstractFunctionInvoker;
 import com.opengamma.language.function.MetaFunction;
 import com.opengamma.language.function.PublishedFunction;
 import com.opengamma.language.position.PortfolioUtils;
-import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.tuple.Pair;
-import com.opengamma.util.tuple.Triple;
 
 /**
  * Creates a view definition
@@ -49,8 +40,6 @@ public class ViewDefinitionFunction extends AbstractFunctionInvoker implements P
    */
   public static final ViewDefinitionFunction INSTANCE = new ViewDefinitionFunction();
 
-  private static final Pattern REQUIREMENT_EXPRESSION = Pattern.compile("^(?:(.+)/)?(.+?)(\\[(.+)\\])?$");
-  private static final String DEFAULT_CONFIG_NAME = "Default";
   private static final long DEFAULT_CALCULATION_TIME = 1000L;
 
   private long _defaultMinDelta = DEFAULT_CALCULATION_TIME;
@@ -112,55 +101,8 @@ public class ViewDefinitionFunction extends AbstractFunctionInvoker implements P
     return _defaultMaxFull;
   }
 
-  /**
-   * Parses the string descriptions of the portfolio requirements into sets of value requirements (string/constraint pairs)
-   * for each calculation configuration referenced.
-   * 
-   * @param portfolioRequirements the user supplied requirements
-   * @return a map of configuration to value requirement sets
-   */
-  protected static Map<String, Set<Pair<String, ValueProperties>>> parseRequirements(final String[] portfolioRequirements) {
-    final Map<String, Set<Pair<String, ValueProperties>>> results = new HashMap<String, Set<Pair<String, ValueProperties>>>();
-    for (String requirement : portfolioRequirements) {
-      if (StringUtils.isBlank(requirement)) {
-        continue;
-      }
-      Triple<String, String, ValueProperties> parsedRequirement = parseRequirement(requirement);
-      Set<Pair<String, ValueProperties>> configRequirements = results.get(parsedRequirement.getFirst());
-      if (configRequirements == null) {
-        configRequirements = new HashSet<Pair<String, ValueProperties>>();
-        results.put(parsedRequirement.getFirst(), configRequirements);
-      }
-      configRequirements.add(Pair.of(parsedRequirement.getSecond(), parsedRequirement.getThird()));
-    }
-    return results;
-  }
-
-  /**
-   * Parses ValueRequirement strings of the form: CalcConfig/ValueName[Constraint1=[Value1,Value2],Constraint2=Value3]
-   * 
-   * @param requirement the requirement string, not null
-   * @return the (configuration, value name, value properties) triple, not null
-   */
-  protected static Triple<String, String, ValueProperties> parseRequirement(String requirement) {
-    ArgumentChecker.notNull(requirement, "requirement");
-    Matcher matcher = REQUIREMENT_EXPRESSION.matcher(requirement);
-    if (!matcher.matches()) {
-      throw new OpenGammaRuntimeException("Invalid requirement syntax: " + requirement);
-    }
-    String calcConfigName = matcher.group(1);
-    if (StringUtils.isBlank(calcConfigName)) {
-      calcConfigName = DEFAULT_CONFIG_NAME;
-    }
-    String requirementName = matcher.group(2);
-    ValueProperties constraints;
-    String constraintsString = matcher.group(4);
-    constraints = ValueProperties.parse(constraintsString);
-    return Triple.of(calcConfigName, requirementName, constraints);
-  }
-
   public ViewDefinition invoke(final SessionContext context, final String viewName, final UniqueId portfolio, final String[] requirements) {
-    final Map<String, Set<Pair<String, ValueProperties>>> portfolioRequirementsByConfiguration = parseRequirements(requirements);
+    final Map<String, Set<Pair<String, ValueProperties>>> portfolioRequirementsByConfiguration = ValueRequirementUtils.parseRequirements(requirements);
     ResultModelDefinition resultModelDefinition = new ResultModelDefinition(ResultOutputMode.TERMINAL_OUTPUTS);
     final ViewDefinition definition = new ViewDefinition(viewName, portfolio, context.getUserContext().getLiveDataUser(), resultModelDefinition);
     definition.setMinDeltaCalculationPeriod(getDefaultMinDelta());
