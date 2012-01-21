6f879965afde14504c323e3e1d53862474eb4eeb
Jonathan Senior <jonathan@opengamma.com>
2012-01-21 12:10:26 -0500

    [PLAT-1896] Adding ValueRenamingFunction which can change the value name of a satisfiable input; adding subclasses for renaming PV and FV to V for PortfolioNodes, Positions and Securities.

---
+++ projects/OG-Financial/src/com/opengamma/financial/value/PositionValueFunction.java
@@ -0,0 +1,11 @@
+package com.opengamma.financial.value;
+
+import com.opengamma.engine.ComputationTargetType;
+
+public class PositionValueFunction extends ValueFunction {
+
+  public PositionValueFunction() {
+    super(ComputationTargetType.POSITION);
+  }
+
+}
