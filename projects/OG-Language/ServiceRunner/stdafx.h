60ac57e9f092377d1f3c86bf493b60e0164cb14a
Andrew Griffin <andrew@opengamma.com>
2012-01-24 15:49:18 +0000

    Configuration dialog support for Windows installation. Run "ServiceRunner configure" to bring up the swing dialog box for selecting a backend installation.

--- projects/OG-Language/ServiceRunner/stdafx.h
+++ projects/OG-Language/ServiceRunner/stdafx.h
@@ -10,6 +10,7 @@
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <shellapi.h>
 #ifdef __cplusplus
 #pragma warning(disable:4995) /* suppress #pragma deprecated warnings from standard C++ headers */
 #endif /* ifdef __cplusplus */
