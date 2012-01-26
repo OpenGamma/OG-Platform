8abcff392cb02547366c53be2e33b0c3f4a06a26
Jonathan Senior <jonathan@opengamma.com>
2012-01-26 09:52:12 -0500

    Skip invalid external functions in the cache rather than quietly giving up

--- projects/OG-Language/Client/src/com/opengamma/language/external/ExternalFunctionProvider.java
+++ projects/OG-Language/Client/src/com/opengamma/language/external/ExternalFunctionProvider.java
@@ -51,14 +51,9 @@ public class ExternalFunctionProvider extends AbstractFunctionProvider {
 
   private static List<MetaFunction> createFunctions(final ExternalFunctionCache cache) {
     final List<MetaFunction> functions = new ArrayList<MetaFunction>();
-    try {
-      for (Class<?> clazz : cache.getClasses()) {
-        final ExternalFunctionHandler handler = new ExternalFunctionHandler(clazz);
-        functions.addAll(handler.getFunctions());
-      }
-    } catch (ClassNotFoundException ex) {
-      s_logger.info("Class not found", ex);
-      return null;
+    for (Class<?> clazz : cache.getClasses()) {
+      final ExternalFunctionHandler handler = new ExternalFunctionHandler(clazz);
+      functions.addAll(handler.getFunctions());
     }
     return functions;
   }
