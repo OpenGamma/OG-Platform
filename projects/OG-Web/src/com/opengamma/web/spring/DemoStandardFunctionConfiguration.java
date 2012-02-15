03494aaa5c9a8cd2e463bd2f05695d617c2cd459
Elaine McLeod <elaine@opengamma.com>
2012-02-15 14:40:11 +0000

    Adding an EUR IR future option surface definition to (hopefully) allow the dependency graph to build for a portfolio with EUR IR future options

--- projects/OG-Web/src/com/opengamma/web/spring/DemoStandardFunctionConfiguration.java
+++ projects/OG-Web/src/com/opengamma/web/spring/DemoStandardFunctionConfiguration.java
@@ -678,12 +678,12 @@ public class DemoStandardFunctionConfiguration extends SingletonFactoryBean<Repo
     functionConfigs.add(functionConfiguration(InterestRateFutureOptionSABRSensitivitiesFunction.class, ValueRequirementNames.PRESENT_VALUE_SABR_NU_SENSITIVITY));
     functionConfigs.add(functionConfiguration(InterestRateFutureOptionSABRSensitivitiesFunction.class, ValueRequirementNames.PRESENT_VALUE_SABR_RHO_SENSITIVITY));
     functionConfigs.add(functionConfiguration(InterestRateFutureOptionVegaFunction.class));
-    functionConfigs.add(functionConfiguration(InterestRateFutureOptionDefaultValuesFunction.class, "FORWARD_3M", "FUNDING", "DEFAULT",
-        ValueRequirementNames.PRESENT_VALUE,
-        ValueRequirementNames.PRESENT_VALUE_SABR_ALPHA_SENSITIVITY,
-        ValueRequirementNames.PRESENT_VALUE_SABR_NU_SENSITIVITY,
-        ValueRequirementNames.PRESENT_VALUE_SABR_RHO_SENSITIVITY,
-        ValueRequirementNames.VEGA_QUOTE_MATRIX));
+//    functionConfigs.add(functionConfiguration(InterestRateFutureOptionDefaultValuesFunction.class, "FORWARD_3M", "FUNDING", "DEFAULT",
+//        ValueRequirementNames.PRESENT_VALUE,
+//        ValueRequirementNames.PRESENT_VALUE_SABR_ALPHA_SENSITIVITY,
+//        ValueRequirementNames.PRESENT_VALUE_SABR_NU_SENSITIVITY,
+//        ValueRequirementNames.PRESENT_VALUE_SABR_RHO_SENSITIVITY,
+//        ValueRequirementNames.VEGA_QUOTE_MATRIX));
     functionConfigs.add(functionConfiguration(InterestRateFutureOptionDefaultValuesFunction.class, "FORWARD_6M", "FUNDING", "DEFAULT",
         ValueRequirementNames.PRESENT_VALUE,
         ValueRequirementNames.PRESENT_VALUE_SABR_ALPHA_SENSITIVITY,
