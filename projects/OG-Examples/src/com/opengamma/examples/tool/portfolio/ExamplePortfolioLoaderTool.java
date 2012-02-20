f7410151260731f6bc9ce7041c62977834d84844
Kevin Vella <kevin@opengamma.com>
2012-02-20 14:26:54 +0000

    Added harness for portfolio loader tool in OG-Examples

---
+++ projects/OG-Examples/src/com/opengamma/examples/tool/portfolio/ExamplePortfolioLoaderTool.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.examples.tool.portfolio;
+
+import com.opengamma.examples.tool.AbstractTool;
+import com.opengamma.financial.loader.LoaderContext;
+import com.opengamma.financial.loader.PortfolioLoaderTool;
+
+/**
+ * Wrapper to expose the Examples classpath to the standard portfolio loader tool
+ */
+public class ExamplePortfolioLoaderTool extends AbstractTool {
+
+  private static String[] s_args;
+
+  //-------------------------------------------------------------------------
+  /**
+   * Main method to run the tool.
+   * No arguments are needed.
+   * 
+   * @param args  the arguments, unused
+   */
+  public static void main(String[] args) {  // CSIGNORE
+    if (init()) {
+      s_args = args;
+      new ExamplePortfolioLoaderTool().run();
+    }
+    System.exit(0);
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Loads the test portfolio into the position master.
+   */
+  @Override 
+  protected void doRun() {
+    LoaderContext context = new LoaderContext();
+    context.setConfigMaster(getToolContext().getConfigMaster());
+    context.setConventionBundleSource(getToolContext().getConventionBundleSource());
+    context.setExchangeSource(getToolContext().getExchangeSource());
+    context.setHistoricalTimeSeriesMaster(getToolContext().getHistoricalTimeSeriesMaster());
+    context.setHistoricalTimeSeriesSource(getToolContext().getHistoricalTimeSeriesSource());
+    context.setHolidaySource(getToolContext().getHolidaySource());
+    context.setPortfolioMaster(getToolContext().getPortfolioMaster());
+    context.setPositionMaster(getToolContext().getPositionMaster());
+    context.setSecurityLoader(getToolContext().getSecurityLoader());
+    context.setSecurityMaster(getToolContext().getSecurityMaster());
+    context.setSecuritySource(getToolContext().getSecuritySource());
+    new PortfolioLoaderTool().run(s_args, context);
+  }
+
+}
