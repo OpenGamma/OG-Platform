4d8c9fd14126126a2336c3c7748a1744eda6f2e3
Stephen Colebourne <stephen@opengamma.com>
2012-02-29 12:01:58 +0000

    Checkstyle

--- projects/OG-Financial/tests/unit/com/opengamma/batch/rest/DataBatchMasterResourceTest.java
+++ projects/OG-Financial/tests/unit/com/opengamma/batch/rest/DataBatchMasterResourceTest.java
@@ -5,8 +5,26 @@
  */
 package com.opengamma.batch.rest;
 
-import com.opengamma.DataNotFoundException;
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
+import java.util.Collections;
+import java.util.List;
+
+import javax.time.Instant;
+import javax.ws.rs.core.Response;
+
+import org.mockito.Mock;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.opengamma.DataNotFoundException;
 import com.opengamma.batch.BatchMaster;
 import com.opengamma.batch.BatchMasterWriter;
 import com.opengamma.batch.domain.CalculationConfiguration;
@@ -16,36 +34,21 @@ import com.opengamma.batch.domain.RiskRunProperty;
 import com.opengamma.id.ObjectId;
 import com.opengamma.id.UniqueId;
 import com.opengamma.id.VersionCorrection;
+import com.opengamma.transport.jaxrs.FudgeResponse;
 import com.opengamma.util.paging.Paging;
 import com.opengamma.util.paging.PagingRequest;
 import com.opengamma.util.test.AbstractFudgeBuilderTestCase;
 import com.opengamma.util.tuple.Pair;
-import org.mockito.Mock;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
-import javax.time.Instant;
-import javax.ws.rs.core.Response;
-
-import java.util.Collections;
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static com.google.common.collect.Sets.newHashSet;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
-import static org.testng.Assert.*;
-
+/**
+ * Test.
+ */
 public class DataBatchMasterResourceTest extends AbstractFudgeBuilderTestCase {
 
   @Mock
   private BatchMasterWriter batchMaster;
 
   private DataBatchMasterResource batchMasterResource;
-
-  //private BatchDocument batchDocument = mock(BatchDocument.class);
 
   private RiskRun riskRun = new RiskRun(       
     new MarketData(UniqueId.of(BatchMaster.BATCH_IDENTIFIER_SCHEME, "market-data")),
@@ -61,21 +64,21 @@ public class DataBatchMasterResourceTest extends AbstractFudgeBuilderTestCase {
 
   @BeforeMethod
   public void setUp() throws Exception {
-
     Pair<List<RiskRun>, Paging> batchSearchResult = Pair.<List<RiskRun>, Paging>of(newArrayList(riskRun), Paging.ofAll(Collections.emptyList()));
-
+    
     initMocks(this);
     batchMasterResource = new DataBatchMasterResource(batchMaster);
     when(batchMaster.searchRiskRun((BatchRunSearchRequest) any())).thenReturn(batchSearchResult);
     when(batchMaster.getRiskRun((ObjectId) any())).thenReturn(riskRun);
   }
 
+  @SuppressWarnings("unchecked")
   @Test
   public void testSearch() throws Exception {
     BatchRunSearchRequest batchRunSearchRequest = new BatchRunSearchRequest();
-
+    
     Pair<List<RiskRun>, Paging> result = (Pair<List<RiskRun>, Paging>) batchMasterResource.searchBatchRuns(batchRunSearchRequest).getEntity();
-
+    
     assertTrue(result.getFirst().size() > 0);
     RiskRun run = result.getFirst().get(0);
     assertEquals(run, riskRun);
@@ -83,56 +86,44 @@ public class DataBatchMasterResourceTest extends AbstractFudgeBuilderTestCase {
 
   @Test
   public void testBatchRun() throws Exception {
-
     String batchUid = "Scheme~MockUniqueId";
-
     DataBatchRunResource batchRunResource = batchMasterResource.batchRuns(batchUid);
-
+    
     batchRunResource.deleteBatchRun();    
-
+    
     Response response = batchRunResource.get();
-
     assertEquals(response.getEntity(), riskRun);
-
   }
 
+  @SuppressWarnings("unchecked")
   @Test(expectedExceptions = com.opengamma.DataNotFoundException.class)
   public void testBatchRunDataNotFound() throws Exception {
     BatchMasterWriter batchMaster = mock(BatchMasterWriter.class);
     when(batchMaster.getRiskRun((ObjectId) any())).thenThrow(DataNotFoundException.class);
     
     DataBatchMasterResource batchMasterResource = new DataBatchMasterResource(batchMaster);    
-
-    String batchUid = "Scheme~MockUniqueId";
-
-    DataBatchRunResource batchRunResource = batchMasterResource.batchRuns(batchUid);
-
-    Response response = batchRunResource.get();
     
-    fail("Execution shouldn't reach here");
+    String batchUid = "Scheme~MockUniqueId";
+    DataBatchRunResource batchRunResource = batchMasterResource.batchRuns(batchUid);
+    
+    batchRunResource.get();
   }
-  
+
   @Test
   public void testSnapshots() throws Exception {
-
     ObjectId snapshotId = riskRun.getMarketData().getObjectId();
     
     when(batchMaster.getMarketDataById((ObjectId) any())).thenReturn(riskRun.getMarketData());
-
+    
     DataMarketDataResource marketDataResource = batchMasterResource.getMarketData(snapshotId.toString());
     
     MarketData marketData = (MarketData) marketDataResource.get().getEntity();
-
     assertEquals(marketData.getObjectId(), snapshotId);   
-
   }
   
   @SuppressWarnings("unchecked")
   @Test
   public void testSearchSnapshots() throws Exception {
-
-    ObjectId snapshotId = riskRun.getMarketData().getObjectId();
-    
     PagingRequest pagingRequest = PagingRequest.FIRST_PAGE; 
     
     List<MarketData> marketDataList = newArrayList(riskRun.getMarketData());
@@ -145,5 +136,5 @@ public class DataBatchMasterResourceTest extends AbstractFudgeBuilderTestCase {
     assertEquals(response.getFirst().size(), 1);   
     assertEquals(response.getSecond(), paging); 
   }
-  
+
 }
--- projects/OG-Financial/tests/unit/com/opengamma/batch/rest/DataBatchRunResourceTest.java
+++ projects/OG-Financial/tests/unit/com/opengamma/batch/rest/DataBatchRunResourceTest.java
@@ -6,6 +6,23 @@
 package com.opengamma.batch.rest;
 
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertSame;
+
+import java.util.List;
+
+import javax.time.Instant;
+import javax.ws.rs.core.Response;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import com.opengamma.batch.BatchMaster;
 import com.opengamma.batch.domain.CalculationConfiguration;
 import com.opengamma.batch.domain.MarketData;
@@ -15,22 +32,11 @@ import com.opengamma.engine.view.ViewResultEntry;
 import com.opengamma.id.ObjectId;
 import com.opengamma.id.UniqueId;
 import com.opengamma.id.VersionCorrection;
+import com.opengamma.transport.jaxrs.FudgeResponse;
 import com.opengamma.util.paging.Paging;
 import com.opengamma.util.paging.PagingRequest;
 import com.opengamma.util.tuple.Pair;
 import com.sun.jersey.api.client.ClientResponse.Status;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import javax.time.Instant;
-import javax.ws.rs.core.Response;
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static com.google.common.collect.Sets.newHashSet;
-import static org.mockito.Mockito.*;
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertSame;
 
 /**
  * Tests BatchRunResource.
@@ -55,7 +61,7 @@ public class DataBatchRunResourceTest {
       VersionCorrection.LATEST,
       UniqueId.of("Scheme", "view-def")
     );
-
+    
     _underlying = mock(BatchMaster.class);
     _resource = new DataBatchRunResource(_riskRunId, _underlying);
     when(_underlying.getRiskRun(_riskRunId)).thenReturn(_riskRun);
@@ -81,18 +87,16 @@ public class DataBatchRunResourceTest {
   @Test
   public void testGetBatchValues() {
     PagingRequest pagingRequest = PagingRequest.FIRST_PAGE;
-
-
     ViewResultEntry mockViewResultEntry = mock(ViewResultEntry.class);
-
+    
     List<ViewResultEntry> viewResultEntries = newArrayList(mockViewResultEntry);
     Paging paging = Paging.of(pagingRequest, viewResultEntries);
-
+    
     when(_underlying.getBatchValues(_riskRunId, pagingRequest)).thenReturn(Pair.of(viewResultEntries, paging));
     Response response = _resource.getBatchValues(pagingRequest);
-
+    
     Pair<List<ViewResultEntry>, Paging> result = (Pair<List<ViewResultEntry>, Paging>) response.getEntity();
-
+    
     assertEquals(Status.OK.getStatusCode(), response.getStatus());
     assertSame(result.getFirst().size(), 1);
     assertSame(result.getFirst().get(0), mockViewResultEntry);
