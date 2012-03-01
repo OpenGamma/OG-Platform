8538d17a0733a238a21cbf207b298885f250fef1
Stephen Colebourne <stephen@opengamma.com>
2012-03-01 17:49:09 +0000

    [PLAT-1658] Move component dependent code to OG component

--- projects/OG-Component/src/com/opengamma/component/spring/RemoteMastersBeanPostProcessor.java
+++ projects/OG-Component/src/com/opengamma/component/spring/RemoteMastersBeanPostProcessor.java
@@ -3,7 +3,7 @@
  *
  * Please see distribution for license.
  */
-package com.opengamma.financial.spring;
+package com.opengamma.component.spring;
 
 import java.lang.reflect.Constructor;
 import java.net.URI;
