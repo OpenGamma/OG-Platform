bf9072950b00c262adac59a7ae87e4d0fe1a976f
Elaine McLeod <elaine@opengamma.com>
2012-03-14 12:47:35 +0000

    PV01 now calculates for IR futures

--- projects/OG-Financial/src/com/opengamma/financial/analytics/model/future/InterestRateFuturePV01Function.java
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/model/future/InterestRateFuturePV01Function.java
@@ -1,9 +1,9 @@
 /**
- * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
  * 
  * Please see distribution for license.
  */
-package com.opengamma.financial.analytics.model.fixedincome;
+package com.opengamma.financial.analytics.model.future;
 
 import java.util.Collections;
 import java.util.Map;
@@ -21,20 +21,21 @@ import com.opengamma.financial.interestrate.YieldCurveBundle;
 /**
  * 
  */
-public class InterestRateInstrumentPV01Function extends InterestRateInstrumentCurveSpecificFunction {
+public class InterestRateFuturePV01Function extends InterestRateFutureCurveSpecificFunction {
   private static final PV01Calculator CALCULATOR = PV01Calculator.getInstance();
 
-  public InterestRateInstrumentPV01Function() {
+  public InterestRateFuturePV01Function() {
     super(ValueRequirementNames.PV01);
   }
 
   @Override
-  public Set<ComputedValue> getResults(final InstrumentDerivative derivative, final String curveName, final InterpolatedYieldCurveSpecificationWithSecurities curveSpec,
+  protected Set<ComputedValue> getResults(final InstrumentDerivative irFuture, final String curveName, final InterpolatedYieldCurveSpecificationWithSecurities curveSpec,
       final YieldCurveBundle curves, final ValueSpecification resultSpec) {
-    final Map<String, Double> pv01 = CALCULATOR.visit(derivative, curves);
+    final Map<String, Double> pv01 = CALCULATOR.visit(irFuture, curves);
     if (!pv01.containsKey(curveName)) {
       throw new OpenGammaRuntimeException("Could not get PV01 for curve named " + curveName + "; should never happen");
     }
     return Collections.singleton(new ComputedValue(resultSpec, pv01.get(curveName)));
   }
+
 }
--- projects/OG-Financial/src/com/opengamma/financial/analytics/model/future/InterestRateFuturePresentValueFunction.java
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/model/future/InterestRateFuturePresentValueFunction.java
@@ -6,23 +6,13 @@
 package com.opengamma.financial.analytics.model.future;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
-import com.google.common.collect.Sets;
-import com.opengamma.engine.ComputationTarget;
-import com.opengamma.engine.function.FunctionCompilationContext;
-import com.opengamma.engine.function.FunctionInputs;
 import com.opengamma.engine.value.ComputedValue;
-import com.opengamma.engine.value.ValuePropertyNames;
-import com.opengamma.engine.value.ValueRequirement;
-import com.opengamma.engine.value.ValueRequirementNames;
 import com.opengamma.engine.value.ValueSpecification;
-import com.opengamma.financial.analytics.ircurve.YieldCurveFunction;
 import com.opengamma.financial.interestrate.InstrumentDerivative;
 import com.opengamma.financial.interestrate.PresentValueCalculator;
 import com.opengamma.financial.interestrate.YieldCurveBundle;
-import com.opengamma.financial.security.FinancialSecurityUtils;
 
 /**
  * Present value function for interest rate futures.
@@ -31,60 +21,8 @@ public class InterestRateFuturePresentValueFunction extends InterestRateFutureFu
   private static final PresentValueCalculator CALCULATOR = PresentValueCalculator.getInstance();
 
   @Override
-  protected Set<ComputedValue> getResults(final InstrumentDerivative irFuture, final YieldCurveBundle data, final ComputationTarget target, final FunctionInputs inputs, final String forwardCurveName,
-      final String fundingCurveName, final String curveCalculationMethod) {
+  protected Set<ComputedValue> getResults(final InstrumentDerivative irFuture, final YieldCurveBundle data, final ValueSpecification spec) {
     final double presentValue = CALCULATOR.visit(irFuture, data);
-    return Collections.singleton(new ComputedValue(getSpecification(target, forwardCurveName, fundingCurveName, curveCalculationMethod), presentValue));
+    return Collections.singleton(new ComputedValue(spec, presentValue));
   }
-
-  @Override
-  public Set<ValueSpecification> getResults(final FunctionCompilationContext context, final ComputationTarget target) {
-    return Sets.newHashSet(getSpecification(target));
-  }
-
-  @Override
-  public Set<ValueRequirement> getRequirements(final FunctionCompilationContext context, final ComputationTarget target, final ValueRequirement desiredValue) {
-    final Set<String> forwardCurves = desiredValue.getConstraints().getValues(YieldCurveFunction.PROPERTY_FORWARD_CURVE);
-    if (forwardCurves == null || forwardCurves.size() != 1) {
-      return null;
-    }
-    final Set<String> fundingCurves = desiredValue.getConstraints().getValues(YieldCurveFunction.PROPERTY_FUNDING_CURVE);
-    if (fundingCurves == null || fundingCurves.size() != 1) {
-      return null;
-    }
-    final Set<String> curveCalculationMethodNames = desiredValue.getConstraints().getValues(ValuePropertyNames.CURVE_CALCULATION_METHOD);
-    if (curveCalculationMethodNames == null || curveCalculationMethodNames.size() != 1) {
-      return null;
-    }
-    final String forwardCurveName = forwardCurves.iterator().next();
-    final String fundingCurveName = fundingCurves.iterator().next();
-    final String curveCalculationMethod = curveCalculationMethodNames.iterator().next();
-    final Set<ValueRequirement> requirements = new HashSet<ValueRequirement>();
-    if (forwardCurveName.equals(fundingCurveName)) {
-      requirements.add(getCurveRequirement(target, forwardCurveName, null, null, curveCalculationMethod));
-      return requirements;
-    }
-    requirements.add(getCurveRequirement(target, forwardCurveName, forwardCurveName, fundingCurveName, curveCalculationMethod));
-    requirements.add(getCurveRequirement(target, fundingCurveName, forwardCurveName, fundingCurveName, curveCalculationMethod));
-    return requirements;
-  }
-
-  private ValueSpecification getSpecification(final ComputationTarget target) {
-    return new ValueSpecification(ValueRequirementNames.PRESENT_VALUE, target.toSpecification(),
-        createValueProperties()
-        .with(ValuePropertyNames.CURRENCY, FinancialSecurityUtils.getCurrency(target.getTrade().getSecurity()).getCode())
-        .withAny(YieldCurveFunction.PROPERTY_FORWARD_CURVE)
-        .withAny(YieldCurveFunction.PROPERTY_FUNDING_CURVE)
-        .withAny(ValuePropertyNames.CURVE_CALCULATION_METHOD).get());
-  }
-
-  private ValueSpecification getSpecification(final ComputationTarget target, final String forwardCurveName, final String fundingCurveName, final String curveCalculationMethod) {
-    return new ValueSpecification(ValueRequirementNames.PRESENT_VALUE, target.toSpecification(),
-        createValueProperties()
-        .with(ValuePropertyNames.CURRENCY, FinancialSecurityUtils.getCurrency(target.getTrade().getSecurity()).getCode())
-        .with(YieldCurveFunction.PROPERTY_FORWARD_CURVE, forwardCurveName)
-        .with(YieldCurveFunction.PROPERTY_FUNDING_CURVE, fundingCurveName)
-        .with(ValuePropertyNames.CURVE_CALCULATION_METHOD, curveCalculationMethod).get());
-  }
-
 }
