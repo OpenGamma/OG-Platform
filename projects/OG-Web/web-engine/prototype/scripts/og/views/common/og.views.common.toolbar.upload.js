ae0389bd6751338a565743d17e9a23c493c0a260
Alan Ayoub <alan@opengamma.com>
2012-03-22 14:53:57 +0000

    changing width and adding iframe to import dialog

--- projects/OG-Web/web-engine/prototype/scripts/og/views/common/og.views.common.toolbar.upload.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/common/og.views.common.toolbar.upload.js
@@ -4,14 +4,15 @@
  */
 $.register_module({
     name: 'og.views.common.toolbar.upload',
-    dependencies: [],
+    dependencies: ['og.common.util.ui.dialog'],
     obj: function () {
         return function () {
-            ui.dialog({
-                width: 400, height: 190,
+            og.common.util.ui.dialog({
+                width: 600, height: 400,
                 type: 'input',
                 title: 'Import Portfolios',
-                custom: '<div>test</div>',
+                custom: '<iframe src="import.ftl" width="580" height="250" marginheight="0" marginwidth="0"\
+                         frameborder="0" />',
                 buttons: {
                     'OK': function () {$(this).dialog('close');},
                     'Cancel': function () {$(this).dialog('close');}
