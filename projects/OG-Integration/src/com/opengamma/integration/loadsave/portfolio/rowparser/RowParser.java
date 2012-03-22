17fda515120481728c173cbeef5ea5637a0d5ae5
Kevin Vella <kevin@opengamma.com>
2012-03-22 15:27:45 +0000

    Set to use ISO date format by default

--- projects/OG-Integration/src/com/opengamma/integration/loadsave/portfolio/rowparser/RowParser.java
+++ projects/OG-Integration/src/com/opengamma/integration/loadsave/portfolio/rowparser/RowParser.java
@@ -39,7 +39,7 @@ public abstract class RowParser {
   
   {
     DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
-    builder.appendPattern("dd/MM/yyyy");
+    builder.appendPattern("yyyy-MM-dd");
     CSV_DATE_FORMATTER = builder.toFormatter();
     builder = new DateTimeFormatterBuilder();
     builder.appendPattern("yyyy-MM-dd");
