da66faec481e80a8fa6e20a936764dc4dfb725ab
Chris Kent <chris@opengamma.com>
2012-03-23 14:29:13 +0000

    [IGN-179] Refactor PortfolioLoaderResource

--- projects/OG-Web/src/com/opengamma/web/loader/PortfolioLoaderResource.java
+++ projects/OG-Web/src/com/opengamma/web/loader/PortfolioLoaderResource.java
@@ -7,15 +7,16 @@ package com.opengamma.web.loader;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
@@ -37,42 +38,41 @@ public class PortfolioLoaderResource {
   @Path("{updatePeriod}/{updateCount}")
   @POST
   @Consumes(MediaType.MULTIPART_FORM_DATA)
-  public void uploadPortfolio(FormDataMultiPart formData,
-                              // TODO according to the docs this should work but jersey won't start with them uncommented
-                              //@FormDataParam("file") FormDataBodyPart fileBodyPart,
-                              //@FormDataParam("dataField") String dataField,
-                              @PathParam("updatePeriod") long updatePeriod,
-                              @PathParam("updateCount") int updateCount,
-                              @Context HttpServletResponse response) throws IOException {
+  public StreamingOutput uploadPortfolio(FormDataMultiPart formData,
+                                         // TODO according to the docs this should work but jersey won't start with them uncommented
+                                         //@FormDataParam("file") FormDataBodyPart fileBodyPart,
+                                         //@FormDataParam("dataField") String dataField,
+                                         @PathParam("updatePeriod") final long updatePeriod,
+                                         @PathParam("updateCount") final int updateCount) throws IOException {
     FormDataBodyPart fileBodyPart = formData.getField("file");
     if (fileBodyPart == null) {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      return;
+      throw new WebApplicationException(Response.Status.BAD_REQUEST);
     }
     FormDataBodyPart dataFieldBodyPart = formData.getField("dataField");
     if (dataFieldBodyPart == null) {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      return;
+      throw new WebApplicationException(Response.Status.BAD_REQUEST);
     }
     String dataField = dataFieldBodyPart.getValue();
     Object fileEntity = fileBodyPart.getEntity();
     if (!(fileEntity instanceof BodyPartEntity)) {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      return;
+      throw new WebApplicationException(Response.Status.BAD_REQUEST);
     }
     InputStream fileStream = ((BodyPartEntity) fileEntity).getInputStream();
     String fileContent = IOUtils.toString(fileStream);
     s_logger.warn("Portfolio uploaded. dataField: {}, portfolio: {}", dataField, fileContent);
-    ServletOutputStream outputStream = response.getOutputStream();
-    for (int i = 0; i < updateCount; i++) {
-      try {
-        Thread.sleep(updatePeriod);
-      } catch (InterruptedException e) {
-        s_logger.warn("This shouldn't happen");
+    return new StreamingOutput() {
+      @Override
+      public void write(OutputStream output) throws IOException, WebApplicationException {
+        for (int i = 0; i < updateCount; i++) {
+          try {
+            Thread.sleep(updatePeriod);
+          } catch (InterruptedException e) {
+            s_logger.warn("This shouldn't happen");
+          }
+          output.write("uploading portfolio...".getBytes());
+          output.flush();
+        }
       }
-      outputStream.write("uploading portfolio...\n".getBytes());
-      outputStream.flush();
-    }
-    response.setStatus(HttpServletResponse.SC_OK);
+    };
   }
 }
