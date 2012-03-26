d77eeabf21f8cdcdc11c035b79e42e2325e1e78b
Afshin Darian <afshin@opengamma.com>
2012-03-26 11:41:27 +0100

    incremental check in of grid stuff, adding handlebars library

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.grid.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.grid.js
@@ -6,17 +6,22 @@ $.register_module({
     name: 'og.analytics.grid',
     dependencies: ['og.api.text', 'og.analytics.Data'],
     obj: function () {
-        var api_text = og.api.text, counter = 1,
-            header_tmpl = {module: 'og.analytics.grid.header'},
-            container_tmpl = {module: 'og.analytics.grid.container'},
+        var module = this, api_text = og.api.text, counter = 1,
+            css_tmpl = api_text.partial({url: module.html_root + 'analytics/grid/og.analytics.grid_tash.css'}),
+            header_tmpl = api_text.partial({module: 'og.analytics.grid.header_tash'}),
+            container_tmpl = api_text.partial({module: 'og.analytics.grid.container_tash'}),
+            templates = null,
             scrollbar_size = 19;
         return function (config) {
             var selector = config.selector;
-            $.when(api_text(header_tmpl), api_text(container_tmpl)).then(function (header_tmpl, container_tmpl) {
+            $.when(css_tmpl(), header_tmpl(), container_tmpl()).then(function (css_tmpl, header_tmpl, container_tmpl) {
                 var gen_rows, gen_head, gen_data, gen_css, viewport, header, width, columns,
-                    data = new og.analytics.Data, id = 'analytics_grid_' + counter++;
-                $(selector).html($.tmpl(container_tmpl, {id: id}));
-                id = '#' + id;
+                    data = new og.analytics.Data, id = '#analytics_grid_' + counter++, col_css;
+                if (!templates) templates = {
+                    css: Handlebars.compile(css_tmpl),
+                    header: Handlebars.compile(header_tmpl),
+                    container: Handlebars.compile(container_tmpl)
+                };
                 gen_data = function (cols) {
                     var matrix = [], i = 60, k, gen_num = function () {
                         return (Math.random() * 1000);
@@ -47,7 +52,7 @@ $.register_module({
                     return html.join('');
                 };
                 gen_head = function (columns, start_at) {
-                    return $.tmpl(header_tmpl, {
+                    return templates.header({
                         width: start_at ? width.scroll : width.fixed,
                         padding_right: start_at ? scrollbar_size : 0,
                         cols: columns.reduce(function (acc, val, idx) {
@@ -56,34 +61,24 @@ $.register_module({
                         }, '')
                     });
                 };
-                gen_css = function () {
-                    var $style = $('<style type="text/css" />').appendTo($('head')), css, gen_group;
-                    gen_group = function (cols, start_at) {
-                        var pwidth = 0, all_cols_width = cols.reduce(function (acc, val, i) {
-                            return acc + parseInt(val.width);
-                        }, 0);
-                        return cols.reduce(function (acc, val, i) {
-                            var width = val.width;
-                            acc.push(id + ' .OG-g .c' + (i + (start_at || 0)) +
-                                '{left: ' + pwidth + 'px; right: ' + (all_cols_width - pwidth - width) + 'px;}');
-                            pwidth += width;
-                            return acc;
-                        }, []).join('\n');
-                    };
-                    css = [
-                        id + ' .OG-g-b-scroll {width: ' + viewport.width + 'px; left: ' + width.fixed + 'px; height: '+
-                            viewport.height +'px}',
-                        id + ' .OG-g-h-scroll {width: ' + viewport.width + 'px; height: ' + header.height +
-                            'px; left: ' + width.fixed + 'px}',
-                        id + ' .OG-g-b-scroll .OG-g-row {width: ' + width.scroll + 'px}',
-                        id + ' .OG-g-b-fixed {width: ' + width.fixed + 'px; height: '+ viewport.height + 'px}',
-                        id + ' .OG-g-h-fixed {width: ' + width.fixed + 'px}',
-                        id + ' .OG-g-b {top: ' + header.height + 'px}',
-                        gen_group(columns.columns_fixed, 0),
-                        gen_group(columns.columns_scroll, columns.columns_fixed.length)
-                    ].join('\n');
+                load_css = function (css) {
+                    var $style = $('<style type="text/css" />').appendTo($('head'));
                     if ($style[0].styleSheet) $style[0].styleSheet.cssText = css; // IE
                     else $style[0].appendChild(document.createTextNode(css));
+                };
+                col_css = function (columns, cols_preceding) {
+                    var partial_width = 0, total_width = columns.reduce(function (acc, val) {
+                        return +val.width + acc;
+                    }, 0);
+                    cols_preceding = cols_preceding || 0;
+                    return columns.map(function (val, idx) {
+                        var width = +val.width, css = {
+                            selector: 'c' + (idx + cols_preceding),
+                            left: partial_width, right: total_width - partial_width - width
+                        };
+                        partial_width += width;
+                        return css;
+                    })
                 };
                 data.init(function (result) {
                     columns = result;
@@ -91,11 +86,21 @@ $.register_module({
                     header = {height: '51'}; // Stub
                     width = (function (columns) { // width of fixed and scrollable column areas
                         return {
-                            fixed: columns.columns_fixed.reduce(function (acc, val) {return acc + val.width}, 0),
-                            scroll: columns.columns_scroll.reduce(function (acc, val) {return acc + val.width}, 0)
+                            fixed: columns.columns_fixed.reduce(function (acc, val) {return acc + val.width;}, 0),
+                            scroll: columns.columns_scroll.reduce(function (acc, val) {return acc + val.width;}, 0)
                         };
                     })(columns);
-                    gen_css();
+                    load_css(templates.css({
+                        id: id,
+                        height: viewport.height,
+                        width: viewport.width,
+                        header_height: header.height,
+                        fixed_width: width.fixed,
+                        scroll_width: width.scroll,
+                        columns: col_css(columns.columns_fixed)
+                            .concat(col_css(columns.columns_scroll, columns.columns_fixed.length))
+                    }));
+                    $(selector).html(templates.container({id: id.substring(1)}));
                     $(id + ' .OG-g-b-scroll').scroll(function (e) { // sync scroll
                         $(id + ' .OG-g-b-fixed').scrollTop(e.target.scrollTop);
                         $(id + ' .OG-g-h-scroll').scrollLeft(e.target.scrollLeft);
