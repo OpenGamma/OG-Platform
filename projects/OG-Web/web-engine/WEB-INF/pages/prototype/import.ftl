061418428de9c01ecc7e65e7bfb59728a6b26400
Alan Ayoub <alan@opengamma.com>
2012-03-27 12:24:10 +0100

    adding import dialog and iframe

--- projects/OG-Web/web-engine/WEB-INF/pages/prototype/import.ftl
+++ projects/OG-Web/web-engine/WEB-INF/pages/prototype/import.ftl
@@ -12,12 +12,31 @@
 ${ogStyle.print('og_all.css', 'all',false)}
 <style type="text/css">
     body {background: #fff;}
+    div {margin-bottom: 20px}
+    small {font-size: 10px;}
+    input[type=text] {width: 200px}
 </style>
 </head>
 <body>
-<div>
-  <p style="margin-bottom: 10px;">Upload a CSV file to import Portfolios, Securities, Timeseries...</p>
-  <input type="file" />
-</div>
+  <form action="http://localhost:8080/jax/portfolioupload/10/1000" enctype="multipart/form-data" method="post">
+    <div>
+      <label>
+        Portfolio Name: <br /><input type="text" name="portfolioName"><br />
+      </label>
+    </div>
+    <div>
+      <label>
+        Timeseries DataField: <br /><input type="text" name="dataField"><br />
+        <small>(comma delimited, e.g. PX_LAST, YLD_YTM_MID)</small>
+      </label>
+    </div>
+    <div>
+      <label>
+        CSV Upload:<br />
+        <input type="file" name="file"><br />
+      </label>
+      <small>CSV Format information is documented on the wiki</small>
+    </div>
+  </form>
 </body>
 </html>
--- projects/OG-Web/web-engine/prototype/scripts/og/views/common/og.views.common.toolbar.upload.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/common/og.views.common.toolbar.upload.js
@@ -8,13 +8,16 @@ $.register_module({
     obj: function () {
         return function () {
             og.common.util.ui.dialog({
-                width: 600, height: 400,
+                width: 600, height: 370,
                 type: 'input',
-                title: 'Import Portfolios',
-                custom: '<iframe src="import.ftl" width="580" height="250" marginheight="0" marginwidth="0"\
+                title: 'Import Portfolio',
+                custom: '<iframe id="import" src="import.ftl" width="500" height="220" marginheight="0" marginwidth="0"\
                          frameborder="0" />',
                 buttons: {
-                    'OK': function () {$(this).dialog('close');},
+                    'Start Import': function () {
+                        $('#import').contents().find('form').submit();
+                        $(this).dialog('option', 'buttons', {'Close': function () {$(this).dialog('close');}});
+                    },
                     'Cancel': function () {$(this).dialog('close');}
                 }
             })
