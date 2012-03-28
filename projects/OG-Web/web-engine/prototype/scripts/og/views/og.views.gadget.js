00e1355d04783c75c375e6a81bf9be61f3c600d9
Afshin Darian <afshin@opengamma.com>
2012-03-28 20:33:35 +0100

    updated gadget viewer, timeseries gadget to be able to accept raw data instead of an ID, added a gadget demo that should be removed

--- projects/OG-Web/web-engine/prototype/scripts/og/views/og.views.gadget.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/og.views.gadget.js
@@ -9,16 +9,17 @@ $.register_module({
         var module = this, view, common = og.common, routes = common.routes;
         return view = {
             init: function () {for (var rule in view.rules) routes.add(view.rules[rule]);},
+            root: function () {$('#gadget_content').html('No gadget was specified.');},
             timeseries: function (args) {
                 var options = {selector: '.OG-timeseries-gadget', datapoints_link: false};
                 $('#gadget_content').html('<section class="' + options.selector.substring(1) + '"></section>');
-                if (args.id)
-                    options.id = args.id;
-                else
-                    options.data = og.api.common.get_cache(args.key) || {};
+                if (args.id) options.id = args.id; else options.data = og.api.common.get_cache(args.key);
+                if (args.key) og.api.common.del_cache(args.key);
+                if (!options.data && !options.id) return $('#gadget_content').html('There is no data to load.');
                 common.gadgets.timeseries(options);
             },
             rules: {
+                root: {route: '/', method: module.name + '.root'},
                 timeseries: {route: '/timeseries/id:?/key:?', method: module.name + '.timeseries'}
             }
         }
