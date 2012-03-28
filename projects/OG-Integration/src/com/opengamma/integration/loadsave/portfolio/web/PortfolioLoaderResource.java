7d0388e4df414f40482feffec8d5fab598633740
Chris Kent <chris@opengamma.com>
2012-03-28 16:30:10 +0100

    comments

--- projects/OG-Integration/src/com/opengamma/integration/loadsave/portfolio/web/PortfolioLoaderResource.java
+++ projects/OG-Integration/src/com/opengamma/integration/loadsave/portfolio/web/PortfolioLoaderResource.java
@@ -157,8 +157,8 @@ public class PortfolioLoaderResource {
 
   /**
    * This wraps the file upload input stream to work around a bug in {@code org.jvnet.mimepull} which is used by Jersey
-   * Multipart.  The bug causes the {@code read()} method to throw an exception if it is called
-   * twice at the end of the stream which violates the contract of {@link InputStream}.  It ought to
+   * Multipart.  The bug causes the {@code read()} method of the file upload stream to throw an exception if it is
+   * called twice at the end of the stream which violates the contract of {@link InputStream}.  It ought to
    * keep returning {@code -1} indefinitely.  This class restores that behaviour.
    * TODO Check if this can be removed when we upgrade Jersey. It is a problem when the CSV file doesn't end with a blank line
    * @see <a href="http://java.net/jira/browse/JAX_WS-965">The bug report</a>
