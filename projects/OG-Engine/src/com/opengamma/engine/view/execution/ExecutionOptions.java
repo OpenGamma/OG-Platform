aa9e281645343e451493a58c609a2cb308f6cdd4
Stephen Colebourne <stephen@opengamma.com>
2012-03-29 11:01:45 +0100

    Reorder methods in class

--- projects/OG-Engine/src/com/opengamma/engine/view/execution/ExecutionOptions.java
+++ projects/OG-Engine/src/com/opengamma/engine/view/execution/ExecutionOptions.java
@@ -27,124 +27,12 @@ public class ExecutionOptions implements ViewExecutionOptions {
   private final ViewCycleExecutionOptions _defaultExecutionOptions;
   private final VersionCorrection _versionCorrection;
 
-  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags) {
-    this(executionSequence, flags, null, null);
-  }
-
-  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags, Integer maxSuccessiveDeltaCycles) {
-    this(executionSequence, flags, maxSuccessiveDeltaCycles, null);
-  }
-
-  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags, ViewCycleExecutionOptions defaultExecutionOptions) {
-    this(executionSequence, flags, null, defaultExecutionOptions);
-  }
-
-  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags,
-      Integer maxSuccessiveDeltaCycles, ViewCycleExecutionOptions defaultExecutionOptions) {
-    this(executionSequence, flags, maxSuccessiveDeltaCycles, defaultExecutionOptions, VersionCorrection.LATEST);
-  }
-
-  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags,
-      Integer maxSuccessiveDeltaCycles, ViewCycleExecutionOptions defaultExecutionOptions, VersionCorrection versionCorrection) {
-    ArgumentChecker.notNull(executionSequence, "executionSequence");
-    ArgumentChecker.notNull(flags, "flags");
-    ArgumentChecker.notNull(versionCorrection, "versionCorrection");
-    
-    _executionSequence = executionSequence;
-    _flags = flags;
-    _maxSuccessiveDeltaCycles = maxSuccessiveDeltaCycles;
-    _defaultExecutionOptions = defaultExecutionOptions;
-    _versionCorrection = versionCorrection;
-  }
-
-  //-------------------------------------------------------------------------
-  @Override
-  public ViewCycleExecutionSequence getExecutionSequence() {
-    return _executionSequence;
-  }
-
-  @Override
-  public Integer getMaxSuccessiveDeltaCycles() {
-    return _maxSuccessiveDeltaCycles;
-  }
-
-  @Override
-  public ViewCycleExecutionOptions getDefaultExecutionOptions() {
-    return _defaultExecutionOptions;
-  }
-
-  @Override
-  public EnumSet<ViewExecutionFlags> getFlags() {
-    return _flags;
-  }
-
-  @Override
-  public VersionCorrection getVersionCorrection() {
-    return _versionCorrection;
-  }
-
-  //-------------------------------------------------------------------------
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + _executionSequence.hashCode();
-    result = prime * result + _flags.hashCode();
-    result = prime * result + ((_defaultExecutionOptions == null) ? 0 : _defaultExecutionOptions.hashCode());
-    result = prime * result + ((_maxSuccessiveDeltaCycles == null) ? 0 : _maxSuccessiveDeltaCycles.hashCode());
-    result = prime * result + _versionCorrection.hashCode();
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (!(obj instanceof ExecutionOptions)) {
-      return false;
-    }
-    ExecutionOptions other = (ExecutionOptions) obj;
-    if (!_executionSequence.equals(other._executionSequence)) {
-      return false;
-    }
-    if (!_flags.equals(other._flags)) {
-      return false;
-    }
-    if (_defaultExecutionOptions == null) {
-      if (other._defaultExecutionOptions != null) {
-        return false;
-      }
-    } else if (!_defaultExecutionOptions.equals(other._defaultExecutionOptions)) {
-      return false;
-    }
-    if (_maxSuccessiveDeltaCycles == null) {
-      if (other._maxSuccessiveDeltaCycles != null) {
-        return false;
-      }
-    } else if (!_maxSuccessiveDeltaCycles.equals(other._maxSuccessiveDeltaCycles)) {
-      return false;
-    } else if (!_versionCorrection.equals(other._versionCorrection)) {
-      return false;
-    }
-    return true;
-  }
-
-  @Override
-  public String toString() {
-    return "ExecutionOptions [executionSequence=" + _executionSequence + ", flags=" + _flags + ", maxSuccessiveDeltaCycles=" + _maxSuccessiveDeltaCycles + ", defaultExecutionOptions=" +
-        _defaultExecutionOptions + ", versionCorrection=" + _versionCorrection + "]";
-  }
-
   //-------------------------------------------------------------------------
   /**
    * Creates a custom execution sequence.
    * 
    * @param cycleExecutionSequence  the execution sequence, not null
-   * @param flags  execution flags, not null
+   * @param flags  the execution flags, not null
    * @return the execution sequence, not null
    */
   public static ViewExecutionOptions of(ViewCycleExecutionSequence cycleExecutionSequence, EnumSet<ViewExecutionFlags> flags) {
@@ -258,6 +146,119 @@ public class ExecutionOptions implements ViewExecutionOptions {
     }
     ViewCycleExecutionSequence sequence = new ArbitraryViewCycleExecutionSequence(Arrays.asList(cycleOptions));
     return of(sequence, flags);
+  }
+
+  //-------------------------------------------------------------------------
+  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags) {
+    this(executionSequence, flags, null, null);
+  }
+
+  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags, Integer maxSuccessiveDeltaCycles) {
+    this(executionSequence, flags, maxSuccessiveDeltaCycles, null);
+  }
+
+  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags, ViewCycleExecutionOptions defaultExecutionOptions) {
+    this(executionSequence, flags, null, defaultExecutionOptions);
+  }
+
+  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags,
+      Integer maxSuccessiveDeltaCycles, ViewCycleExecutionOptions defaultExecutionOptions) {
+    this(executionSequence, flags, maxSuccessiveDeltaCycles, defaultExecutionOptions, VersionCorrection.LATEST);
+  }
+
+  public ExecutionOptions(ViewCycleExecutionSequence executionSequence, EnumSet<ViewExecutionFlags> flags,
+      Integer maxSuccessiveDeltaCycles, ViewCycleExecutionOptions defaultExecutionOptions, VersionCorrection versionCorrection) {
+    ArgumentChecker.notNull(executionSequence, "executionSequence");
+    ArgumentChecker.notNull(flags, "flags");
+    ArgumentChecker.notNull(versionCorrection, "versionCorrection");
+    
+    _executionSequence = executionSequence;
+    _flags = flags;
+    _maxSuccessiveDeltaCycles = maxSuccessiveDeltaCycles;
+    _defaultExecutionOptions = defaultExecutionOptions;
+    _versionCorrection = versionCorrection;
+  }
+
+  //-------------------------------------------------------------------------
+  @Override
+  public ViewCycleExecutionSequence getExecutionSequence() {
+    return _executionSequence;
+  }
+
+  @Override
+  public Integer getMaxSuccessiveDeltaCycles() {
+    return _maxSuccessiveDeltaCycles;
+  }
+
+  @Override
+  public ViewCycleExecutionOptions getDefaultExecutionOptions() {
+    return _defaultExecutionOptions;
+  }
+
+  @Override
+  public EnumSet<ViewExecutionFlags> getFlags() {
+    return _flags;
+  }
+
+  @Override
+  public VersionCorrection getVersionCorrection() {
+    return _versionCorrection;
+  }
+
+  //-------------------------------------------------------------------------
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof ExecutionOptions)) {
+      return false;
+    }
+    ExecutionOptions other = (ExecutionOptions) obj;
+    if (!_executionSequence.equals(other._executionSequence)) {
+      return false;
+    }
+    if (!_flags.equals(other._flags)) {
+      return false;
+    }
+    if (_defaultExecutionOptions == null) {
+      if (other._defaultExecutionOptions != null) {
+        return false;
+      }
+    } else if (!_defaultExecutionOptions.equals(other._defaultExecutionOptions)) {
+      return false;
+    }
+    if (_maxSuccessiveDeltaCycles == null) {
+      if (other._maxSuccessiveDeltaCycles != null) {
+        return false;
+      }
+    } else if (!_maxSuccessiveDeltaCycles.equals(other._maxSuccessiveDeltaCycles)) {
+      return false;
+    } else if (!_versionCorrection.equals(other._versionCorrection)) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + _executionSequence.hashCode();
+    result = prime * result + _flags.hashCode();
+    result = prime * result + ((_defaultExecutionOptions == null) ? 0 : _defaultExecutionOptions.hashCode());
+    result = prime * result + ((_maxSuccessiveDeltaCycles == null) ? 0 : _maxSuccessiveDeltaCycles.hashCode());
+    result = prime * result + _versionCorrection.hashCode();
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "ExecutionOptions [executionSequence=" + _executionSequence + ", flags=" + _flags + ", maxSuccessiveDeltaCycles=" + _maxSuccessiveDeltaCycles + ", defaultExecutionOptions=" +
+        _defaultExecutionOptions + ", versionCorrection=" + _versionCorrection + "]";
   }
 
 }
