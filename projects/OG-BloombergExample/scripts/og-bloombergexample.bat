8ec8c23da4dbc28e019f4cb557b7e6c86910c264
Kevin Vella <kevin@opengamma.com>
2012-04-01 12:24:42 +0100

    Updated Win batch files for OG-Examples and OG-BloombergExample; as yet untested.

--- projects/OG-BloombergExample/scripts/og-bloombergexample.bat
+++ projects/OG-BloombergExample/scripts/og-bloombergexample.bat
@@ -1,34 +1,47 @@
 @ECHO OFF
 
 REM PLAT-1527
-pushd %~dp0\..
+PUSHD %~dp0\..
 
-IF NOT EXIST .\temp\hsqldb .\scripts\init-bloombergexample-db.bat
+set BASEDIR=%cd%
+set SCRIPTDIR=%BASEDIR%\scripts
+set PROJECT=og-bloombergexample
+set PROJECTJAR=%PROJECT%.jar
+set LOGBACK_CONFIG=jetty-logback.xml
+set CONFIG=config\fullstack\bloombergexample-bin.properties
+
+IF NOT EXIST %BASEDIR%\temp\hsqldb goto :nodb 
 
 IF "%JAVA_HOME%" == "" ECHO Warning: JAVA_HOME is not set
 SET JAVACMD=%JAVA_HOME%\bin\java.exe
-SET CMDLINE="%JAVACMD%" ^
+
+IF "%1"=="start" goto :start
+IF "%1"=="debug" goto :start
+IF "%1"=="restart" goto :start
+IF "%1"=="reload" goto :start
+IF "%1"=="stop" ECHO Stop not supported
+IF "%1"=="status" ECHO Status not supported
+IF "%1"=="" ECHO Usage: %0 start|stop|restart|status|reload|debug
+GOTO :exit
+
+:nodb
+ECHO ERROR: The %PROJECT% database could not be found.
+ECHO Please run %SCRIPTDIR%\init-%PROJECT%-db.bat to create and populate the database.
+ECHO Exiting immediately...
+GOTO :exit
+
+:start
+"%JAVACMD%" ^
   -Xms1024m -Xmx3072m -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC ^
   -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing ^
-  -Dlogback.configurationFile=bloombergexample-logback.xml ^
+  -Dlogback.configurationFile=%LOGBACK_CONFIG% ^
         -Dcommandmonitor.secret=OpenGamma ^
-  -cp config;og-bloombergexample.jar;lib ^
+  -cp config;%PROJECTJAR%;lib ^
   com.opengamma.component.OpenGammaComponentServer ^
-  config\fullstack\bloombergexample-bin.properties
+  %CONFIG%
+GOTO :exit
 
-IF "%1"=="start" "%CMDLINE%"
-
-IF "%1"=="debug" "%CMDLINE%"
-
-IF "%1"=="restart" "%CMDLINE%"
-
-IF "%1"=="reload" "%CMDLINE%"
-
-IF "%1"=="stop" ECHO Stop not supported
-
-IF "%1"=="status" ECHO Status not supported
-
-IF "%1"=="" ECHO Usage: %0 "start|stop|restart|status|reload|debug"
-
+:exit
 REM PLAT-1527
-popd
+POPD
+EXIT /B
--- projects/OG-Examples/scripts/og-examples.bat
+++ projects/OG-Examples/scripts/og-examples.bat
@@ -3,38 +3,45 @@
 REM PLAT-1527
 PUSHD %~dp0\..
 
-IF NOT EXIST .\temp\hsqldb echo The HSQL database directory is not present. Please run scripts\init-examples-db.bat to create and populate the database.
+set BASEDIR=%cd%
+set SCRIPTDIR=%BASEDIR%\scripts
+set PROJECT=og-examples
+set PROJECTJAR=%PROJECT%.jar
+set LOGBACK_CONFIG=jetty-logback.xml
+set CONFIG=config\fullstack\example-bin.properties
+
+IF NOT EXIST %BASEDIR%\temp\hsqldb goto :nodb 
 
 IF "%JAVA_HOME%" == "" ECHO Warning: JAVA_HOME is not set
 SET JAVACMD=%JAVA_HOME%\bin\java.exe
-SET CMDLINE=IF "%1"=="start" goto :start
 
+IF "%1"=="start" goto :start
 IF "%1"=="debug" goto :start
-
 IF "%1"=="restart" goto :start
-
 IF "%1"=="reload" goto :start
-
 IF "%1"=="stop" ECHO Stop not supported
-
 IF "%1"=="status" ECHO Status not supported
-
 IF "%1"=="" ECHO Usage: %0 start|stop|restart|status|reload|debug
+GOTO :exit
 
-REM PLAT-1527
-POPD
-
-EXIT
-
+:nodb
+ECHO ERROR: The %PROJECT% database could not be found.
+ECHO Please run %SCRIPTDIR%\init-%PROJECT%-db.bat to create and populate the database.
+ECHO Exiting immediately...
+GOTO :exit
 
 :start
 "%JAVACMD%" ^
   -Xms1024m -Xmx3072m -XX:MaxPermSize=256M -XX:+UseConcMarkSweepGC ^
   -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing ^
-  -Dlogback.configurationFile=examples-logback.xml ^
+  -Dlogback.configurationFile=%LOGBACK_CONFIG% ^
         -Dcommandmonitor.secret=OpenGamma ^
-  -cp config;og-examples.jar;lib ^
+  -cp config;%PROJECTJAR%;lib ^
   com.opengamma.component.OpenGammaComponentServer ^
-  config\fullstack\example-bin.properties
+  %CONFIG%
+GOTO :exit
 
-
+:exit
+REM PLAT-1527
+POPD
+EXIT /B
