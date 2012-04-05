b07768a1f7dd3bf76b5c64cdc8cd063250b3806b
Afshin Darian <afshin@opengamma.com>
2012-04-05 12:24:01 +0100

    updating grid manager to deal with resizes properly

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.manager.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.manager.js
@@ -6,18 +6,18 @@ $.register_module({
     name: 'og.common.gadgets.manager',
     dependencies: [],
     obj: function () {
-        var manager, collector, gadgets = [], interval = 5 * 60 * 1000;
+        var manager, collector, gadgets = [], interval = 5 * 60 * 1000, layout, first = true,
+            resize = function () {
+                return layout && first ? first = false : manager.clean().forEach(function (gadget) {gadget.resize();});
+            };
         setTimeout((collector = function () {return manager.clean(), setTimeout(collector, interval);}), interval);
-        $(function () {if (!og.views.common.layout) return $(window).on('resize', manager.resize);});
+        $(function () {if (!(layout = !!og.views.common.layout)) $(window).on('resize', manager.resize);});
         return manager = {
             clean: function () {return gadgets = gadgets.filter(function (gadget) {return gadget.alive();});},
             gadgets: function () {return gadgets;},
             register: function (gadget) {manager.clean().push(gadget);},
             resize: (function (timeout) {
-                return function () {
-                    timeout = clearTimeout(timeout) || // ignore multiple calls and throttle: one resize per 200ms
-                        setTimeout(function () {manager.clean().forEach(function (gadget) {gadget.resize();});}, 200);
-                };
+                return function () {timeout = clearTimeout(timeout) || setTimeout(resize, layout ? 0 : 200);};
             })(null)
         };
     }
