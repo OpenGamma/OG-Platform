b20b4208955641064b8d99311d57953cd1170d37
Kevin Vella <kevin@opengamma.com>
2012-04-10 12:48:26 +0100

    Changed remaining master DB tests to fit revised test data (external ID GH~HI rather than G~HI)

--- projects/OG-MasterDB/tests/unit/com/opengamma/masterdb/security/QuerySecurityDbSecurityMasterWorkerSearchTest.java
+++ projects/OG-MasterDB/tests/unit/com/opengamma/masterdb/security/QuerySecurityDbSecurityMasterWorkerSearchTest.java
@@ -365,7 +365,7 @@ public class QuerySecurityDbSecurityMasterWorkerSearchTest extends AbstractDbSec
   @Test
   public void test_search_oneKey_Any_GHI() {
     SecuritySearchRequest request = new SecuritySearchRequest();
-    request.addExternalId(ExternalId.of("G", "HI"));
+    request.addExternalId(ExternalId.of("GH", "HI"));
     SecuritySearchResult test = _secMaster.search(request);
     
     assertEquals(1, test.getDocuments().size());
@@ -397,7 +397,7 @@ public class QuerySecurityDbSecurityMasterWorkerSearchTest extends AbstractDbSec
   @Test
   public void test_search_twoKeys_EF_GHI() {
     SecuritySearchRequest request = new SecuritySearchRequest();
-    request.addExternalIds(ExternalId.of("E", "F"), ExternalId.of("G", "HI"));
+    request.addExternalIds(ExternalId.of("E", "F"), ExternalId.of("GH", "HI"));
     SecuritySearchResult test = _secMaster.search(request);
     
     assertEquals(3, test.getDocuments().size());
@@ -456,7 +456,7 @@ public class QuerySecurityDbSecurityMasterWorkerSearchTest extends AbstractDbSec
   @Test
   public void test_search_oneKey_All_GHI() {
     SecuritySearchRequest request = new SecuritySearchRequest();
-    request.addExternalId(ExternalId.of("G", "HI"));
+    request.addExternalId(ExternalId.of("GH", "HI"));
     request.getExternalIdSearch().setSearchType(ExternalIdSearchType.ALL);
     SecuritySearchResult test = _secMaster.search(request);
     
@@ -524,7 +524,7 @@ public class QuerySecurityDbSecurityMasterWorkerSearchTest extends AbstractDbSec
   @Test
   public void test_search_threeKeys_All_AB_CD_GHI() {
     SecuritySearchRequest request = new SecuritySearchRequest();
-    request.addExternalIds(ExternalId.of("A", "B"), ExternalId.of("C", "D"), ExternalId.of("G", "HI"));
+    request.addExternalIds(ExternalId.of("A", "B"), ExternalId.of("C", "D"), ExternalId.of("GH", "HI"));
     request.getExternalIdSearch().setSearchType(ExternalIdSearchType.ALL);
     SecuritySearchResult test = _secMaster.search(request);
     
@@ -580,7 +580,7 @@ public class QuerySecurityDbSecurityMasterWorkerSearchTest extends AbstractDbSec
   @Test
   public void test_search_threeKeys_Exact_AB_CD_GHI() {
     SecuritySearchRequest request = new SecuritySearchRequest();
-    request.addExternalIds(ExternalId.of("A", "B"), ExternalId.of("C", "D"), ExternalId.of("G", "HI"));
+    request.addExternalIds(ExternalId.of("A", "B"), ExternalId.of("C", "D"), ExternalId.of("GH", "HI"));
     request.getExternalIdSearch().setSearchType(ExternalIdSearchType.EXACT);
     SecuritySearchResult test = _secMaster.search(request);
     
