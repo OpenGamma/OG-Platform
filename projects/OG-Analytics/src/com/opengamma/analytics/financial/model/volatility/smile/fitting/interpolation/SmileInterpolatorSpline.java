2ff6a82eabe677e470a5f49a7d5851c551e3b45c
Elaine McLeod <elaine@opengamma.com>
2012-04-12 12:31:38 +0100

    Fixing tests

--- projects/OG-Analytics/src/com/opengamma/analytics/financial/model/volatility/smile/fitting/interpolation/SmileInterpolatorSpline.java
+++ projects/OG-Analytics/src/com/opengamma/analytics/financial/model/volatility/smile/fitting/interpolation/SmileInterpolatorSpline.java
@@ -43,13 +43,14 @@ public class SmileInterpolatorSpline implements GeneralSmileInterpolator {
   //    TRANSFORMS = new UncoupledParameterTransforms(new DoubleMatrix1D(0.0, 1.0), new ParameterLimitsTransform[] {a, b }, new BitSet());
   //  }
 
+  @Override
   public Function1D<Double, Double> getVolatilityFunction(final double forward, final double[] strikes, final double expiry, final double[] impliedVols) {
     final int n = strikes.length;
     ArgumentChecker.isTrue(impliedVols.length == n, "#strikes does not mach #vols");
     final double kL = strikes[0];
     final double kH = strikes[n - 1];
-    ArgumentChecker.isTrue(kL <= forward, "Cannot do left tail extrapolation when the lowest strike ({}) is greater that the forward ({})", kL, forward);
-    ArgumentChecker.isTrue(kH >= forward, "Cannot do right tail extrapolation when the highest strike ({}) is less that the forward ({})", kH, forward);
+    ArgumentChecker.isTrue(kL <= forward, "Cannot do left tail extrapolation when the lowest strike ({}) is greater than the forward ({})", kL, forward);
+    ArgumentChecker.isTrue(kH >= forward, "Cannot do right tail extrapolation when the highest strike ({}) is less than the forward ({})", kH, forward);
     final double volL = impliedVols[0];
     final double volH = impliedVols[n - 1];
 
@@ -61,23 +62,23 @@ public class SmileInterpolatorSpline implements GeneralSmileInterpolator {
 
     final Function1D<Double, Double> interpFunc = new Function1D<Double, Double>() {
       @Override
-      public Double evaluate(Double k) {
-        double m = Math.log(k / forward);
+      public Double evaluate(final Double k) {
+        final double m = Math.log(k / forward);
         return _interpolator.interpolate(data, m);
       }
     };
 
     final Function1D<Double, Boolean> domain = new Function1D<Double, Boolean>() {
       @Override
-      public Boolean evaluate(Double k) {
+      public Boolean evaluate(final Double k) {
         return k >= kL && k <= kH;
       }
     };
 
-    Function1D<Double, Double> dSigmaDx = DIFFERENTIATOR.differentiate(interpFunc, domain);
+    final Function1D<Double, Double> dSigmaDx = DIFFERENTIATOR.differentiate(interpFunc, domain);
 
-    double gradL = dSigmaDx.evaluate(kL);
-    double gradH = dSigmaDx.evaluate(kH);
+    final double gradL = dSigmaDx.evaluate(kL);
+    final double gradH = dSigmaDx.evaluate(kH);
     //
     //    Function1D<DoubleMatrix1D, DoubleMatrix1D> f1 = getDifferenceFunc(forward, kL, expiry, volL, gradL);
     //    Function1D<DoubleMatrix1D, DoubleMatrix1D> f2 = getDifferenceFunc(forward, kH, expiry, volH, gradH);
@@ -90,7 +91,7 @@ public class SmileInterpolatorSpline implements GeneralSmileInterpolator {
 
     return new Function1D<Double, Double>() {
       @Override
-      public Double evaluate(Double k) {
+      public Double evaluate(final Double k) {
         if (k < kL) {
           return ShiftedLogNormalTailExtrapolation.impliedVolatility(forward, k, expiry, res1[0], res1[1]);
         } else if (k > kH) {
--- projects/OG-Financial/src/com/opengamma/financial/analytics/fudgemsg/FittedVolatilitySurfaceBuilder.java
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/fudgemsg/FittedVolatilitySurfaceBuilder.java
@@ -24,28 +24,6 @@ import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurf
   private FittedVolatilitySurfaceBuilder() {
   }
 
-  //  @FudgeBuilderFor(MoneynessPiecewiseSABRSurfaceFitter.class)
-  //  public static final class MoneynessPiecewiseSABRSurfaceFitterBuilder extends AbstractFudgeBuilder<MoneynessPiecewiseSABRSurfaceFitter> {
-  //    private static final String LOG_TIME_FIELD_NAME = "logTimeField";
-  //    private static final String INTEGRATED_VARIANCE_FIELD_NAME = "integratedVarianceField";
-  //    private static final String LOG_VALUE_FIELD_NAME = "logValueField";
-  //
-  //    @Override
-  //    public MoneynessPiecewiseSABRSurfaceFitter buildObject(final FudgeDeserializer deserializer, final FudgeMsg message) {
-  //      final boolean useLogTime = message.getBoolean(LOG_TIME_FIELD_NAME);
-  //      final boolean useIntegratedVariance = message.getBoolean(INTEGRATED_VARIANCE_FIELD_NAME);
-  //      final boolean useLogValue = message.getBoolean(LOG_VALUE_FIELD_NAME);
-  //      return new MoneynessPiecewiseSABRSurfaceFitter(useLogTime, useIntegratedVariance, useLogValue);
-  //    }
-  //
-  //    @Override
-  //    protected void buildMessage(final FudgeSerializer serializer, final MutableFudgeMsg message, final MoneynessPiecewiseSABRSurfaceFitter object) {
-  //      message.add(LOG_TIME_FIELD_NAME, null, object.useLogTime());
-  //      message.add(INTEGRATED_VARIANCE_FIELD_NAME, null, object.useIntegratedVariance());
-  //      message.add(LOG_VALUE_FIELD_NAME, null, object.useLogValue());
-  //    }
-  //  }
-
   @FudgeBuilderFor(VolatilitySurfaceInterpolator.class)
   public static final class VolatilitySurfaceInterpolatorBuilder extends AbstractFudgeBuilder<VolatilitySurfaceInterpolator> {
     private static final String LOG_TIME_FIELD_NAME = "logTimeField";
@@ -75,15 +53,12 @@ import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurf
     private static final String STRIKES_FIELD_NAME = "strikesField";
     private static final String VOLS_FIELD_NAME = "volsField";
 
-    //private static final String IS_CALL_FIELD_NAME = "isCallField";
-
     @Override
     public StandardSmileSurfaceDataBundle buildObject(final FudgeDeserializer deserializer, final FudgeMsg message) {
       final ForwardCurve forwardCurve = deserializer.fieldValueToObject(ForwardCurve.class, message.getByName(FORWARD_CURVE_FIELD_NAME));
       final double[] expiries = deserializer.fieldValueToObject(double[].class, message.getByName(EXPIRIES_FIELD_NAME));
       final double[][] strikes = deserializer.fieldValueToObject(double[][].class, message.getByName(STRIKES_FIELD_NAME));
       final double[][] vols = deserializer.fieldValueToObject(double[][].class, message.getByName(VOLS_FIELD_NAME));
-      //final boolean isCallData = message.getBoolean(IS_CALL_FIELD_NAME);
       return new StandardSmileSurfaceDataBundle(forwardCurve, expiries, strikes, vols, true);
     }
 
@@ -93,7 +68,6 @@ import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurf
       serializer.addToMessage(message, EXPIRIES_FIELD_NAME, null, object.getExpiries());
       serializer.addToMessage(message, STRIKES_FIELD_NAME, null, object.getStrikes());
       serializer.addToMessage(message, VOLS_FIELD_NAME, null, object.getVolatilities());
-      //   serializer.addToMessage(message, IS_CALL_FIELD_NAME, null, object.isCallData());
     }
 
   }
@@ -104,7 +78,6 @@ import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurf
     private static final String EXPIRIES_FIELD_NAME = "expiriesField";
     private static final String STRIKES_FIELD_NAME = "strikesField";
     private static final String VOLS_FIELD_NAME = "volsField";
-    private static final String IS_CALL_FIELD_NAME = "isCallField";
 
     @Override
     public ForexSmileDeltaSurfaceDataBundle buildObject(final FudgeDeserializer deserializer, final FudgeMsg message) {
@@ -112,7 +85,6 @@ import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurf
       final double[] expiries = deserializer.fieldValueToObject(double[].class, message.getByName(EXPIRIES_FIELD_NAME));
       final double[][] strikes = deserializer.fieldValueToObject(double[][].class, message.getByName(STRIKES_FIELD_NAME));
       final double[][] vols = deserializer.fieldValueToObject(double[][].class, message.getByName(VOLS_FIELD_NAME));
-      //      final boolean isCallData = message.getBoolean(IS_CALL_FIELD_NAME);
       return new ForexSmileDeltaSurfaceDataBundle(forwardCurve, expiries, strikes, vols, true);
     }
 
@@ -122,7 +94,6 @@ import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurf
       serializer.addToMessage(message, EXPIRIES_FIELD_NAME, null, object.getExpiries());
       serializer.addToMessage(message, STRIKES_FIELD_NAME, null, object.getStrikes());
       serializer.addToMessage(message, VOLS_FIELD_NAME, null, object.getVolatilities());
-      //  serializer.addToMessage(message, IS_CALL_FIELD_NAME, null, object.isCallData());
     }
 
   }
