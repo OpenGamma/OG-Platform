8a6050782c8d4fca8c0c9b15296cf82305c1aca3
Ana Nelson <ana@ananelson.com>
2012-04-12 05:07:56 +0000

    Add install instructions to cover prerequisites for compiling OG-RStats.

--- projects/OG-Analytics/docs/install/full-setup.sh
+++ projects/OG-Analytics/docs/install/full-setup.sh
@@ -3,16 +3,51 @@ sudo apt-get -y --force-yes upgrade
 
 sudo apt-get install -y --force-yes python-dev
 sudo apt-get install -y --force-yes default-jdk
-sudo apt-get install -y --force-yes ant
 sudo apt-get install -y --force-yes git
 sudo apt-get install -y --force-yes r-base-core
 sudo apt-get install -y --force-yes python-pip
+sudo apt-get install -y --force-yes liblog4cxx10
 
+# install ant
+sudo apt-get install -y --force-yes ant
+# ant-contrib for cpptasks
+wget http://downloads.sourceforge.net/project/ant-contrib/ant-contrib/1.0b3/ant-contrib-1.0b3-bin.zip
+unzip ant-contrib-1.0b3-bin.zip 
+sudo cp ant-contrib/ant-contrib-1.0b3.jar /usr/share/ant/lib/
+
+# texlive
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+tar -xzf install-tl-unx.tar.gz
+cd install-tl-*
+touch texlive.profile # blank texlive.profile forces non-interactive mode
+sudo ./install-tl -profile texlive.profile
+cd ..
+
+# phantom.js
+wget http://phantomjs.googlecode.com/files/phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz
+tar -xf phantomjs-1.5.0-linux-x86_64-dynamic.tar.gz 
+sudo ln -s phantomjs/bin/phantomjs /usr/local/bin/phantomjs
+
+# dexy
 git clone https://github.com/ananelson/dexy
 cd dexy
 sudo pip install -e .
 cd ..
 
+cd /mnt # More room here.
+mkdir work
+cd work
+
+# Install Fudge Messaging 
+sudo apt-get install -y --force-yes libtool
+git clone https://github.com/vrai/Fudge-C.git
+cd Fudge-C
+./reconf
+./configure 
+make && sudo make install
+cd ..
+
+# OpenGamma Platform (finally)
 git clone https://github.com/OpenGamma/OG-Platform.git
 cd OG-Platform
 
@@ -23,3 +58,27 @@ ant init
 ant clone-or-pull
 ant build
 
+# Start OG Server
+cd projects/OG-Examples
+ant new-hsqldb
+ant init-database
+ant jetty
+cd ../..
+
+# Configure OG-RStats
+mkdir -p ~/etc/OpenGammaLtd
+
+echo "jvmLibrary=/usr/lib/jvm/java-6-openjdk/jre/lib/amd64/server/libjvm.so
+jvmProperty.opengamma.configuration.url=http://localhost:8080/jax/configuration/0/
+" > ~/etc/OpenGammaLtd/LanguageIntegration
+
+echo "serviceExecutable=/mnt/work/OG-Platform/projects/OG-RStats/build/Release/ServiceRunner
+connectorLogConfiguration=/mnt/work/OG-Platform/projects/OG-RStats/config/log4cxx.properties
+"> ~/etc/OpenGammaLtd/OpenGammaR
+
+# Install OG-RStats
+cd projects
+git clone https://github.com/OpenGamma/OG-RStats.git
+cd OG-RStats
+ant install
+cd ../..
