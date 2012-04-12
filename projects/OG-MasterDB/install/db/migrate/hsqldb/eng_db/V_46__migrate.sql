1eaf6cac9a10ea708e51f2f841b6704afc9e024d
Kevin Vella <kevin@opengamma.com>
2012-04-12 13:32:06 +0100

    New db schema version for engine, portfolio, position and security to fix field name collisions with SQL Server keywords FUNCTION and KEY. Updating SQL creation and migration scripts for HSQL and Postgres and the relevant field name references in ElSql bundles.
    Conflicts (resolved):
    
    	projects/OG-MasterDB/src/com/opengamma/masterdb/engine/stats/DbFunctionCostsMaster.elsql

---
+++ projects/OG-MasterDB/install/db/migrate/hsqldb/eng_db/V_46__migrate.sql
@@ -0,0 +1,5 @@
+START TRANSACTION;
+  UPDATE eng_schema_version SET version_value='46' WHERE version_key='schema_patch';
+  ALTER  TABLE eng_functioncosts ALTER COLUMN function RENAME TO function_name;
+  ALTER  TABLE eng_functioncosts ADD PRIMARY KEY (configuration, function_name, version_instant);
+COMMIT;
