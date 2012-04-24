b908510bf596d15e43a09a15885c0b73f1a13ff0
Andrew Griffin <andrew@opengamma.com>
2012-04-24 10:38:23 +0100

    Move "firehose" market data feed abstraction into OG-Livedata.

---
+++ projects/OG-LiveData/src/com/opengamma/livedata/firehose/RecordNetworkStream.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+package com.opengamma.livedata.firehose;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Records the content of a network stream to a file.
+ */
+public class RecordNetworkStream {
+
+  public static void main(final String[] args) throws IOException { // CSIGNORE
+    final String host = args[0];
+    final Integer port = Integer.parseInt(args[1]);
+    final String file = args[2];
+    final Socket socket = new Socket(host, port);
+    final BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(file));
+    try {
+      final BufferedInputStream input = new BufferedInputStream(socket.getInputStream());
+      try {
+        final byte[] buffer = new byte[4096];
+        final long start = System.nanoTime();
+        while (System.nanoTime() - start < 300000000000L) {
+          final int bytes = input.read(buffer);
+          if (bytes < 0) {
+            return;
+          }
+          output.write(buffer, 0, bytes);
+        }
+      } finally {
+        input.close();
+      }
+    } finally {
+      output.close();
+    }
+  }
+
+}
