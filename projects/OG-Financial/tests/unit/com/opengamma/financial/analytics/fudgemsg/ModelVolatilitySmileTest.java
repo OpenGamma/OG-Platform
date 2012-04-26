7820a1fb373fda95bae78ced309a7978a5447e8e
Richard White <richard@opengamma.com>
2012-04-26 09:41:59 +0100

    Commit to change branch

--- projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/fudgemsg/ModelVolatilitySmileTest.java
+++ projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/fudgemsg/ModelVolatilitySmileTest.java
@@ -30,9 +30,9 @@ public class ModelVolatilitySmileTest extends AnalyticsTestBase {
   private static final double[] EXPIRIES = new double[] {7. / 365, 14 / 365., 21 / 365., 1 / 12., 3 / 12., 0.5, 0.75, 1, 5, 10 };
   private static final double[] ATM = new double[] {0.17045, 0.1688, 0.167425, 0.1697, 0.1641, 0.1642, 0.1641, 0.1642, 0.138, 0.12515 };
   private static final double[][] RR = new double[][] { {-0.0168, -0.02935, -0.039125, -0.047325, -0.058325, -0.06055, -0.0621, -0.063, -0.032775, -0.023925 },
-    {-0.012025, -0.02015, -0.026, -0.0314, -0.0377, -0.03905, -0.0396, -0.0402, -0.02085, -0.015175 } };
+      {-0.012025, -0.02015, -0.026, -0.0314, -0.0377, -0.03905, -0.0396, -0.0402, -0.02085, -0.015175 } };
   private static final double[][] BUTT = new double[][] { {0.00665, 0.00725, 0.00835, 0.009075, 0.013175, 0.01505, 0.01565, 0.0163, 0.009275, 0.007075, },
-    {0.002725, 0.00335, 0.0038, 0.004, 0.0056, 0.0061, 0.00615, 0.00635, 0.00385, 0.002575 } };
+      {0.002725, 0.00335, 0.0038, 0.004, 0.0056, 0.0061, 0.00615, 0.00635, 0.00385, 0.002575 } };
   private static final double[] DELTAS = new double[] {0.15, 0.25 };
   private static final double[] FORWARDS = new double[] {1.34, 1.35, 1.36, 1.38, 1.4, 1.43, 1.45, 1.48, 1.5, 1.52 };
   private static final CombinedInterpolatorExtrapolator EXTRAPOLATOR_1D =
