3570874e14d260d472e502238708fc2081298871
Elaine McLeod <elaine@opengamma.com>
2012-05-01 18:23:37 +0100

    Adding objects that contain information about how yield curves are to be calculated in a multi-curve setup

---
+++ projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/ircurve/calcconfig/CurveInstrumentConfigTest.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.ircurve.calcconfig;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import com.opengamma.financial.analytics.ircurve.StripInstrumentType;
+import com.opengamma.financial.fudgemsg.FinancialTestBase;
+
+/**
+ * 
+ */
+public class CurveInstrumentConfigTest extends FinancialTestBase {
+  private static final String FUNDING_CURVE_NAME = "Funding";
+  private static final String FORWARD_3M_CURVE_NAME = "Forward3M";
+  private static final CurveInstrumentConfig FUNDING_CONFIG;
+  private static final CurveInstrumentConfig FORWARD_CONFIG;
+
+  static {
+    final String[] fundingOnly = new String[] {FUNDING_CURVE_NAME};
+    final String[] forward3MOnly = new String[] {FORWARD_3M_CURVE_NAME};
+    final String[] fundingForward3M = new String[] {FUNDING_CURVE_NAME, FORWARD_3M_CURVE_NAME};
+    final Map<StripInstrumentType, String[]> fundingConfig = new HashMap<StripInstrumentType, String[]>();
+    fundingConfig.put(StripInstrumentType.CASH, fundingOnly);
+    fundingConfig.put(StripInstrumentType.OIS_SWAP, new String[] {FUNDING_CURVE_NAME, FUNDING_CURVE_NAME});
+    final Map<StripInstrumentType, String[]> forward3MConfig = new HashMap<StripInstrumentType, String[]>();
+    forward3MConfig.put(StripInstrumentType.LIBOR, forward3MOnly);
+    forward3MConfig.put(StripInstrumentType.FUTURE, fundingForward3M);
+    forward3MConfig.put(StripInstrumentType.FRA_3M, fundingForward3M);
+    forward3MConfig.put(StripInstrumentType.SWAP_6M, fundingForward3M);
+    FUNDING_CONFIG = new CurveInstrumentConfig(fundingConfig);
+    FORWARD_CONFIG = new CurveInstrumentConfig(forward3MConfig);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testWrongStrip() {
+    FUNDING_CONFIG.getExposuresForInstrument(StripInstrumentType.BANKERS_ACCEPTANCE);
+  }
+
+  @Test
+  public void test() {
+    final Map<StripInstrumentType, String[]> fundingConfig = new HashMap<StripInstrumentType, String[]>();
+    fundingConfig.put(StripInstrumentType.CASH, new String[] {FUNDING_CURVE_NAME} );
+    fundingConfig.put(StripInstrumentType.OIS_SWAP, new String[] {FUNDING_CURVE_NAME, FUNDING_CURVE_NAME});
+    final CurveInstrumentConfig config = new CurveInstrumentConfig(fundingConfig);
+    assertFalse(FUNDING_CONFIG.equals(FORWARD_CONFIG));
+    assertEquals(FUNDING_CONFIG, config);
+  }
+
+  @Test
+  public void testBuilder() {
+    assertEquals(FUNDING_CONFIG, cycleObject(CurveInstrumentConfig.class, FUNDING_CONFIG));
+    assertEquals(FORWARD_CONFIG, cycleObject(CurveInstrumentConfig.class, FORWARD_CONFIG));
+  }
+}
--- projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/ircurve/calcconfig/MultiCurveCalculationConfigTest.java
+++ projects/OG-Financial/tests/unit/com/opengamma/financial/analytics/ircurve/calcconfig/MultiCurveCalculationConfigTest.java
@@ -8,6 +8,9 @@ package com.opengamma.financial.analytics.ircurve.calcconfig;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.annotations.Test;
 import org.testng.internal.junit.ArrayAsserts;
 
@@ -31,12 +34,20 @@ public class MultiCurveCalculationConfigTest extends FinancialTestBase {
   private static final String[] DEFAULT_INR_YIELD_CURVE_NAMES = new String[] {"FUNDING"};
   private static final UniqueIdentifiable[] DEFAULT_INR_IDS = new UniqueIdentifiable[] {Currency.of("INR")};
   private static final String DEFAULT_INR_CALCULATION_METHOD = "FXImplied";
-  private static final MultiCurveCalculationConfig DEFAULT_USD_CONFIG = new MultiCurveCalculationConfig(DEFAULT_USD_CONFIG_NAME, DEFAULT_USD_YIELD_CURVE_NAMES, DEFAULT_USD_IDS,
-      DEFAULT_USD_CALCULATION_METHODS);
-  private static final MultiCurveCalculationConfig EXTRA_USD_CONFIG = new MultiCurveCalculationConfig(EXTRA_USD_CONFIG_NAME, EXTRA_USD_YIELD_CURVE_NAMES, EXTRA_USD_IDS, EXTRA_USD_CALCULATION_METHODS,
-      new String[] {DEFAULT_USD_CONFIG_NAME});
-  private static final MultiCurveCalculationConfig DEFAULT_INR_CONFIG = new MultiCurveCalculationConfig(DEFAULT_INR_CONFIG_NAME, DEFAULT_INR_YIELD_CURVE_NAMES, DEFAULT_INR_IDS, DEFAULT_INR_CALCULATION_METHOD,
-      new String[] {DEFAULT_USD_CONFIG_NAME});
+  private static final MultiCurveCalculationConfig DEFAULT_USD_CONFIG;
+  private static final MultiCurveCalculationConfig EXTRA_USD_CONFIG;
+  private static final MultiCurveCalculationConfig DEFAULT_INR_CONFIG;
+  private static final Map<String, CurveInstrumentConfig> CURVE_EXPOSURES;
+
+  static {
+    CURVE_EXPOSURES = new HashMap<String, CurveInstrumentConfig>();
+    DEFAULT_USD_CONFIG = new MultiCurveCalculationConfig(DEFAULT_USD_CONFIG_NAME, DEFAULT_USD_YIELD_CURVE_NAMES, DEFAULT_USD_IDS,
+        DEFAULT_USD_CALCULATION_METHODS, CURVE_EXPOSURES);
+    EXTRA_USD_CONFIG = new MultiCurveCalculationConfig(EXTRA_USD_CONFIG_NAME, EXTRA_USD_YIELD_CURVE_NAMES, EXTRA_USD_IDS, EXTRA_USD_CALCULATION_METHODS,
+        CURVE_EXPOSURES, new String[] {DEFAULT_USD_CONFIG_NAME});
+    DEFAULT_INR_CONFIG = new MultiCurveCalculationConfig(DEFAULT_INR_CONFIG_NAME, DEFAULT_INR_YIELD_CURVE_NAMES, DEFAULT_INR_IDS, DEFAULT_INR_CALCULATION_METHOD,
+        CURVE_EXPOSURES, new String[] {DEFAULT_USD_CONFIG_NAME});
+  }
 
   @Test
   public void test() {
@@ -46,7 +57,7 @@ public class MultiCurveCalculationConfigTest extends FinancialTestBase {
     final String[] curveNames = new String[] {"FORWARD_6M", "FORWARD_12M", "FORWARD_1M"};
     final UniqueIdentifiable[] ids = new UniqueIdentifiable[] {Currency.USD, Currency.USD, Currency.USD};
     final String[] methods = new String[] {"ParRate", "ParRate", "ParRate"};
-    final MultiCurveCalculationConfig config = new MultiCurveCalculationConfig(name, curveNames, ids, methods, new String[] {DEFAULT_USD_CONFIG_NAME});
+    final MultiCurveCalculationConfig config = new MultiCurveCalculationConfig(name, curveNames, ids, methods, CURVE_EXPOSURES, new String[] {DEFAULT_USD_CONFIG_NAME});
     assertEquals(EXTRA_USD_CONFIG, config);
     ArrayAsserts.assertArrayEquals(config.getCalculationMethods(), methods);
     assertEquals(config.getCalculationConfigName(), name);
