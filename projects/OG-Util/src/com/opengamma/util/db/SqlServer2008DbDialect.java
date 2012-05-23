3043d50c9788450c9d7906ac812268f194e1a819
Kevin Vella <kevin@opengamma.com>
2012-05-23 17:24:16 +0100

    Dynamic class loading for SQL Server JDBC driver to enable build when driver is not available.

--- projects/OG-Util/src/com/opengamma/util/db/SqlServer2008DbDialect.java
+++ projects/OG-Util/src/com/opengamma/util/db/SqlServer2008DbDialect.java
@@ -33,10 +33,16 @@ public class SqlServer2008DbDialect extends DbDialect {
   }
 
   //-------------------------------------------------------------------------
+  @SuppressWarnings("unchecked")
   @Override
   public Class<? extends Driver> getJDBCDriverClass() {
+    try {
+      return (Class<? extends Driver>) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    } catch (ClassNotFoundException ex) {
+      throw new OpenGammaRuntimeException("Could not load the Microsoft JDBC driver: " + ex.getMessage());
+    }
     // Use the MS driver...
-    return com.microsoft.sqlserver.jdbc.SQLServerDriver.class;
+    // return com.microsoft.sqlserver.jdbc.SQLServerDriver.class;
     // ...or the open-source driver (LGPLed)
     // return net.sourceforge.jtds.jdbc.Driver.class;
   }
