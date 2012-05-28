c13434692e41a3e62b87a54825fd996456d77790
Casey Clements <casey@opengamma.com>
2012-05-28 13:41:10 +0100

    New VolSurface InstrumentProvider for EquityFutureOptions and refactor of IR FUtureOptions to match

---
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/model/FutureOptionExpiries.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.model;
+
+import javax.time.calendar.DateAdjuster;
+import javax.time.calendar.DateAdjusters;
+import javax.time.calendar.DayOfWeek;
+import javax.time.calendar.LocalDate;
+
+import org.apache.commons.lang.Validate;
+
+import com.opengamma.analytics.util.time.TimeCalculator;
+import com.opengamma.financial.analytics.ircurve.NextExpiryAdjuster;
+import com.opengamma.financial.analytics.model.equity.NextEquityExpiryAdjuster;
+import com.opengamma.financial.analytics.model.equity.SaturdayAfterThirdFridayAdjuster;
+
+/**
+ *  Utility Class for computing Expiries of Future Options from ordinals (i.e. nth future after valuationDate)
+ *  For IR Options use: DateAdjusters.dayOfWeekInMonth(3, DayOfWeek.WEDNESDAY), new NextExpiryAdjuster()
+ *  For Equity Options use: new SaturdayAfterThirdFridayAdjuster(), new NextEquityExpiryAdjuster()
+ */
+public final class FutureOptionExpiries {
+
+  /** Instance of {@code FutureOptionExpiries} used for Interest Rate Future Options. (Expiries on 3rd Wednesdays) */
+  public static final FutureOptionExpiries IR = FutureOptionExpiries.of(DateAdjusters.dayOfWeekInMonth(3, DayOfWeek.WEDNESDAY), new NextExpiryAdjuster());
+  /** Instance of {@code FutureOptionExpiries} used for Equity Future Options. (Expiries on Saturdays after 3rd Fridays) */
+  public static final FutureOptionExpiries EQUITY = FutureOptionExpiries.of(new SaturdayAfterThirdFridayAdjuster(), new NextEquityExpiryAdjuster());
+  
+  /** Adjuster moves date to day within month. eg 3rd Wednesday */
+  private final DateAdjuster _dayOfMonthAdjuster;
+  private final DateAdjuster _nextExpiryAdjuster;
+  
+  /**
+   * Utility Class for computing Expiries of Future Options from ordinals (i.e. nth future after valuationDate)
+   * @param dayOfMonthAdjuster Examples: DateAdjusters.dayOfWeekInMonth(), SaturdayAfterThirdFridayAdjuster
+   * @param nextExpiryAdjuster Examples: NextExpiryAdjuster, NextExpiryAdjuster
+   */
+  private FutureOptionExpiries(final DateAdjuster dayOfMonthAdjuster, final DateAdjuster nextExpiryAdjuster) {
+    Validate.notNull(dayOfMonthAdjuster, "dayOfMonthAdjuster was null. Example: DateAdjusters.dayOfWeekInMonth(3, DayOfWeek.WEDNESDAY)");
+    Validate.notNull(nextExpiryAdjuster, "nextExpiryAdjuster was null. Example: NextExpiryAdjuster");
+    _dayOfMonthAdjuster = dayOfMonthAdjuster;
+    _nextExpiryAdjuster = nextExpiryAdjuster;   
+  }
+
+  /**
+   * Create instance of {@code FutureOptionExpiries}, 
+   * a utility Class for computing Expiries of Future Options from ordinals (i.e. nth option after valuationDate)
+   * @param dayOfMonthAdjuster Examples: DateAdjusters.dayOfWeekInMonth(), SaturdayAfterThirdFridayAdjuster
+   * @param nextExpiryAdjuster Examples: NextExpiryAdjuster, NextExpiryAdjuster
+   * @return the FutureOptionExpiries class, never null
+   */
+  public static FutureOptionExpiries of(final DateAdjuster dayOfMonthAdjuster, final DateAdjuster nextExpiryAdjuster) {
+    Validate.notNull(dayOfMonthAdjuster, "dayOfMonthAdjuster was null. Example: DateAdjusters.dayOfWeekInMonth(3, DayOfWeek.WEDNESDAY)");
+    Validate.notNull(nextExpiryAdjuster, "nextExpiryAdjuster was null. Example: NextExpiryAdjuster");
+    return new FutureOptionExpiries(dayOfMonthAdjuster, nextExpiryAdjuster);
+  }
+  
+  /**
+   * Compute time between now and future or future option's settlement date, 
+   * typically two business days before the third wednesday of the expiry month. 
+   * @param n nth Future after now
+   * @param today Valuation Date
+   * @return OG-Analytic Time in years between now and the future's settlement date
+   */
+  public Double getFutureOptionTtm(final int n, final LocalDate today) {
+    final LocalDate expiry = getFutureOptionExpiry(n, today);
+    final LocalDate previousMonday = expiry.minusDays(2); //TODO this should take a calendar and do two business days, and should use a convention for the number of days
+    return TimeCalculator.getTimeBetween(today, previousMonday);
+  }
+ 
+  /**
+   * Compute time between now and future or future option's settlement date, 
+   * typically two business days before the third wednesday of the expiry month. 
+   * @param n nth Future after now
+   * @param today Valuation Date
+   * @return OG-Analytic Time in years between now and the future's settlement date
+   */
+  public Double getFutureTtm(final int n, final LocalDate today) {
+    final LocalDate expiry = getQuarterlyExpiry(n, today);
+    final LocalDate previousMonday = expiry.minusDays(2); //TODO this should take a calendar and do two business days, and should use a convention for the number of days
+    return TimeCalculator.getTimeBetween(today, previousMonday);
+  }
+  
+  public LocalDate getFutureOptionExpiry(final int nthFuture, final LocalDate valDate) {
+    Validate.isTrue(nthFuture > 0, "nthFuture must be greater than 0.");
+    if (nthFuture <= 6) { // We look for expiries in the first 6 serial months after curveDate
+      return getMonthlyExpiry(nthFuture, valDate);
+    } else {  // And for Quarterly expiries thereafter
+      final int nthExpiryAfterSixMonths = nthFuture - 6;
+      final LocalDate sixMonthsForward = valDate.plusMonths(6);
+      return getQuarterlyExpiry(nthExpiryAfterSixMonths, sixMonthsForward);
+    }
+  }
+ 
+  public LocalDate getMonthlyExpiry(final int nthMonth, final LocalDate valDate) {
+    Validate.isTrue(nthMonth > 0, "nthFuture must be greater than 0.");
+    LocalDate expiry = valDate.with(_dayOfMonthAdjuster); // Compute the expiry of valuationDate's month
+    if (!expiry.isAfter(valDate)) { // If it is not strictly after valuationDate...
+      expiry = (valDate.plusMonths(1)).with(_dayOfMonthAdjuster);  // nextExpiry is third Wednesday of next month
+    }
+    if (nthMonth > 1) { 
+      expiry = (expiry.plusMonths(nthMonth - 1)).with(_dayOfMonthAdjuster);
+    }
+    return expiry;
+  }
+   
+   
+  public LocalDate getQuarterlyExpiry(final int nthFuture, final LocalDate valDate) {
+    Validate.isTrue(nthFuture > 0, "nthFuture must be greater than 0.");
+    LocalDate expiry = valDate.with(_nextExpiryAdjuster);
+    for (int i = 1; i < nthFuture; i++) {
+      expiry = (expiry.plusDays(7)).with(_nextExpiryAdjuster);
+    }
+    return expiry;
+  }
+}
+
+
---
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/volatility/surface/BloombergFutureOptionVolatilitySurfaceInstrumentProvider.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.volatility.surface;
+
+import javax.time.calendar.LocalDate;
+
+import org.apache.commons.lang.Validate;
+
+import com.opengamma.OpenGammaRuntimeException;
+import com.opengamma.id.ExternalId;
+
+/**
+ * Provides ExternalId's for FutureOptions used to build the Volatility Surface  
+ */
+public abstract class BloombergFutureOptionVolatilitySurfaceInstrumentProvider implements CallPutSurfaceInstrumentProvider<Number, Double> {
+  
+  private final String _futureOptionPrefix;
+  private final String _postfix;
+  private final String _dataFieldName; 
+  private final Double _useCallAboveStrike;
+
+  /**
+   * @param futureOptionPrefix the prefix to the resulting code
+   * @param postfix the postfix to the resulting code
+   * @param dataFieldName the name of the data field. Expecting MarketDataRequirementNames.IMPLIED_VOLATILITY or OPT_IMPLIED_VOLATILITY_MID
+   * @param useCallAboveStrike the strike above which to use calls rather than puts
+   */
+  protected BloombergFutureOptionVolatilitySurfaceInstrumentProvider(final String futureOptionPrefix, final String postfix, final String dataFieldName, final Double useCallAboveStrike) {
+    Validate.notNull(futureOptionPrefix, "future option prefix");
+    Validate.notNull(postfix, "postfix");
+    Validate.notNull(dataFieldName, "data field name");
+    Validate.notNull(useCallAboveStrike, "use call above this strike");
+    _futureOptionPrefix = futureOptionPrefix;
+    _postfix = postfix;
+    _dataFieldName = dataFieldName;
+    _useCallAboveStrike = useCallAboveStrike;
+  }
+
+  @Override
+  /** 
+   * Primary method of class builds up a single Bloomberg ticker 
+   * @return Ticker ID
+   * @param futureOptionNumber n'th future after surfaceDate
+   * @param strike absolute value of strike
+   * @param surfaceDate valuation date
+   */
+  public abstract ExternalId getInstrument(final Number futureOptionNumber, final Double strike, final LocalDate surfaceDate);
+  
+  @Override
+  public ExternalId getInstrument(final Number futureOptionNumber, final Double strike) {
+    throw new OpenGammaRuntimeException("Need a surface date to create an interest rate future option surface");
+  }
+  
+  public String getFutureOptionPrefix() {
+    return _futureOptionPrefix;
+  }
+
+  public String getPostfix() {
+    return _postfix;
+  }
+
+  @Override
+  public Double useCallAboveStrike() {
+    return _useCallAboveStrike;
+  }
+
+  @Override
+  public String getDataFieldName() {
+    return _dataFieldName;
+  }
+
+  @Override
+  public int hashCode() {
+    return getFutureOptionPrefix().hashCode() + getPostfix().hashCode() + getDataFieldName().hashCode() + useCallAboveStrike().hashCode();
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof BloombergFutureOptionVolatilitySurfaceInstrumentProvider)) {
+      return false;
+    }
+    final BloombergFutureOptionVolatilitySurfaceInstrumentProvider other = (BloombergFutureOptionVolatilitySurfaceInstrumentProvider) obj;
+    return getFutureOptionPrefix().equals(other.getFutureOptionPrefix()) &&
+        getPostfix().equals(other.getPostfix()) &&
+        useCallAboveStrike().equals(other.useCallAboveStrike()) &&
+        getDataFieldName().equals(other.getDataFieldName());
+  }
+}
