94f0bc9ba6b623d69690efb615c18d3a971c93a6
Alan Ayoub <alan@opengamma.com>
2012-06-07 17:45:12 +0100

    update layout manager and turn on state management

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.manager.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.manager.js
@@ -6,10 +6,8 @@ $.register_module({
     name: 'og.common.gadgets.manager',
     dependencies: [],
     obj: function () {
-        var manager, collector, gadgets = [], interval = 5 * 60 * 1000, layout, first = true,
-            resize = function () {
-                return layout && first ? first = false : manager.clean().forEach(function (gadget) {gadget.resize();});
-            };
+        var manager, collector, gadgets = [], interval = 5 * 60 * 1000, layout,
+            resize = function () {return layout && manager.clean().forEach(function (gadget) {gadget.resize();});};
         setTimeout((collector = function () {return manager.clean(), setTimeout(collector, interval);}), interval);
         $(function () {if (!(layout = !!og.views.common.layout)) $(window).on('resize', manager.resize);});
         return manager = {
