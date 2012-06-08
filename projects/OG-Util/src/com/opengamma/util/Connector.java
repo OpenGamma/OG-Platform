9e297f86fee543edbfb65f2becf16db4eb1661b4
Stephen Colebourne <stephen@opengamma.com>
2012-06-08 11:46:44 +0100

    [PLAT-2299] Avoid hard-coding connector types

---
+++ projects/OG-Util/src/com/opengamma/util/Connector.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.util;
+
+import java.io.Closeable;
+
+/**
+ * Connector used to access a resource, such as a database.
+ * <p>
+ * See the implementation classes for more information.
+ * <p>
+ * This interface is essentially a marker that allows connectors to be identified.
+ */
+public interface Connector extends Closeable {
+
+  /**
+   * Gets the display name of the connector.
+   * 
+   * @return a name usable for display, not null
+   */
+  String getName();
+
+  /**
+   * Gets the type of the connector.
+   * <p>
+   * This returns the type that should be used to manage the connector.
+   * For example, consider {@code BarConnector} and its subclass {@code FooBarConnector}.
+   * When this method is called on either type, it should return {@code BarConnector}, as that
+   * is the type under which the connector should be managed.
+   * 
+   * @return a name usable for display, not null
+   */
+  Class<? extends Connector> getType();
+
+}
