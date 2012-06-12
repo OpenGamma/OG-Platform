5997cafd842cbf191ecb4722c8f9ba722f911f8a
Daniel Kwiecinski <daniel@opengamma.com>
2012-06-12 16:45:40 +0100

    fixing 48 migration

--- projects/OG-MasterDB/install/db/migrate/hsqldb/sec_db/V_48__migrate.sql
+++ projects/OG-MasterDB/install/db/migrate/hsqldb/sec_db/V_48__migrate.sql
@@ -9,22 +9,109 @@ START TRANSACTION;
       PRIMARY KEY (id)
   );
   
-  INSERT INTO sec_contract_category (ID, NAME)
-  SELECT sec_bondfuturetype.id, sec_bondfuturetype.name 
-  FROM sec_future
-  INNER JOIN sec_bondfuturetype ON sec_future.bondtype_id = sec_bondfuturetype.id;
-  
-  INSERT INTO sec_contract_category (ID, NAME)
-  SELECT sec_commodityfuturetype.id, sec_commodityfuturetype.name 
-  FROM sec_future
-  INNER JOIN sec_commodityfuturetype ON sec_future.commoditytype_id = sec_commodityfuturetype.id;
-    
-  
   ALTER TABLE sec_future ADD COLUMN contract_category_id bigint; -- most of the current future has no category defined so the column needs to stay nullable
   ALTER TABLE sec_future ADD CONSTRAINT sec_fk_future2contract_category FOREIGN KEY (contract_category_id) REFERENCES sec_contract_category (id);
- 
-  UPDATE sec_future SET contract_category_id = bondtype_id WHERE bondtype_id is not null;
-  UPDATE sec_future SET contract_category_id = commoditytype_id WHERE commoditytype_id is not null;  
+  
+  INSERT INTO sec_contract_category (ID, NAME)
+  VALUES 
+    (call next value for sec_security_seq, 'Precious Matal'),
+    (call next value for sec_security_seq, 'Crude Oil'),
+    (call next value for sec_security_seq, 'Natural Gas'),
+    (call next value for sec_security_seq, 'Wheat'),
+    (call next value for sec_security_seq, 'Livestock'),
+    (call next value for sec_security_seq, 'Equity Index'),
+    (call next value for sec_security_seq, 'Interest Rate'),
+    (call next value for sec_security_seq, 'Bond'),
+    (call next value for sec_security_seq, 'Cross Currency'),
+    (call next value for sec_security_seq, 'STOCK FUTURE');
+              
+  -- setting contract categories from commodity type                    
+                      
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_commodityfuturetype T, sec_contract_category C
+  WHERE
+    C.name = 'Precious Matal'
+  AND   
+    T.id = sec_future.commoditytype_id AND T.name = 'Precious Metal'; 
+    
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_commodityfuturetype T, sec_contract_category C
+  WHERE
+    C.name = 'Crude Oil'
+  AND   
+    T.id = sec_future.commoditytype_id AND T.name = 'Crude Oil'; 
+    
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_commodityfuturetype T, sec_contract_category C
+  WHERE
+    C.name = 'Natural Gas'
+  AND   
+    T.id = sec_future.commoditytype_id AND T.name = 'Natural Gas';
+    
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_commodityfuturetype T, sec_contract_category C
+  WHERE
+    C.name = 'Wheat'
+  AND   
+    T.id = sec_future.commoditytype_id AND T.name = 'Wheat';  
+  
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_commodityfuturetype T, sec_contract_category C
+  WHERE
+    C.name = 'Livestock'
+  AND   
+    T.id = sec_future.commoditytype_id AND T.name = 'Livestock';  
+          
+  -- setting contract categories from future type
+    
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_contract_category C
+  WHERE
+    C.name = 'Equity Index'
+  AND   
+    sec_future.future_type = 'Index';
+  
+  
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_contract_category C
+  WHERE
+    C.name = 'Equity Index'
+  AND   
+    sec_future.future_type = 'Equity Index Dividend';
+  
+  
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_contract_category C
+  WHERE
+    C.name = 'Interest Rate'
+  AND   
+    sec_future.future_type = 'Interest Rate';
+  
+  
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_contract_category C
+  WHERE
+    C.name = 'Bond'
+  AND   
+    sec_future.future_type = 'Bond';
+  
+  
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_contract_category C
+  WHERE
+    C.name = 'STOCK FUTURE'
+  AND   
+    sec_future.future_type = 'Equity';
+  
+  
+  UPDATE sec_future SET contract_category_id = C.id
+  FROM sec_contract_category C
+  WHERE
+    C.name = 'Cross Currency'
+  AND   
+    sec_future.future_type = 'FX';
+  
+  --------------------------------------------------------------                     
   
   ALTER TABLE sec_future DROP CONSTRAINT sec_fk_future2bondfuturetype;
   ALTER TABLE sec_future DROP CONSTRAINT sec_fk_future2commodityfuturetype;
