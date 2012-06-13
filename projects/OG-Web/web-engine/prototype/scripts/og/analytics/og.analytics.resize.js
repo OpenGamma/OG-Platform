aa6905f8a6aec446dac96ec6fa0282154c91718c
Afshin Darian <afshin@opengamma.com>
2012-06-13 18:33:09 +0100

    more context menu fun, IE8 edition

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.resize.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.resize.js
@@ -6,28 +6,23 @@ $.register_module({
     name: 'og.analytics.resize',
     dependencies: ['og.views.common.layout', 'og.common.gadgets.manager'],
     obj: function () {
+        var resizer_html = '<div class="OG-analytics-resize og-resizer" title="Drag (resize) / Right click (menu)" />';
         return function () {
-            var $main = $('.OG-layout-analytics-center'), $menu, block_menu = false,
-                $resizer = $('<div class="OG-analytics-resize og-resizer" ' +
-                    'title="Drag (resize) / Right click (menu)" />'),
+            var $main = $('.OG-layout-analytics-center'), $menu, block_menu = false, $resizer = $(resizer_html),
                 layout = og.views.common.layout, icon_size = 16, offset = $main.offset();
             var left_handler = function () {
                 var $bars = $('<div class="OG-analytics-resize og-bars"></div>'),
                     $overlay = $('<div class="OG-analytics-resize og-overlay"></div>'),
                     offset = $main.offset(), right, bottom;
                 if ($menu) $menu.remove();
-                $bars.css({
-                    'width': $main.width(), 'height': $main.height() + 1,
-                    'top': offset.top, 'left': offset.left + 1
-                });
+                $bars.css({width: $main.width(), height: $main.height() + 1, top: offset.top, left: offset.left + 1});
                 $overlay.on('mousemove', function (event) {
                     $bars.css({
                         width: 'auto', height: 'auto',
                         right: right = $(document).outerWidth() - event.pageX,
                         bottom: bottom = $(document).outerHeight() - event.pageY
                     });
-                });
-                $overlay.on('mouseup', function () {
+                }).on('mouseup', function () {
                     if (!bottom || !right) return;
                     layout.inner.sizePane('south', bottom - layout.main.south.options.size);
                     layout.main.sizePane('east', right);
@@ -46,28 +41,23 @@ $.register_module({
                 right('north', '33%');
                 right('south', '33%');
             };
+            var mousedown_handler = function (event) {
+                return event.which !== 3 || event.button !== 2 ? left_handler()
+                    : event.stopPropagation(), (block_menu = true), right_handler();
+            };
             var resize = function () {
                 $resizer.css({
                     left: offset.left + $main.width() - icon_size, top: offset.top + $main.height() - icon_size
                 });
             };
-            var mousedown_handler = function (event) {
-                if (event.which === 3 || event.button === 2) {
-                    event.stopPropagation();
-                    block_menu = true;
-                    return right_handler();
-                };
-                return left_handler();
-            };
             var right_handler = function () {
                 $.when(og.api.text({module: 'og.analytics.resize_menu_tash'})).then(function (template) {
-                    $menu = $(template)
-                        .position({my: 'left top', at: 'right bottom', of: $resizer})
+                    $menu = $(template).position({my: 'left top', at: 'right bottom', of: $resizer})
                         .on('mousedown', 'div', function () {
                             // extract preset number from class and load
                             load_preset(+$(this).find('> span').attr('class').replace(/^(?:.*)([1-3])$/, '$1'));
-                        })
-                        .appendTo('body');
+                            $menu.remove(); // it should already be gone, but just in case you are IE8
+                        }).appendTo('body');
                     $(document).on('mouseup.og_analytics_resize', function () {
                         $(document).off('mouseup.og_analytics_resize');
                         setTimeout(function () {
