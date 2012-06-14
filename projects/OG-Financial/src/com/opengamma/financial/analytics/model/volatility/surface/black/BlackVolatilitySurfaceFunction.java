ad2d66d45a4858f11dd383ea699f303fdbbc295b
Casey Clements <casey@opengamma.com>
2012-06-14 19:34:17 +0100

    uncommitting what i thought i had done a checkout on but must have been committed

--- projects/OG-Financial/src/com/opengamma/financial/analytics/model/volatility/surface/black/BlackVolatilitySurfaceFunction.java
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/model/volatility/surface/black/BlackVolatilitySurfaceFunction.java
@@ -48,7 +48,7 @@ public abstract class BlackVolatilitySurfaceFunction extends AbstractFunction.No
     final SmileSurfaceDataBundle data = getData(inputs, getVolatilityDataRequirement(target, surfaceName, getInstrumentType(), getSurfaceQuoteType(), getSurfaceQuoteUnits()),
         getForwardCurveRequirement(target, desiredValue));
     
-    final BlackVolatilitySurfaceMoneyness impliedVolatilitySurface = surfaceInterpolator.getVolatilitySurface(data); // !!!!!!!!!! YOU DIRTY RASCAL
+    final BlackVolatilitySurfaceMoneyness impliedVolatilitySurface = surfaceInterpolator.getVolatilitySurface(data);
     final ValueProperties properties = getResultProperties(desiredValue);
     final ValueSpecification spec = new ValueSpecification(ValueRequirementNames.BLACK_VOLATILITY_SURFACE, target.toSpecification(), properties);
     return Collections.singleton(new ComputedValue(spec, impliedVolatilitySurface));
