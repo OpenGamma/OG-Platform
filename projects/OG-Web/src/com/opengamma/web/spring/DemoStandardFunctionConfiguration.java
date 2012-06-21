5873ac4536a7b03900bfc8cf277065db19e746cf
Casey Clements <casey@opengamma.com>
2012-06-21 12:51:01 +0100

    Updated IRFutures Defaults

--- projects/OG-Web/src/com/opengamma/web/spring/DemoStandardFunctionConfiguration.java
+++ projects/OG-Web/src/com/opengamma/web/spring/DemoStandardFunctionConfiguration.java
@@ -781,7 +781,7 @@ public class DemoStandardFunctionConfiguration extends SingletonFactoryBean<Repo
     functionConfigs.add(functionConfiguration(InterestRateFuturePresentValueFunction.class));
     functionConfigs.add(functionConfiguration(InterestRateFuturePV01Function.class));
     functionConfigs.add(functionConfiguration(InterestRateFutureYieldCurveNodeSensitivitiesFunction.class));
-    functionConfigs.add(functionConfiguration(InterestRateFutureDefaultValuesFunction.class, "FORWARD_3M", "FUNDING", "PresentValue", "USD", "EUR"));
+    functionConfigs.add(functionConfiguration(InterestRateFutureDefaultValuesFunction.class, "FUTURES", "FUNDING", "PresentValue", "USD", "EUR", "GBP", "AUD"));
   }
 
   private static void addInterestRateFutureOptionCalculators(final List<FunctionConfiguration> functionConfigs) {
