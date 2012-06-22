93404614fa6a75765187cfaf619630d04a290f3e
Alan Ayoub <alan@opengamma.com>
2012-06-22 15:38:28 +0100

    add basic functionality to play/pause toggle, and style

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.js
@@ -30,6 +30,33 @@ $.register_module({
                     };
                 });
             };
+            var Status = function (selector) {
+                var status = this, interval, init = false;
+                $(selector + ' button').on('click', function () {
+                    if (!status.status || status.status === 'paused') return status.play();
+                    if (status.status === 'playing') return status.pause();
+                });
+                status.pause = function () {
+                    $(selector + ' em').html('paused').removeClass('live').addClass('paused');
+                    $(selector + ' button').removeClass('og-icon-play').addClass('og-icon-pause');
+                    status.message('');
+                    clearInterval(interval);
+                    status.status = 'paused';
+                };
+                status.play = function () {
+                    //if (!init) init = !!$(selector + ' button').removeClass('og-disabled');
+                    $(selector + ' em').html('live').removeClass('paused').addClass('live');
+                    $(selector + ' button').removeClass('og-icon-pause').addClass('og-icon-play');
+                    status.message('starting...');
+                    interval = setInterval(function () {
+                        status.message('updated ' + (Math.random() + 1).toFixed(2) + ' seconds ago');
+                    }, 1000);
+                    status.status = 'playing';
+                };
+                status.message = function (message) {$(selector + ' .og-message').html(message);};
+                status.status = null;
+                return status;
+            };
             $.when(
                 og.api.text({module: 'og.analytics.form_tash'}),
                 og.api.rest.configs.get({handler: function (r) {return r}})
@@ -122,6 +149,7 @@ $.register_module({
                 var datasources_menu = new FormCombo(
                     selector + ' .og-datasources', 'og.analytics.form_datasources_tash', response.datasources
                 );
+                new Status(selector + ' .og-status'); // .play()
                 auto_combo_menu.select();
                 og.views.common.layout.main.allowOverflow('north');
             });
