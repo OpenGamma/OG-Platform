2dbf1e88dda77e8954a85d0dee809450162c9ed2
Casey Clements <casey@opengamma.com>
2012-06-26 20:41:48 +0100

    TimeCalculator: Added forms for combos of zonedDateTime nd LocalDate

--- projects/OG-Analytics/src/com/opengamma/analytics/util/time/TimeCalculator.java
+++ projects/OG-Analytics/src/com/opengamma/analytics/util/time/TimeCalculator.java
@@ -23,12 +23,11 @@ public abstract class TimeCalculator {
   public static double getTimeBetween(final ZonedDateTime date1, final ZonedDateTime date2) {
     ArgumentChecker.notNull(date1, "date1");
     ArgumentChecker.notNull(date1, "date2");
-    
+
     // Note that here we convert date2 to the same zone as date1 so we don't accidentally gain or lose a day.
     ZonedDateTime rebasedDate2 = date2.withZoneSameInstant(date1.getZone());
-    
-    final boolean timeIsNegative = date1.isAfter(rebasedDate2); // date1 >= date2
 
+    final boolean timeIsNegative = date1.isAfter(rebasedDate2); // date1 >= date2
 
     if (!timeIsNegative) {
       final double time = ACT_ACT.getDayCountFraction(date1, rebasedDate2);
@@ -39,5 +38,17 @@ public abstract class TimeCalculator {
 
   public static double getTimeBetween(final LocalDate date1, final LocalDate date2) {
     return getTimeBetween(date1.atStartOfDayInZone(TimeZone.UTC), date2.atStartOfDayInZone(TimeZone.UTC));
+  }
+
+  public static double getTimeBetween(final ZonedDateTime zdt1, final LocalDate date2) {
+    ZonedDateTime zdt2 = date2.atStartOfDayInZone(TimeZone.UTC);
+    ZonedDateTime rebasedZdt1 = zdt1.withZoneSameInstant(TimeZone.UTC);
+    return getTimeBetween(rebasedZdt1, zdt2);
+  }
+
+  public static double getTimeBetween(final LocalDate date1, final ZonedDateTime zdt2) {
+    ZonedDateTime zdt1 = date1.atStartOfDayInZone(TimeZone.UTC);
+    ZonedDateTime rebasedZdt2 = zdt2.withZoneSameInstant(TimeZone.UTC);
+    return getTimeBetween(zdt1, rebasedZdt2);
   }
 }
