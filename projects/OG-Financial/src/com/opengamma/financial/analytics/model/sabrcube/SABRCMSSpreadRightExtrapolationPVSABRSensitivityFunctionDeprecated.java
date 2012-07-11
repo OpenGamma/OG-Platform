c0b6b354912339b19228cd82807ededc5fa4f21a
Elaine McLeod <elaine@opengamma.com>
2012-07-11 11:51:36 +0100

    Renaming SABR functions in preparation for deprecation and replacement with version that use curve configurations

---
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/model/sabrcube/SABRCMSSpreadRightExtrapolationPVSABRSensitivityFunctionDeprecated.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.model.sabrcube;
+
+import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
+import com.opengamma.analytics.financial.interestrate.PresentValueSABRSensitivityDataBundle;
+import com.opengamma.analytics.financial.interestrate.PresentValueSABRSensitivitySABRRightExtrapolationCalculator;
+import com.opengamma.analytics.financial.model.option.definition.SABRInterestRateDataBundle;
+import com.opengamma.engine.value.ValueRequirement;
+import com.opengamma.engine.value.ValueRequirementNames;
+import com.opengamma.financial.analytics.DoubleLabelledMatrix2D;
+
+/**
+ *
+ */
+public abstract class SABRCMSSpreadRightExtrapolationPVSABRSensitivityFunctionDeprecated extends SABRCMSSpreadRightExtrapolationFunctionDeprecated {
+
+  @Override
+  protected Object getResult(final InstrumentDerivative derivative, final SABRInterestRateDataBundle data, final ValueRequirement desiredValue) {
+    final Double cutoff = Double.parseDouble(desiredValue.getConstraint(SABRRightExtrapolationFunctionDeprecated.PROPERTY_CUTOFF_STRIKE));
+    final Double mu = Double.parseDouble(desiredValue.getConstraint(SABRRightExtrapolationFunctionDeprecated.PROPERTY_TAIL_THICKNESS_PARAMETER));
+    final PresentValueSABRSensitivitySABRRightExtrapolationCalculator calculator = new PresentValueSABRSensitivitySABRRightExtrapolationCalculator(cutoff, mu);
+    return getResultAsMatrix(calculator.visit(derivative, data));
+  }
+
+  protected abstract DoubleLabelledMatrix2D getResultAsMatrix(final PresentValueSABRSensitivityDataBundle sensitivities);
+
+  /**
+   * Function to get the sensitivity to the alpha parameter
+   */
+  public static class Alpha extends SABRCMSSpreadRightExtrapolationPVSABRSensitivityFunctionDeprecated {
+
+    @Override
+    protected String getValueRequirement() {
+      return ValueRequirementNames.PRESENT_VALUE_SABR_ALPHA_SENSITIVITY;
+    }
+
+    @Override
+    protected DoubleLabelledMatrix2D getResultAsMatrix(final PresentValueSABRSensitivityDataBundle sensitivities) {
+      return SABRCubeUtil.toDoubleLabelledMatrix2D(sensitivities.getAlpha());
+    }
+
+  }
+
+  /**
+   * Function to get the sensitivity to the rho parameter
+   */
+  public static class Rho extends SABRCMSSpreadRightExtrapolationPVSABRSensitivityFunctionDeprecated {
+
+    @Override
+    protected String getValueRequirement() {
+      return ValueRequirementNames.PRESENT_VALUE_SABR_RHO_SENSITIVITY;
+    }
+
+    @Override
+    protected DoubleLabelledMatrix2D getResultAsMatrix(final PresentValueSABRSensitivityDataBundle sensitivities) {
+      return SABRCubeUtil.toDoubleLabelledMatrix2D(sensitivities.getRho());
+    }
+
+  }
+
+  /**
+   * Function to get the sensitivity to the nu parameter
+   */
+  public static class Nu extends SABRCMSSpreadRightExtrapolationPVSABRSensitivityFunctionDeprecated {
+
+    @Override
+    protected String getValueRequirement() {
+      return ValueRequirementNames.PRESENT_VALUE_SABR_NU_SENSITIVITY;
+    }
+
+    @Override
+    protected DoubleLabelledMatrix2D getResultAsMatrix(final PresentValueSABRSensitivityDataBundle sensitivities) {
+      return SABRCubeUtil.toDoubleLabelledMatrix2D(sensitivities.getNu());
+    }
+
+  }
+
+}
---
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/model/sabrcube/SABRCMSSpreadRightExtrapolationPresentValueFunctionDeprecated.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.model.sabrcube;
+
+import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
+import com.opengamma.analytics.financial.interestrate.PresentValueCalculator;
+import com.opengamma.analytics.financial.interestrate.PresentValueSABRExtrapolationCalculator;
+import com.opengamma.analytics.financial.model.option.definition.SABRInterestRateDataBundle;
+import com.opengamma.engine.value.ValueRequirement;
+import com.opengamma.engine.value.ValueRequirementNames;
+
+/**
+ *
+ */
+public class SABRCMSSpreadRightExtrapolationPresentValueFunctionDeprecated extends SABRCMSSpreadRightExtrapolationFunctionDeprecated {
+
+  @Override
+  protected String getValueRequirement() {
+    return ValueRequirementNames.PRESENT_VALUE;
+  }
+
+  @Override
+  protected Object getResult(final InstrumentDerivative derivative, final SABRInterestRateDataBundle data, final ValueRequirement desiredValue) {
+    final Double cutoff = Double.parseDouble(desiredValue.getConstraint(SABRRightExtrapolationFunctionDeprecated.PROPERTY_CUTOFF_STRIKE));
+    final Double mu = Double.parseDouble(desiredValue.getConstraint(SABRRightExtrapolationFunctionDeprecated.PROPERTY_TAIL_THICKNESS_PARAMETER));
+    final PresentValueCalculator calculator = new PresentValueSABRExtrapolationCalculator(cutoff, mu);
+    return calculator.visit(derivative, data);
+  }
+}
