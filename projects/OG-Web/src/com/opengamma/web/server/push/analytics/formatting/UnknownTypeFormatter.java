de87facc6e1b49bbaa074fe3f41ce678e385d4b1
Chris Kent <chris@opengamma.com>
2012-07-13 09:49:51 +0100

    [PLAT-2271] Log when a value is received for columns with no type

--- projects/OG-Web/src/com/opengamma/web/server/push/analytics/formatting/UnknownTypeFormatter.java
+++ projects/OG-Web/src/com/opengamma/web/server/push/analytics/formatting/UnknownTypeFormatter.java
@@ -5,10 +5,34 @@
  */
 package com.opengamma.web.server.push.analytics.formatting;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.opengamma.engine.value.ValueSpecification;
+
 /**
  * Formatter for columns whose type is unknown.
  */
 /* package */ class UnknownTypeFormatter extends DefaultFormatter {
+
+  private static final Logger s_logger = LoggerFactory.getLogger(UnknownTypeFormatter.class);
+
+  @Override
+  public String formatForDisplay(Object value, ValueSpecification valueSpec) {
+    logType(value, valueSpec);
+    return super.formatForDisplay(value, valueSpec);
+  }
+
+  @Override
+  public Object formatForExpandedDisplay(Object value, ValueSpecification valueSpec) {
+    logType(value, valueSpec);
+    return super.formatForExpandedDisplay(value, valueSpec);
+  }
+
+  private static void logType(Object value, ValueSpecification valueSpec) {
+    String typeName = value == null ? null : value.getClass().getName();
+    s_logger.info("Value received for unknown type, value name: {}, type: {}", valueSpec.getValueName(), typeName);
+  }
 
   @Override
   public FormatType getFormatType() {
