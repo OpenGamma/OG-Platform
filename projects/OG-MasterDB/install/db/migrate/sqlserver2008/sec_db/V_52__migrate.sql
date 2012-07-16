c3d669e744dbecd29e80c71d0a771c06f7bb180f
Jonathan Senior <jonathan@opengamma.com>
2012-07-16 17:21:13 +0100

    [PLAT-2375] Allow interest accrual date and first coupon date to be null

---
+++ projects/OG-MasterDB/install/db/migrate/sqlserver2008/sec_db/V_52__migrate.sql
@@ -0,0 +1,11 @@
+BEGIN TRAN;
+
+  -- update the version
+  UPDATE sec_schema_version SET version_value='52' WHERE version_key='schema_patch';
+
+  ALTER TABLE sec_bond ALTER COLUMN interestaccrual_date DATETIME2(6);
+  ALTER TABLE sec_bond ALTER COLUMN interestaccrual_zone VARCHAR(50);
+  ALTER TABLE sec_bond ALTER COLUMN firstcoupon_date DATETIME2(6);
+  ALTER TABLE sec_bond ALTER COLUMN firstcoupon_zone VARCHAR(50);
+    
+COMMIT;
