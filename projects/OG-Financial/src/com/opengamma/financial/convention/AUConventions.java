c324cb5f7a1d3a8f28e5ef3e95cf86aa7269330a
Elaine McLeod <elaine@opengamma.com>
2012-07-18 14:20:07 +0100

    [PLAT-2406] Adding conventions for Tullett-Prebon

--- projects/OG-Financial/src/com/opengamma/financial/convention/AUConventions.java
+++ projects/OG-Financial/src/com/opengamma/financial/convention/AUConventions.java
@@ -5,6 +5,11 @@
  */
 package com.opengamma.financial.convention;
 
+import static com.opengamma.core.id.ExternalSchemes.bloombergTickerSecurityId;
+import static com.opengamma.core.id.ExternalSchemes.ricSecurityId;
+import static com.opengamma.core.id.ExternalSchemes.tullettPrebonSecurityId;
+import static com.opengamma.financial.convention.InMemoryConventionBundleMaster.simpleNameSecurityId;
+
 import javax.time.calendar.Period;
 
 import org.apache.commons.lang.Validate;
@@ -19,10 +24,6 @@ import com.opengamma.financial.convention.frequency.Frequency;
 import com.opengamma.financial.convention.frequency.SimpleFrequencyFactory;
 import com.opengamma.id.ExternalId;
 import com.opengamma.id.ExternalIdBundle;
-
-import static com.opengamma.core.id.ExternalSchemes.bloombergTickerSecurityId;
-import static com.opengamma.core.id.ExternalSchemes.ricSecurityId;
-import static com.opengamma.financial.convention.InMemoryConventionBundleMaster.simpleNameSecurityId;
 
 /**
  * 
@@ -43,7 +44,7 @@ public class AUConventions {
 
     final Integer overnightPublicationLag = 0;
 
-    ConventionBundleMasterUtils utils = new ConventionBundleMasterUtils(conventionMaster);
+    final ConventionBundleMasterUtils utils = new ConventionBundleMasterUtils(conventionMaster);
     // IR FUTURES
     utils.addConventionBundle(ExternalIdBundle.of(simpleNameSecurityId("AUD_IR_FUTURE")), "AUD_IR_FUTURE", act365, modified, Period.ofMonths(3), 0, true, au);
 
@@ -61,13 +62,13 @@ public class AUConventions {
         following, Period.ofMonths(1), 2, false, au);
     utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0002M Index"), simpleNameSecurityId("AUD LIBOR 2m")), "AUD LIBOR 2m", act365,
         following, Period.ofMonths(2), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0003M Index"), simpleNameSecurityId("AUD LIBOR 3m")), "AUD LIBOR 3m", act365, 
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0003M Index"), simpleNameSecurityId("AUD LIBOR 3m")), "AUD LIBOR 3m", act365,
         following, Period.ofMonths(3), 2, false, au);
     utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0004M Index"), simpleNameSecurityId("AUD LIBOR 4m")), "AUD LIBOR 4m", act365,
         following, Period.ofMonths(4), 2, false, au);
     utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0005M Index"), simpleNameSecurityId("AUD LIBOR 5m")), "AUD LIBOR 5m", act365,
         following, Period.ofMonths(5), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0006M Index"), simpleNameSecurityId("AUD LIBOR 6m")), "AUD LIBOR 6m", act365, 
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0006M Index"), simpleNameSecurityId("AUD LIBOR 6m")), "AUD LIBOR 6m", act365,
         following, Period.ofMonths(6), 2, false, au);
     utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0007M Index"), simpleNameSecurityId("AUD LIBOR 7m")), "AUD LIBOR 7m", act365,
         following, Period.ofMonths(7), 2, false, au);
@@ -79,7 +80,7 @@ public class AUConventions {
         following, Period.ofMonths(10), 2, false, au);
     utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0011M Index"), simpleNameSecurityId("AUD LIBOR 11m")), "AUD LIBOR 11m", act365,
         following, Period.ofMonths(11), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0012M Index"), simpleNameSecurityId("AUD LIBOR 12m")), "AUD LIBOR 12m", act365, 
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("AU0012M Index"), simpleNameSecurityId("AUD LIBOR 12m")), "AUD LIBOR 12m", act365,
         following, Period.ofMonths(12), 2, false, au);
 
     //TODO need to check that these are right for deposit rates
@@ -89,36 +90,36 @@ public class AUConventions {
         following, Period.ofDays(1), 0, false, au);
     utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR3T Curncy"), simpleNameSecurityId("AUD DEPOSIT 3d")), "AUD DEPOSIT 3d", act365,
         following, Period.ofDays(1), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR1Z Curncy"), simpleNameSecurityId("AUD DEPOSIT 1w")), "AUD DEPOSIT 1w", act365,
-        following, Period.ofDays(7), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR2Z Curncy"), simpleNameSecurityId("AUD DEPOSIT 2w")), "AUD DEPOSIT 2w", act365,
-        following, Period.ofDays(14), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR3Z Curncy"), simpleNameSecurityId("AUD DEPOSIT 3w")), "AUD DEPOSIT 3w", act365,
-        following, Period.ofDays(21), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRA Curncy"), simpleNameSecurityId("AUD DEPOSIT 1m")), "AUD DEPOSIT 1m", act365,
-        following, Period.ofMonths(1), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRB Curncy"), simpleNameSecurityId("AUD DEPOSIT 2m")), "AUD DEPOSIT 2m", act365,
-        following, Period.ofMonths(2), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRC Curncy"), simpleNameSecurityId("AUD DEPOSIT 3m")), "AUD DEPOSIT 3m", act365,
-        following, Period.ofMonths(3), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRD Curncy"), simpleNameSecurityId("AUD DEPOSIT 4m")), "AUD DEPOSIT 4m", act365,
-        following, Period.ofMonths(4), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRE Curncy"), simpleNameSecurityId("AUD DEPOSIT 5m")), "AUD DEPOSIT 5m", act365,
-        following, Period.ofMonths(5), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRF Curncy"), simpleNameSecurityId("AUD DEPOSIT 6m")), "AUD DEPOSIT 6m", act365,
-        following, Period.ofMonths(6), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRG Curncy"), simpleNameSecurityId("AUD DEPOSIT 7m")), "AUD DEPOSIT 7m", act365,
-        following, Period.ofMonths(7), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRH Curncy"), simpleNameSecurityId("AUD DEPOSIT 8m")), "AUD DEPOSIT 8m", act365,
-        following, Period.ofMonths(8), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRI Curncy"), simpleNameSecurityId("AUD DEPOSIT 9m")), "AUD DEPOSIT 9m", act365,
-        following, Period.ofMonths(9), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRJ Curncy"), simpleNameSecurityId("AUD DEPOSIT 10m")), "AUD DEPOSIT 10m", act365,
-        following, Period.ofMonths(10), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRK Curncy"), simpleNameSecurityId("AUD DEPOSIT 11m")), "AUD DEPOSIT 11m", act365,
-        following, Period.ofMonths(11), 2, false, au);
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR1 Curncy"), simpleNameSecurityId("AUD DEPOSIT 1y")), "AUD DEPOSIT 1y", act365,
-        following, Period.ofYears(1), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR1Z Curncy"), simpleNameSecurityId("AUD DEPOSIT 1w"),
+        tullettPrebonSecurityId("ASDEPAUDSPT01W")), "AUD DEPOSIT 1w", act365, following, Period.ofDays(7), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR2Z Curncy"), simpleNameSecurityId("AUD DEPOSIT 2w"),
+        tullettPrebonSecurityId("ASDEPAUDSPT02W")), "AUD DEPOSIT 2w", act365, following, Period.ofDays(14), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR3Z Curncy"), simpleNameSecurityId("AUD DEPOSIT 3w"),
+        tullettPrebonSecurityId("ASDEPAUDSPT03W")), "AUD DEPOSIT 3w", act365, following, Period.ofDays(21), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRA Curncy"), simpleNameSecurityId("AUD DEPOSIT 1m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT01M")), "AUD DEPOSIT 1m", act365, following, Period.ofMonths(1), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRB Curncy"), simpleNameSecurityId("AUD DEPOSIT 2m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT02M")), "AUD DEPOSIT 2m", act365, following, Period.ofMonths(2), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRC Curncy"), simpleNameSecurityId("AUD DEPOSIT 3m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT03M")), "AUD DEPOSIT 3m", act365, following, Period.ofMonths(3), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRD Curncy"), simpleNameSecurityId("AUD DEPOSIT 4m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT04M")), "AUD DEPOSIT 4m", act365, following, Period.ofMonths(4), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRE Curncy"), simpleNameSecurityId("AUD DEPOSIT 5m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT05M")), "AUD DEPOSIT 5m", act365, following, Period.ofMonths(5), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRF Curncy"), simpleNameSecurityId("AUD DEPOSIT 6m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT06M")), "AUD DEPOSIT 6m", act365, following, Period.ofMonths(6), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRG Curncy"), simpleNameSecurityId("AUD DEPOSIT 7m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT07M")), "AUD DEPOSIT 7m", act365, following, Period.ofMonths(7), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRH Curncy"), simpleNameSecurityId("AUD DEPOSIT 8m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT08M")), "AUD DEPOSIT 8m", act365, following, Period.ofMonths(8), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRI Curncy"), simpleNameSecurityId("AUD DEPOSIT 9m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT09M")), "AUD DEPOSIT 9m", act365, following, Period.ofMonths(9), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRJ Curncy"), simpleNameSecurityId("AUD DEPOSIT 10m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT10M")), "AUD DEPOSIT 10m", act365, following, Period.ofMonths(10), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDRK Curncy"), simpleNameSecurityId("AUD DEPOSIT 11m"),
+        tullettPrebonSecurityId("ASDEPAUDSPT11M")), "AUD DEPOSIT 11m", act365, following, Period.ofMonths(11), 2, false, au);
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR1 Curncy"), simpleNameSecurityId("AUD DEPOSIT 1y"),
+        tullettPrebonSecurityId("ASDEPAUDSPT12M")), "AUD DEPOSIT 1y", act365, following, Period.ofYears(1), 2, false, au);
     utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR2 Curncy"), simpleNameSecurityId("AUD DEPOSIT 2y")), "AUD DEPOSIT 2y", act365,
         following, Period.ofYears(2), 2, false, au);
     utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("ADDR3 Curncy"), simpleNameSecurityId("AUD DEPOSIT 3y")), "AUD DEPOSIT 3y", act365,
@@ -153,15 +154,15 @@ public class AUConventions {
 
     utils.addConventionBundle(ExternalIdBundle.of(simpleNameSecurityId("AUD_3M_FRA")), "AUD_3M_FRA", act365, modified, quarterly, 0, au, act365,
         modified, quarterly, 0, simpleNameSecurityId(IndexType.BBSW + "_AUD_P3M"), au, true);
-    utils.addConventionBundle(ExternalIdBundle.of(simpleNameSecurityId("AUD_6M_FRA")), "AUD_6M_FRA", act365, modified, semiAnnual, 0, au, act365, 
+    utils.addConventionBundle(ExternalIdBundle.of(simpleNameSecurityId("AUD_6M_FRA")), "AUD_6M_FRA", act365, modified, semiAnnual, 0, au, act365,
         modified, semiAnnual, 0, simpleNameSecurityId(IndexType.BBSW + "_AUD_P6M"), au, true);
-    utils.addConventionBundle(ExternalIdBundle.of(simpleNameSecurityId("AUD_OIS_SWAP")), "AUD_OIS_SWAP", act365, modified, annual, 0, au, act365, 
+    utils.addConventionBundle(ExternalIdBundle.of(simpleNameSecurityId("AUD_OIS_SWAP")), "AUD_OIS_SWAP", act365, modified, annual, 0, au, act365,
         modified, annual, 0, simpleNameSecurityId("RBA OVERNIGHT CASH RATE"), au, true, overnightPublicationLag);
 
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("BBSW3M Index"), ricSecurityId("AUBABSL3M=AFMA"), simpleNameSecurityId(IndexType.BBSW + "_AUD_P3M")), 
-            "AUD Bank Bill 3m", act365, modified, Period.ofMonths(3), 0, true, au); // "AUD Bank Bill 3m"
-    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("BBSW6M Index"), ricSecurityId("AUBABSL6M=AFMA"), simpleNameSecurityId(IndexType.BBSW + "_AUD_P6M")), 
-            "AUD Bank Bill 6m", act365, modified, Period.ofMonths(6), 0, true, au); // "AUD Bank Bill 6m"
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("BBSW3M Index"), ricSecurityId("AUBABSL3M=AFMA"), simpleNameSecurityId(IndexType.BBSW + "_AUD_P3M")),
+        "AUD Bank Bill 3m", act365, modified, Period.ofMonths(3), 0, true, au); // "AUD Bank Bill 3m"
+    utils.addConventionBundle(ExternalIdBundle.of(bloombergTickerSecurityId("BBSW6M Index"), ricSecurityId("AUBABSL6M=AFMA"), simpleNameSecurityId(IndexType.BBSW + "_AUD_P6M")),
+        "AUD Bank Bill 6m", act365, modified, Period.ofMonths(6), 0, true, au); // "AUD Bank Bill 6m"
     utils.addConventionBundle(ExternalIdBundle.of(simpleNameSecurityId("AUD_SWAPTION")), "AUD_SWAPTION", false);
   }
 
