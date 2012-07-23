f35d5434288be8be7b1c4d77712dd55c73fb22fd
Kirk Wylie <kirk@opengamma.com>
2012-07-23 11:01:06 +0100

    [PLAT-2456] Added support for available servers and capabilities to the connection response message.

--- projects/OG-LiveData/src/com/opengamma/livedata/cogda/msg/ConnectionResponseBuilder.java
+++ projects/OG-LiveData/src/com/opengamma/livedata/cogda/msg/ConnectionResponseBuilder.java
@@ -5,6 +5,7 @@
  */
 package com.opengamma.livedata.cogda.msg;
 
+import org.fudgemsg.FudgeField;
 import org.fudgemsg.FudgeMsg;
 import org.fudgemsg.MutableFudgeMsg;
 import org.fudgemsg.mapping.FudgeBuilder;
@@ -21,6 +22,12 @@ public class ConnectionResponseBuilder implements FudgeBuilder<ConnectionRespons
     
     msg.add("result", response.getResult().name());
     
+    for (String availableServer : response.getAvailableServers()) {
+      msg.add("availableServer", availableServer);
+    }
+    
+    msg.add("capabilities", response.getCapabilities());
+    
     return msg;
   }
   
@@ -32,6 +39,13 @@ public class ConnectionResponseBuilder implements FudgeBuilder<ConnectionRespons
   public static ConnectionResponseMessage buildObjectStatic(FudgeDeserializer deserializer, FudgeMsg message) {
     ConnectionResponseMessage response = new ConnectionResponseMessage();
     response.setResult(ConnectionResult.valueOf(message.getString("result")));
+    
+    for (FudgeField field : message.getAllByName("availableServer")) {
+      response.getAvailableServers().add((String) field.getValue());
+    }
+    
+    response.applyCapabilities(message.getMessage("capabilities"));
+    
     return response;
   }
   
