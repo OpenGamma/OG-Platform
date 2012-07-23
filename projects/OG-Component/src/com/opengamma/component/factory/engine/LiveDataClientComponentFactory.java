a9da3da515541d5e68a70d18e70ae9e7971d6feb
Stephen Colebourne <stephen@opengamma.com>
2012-07-23 13:50:36 +0100

    [PLAT-1170] Move live data component factory

--- projects/OG-Component/src/com/opengamma/component/factory/engine/LiveDataClientComponentFactory.java
+++ projects/OG-Component/src/com/opengamma/component/factory/engine/LiveDataClientComponentFactory.java
@@ -3,7 +3,7 @@
  * 
  * Please see distribution for license.
  */
-package com.opengamma.integration.marketdata;
+package com.opengamma.component.factory.engine;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
