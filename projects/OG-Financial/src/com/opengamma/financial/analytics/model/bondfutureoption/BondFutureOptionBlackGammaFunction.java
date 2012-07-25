713ec032f46a13999b4902364bb9aeb28029399c
Elaine McLeod <elaine@opengamma.com>
2012-07-25 16:09:28 +0100

    [PLAT-2466] Added a Black gamma function for bond future options

---
+++ projects/OG-Financial/src/com/opengamma/financial/analytics/model/bondfutureoption/BondFutureOptionBlackGammaFunction.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.model.bondfutureoption;
+
+import java.util.Collections;
+import java.util.Set;
+
+import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
+import com.opengamma.analytics.financial.interestrate.PresentValueBlackGammaCalculator;
+import com.opengamma.analytics.financial.model.option.definition.YieldCurveWithBlackCubeBundle;
+import com.opengamma.engine.value.ComputedValue;
+import com.opengamma.engine.value.ValueRequirementNames;
+import com.opengamma.engine.value.ValueSpecification;
+
+/**
+ * 
+ */
+public class BondFutureOptionBlackGammaFunction extends BondFutureOptionBlackFunction {
+  private static final PresentValueBlackGammaCalculator CALCULATOR = PresentValueBlackGammaCalculator.getInstance();
+
+  public BondFutureOptionBlackGammaFunction() {
+    super(ValueRequirementNames.VALUE_GAMMA);
+  }
+
+  @Override
+  protected Set<ComputedValue> getResult(final InstrumentDerivative bondFutureOption, final YieldCurveWithBlackCubeBundle data, final ValueSpecification spec) {
+    final Double gamma = CALCULATOR.visit(bondFutureOption, data);
+    return Collections.singleton(new ComputedValue(spec, gamma));
+  }
+
+}
