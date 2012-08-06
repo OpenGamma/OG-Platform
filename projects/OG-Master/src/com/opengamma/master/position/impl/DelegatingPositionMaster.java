5135690459edb5e61171ac3381fc66b6336fa9dc
Stephen Colebourne <stephen@opengamma.com>
2012-08-06 16:24:42 +0100

    Extend functionality of delegating position master

--- projects/OG-Master/src/com/opengamma/master/position/impl/DelegatingPositionMaster.java
+++ projects/OG-Master/src/com/opengamma/master/position/impl/DelegatingPositionMaster.java
@@ -10,8 +10,19 @@ import java.util.Map;
 
 import com.opengamma.core.change.AggregatingChangeManager;
 import com.opengamma.core.change.ChangeManager;
-import com.opengamma.id.*;
-import com.opengamma.master.position.*;
+import com.opengamma.id.ObjectId;
+import com.opengamma.id.ObjectIdentifiable;
+import com.opengamma.id.UniqueId;
+import com.opengamma.id.UniqueIdSchemeDelegator;
+import com.opengamma.id.VersionCorrection;
+import com.opengamma.master.position.ManageableTrade;
+import com.opengamma.master.position.PositionDocument;
+import com.opengamma.master.position.PositionHistoryRequest;
+import com.opengamma.master.position.PositionHistoryResult;
+import com.opengamma.master.position.PositionMaster;
+import com.opengamma.master.position.PositionSearchRequest;
+import com.opengamma.master.position.PositionSearchResult;
+import com.opengamma.util.ArgumentChecker;
 
 /**
  * A master of positions that uses the scheme of the unique identifier to determine which
@@ -47,8 +58,8 @@ public class DelegatingPositionMaster extends UniqueIdSchemeDelegator<PositionMa
   public DelegatingPositionMaster(PositionMaster defaultMaster, Map<String, PositionMaster> schemePrefixToMasterMap) {
     super(defaultMaster, schemePrefixToMasterMap);
     AggregatingChangeManager changeManager = new AggregatingChangeManager();
-
-    // REVIEW jonathan 2011-08-03 -- this assumes that the delegating master lasts for the lifetime of the engine as we
+    
+    // REVIEW jonathan 2012-08-03 -- this assumes that the delegating master lasts for the lifetime of the engine as we
     // never detach from the underlying change managers.
     changeManager.addChangeManager(defaultMaster.changeManager());
     for (PositionMaster master : schemePrefixToMasterMap.values()) {
@@ -57,53 +68,63 @@ public class DelegatingPositionMaster extends UniqueIdSchemeDelegator<PositionMa
     _changeManager = changeManager;
   }
 
+  //-------------------------------------------------------------------------
   @Override
   public PositionHistoryResult history(PositionHistoryRequest request) {
+    ArgumentChecker.notNull(request, "request");
     return chooseDelegate(request.getObjectId().getScheme()).history(request);
   }
 
   @Override
   public PositionSearchResult search(PositionSearchRequest request) {
+    ArgumentChecker.notNull(request, "request");
     List<ObjectId> ids = request.getPositionObjectIds();
-    if (ids.size() > 0) {
-      return chooseDelegate(ids.get(0).getScheme()).search(request);
-    } else {
-      return new PositionSearchResult();
+    if (ids == null || ids.isEmpty()) {
+      return getDefaultDelegate().search(request);
     }
+    return chooseDelegate(ids.get(0).getScheme()).search(request);
   }
 
   @Override
   public PositionDocument get(UniqueId uniqueId) {
+    ArgumentChecker.notNull(uniqueId, "uniqueId");
     return chooseDelegate(uniqueId.getScheme()).get(uniqueId);
   }
 
   @Override
   public PositionDocument get(ObjectIdentifiable objectId, VersionCorrection versionCorrection) {
+    ArgumentChecker.notNull(objectId, "objectId");
+    ArgumentChecker.notNull(versionCorrection, "versionCorrection");
     return chooseDelegate(objectId.getObjectId().getScheme()).get(objectId, versionCorrection);
   }
 
   @Override
   public PositionDocument add(PositionDocument document) {
-    throw new UnsupportedOperationException("The delegationg position master is read only");
+    ArgumentChecker.notNull(document, "document");
+    return getDefaultDelegate().add(document);
   }
 
   @Override
   public PositionDocument update(PositionDocument document) {
-    throw new UnsupportedOperationException("The delegationg position master is read only");
+    ArgumentChecker.notNull(document, "document");
+    return chooseDelegate(document.getObjectId().getScheme()).update(document);
   }
 
   @Override
   public void remove(UniqueId uniqueId) {
-    throw new UnsupportedOperationException("The delegationg position master is read only");
+    ArgumentChecker.notNull(uniqueId, "uniqueId");
+    chooseDelegate(uniqueId.getScheme()).remove(uniqueId);
   }
 
   @Override
   public PositionDocument correct(PositionDocument document) {
-    throw new UnsupportedOperationException("The delegationg position master is read only");
+    ArgumentChecker.notNull(document, "document");
+    return chooseDelegate(document.getObjectId().getScheme()).correct(document);
   }
 
   @Override
   public ManageableTrade getTrade(UniqueId uniqueId) {
+    ArgumentChecker.notNull(uniqueId, "uniqueId");
     return chooseDelegate(uniqueId.getScheme()).getTrade(uniqueId);
   }
 
