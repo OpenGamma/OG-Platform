7110c3714efbf7288d1e0097c3b1315a93dec009
Craig <craig@opengamma.com>
2012-08-21 10:55:22 +0100

    Commit

--- projects/OG-Analytics/src/com/opengamma/analytics/financial/credit/creditindexdefaultswap/definition/CreditIndexDefaultSwapDefinition.java
+++ projects/OG-Analytics/src/com/opengamma/analytics/financial/credit/creditindexdefaultswap/definition/CreditIndexDefaultSwapDefinition.java
@@ -9,36 +9,50 @@ import java.util.Vector;
 
 import javax.time.calendar.ZonedDateTime;
 
+import com.opengamma.financial.convention.calendar.Calendar;
+import com.opengamma.util.money.Currency;
+
 /**
  * 
  */
 public class CreditIndexDefaultSwapDefinition {
   
-  /*
-  ZonedDateTime _startDate;
-  ZonedDateTime _effectiveDate;
-  ZonedDateTime _maturityDate;
-  ZonedDateTime _valuationDate;
-  */
   
+  // From the users perspective, are we buying or selling protection
+  private final String _buySellProtection;
+
+  // Identifiers for the (three) counterparties in the trade
+  private final String _protectionBuyer;
+  private final String _protectionSeller;
+  private final String _referenceEntity;
+  
+//The currency the trade is executed in e.g. USD
+ private final Currency _currency;
+ 
+ // Holiday calendar for the determination of adjusted business days in the cashflow schedule
+ private final Calendar _calendar;
+ 
+ // The date of the contract inception
+ private final ZonedDateTime _startDate;
+ 
+ // The effective date for protection to begin (usually T + 1 for legacy CDS)
+ private final ZonedDateTime _effectiveDate;
+ 
+ // The maturity date of the contract (when premium and protection coverage ceases)
+ private final ZonedDateTime _maturityDate;
+ 
+ // The date on which we want to calculate the CDS MtM
+ private final ZonedDateTime _valuationDate;
+  
+  // The nuber of obligors in the underlying pool (usually 125 for CDX and iTraxx - although defaults can reduce this)
   private final int _numberOfObligors;
   
+  //The trade notional (in the trade currency)
   private final double _notional;
   
   private final double _spread;
-
   
-  /*
-  public CreditIndexDefaultSwapDefinition(ZonedDateTime startDate, 
-      ZonedDateTime effectiveDate, 
-      ZonedDateTime maturityDate, 
-      ZonedDateTime valuationDate, 
-      int numberOfObligors, 
-      double notional, 
-      double spread)
-  */
-  
-  
+  //Constructor for a CDS index swap definition object (all fields are user specified)
   public CreditIndexDefaultSwapDefinition(double notional, double spread, int numberOfObligors) {
     
     //_startDate = startDate;
@@ -52,7 +66,7 @@ public class CreditIndexDefaultSwapDefinition {
     
     _spread = spread;
     
-    Vector<String> underlyingPool = new Vector(_numberOfObligors);
+    //Vector<String> underlyingPool = new Vector(_numberOfObligors);
   }
 
 //----------------------------------------------------------------------------------------------------------------------------------------
