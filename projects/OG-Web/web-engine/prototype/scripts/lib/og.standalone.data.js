cda6ca75d989e14b76304d9ec64ea3e44ca10c4b
Antonio Greco <antonio@opengamma.com>
2012-09-03 15:46:56 +0100

    initial ticking data gadget

--- projects/OG-Web/web-engine/prototype/scripts/lib/og.standalone.data.js
+++ projects/OG-Web/web-engine/prototype/scripts/lib/og.standalone.data.js
@@ -4,37 +4,25 @@
         max_col_width: 300 // the maximum default width for a column
     };
     $.fn.ogdata = function (input) {
-        var $selector = $(this), grid_width = $selector.width() / input.length, util = {}, gadget = {};
+        var $selector = $(this), grid_width = $selector.width() / input.length, util = {}, gadget = {}, grid;
         /**
          * @param data {Object} object with data & labels arrays
          * @param num
          */
         gadget.add_grid = function (data, num) {
-            if (!data.labels) data.labels = ['Label', 'Value'];
-            var ismatrix = ($.isArray(data.labels[0])),
-                slick_data, slick_columns = [],
-                xlabels = ismatrix ? data.labels[0] : data.labels,
-                ylabels = ismatrix ? data.labels[1] : null,
-                col_width = util.column_width(data, settings.max_col_width, ismatrix);
-            if (ismatrix){
-                slick_columns.push({id: 'ylabelscol', name: '', field: 'ylabelscol', width: col_width});
-                $selector.addClass('matrix');
-            }
-            xlabels.forEach(function (val) {
-                slick_columns.push({id: val, name: val, field: val, width: col_width});
-            });
-            slick_data = data.data.slice().reverse().reduce(function (acc, val, i) {
-                var obj = {};
-                if (ismatrix) obj.ylabelscol = ylabels[i];
-                val.forEach(function (val, i) {obj[xlabels[i]] = val;});
-                return acc.push(obj) && acc;
-            }, []);
-            new Slick.Grid($selector.find('.data_' + num), slick_data, slick_columns);
+            var pdata = util.process_data(data);
+            grid = new Slick.Grid($selector.find('.data_' + num), pdata.data, pdata.columns);
         };
         gadget.load = function () {
             $selector.css('backgroundColor', settings.background_color);
             util.setup_framework();
             input.forEach(function (val, i) {gadget.add_grid(input[i], i);});
+        };
+        gadget.update = function(input){
+            var pdata = util.process_data(input);
+            grid.setData(pdata.data);
+            grid.setColumns(pdata.columns);
+            grid.invalidate();
         };
         /**
          * Create html columns to house the grids
@@ -50,6 +38,32 @@
             }
             $selector.html(html);
         };
+        util.process_data = function(data){
+            if (!data.labels) data.labels = ['Label', 'Value'];
+            var ismatrix = ($.isArray(data.labels[0])),
+                xlabels = ismatrix ? data.labels[0] : data.labels,
+                ylabels = ismatrix ? data.labels[1] : null,
+                col_width = util.column_width(data, settings.max_col_width, ismatrix);
+                cols = [];
+            if (ismatrix){
+                cols.push({id: 'ylabelscol', name: '', field: 'ylabelscol', width: col_width});
+                $selector.addClass('matrix');
+            }else{
+                $selector.removeClass('matrix');
+            }
+            xlabels.forEach(function (val) {
+                cols.push({id: val, name: val, field: val, width: col_width});
+            });
+            return { 
+                data: data.data.slice().reverse().reduce(function (acc, val, i) {
+                    var obj = {};
+                    if (ismatrix) obj.ylabelscol = ylabels[i];
+                    val.forEach(function (val, i) {obj[xlabels[i]] = val;});
+                    return acc.push(obj) && acc;
+                }, []),
+                columns:cols
+            };
+        };
         /**
          * Calculate the default width of columns
          * @param data {Object} object with data & labels arrays
@@ -64,5 +78,6 @@
             return potential_width > max ? max : potential_width - (scrollbar_width / cols);
         };
         gadget.load();
+        return gadget;
     }
 })(jQuery);
