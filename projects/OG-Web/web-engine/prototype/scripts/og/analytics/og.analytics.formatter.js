e2a691d3d650a4dd768d635ffad7823a756552eb
Alan Ayoub <alan@opengamma.com>
2012-09-11 15:26:12 +0100

    curve formatter

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.formatter.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.formatter.js
@@ -23,12 +23,23 @@ $.register_module({
                 var type = value.t;
                 return value && formatter[type] ? formatter[type](value) : value && value.v || '';
             };
+            formatter.CURVE = function (value) {
+                return '<span class="flot">[' + JSON.stringify(value.v) + ']</span>'
+            };
             grid.on('render', function () {
                 var options = {
                     type: 'line', lineColor: '#b0b0b0', fillColor: '#ecedee', spotColor: '#b0b0b0',
                     minSpotColor: '#b0b0b0', maxSpotColor: '#b0b0b0', disableInteraction: true
                 };
                 grid.elements.parent.find('.OG-g .sp').sparkline('html', options);
+                $('.OG-g .flot').each(function () {
+                    $.plot($(this), JSON.parse($(this).text()), {
+                        colors: ['#456899'],
+                        grid: {borderWidth: 0, minBorderMargin: 1},
+                        series: {shadowSize: 0, lines: {lineWidth: 1}},
+                        xaxis: {show: false}, yaxis: {show: false}
+                    });
+                });
             });
         };
     }
