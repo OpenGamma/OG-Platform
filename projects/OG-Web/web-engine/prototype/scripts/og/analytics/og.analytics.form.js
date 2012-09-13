49fe19ced8b2f4d2a80d02e083339539ed9b9602
Antonio Greco <antonio@opengamma.com>
2012-09-13 17:40:22 +0100

    removed DOM triggers, more tidy approach to event handling for Dropdown

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.js
@@ -7,7 +7,7 @@ $.register_module({
     dependencies: ['og.common.util.ui.AutoCombo', 'og.views.common.layout'],
     obj: function () { 
         return function (selector) {
-            var emitter = new EventEmitter(), Form, FormCombo, Status, handle_error, ds_response = { // dummy
+            var emitter = new EventEmitter(), Form, Dropmenu, Status, handle_error, ds_response = { // dummy
                     type: ['Live', 'Snapsot', 'Historical', 'Data Type'],
                     live: ['Bloomberg', 'Reuters'],
                     snapshot: ['Alan', 'Alan 2'],
@@ -20,31 +20,37 @@ $.register_module({
                         {num: 4, type: 'Data Type', value: 'type 2', last: true}
                     ]
             };
-            FormCombo = function (selector) {
-                var FormCombo = this, Title, Menu;
+            Dropmenu = function (config) {
+                var Dropmenu = this, Title, Menu;
                 return Title = function (){
                     var title = this;
-                    return title.$el = $('.og-option-title', selector).on('click', function (event) {
+                    return title.$el = config.$title.on('click', function (event) {
                             event.stopPropagation();
-                            FormCombo.state === 'open' ? FormCombo.close() : FormCombo.open().focus();
+                            Dropmenu.state === 'open' ? Dropmenu.close() : Dropmenu.open().focus();
                         }), title;
                 },
                 Menu = function () {
-                    var menu = this, title = new Title();
-                    return menu.state = 'closed', menu.$el = $(selector + ' .OG-analytics-form-menu'),
+                    var menu = this, title = new Title(),
+                        events = {
+                            focused:'dropmenu: focused',
+                            opened: 'dropmenu: opened',
+                            closed: 'dropmenu: closed'
+                        };
+                    return menu.state = 'closed', menu.opened = false, menu.$el = config.$menu,
                         menu.focus = function () {
-                            return menu.$el.find('select').first().focus(), menu; 
+                            return menu.$el.find('select').first().focus(), menu.state = 'focused',
+                                    emitter.emitEvent(events.focused, [menu]), menu;
                         },
                         menu.open = function () {
-                            return menu.$el.show().blurkill(menu.close), emitter.emitEvent('menu:open', {
-                                elem: title.$el, state: (menu.state = 'open'), scope: menu}),
-                                title.$el.addClass('og-active'), menu;
+                            return menu.$el.show().blurkill(menu.close), menu.state = 'open', menu.opened = true,
+                                    title.$el.addClass('og-active'), emitter.emitEvent(events.opened, [menu]), menu;
                         },
                         menu.close = function () { 
-                            return (menu.$el ? menu.$el.hide() : null), title.$el.removeClass('og-active'),
-                                menu.state = 'closed', menu;
-                        }, menu;
-                }, Menu.prototype = EventEmitter.prototype, FormCombo = new Menu();
+                            return (menu.$el ? menu.$el.hide() : null), menu.state = 'closed', menu.opened = false,
+                                    title.$el.removeClass('og-active'), emitter.emitEvent(events.closed, [menu]), menu;
+                        },
+                        menu;
+                }, Menu.prototype = EventEmitter.prototype, Dropmenu = new Menu();
             };
             Status = function (selector) {
                 var status = this, interval, ini
@@ -74,8 +80,8 @@ $.register_module({
                 status.status = null;
                 return status;
             };
-            MastHead = function (template, search, aggregators) { // TODO AG: Keep things as DRY as poss!
-                var MastHead = this, Form, ag_menu, ds_menu, vd_s = '.og-view', ag_s = '.og-aggregation',
+            MastHead = function (template, search, aggregators) { //TODO AG: Keep things DRY as poss!
+                var MastHead = this, Form, ag_dropdwn, ds_dropdwn, vd_s = '.og-view', ag_s = '.og-aggregation',
                     ds_s = '.og-datasources', load_s = '.og-load', fcntrls_s = 'input, select, button',
                     $form = $(selector).html(template), $ag = $(ag_s, $form), $ds = $(ds_s, $form),
                     $ag_fcntrls, $ds_fcntrls, $load_btn = $(load_s, $form), status, auto_combo_menu;
@@ -83,58 +89,46 @@ $.register_module({
                     var form = this,
                         keydown_handler = function (event) {
                             if (event.keyCode !== 9) return;
-                            var $elem = $(this), shift_key = event.shiftKey,
-                                trigger = function (menu) {$elem.trigger('tab', menu);},
+                            var $elem = $(this), shift_key = event.shiftKey, menu = '',
                                 active_pos = function (elms, pos) {return $elem.is(elms[pos]())},
                                 view = $elem.closest('.og-view').length,
                                 aggregation = $elem.closest(ag_s).length,
                                 datasources = $elem.closest(ds_s).length,
                                 load = $elem.hasClass('og-load');
-                            if (view && shift_key)
-                                return; //view.find('ui-autocomplete-input').focus();
-                            if (view && !shift_key)
-                                return trigger('aggregation');
-                            if (aggregation && active_pos($ag_fcntrls, 'last') && !shift_key) 
-                                return trigger('datasources');
-                            if (aggregation && active_pos($ag_fcntrls, 'first') && shift_key) 
-                                return trigger('view');
-                            if (datasources && active_pos($ds_fcntrls, 'first') && shift_key) 
-                                return trigger('aggregation');
-                            if (datasources && active_pos($ds_fcntrls, 'last') && !shift_key)
-                                return trigger('load');
-                            if (load && shift_key)
-                                return trigger('datasources');
+                            if (view && shift_key) return;
+                            if (aggregation && active_pos($ag_fcntrls, 'first') && shift_key) menu = 'view';
+                            else if (view && !shift_key) menu = 'aggregation';
+                            else if (aggregation && active_pos($ag_fcntrls, 'last') && !shift_key) menu = 'datasources';
+                            else if (datasources && active_pos($ds_fcntrls, 'first') && shift_key) menu = 'aggregation';
+                            else if (datasources && active_pos($ds_fcntrls, 'last') && !shift_key) menu = 'load';
+                            else if (load && shift_key) menu = 'datasources';
+                            return toggle_menu(menu);
                         },
-                        tab_handler = function (event, type) {
-                            switch (type) {
-                                case 'aggregation': ag_menu.open(); ds_menu.close(); break;
-                                case 'datasources': ds_menu.open(); ag_menu.close(); break;
-                                case 'load':  ag_menu.close(); ds_menu.close(); break;
-                                case 'view': ag_menu.close(); ds_menu.close(); break;
-
+                        button_handler = function(val){
+                            var type = (ag_dropdwn.opened ? 'datasources' : (ds_dropdwn.opened ? 'load' : void 0));
+                            if (val === 'OK') {
+                                toggle_menu(type);
+                                if (type === 'datasources') ds_dropdwn.focus();
+                                else if (type === 'load') $load_btn.focus();
+                            } else if (val === 'Cancel'){
+                                auto_combo_menu.select(); 
+                                toggle_menu('cancel');
                             }
                         },
                         click_handler = function (event) {
-                           var $elem = $(event.srcElement), txt_val = $elem.text(), type;
-                           if ($elem.is('button')) {
-                                type = $elem.parent('.og-menu-datasources') ? 'datasources' :
-                                    ($elem.parent('.og-menu-datasources') ? 'load' : '');    
-                                $(selector).trigger('tab', type);
-                                if (txt_val === 'OK') {
-                                    if (type === 'datasources') ds_menu.focus();
-                                    if (type === 'load') $load_btn.focus();
-                                } else if (txt_val === 'Cancel') { 
-                                    if (type === 'datasources') ds_menu.close(); 
-                                    if (type === 'load') ag_menu.close();
-                                }
-                                auto_combo_menu.select();
+                           var $elem = $(event.srcElement), txt_val = $elem.text();
+                           if ($elem.is('button')) button_handler(txt_val);
+                        },
+                        toggle_menu = function (type) {
+                            switch (type) {
+                                case 'view': ag_dropdwn.close(); break;
+                                case 'aggregation': ag_dropdwn.open(); ds_dropdwn.close(); break;
+                                case 'datasources': ds_dropdwn.open(); ag_dropdwn.close(); break;
+                                case 'load': ag_dropdwn.close(); ds_dropdwn.close(); break;
+                                case 'cancel': ag_dropdwn.close(); ds_dropdwn.close(); break;
                             }
-                            event.preventDefault();
-                            return false;
                         };
-                    return $form.on('keydown', fcntrls_s, keydown_handler)
-                            .on('tab', tab_handler)
-                            .on('click', click_handler),
+                    return $form.on('keydown', fcntrls_s, keydown_handler).on('click', click_handler),
                         auto_combo_menu = new og.common.util.ui.AutoCombo('.OG-analytics-form .og-view',
                             'search...', search.data).on('input autocompletechange autocompleteselect',
                                 function (event, ui) {
@@ -150,17 +144,17 @@ $.register_module({
                             $ag.html($((Handlebars.compile(aggregation_markup))(aggregators.data))),
                             $ds.html($((Handlebars.compile(datasources_markup))(ds_response))),
                             $ag_fcntrls = $ag.find(fcntrls_s), $ds_fcntrls = $ds.find(fcntrls_s),
-                            ag_menu = new FormCombo(ag_s), ds_menu = new FormCombo(ds_s);
-                        }),
-                        emitter.addListener('menu:open', function (elm) {
-                            if (!ag_menu || !ds_menu) return;
-                            var elem = $(elm);
-                            if (elem.closest('.og-view').length) ds_menu.close(), ag_menu.close();
-                            if (elem.closest(ag_s).length) ds_menu.close();
-                            if (elem.closest(ds_s).length) ag_menu.close();
+                            ag_dropdwn = new Dropmenu({
+                                $title: $('.og-option-title', ag_s),
+                                $menu: $('.OG-analytics-form-menu', ag_s)
+                            }), ds_dropdwn = new Dropmenu({
+                                $title:$('.og-option-title', ds_s),
+                                $menu:$('.OG-analytics-form-menu', ds_s)
+                            });
                         }),
                         og.views.common.layout.main.allowOverflow('north'), 
-                        status = new Status(selector + ' .og-status'), form;
+                        status = new Status(selector + ' .og-status'),
+                        form;
                 }, Form.prototype = EventEmitter.prototype, MastHead = new Form();
             };
             $.when(
