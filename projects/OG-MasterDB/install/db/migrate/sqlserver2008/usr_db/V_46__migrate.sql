0b15aee14bc50260548211efaa07b8e81dfec65f
Kevin Vella <kevin@opengamma.com>
2012-09-20 11:22:22 +0100

    Fix usr_db V 46 migration script for SQL Server... third time lucky?

--- projects/OG-MasterDB/install/db/migrate/sqlserver2008/usr_db/V_46__migrate.sql
+++ projects/OG-MasterDB/install/db/migrate/sqlserver2008/usr_db/V_46__migrate.sql
@@ -5,10 +5,10 @@ BEGIN TRANSACTION;
 
   DROP INDEX ix_usr_oguser_name ON usr_oguser;
 
-  ALTER TABLE sec_future ADD userid varchar(255);
+  ALTER TABLE usr_oguser ADD userid varchar(255);
   UPDATE usr_oguser SET userid = name;
-  ALTER TABLE sec_future ALTER COLUMN userid varchar(255) NOT NULL;
-  ALTER TABLE sec_future DROP COLUMN name;
+  ALTER TABLE usr_oguser ALTER COLUMN userid varchar(255) NOT NULL;
+  ALTER TABLE usr_oguser DROP COLUMN name;
 
   ALTER TABLE usr_oguser ADD name varchar(255);
   ALTER TABLE usr_oguser ADD time_zone varchar(255);
