8eaa2fffbb89f6679b06bfb6971b961c96c743d2
Antonio Greco <antonio@opengamma.com>
2012-09-26 17:13:06 +0100

    build aggregation query

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.aggregators_menu.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.aggregators_menu.js
@@ -9,31 +9,20 @@ $.register_module({
                 default_sel_txt = 'select aggregation type...', options_s = '.OG-dropmenu-options';
                 $dom.title_prefix.append('<span>Aggregated by</span>');
                 $dom.title_infix.append('<span>then</span>');
-                select_handler = function (elem) {
-                    var parent = elem.parents(options_s), pos = parent.data('pos'), text, val = elem.val(),
-                        entry = ag_opts.pluck('pos').indexOf(pos);
-                    if (val === default_sel_txt) remove_entry(entry);
-                    else if (entry !== -1) replace_entry(entry, val);
-                    else ag_opts[pos] = {pos:pos, val:val};
-                    process_ag_opts();
-                },
-                checkbox_handler = function (elem) {
-                    /*
-                     var parent = elem.closest(options_s), pos = parent.data('data'),
-                        entry = ag_opts.pluck('pos').indexOf(pos);
-                    if (elem.checked && entry === -1) ag_opts[pos].required_field = true; // probably use add_infix here!?
-                    else if (elem.checked && entry !== -1) ag_opts[entry].required_field = true;
-                    */
-                    elem.focus();
-                },
                 process_ag_opts = function () {
-                    var arr = [], i = 0, query;
-                    ag_opts.forEach(function (entry) { arr[i++] = entry; });
-                    query = arr.reduce(function (a, v, j) {
-                        return a += (j%2) ? $dom.title_infix.html() + " " + v.val : v.val;
-                    }, '');
+                    var i = 0, arr = [], query, str;
+                    ag_opts.sort(sort_ag_opts).forEach(function (entry) {
+                        if (i > 0) arr[i++] = $dom.title_infix.html() + " ";
+                        arr[i++] = entry;
+                    });
+                    query = arr.reduce(function (a, v) {return a += v.val ? v.val : v;}, '');
                     $ag_selection.html(query);
                 },
+                sort_ag_opts = function (a, b) {
+                    if (a.pos < b.pos) return -1;
+                    if (a.pos > b.pos) return 1;
+                    return 0;
+                }
                 replace_entry = function (pos, val) {
                     ag_opts[pos].val = val;
                 },
@@ -45,19 +34,39 @@ $.register_module({
                     var arr = this;
                     if (arr.length > 1) arr.splice(-1, 0, $dom.title_infix.html());
                 },
+                del_handler = function (elem) {
+                    var parent = elem.parents(options_s), pos = parent.data('pos'), 
+                        entry = ag_opts.pluck('pos').indexOf(pos);
+                    menu.del_handler(parent);
+                    remove_entry(entry);
+                    process_ag_opts();
+                },
+                add_handler = function () {
+                    if (data.length === opts.length) return;
+                    menu.add_handler(); 
+                };
+                select_handler = function (elem) {
+                    var parent = elem.parents(options_s), pos = parent.data('pos'), text, val = elem.val(),
+                        entry = ag_opts.pluck('pos').indexOf(pos);
+                    if (val === default_sel_txt) remove_entry(entry);
+                    else if (entry !== -1) replace_entry(entry, val);
+                    else ag_opts.splice(pos, 0, {pos:pos, val:val});
+                    process_ag_opts();
+                },
+                /*checkbox_handler = function (elem) {
+                     var parent = elem.closest(options_s), pos = parent.data('data'),
+                        entry = ag_opts.pluck('pos').indexOf(pos);
+                    if (elem.checked && entry === -1) ag_opts[pos].required_field = true; // probably use add_infix here!?
+                    else if (elem.checked && entry !== -1) ag_opts[entry].required_field = true;
+                    elem.focus();
+                },*/
                 menu_handler = function (event) {
                     var elem = $(event.target);
-                    if (elem.is(menu.$dom.add)) {
-                        if (data.length === opts.length) return;
-                        menu.add_handler(); 
-                        menu.stop(event);
-                    }
-                    if (elem.is('.og-icon-delete')) {
-                        menu.del_handler(elem.closest(options_s));
-                        menu.stop(event);
-                    }
+                    if (elem.is(menu.$dom.add)) add_handler();
+                    if (elem.is('.og-icon-delete')) del_handler(elem);
                     if (elem.is(':checkbox')) checkbox_handler(elem);
                     if (elem.is('select')) select_handler(elem);
+                    menu.stop(event);
                 };
             $dom.title.on('click', menu.title_handler.bind(menu));
             $dom.menu.on('click', menu_handler).on('change', menu_handler);
