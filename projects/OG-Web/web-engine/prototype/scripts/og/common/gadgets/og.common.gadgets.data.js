af508caaae1de18d71215a5b6d7b4be46b18f631
Afshin Darian <afshin@opengamma.com>
2012-10-04 20:27:08 +0100

    minor data gadget update

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.data.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.data.js
@@ -10,11 +10,7 @@ $.register_module({
         return function (config) {
             var gadget = this, instantiated = false, alive = prefix + counter++,
                 css_position = {position: 'absolute', top: '0', left: 0, right: 0, bottom: 0}, $data_grid;
-            gadget.alive = function () {
-                var live = $(config.selector).length;
-                if (!live) gadget.die();
-                return live;
-            };
+            gadget.alive = function () {return $(config.selector).length ? true : gadget.die(), false;};
             gadget.load = function () {
                 $(config.selector).addClass(alive).css(css_position);
                 gadget.dataman = new og.analytics.Cell({source: config.source, col: config.col, row: config.row})
@@ -30,14 +26,12 @@ $.register_module({
                     });
             };
             gadget.die = function () {
-                try {$data_grid.die();} catch (error) {};
-                try {gadget.dataman.kill();} catch (error) {};
+                try {$data_grid.die();} catch (error) {}
+                try {gadget.dataman.kill();} catch (error) {}
             };
             gadget.update = function (input) {$data_grid.update(input);};
+            gadget.resize = function () {try {$data_grid.resize();} catch (error) {};};
             gadget.load();
-            gadget.resize = function () {
-                try {$data_grid.resize();} catch (error) {};
-            }
             if (!config.child) og.common.gadgets.manager.register(gadget);
         };
     }
