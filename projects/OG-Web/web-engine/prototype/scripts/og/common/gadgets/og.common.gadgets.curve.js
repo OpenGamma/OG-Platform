7c76c157460cb749e0b78adce1e22560f9a103f7
Alan Ayoub <alan@opengamma.com>
2012-10-04 13:41:45 +0100

    Small curve gadget refactor

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.curve.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.curve.js
@@ -9,26 +9,24 @@ $.register_module({
         var prefix = 'og_curve_gadget_', counter = 1, module = this;
         return function (config) {
             var gadget = this, curve, alive = prefix + counter++, d;
+            gadget.dataman = new og.analytics.Cell({source: config.source, row: config.row, col: config.col})
+                .on('data', function (data) {
+                    if (data.t !== 'CURVE') return og.dev.warn(module.name + ': data.v should be CURVE');
+                    d = $.isArray(data.v) && [{curve: data.v}];
+                    curve ? curve.update(d) : gadget.resize();
+                });
             gadget.alive = function () {
-                var alive = !!$('.' + alive).length;
-                if (!alive) gadget.dataman.kill();
-                return alive;
-            };
-            gadget.load = function () {
-                gadget.dataman = new og.analytics.Cell({source: config.source, row: config.row, col: config.col})
-                    .on('data', function (data) {
-                        if (data.t !== 'CURVE') return og.dev.warn(module.name + ': data.v should be CURVE');
-                        d = $.isArray(data.v) && [{curve: data.v}];
-                        curve ? curve.update(d) : gadget.resize();
-                    });
+                var live = !!$('.' + alive).length;
+                if (!live) gadget.dataman.kill();
+                return live;
             };
             gadget.resize = function () {
+                if (!$(config.selector).length) return;
                 curve = $(config.selector)
                     .addClass(alive)
                     .css({position: 'absolute', top: 0, left: 0, right: 0, bottom: 0})
                     .ogcurve(d);
             };
-            gadget.load();
             if (!config.child) og.common.gadgets.manager.register(gadget);
         };
     }
