02da803b4760a3fd2595b48d8ffaaa36d3e6c301
Antonio Greco <antonio@opengamma.com>
2012-10-12 13:09:47 +0100

    reordering source options and displaying query fix

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.datasources_menu.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.datasources_menu.js
@@ -4,130 +4,120 @@ $.register_module({
     dependencies: ['og.analytics.DropMenu'],
     obj: function () { 
         return function (config) {
-            var menu = new og.analytics.DropMenu(config), opts = menu.opts, data = menu.data, query_opts = [], ds_val, 
+            var menu = new og.analytics.DropMenu(config), opts = menu.opts, data = menu.data, query = [], ds_val, 
                 type_val, sel_pos, default_type_txt = 'select type...', default_sel_txt = 'select data source...',
-                del_s = '.og-icon-delete', options_s = '.OG-dropmenu-options', dummy_s = '<wrapper>', type_s = '.type', 
+                del_s = '.og-icon-delete', options_s = '.OG-dropmenu-options', wrapper_s = '<wrapper>', type_s = '.type', 
                 ds_s = '.source', select_s = 'select', menu_click_s = 'input, div.og-icon-delete, a.OG-link-add', 
-                $dom = menu.$dom,  $type_select, $ds_select, $sel_parent, $ds_selection, $sel_opt, $snapshot_opts_tmpl,
-                $historical_opts_tmpl,
+                $dom = menu.$dom,  $type_select, $source_select, $parent, $query, $option, $snapshot_opts,
+                $historical_opts,
                 events = {
                     type_reset: 'dropmenu:ds:typereset',
                     type_selected:'dropmenu:ds:typesselected',
                     data_selected: 'dropmenu:ds:dataselected',
                     opts_repositioned: 'dropmenu:ds:optsrespositioned'
                 },
-                populate_type_opts = function (data) {
-                    $sel_parent.data('type', type_val).addClass(type_val);
-                    $ds_select.empty().append($($sel_opt.html()).text(default_sel_txt));
-                    data.forEach(function (d) {$ds_select.append($($sel_opt.html()).text(d.name || d));});
+                populate_type_options = function (data) {
+                    $parent.data('type', type_val).addClass(type_val);
+                    $source_select.empty().append($($option.html()).text(default_sel_txt));
+                    data.forEach(function (d) {$source_select.append($($option.html()).text(d.name || d));});
                 },
                 populate_livedatasources = function () {
                     og.api.rest.livedatasources.get().pipe(function (resp) {
-                        populate_type_opts(resp.data);
+                        populate_type_options(resp.data);
                     });
                 },
                 populate_marketdatasnapshots = function () {
                     og.api.rest.marketdatasnapshots.get().pipe(function (resp) {
-                        populate_type_opts(resp.data[0].snapshots);
+                        populate_type_options(resp.data[0].snapshots);
                     }).pipe(function () {
-                        $ds_select.after($snapshot_opts_tmpl.html());
+                        $source_select.after($snapshot_opts.html());
                     });
                 },
                 populate_historical = function () {
-                    populate_type_opts(['Mock A','Mock B','Mock C', 'Mock D']);
-                    $ds_select.before($historical_opts_tmpl.html());
+                    populate_type_options(['Mock A','Mock B','Mock C', 'Mock D']);
+                    $source_select.before($historical_opts.html());
                 },
-                process_query_opts = function () {
-                    if(query_opts.length) {
+                display_query = function () {
+                    if(query.length) {
                         var i = 0, arr = [];
-                        query_opts.sort(sort_opts).forEach(function (entry) { // revisit the need for sorting this..
+                        query.sort(sort_opts).forEach(function (entry) { // revisit the need for sorting this..
                             if (i > 0) arr[i++] = $dom.title_infix.html() + " ";
                             arr[i++] = entry;
                         });
-                        $ds_selection.html(arr.reduce(function (a, v) {
+                        $query.html(arr.reduce(function (a, v) {
                             return a += v.type ? v.type + ":" + v.ds : v;
                         }, ''));
-                    }
+                    } else $query.text(default_sel_txt);
+                    console.log(query);
                 },
                 sort_opts = function (a, b) {
                     if (a.pos < b.pos) return -1;
                     if (a.pos > b.pos) return 1;
                     return 0;
                 },
-                type_select_handler = function (entry) {
-                    var type = $sel_parent.data('type');
-                    if ($sel_parent.hasClass(type)) remove_orphans(entry, type);
+                remove_entry = function (entry) {
+                    if (query.length === 1) return query.length = 0; // emitEvent; reset_query
+                    query.splice(entry, 1);
+                },  
+                remove_orphans = function (type) {
+                    $parent.removeClass(type ? type : '').find('.extra-opts').remove(); 
+                    $source_select.empty().append($($option.html()).text(default_sel_txt));
+                },
+                reset_query = function () {
+                    return $query.text(default_sel_txt), $type_select.val(default_sel_txt).focus(), 
+                        $source_select.empty().append($($option.html()).text(default_sel_txt)), remove_entry();
+                },
+                type_handler = function (entry) {
+                    var type = $parent.data('type');
+                    if (type_val === default_type_txt){
+                        if (menu.opts.length === 1 && query.length === 1) return reset_query();
+                        return remove_entry(entry), remove_orphans(type), display_query(); // emitEvent; type_selected
+                    }
+                    if ($parent.hasClass(type)) remove_entry(entry), remove_orphans(type), display_query();
                     switch (type_val) {
                         case 'live': populate_livedatasources(); break;
                         case 'snapshot': populate_marketdatasnapshots(); break;
                         case 'historical': populate_historical(); break;
-                        case default_type_txt: {
-                            if (menu.opts.length === 1 && query_opts.length === 1) return reset_query();
-                            return remove_entry(entry), process_query_opts(); // Emit event; type_selected
-                        }
                     }
-                    if (query_opts.length === 0) return $ds_selection.text(default_sel_txt);
                 },
-                ds_select_handler = function (entry) {
-                    if (ds_val === default_sel_txt) {
-                        remove_entry(entry);
-                        if (query_opts.length === 0) return $ds_selection.text(default_sel_txt);
-                    } else if (~entry) query_opts[entry] = {pos:sel_pos, type:type_val, ds:ds_val};
-                    else query_opts.splice(sel_pos, 0, {pos:sel_pos, type:type_val, ds:ds_val});
-                    process_query_opts(); // Emit event; data_selected 
-                },
-                remove_entry = function (entry) {
-                    if (query_opts.length === 1) return query_opts.length = 0; // Emit event; reset_query
-                    query_opts.splice(entry, 1);
-                },  
-                remove_orphans = function (entry, type) {
-                    $sel_parent.removeClass(type ? type : '').find('.extra-opts').remove(); 
-                    $ds_select.empty().append($($sel_opt.html()).text(default_sel_txt));
-                    if (type_val !== default_type_txt || type_val !== type) remove_entry(entry), process_query_opts();
-                },
-                reset_query = function () {
-                    return $ds_selection.text(default_sel_txt), $type_select.val(default_sel_txt).focus(), 
-                        $ds_select.empty().append($($sel_opt.html()).text(default_sel_txt)), remove_entry();
-                },
-                repos_query_opts = function (entry) {
-                    if (menu.opts.length) {
-                        for (var i = ~entry ? entry : sel_pos, len = query_opts.length; i < len; query_opts[i++].pos-=1);
-                        if (~entry) {
-                            remove_entry(entry);
-                            process_query_opts(); // Emit event; opts_repositioned 
-                        }
-                    }
+                source_handler = function (entry) {
+                    if (ds_val === default_sel_txt) return remove_entry(entry), display_query();
+                    else if (~entry) query[entry] = {pos:sel_pos, type:type_val, ds:ds_val};
+                    else query.splice(sel_pos, 0, {pos:sel_pos, type:type_val, ds:ds_val});
+                    display_query(); // emitEvent; data_selected 
                 },
                 add_handler = function () {
                     menu.add_handler(); 
                 },
-                del_handler = function (entry) {
-                    if (menu.opts.length === 1) return remove_orphans(entry, $sel_parent.data('type')), reset_query();
-                    else if (menu.opts.length > 1 && query_opts.length === 1) reset_query();
-                    if ($type_select !== undefined && menu.opts.length > 1) menu.del_handler($sel_parent);
-                    repos_query_opts(entry);    
+                delete_handler = function (entry) {
+                    if (menu.opts.length === 1) {
+                        return remove_entry(entry), remove_orphans($parent.data('type')), reset_query();
+                    }
+                    if ($type_select !== undefined) menu.del_handler($parent);
+                    if (menu.opts.length) {
+                        for (var i = ~entry ? entry : sel_pos, len = query.length; i < len; query[i++].pos -= 1);
+                        if (~entry) return remove_entry(entry), display_query(); // emitEvent; opts_repositioned 
+                    }
                 },
                 menu_handler = function (event) {
-                    var target = event.srcElement || event.target,
-                        elem = $(target), entry;
-                    $sel_parent = elem.parents(options_s);
-                    $type_select = $sel_parent.find(type_s);
-                    $ds_select = $sel_parent.find(ds_s);
+                    var elem = $(event.srcElement || event.target), entry;
+                    $parent = elem.parents(options_s);
+                    $type_select = $parent.find(type_s);
+                    $source_select = $parent.find(ds_s);
                     type_val = $type_select.val();
-                    ds_val = $ds_select.val();
-                    sel_pos = $sel_parent.data('pos');
-                    entry = (query_opts.pluck('pos').indexOf(sel_pos));
+                    ds_val = $source_select.val();
+                    sel_pos = $parent.data('pos');
+                    entry = (query.pluck('pos').indexOf(sel_pos));
                     if (elem.is(menu.$dom.add)) return add_handler();
-                    if (elem.is(del_s)) return del_handler(entry);
-                    if (elem.is(select_s+'.'+type_s)) 
-                        return type_val = type_val.toLowerCase(), type_select_handler(entry);
-                    if (elem.is(select_s+'.'+ds_s)) return ds_select_handler(entry);
+                    if (elem.is(del_s)) return delete_handler(entry);
+                    if (elem.is(select_s+'.'+type_s)) return type_val = type_val.toLowerCase(), type_handler(entry);
+                    if (elem.is(select_s+'.'+ds_s)) return source_handler(entry);
                 };
             if ($dom) {
                 $dom.title_infix.append('<span>then</span>'); // Move to DropMenu class
-                $ds_selection = $('.datasources-selection', $dom.title);
-                $sel_opt = $(dummy_s).append('<option>');
-                $snapshot_opts_tmpl = $(dummy_s).append([
+                $query = $('.datasources-query', $dom.title);
+                $option = $(wrapper_s).append('<option>');
+                $snapshot_opts = $(wrapper_s).append([
                     '<div class="extra-opts">',
                         '<span>Versions:</span>',
                         '<button class="latest active">Latest</button>', 
@@ -137,7 +127,7 @@ $.register_module({
                         '<button class="custom">Custom</button>',
                     '</div>'
                 ].join(''));
-                $historical_opts_tmpl = $(dummy_s).append([
+                $historical_opts = $(wrapper_s).append([
                     '<div class="extra-opts">',
                         '<input class="OG-js-datetimepicker og-datetimepicker" />',
                     '</div>'
