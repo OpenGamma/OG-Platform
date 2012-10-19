736eccd6a7aaadcd96baf82d5f4090ff4b8bbe6a
Alan Ayoub <alan@opengamma.com>
2012-10-19 13:30:53 +0100

    dont load surface if the data isnt plotable

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.surface.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.surface.js
@@ -16,6 +16,7 @@ $.register_module({
             $(config.selector).addClass(alive).css({position: 'absolute', top: 0, left: 0, right: 0, bottom: 0});
             gadget.dataman = new og.analytics.Cell({source: config.source, row: config.row, col: config.col})
                 .on('data', function (data) {
+                    if (!data.x_values || !data.y_values) return /* TODO: load in data gadget instead */;
                     surface_options.data = {
                         vol: data.vol,
                         xs: data.x_values,
@@ -34,7 +35,7 @@ $.register_module({
                         };
                         gadget.resize = surface.resize;
                         if (!config.child) og.common.gadgets.manager.register(gadget);
-                    } else surface.surface.update_surface_plane(surface_options.data);
+                    } else surface.update_surface_plane(surface_options.data);
                 });
         }
     }
