78d050c4383b097fa442983d2c87fcc63357c8e3
Antonio Greco <antonio@opengamma.com>
2012-10-26 18:37:12 +0100

    initial replay query form from loaded url

--- projects/OG-Web/web-engine/prototype/scripts/og/views/og.views.analytics2.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/og.views.analytics2.js
@@ -4,6 +4,7 @@
  */
 $.register_module({
     name: 'og.views.analytics2',
+    dependencies: ['og.analytics.form'],
     obj: function () {
         var routes = og.common.routes, module = this, view, default_template, default_object,
             main_selector = '.OG-layout-analytics-center', form = og.analytics.form('.OG-layout-analytics-masthead');
@@ -11,22 +12,27 @@ $.register_module({
         return view = {
             check_state: og.views.common.state.check.partial('/'),
             default_details: function () {
-                form.reset_query();
+                if (og.analytics.url.last.main) form.replay_query(og.analytics.url.last.main);
                 og.analytics.containers.initialize();
                 og.api.text({module: 'og.analytics.grid.configure_tash'}).pipe(function (markup) {
                     default_template = Handlebars.compile(markup);
                     $(main_selector).html(default_template(default_object));
-                    og.analytics.form('.OG-layout-analytics-masthead');
                 });
             },
             load: function (args) {
+                console.log('load');
                 var new_page = false;
+                view.check_state({args: args, conditions: [
+                    {new_page: function () {new_page = true; view.default_details();}}
+                ]});
                 og.analytics.resize();
-                if (og.analytics.url.last.main) form.replay_query(og.analytics.url.last.main);
-                if (!new_page && !args.data && og.analytics.url.last.main)
+                if (!new_page && !args.data && og.analytics.url.last.main) {
+                    form.replay_query(og.analytics.url.last.main);
                     og.analytics.url.clear_main(), $(main_selector).html(default_template(default_object));
+                }
             },
             load_item: function (args) {
+                console.log('load_item');
                 view.check_state({args: args, conditions: [{new_page: view.load}]});
                 og.analytics.url.process(args, function () {
                     if (og.analytics.url.last.main) form.replay_query(og.analytics.url.last.main);
