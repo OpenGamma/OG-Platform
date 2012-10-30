0753a0d7cb199b822293d9d0154915838a3afae0
Ian Turner <ian@opengamma.com>
2012-10-30 17:06:12 +0000

    more inplace gadget view template

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.cellmenu.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.cellmenu.js
@@ -17,57 +17,63 @@ $.register_module({
                 inplace, inplace_config;
             if (og.analytics.containers.initialize) throw new Error(module.name + ': there are no panels');
             og.api.text({module: 'og.analytics.cell_options'}).pipe(function (template) {
-
-
-
-                (cellmenu.menu = $(template)).hide().on('mouseleave', function () {
+                (cellmenu.menu = $(template)).hide()
+                .on('mouseleave', function () {
                     clearTimeout(timer), cellmenu.menu.removeClass(expand_class), cellmenu.hide();
-                }).on('mouseenter', open_icon, function () {
-                    timer = clearTimeout(timer), setTimeout(function () {cellmenu.menu.addClass(expand_class);}, 500);
-                }).on('mouseenter', function () {
+                })
+                .on('mouseenter', open_icon, function () {
+                    clearTimeout(timer), timer = setTimeout(function () {cellmenu.menu.addClass(expand_class);}, 500);
+                })
+                .on('mouseenter', function () {
+                    console.log('mouseenter');
                     $.data(cellmenu, 'hover', true);
-                }).on('click', open_icon, function () {
+                })
+                .on('click', open_icon, function () {
                     cellmenu.menu.addClass(expand_class);
-                /*}).on('click',  inplace.$dom.toggle, function () {
-                    inplace.toggle_handler.bind(inplace);
-                    var panel, options = mapping.options(cellmenu.current, grid, panel);
-
-                    og.analytics.url.launch(options);
-                    console.log('open dialog in place here');
-*/
-                }).on('mouseenter', icons, function () {
+                })
+                .on('mouseenter', icons, function () {
                     var panel = panels[$(this).text() - 1];
                     panels.forEach(function (val) {og.analytics.containers[val].highlight(true, val === panel);});
-                }).on('mouseleave', icons, function () {
+                })
+                .on('mouseleave', icons, function () {
                     panels.forEach(function (val) {og.analytics.containers[val].highlight(false);});
-                }).on('click', icons, function () {
+                })
+                .on('click', icons, function () {
                     var panel = panels[+$(this).text() - 1], cell = cellmenu.current,
                         options = mapping.options(cell, grid, panel);
                     cellmenu.hide();
                     if (!panel) og.analytics.url.launch(options); else og.analytics.url.add(panel, options);
                 });
                 grid.on('cellhoverin', function (cell) {
-                    var type = cell.type, hide = !(cellmenu.current = cell).value  || (cell.col < (depgraph ? 1 : 2)) || (cell.right > parent.width())                       || (depgraph && $.inArray(type, onlydepgraphs) > -1);
+                    cellmenu.menu.removeClass(expand_class);
+                    clearTimeout(timer);
+                    var type = cell.type, hide = !(cellmenu.current = cell).value
+                        || (cell.col < (depgraph ? 1 : 2)) || (cell.right > parent.width())
+                        || (depgraph && $.inArray(type, onlydepgraphs) > -1);
                     if (hide) cellmenu.hide(); else cellmenu.show();
                 }).on('cellhoverout', function () {
-                    setTimeout(function () {if(!$(cellmenu).data('hover')) cellmenu.hide();}, 100);
+                    console.log('cellhoverout', cellmenu.menu.is(':hover'));
+                    clearTimeout(timer);
+                    setTimeout(function () {if (!cellmenu.menu.is(':hover')) {cellmenu.hide();}});
                 });
-            });
-            og.api.text({module: 'og.analytics.inplace_tash'}).pipe(function (template) {
-                console.log(template);
-                inplace_config = ({$cntr:  $('.og-icon-down-chevron', cellmenu.menu), tmpl: template});
-                inplace = new og.common.util.ui.DropMenu(inplace_config);
+
+                og.api.text({module: 'og.analytics.inplace_tash'}).pipe(function (t) {
+                    inplace_config = ({$cntr:  $('.og-inplace', cellmenu.menu), tmpl: t});
+                    inplace = new og.common.util.ui.DropMenu(inplace_config);
+                    inplace.$dom.toggle.on('click', inplace.toggle_handler.bind(inplace));
+                });
+
             });
 
 
 
         };
         constructor.prototype.hide = function () {
-           /* var cellmenu = this;
+           var cellmenu = this;
             if (cellmenu.menu && cellmenu.menu.length) {
                 cellmenu.menu.hide();
                 $.data(cellmenu, 'hover', false);
-            }*/
+            }
         };
         constructor.prototype.show = function () {
             var cellmenu = this, current = this.current;
