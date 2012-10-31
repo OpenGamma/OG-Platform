9611c08d350e3f44b9e376a26220a88c06e08618
Ian Turner <ian@opengamma.com>
2012-10-31 18:46:56 +0000

    menu opening in place

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.cellmenu.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.cellmenu.js
@@ -6,15 +6,20 @@ $.register_module({
     name: 'og.analytics.CellMenu',
     dependencies: ['og.common.gadgets.mapping'],
     obj: function () {
-        var module = this, icons = '.og-num, .og-icon-new-window-2', open_icon = '.og-small', 
-            open_inplace = '.og-icon-down-chevron', expand_class = 'og-expanded', 
-            panels = ['south', 'dock-north', 'dock-center', 'dock-south'], width = 34,
-            mapping = og.common.gadgets.mapping, type_map = mapping.data_type_map,
+        var module = this, 
+            icons = '.og-num, .og-icon-new-window-2', 
+            open_icon = '.og-small', 
+            open_inplace = '.og-icon-down-chevron', 
+            expand_class = 'og-expanded', 
+            panels = ['south', 'dock-north', 'dock-center', 'dock-south'], 
+            width = 34,
+            mapping = og.common.gadgets.mapping, 
+            type_map = mapping.data_type_map,
             onlydepgraphs = Object.keys(type_map) // a list of datatypes that only support depgraph gadgets
                 .filter(function (key) {return type_map[key].length === 1 && type_map[key][0] === 0});
         var constructor = function (grid) {
             var cellmenu = this, timer, depgraph = !!grid.config.source.depgraph, parent = grid.elements.parent,
-                inplace, inplace_config;
+                inplace_config; cellmenu.grid = grid;
             if (og.analytics.containers.initialize) throw new Error(module.name + ': there are no panels');
             og.api.text({module: 'og.analytics.cell_options'}).pipe(function (template) {
                 (cellmenu.menu = $(template)).hide()
@@ -55,25 +60,39 @@ $.register_module({
                     setTimeout(function () {if (!cellmenu.menu.is(':hover')) {cellmenu.hide();}});
                 });
 
-                og.api.text({module: 'og.analytics.inplace_tash'}).pipe(function (t) {
-                    inplace_config = ({$cntr:  $('.og-inplace', cellmenu.menu), tmpl: t});
-                    inplace = new og.common.util.ui.DropMenu(inplace_config);
-                    inplace.$dom.toggle.on('click', function() {
-                       inplace.toggle_handler.bind(inplace);
+                og.api.text({module: 'og.analytics.inplace_tash'}).pipe(function (tmpl_inplace) {
+                    $.when(
+                        inplace_config = ({$cntr:  $('.og-inplace', cellmenu.menu), tmpl: tmpl_inplace}))
+                    .then(function(){
+                        cellmenu.inplace = new og.common.util.ui.DropMenu(inplace_config);
+                        container = new og.common.gadgets.GadgetsContainer('.OG-analytics-inplace-', 'container');
+                        container.init();
+                        cellmenu.inplace.$dom.toggle.on('click', function() {
+                            cellmenu.inplace.toggle();
+                            cellmenu.create_inplace();
+                        });
+
                     });
+
                 });
 
             });
-
-
-
+        };
+        constructor.prototype.create_inplace = function () {
+            var cellmenu = this, cell = cellmenu.current, panel = 'inplace', options;
+            options = mapping.options(cell, cellmenu.grid, panel);
+            container.add([options]);
+            //freeze this menu
+            //create new menu for grid
+            //add new gadget to container
         };
         constructor.prototype.hide = function () {
-           var cellmenu = this;
+          /* var cellmenu = this;
             if (cellmenu.menu && cellmenu.menu.length) {
                 cellmenu.menu.hide();
+                cellmenu.inplace.close();
                 $.data(cellmenu, 'hover', false);
-            }
+            }*/
         };
         constructor.prototype.show = function () {
             var cellmenu = this, current = this.current;
