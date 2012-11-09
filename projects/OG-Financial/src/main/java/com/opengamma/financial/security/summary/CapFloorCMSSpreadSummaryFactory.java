22c826a71b6389afea97c8a5454937d6cb826f84
Jonathan Senior <jonathan@opengamma.com>
2012-11-09 11:13:06 +0000

    Moving security summary factories from OG-Excel to OG-Financial for re-use elsewhere

---
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/security/summary/CapFloorCMSSpreadSummaryFactory.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.security.summary;
+
+import com.opengamma.financial.security.capfloor.CapFloorCMSSpreadSecurity;
+
+/**
+ * Summary factory for {@link CapFloorCMSSpreadSecurity}.
+ */
+public class CapFloorCMSSpreadSummaryFactory implements SummaryFactory<CapFloorCMSSpreadSecurity> {
+
+  @Override
+  public String getSecurityType() {
+    return CapFloorCMSSpreadSecurity.SECURITY_TYPE;
+  }
+  
+  @Override
+  public Summary getSummary(CapFloorCMSSpreadSecurity security) {
+    return SummaryBuilder.create(security)
+        .with(SummaryField.DESCRIPTION, security.getLongId() + "/" + security.getShortId())
+        .with(SummaryField.START, security.getStartDate())
+        .with(SummaryField.MATURITY, security.getMaturityDate())
+        .with(SummaryField.NOTIONAL, security.getNotional())
+        .with(SummaryField.STRIKE, security.getStrike())
+        .with(SummaryField.FREQUENCY, security.getFrequency().getConventionName()).build();
+  }
+
+}
