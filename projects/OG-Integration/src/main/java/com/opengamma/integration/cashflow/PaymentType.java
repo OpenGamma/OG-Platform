39d6c1241dcf006033e49bb3173a88f07ce5e323
Jonathan Senior <jonathan@opengamma.com>
2012-11-09 11:14:51 +0000

    Adding payment service

---
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/cashflow/PaymentType.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.integration.cashflow;
+
+/**
+ * 
+ */
+public enum PaymentType {
+
+  /**
+   * 
+   */
+  FIXED,
+  /**
+   * 
+   */
+  FLOAT
+  
+}
