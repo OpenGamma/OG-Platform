cd687609767d10b1f4e2a74f1a234ee458755330
Afshin Darian <afshin@opengamma.com>
2012-11-14 13:00:38 +0000

    superfluous setTimeout

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.cellmenu.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.cellmenu.js
@@ -90,7 +90,7 @@ $.register_module({
         };
         constructor.prototype.destroy_frozen = function () {
            $('.OG-cell-options.og-frozen').remove();
-           setTimeout(og.common.gadgets.manager.clean);
+           og.common.gadgets.manager.clean();
         };
         constructor.prototype.create_inplace = function () {
             var cellmenu = this, panel = 'inplace', options, cell = cellmenu.current, fingerprint,
