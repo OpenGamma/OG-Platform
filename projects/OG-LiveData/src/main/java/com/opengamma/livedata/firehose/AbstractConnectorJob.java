ef057354edc1000f8d1ce0e1cbd17648f5a8d715
Kevin Vella <kevin@opengamma.com>
2012-11-27 17:37:17 +0100

    Minor change to debug msg

--- projects/OG-LiveData/src/main/java/com/opengamma/livedata/firehose/AbstractConnectorJob.java
+++ projects/OG-LiveData/src/main/java/com/opengamma/livedata/firehose/AbstractConnectorJob.java
@@ -146,16 +146,16 @@ public abstract class AbstractConnectorJob<Record> implements Runnable {
           final BlockingQueue<Object> records = decoder.getQueue();
           try {
 
-            s_logger.debug("Chunker about to take a record from queue (size " + records.size() + ")");
+            s_logger.debug("Chunker about to take a record from queue (size {})", records.size());
             Object record = records.take();
-            s_logger.debug("Chunker decoder took a record from queue (size " + records.size() + ")");
+            s_logger.debug("Chunker decoder took a record from queue (size {})", records.size());
 
             while (record != s_eof) {
-              s_logger.debug("Chunker about to invoke call-back method to send record " + record + " on MQ");
+              s_logger.debug("Chunker about to invoke call-back method to send record {} on MQ", record);
               getCallback().received((Record) record);
-              s_logger.debug("Call-back completed, chunker about to take another record from queue (size " + records.size() + ")");
+              s_logger.debug("Call-back completed, chunker about to take another record from queue (size {})", records.size());
               record = records.take();
-              s_logger.debug("Chunker decoder took another record from queue (size " + records.size() + ")");
+              s_logger.debug("Chunker decoder took another record from queue (size {})", records.size());
             }
           } catch (InterruptedException e) {
             throw new OpenGammaRuntimeException("Interrupted", e);
