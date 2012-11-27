b9230dec6086904f5b3f536b9e6cfa396633f76e
Stuart Wakefield <stuart.wakefield@opengamma.com>
2012-11-27 17:04:26 +0000

    Add CommodityFutureOption calculators

---
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/commodity/calculator/CommodityFutureOptionForwardDeltaCalculator.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.analytics.financial.commodity.calculator;
+
+import com.opengamma.analytics.financial.commodity.derivative.AgricultureFutureOption;
+import com.opengamma.analytics.financial.commodity.derivative.EnergyFutureOption;
+import com.opengamma.analytics.financial.commodity.derivative.MetalFutureOption;
+import com.opengamma.analytics.financial.equity.StaticReplicationDataBundle;
+import com.opengamma.analytics.financial.interestrate.InstrumentDerivativeVisitorAdapter;
+import com.opengamma.util.ArgumentChecker;
+
+/**
+ *
+ */
+public class CommodityFutureOptionForwardDeltaCalculator extends InstrumentDerivativeVisitorAdapter<StaticReplicationDataBundle, Double> {
+
+  private static final CommodityFutureOptionForwardDeltaCalculator s_instance = new CommodityFutureOptionForwardDeltaCalculator();
+  private static final CommodityFutureOptionBlackMethod PRICER = CommodityFutureOptionBlackMethod.getInstance();
+
+  public static CommodityFutureOptionForwardDeltaCalculator getInstance() {
+    return s_instance;
+  }
+
+  /**
+   *
+   */
+  public CommodityFutureOptionForwardDeltaCalculator() {
+  }
+
+  @Override
+  public Double visitAgricultureFutureOption(final AgricultureFutureOption derivative, final StaticReplicationDataBundle data) {
+    ArgumentChecker.notNull(derivative, "derivative");
+    ArgumentChecker.notNull(data, "data");
+    return PRICER.forwardDelta(derivative, data);
+  }
+
+  @Override
+  public Double visitEnergyFutureOption(final EnergyFutureOption derivative, final StaticReplicationDataBundle data) {
+    ArgumentChecker.notNull(derivative, "derivative");
+    ArgumentChecker.notNull(data, "data");
+    return PRICER.forwardDelta(derivative, data);
+  }
+
+  @Override
+  public Double visitMetalFutureOption(final MetalFutureOption derivative, final StaticReplicationDataBundle data) {
+    ArgumentChecker.notNull(derivative, "derivative");
+    ArgumentChecker.notNull(data, "data");
+    return PRICER.forwardDelta(derivative, data);
+  }
+
+  @Override
+  public Double visitAgricultureFutureOption(final AgricultureFutureOption derivative) {
+    throw new UnsupportedOperationException("This visitor (" + this.getClass() + ") does not support an visitAgricultureFutureOption without a StaticReplicationDataBundle");
+  }
+
+  @Override
+  public Double visitEnergyFutureOption(final EnergyFutureOption derivative) {
+    throw new UnsupportedOperationException("This visitor (" + this.getClass() + ") does not support an visitEnergyFutureOption without a StaticReplicationDataBundle");
+  }
+
+  @Override
+  public Double visitMetalFutureOption(final MetalFutureOption derivative) {
+    throw new UnsupportedOperationException("This visitor (" + this.getClass() + ") does not support an visitMetalFutureOption without a StaticReplicationDataBundle");
+  }
+
+}
