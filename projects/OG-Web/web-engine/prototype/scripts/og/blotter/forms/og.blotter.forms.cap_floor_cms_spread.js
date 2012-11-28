65e48c2255cd0df5bda8b504d7bc6f2d709f9747
Ian Turner <ian@opengamma.com>
2012-11-28 12:15:12 +0000

    cap floor forms

---
+++ projects/OG-Web/web-engine/prototype/scripts/og/blotter/forms/og.blotter.forms.cap_floor_cms_spread.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * Please see distribution for license.
+ */
+$.register_module({
+    name: 'og.blotter.forms.Cap_floor_cms_spread',
+    dependencies: [],
+    obj: function () {   
+        return function () {
+            var contructor = this;
+            contructor.load = function () {
+                var config = {}, dialog; 
+                config.title = 'Cap/Floor';
+                var form = new og.common.util.ui.Form({
+                    module: 'og.blotter.forms.blocks.cap_floor_tash',
+                    data: {},
+                    type_map: {},
+                    selector: '.OG-blotter-form-block',
+                    extras:{}
+                });
+                form.dom();
+                $('.OG-blotter-form-title').html(config.title);
+            }; 
+            contructor.load();
+            contructor.kill = function () {
+            };
+        };
+    }
+});
