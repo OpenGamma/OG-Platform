8e9f3e08a55d83f2d25bbba8ff9dba767e63a233
Afshin Darian <afshin@opengamma.com>
2012-12-04 19:11:09 +0000

    switching a string replace with a handlebars template

--- projects/OG-Web/web-engine/prototype/scripts/og/common/util/ui/og.common.util.ui.attributes.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/util/ui/og.common.util.ui.attributes.js
@@ -7,26 +7,26 @@ $.register_module({
     dependencies: ['og.common.util.ui.Form'],
     obj: function () {
         var module = this, Block = og.common.util.ui.Block, add_list = '.og-attributes-add-list',
-        attribute = '<li><div class="og-del og-js-rem"></div>{KEY} = {VALUE}</li>';
+        attribute = Handlebars.compile('<li><div class="og-del og-js-rem"></div>{{{key}}} = {{{value}}}</li>');
         var Attributes = function (config) {
             var block = this, attr, form = config.form;
             attr = {module: 'og.views.forms.attributes_tash'};
             form.Block.call(block, attr);
             block.on('form:load', function () {              
-                $(add_list).find('.og-js-rem').on('click').click(function (e) {$(e.target).parent().remove();});    
-            }).on('click', '.og-js-add-attribute', function (e) {
-                    e.preventDefault();
-                    var $group = $(e.target).parent();
+                $(add_list).find('.og-js-rem').on('click').click(function (event) {$(event.target).parent().remove();});
+            }).on('click', '.og-js-add-attribute', function (event) {
+                    event.preventDefault();
+                    var $group = $(event.target).parent();
                     if (!$group.find('[name=attr_key]').val() || !$group.find('[name=attr_val]').val()) return;
-                    $(add_list).prepend(attribute
-                        .replace('{KEY}', $group.find('[name=attr_key]').val())
-                        .replace('{VALUE}', $group.find('[name=attr_val]').val())
-                    );
+                    $(add_list).prepend(attribute({
+                        key: $group.find('[name=attr_key]').val(),
+                        value: $group.find('[name=attr_val]').val()
+                    }));
                     $group.find('[name^=attr]').val('');
                     block.load();
             });
         };
-        Attributes.prototype = new Block(); // inherit Block prototype
+        Attributes.prototype = new Block; // inherit Block prototype
         return Attributes;
     }
 });
