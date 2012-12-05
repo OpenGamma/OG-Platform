c14c17a140bf3145d344916fd0d59a9fb6169806
Andrew Griffin <andrew@opengamma.com>
2012-12-05 11:38:06 +0000

    Adjust PnL functions to handle absent currency constraints if position values are of the same currency.

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/equity/portfoliotheory/PortfolioEquityPnLFunction.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/equity/portfoliotheory/PortfolioEquityPnLFunction.java
@@ -12,38 +12,36 @@ import com.opengamma.core.position.Position;
 import com.opengamma.core.position.Trade;
 import com.opengamma.core.position.impl.PositionAccumulator;
 import com.opengamma.engine.ComputationTarget;
-import com.opengamma.engine.ComputationTargetType;
 import com.opengamma.engine.function.FunctionCompilationContext;
 import com.opengamma.financial.analytics.model.pnl.AbstractPortfolioPnLFunction;
 import com.opengamma.financial.security.equity.EquitySecurity;
 
 /**
- * 
+ *
  */
 public class PortfolioEquityPnLFunction extends AbstractPortfolioPnLFunction {
-  
+
   @Override
-  public boolean canApplyTo(FunctionCompilationContext context, ComputationTarget target) {
-    if (target.getType() == ComputationTargetType.PORTFOLIO_NODE) {
-      final PortfolioNode node = target.getPortfolioNode();
-      final Set<Position> allPositions = PositionAccumulator.getAccumulatedPositions(node);
-      for (Position position : allPositions) {
-        if (position.getSecurity() instanceof EquitySecurity) {
-          for (Trade trade : position.getTrades()) {
-            if (!(trade.getSecurity() instanceof EquitySecurity)) {
-              return false;
-            }
+  public boolean canApplyTo(final FunctionCompilationContext context, final ComputationTarget target) {
+    final PortfolioNode node = target.getPortfolioNode();
+    final Set<Position> allPositions = PositionAccumulator.getAccumulatedPositions(node);
+    for (final Position position : allPositions) {
+      if (position.getSecurity() instanceof EquitySecurity) {
+        for (final Trade trade : position.getTrades()) {
+          if (!(trade.getSecurity() instanceof EquitySecurity)) {
+            return false;
           }
-          return true;
-        } 
+        }
+      } else {
+        return false;
       }
     }
-    return false;
+    return true;
   }
-  
+
   @Override
   public String getShortName() {
     return "PortfolioDailyEquityPnL";
   }
-  
+
 }
