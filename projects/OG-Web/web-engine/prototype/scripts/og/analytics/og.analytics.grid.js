5351e06316be6d80a2e93655b6e8da0f0364539c
Afshin Darian <afshin@opengamma.com>
2012-12-11 14:06:54 +0000

    changes relating to new col types

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.grid.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.grid.js
@@ -11,7 +11,7 @@ $.register_module({
                 var html = '<div style="width: 100px; height: 100px; position: absolute; \
                     visibility: hidden; overflow: auto; left: -10000px; z-index: -10000; bottom: 100px" />';
                 return 100 - $(html).appendTo('body').append('<div />').find('div').css('height', '200px').width();
-            })(), do_not_expand = {DOUBLE: null, PRIMITIVE: null};
+            })(), do_not_expand = {DOUBLE: null, NODE_ID: null, POSITION_ID: null, STRING: null};
         var available = (function () {
             var nodes;
             var all = function (total) {
