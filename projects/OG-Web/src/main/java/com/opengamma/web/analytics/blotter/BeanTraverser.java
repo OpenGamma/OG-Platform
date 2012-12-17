a77ded62819dd313057e112362e552fba47293b2
Chris Kent <chris@opengamma.com>
2012-12-17 16:47:44 +0000

    [PLAT-2889] Collect exceptions during bean traversal and throw at end

--- projects/OG-Web/src/main/java/com/opengamma/web/analytics/blotter/BeanTraverser.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/analytics/blotter/BeanTraverser.java
@@ -16,6 +16,9 @@ import org.joda.beans.Bean;
 import org.joda.beans.MetaBean;
 import org.joda.beans.MetaProperty;
 
+import com.google.common.collect.Lists;
+import com.opengamma.util.ArgumentChecker;
+
 /**
  *
  */
@@ -29,28 +32,42 @@ import org.joda.beans.MetaProperty;
 
   /* package */ BeanTraverser(BeanVisitorDecorator... decorators) {
     _decorators = Arrays.asList(decorators);
+    // first decorator in the list should be on the outside, need to reverse before wrapping
+    Collections.reverse(_decorators);
   }
 
+  // TODO should this have a type param? if the visitor is decorated the return type doesn't have to be T
+  // the return type is decided by the outermost decorator
+  // is there a way to solve this without severely limiting what the decorators can do or being too complicated?
   /* package */ Object traverse(MetaBean bean, BeanVisitor<?> visitor) {
     BeanVisitor<?> decoratedVisitor = decorate(visitor);
     decoratedVisitor.visitBean(bean);
+    List<Failure> failures = Lists.newArrayList();
     for (MetaProperty<?> property : bean.metaPropertyIterable()) {
       Class<?> propertyType = property.propertyType();
-      if (Bean.class.isAssignableFrom(propertyType)) {
-        decoratedVisitor.visitBeanProperty(property, this);
-      } else if (Set.class.isAssignableFrom(propertyType)) {
-        decoratedVisitor.visitSetProperty(property);
-      } else if (List.class.isAssignableFrom(propertyType)) {
-        decoratedVisitor.visitListProperty(property);
-      } else if (Collection.class.isAssignableFrom(propertyType)) {
-        decoratedVisitor.visitCollectionProperty(property);
-      } else if (Map.class.isAssignableFrom(propertyType)) {
-        decoratedVisitor.visitMapProperty(property);
-      } else {
-        decoratedVisitor.visitProperty(property);
+      try {
+        if (Bean.class.isAssignableFrom(propertyType)) {
+          decoratedVisitor.visitBeanProperty(property, this);
+        } else if (Set.class.isAssignableFrom(propertyType)) {
+          decoratedVisitor.visitSetProperty(property);
+        } else if (List.class.isAssignableFrom(propertyType)) {
+          decoratedVisitor.visitListProperty(property);
+        } else if (Collection.class.isAssignableFrom(propertyType)) {
+          decoratedVisitor.visitCollectionProperty(property);
+        } else if (Map.class.isAssignableFrom(propertyType)) {
+          decoratedVisitor.visitMapProperty(property);
+        } else {
+          decoratedVisitor.visitProperty(property);
+        }
+      } catch (Exception e) {
+        failures.add(new Failure(e, property));
       }
     }
-    return decoratedVisitor.finish();
+    if (failures.isEmpty()) {
+      return decoratedVisitor.finish();
+    } else {
+      throw new TraversalException(failures);
+    }
   }
 
   private BeanVisitor<?> decorate(BeanVisitor<?> visitor) {
@@ -59,5 +76,41 @@ import org.joda.beans.MetaProperty;
       decoratedVisitor = decorator.decorate(decoratedVisitor);
     }
     return decoratedVisitor;
+  }
+
+  /* package */ static final class Failure {
+
+    private final Exception _exception;
+    private final MetaProperty<?> _property;
+
+    private Failure(Exception exception, MetaProperty<?> property) {
+      ArgumentChecker.notNull(exception, "exception");
+      ArgumentChecker.notNull(property, "property");
+      _exception = exception;
+      _property = property;
+    }
+
+    /* package */ Exception getException() {
+      return _exception;
+    }
+
+    /* package */ MetaProperty<?> getProperty() {
+      return _property;
+    }
+  }
+
+  /* package */ static final class TraversalException extends RuntimeException {
+
+    private final List<Failure> _failures;
+
+    /* package */ TraversalException(List<Failure> failures) {
+      super("Bean traversal failed");
+      ArgumentChecker.notEmpty(failures, "failures");
+      _failures = failures;
+    }
+
+    /* package */ List<Failure> getFailures() {
+      return _failures;
+    }
   }
 }
