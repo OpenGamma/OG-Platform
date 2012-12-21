e4ea92886bdd064a381d647579fb4d316577a7a6
Craig <craig@opengamma.com>
2012-12-21 14:10:10 +0000

    Minor change. Added a bit more detail to the Central Counterparty class

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/credit/centralcounterparty/CentralCounterpartyDefinition.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/credit/centralcounterparty/CentralCounterpartyDefinition.java
@@ -5,9 +5,37 @@
  */
 package com.opengamma.analytics.financial.credit.centralcounterparty;
 
+import com.opengamma.util.ArgumentChecker;
+
 /**
  * Class to define the characteristics of a central counterparty (for clearing of trades) e.g. ICE or CME
  */
 public class CentralCounterpartyDefinition {
 
+  // ----------------------------------------------------------------------------------------------------------------------------------------
+
+  // The name of the Central Counterparty
+  private final String _ccpName;
+
+  // ----------------------------------------------------------------------------------------------------------------------------------------
+
+  public CentralCounterpartyDefinition(
+      final String ccpName) {
+
+    // ----------------------------------------------------------------------------------------------------------------------------------------
+
+    ArgumentChecker.notNull(ccpName, "Central Counterparty name");
+
+    // ----------------------------------------------------------------------------------------------------------------------------------------
+
+    _ccpName = ccpName;
+
+    // ----------------------------------------------------------------------------------------------------------------------------------------
+  }
+
+  public String getCentralCounterpartyName() {
+    return _ccpName;
+  }
+
+  // ----------------------------------------------------------------------------------------------------------------------------------------
 }
