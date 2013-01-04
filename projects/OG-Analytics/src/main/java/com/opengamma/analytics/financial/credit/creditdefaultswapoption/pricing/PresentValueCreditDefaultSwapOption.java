d625135444ea9e23e9829128282fee277a62b5fb
Craig <craig@opengamma.com>
2013-01-04 14:54:19 +0000

    Miscellaneous minor changes to some files.
    Added a stochastic recovery model (needs further testing - not quite right yet). Recovery is sampled from a beta distribution. This will be very useful when bolting together different instruments, obligors etc into a larger simulation framework (recovery rates are a key driver in exposure/loss distributions)

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/credit/creditdefaultswapoption/pricing/PresentValueCreditDefaultSwapOption.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/credit/creditdefaultswapoption/pricing/PresentValueCreditDefaultSwapOption.java
@@ -29,6 +29,7 @@ public class PresentValueCreditDefaultSwapOption {
   // TODO : Need to sort out the calculation of the forward starting spread
   // TODO : Need to check through this model in detail
   // TODO : Need to add error checking for d1 and d2 calculations
+  // TODO : Need to check that strike is not equal to zero
 
   // NOTE : Have not included the PriceType field for the CDS - assume this is entered as part of the underlying CDS contract definition
 
