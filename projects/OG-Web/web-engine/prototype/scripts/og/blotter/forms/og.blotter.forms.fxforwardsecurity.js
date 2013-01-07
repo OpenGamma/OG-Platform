18f5adc61e9dc807a581a01b54b912333f09cd26
Ian Turner <ian@opengamma.com>
2013-01-07 19:04:58 +0000

    creation and edit of fx forward working

--- projects/OG-Web/web-engine/prototype/scripts/og/blotter/forms/og.blotter.forms.fxforwardsecurity.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/blotter/forms/og.blotter.forms.fxforwardsecurity.js
@@ -3,25 +3,37 @@
  * Please see distribution for license.
  */
 $.register_module({
-    name: 'og.blotter.forms.Fx_forward',
+    name: 'og.blotter.forms.fxforwardsecurity',
     dependencies: [],
     obj: function () {   
         return function (config) {
-            config = og.blotter.util.FAKE_FX_FORWARD;
+            //config = og.blotter.util.FAKE_FX_FORWARD;
             var constructor = this, form, ui = og.common.util.ui, data = config || {};
+            if(config) {
+                data = config; 
+                data.id = config.trade.uniqueId;
+            }
+            else {
+                data = {security: {
+                            type: "FXForwardSecurity",
+                            name: "FXForwardSecurity ABC",
+                            regionId: "ABC~123",
+                            externalIdBundle: ""}, 
+                        trade: og.blotter.util.FAKE_TRADE};
+            }
+            console.log(data);
             constructor.load = function () {
                 constructor.title = 'FX Forward';
                 form = new og.common.util.ui.Form({
                     module: 'og.blotter.forms.fx_option_tash',
                     selector: '.OG-blotter-form-block',
-                    data: {security:  {type: "FXForwardSecurity",name: "FXForwardSecurity ABC",regionId: "ABC~123",
-                        externalIdBundle: ""}, trade: og.blotter.util.FAKE_TRADE}
+                    data: data
                 });
                 form.children.push(
                     new og.blotter.forms.blocks.Portfolio({form: form}),
                     new form.Block({
                         module: 'og.blotter.forms.blocks.fx_forward_tash',
-                        extras: {pay: data.payAmount, receive: data.receiveAmount},
+                        extras: {pay: data.security.payAmount, receive: data.security.receiveAmount},
                         children: [
                             new form.Block({module:'og.views.forms.currency_tash', 
                                 extras:{name: 'security.payCurrency'}}),
@@ -29,15 +41,15 @@ $.register_module({
                                 extras:{name: 'security.receiveCurrency'}})
                         ]
                     }),                    
-                    new og.common.util.ui.Attributes({form: form, attributes: data.attributes})
+                    new og.common.util.ui.Attributes({form: form, attributes: data.security.attributes})
                 );
                 form.dom();
                 form.on('form:load', function (){
                     og.blotter.util.add_datetimepicker("security.forwardDate");
                     if(data.length) return;
-                    og.blotter.util.set_select("security.receiveCurrency", data.receiveCurrency);
-                    og.blotter.util.set_select("security.payCurrency", data.payCurrency);
-                    og.blotter.util.set_datetime("security.forwardDate", data.forwardDate);
+                    og.blotter.util.set_select("security.receiveCurrency", data.security.receiveCurrency);
+                    og.blotter.util.set_select("security.payCurrency", data.security.payCurrency);
+                    og.blotter.util.set_datetime("security.forwardDate", data.security.forwardDate);
                 });
                 form.on('form:submit', function (result){
                     og.api.rest.blotter.trades.put(result.data).pipe(/*console.log*/);
