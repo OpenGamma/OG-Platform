b0ca8bef16f2e1f0cf4a8392372a59aa1885d80f
Daniel Kwiecinski <daniel@opengamma.com>
2013-01-17 11:27:39 +0000

    Organisation Master

---
+++ projects/OG-MasterDB/src/main/java/com/opengamma/masterdb/orgs/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+/**
+ * Organisation master implementation using an SQL database.
+ */
+package com.opengamma.masterdb.orgs;
