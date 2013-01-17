a48aded60748ebcae9b2a1d6b20e0f40a1a1db71
Alan Ayoub <alan@opengamma.com>
2013-01-17 17:00:07 +0000

    changed default config renderer from JSON to XML

--- projects/OG-Web/web-engine/prototype/scripts/og/views/config_forms/og.views.config_forms.default.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/config_forms/og.views.config_forms.default.js
@@ -12,31 +12,23 @@ $.register_module({
         var module = this, Form = og.common.util.ui.Form, api = og.api.rest, constructor;
         constructor = function (config) {
             var load_handler = config.handler || $.noop, selector = config.selector,
-                master = config.data.template_data.configJSON.data, config_type = config.type,
+                master = config.data.template_data.configXML, config_type = config.type,
                 loading = config.loading || $.noop, deleted = config.data.template_data.deleted, is_new = config.is_new,
-                orig_name = config.data.template_data.name,
-                resource_id = config.data.template_data.object_id, type_map = config.type_map || {},
+                orig_name = config.data.template_data.name, resource_id = config.data.template_data.object_id,
                 save_new_handler = config.save_new_handler, save_handler = config.save_handler, new_name = '',
                 form = new Form({
                     module: 'og.views.forms.config_default_tash',
-                    data: {name: null}, type_map: type_map, selector: selector,
-                    extras: {name: orig_name, raw: is_new ? '{}' : JSON.stringify(master, null, 2)},
-                    processor: function (data) {
-                        var key, parsed = JSON.parse(data.raw);
-                        delete data.raw;
-                        // turn data into parsed object so meta mapping can happen
-                        for (key in parsed) data[key] = parsed[key];
-                        new_name = data.name;
-                        if (!type_map.name) delete data.name;
-                    }
+                    data: {name: null}, selector: selector,
+                    extras: {name: orig_name, raw: is_new ? '<xml />' : master},
+                    processor: function (data) {new_name = data.name;}
                 }), form_id = '#' + form.id;
             var save_resource = function (result) {
-                var data = result.data, meta = result.meta, as_new = result.extras.as_new;
+                var as_new = result.extras.as_new;
                 if (!deleted && !is_new && as_new && (orig_name === new_name))
                     return window.alert('Please select a new name.');
                 api.configs.put({
                     id: as_new ? void 0 : resource_id,
-                    name: new_name, json: JSON.stringify({data: data, meta: meta}), type: config_type, loading: loading
+                    name: new_name, xml: result.data.raw, type: config_type, loading: loading
                 }).pipe(as_new ? save_new_handler : save_handler);
             };
             og.dev.warn('using default config template for config type:\n' + config_type);
