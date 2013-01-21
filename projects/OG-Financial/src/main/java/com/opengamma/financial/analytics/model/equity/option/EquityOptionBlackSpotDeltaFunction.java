e7e050828e30d73998f6d111fecbd0b8c3cb604f
Elaine McLeod <elaine@opengamma.com>
2013-01-21 12:39:21 +0000

    Refactoring Black calculators for equity and equity index options

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/equity/option/EquityOptionBlackSpotDeltaFunction.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/equity/option/EquityOptionBlackSpotDeltaFunction.java
@@ -6,45 +6,59 @@
 package com.opengamma.financial.analytics.model.equity.option;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.Sets;
+import com.opengamma.analytics.financial.equity.EquityOptionBlackSpotDeltaCalculator;
 import com.opengamma.analytics.financial.equity.StaticReplicationDataBundle;
-import com.opengamma.analytics.financial.equity.option.EquityIndexOption;
-import com.opengamma.analytics.financial.equity.option.EquityIndexOptionBlackMethod;
-import com.opengamma.analytics.financial.equity.option.EquityOption;
-import com.opengamma.analytics.financial.equity.option.EquityOptionBlackMethod;
 import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
+import com.opengamma.analytics.financial.interestrate.InstrumentDerivativeVisitor;
+import com.opengamma.engine.ComputationTarget;
 import com.opengamma.engine.ComputationTargetSpecification;
+import com.opengamma.engine.function.FunctionCompilationContext;
 import com.opengamma.engine.function.FunctionInputs;
 import com.opengamma.engine.value.ComputedValue;
 import com.opengamma.engine.value.ValueProperties;
+import com.opengamma.engine.value.ValuePropertyNames;
 import com.opengamma.engine.value.ValueRequirement;
 import com.opengamma.engine.value.ValueRequirementNames;
 import com.opengamma.engine.value.ValueSpecification;
 
 /**
- * Calculates the spot value delta of an equity index or equity option using the Black formula.
+ * Calculates the spot delta of an equity index or equity option using the Black formula.
  */
 public class EquityOptionBlackSpotDeltaFunction extends EquityOptionBlackFunction {
+  /** Spot delta calculator */
+  private static final InstrumentDerivativeVisitor<StaticReplicationDataBundle, Double> CALCULATOR = EquityOptionBlackSpotDeltaCalculator.getInstance();
 
   /**
    * Default constructor
    */
   public EquityOptionBlackSpotDeltaFunction() {
-    super(ValueRequirementNames.VALUE_DELTA);
+    super(ValueRequirementNames.DELTA);
   }
 
   @Override
   protected Set<ComputedValue> computeValues(final InstrumentDerivative derivative, final StaticReplicationDataBundle market, final FunctionInputs inputs,
       final Set<ValueRequirement> desiredValues, final ComputationTargetSpecification targetSpec, final ValueProperties resultProperties) {
     final ValueSpecification resultSpec = new ValueSpecification(getValueRequirementNames()[0], targetSpec, resultProperties);
-    //FIXME
-    if (derivative instanceof EquityIndexOption) {
-      final EquityIndexOptionBlackMethod model = EquityIndexOptionBlackMethod.getInstance();
-      return Collections.singleton(new ComputedValue(resultSpec, model.deltaWrtSpot((EquityIndexOption) derivative, market)));
-    }
-    final EquityOptionBlackMethod model = EquityOptionBlackMethod.getInstance();
-    return Collections.singleton(new ComputedValue(resultSpec, model.deltaWrtSpot((EquityOption) derivative, market)));
+    final double spotDelta = derivative.accept(CALCULATOR, market);
+    return Collections.singleton(new ComputedValue(resultSpec, spotDelta));
   }
 
+  @Override
+  public Set<ValueSpecification> getResults(final FunctionCompilationContext context, final ComputationTarget target, final Map<ValueSpecification, ValueRequirement> inputs) {
+    final Set<ValueSpecification> results = super.getResults(context, target, inputs);
+    final Set<ValueSpecification> resultsWithoutCurrency = Sets.newHashSetWithExpectedSize(results.size());
+    for (final ValueSpecification spec : results) {
+      final String name = spec.getValueName();
+      final ComputationTargetSpecification targetSpec = spec.getTargetSpecification();
+      final ValueProperties properties = spec.getProperties().copy()
+          .withoutAny(ValuePropertyNames.CURRENCY)
+          .get();
+      resultsWithoutCurrency.add(new ValueSpecification(name, targetSpec, properties));
+    }
+    return results;
+  }
 }
