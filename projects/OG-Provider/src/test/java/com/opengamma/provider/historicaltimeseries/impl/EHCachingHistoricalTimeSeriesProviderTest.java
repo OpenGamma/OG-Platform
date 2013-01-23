99f7eee4e368650a092af352a63c907ee6be58ec
Stephen Colebourne <stephen@opengamma.com>
2013-01-23 11:10:11 +0000

    [PLAT-3015] Update OG-Provider to ThreeTen

--- projects/OG-Provider/src/test/java/com/opengamma/provider/historicaltimeseries/impl/EHCachingHistoricalTimeSeriesProviderTest.java
+++ projects/OG-Provider/src/test/java/com/opengamma/provider/historicaltimeseries/impl/EHCachingHistoricalTimeSeriesProviderTest.java
@@ -10,14 +10,12 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.testng.AssertJUnit.assertEquals;
-
-import javax.time.calendar.LocalDate;
-
 import net.sf.ehcache.CacheManager;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.threeten.bp.LocalDate;
 
 import com.opengamma.id.ExternalIdBundle;
 import com.opengamma.provider.historicaltimeseries.HistoricalTimeSeriesProvider;
