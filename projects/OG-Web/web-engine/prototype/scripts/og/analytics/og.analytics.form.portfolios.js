434e7c8ecb61fb54cceec7a440dac9c4e33f0bac
Antonio Greco <antonio@opengamma.com>
2013-01-31 17:46:21 +0000

    restyling of Portfolio searchable dropdown

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.portfolios.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.portfolios.js
@@ -21,8 +21,21 @@ $.register_module({
                 }
             });
             block.on('form:load', function () {
-                var select = $('#' + form.id + ' select[name=' + index + ']').searchable().focus()
-                select.hide().siblings('input').removeAttr('style').show();
+                var select = $('#' + form.id + ' select[name=' + index + ']').searchable().focus().hide()
+                select.siblings('input').removeAttr('style').show().blur(function () {
+                    $(this).show();
+                    select.siblings('select').hide();
+                }).keydown(function (event) {
+                    if (event.keyCode === 38 || event.keyCode === 40) select.siblings('select').show();
+                    $(this).show();
+                }).click(function (event) { $(this).show() });
+                select.parent().siblings('.og-icon-down').click(function () {
+                     select.siblings('select').show();
+                     select.siblings('input').focus();
+                })
+                select.siblings('select').click(function (event) {
+                    select.siblings('input').show().val($(event.target).text());
+                })
             });
         };
         Portfolios.prototype = new Block;
