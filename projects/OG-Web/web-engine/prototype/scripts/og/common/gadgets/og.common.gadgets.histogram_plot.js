6bef88ca41eb5ab98aafd1f407a383d3f14a820e
Ian Turner <ian@opengamma.com>
2013-02-01 18:22:07 +0000

    start at flot graph

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.histogram_plot.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.histogram_plot.js
@@ -8,10 +8,20 @@ $.register_module({
     obj: function () {
         var module = this, loading_template;
         return function (config) {
-            var gadget = this;
+            var gadget = this, selector = config.selector, $plot, options, disabled_options, data = [];
             gadget.resize = function () {
 
             };
+            for (var i = 0; i < 10; i += 0.5){ 
+                data.push([i, Math.tan(i)]); 
+            }
+            options = { 
+                series: { 
+                    lines: { show: true }, 
+                    points: { show: true } 
+                    } 
+                };
+            $plot = $.plot($(selector), data, options);
             if (!config.child) og.common.gadgets.manager.register(gadget);
         };
     }
