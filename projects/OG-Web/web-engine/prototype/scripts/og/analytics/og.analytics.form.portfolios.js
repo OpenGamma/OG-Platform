985b34cca88447d5c36dccc6428e5135e35287c4
Antonio Greco <antonio@opengamma.com>
2013-02-04 13:18:00 +0000

    view definitions using searchable

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.portfolios.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.portfolios.js
@@ -21,34 +21,35 @@ $.register_module({
                 }
             });
             block.on('form:load', function () {
-                var select = $('#' + form.id + ' select[name=' + index + ']').searchable().focus().hide(),
+                var select = $('#' + form.id + ' select[name=' + index + ']').searchable().focus().hide(),selectedIndex,
                     list = select.siblings('select').addClass('dropdown-list'),
-                    input = select.siblings('input').val('Select portfolio'),
+                    input = select.siblings('input').attr('placeholder', 'Select portfolio'),
                     toggle = select.parent().siblings('.og-icon-down');
 
                 input.removeAttr('style').show().blur(function () {
                     $(this).show();
                     list.hide();
-                    if (input.val() === '') input.val('Select portfolio');
                 }).keydown(function (event) {
                     if (event.keyCode === 38 || event.keyCode === 40) list.show();
                     $(this).show();
-                }).focus(function (event) {
-                    if (input.val() === 'Select portfolio') input.val('');
                 }).click(function (event) {
-                    list.show().prop('selectedIndex', selectedIndex);
-                    select.prop('selectedIndex', selectedIndex);
+                    if (input.val() !== '') {
+                        list.show().prop('selectedIndex', selectedIndex);
+                        select.prop('selectedIndex', selectedIndex);
+                    }
                     $(this).show();
                 });
 
                 toggle.click(function (event) {
-                    select.prop('selectedIndex', selectedIndex);
-                    list.show().prop('selectedIndex', selectedIndex);
+                    if (input.val() !== '') {
+                        select.prop('selectedIndex', selectedIndex);
+                        list.show().prop('selectedIndex', selectedIndex);
+                    }
                     input.focus(0).trigger('keydown');
                 });
 
                 list.on('mousedown', function (event) {
-                    var text = $(event.target).text();
+                    var text = $(event.target).text(),
                         selectedIndex = $(this).prop('selectedIndex');
                     select.prop('selectedIndex', selectedIndex);
                     list.prop('selectedIndex', selectedIndex);
--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.viewdefinitions.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.form.viewdefinitions.js
@@ -6,12 +6,14 @@ $.register_module({
     name: 'og.analytics.form.ViewDefinitions',
     dependencies: ['og.common.util.ui.Dropdown'],
     obj: function () {
-        var module = this, menu, Block = og.common.util.ui.Block;
+        var Block = og.common.util.ui.Block;
         var ViewDefinitions = function (config) {
             var block = this, form = config.form, index = 'viewdefinition', title = 'calculations';
             form.Block.call(block, {
                 template: '<div class="og-option-title">' +
-                    '<header class="OG-background-05">' + title + ':</header>{{{children}}}</div>',
+                    '<header class="OG-background-05">' + title + ':</header>'+
+                    '{{{children}}}'+
+                    '<div class="OG-icon og-icon-down"></div></div>',
                 children: [new og.common.util.ui.Dropdown({
                     form: form, index: index, resource: 'viewdefinitions', fields: ['id', 'name'],
                     style: 'width: 100%', value: config.val, placeholder: 'Select...',
@@ -22,7 +24,42 @@ $.register_module({
                         data[index] = $('#' + form.id + ' select[name=' + index + ']').siblings('select').val();
                 }
             });
-            block.on('form:load', function () {$('#' + form.id + ' select[name=' + index + ']').searchable();});
+            block.on('form:load', function () {
+                var select = $('#' + form.id + ' select[name=' + index + ']').searchable().hide(), selectedIndex,
+                    list = select.siblings('select').addClass('dropdown-list'),
+                    input = select.siblings('input').attr('placeholder', 'Select..'),
+                    toggle = select.parent().siblings('.og-icon-down');
+
+                input.removeAttr('style').show().blur(function () {
+                    $(this).show();
+                    list.hide();
+                }).keydown(function (event) {
+                    if (event.keyCode === 38 || event.keyCode === 40) list.show();
+                    $(this).show();
+                }).click(function (event) {
+                    if (input.val() !== '') {
+                        list.show().prop('selectedIndex', selectedIndex);
+                        select.prop('selectedIndex', selectedIndex);
+                    }
+                    $(this).show();
+                });
+
+                toggle.click(function (event) {
+                    if (input.val() !== '') {
+                        select.prop('selectedIndex', selectedIndex);
+                        list.show().prop('selectedIndex', selectedIndex);
+                    }
+                    input.focus(0).trigger('keydown');
+                });
+
+                list.on('mousedown', function (event) {
+                    var text = $(event.target).text(),
+                        selectedIndex = $(this).prop('selectedIndex');
+                    select.prop('selectedIndex', selectedIndex);
+                    list.prop('selectedIndex', selectedIndex);
+                    input.show().val(text).focus(0);
+                });
+            });
         };
         ViewDefinitions.prototype = new Block;
         return ViewDefinitions;
