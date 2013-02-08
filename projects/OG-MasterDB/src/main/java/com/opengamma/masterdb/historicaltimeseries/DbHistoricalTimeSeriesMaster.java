24414c23b81b0ea66e1c8aa87e6965815630070e
Stephen Colebourne <stephen@opengamma.com>
2013-02-08 18:35:48 +0000

    [PLAT-3102] Avoid NPE in HTS master

--- projects/OG-MasterDB/src/main/java/com/opengamma/masterdb/historicaltimeseries/DbHistoricalTimeSeriesMaster.java
+++ projects/OG-MasterDB/src/main/java/com/opengamma/masterdb/historicaltimeseries/DbHistoricalTimeSeriesMaster.java
@@ -513,9 +513,9 @@ public class DbHistoricalTimeSeriesMaster extends AbstractDocumentDbMaster<Histo
         }
         final String idScheme = rs.getString("KEY_SCHEME");
         final String idValue = rs.getString("KEY_VALUE");
-        final LocalDate validFrom = DbDateUtils.fromSqlDateNullFarPast(rs.getDate("KEY_VALID_FROM"));
-        final LocalDate validTo = DbDateUtils.fromSqlDateNullFarFuture(rs.getDate("KEY_VALID_TO"));
         if (idScheme != null && idValue != null) {
+          final LocalDate validFrom = DbDateUtils.fromSqlDateNullFarPast(rs.getDate("KEY_VALID_FROM"));
+          final LocalDate validTo = DbDateUtils.fromSqlDateNullFarFuture(rs.getDate("KEY_VALID_TO"));
           ExternalIdWithDates id = ExternalIdWithDates.of(ExternalId.of(idScheme, idValue), validFrom, validTo);
           _info.setExternalIdBundle(_info.getExternalIdBundle().withExternalId(id));
         }
