993b828be6338c8b0ccc46f0fc8a56125ac89ea6
Elaine McLeod <elaine@opengamma.com>
2013-02-10 18:08:30 -0500

    [PLAT-3092] FX spot convention

--- projects/OG-Financial/src/main/java/com/opengamma/financial/convention/InterestRateFutureConvention.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/convention/InterestRateFutureConvention.java
@@ -36,12 +36,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
   private String _indexConvention;
 
   /**
-   * The expiry convention.
-   */
-  @PropertyDefinition(validate = "notNull")
-  private String _expiryConvention;
-
-  /**
    * For the builder.
    */
   public InterestRateFutureConvention() {
@@ -76,8 +70,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
     switch (propertyName.hashCode()) {
       case -668532253:  // indexConvention
         return getIndexConvention();
-      case 2143523076:  // expiryConvention
-        return getExpiryConvention();
     }
     return super.propertyGet(propertyName, quiet);
   }
@@ -88,9 +80,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
       case -668532253:  // indexConvention
         setIndexConvention((String) newValue);
         return;
-      case 2143523076:  // expiryConvention
-        setExpiryConvention((String) newValue);
-        return;
     }
     super.propertySet(propertyName, newValue, quiet);
   }
@@ -98,7 +87,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
   @Override
   protected void validate() {
     JodaBeanUtils.notNull(_indexConvention, "indexConvention");
-    JodaBeanUtils.notNull(_expiryConvention, "expiryConvention");
     super.validate();
   }
 
@@ -110,7 +98,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
     if (obj != null && obj.getClass() == this.getClass()) {
       InterestRateFutureConvention other = (InterestRateFutureConvention) obj;
       return JodaBeanUtils.equal(getIndexConvention(), other.getIndexConvention()) &&
-          JodaBeanUtils.equal(getExpiryConvention(), other.getExpiryConvention()) &&
           super.equals(obj);
     }
     return false;
@@ -120,7 +107,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
   public int hashCode() {
     int hash = 7;
     hash += hash * 31 + JodaBeanUtils.hashCode(getIndexConvention());
-    hash += hash * 31 + JodaBeanUtils.hashCode(getExpiryConvention());
     return hash ^ super.hashCode();
   }
 
@@ -152,32 +138,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
 
   //-----------------------------------------------------------------------
   /**
-   * Gets the expiry convention.
-   * @return the value of the property, not null
-   */
-  public String getExpiryConvention() {
-    return _expiryConvention;
-  }
-
-  /**
-   * Sets the expiry convention.
-   * @param expiryConvention  the new value of the property, not null
-   */
-  public void setExpiryConvention(String expiryConvention) {
-    JodaBeanUtils.notNull(expiryConvention, "expiryConvention");
-    this._expiryConvention = expiryConvention;
-  }
-
-  /**
-   * Gets the the {@code expiryConvention} property.
-   * @return the property, not null
-   */
-  public final Property<String> expiryConvention() {
-    return metaBean().expiryConvention().createProperty(this);
-  }
-
-  //-----------------------------------------------------------------------
-  /**
    * The meta-bean for {@code InterestRateFutureConvention}.
    */
   public static class Meta extends ExchangeTradedFutureAndOptionConvention.Meta {
@@ -192,17 +152,11 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
     private final MetaProperty<String> _indexConvention = DirectMetaProperty.ofReadWrite(
         this, "indexConvention", InterestRateFutureConvention.class, String.class);
     /**
-     * The meta-property for the {@code expiryConvention} property.
-     */
-    private final MetaProperty<String> _expiryConvention = DirectMetaProperty.ofReadWrite(
-        this, "expiryConvention", InterestRateFutureConvention.class, String.class);
-    /**
      * The meta-properties.
      */
     private final Map<String, MetaProperty<?>> _metaPropertyMap$ = new DirectMetaPropertyMap(
       this, (DirectMetaPropertyMap) super.metaPropertyMap(),
-        "indexConvention",
-        "expiryConvention");
+        "indexConvention");
 
     /**
      * Restricted constructor.
@@ -215,8 +169,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
       switch (propertyName.hashCode()) {
         case -668532253:  // indexConvention
           return _indexConvention;
-        case 2143523076:  // expiryConvention
-          return _expiryConvention;
       }
       return super.metaPropertyGet(propertyName);
     }
@@ -243,14 +195,6 @@ public class InterestRateFutureConvention extends ExchangeTradedFutureAndOptionC
      */
     public final MetaProperty<String> indexConvention() {
       return _indexConvention;
-    }
-
-    /**
-     * The meta-property for the {@code expiryConvention} property.
-     * @return the meta-property, not null
-     */
-    public final MetaProperty<String> expiryConvention() {
-      return _expiryConvention;
     }
 
   }
