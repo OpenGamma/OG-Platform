c9d19e7d7a2e920700c83b4144f8f355981751a0
Elaine McLeod <elaine@opengamma.com>
2013-02-14 14:24:20 -0500

    First pass at adding new curve strips

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/ircurve/strips/SwapStrip.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/ircurve/strips/SwapStrip.java
@@ -8,67 +8,51 @@ package com.opengamma.financial.analytics.ircurve.strips;
 import org.apache.commons.lang.ObjectUtils;
 import org.threeten.bp.Period;
 
-import com.opengamma.financial.security.cash.CashSecurity;
 import com.opengamma.util.ArgumentChecker;
 
 /**
- * Cash strip for use in curves containing sufficient information to construct a {@link CashSecurity}
+ * 
  */
-public class CashStrip {
-  /** The start tenor of the strip */
+public class SwapStrip {
   private final Period _startTenor;
-  /** The maturity tenor of the strip */
   private final Period _maturityTenor;
-  /** The convention to use in constructing a {@link CashSecurity} */
-  private final String _conventionName;
-  /** The curve specification name */
+  private final String _payLegConventionName;
+  private final String _receiveLegConventionName;
   private final String _curveSpecificationName;
 
   /**
-   * @param startTenor The start tenor, not null
-   * @param maturityTenor The maturity tenor, not null
-   * @param conventionName The convention, not null
-   * @param curveSpecificationName The name of the curve specification, not null
+   * 
    */
-  public CashStrip(final Period startTenor, final Period maturityTenor, final String conventionName, final String curveSpecificationName) {
+  public SwapStrip(final Period startTenor, final Period maturityTenor, final String payLegConventionName, final String receiveLegConventionName,
+      final String curveSpecificationName) {
     ArgumentChecker.notNull(startTenor, "start tenor");
     ArgumentChecker.notNull(maturityTenor, "maturity tenor");
-    ArgumentChecker.notNull(conventionName, "convention name");
+    ArgumentChecker.notNull(payLegConventionName, "pay leg convention name");
+    ArgumentChecker.notNull(receiveLegConventionName, "receive leg convention name");
     ArgumentChecker.notNull(curveSpecificationName, "curve specification name");
     _startTenor = startTenor;
     _maturityTenor = maturityTenor;
-    _conventionName = conventionName;
+    _payLegConventionName = payLegConventionName;
+    _receiveLegConventionName = receiveLegConventionName;
     _curveSpecificationName = curveSpecificationName;
   }
 
-  /**
-   * Gets the start tenor.
-   * @return the start tenor
-   */
   public Period getStartTenor() {
     return _startTenor;
   }
 
-  /**
-   * Gets the maturity tenor.
-   * @return the maturity tenor
-   */
   public Period getMaturityTenor() {
     return _maturityTenor;
   }
 
-  /**
-   * Gets the convention name.
-   * @return the convention name
-   */
-  public String getConventionName() {
-    return _conventionName;
+  public String getPayLegConventionName() {
+    return _payLegConventionName;
   }
 
-  /**
-   * Gets the curve specification name.
-   * @return the curve specification name
-   */
+  public String getReceiveLegConventionName() {
+    return _receiveLegConventionName;
+  }
+
   public String getCurveSpecificationName() {
     return _curveSpecificationName;
   }
@@ -77,9 +61,10 @@ public class CashStrip {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + _conventionName.hashCode();
     result = prime * result + _curveSpecificationName.hashCode();
     result = prime * result + _maturityTenor.hashCode();
+    result = prime * result + _payLegConventionName.hashCode();
+    result = prime * result + _receiveLegConventionName.hashCode();
     result = prime * result + _startTenor.hashCode();
     return result;
   }
@@ -89,14 +74,17 @@ public class CashStrip {
     if (this == obj) {
       return true;
     }
-    if (!(obj instanceof CashStrip)) {
+    if (!(obj instanceof SwapStrip)) {
       return false;
     }
-    final CashStrip other = (CashStrip) obj;
+    final SwapStrip other = (SwapStrip) obj;
     if (!ObjectUtils.equals(_maturityTenor, other._maturityTenor)) {
       return false;
     }
-    if (!ObjectUtils.equals(_conventionName, other._conventionName)) {
+    if (!ObjectUtils.equals(_payLegConventionName, other._payLegConventionName)) {
+      return false;
+    }
+    if (!ObjectUtils.equals(_receiveLegConventionName, other._receiveLegConventionName)) {
       return false;
     }
     if (!ObjectUtils.equals(_startTenor, other._startTenor)) {
@@ -107,6 +95,7 @@ public class CashStrip {
     }
     return true;
   }
+
 
 
 }
