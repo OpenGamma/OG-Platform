74537a5107babd6eb32880b8db982fcf0777a580
Andrew Griffin <andrew@opengamma.com>
2013-02-18 18:10:11 +0000

    [PLAT-3044] Re-introducing distinction between market data resolution and availability checking.

--- projects/OG-Engine/src/main/java/com/opengamma/engine/marketdata/availability/MarketDataAvailabilityChecker.java
+++ projects/OG-Engine/src/main/java/com/opengamma/engine/marketdata/availability/MarketDataAvailabilityChecker.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
  *
  * Please see distribution for license.
  */
@@ -12,27 +12,24 @@ import com.opengamma.engine.value.ValueSpecification;
 import com.opengamma.id.ExternalBundleIdentifiable;
 import com.opengamma.id.ExternalIdentifiable;
 import com.opengamma.id.UniqueIdentifiable;
-import com.opengamma.util.PublicSPI;
 
 /**
- * Used to obtain the availability status of market data for a given {@link MarketDataProvider} instance.
+ * Used to obtain the availability status of market data.
  * <p>
- * For example, in dependency graph building, this is used to decide whether a requirement can be satisfied directly from market data, or whether a function is required to produce the requirement.
+ * This availability check may be used as the basis for the {@link MarketDataAvailabilityProvider} implementation that a {@link MarketDataProvider} is coupled with. Most consumers of market data will
+ * use a {@code MarketDataAvailabilityProvider} as they will also require value resolution rather than just availability indication.
  */
-@PublicSPI
-public interface MarketDataAvailabilityProvider {
+public interface MarketDataAvailabilityChecker {
 
   /**
-   * Tests whether a data requirement can be satisfied by this availability provider. If the data is available then a suitable value specification to describe it, that satisfies the original value
-   * requirement, must be returned. If the value requirement cannot be satisfied by this provider then a return value of null will cause the dependency graph builder to continue its construction. If
-   * the requirement must not be satisfied then the method may throw an exception to abort the graph construction.
+   * Tests whether a data requirement can be satisfied.
    * <p>
    * The target described by the requirement is resolved and described formally as the {@code targetSpec} parameter. This may be null if the target reference could not be resolved. If provided, this
    * specification can be used to construct the returned {@code ValueSpecification}. The resolved form, if the object exists within the system, may be passed as the {@code target} parameter. If the
    * object cannot be resolved to an item of the type indicated by the target specification, a minimum of {@link ExternalBundleIdentifiable}, {@link ExternalIdentifiable} or {@link UniqueIdentifiable}
    * will be passed.
    * <p>
-   * The {@code ValueSpecification} returned from this method will be the one used to establish the subscription from a relevant {@link MarketDataProvider} instance.
+   * The {@code ValueSpecification} returned from this method will be the one used to establish the subscription from a relevant {@link MarketDataProvider} instance. TODO: CHANGE THIS ONE
    * 
    * @param targetSpec the resolved target specification from the data requirement, not null
    * @param target the resolved target the requirement corresponds to, not null (unless the target specification is {@link ComputationTargetSpecification#NULL})
