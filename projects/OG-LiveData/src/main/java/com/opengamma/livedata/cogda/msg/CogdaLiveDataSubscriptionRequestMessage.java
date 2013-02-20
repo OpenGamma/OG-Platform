cda0c49fb1bcccce65559028054a59145bb99936
Stephen Colebourne <stephen@opengamma.com>
2013-02-20 16:33:15 +0000

    Regenerate Joda-Beans

--- projects/OG-LiveData/src/main/java/com/opengamma/livedata/cogda/msg/CogdaLiveDataSubscriptionRequestMessage.java
+++ projects/OG-LiveData/src/main/java/com/opengamma/livedata/cogda/msg/CogdaLiveDataSubscriptionRequestMessage.java
@@ -79,7 +79,7 @@ public class CogdaLiveDataSubscriptionRequestMessage extends CogdaLiveDataComman
      * The meta-properties.
      */
     private final Map<String, MetaProperty<?>> _metaPropertyMap$ = new DirectMetaPropertyMap(
-      this, (DirectMetaPropertyMap) super.metaPropertyMap());
+        this, (DirectMetaPropertyMap) super.metaPropertyMap());
 
     /**
      * Restricted constructor.
