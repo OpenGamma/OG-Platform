a396c00fe0ef2a88d27346369d4263631b25091a
Stephen Colebourne <stephen@opengamma.com>
2013-02-20 16:36:42 +0000

    Regenerate Joda-Beans

--- projects/OG-BloombergExample/src/main/java/com/opengamma/bloombergexample/component/ExampleMarketDataComponentFactory.java
+++ projects/OG-BloombergExample/src/main/java/com/opengamma/bloombergexample/component/ExampleMarketDataComponentFactory.java
@@ -349,7 +349,7 @@ public class ExampleMarketDataComponentFactory extends AbstractComponentFactory 
      * The meta-properties.
      */
     private final Map<String, MetaProperty<?>> _metaPropertyMap$ = new DirectMetaPropertyMap(
-      this, (DirectMetaPropertyMap) super.metaPropertyMap(),
+        this, (DirectMetaPropertyMap) super.metaPropertyMap(),
         "classifier",
         "securitySource",
         "serverMetaDataProvider",
