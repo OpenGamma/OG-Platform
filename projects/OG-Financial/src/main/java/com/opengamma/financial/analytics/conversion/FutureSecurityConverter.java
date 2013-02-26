9c02617a6e3974846cbe74fef981e2a72c53dd91
Marc Henrard <marc@opengamma.com>
2013-02-26 18:46:19 +0000

    [PLAT-3042] Impact of the STIR futures split on OG-Financial.

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/conversion/FutureSecurityConverter.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/conversion/FutureSecurityConverter.java
@@ -17,6 +17,7 @@ import com.opengamma.analytics.financial.commodity.definition.SettlementType;
 import com.opengamma.analytics.financial.equity.future.definition.EquityFutureDefinition;
 import com.opengamma.analytics.financial.instrument.InstrumentDefinitionWithData;
 import com.opengamma.analytics.financial.instrument.future.BondFutureDefinition;
+import com.opengamma.analytics.financial.instrument.future.InterestRateFutureSecurityDefinition;
 import com.opengamma.analytics.financial.instrument.future.InterestRateFutureTransactionDefinition;
 import com.opengamma.financial.security.FinancialSecurityVisitor;
 import com.opengamma.financial.security.FinancialSecurityVisitorAdapter;
@@ -143,7 +144,7 @@ public class FutureSecurityConverter extends FinancialSecurityVisitorAdapter<Ins
 
   @Override
   public InstrumentDefinitionWithData<?, Double> visitInterestRateFutureSecurity(final InterestRateFutureSecurity security) {
-    return (InterestRateFutureTransactionDefinition) security.accept(_irFutureConverter);
+    return (InterestRateFutureSecurityDefinition) security.accept(_irFutureConverter); 
   }
 
   @Override
