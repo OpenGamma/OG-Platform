72123eb71283a8e0766ad50f778c8fd36cb066c3
Andrew Griffin <andrew@opengamma.com>
2013-03-04 10:03:34 +0000

    [PLAT-1292] Return the Java string to the caller.

--- projects/OG-Language/Service/ErrorFeedback.cpp
+++ projects/OG-Language/Service/ErrorFeedback.cpp
@@ -24,6 +24,8 @@ void CErrorFeedback::Write (const TCHAR *pszMessage) {
 	LOGERROR (TEXT ("A major error occurred"));
 	TCHAR *psz = _tcsdup (pszMessage);
 	if (psz) {
+		// TODO: Implement this as writing to a file in a known location perhaps. The caller will already have
+		// logged suitable messages, so we only need to report at DEBUG level.
 		TCHAR *pszNext;
 		TCHAR *pszLine = _tcstok_s (psz, TEXT ("\n"), &pszNext);
 		while (pszLine) {
