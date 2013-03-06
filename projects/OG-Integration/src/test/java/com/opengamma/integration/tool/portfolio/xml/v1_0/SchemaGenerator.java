5b5c629a6df41ea6aac453718bfca9ac1d8523dc
Casey Clements <casey@opengamma.com>
2013-03-06 20:02:04 +0000

    Commented out SchemaGenerator

--- projects/OG-Integration/src/test/java/com/opengamma/integration/tool/portfolio/xml/v1_0/SchemaGenerator.java
+++ projects/OG-Integration/src/test/java/com/opengamma/integration/tool/portfolio/xml/v1_0/SchemaGenerator.java
@@ -6,6 +6,7 @@
 package com.opengamma.integration.tool.portfolio.xml.v1_0;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.xml.bind.JAXBContext;
@@ -16,10 +17,7 @@ import javax.xml.transform.dom.DOMResult;
 
 import org.w3c.dom.Document;
 
-import com.beust.jcommander.internal.Sets;
 import com.opengamma.integration.tool.portfolio.xml.v1_0.jaxb.PortfolioDocumentV1_0;
-import com.sun.org.apache.xml.internal.serialize.OutputFormat;
-import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 /**
  * Utility class used for generating a schema from a base portfolio document. This can
@@ -36,15 +34,15 @@ public class SchemaGenerator {
 
     Document document = (Document) result.getNode();
 
-    OutputFormat format = new OutputFormat(document);
-    format.setIndenting(true);
-    XMLSerializer serializer = new XMLSerializer(System.out, format);
-    serializer.serialize(document);
+//    OutputFormat format = new OutputFormat(document);
+//    format.setIndenting(true);
+//    XMLSerializer serializer = new XMLSerializer(System.out, format);
+//    serializer.serialize(document);
   }
 
   private static DOMResult extractSchemaResult(JAXBContext ctx) throws IOException {
 
-    final Set<DOMResult> resultWrapper = Sets.newHashSet();
+    final Set<DOMResult> resultWrapper = new HashSet<DOMResult>();
 
     ctx.generateSchema(new SchemaOutputResolver() {
       @Override
