6fc132644e0e5f0e36600c82fd2332cca2dbf7cb
Afshin Darian <afshin@opengamma.com>
2013-03-07 12:33:56 +0000

    on unload, the gadgetscontainer fires its alive function so all the child gadgets can kill their data connections

--- projects/OG-Web/web-engine/prototype/scripts/og/views/og.views.gadget.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/og.views.gadget.js
@@ -21,7 +21,12 @@ $.register_module({
             },
             gadgetscontainer: function (args) {
                 og.api.rest.compressor.get({content: args.data}).pipe(function (result) {
-                    new og.common.gadgets.GadgetsContainer('.OG-gadgets-container-', 'center').add(result.data.data);
+                    var gadgets = new og.common.gadgets.GadgetsContainer('.OG-gadgets-container-', 'center');
+                    gadgets.add(result.data.data);
+                    $(window).off('unload.gadgetscontainer').on('unload.gadgets.container', function () {
+                        $('.OG-gadgets-container-center').remove();
+                        gadgets.alive();
+                    });
                 });
             },
             grid: function (args) {
