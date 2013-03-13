3a37210073ac5694a671f353145c808aede137b1
Afshin Darian <afshin@opengamma.com>
2013-03-13 17:08:42 +0000

    bugfix for labelled matrix 2D clipboards where there are as many column headings as columns, GUI-119

--- projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.clipboard.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/analytics/og.analytics.clipboard.js
@@ -23,9 +23,10 @@ $.register_module({
                 if (!single) return value.v || '**2D MATRIX**';
                 var rows, cols, matrix;
                 value = value.v || value;
-                cols = [''].concat(value['xLabels']);
                 rows = value['yLabels'];
                 matrix = value.matrix;
+                cols = value['xLabels'].length === 1 + matrix[0].length ? value['xLabels']
+                    : [''].concat(value['xLabels']);
                 return cols.join(tab) + line + matrix
                     .map(function (row, idx) {return rows[idx] + tab + row.join(tab);}).join(line);
             },
