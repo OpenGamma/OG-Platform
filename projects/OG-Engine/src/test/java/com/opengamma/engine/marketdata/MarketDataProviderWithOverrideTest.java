37597804b5ae2e1b9866c2c2269fc8d402282985
Andrew Griffin <andrew@opengamma.com>
2013-03-18 10:14:16 +0000

    Correct Mockito assertions - callback no longer a singleton set, but arraylist with one element.

--- projects/OG-Engine/src/test/java/com/opengamma/engine/marketdata/MarketDataProviderWithOverrideTest.java
+++ projects/OG-Engine/src/test/java/com/opengamma/engine/marketdata/MarketDataProviderWithOverrideTest.java
@@ -10,6 +10,8 @@ import static org.mockito.Mockito.verify;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.mockito.Mockito;
@@ -60,7 +62,7 @@ public class MarketDataProviderWithOverrideTest {
     p1.awaitSubscriptionResponses();
     p2.awaitSubscriptionResponses();
 
-    verify(listener).subscriptionsSucceeded(Collections.singleton(spec));
+    verify(listener).subscriptionsSucceeded(new ArrayList<ValueSpecification>(Arrays.asList(spec)));
     verify(listener, VerificationModeFactory.noMoreInteractions()).subscriptionsSucceeded(Collections.singleton(Mockito.<ValueSpecification>anyObject()));
     verify(listener, VerificationModeFactory.noMoreInteractions()).subscriptionFailed(Mockito.<ValueSpecification>anyObject(), Mockito.anyString());
 
