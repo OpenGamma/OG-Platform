81e25c168f0266bdaeba31c6c7e1060bf74c68d9
Ian Turner <ian@opengamma.com>
2013-03-26 10:13:11 +0000

    start at legacy fixed recovery cds

---
+++ projects/OG-Web/web-engine/prototype/scripts/og/blotter/forms/og.blotter.forms.legacyfixedrecoverycds.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * Please see distribution for license.
+ */
+$.register_module({
+    name: 'og.blotter.forms.legacyfixedrecoverycds',
+    dependencies: [],
+    obj: function () {
+        return function (config) {
+            var constructor = this, form, ui = og.common.util.ui, data, validate;
+            if(config.details) {data = config.details.data; data.id = config.details.data.trade.uniqueId;}
+            else {data = {security: {type: "LegacyFixedRecoveryCDS", externalIdBundle: "", attributes: {}}, 
+                trade: og.blotter.util.otc_trade};}
+            data.nodeId = config.portfolio ? config.portfolio.id : null;
+            constructor.load = function () {
+                constructor.title = 'LegacyFixedRecoveryCDS';
+                form = new og.common.util.ui.Form({
+                    module: 'og.blotter.forms.simple_tash',
+                    selector: '.OG-blotter-form-block',
+                    data: data,
+                    processor: function (data) {
+                        data.security.name = og.blotter.util.create_name(data);
+                        og.blotter.util.cleanup(data);
+                    }
+                });
+                form.children.push(
+                    new og.blotter.forms.blocks.Portfolio({form: form, counterparty: data.trade.counterparty,
+                        portfolio: data.nodeId, trade: data.trade}),
+                    new og.common.util.ui.Attributes({
+                        form: form, attributes: data.trade.attributes, index: 'trade.attributes'
+                    })
+                );
+                form.dom();
+                form.on('form:load', function (){
+                    og.blotter.util.add_date_picker('.blotter-date');
+                    og.blotter.util.add_time_picker('.blotter-time');
+                    og.blotter.util.set_initial_focus();
+                    if(data.security.length) return;
+                });
+                form.on('form:submit', function (result){
+                    $.when(config.handler(result.data)).then(validate);
+                });
+            };
+            constructor.load();
+            constructor.submit = function (handler) {
+                validate = handler;
+                form.submit();
+            };
+            constructor.submit_new = function (handler) {
+                validate = handler;
+                delete data.id;
+                form.submit();
+            };
+        };
+    }
+});
