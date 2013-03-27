0ed0e0fb3cfafb6fe51a4d4f26f42c8e4da8d0fa
Ian Turner <ian@opengamma.com>
2013-03-27 17:28:32 +0000

    base cds file to handle 6 legacy and standard cds

--- projects/OG-Web/web-engine/prototype/scripts/og/blotter/forms/og.blotter.forms.legacyfixedrecoverycds.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/blotter/forms/og.blotter.forms.legacyfixedrecoverycds.js
@@ -7,51 +7,10 @@ $.register_module({
     dependencies: [],
     obj: function () {
         return function (config) {
-            var constructor = this, form, ui = og.common.util.ui, data, validate;
-            if(config.details) {data = config.details.data; data.id = config.details.data.trade.uniqueId;}
-            else {data = {security: {type: "LegacyFixedRecoveryCDS", externalIdBundle: "", attributes: {}}, 
-                trade: og.blotter.util.otc_trade};}
-            data.nodeId = config.portfolio ? config.portfolio.id : null;
-            constructor.load = function () {
-                constructor.title = 'Legacy Fixed Recovery CDS';
-                form = new og.common.util.ui.Form({
-                    module: 'og.blotter.forms.simple_tash',
-                    selector: '.OG-blotter-form-block',
-                    data: data,
-                    processor: function (data) {
-                        data.security.name = og.blotter.util.create_name(data);
-                        og.blotter.util.cleanup(data);
-                    }
-                });
-                form.children.push(
-                    new og.blotter.forms.blocks.Portfolio({form: form, counterparty: data.trade.counterparty,
-                        portfolio: data.nodeId, trade: data.trade}),
-                    new og.blotter.forms.blocks.cds({form: form, data: data, standard: true, stdvanilla: true}),
-                    new og.common.util.ui.Attributes({
-                        form: form, attributes: data.trade.attributes, index: 'trade.attributes'
-                    })
-                );
-                form.dom();
-                form.on('form:load', function (){
-                    og.blotter.util.add_date_picker('.blotter-date');
-                    og.blotter.util.add_time_picker('.blotter-time');
-                    og.blotter.util.set_initial_focus();
-                    if(data.security.length) return;
-                });
-                form.on('form:submit', function (result){
-                    $.when(config.handler(result.data)).then(validate);
-                });
-            };
-            constructor.load();
-            constructor.submit = function (handler) {
-                validate = handler;
-                form.submit();
-            };
-            constructor.submit_new = function (handler) {
-                validate = handler;
-                delete data.id;
-                form.submit();
-            };
-        };
+            config.title = 'Legacy Fixed Recovery CDS';
+            config.type = 'LegacyFixedRecoveryCDS';
+            config.legacy = true;
+            return new og.blotter.forms.cds(config);
+        };    
     }
 });
