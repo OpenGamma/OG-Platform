893d629f033bf383ed3ab0eeb724e8516587c271
Antonio Greco <antonio@opengamma.com>
2013-04-08 17:40:13 +0100

    css OG namespace, initial tooltip blurkill

--- projects/OG-Web/web-engine/prototype/scripts/og/common/og.common.tooltip.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/og.common.tooltip.js
@@ -11,16 +11,6 @@ $.register_module({
                 var module = this, tooltip, tooltip_offsets, orientation = '', offsets, height, width,
                     viewport, timed_hover, total_width, total_height;
 
-                $('body').append(
-                    '<div class="tooltip">'+
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'+
-                        'Nullam consectetur quam a sapien egestas eget scelerisque'+
-                        'lectus tempor. Duis placerat tellus at erat pellentesque nec'+
-                        'ultricies erat molestie. Integer nec orci id tortor molestie'+
-                        'porta. Suspendisse eu sagittis quam.'+
-                    '</div>'
-                );
-
                 var hide_tooltip = function (event) {
                     tooltip.removeAttr('style').removeClass(orientation).hide();
                 };
@@ -28,56 +18,56 @@ $.register_module({
                 var show_tooltip = function (dir) {
                     orientation = dir;
                     switch (dir) {
-                        case 'north' : {
+                        case 'og-north' : {
                             tooltip.addClass(orientation).css({
                                 top: offsets.top + height + 'px',
                                 left: offsets.left - (tooltip.outerWidth()/2) + width/2 + 'px'
                             })
                             break;
                         }
-                        case 'north-east-flip' : {
+                        case 'og-north-east-flip' : {
                             tooltip.addClass(orientation).css({
                                 top: offsets.top + height/2 + 'px',
                                 left: offsets.left - tooltip.outerWidth() + 'px'
                             })
                             break;
                         }
-                        case 'north-west-flip' : {
+                        case 'og-north-west-flip' : {
                             tooltip.addClass(orientation).css({
                                 top: offsets.top + height/2 + 'px',
                                 left: offsets.left + width + 'px'
                             })
                             break;
                         }
-                        case 'east' : {
+                        case 'og-east' : {
                             tooltip.addClass(orientation).css({
                                 top: offsets.top - ((tooltip.outerHeight()-height)/2) + 'px',
                                 left: offsets.left - tooltip.outerWidth() + 'px'
                             });
                             break;
                         }
-                        case 'south' : {
+                        case 'og-south' : {
                             tooltip.addClass(orientation).css({
                                 top: offsets.top - tooltip.outerHeight() + 'px',
                                 left: offsets.left - (tooltip.outerWidth()/2) + width/2 + 'px'
                             });
                             break;
                         }
-                        case 'south-east-flip' : {
+                        case 'og-south-east-flip' : {
                             tooltip.addClass(orientation).css({
                                 top: offsets.top - tooltip.outerHeight() + height/2 + 'px',
                                 left: offsets.left - tooltip.outerWidth() + 'px'
                             });
                             break;
                         }
-                        case 'south-west-flip' : {
+                        case 'og-south-west-flip' : {
                             tooltip.addClass(orientation).css({
                                 top: offsets.top - tooltip.outerHeight() + height/2 + 'px',
                                 left: offsets.left + width + 'px'
                             });
                             break;
                         }
-                        case 'west' : {
+                        case 'og-west' : {
                             tooltip.addClass(orientation).css({
                                 top: offsets.top - ((tooltip.outerHeight()-height)/2) + 'px',
                                 left: offsets.left + width + 'px'
@@ -90,47 +80,56 @@ $.register_module({
 
                 $('[data-tooltip]').live('click', function (event) {
                     var elem = $(this);
-
+                    tooltip_offsets = tooltip.offset();
+                    tooltip.removeClass(orientation);
                     offsets = elem.offset(); width = elem.outerWidth(); height = elem.outerHeight();
                     viewport = { height: $(window).height(), width: $(window).width() };
                     total_width = offsets.left + width, total_height = offsets.top + height;
 
-                    if (!tooltip) {
-                        tooltip = $('div.tooltip');
-                        tooltip_offsets = tooltip.offset();
-                    }
-
-                    tooltip.removeClass(orientation);
-
                     // north
                     if (total_height - tooltip.outerHeight() < 0 && total_width - tooltip.outerWidth() > 0 &&
-                        total_width + tooltip.outerWidth() < viewport.width) return show_tooltip('north');
+                        total_width + tooltip.outerWidth() < viewport.width) return show_tooltip('og-north');
 
                     // north east
                     else if (total_width + tooltip.outerWidth() > viewport.width &&
-                        total_height - tooltip.outerHeight() < 0) return show_tooltip('north-east-flip');
+                        total_height - tooltip.outerHeight() < 0) return show_tooltip('og-north-east-flip');
 
                     // north west
                     else if (total_height - tooltip.outerHeight() < 0 && total_width - tooltip.outerWidth() < 0)
-                        return show_tooltip('north-west-flip');
+                        return show_tooltip('og-north-west-flip');
 
                     // south
                     else if (total_height + tooltip.outerHeight() > viewport.height &&
                         total_width + tooltip.outerWidth() < viewport.width &&
-                        total_width - tooltip.outerWidth() > 0) return show_tooltip('south');
+                        total_width - tooltip.outerWidth() > 0) return show_tooltip('og-south');
 
                     //south east
                     else if (total_height + tooltip.outerHeight() > viewport.height &&
-                        total_width + tooltip.outerWidth() > viewport.width) return show_tooltip('south-east-flip');
+                        total_width + tooltip.outerWidth() > viewport.width) return show_tooltip('og-south-east-flip');
 
                     // south west
                     else if (total_height + tooltip.outerHeight() > viewport.height &&
-                        total_width - tooltip.outerWidth() < 0) return show_tooltip('south-west-flip');
+                        total_width - tooltip.outerWidth() < 0) return show_tooltip('og-south-west-flip');
 
                     // east or west
                     else {
-                        if (total_width + tooltip.outerWidth() > viewport.width) return show_tooltip('east');
-                        else if (total_width + tooltip.outerWidth() < viewport.width) return show_tooltip('west');
+                        if (total_width + tooltip.outerWidth() > viewport.width) return show_tooltip('og-east');
+                        else if (total_width + tooltip.outerWidth() < viewport.width) return show_tooltip('og-west');
+                    }
+                });
+
+                $(function () {
+                    if (!tooltip) {
+                        tooltip = $(
+                            '<div class="OG-tooltip">'+
+                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'+
+                                'Nullam consectetur quam a sapien egestas eget scelerisque'+
+                                'lectus tempor. Duis placerat tellus at erat pellentesque nec'+
+                                'ultricies erat molestie. Integer nec orci id tortor molestie'+
+                                'porta. Suspendisse eu sagittis quam.'+
+                            '</div>'
+                        ).blurkill(hide_tooltip);
+                        $('body').append(tooltip);
                     }
                 });
             })()
