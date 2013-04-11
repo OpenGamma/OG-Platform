8d915da04af19768bc84c3bcbffa81de1522ed66
Andrew Griffin <andrew@opengamma.com>
2013-04-11 14:00:23 +0100

    Don't create an intermediate list of result fragments (contains debug code)

--- projects/OG-Engine/src/main/java/com/opengamma/engine/view/cycle/CalculationJobResultStreamConsumer.java
+++ projects/OG-Engine/src/main/java/com/opengamma/engine/view/cycle/CalculationJobResultStreamConsumer.java
@@ -5,15 +5,13 @@
  */
 package com.opengamma.engine.view.cycle;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.opengamma.engine.exec.ExecutionResult;
+import com.opengamma.engine.view.impl.InMemoryViewComputationResultModel;
 import com.opengamma.util.TerminatableJob;
 
 /**
@@ -22,42 +20,36 @@ import com.opengamma.util.TerminatableJob;
  * The results are passed in batches to a {@link SingleComputationCycle} for processing.
  */
 public class CalculationJobResultStreamConsumer extends TerminatableJob {
-  
+
   private static final Logger s_logger = LoggerFactory.getLogger(CalculationJobResultStreamConsumer.class);
-  
+
   private static final ExecutionResult POISON = new ExecutionResult(null, null);
-  
+
   private final BlockingQueue<ExecutionResult> _resultQueue;
   private final SingleComputationCycle _computationCycle;
-  
+
   public CalculationJobResultStreamConsumer(BlockingQueue<ExecutionResult> resultQueue, SingleComputationCycle computationCycle) {
     _resultQueue = resultQueue;
     _computationCycle = computationCycle;
   }
-  
+
   @Override
   protected void runOneCycle() {
     try {
-      List<ExecutionResult> results = new LinkedList<ExecutionResult>();
-      
+      final InMemoryViewComputationResultModel fragmentResultModel = _computationCycle.constructTemplateResultModel();
+      final InMemoryViewComputationResultModel fullResultModel = _computationCycle.getResultModel();
       // Block until at least one item is added, then drain any further items
-      results.add(_resultQueue.take());
-      _resultQueue.drainTo(results);
-      
-      boolean poisoned = false;
-      Iterator<ExecutionResult> it = results.iterator();
-      while (it.hasNext()) {
-        final ExecutionResult result = it.next();
+      ExecutionResult result = _resultQueue.take();
+      do {
         if (result == POISON) {
-          poisoned = true;
-          it.remove();
+          super.terminate();
+        } else {
+          _computationCycle.processExecutionResult(result, fragmentResultModel, fullResultModel);
         }
-      }
-      if (!results.isEmpty()) {
-        _computationCycle.calculationJobsCompleted(results);
-      }
-      if (poisoned) {
-        super.terminate();
+        result = _resultQueue.poll();
+      } while (result != null);
+      if (!fragmentResultModel.isEmpty()) {
+        _computationCycle.notifyFragmentCompleted(fragmentResultModel);
       }
     } catch (InterruptedException e) {
       s_logger.debug("Interrupted while waiting for computation job results");
@@ -66,6 +58,7 @@ public class CalculationJobResultStreamConsumer extends TerminatableJob {
     }
   }
 
+  @Override
   public void terminate() {
     try {
       // Poison the queue
@@ -74,5 +67,5 @@ public class CalculationJobResultStreamConsumer extends TerminatableJob {
       super.terminate();
     }
   }
-  
+
 }
