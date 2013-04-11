d065d8268bc22f94448ea6b720b3b03e581068ed
Andrew Griffin <andrew@opengamma.com>
2013-04-11 18:00:06 +0100

    Allow summation (to zero) at empty nodes.

--- projects/OG-Financial/src/main/java/com/opengamma/financial/value/ValueRequirementAliasFunction.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/value/ValueRequirementAliasFunction.java
@@ -25,25 +25,27 @@ import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.async.AsynchronousExecution;
 
 /**
- * A generic function for exposing a particular value requirement under a different name. This may allow the value to
- * be delivered through a heavily constrained value requirement, but output through a simple value requirement name.
+ * A generic function for exposing a particular value requirement under a different name. This may allow the value to be delivered through a heavily constrained value requirement, but output through a
+ * simple value requirement name.
  */
 public class ValueRequirementAliasFunction extends AbstractFunction.NonCompiledInvoker {
+
+  // REVIEW 2013-04-12 Andrew - This is a poor implementation that doesn't propogate constraints particularly satisfactorily; use ValueRenamingFunction instead
 
   private final String _aliasedValueRequirementName;
   private final String _inputRequirementName;
   private final ValueProperties _inputConstraints;
   private final Set<String> _preservedProperties;
   private final ComputationTargetType _targetType;
-  
+
   /**
    * Constructs an instance.
    * 
-   * @param aliasedValueRequirementName  the value requirement name under which to expose the input value, not null
-   * @param inputRequirementName  the input value requirement name, not null
-   * @param inputConstraints  the static input value requirement constraints, not null
-   * @param preservedProperties  the properties preserved on the output which may be constrained, not null
-   * @param targetType  the function target type, not null
+   * @param aliasedValueRequirementName the value requirement name under which to expose the input value, not null
+   * @param inputRequirementName the input value requirement name, not null
+   * @param inputConstraints the static input value requirement constraints, not null
+   * @param preservedProperties the properties preserved on the output which may be constrained, not null
+   * @param targetType the function target type, not null
    */
   public ValueRequirementAliasFunction(String aliasedValueRequirementName, String inputRequirementName,
       ValueProperties inputConstraints, Set<String> preservedProperties, ComputationTargetType targetType) {
@@ -58,7 +60,7 @@ public class ValueRequirementAliasFunction extends AbstractFunction.NonCompiledI
     _preservedProperties = preservedProperties;
     _targetType = targetType;
   }
-  
+
   @Override
   public ComputationTargetType getTargetType() {
     return _targetType;
@@ -72,12 +74,12 @@ public class ValueRequirementAliasFunction extends AbstractFunction.NonCompiledI
     }
     return ImmutableSet.of(new ValueSpecification(_aliasedValueRequirementName, target.toSpecification(), builder.get()));
   }
-  
+
   @Override
   public Set<ValueRequirement> getRequirements(FunctionCompilationContext context, ComputationTarget target, ValueRequirement desiredValue) {
     return ImmutableSet.of(createValueRequirement(target, desiredValue));
   }
-  
+
   @Override
   public Set<ValueSpecification> getResults(FunctionCompilationContext context, ComputationTarget target, Map<ValueSpecification, ValueRequirement> inputs) {
     final ValueSpecification inputSpec = Iterables.getOnlyElement(inputs.keySet());
@@ -92,7 +94,7 @@ public class ValueRequirementAliasFunction extends AbstractFunction.NonCompiledI
     final ValueRequirement desiredValue = desiredValues.iterator().next();
     return ImmutableSet.of(new ComputedValue(new ValueSpecification(desiredValue.getValueName(), target.toSpecification(), desiredValue.getConstraints()), result));
   }
-  
+
   private ValueRequirement createValueRequirement(ComputationTarget target, ValueRequirement desiredValue) {
     final Builder constraints = _inputConstraints.copy();
     appendPreservedProperties(desiredValue.getConstraints(), constraints);
