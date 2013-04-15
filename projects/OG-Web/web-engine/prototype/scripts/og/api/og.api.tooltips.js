6f9e07dfcdc9741a14a87bae43d8e5b4ddbc2f06
Antonio Greco <antonio@opengamma.com>
2013-04-15 13:22:20 +0100

    help cursor style, viewdef persistent tooltip added.

--- projects/OG-Web/web-engine/prototype/scripts/og/api/og.api.tooltips.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/api/og.api.tooltips.js
@@ -3,14 +3,10 @@ $.register_module({
     dependencies: [],
     obj: function () {
         return {
-            '1': '<em>A list of named <strong>column sets</strong>. These allow the same analytics '+
-                 'to be computed using different settings and viewed side by side at the same time</em>',
-            '2': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'+
-                 'Nullam consectetur quam a sapien egestas eget scelerisque'+
-                 'lectus tempor. Duis placerat tellus at erat pellentesque nec'+
-                 'ultricies erat molestie. Integer nec orci id tortor molestie'+
-                 'porta. Suspendisse eu sagittis quam.',
-
+            '1': 'If checked, this view will start running when the engine starts even if a user does '+
+                 'not load it in a client.',
+            '2': '<em>A list of named <strong>column sets</strong>. These allow the same analytics '+
+                 'to be computed using different settings and viewed side by side at the same time</em>'
         }
     }
 });
