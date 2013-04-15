b9a01a89d0109626429621f8aa70e443ce4824a7
Chris Kent <cjkent@hotmail.com>
2013-04-16 00:05:52 +0100

    Reinstated TimingAnalyticsView to help with UI debugging

--- projects/OG-Web/src/main/java/com/opengamma/web/analytics/AnalyticsViewManager.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/analytics/AnalyticsViewManager.java
@@ -69,17 +69,17 @@ public class AnalyticsViewManager {
   private final ExecutorService _portfolioResolutionExecutor;
 
   public AnalyticsViewManager(ViewProcessor viewProcessor,
-      ExecutionFlags.ParallelRecompilationMode parallelViewRecompilation,
-      AggregatedViewDefinitionManager aggregatedViewDefManager,
-      MarketDataSnapshotMaster snapshotMaster,
-      ComputationTargetResolver targetResolver,
-      NamedMarketDataSpecificationRepository marketDataSpecificationRepository,
-      BlotterColumnMapper blotterColumnMapper,
-      PositionSource positionSource,
-      ConfigSource configSource,
-      SecuritySource securitySource,
-      SecurityMaster securityMaster,
-      PositionMaster positionMaster) {
+                              ExecutionFlags.ParallelRecompilationMode parallelViewRecompilation,
+                              AggregatedViewDefinitionManager aggregatedViewDefManager,
+                              MarketDataSnapshotMaster snapshotMaster,
+                              ComputationTargetResolver targetResolver,
+                              NamedMarketDataSpecificationRepository marketDataSpecificationRepository,
+                              BlotterColumnMapper blotterColumnMapper,
+                              PositionSource positionSource,
+                              ConfigSource configSource,
+                              SecuritySource securitySource,
+                              SecurityMaster securityMaster,
+                              PositionMaster positionMaster) {
     ArgumentChecker.notNull(viewProcessor, "viewProcessor");
     ArgumentChecker.notNull(aggregatedViewDefManager, "aggregatedViewDefManager");
     ArgumentChecker.notNull(snapshotMaster, "snapshotMaster");
@@ -116,7 +116,6 @@ public class AnalyticsViewManager {
 
   /**
    * Creates a new view.
-   * 
    * @param request Details of the view
    * @param clientId ID of the client connection
    * @param user User requesting the view
@@ -127,13 +126,13 @@ public class AnalyticsViewManager {
    * @param primitivesGridId ID that's passed to the listener when the view's primitives grid structure changes
    */
   public void createView(ViewRequest request,
-      String clientId,
-      UserPrincipal user,
-      ClientConnection clientConnection,
-      String viewId,
-      Object viewCallbackId,
-      String portfolioGridId,
-      String primitivesGridId) {
+                         String clientId,
+                         UserPrincipal user,
+                         ClientConnection clientConnection,
+                         String viewId,
+                         Object viewCallbackId,
+                         String portfolioGridId,
+                         String primitivesGridId) {
     if (_viewConnections.containsKey(viewId)) {
       throw new IllegalArgumentException("View ID " + viewId + " is already in use");
     }
@@ -156,10 +155,10 @@ public class AnalyticsViewManager {
       resolvedPortfolio = null;
     }
     portfolioSupplier = new PortfolioSupplier(portfolioObjectId,
-        versionCorrection,
-        _positionSource,
-        _securitySource,
-        _portfolioResolutionExecutor);
+                                              versionCorrection,
+                                              _positionSource,
+                                              _securitySource,
+                                              _portfolioResolutionExecutor);
     // TODO something a bit more sophisticated with the executor
     ViewClient viewClient = _viewProcessor.createViewClient(user);
     s_logger.debug("Client ID {} creating new view with ID {}", clientId, viewId);
@@ -167,24 +166,25 @@ public class AnalyticsViewManager {
     PortfolioEntityExtractor entityExtractor = new PortfolioEntityExtractor(versionCorrection, _securityMaster);
     // TODO add filtering change listener to portfolio master which calls portfolioChanged() on the outer view
     AnalyticsView view = new SimpleAnalyticsView(resolvedPortfolio,
-        versionCorrection,
-        viewId,
-        portfolioGridId,
-        primitivesGridId,
-        _targetResolver,
-        viewportListener,
-        _blotterColumnMapper,
-        portfolioSupplier,
-        entityExtractor,
-        request.showBlotterColumns());
+                                                 versionCorrection,
+                                                 viewId,
+                                                 portfolioGridId,
+                                                 primitivesGridId,
+                                                 _targetResolver,
+                                                 viewportListener,
+                                                 _blotterColumnMapper,
+                                                 portfolioSupplier,
+                                                 entityExtractor,
+                                                 request.showBlotterColumns());
     AnalyticsView lockingView = new LockingAnalyticsView(view);
-    //AnalyticsView timingView = new TimingAnalyticsView(lockingView);
     AnalyticsView notifyingView = new NotifyingAnalyticsView(lockingView, clientConnection);
-    AutoCloseable securityListener = new MasterNotificationListener<>(_securityMaster, notifyingView);
-    AutoCloseable positionListener = new MasterNotificationListener<>(_positionMaster, notifyingView);
-    AutoCloseable portfolioListener = new PortfolioListener(portfolioObjectId, notifyingView, _positionSource);
+    AnalyticsView timingView = new TimingAnalyticsView(notifyingView);
+    AutoCloseable securityListener = new MasterNotificationListener<>(_securityMaster, timingView);
+    AutoCloseable positionListener = new MasterNotificationListener<>(_positionMaster, timingView);
+    AutoCloseable portfolioListener = new PortfolioListener(portfolioObjectId, timingView, _positionSource);
     List<AutoCloseable> listeners = Lists.newArrayList(securityListener, positionListener, portfolioListener);
-    AnalyticsViewClientConnection connection = new AnalyticsViewClientConnection(this, request, aggregatedViewDef, viewClient, notifyingView, listeners);
+    AnalyticsViewClientConnection connection =
+        new AnalyticsViewClientConnection(this, request, aggregatedViewDef, viewClient, timingView, listeners);
     _viewConnections.put(viewId, connection);
     // need to notify the listener that the view has been created
     // TODO would it be neater to leave this to the constructor of NotifyingAnalyticsView
@@ -195,7 +195,6 @@ public class AnalyticsViewManager {
 
   /**
    * Deletes a view.
-   * 
    * @param viewId ID of the view
    * @throws DataNotFoundException If there's no view with the specified ID
    */
@@ -210,7 +209,6 @@ public class AnalyticsViewManager {
 
   /**
    * Returns a view given its ID.
-   * 
    * @param viewId ID of the view
    * @return The view
    * @throws DataNotFoundException If there's no view with the specified ID
