33b88ac46dee0a3d265e76fc6782a5388e0620cd
Andrew Griffin <andrew@opengamma.com>
2013-04-16 16:34:23 +0100

    Use the PoolExecutor if one is available on the stack for the default bulk operations.

--- projects/OG-Core/src/main/java/com/opengamma/core/security/AbstractSecuritySource.java
+++ projects/OG-Core/src/main/java/com/opengamma/core/security/AbstractSecuritySource.java
@@ -6,19 +6,46 @@
 package com.opengamma.core.security;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
+import com.opengamma.OpenGammaRuntimeException;
 import com.opengamma.core.AbstractSource;
 import com.opengamma.id.ExternalIdBundle;
 import com.opengamma.id.VersionCorrection;
+import com.opengamma.util.PoolExecutor;
 
 /**
  * Partial implementation of {@link SecuritySource}.
  */
 public abstract class AbstractSecuritySource extends AbstractSource<Security> implements SecuritySource {
 
-  public static Map<ExternalIdBundle, Collection<Security>> getAll(final SecuritySource securitySource, final Collection<ExternalIdBundle> bundles, final VersionCorrection versionCorrection) {
+  public static Map<ExternalIdBundle, Collection<Security>> getAllMultiThread(final PoolExecutor executor, final SecuritySource securitySource, final Collection<ExternalIdBundle> bundles,
+      final VersionCorrection versionCorrection) {
+    final PoolExecutor.Service<Void> jobs = executor.createService(null);
+    final Map<ExternalIdBundle, Collection<Security>> results = Maps.newHashMapWithExpectedSize(bundles.size());
+    for (final ExternalIdBundle bundle : bundles) {
+      jobs.execute(new Runnable() {
+        @Override
+        public void run() {
+          final Collection<Security> result = securitySource.get(bundle, versionCorrection);
+          if ((result != null) && !result.isEmpty()) {
+            results.put(bundle, result);
+          }
+        }
+      });
+    }
+    try {
+      jobs.join();
+    } catch (InterruptedException e) {
+      throw new OpenGammaRuntimeException("Interrupted", e);
+    }
+    return results;
+  }
+
+  public static Map<ExternalIdBundle, Collection<Security>> getAllSingleThread(final SecuritySource securitySource, final Collection<ExternalIdBundle> bundles,
+      final VersionCorrection versionCorrection) {
     final Map<ExternalIdBundle, Collection<Security>> results = Maps.newHashMapWithExpectedSize(bundles.size());
     for (ExternalIdBundle bundle : bundles) {
       final Collection<Security> result = securitySource.get(bundle, versionCorrection);
@@ -29,12 +56,55 @@ public abstract class AbstractSecuritySource extends AbstractSource<Security> im
     return results;
   }
 
+  public static Map<ExternalIdBundle, Collection<Security>> getAll(final SecuritySource securitySource, final Collection<ExternalIdBundle> bundles, final VersionCorrection versionCorrection) {
+    if (bundles.isEmpty()) {
+      return Collections.emptyMap();
+    } else if (bundles.size() == 1) {
+      final ExternalIdBundle bundle = bundles.iterator().next();
+      final Collection<Security> result = securitySource.get(bundle, versionCorrection);
+      if ((result != null) && !result.isEmpty()) {
+        return Collections.singletonMap(bundle, result);
+      } else {
+        return Collections.emptyMap();
+      }
+    }
+    final PoolExecutor executor = PoolExecutor.instance();
+    if (executor != null) {
+      return getAllMultiThread(executor, securitySource, bundles, versionCorrection);
+    } else {
+      return getAllSingleThread(securitySource, bundles, versionCorrection);
+    }
+  }
+
   @Override
   public Map<ExternalIdBundle, Collection<Security>> getAll(final Collection<ExternalIdBundle> bundles, final VersionCorrection versionCorrection) {
     return getAll(this, bundles, versionCorrection);
   }
 
-  public static Map<ExternalIdBundle, Security> getSingle(final SecuritySource securitySource, final Collection<ExternalIdBundle> bundles, final VersionCorrection versionCorrection) {
+  public static Map<ExternalIdBundle, Security> getSingleMultiThread(final PoolExecutor executor, final SecuritySource securitySource, final Collection<ExternalIdBundle> bundles,
+      final VersionCorrection versionCorrection) {
+    final PoolExecutor.Service<Void> jobs = executor.createService(null);
+    final Map<ExternalIdBundle, Security> results = Maps.newHashMapWithExpectedSize(bundles.size());
+    for (final ExternalIdBundle bundle : bundles) {
+      jobs.execute(new Runnable() {
+        @Override
+        public void run() {
+          final Security result = securitySource.getSingle(bundle, versionCorrection);
+          if (result != null) {
+            results.put(bundle, result);
+          }
+        }
+      });
+    }
+    try {
+      jobs.join();
+    } catch (InterruptedException e) {
+      throw new OpenGammaRuntimeException("Interrupted", e);
+    }
+    return results;
+  }
+
+  public static Map<ExternalIdBundle, Security> getSingleSingleThread(final SecuritySource securitySource, final Collection<ExternalIdBundle> bundles, final VersionCorrection versionCorrection) {
     final Map<ExternalIdBundle, Security> results = Maps.newHashMapWithExpectedSize(bundles.size());
     for (ExternalIdBundle bundle : bundles) {
       final Security result = securitySource.getSingle(bundle, versionCorrection);
@@ -43,6 +113,26 @@ public abstract class AbstractSecuritySource extends AbstractSource<Security> im
       }
     }
     return results;
+  }
+
+  public static Map<ExternalIdBundle, Security> getSingle(final SecuritySource securitySource, final Collection<ExternalIdBundle> bundles, final VersionCorrection versionCorrection) {
+    if (bundles.isEmpty()) {
+      return Collections.emptyMap();
+    } else if (bundles.size() == 1) {
+      final ExternalIdBundle bundle = bundles.iterator().next();
+      final Security security = securitySource.getSingle(bundle, versionCorrection);
+      if (security != null) {
+        return Collections.singletonMap(bundle, security);
+      } else {
+        return Collections.emptyMap();
+      }
+    }
+    final PoolExecutor executor = PoolExecutor.instance();
+    if (executor != null) {
+      return getSingleMultiThread(executor, securitySource, bundles, versionCorrection);
+    } else {
+      return getSingleSingleThread(securitySource, bundles, versionCorrection);
+    }
   }
 
   @Override
--- projects/OG-Engine/src/main/java/com/opengamma/engine/target/resolver/AbstractIdentifierResolver.java
+++ projects/OG-Engine/src/main/java/com/opengamma/engine/target/resolver/AbstractIdentifierResolver.java
@@ -6,6 +6,7 @@
 package com.opengamma.engine.target.resolver;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
@@ -21,7 +22,7 @@ import com.opengamma.util.PoolExecutor;
  */
 public abstract class AbstractIdentifierResolver implements IdentifierResolver {
 
-  public static Map<ExternalIdBundle, UniqueId> resolveExternalIds(final PoolExecutor executor, final IdentifierResolver resolver, final Collection<ExternalIdBundle> identifiers,
+  public static Map<ExternalIdBundle, UniqueId> resolveExternalIdsMultiThread(final PoolExecutor executor, final IdentifierResolver resolver, final Collection<ExternalIdBundle> identifiers,
       final VersionCorrection versionCorrection) {
     final PoolExecutor.Service<Void> jobs = executor.createService(null);
     final Map<ExternalIdBundle, UniqueId> result = Maps.newHashMapWithExpectedSize(identifiers.size());
@@ -46,24 +47,40 @@ public abstract class AbstractIdentifierResolver implements IdentifierResolver {
     return result;
   }
 
+  public static Map<ExternalIdBundle, UniqueId> resolveExternalIdsSingleThread(final IdentifierResolver resolver, final Collection<ExternalIdBundle> identifiers,
+      final VersionCorrection versionCorrection) {
+    final Map<ExternalIdBundle, UniqueId> result = Maps.newHashMapWithExpectedSize(identifiers.size());
+    for (final ExternalIdBundle identifier : identifiers) {
+      final UniqueId uid = resolver.resolveExternalId(identifier, versionCorrection);
+      if (uid != null) {
+        result.put(identifier, uid);
+      }
+    }
+    return result;
+  }
+
   public static Map<ExternalIdBundle, UniqueId> resolveExternalIds(final IdentifierResolver resolver, final Collection<ExternalIdBundle> identifiers,
       final VersionCorrection versionCorrection) {
+    if (identifiers.isEmpty()) {
+      return Collections.emptyMap();
+    } else if (identifiers.size() == 1) {
+      final ExternalIdBundle identifier = identifiers.iterator().next();
+      final UniqueId uid = resolver.resolveExternalId(identifier, versionCorrection);
+      if (uid != null) {
+        return Collections.singletonMap(identifier, uid);
+      } else {
+        return Collections.emptyMap();
+      }
+    }
     final PoolExecutor executor = PoolExecutor.instance();
     if (executor != null) {
-      return resolveExternalIds(executor, resolver, identifiers, versionCorrection);
+      return resolveExternalIdsMultiThread(executor, resolver, identifiers, versionCorrection);
     } else {
-      final Map<ExternalIdBundle, UniqueId> result = Maps.newHashMapWithExpectedSize(identifiers.size());
-      for (final ExternalIdBundle identifier : identifiers) {
-        final UniqueId uid = resolver.resolveExternalId(identifier, versionCorrection);
-        if (uid != null) {
-          result.put(identifier, uid);
-        }
-      }
-      return result;
+      return resolveExternalIdsSingleThread(resolver, identifiers, versionCorrection);
     }
   }
 
-  public static Map<ObjectId, UniqueId> resolveObjectIds(final PoolExecutor executor, final IdentifierResolver resolver, final Collection<ObjectId> identifiers,
+  public static Map<ObjectId, UniqueId> resolveObjectIdsMultiThread(final PoolExecutor executor, final IdentifierResolver resolver, final Collection<ObjectId> identifiers,
       final VersionCorrection versionCorrection) {
     final PoolExecutor.Service<Void> jobs = executor.createService(null);
     final Map<ObjectId, UniqueId> result = Maps.newHashMapWithExpectedSize(identifiers.size());
@@ -88,19 +105,34 @@ public abstract class AbstractIdentifierResolver implements IdentifierResolver {
     return result;
   }
 
+  public static Map<ObjectId, UniqueId> resolveObjectIdsSingleThread(final IdentifierResolver resolver, final Collection<ObjectId> identifiers, final VersionCorrection versionCorrection) {
+    final Map<ObjectId, UniqueId> result = Maps.newHashMapWithExpectedSize(identifiers.size());
+    for (final ObjectId identifier : identifiers) {
+      final UniqueId uid = resolver.resolveObjectId(identifier, versionCorrection);
+      if (uid != null) {
+        result.put(identifier, uid);
+      }
+    }
+    return result;
+  }
+
   public static Map<ObjectId, UniqueId> resolveObjectIds(final IdentifierResolver resolver, final Collection<ObjectId> identifiers, final VersionCorrection versionCorrection) {
+    if (identifiers.isEmpty()) {
+      return Collections.emptyMap();
+    } else if (identifiers.size() == 1) {
+      final ObjectId identifier = identifiers.iterator().next();
+      final UniqueId uid = resolver.resolveObjectId(identifier, versionCorrection);
+      if (uid != null) {
+        return Collections.singletonMap(identifier, uid);
+      } else {
+        return Collections.emptyMap();
+      }
+    }
     final PoolExecutor executor = PoolExecutor.instance();
     if (executor != null) {
-      return resolveObjectIds(executor, resolver, identifiers, versionCorrection);
+      return resolveObjectIdsMultiThread(executor, resolver, identifiers, versionCorrection);
     } else {
-      final Map<ObjectId, UniqueId> result = Maps.newHashMapWithExpectedSize(identifiers.size());
-      for (final ObjectId identifier : identifiers) {
-        final UniqueId uid = resolver.resolveObjectId(identifier, versionCorrection);
-        if (uid != null) {
-          result.put(identifier, uid);
-        }
-      }
-      return result;
+      return resolveObjectIdsSingleThread(resolver, identifiers, versionCorrection);
     }
   }
 
