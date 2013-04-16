11a89f3e8ee2f552cef0a78b85b4420e6ec5289c
Jonathan Senior <jonathan@opengamma.com>
2013-04-16 14:23:12 +0100

    Negative notional if sell

--- projects/OG-Financial/src/main/java/com/opengamma/financial/security/FinancialSecurityUtils.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/security/FinancialSecurityUtils.java
@@ -1209,14 +1209,14 @@ public class FinancialSecurityUtils {
         public CurrencyAmount visitStandardVanillaCDSSecurity(final StandardVanillaCDSSecurity security) {
           final InterestRateNotional notional = security.getNotional();
           final int sign = security.isBuy() ? 1 : -1;
-          return CurrencyAmount.of(notional.getCurrency(), notional.getAmount());
+          return CurrencyAmount.of(notional.getCurrency(), sign * notional.getAmount());
         }
 
         @Override
         public CurrencyAmount visitLegacyVanillaCDSSecurity(final LegacyVanillaCDSSecurity security) {
           final InterestRateNotional notional = security.getNotional();
           final int sign = security.isBuy() ? 1 : -1;
-          return CurrencyAmount.of(notional.getCurrency(), notional.getAmount());
+          return CurrencyAmount.of(notional.getCurrency(), sign * notional.getAmount());
         }
 
         @Override
