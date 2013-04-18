e7c5802568aae041ebc0d6c7e1120851b843e913
Antonio Greco <agreco13@gmail.com>
2013-04-18 10:27:21 +0100

    removed em tag from 'column sets' tooltip

--- projects/OG-Web/web-engine/prototype/scripts/og/api/og.api.tooltips.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/api/og.api.tooltips.js
@@ -5,8 +5,8 @@ $.register_module({
         return {
             '1': 'If checked, this view will start running when the engine starts even if a user does '+
                  'not load it in a client.',
-            '2': '<em>A list of named <strong>column sets</strong>. These allow the same analytics '+
-                 'to be computed using different settings and viewed side by side at the same time</em>'
+            '2': 'A list of named <strong>column sets</strong>. These allow the same analytics '+
+                 'to be computed using different settings and viewed side by side at the same time'
         }
     }
 });
