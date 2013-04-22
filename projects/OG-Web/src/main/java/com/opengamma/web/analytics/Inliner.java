d084f98aa82a3353dfa9d7609515a4110615548a
Chris Kent <chris@opengamma.com>
2013-04-22 11:08:37 +0100

    [GUI-227] Column type for inline 1D matrix columns

--- projects/OG-Web/src/main/java/com/opengamma/web/analytics/Inliner.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/analytics/Inliner.java
@@ -12,7 +12,6 @@ import java.util.Set;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.opengamma.engine.value.ValueRequirementNames;
-import com.opengamma.financial.analytics.LabelledMatrix1D;
 import com.opengamma.financial.analytics.LocalDateLabelledMatrix1D;
 
 /**
@@ -21,43 +20,15 @@ import com.opengamma.financial.analytics.LocalDateLabelledMatrix1D;
 /* package */ class Inliner {
 
   // TODO this is crude and temporary
-  private static Set<String> INLINE_VALUE_NAMES = Sets.newHashSet(
+  private static final Set<String> INLINE_VALUE_NAMES = Sets.newHashSet(
       ValueRequirementNames.BUCKETED_CS01,
       ValueRequirementNames.BUCKETED_GAMMA_CS01,
       ValueRequirementNames.BUCKETED_IR01);
-
-  /**
-   * If the value can be inlined and displayed over multiple columns this method returns the number of columns.
-   * If it can't be inlined then it returns null.
-   * @param value A calculated value, possibly null
-   * @return The number of columns required to display the inlined value or null if it is null or can't be inlined.
-   */
-  /* package */ static Integer columnCount(Object value) {
-    // do something a bit more sophisticated if we need to support this for more types
-    if (value instanceof LocalDateLabelledMatrix1D) {
-      return ((LocalDateLabelledMatrix1D) value).getValues().length;
-    } else {
-      return null;
-    }
-  }
 
   public static boolean isDisplayableInline(Class<?> type, ColumnSpecification spec) {
     return type != null &&
         LocalDateLabelledMatrix1D.class.isAssignableFrom(type) &&
         INLINE_VALUE_NAMES.contains(spec.getValueName());
-  }
-
-  public static List<String> columnHeaders(Object value) {
-    // do something a bit more generic. label generators etc registered by class
-    if (!(value instanceof LocalDateLabelledMatrix1D)) {
-      return Collections.emptyList();
-    }
-    Object[] labelsObjects = ((LabelledMatrix1D) value).getLabels();
-    List<String> labels = Lists.newArrayListWithCapacity(labelsObjects.length);
-    for (Object labelObject : labelsObjects) {
-      labels.add(labelObject.toString());
-    }
-    return labels;
   }
 
   public static List<ColumnMeta> columnMeta(Object value) {
@@ -66,8 +37,9 @@ import com.opengamma.financial.analytics.LocalDateLabelledMatrix1D;
     }
     LocalDateLabelledMatrix1D matrix = (LocalDateLabelledMatrix1D) value;
     List<ColumnMeta> meta = Lists.newArrayListWithCapacity(matrix.size());
-    for (int i = 0; i < matrix.size(); i++) {
-      meta.add(new ColumnMeta(matrix.getKeys()[i], matrix.getKeys()[i].toString(), Double.class));
+    meta.add(new ColumnMeta(matrix.getKeys()[0], matrix.getKeys()[0].toString(), Double.class, LocalDateLabelledMatrix1D.class));
+    for (int i = 1; i < matrix.size(); i++) {
+      meta.add(new ColumnMeta(matrix.getKeys()[i], matrix.getKeys()[i].toString(), Double.class, null));
     }
     return meta;
   }
@@ -78,12 +50,14 @@ import com.opengamma.financial.analytics.LocalDateLabelledMatrix1D;
   private final Comparable _key;
   private final String _header;
 
-  private final Class<?> _columnType;
+  private final Class<?> _type;
+  private final Class<?> _underlyingType;
 
-  /* package */ ColumnMeta(Comparable key, String header, Class<?> columnType) {
+  /* package */ ColumnMeta(Comparable key, String header, Class<?> type, Class<?> underlyingType) {
     _key = key;
     _header = header;
-    _columnType = columnType;
+    _type = type;
+    _underlyingType = underlyingType;
   }
 
   @SuppressWarnings("unchecked")
@@ -104,8 +78,12 @@ import com.opengamma.financial.analytics.LocalDateLabelledMatrix1D;
     return _header;
   }
 
-  /* package */ Class<?> getColumnType() {
-    return _columnType;
+  /* package */ Class<?> getType() {
+    return _type;
+  }
+
+  /* package */ Class<?> getUnderlyingType() {
+    return _underlyingType;
   }
 
   @Override
