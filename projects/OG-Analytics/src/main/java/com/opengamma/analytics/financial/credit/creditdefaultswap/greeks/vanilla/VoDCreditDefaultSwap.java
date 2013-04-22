1cb79e1764d4a2b10c3e661b94b941fcf4f2c914
Craig <craig@opengamma.com>
2013-04-22 09:23:29 +0100

    Fixed error in jump-to-default calculation for SNCDS

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/credit/creditdefaultswap/greeks/vanilla/VoDCreditDefaultSwap.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/credit/creditdefaultswap/greeks/vanilla/VoDCreditDefaultSwap.java
@@ -23,8 +23,9 @@ import com.opengamma.util.ArgumentChecker;
  * Class containing methods for the computation of Value-on-Default (VoD or jump-to-default risk) for a vanilla Legacy CDS
  */
 public class VoDCreditDefaultSwap {
+
   private static final DayCount ACT_365 = new ActualThreeSixtyFive();
-  private final double _tolerance = 1e-15;
+
   //-------------------------------------------------------------------------------------------------
 
   // TODO : Lots of ongoing work to do in this class - Work In Progress
@@ -32,20 +33,33 @@ public class VoDCreditDefaultSwap {
   // TODO : Further checks on efficacy of input arguments
   // TODO : Need to consider more sophisticated sensitivity calculations e.g. algorithmic differentiation
 
-  // TODO : Need to remove the Math.abs() calls on the MtM calc
+  // NOTE : The calibration is done with PriceType = CLEAN and the valuation is done with whatever the priceType input is
+
+  // NOTE : The SNCDS VoD is calculated as follows.
+  // NOTE : In the event of a default the CDS contract extinguishes with an effective value of 0. The change in MtM on default is
+  // NOTE : therefore 0 - MtM (just prior to default). This is the same for both buy an sell protection positions (since the MtM
+  // NOTE : can be either positive or negative). In addition to this amount a buy protection position will receive LGD from the 
+  // NOTE : payoff of the contingent leg. A sell protection position will make a payment of LGD.
 
   // ------------------------------------------------------------------------------------------------------------------------------------------
 
   // Compute the VoD
 
-  public double getValueOnDefaultCreditDefaultSwap(final ZonedDateTime valuationDate, final LegacyVanillaCreditDefaultSwapDefinition cds, final ISDADateCurve/*ISDACurve*/yieldCurve,
-      final ZonedDateTime[] marketTenors, final double[] marketSpreads, final PriceType priceType) {
+  public double getValueOnDefaultCreditDefaultSwap(
+      final ZonedDateTime valuationDate,
+      final LegacyVanillaCreditDefaultSwapDefinition cds,
+      final ISDADateCurve yieldCurve,
+      final ZonedDateTime[] marketTenors,
+      final double[] marketSpreads,
+      final PriceType priceType) {
+
     ArgumentChecker.notNull(valuationDate, "Valuation date");
     ArgumentChecker.notNull(cds, "CreditDefaultSwapDefinition");
     ArgumentChecker.notNull(yieldCurve, "YieldCurve");
     ArgumentChecker.notNull(marketTenors, "Market tenors");
     ArgumentChecker.notNull(marketSpreads, "Market spreads");
     ArgumentChecker.notNull(priceType, "price type");
+
     CreditMarketDataUtils.checkSpreadData(valuationDate, marketTenors, marketSpreads);
 
     // Vector of time nodes for the hazard rate curve
@@ -60,7 +74,7 @@ public class VoDCreditDefaultSwap {
     final CalibrateHazardRateCurveLegacyCreditDefaultSwap hazardRateCurve = new CalibrateHazardRateCurveLegacyCreditDefaultSwap();
 
     // Calibrate the hazard rate curve to the market observed par CDS spreads (returns calibrated hazard rates as a vector of doubles)
-    final double[] calibratedHazardRates = hazardRateCurve.getCalibratedHazardRateTermStructure(valuationDate, cds, marketTenors, marketSpreads, yieldCurve, priceType);
+    final double[] calibratedHazardRates = hazardRateCurve.getCalibratedHazardRateTermStructure(valuationDate, cds, marketTenors, marketSpreads, yieldCurve, PriceType.CLEAN);
 
     // Build a hazard rate curve object based on the input market data
     final HazardRateCurve calibratedHazardRateCurve = new HazardRateCurve(marketTenors, times, calibratedHazardRates, 0.0);
@@ -83,9 +97,9 @@ public class VoDCreditDefaultSwap {
     double valueOnDefault = 0.0;
 
     if (cds.getBuySellProtection() == BuySellProtection.BUY) {
-      valueOnDefault = -Math.abs(presentValue) + lossGivenDefault;
+      valueOnDefault = -presentValue + lossGivenDefault;
     } else {
-      valueOnDefault = -Math.abs(presentValue) - lossGivenDefault;
+      valueOnDefault = -presentValue - lossGivenDefault;
     }
 
     return valueOnDefault;
