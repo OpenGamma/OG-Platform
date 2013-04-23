f17784d62c1cfaf7ec18910781bb1336a17eaaf2
Ian Turner <ian@opengamma.com>
2013-04-23 17:04:06 +0100

    change histogram bucket sizes

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.histogram.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.histogram.js
@@ -7,7 +7,7 @@ $.register_module({
     dependencies: ['og.common.gadgets.manager'],
     obj: function () {
         return function (config) {
-            var gadget = this, $selector = $(config.selector), histogram, stripped,
+            var gadget = this, $selector = $(config.selector), histogram, stripped, buckets, max, min, range;
                 alive = og.common.id('gadget_histogram');
             $(config.selector).addClass(alive).css({
                 position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: '#fff'
@@ -22,14 +22,20 @@ $.register_module({
                 stripped = data.timeseries.data.reduce(function (a, b){return a.concat(b[1]);}, []);
             };
             var histogram_data = function (data) {
-                var length = stripped.length, count = [], maxcount = 0, i = 0, label,
-                    max_buckets = 50, bucket_calc = Math.ceil(Math.sqrt(length)),
-                    buckets = bucket_calc < max_buckets ? bucket_calc : max_buckets,
-                    max = Math.max.apply(Math, stripped), min = Math.min.apply(Math, stripped),
-                    range = max - min, interval = range / buckets;
+                var length = stripped.length, max_buckets = 50, min_buckets = 10,
+                    bucket_calc = Math.ceil(Math.sqrt(length));
+                buckets = bucket_calc < max_buckets ? bucket_calc : max_buckets;
+                buckets = buckets < min_buckets ? min_buckets : buckets;
+                max = Math.max.apply(Math, stripped);
+                min = Math.min.apply(Math, stripped);
+                range = max - min;
+                return bucket_data(buckets);
+            };
+            var bucket_data = function (buckets) {
+                var interval = range / buckets, count = [], label, i = 0, maxcount = 0;
                 // 2D count array, 1D is the label made up of the lower bound of the bucket
                 for (; i < buckets; i++) {
-                    label = (min + (interval * i))/*/length*/;
+                    label = (min + (interval * i));
                     count[i] = [label, 0];
                 }
                 // 2D count array, 2D is the number of occurances in [bound, next bound)
@@ -40,36 +46,43 @@ $.register_module({
                 });
                 // all values which match the max in the dataset are ignored above and added to the final bucket
                 count[buckets - 1][1] = count[buckets - 1][1] + maxcount;
-                /* test to ensure no data is lost - sum of all items in buckets must match dataset length
+                /* test to ensure no data is lost - sum of all items in buckets must match dataset length */
+                /*
                 if (count.map(function(v) { return v[1]; }).reduce(function(a,b) { return a + b; }) != length)
                     og.dev.warn('bucket totals to not match dataset length');
                 */
-                return {histogram_data: count, interval: interval/*/length*/};
+                return {histogram_data: count, interval: interval};
             };
-            /*normpdf = function (x, mu, sigma, constant) {
+            var bucket_range = function () {
+                var step = buckets/2.5, double_step = 2 * step, round = Math.round, triple_step = 3 * step;
+                return { buckets: { oleft: round(buckets - double_step),ileft: round(buckets - step), mid: buckets,
+                            iright: round(buckets + step), oright: round(buckets + double_step),
+                            ooright: round(buckets + triple_step)}};
+            };
+            var normpdf = function (x, mu, sigma, constant) {
                 var diff = x-mu;
                 return (Math.exp(-( (diff*diff) / (2*(sigma*sigma)) ))) / (sigma*constant);
             };
-            normpdf_data = function () {
+            var normpdf_data = function () {
                 var norm = [], diff = 0, sigma, constant = Math.sqrt(2*Math.PI),
                     mu = stripped.reduce(function(a,b){return a+b;})/stripped.length;
                 $.each(stripped, function(index, value){
                     diff +=  (value-mu)*(value-mu);
                 });
                 sigma = Math.sqrt(diff/(stripped.length-1));
-                //stripped.sort();
                 $.each(stripped, function(index, value){
                     norm.push([value, (normpdf(value, mu, sigma, constant))]);
                 });
                 return {norm_pdf_data:norm};
-            };*/
+            };
             gadget.dataman = new og.analytics.Cell({
                 source: config.source, row: config.row, col: config.col, format: 'EXPANDED'}, 'histogram')
                 .on('data', function (value) {
                     var input, data = typeof value.v !== 'undefined' ? value.v : value;
                     if (!histogram && data && (typeof data === 'object')) {
                         prepare_data(data);
-                        input = $.extend(true, {}, config, histogram_data(data)/*, normpdf_data()*/);
+                        input = $.extend(true, {}, config, histogram_data(data), normpdf_data(),
+                            {callback: bucket_data}, bucket_range());
                         histogram = new og.common.gadgets.HistogramPlot(input);
                     }
                 })
--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.histogram_plot.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.histogram_plot.js
@@ -8,20 +8,54 @@ $.register_module({
     obj: function () {
         var module = this, loading_template;
         return function (config) {
-            var gadget = this, selector = config.selector, $plot, options = {};
+            var gadget = this, selector = config.selector, $selector, $plot, options = {}, plot_template,
+                alive = og.common.id('gadget_histogram_plot'), width, height, buckets_height = 30;;
             gadget.resize = function () {
+                height = $selector.parent().height();
+                width = $selector.width();
+                $selector.find('.og-buckets').height(buckets_height);
+                $selector.find('.og-histogram-plot').height(height - buckets_height);
+                $selector.find('.og-histogram-plot').width(width);
                 load_plots();
             };
-            show_tooltip = function (x, y, contents){
+            gadget.alive = function () {
+                return !!$('.' + alive).length;
+            };
+            show_tooltip = function (x, y, contents) {
                 $('<div id="tooltip">' + contents + '</div>').css( {
                     position: 'absolute', display: 'none', top: y - 70, left: x - 20,
                     padding: '2px', backgroundColor: '#000', opacity: 0.50, zIndex: 6, color: '#fff'
                 }).appendTo("body").fadeIn(200);
             };
-            /*percentage = function (val, points) {
-                return (val*100).toFixed(points || 2) +"%";  
-            };*/
-            load_plots = function () {
+            var setup = function () {
+                $.when(og.api.text({module: 'og.views.gadgets.histogram.plot_tash'})).then(function (plot_tmpl) {
+                    plot_template = plot_tmpl;
+                    $selector = $(selector);
+                    $selector.html((Handlebars.compile(plot_template))({alive: alive, buckets: config.buckets}));
+                    $selector.find('.og-bucket-mid').addClass('OG-link-active');
+                    $selector.find('span').bind('click', function (event) {
+                        var $elm = $(event.target), new_data = config.callback($elm .attr('name'));
+                        $elm.siblings().removeClass('OG-link-active');
+                        $elm.addClass('OG-link-active');
+                        $plot.setData(get_data(new_data));
+                        $plot.setupGrid();
+                        $plot.draw();
+                    });
+                    gadget.resize();
+                });
+            };
+            var get_data = function (input) {
+                return data = [{
+                    label: 'Histogram', hoverable: true,
+                    data: input ? input.histogram_data : config.histogram_data,
+                    bars: { show: true, barWidth: input ? input.interval : config.interval,
+                            fill: true, lineWidth: 1, order: 1, fillColor: '#42669a'},
+                    color: '#fff'},
+                    {label: "Probability density - Normal Distribution", data: config.norm_pdf_data, hoverable: false,
+                     lines: {show: false, fill: false}, points: {show: true}, color: '#AA4643', yaxis: 2 }
+                ];
+            }
+            var load_plots = function () {
                 var previousPoint = null;
                 options = {
                     grid: {
@@ -29,51 +63,14 @@ $.register_module({
                         hoverable: true, aboveData: false
                     },
                     legend: {show: false},
-                    bars: {
-                        show: true,
-                        lineWidth: 0, // in pixels
-                        barWidth: 0, // in units of the x axis
-                        fill: true,
-                        align: 'left', // or "center"
-                        horizontal: false
-                    }/*,
-                    xaxis: {tickFormatter: function (val, axis) {return percentage(val);}}*/
+                    bars: { show: true, lineWidth: 0, barWidth: 0, fill: true, align: 'left', horizontal: false }
                 };
-                var data = [
-                    {
-                        label: 'Histogram',
-                        hoverable: true,
-                        data: config.histogram_data,
-                        bars: {
-                            show: true,
-                            barWidth: config.interval,
-                            fill: true,
-                            lineWidth: 1,
-                            order: 1,
-                            fillColor: '#42669a'
-                         },
-                         color: '#fff'
-                    }/*,
-                    {
-                        label: "Probability density - Normal Distribution",
-                        data: config.norm_pdf_data,
-                        hoverable: false,
-                        lines: {
-                            show: false,
-                            fill: false
-                        },
-                        points: {show: true},
-                        color: '#AA4643',
-                        yaxis: 2
-                    }*/
-                ];
-                $plot = $.plot($(selector), data, options);
+                $plot = $.plot($(selector + ' .og-histogram-plot'), get_data(), options);
                 $(selector).bind('plothover', function (event, pos, item) {
                     if (item) {
                         if (previousPoint != item.dataIndex) {
                             var x = item.datapoint[0], y = item.datapoint[1], delta = x+config.interval,
                                 occur = y == 1 ? ' occurrence ' : ' occurrences ',
-                                //msg = y + occur + 'in range<br/>' + percentage(x, 5) + ' to ' + percentage(delta,5);
                                 msg = y + occur + 'in range<br/>' + x.toFixed(5) + ' to ' + x.toFixed(5);
                                 previousPoint = item.dataIndex;
                             $('#tooltip').remove();
@@ -87,7 +84,7 @@ $.register_module({
                 });
             };
             if (!config.child) og.common.gadgets.manager.register(gadget);
-            load_plots();
+            setup();
         };
     }
 });
