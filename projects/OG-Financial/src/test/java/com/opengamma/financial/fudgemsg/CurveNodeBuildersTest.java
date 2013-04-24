1d0d989d3d7cbc38ff041b637cf354b57290033e
Elaine McLeod <elaine@opengamma.com>
2013-04-24 14:42:51 +0100

    [PLAT-3379] Adding and testing a fudge builder for discount factor nodes

--- projects/OG-Financial/src/test/java/com/opengamma/financial/fudgemsg/CurveNodeBuildersTest.java
+++ projects/OG-Financial/src/test/java/com/opengamma/financial/fudgemsg/CurveNodeBuildersTest.java
@@ -14,8 +14,10 @@ import com.opengamma.financial.analytics.fudgemsg.AnalyticsTestBase;
 import com.opengamma.financial.analytics.ircurve.strips.CashNode;
 import com.opengamma.financial.analytics.ircurve.strips.CreditSpreadNode;
 import com.opengamma.financial.analytics.ircurve.strips.CurveNodeWithIdentifier;
+import com.opengamma.financial.analytics.ircurve.strips.DiscountFactorNode;
 import com.opengamma.financial.analytics.ircurve.strips.FRANode;
 import com.opengamma.financial.analytics.ircurve.strips.RateFutureNode;
+import com.opengamma.financial.analytics.ircurve.strips.SwapNode;
 import com.opengamma.id.ExternalId;
 import com.opengamma.util.test.TestGroup;
 import com.opengamma.util.time.Tenor;
@@ -27,15 +29,21 @@ import com.opengamma.util.time.Tenor;
 public class CurveNodeBuildersTest extends AnalyticsTestBase {
 
   @Test
+  public void testCashNodeBuilder() {
+    final CashNode node = new CashNode(Tenor.ONE_DAY, Tenor.ONE_YEAR, ExternalId.of("convention", "name"), "TEST");
+    assertEquals(node, cycleObject(CashNode.class, node));
+  }
+
+  @Test
   public void testCreditCurveNodeBuilder() {
     final CreditSpreadNode node = new CreditSpreadNode("TEST", Tenor.EIGHT_MONTHS);
     assertEquals(node, cycleObject(CreditSpreadNode.class, node));
   }
 
   @Test
-  public void testCashNodeBuilder() {
-    final CashNode node = new CashNode(Tenor.ONE_DAY, Tenor.ONE_YEAR, ExternalId.of("convention", "name"), "TEST");
-    assertEquals(node, cycleObject(CashNode.class, node));
+  public void testDiscountFactorNodeBuilder() {
+    final DiscountFactorNode node = new DiscountFactorNode("TEST", Tenor.EIGHT_MONTHS);
+    assertEquals(node, cycleObject(DiscountFactorNode.class, node));
   }
 
   @Test
@@ -48,6 +56,12 @@ public class CurveNodeBuildersTest extends AnalyticsTestBase {
   public void testRateFutureNodeBuilder() {
     final RateFutureNode node = new RateFutureNode(2, Tenor.ONE_DAY, Tenor.THREE_MONTHS, Tenor.ONE_MONTH, ExternalId.of("convention", "future"), ExternalId.of("convention", "underlying"), "TEST");
     assertEquals(node, cycleObject(RateFutureNode.class, node));
+  }
+
+  @Test
+  public void testSwapNodeBuilder() {
+    final SwapNode node = new SwapNode(Tenor.ONE_DAY, Tenor.TEN_YEARS, ExternalId.of("convention", "pay"), ExternalId.of("convention", "receive"), "TEST");
+    assertEquals(node, cycleObject(SwapNode.class, node));
   }
 
   @Test
