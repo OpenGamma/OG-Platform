67fdd0a211ddc85052524c382a4d2c37a307f669
Elaine McLeod <elaine@opengamma.com>
2013-04-24 14:48:50 +0100

    Added a function that pulls out market data for new curves

---
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/curve/CurveMarketDataFunction.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.curve;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.threeten.bp.Instant;
+import org.threeten.bp.LocalTime;
+import org.threeten.bp.ZoneOffset;
+import org.threeten.bp.ZonedDateTime;
+
+import com.opengamma.core.config.ConfigSource;
+import com.opengamma.core.marketdatasnapshot.SnapshotDataBundle;
+import com.opengamma.core.value.MarketDataRequirementNames;
+import com.opengamma.engine.ComputationTarget;
+import com.opengamma.engine.ComputationTargetSpecification;
+import com.opengamma.engine.function.AbstractFunction;
+import com.opengamma.engine.function.CompiledFunctionDefinition;
+import com.opengamma.engine.function.FunctionCompilationContext;
+import com.opengamma.engine.function.FunctionExecutionContext;
+import com.opengamma.engine.function.FunctionInputs;
+import com.opengamma.engine.marketdata.ExternalIdBundleResolver;
+import com.opengamma.engine.target.ComputationTargetType;
+import com.opengamma.engine.value.ComputedValue;
+import com.opengamma.engine.value.ValueProperties;
+import com.opengamma.engine.value.ValuePropertyNames;
+import com.opengamma.engine.value.ValueRequirement;
+import com.opengamma.engine.value.ValueRequirementNames;
+import com.opengamma.engine.value.ValueSpecification;
+import com.opengamma.financial.OpenGammaCompilationContext;
+import com.opengamma.financial.analytics.ircurve.strips.CurveNodeWithIdentifier;
+import com.opengamma.id.ExternalIdBundle;
+import com.opengamma.util.ArgumentChecker;
+import com.opengamma.util.async.AsynchronousExecution;
+
+/**
+ * 
+ */
+public class CurveMarketDataFunction extends AbstractFunction {
+  private final String _curveName;
+
+  public CurveMarketDataFunction(final String curveName) {
+    ArgumentChecker.notNull(curveName, "curve name");
+    _curveName = curveName;
+  }
+
+  @Override
+  public CompiledFunctionDefinition compile(final FunctionCompilationContext context, final Instant atInstant) {
+    final ZonedDateTime atZDT = ZonedDateTime.ofInstant(atInstant, ZoneOffset.UTC);
+    final ValueProperties properties = createValueProperties()
+        .with(ValuePropertyNames.CURVE, _curveName)
+        .get();
+    final ValueSpecification spec = new ValueSpecification(ValueRequirementNames.CURVE_MARKET_DATA, ComputationTargetSpecification.NULL, properties);
+    return new AbstractInvokingCompiledFunction(atZDT.with(LocalTime.MIDNIGHT), atZDT.plusDays(1).with(LocalTime.MIDNIGHT).minusNanos(1000000)) {
+
+      @Override
+      public Set<ComputedValue> execute(final FunctionExecutionContext executionContext, final FunctionInputs inputs, final ComputationTarget target,
+          final Set<ValueRequirement> desiredValues) throws AsynchronousExecution {
+        final SnapshotDataBundle marketData = new SnapshotDataBundle();
+        final ExternalIdBundleResolver resolver = new ExternalIdBundleResolver(context.getComputationTargetResolver());
+        for (final ComputedValue value : inputs.getAllValues()) {
+          final ExternalIdBundle identifiers = value.getSpecification().getTargetSpecification().accept(resolver);
+          final double rate = (Double) value.getValue();
+          marketData.setDataPoint(identifiers, rate);
+        }
+        return Collections.singleton(new ComputedValue(spec, marketData));
+      }
+
+      @Override
+      public ComputationTargetType getTargetType() {
+        return ComputationTargetType.NULL;
+      }
+
+      @Override
+      public Set<ValueSpecification> getResults(final FunctionCompilationContext context, final ComputationTarget target) {
+        return Collections.singleton(spec);
+      }
+
+      @Override
+      public Set<ValueRequirement> getRequirements(final FunctionCompilationContext context, final ComputationTarget target, final ValueRequirement desiredValue) {
+        final Set<ValueRequirement> requirements = new HashSet<>();
+        final ConfigSource configSource = OpenGammaCompilationContext.getConfigSource(context);
+        final CurveSpecification specification = CurveUtils.getCurveSpecification(atInstant, configSource, atZDT.toLocalDate(), _curveName);
+        for (final CurveNodeWithIdentifier id : specification.getNodes()) {
+          requirements.add(new ValueRequirement(MarketDataRequirementNames.MARKET_VALUE, ComputationTargetType.PRIMITIVE, id.getIdentifier()));
+        }
+        return requirements;
+      }
+
+      @Override
+      public boolean canHandleMissingRequirements() {
+        return true;
+      }
+
+      @Override
+      public boolean canHandleMissingInputs() {
+        return true;
+      }
+    };
+  }
+}
