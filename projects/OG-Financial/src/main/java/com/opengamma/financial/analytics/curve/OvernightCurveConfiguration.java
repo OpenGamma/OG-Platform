0eb4c6422699b6b36fd9f4b6efd3942214983458
Elaine McLeod <elaine@opengamma.com>
2013-04-24 19:08:36 +0100

    First pass at curve exposure configuration objects

---
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/curve/OvernightCurveConfiguration.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.curve;
+
+/**
+ * 
+ */
+public class OvernightCurveConfiguration {
+
+  /**
+   * 
+   */
+  public OvernightCurveConfiguration() {
+  }
+
+}
