f6ff7e64545213075df8c0a75e6e8ce18c0a0ba3
Andrew Griffin <andrew@opengamma.com>
2013-04-26 15:41:41 +0100

    Mark a "warn" level when an exception is reported. Produce verbose text for missing inputs and outputs when in FULL mode.

--- projects/OG-Engine/src/main/java/com/opengamma/engine/view/cycle/CalculationJobResultStreamConsumer.java
+++ projects/OG-Engine/src/main/java/com/opengamma/engine/view/cycle/CalculationJobResultStreamConsumer.java
@@ -5,6 +5,8 @@
  */
 package com.opengamma.engine.view.cycle;
 
+import java.util.Collection;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
@@ -19,6 +21,7 @@ import com.google.common.collect.Sets;
 import com.opengamma.engine.cache.CacheSelectHint;
 import com.opengamma.engine.cache.ViewComputationCache;
 import com.opengamma.engine.calcnode.CalculationJobResultItem;
+import com.opengamma.engine.calcnode.MutableExecutionLog;
 import com.opengamma.engine.depgraph.DependencyGraph;
 import com.opengamma.engine.depgraph.DependencyNode;
 import com.opengamma.engine.exec.DefaultAggregatedExecutionLog;
@@ -29,10 +32,13 @@ import com.opengamma.engine.value.ComputedValueResult;
 import com.opengamma.engine.value.ValueRequirement;
 import com.opengamma.engine.value.ValueSpecification;
 import com.opengamma.engine.view.AggregatedExecutionLog;
+import com.opengamma.engine.view.ExecutionLog;
 import com.opengamma.engine.view.ExecutionLogMode;
 import com.opengamma.engine.view.impl.ExecutionLogModeSource;
 import com.opengamma.engine.view.impl.InMemoryViewComputationResultModel;
 import com.opengamma.util.TerminatableJob;
+import com.opengamma.util.log.LogLevel;
+import com.opengamma.util.log.SimpleLogEvent;
 import com.opengamma.util.tuple.Pair;
 
 /**
@@ -52,6 +58,29 @@ public class CalculationJobResultStreamConsumer extends TerminatableJob {
   public CalculationJobResultStreamConsumer(final BlockingQueue<ExecutionResult> resultQueue, final SingleComputationCycle computationCycle) {
     _resultQueue = resultQueue;
     _computationCycle = computationCycle;
+  }
+
+  private String toString(final String prefix, final Collection<?> values) {
+    final StringBuilder sb = new StringBuilder(prefix);
+    if (values.size() > 1) {
+      sb.append("s - { ");
+      int count = 0;
+      for (Object value : values) {
+        count++;
+        if (count > 1) {
+          sb.append(", ");
+          if (count > 10) {
+            sb.append("...");
+            break;
+          }
+        }
+        sb.append(value.toString());
+      }
+      sb.append(" }");
+    } else {
+      sb.append(" - ").append(values.iterator().next().toString());
+    }
+    return sb.toString();
   }
 
   @Override
@@ -83,21 +112,54 @@ public class CalculationJobResultStreamConsumer extends TerminatableJob {
             assert itrNode.hasNext();
             final CalculationJobResultItem jobResultItem = itrResultItem.next();
             final DependencyNode node = itrNode.next();
-            final Set<AggregatedExecutionLog> inputLogs = new LinkedHashSet<AggregatedExecutionLog>();
-            for (final ValueSpecification inputValueSpec : node.getInputValues()) {
-              final DependencyNodeJobExecutionResult nodeResult = jobExecutionResultCache.get(inputValueSpec);
-              if (nodeResult == null) {
-                // Market data
-                continue;
-              }
-              inputLogs.add(nodeResult.getAggregatedExecutionLog());
-            }
             final ExecutionLogMode executionLogMode = logModes.getLogMode(node);
             final AggregatedExecutionLog aggregatedExecutionLog;
             if (executionLogMode == ExecutionLogMode.FULL) {
-              aggregatedExecutionLog = DefaultAggregatedExecutionLog.fullLogMode(node, jobResultItem.getExecutionLog(), inputLogs);
+              final ExecutionLog log = jobResultItem.getExecutionLog();
+              MutableExecutionLog logCopy = null;
+              final Set<AggregatedExecutionLog> inputLogs = new LinkedHashSet<AggregatedExecutionLog>();
+              Set<ValueSpecification> missing = jobResultItem.getMissingInputs();
+              if (!missing.isEmpty()) {
+                if (logCopy == null) {
+                  logCopy = new MutableExecutionLog(log);
+                }
+                logCopy.add(new SimpleLogEvent(log.hasException() ? LogLevel.WARN : LogLevel.INFO, toString("Missing input", missing)));
+              }
+              missing = jobResultItem.getMissingOutputs();
+              if (!missing.isEmpty()) {
+                if (logCopy == null) {
+                  logCopy = new MutableExecutionLog(log);
+                }
+                logCopy.add(new SimpleLogEvent(LogLevel.WARN, toString("Failed to produce output", missing)));
+              }
+              for (final ValueSpecification inputValueSpec : node.getInputValues()) {
+                final DependencyNodeJobExecutionResult nodeResult = jobExecutionResultCache.get(inputValueSpec);
+                if (nodeResult == null) {
+                  // Market data
+                  continue;
+                }
+                inputLogs.add(nodeResult.getAggregatedExecutionLog());
+              }
+              aggregatedExecutionLog = DefaultAggregatedExecutionLog.fullLogMode(node, (logCopy != null) ? logCopy : log, inputLogs);
             } else {
-              aggregatedExecutionLog = DefaultAggregatedExecutionLog.indicatorLogMode(jobResultItem.getExecutionLog().getLogLevels());
+              EnumSet<LogLevel> logs = jobResultItem.getExecutionLog().getLogLevels();
+              boolean copied = false;
+              for (final ValueSpecification inputValueSpec : node.getInputValues()) {
+                final DependencyNodeJobExecutionResult nodeResult = jobExecutionResultCache.get(inputValueSpec);
+                if (nodeResult == null) {
+                  // Market data
+                  continue;
+                }
+                if (logs.containsAll(nodeResult.getAggregatedExecutionLog().getLogLevels())) {
+                  continue;
+                }
+                if (!copied) {
+                  copied = true;
+                  logs = EnumSet.copyOf(logs);
+                }
+                logs.addAll(nodeResult.getAggregatedExecutionLog().getLogLevels());
+              }
+              aggregatedExecutionLog = DefaultAggregatedExecutionLog.indicatorLogMode(logs);
             }
             final DependencyNodeJobExecutionResult jobExecutionResult = new DependencyNodeJobExecutionResult(computeNodeId, jobResultItem, aggregatedExecutionLog);
             final Set<ValueSpecification> nodeTerminals = node.getTerminalOutputValues();
