0de41477667ecdbba4f902d030c71743461b166c
Richard White <richard@opengamma.com>
2013-04-29 11:45:47 +0100

    [PLAT-3306], [PLAT-3309] still some clean up to do

---
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/volatility/VolatilityTermStructure.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.analytics.financial.model.volatility;
+
+/**
+ * Gives an volatility as a function of time 
+ */
+public interface VolatilityTermStructure extends VolatilityModel<Double> {
+
+}
