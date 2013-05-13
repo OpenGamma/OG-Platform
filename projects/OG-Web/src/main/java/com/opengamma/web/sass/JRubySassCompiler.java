38c6e9f880547b1d2fe40031f64704839c81b7cd
Yomi Ayodele <yomi@opengamma.com>
2013-05-13 11:03:11 +0100

    [PLAT-3397] parse ruby scripts to improve performance

--- projects/OG-Web/src/main/java/com/opengamma/web/sass/JRubySassCompiler.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/sass/JRubySassCompiler.java
@@ -8,18 +8,24 @@ package com.opengamma.web.sass;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.JarURLConnection;
 import java.net.URL;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import java.net.URLConnection;
+import java.util.jar.JarFile;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+import org.jruby.RubyInstanceConfig.CompileMode;
+import org.jruby.embed.ScriptingContainer;
+import org.jruby.javasupport.JavaEmbedUtils.EvalUnit;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.opengamma.OpenGammaRuntimeException;
 import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.ZipUtils;
+import com.opengamma.util.monitor.OperationTimer;
 
 /**
  * Jruby sass compiler
@@ -28,7 +34,7 @@ import com.opengamma.util.ZipUtils;
  */
 public final class JRubySassCompiler {
   
-  private static final JRubySassCompiler s_instance = new JRubySassCompiler();
+  private static final Logger s_logger = LoggerFactory.getLogger(JRubySassCompiler.class);
   
   /**
    * Jruby GEMS_PATH
@@ -43,25 +49,39 @@ public final class JRubySassCompiler {
     extractRubySassGem();
   }
   
-  private String _options;
+  private static final JRubySassCompiler s_instance = new JRubySassCompiler();
   
+  private final String _options;
+  private final ScriptingContainer _container;
+  private final EvalUnit _sassConvertUnit;
+  private final EvalUnit _updateStyleSheetsUnit;
+ 
   private JRubySassCompiler() {
-    
     File cacheLocation = new File(new File(System.getProperty("java.io.tmpdir")), "sass-cache");
     _options = ":syntax => :scss, :always_update => true, :style => :expanded, :cache_location => '" + cacheLocation.toString() + "'";
+    _container = new ScriptingContainer();
+    _container.getProvider().getRubyInstanceConfig().setCompileMode(CompileMode.JIT);
+    _sassConvertUnit = _container.parse(buildSassConvertScript());
+    _updateStyleSheetsUnit = _container.parse(buildUpdateStyleSheetsScript());
   }
   
   private static void extractRubySassGem() {
     ClassLoader classLoader = JRubySassCompiler.class.getClassLoader();
     URL sassGemUrl = classLoader.getResource(SASS_GEM_RESOURCE);
-      
+    
     if (sassGemUrl == null) {
       throw new OpenGammaRuntimeException("Sass gem jar is missing in the classpath");
     }
     try {
-      ZipUtils.unzipArchive(new File(sassGemUrl.toURI()), GEMS_PATH);
+      URLConnection urlConnection = sassGemUrl.openConnection();
+      if (urlConnection instanceof JarURLConnection) {
+        JarFile jarFile = ((JarURLConnection) urlConnection).getJarFile();
+        ZipUtils.unzipArchive(jarFile, GEMS_PATH);
+      } else {
+        ZipUtils.unzipArchive(new File(sassGemUrl.toURI()), GEMS_PATH);
+      }
     } catch (Exception ex) {
-      throw new OpenGammaRuntimeException("Error expanding sass gems jar to given GEM_PATH", ex);
+      throw new OpenGammaRuntimeException("Error extracting Sass gem jar to " + GEMS_PATH.getPath(), ex);
     }
   }
   
@@ -73,18 +93,19 @@ public final class JRubySassCompiler {
     return s_instance;
   }
 
-  private String buildUpdateStyleSheetsScript(final String templateDir, final String cssDir) {
+  private String buildUpdateStyleSheetsScript() {
     StringWriter raw = new StringWriter();
     PrintWriter script = new PrintWriter(raw);
-    
-    script.println("  ENV['GEM_PATH']='" + GEMS_PATH.getPath() + "'                                           ");
-    script.println("  require 'rubygems'                                                            ");
-    script.println("  require 'sass/plugin'                                                         ");
-    script.println("  require 'sass'                                                                ");
-    script.println("  Sass::Plugin.options.merge!(" + _options + ")                                 ");
-    script.println("  Sass::Plugin.add_template_location('" + templateDir + "', '" + cssDir + "')   ");
-    script.println("  Sass::Plugin.update_stylesheets                                               ");
+       
+    script.println("  ENV['GEM_PATH']='" + GEMS_PATH.getPath() + "'                ");
+    script.println("  require 'rubygems'                                           ");
+    script.println("  require 'sass/plugin'                                        ");
+    script.println("  require 'sass'                                               ");
+    script.println("  Sass::Plugin.options.merge!(" + _options + ")                ");
+    script.println("  Sass::Plugin.add_template_location(@templateDir, @cssDir)    ");
+    script.println("  Sass::Plugin.update_stylesheets                              ");
     script.flush();
+    
     return raw.toString();
   }
     
@@ -95,23 +116,20 @@ public final class JRubySassCompiler {
    * @return the complied sass content.
    */
   public String sassConvert(final String sass) {
-    String css = StringUtils.EMPTY;
-    if (!StringUtils.isEmpty(sass)) {
-      Object result = executeRubyScript(buildSassConvertScript(sass));
-      if (result != null) {
-        css = result.toString();
+    String result = null;
+    String input = StringUtils.trimToNull(sass);
+    if (input != null) {
+      s_logger.debug("<sass mode=raw>\n{}\n</sass>", sass);
+      final OperationTimer timer = new OperationTimer(s_logger, "Sass text compilation");
+      _container.put("@source", input);
+      IRubyObject rubyResult = _sassConvertUnit.run();
+      if (rubyResult != null) {
+        result = rubyResult.asJavaString();
       }
+      long duration = timer.finished();
+      s_logger.debug("<sass mode=compiled duration={}ms>\n{}\n</sass>", duration, sass);
     }
-    return css;
-  }
-
-  private Object executeRubyScript(final String script) {
-    try {
-      ScriptEngine rubyEngine = new ScriptEngineManager().getEngineByName("jruby");
-      return rubyEngine.eval(script);
-    } catch (final ScriptException e) {
-      throw new OpenGammaRuntimeException(e.getMessage(), e);
-    }
+    return result;
   }
   
   /**
@@ -126,19 +144,21 @@ public final class JRubySassCompiler {
   public void updateStyleSheets(final File templateDir, final File cssDir) {
     ArgumentChecker.notNull(templateDir, "template directory");
     ArgumentChecker.notNull(cssDir, "css directory");
-    executeRubyScript(buildUpdateStyleSheetsScript(templateDir.toString(), cssDir.toString()));
+    
+    _container.put("@templateDir", templateDir.toString());
+    _container.put("@cssDir", cssDir.toString());
+    _updateStyleSheetsUnit.run();
   }
 
-  private String buildSassConvertScript(final String sass) {
-    ArgumentChecker.notNull(sass, "sass content");
+  private String buildSassConvertScript() {
     final StringWriter raw = new StringWriter();
     final PrintWriter script = new PrintWriter(raw);
 
-    script.println("  ENV['GEM_PATH']='" + GEMS_PATH.getPath() + "'                      ");
+    script.println("  ENV['GEM_PATH']='" + GEMS_PATH.getPath() + "'            ");
     script.println("  require 'rubygems'                                       ");
     script.println("  require 'sass/plugin'                                    ");
     script.println("  require 'sass/engine'                                    ");
-    script.println("  source = '" + sass.replace("'", "\"") + "'               ");
+    script.println("  source = @source                                         ");
     script.println("  engine = Sass::Engine.new(source, {" + _options + "})    ");
     script.println("  result = engine.render                                   ");
     script.flush();
