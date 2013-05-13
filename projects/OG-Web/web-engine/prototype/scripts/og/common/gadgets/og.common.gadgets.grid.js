b4bcc0261a5a50b19ed40b3bae68fd283dcd3d49
Alan Ayoub <alan@opengamma.com>
2013-05-13 16:05:30 +0100

    higher resolution of cellhover event firing in grid

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.grid.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.grid.js
@@ -234,7 +234,7 @@ $.register_module({
             grid.clipboard = new og.common.grid.Clipboard(grid);
         };
         var init_elements = function () {
-            var grid = this, config = grid.config, elements, in_timeout, out_timeout, stall = 15,
+            var grid = this, config = grid.config, elements, in_timeout, out_timeout, stall = 5,
                 last_x, last_y, page_x, page_y, last_corner, cell; // cached values for hover events
             var hoverin_handler = function (event, silent) {
                 (page_x = event.pageX), (page_y = event.pageY);
