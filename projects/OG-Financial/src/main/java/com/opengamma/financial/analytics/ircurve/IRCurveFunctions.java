b484fe740e886572a86fd54855da602b006558f9
stuartw <stuart.wakefield@gmail.com>
2013-05-14 15:06:40 +0100

    Add functions for new curves found in database

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/ircurve/IRCurveFunctions.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/ircurve/IRCurveFunctions.java
@@ -10,9 +10,11 @@ import java.util.List;
 import com.opengamma.engine.function.config.AbstractFunctionConfigurationBean;
 import com.opengamma.engine.function.config.FunctionConfiguration;
 import com.opengamma.engine.function.config.FunctionConfigurationSource;
+import com.opengamma.financial.analytics.curve.CurveDefinition;
 import com.opengamma.financial.analytics.curve.CurveDefinitionFunction;
 import com.opengamma.financial.analytics.curve.CurveMarketDataFunction;
 import com.opengamma.financial.analytics.curve.CurveSpecificationFunction;
+import com.opengamma.financial.analytics.curve.InterpolatedCurveDefinition;
 import com.opengamma.master.config.ConfigDocument;
 import com.opengamma.master.config.ConfigMaster;
 import com.opengamma.master.config.ConfigSearchRequest;
@@ -59,9 +61,15 @@ public class IRCurveFunctions extends AbstractFunctionConfigurationBean {
       functions.add(functionConfiguration(YieldCurveSpecificationFunction.class, currency, curveName));
     }
 
+    protected void addCurveFunctions(final List<FunctionConfiguration> functions, final String curveName) {
+      functions.add(functionConfiguration(CurveDefinitionFunction.class, curveName));
+      functions.add(functionConfiguration(CurveSpecificationFunction.class, curveName));
+      functions.add(functionConfiguration(CurveMarketDataFunction.class, curveName));
+    }
+
     @Override
     protected void addAllConfigurations(final List<FunctionConfiguration> functions) {
-      final ConfigSearchRequest<YieldCurveDefinition> searchRequest = new ConfigSearchRequest<YieldCurveDefinition>();
+      final ConfigSearchRequest<YieldCurveDefinition> searchRequest = new ConfigSearchRequest<>();
       searchRequest.setType(YieldCurveDefinition.class);
       for (final ConfigDocument configDocument : ConfigSearchIterator.iterable(getConfigMaster(), searchRequest)) {
         final String documentName = configDocument.getName();
@@ -73,6 +81,16 @@ public class IRCurveFunctions extends AbstractFunctionConfigurationBean {
         final String currencyISO = documentName.substring(underscore + 1);
         addYieldCurveFunctions(functions, currencyISO, curveName);
       }
+
+      // new curves
+      Class[] curveClasses = new Class[] {CurveDefinition.class, InterpolatedCurveDefinition.class};
+      for (final Class klass : curveClasses) {
+        searchRequest.setType(klass);
+        for (final ConfigDocument configDocument : ConfigSearchIterator.iterable(getConfigMaster(), searchRequest)) {
+          final String documentName = configDocument.getName();
+          addCurveFunctions(functions, documentName);
+        }
+      }
     }
 
   }
@@ -83,10 +101,6 @@ public class IRCurveFunctions extends AbstractFunctionConfigurationBean {
     functions.add(functionConfiguration(DefaultYieldCurveShiftFunction.class));
     functions.add(functionConfiguration(YieldCurveMarketDataShiftFunction.class));
     functions.add(functionConfiguration(YieldCurveShiftFunction.class));
-
-    functions.add(functionConfiguration(CurveDefinitionFunction.class, "Test_USD"));
-    functions.add(functionConfiguration(CurveSpecificationFunction.class, "Test_USD"));
-    functions.add(functionConfiguration(CurveMarketDataFunction.class, "Test_USD"));
   }
 
 }
