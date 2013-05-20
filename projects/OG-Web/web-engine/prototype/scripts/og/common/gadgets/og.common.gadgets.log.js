dc7b3f7cf194cc10c1dd716c58761a04f74a8477
Alan Ayoub <alan@opengamma.com>
2013-05-20 12:25:49 +0100

    logger conflict

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.log.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.log.js
@@ -10,10 +10,9 @@ $.register_module({
         return function (config) {
             var gadget = this, alive = og.common.id('gadget_log'), $selector = $(config.selector),
                 instantiated, tash, $msg, sbar_size = og.common.util.scrollbar_size + 'px',
-                cell_options = {
-                    source: config.source, single: {row: config.row, col: config.col}, format: 'EXPANDED', log: true
-                }, css_position = {position: 'absolute', top: '0', left: 0, right: 0, bottom: 0},
-                nolog = '<div class="OG-logger og-nolog">No log information available</div>';
+                cell_options = {source: config.source, col: config.col, row: config.row, format: 'EXPANDED', log: true},
+                css_position = {position: 'absolute', top: '0', left: 0, right: 0, bottom: 0},
+                nolog = '<div class="OG-logger"><div class="og-nolog">No log information available</div></div>';
             gadget.alive = function () {return $(config.selector).length ? true : (gadget.die(), false);};
             gadget.load = function () {
                 $selector.addClass(alive).css(css_position).html(loading_template({text: 'loading...'}));
