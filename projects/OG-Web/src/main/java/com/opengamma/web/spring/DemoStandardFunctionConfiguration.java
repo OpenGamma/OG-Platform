8a37c5358be234f33476d8a010e350301d5d6442
Elaine McLeod <elaine@opengamma.com>
2013-05-20 14:53:30 +0100

    Adding defaults for total yield curve P&L series for FX forwards

--- projects/OG-Web/src/main/java/com/opengamma/web/spring/DemoStandardFunctionConfiguration.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/spring/DemoStandardFunctionConfiguration.java
@@ -80,18 +80,17 @@ public class DemoStandardFunctionConfiguration extends StandardFunctionConfigura
   @Override
   protected CurrencyInfo audCurrencyInfo() {
     final CurrencyInfo i = super.audCurrencyInfo();
-    i.setCurveConfiguration(null, "DefaultTwoCurveAUDConfig");
+    i.setCurveConfiguration(null, "AUDFX");
     i.setCurveConfiguration("model/credit/yield", "ISDAAUDCurveConfig");
     i.setCurveConfiguration("model/forex", "AUDFX");
     i.setCurveConfiguration("model/pnl", "AUDFX");
-    i.setCurveName(null, "Discounting");
+    i.setCurveName(null, "FX");
     i.setCurveName("model/credit/yield", "ISDA");
     i.setCurveName("model/forex/discounting", "FX");
     i.setCurveName("model/pnl/discounting", "FX");
     i.setCurveCalculationMethodName("model/credit/yield", "ISDA");
     i.setCurveCalculationMethodName("model/credit/hazardrate", "ISDA");
     i.setCurveCalculationMethodName("model/forex/discounting", "FXImplied");
-    i.setCurveCalculationMethodName("model/pnl/discounting", "FXImplied");
     i.setCubeName(null, "BLOOMBERG");
     return i;
   }
