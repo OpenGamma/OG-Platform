1a8929bd1beff150a688098750b66c0a6a69d18a
Yomi Ayodele <yomi@opengamma.com>
2013-05-21 12:01:29 +0100

    [GUI-221] Make pause/resume endpoint more RESTful

--- projects/OG-Web/src/main/java/com/opengamma/web/analytics/rest/ViewResource.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/analytics/rest/ViewResource.java
@@ -6,11 +6,18 @@
 package com.opengamma.web.analytics.rest;
 
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.opengamma.engine.view.client.ViewClient;
+import com.opengamma.engine.view.client.ViewClientState;
 import com.opengamma.util.ArgumentChecker;
 import com.opengamma.web.analytics.AnalyticsView;
 import com.opengamma.web.analytics.AnalyticsViewManager;
@@ -20,6 +27,8 @@ import com.opengamma.web.analytics.AnalyticsViewManager;
  */
 @Path("views/{viewId}")
 public class ViewResource {
+  
+  private static final Logger s_logger = LoggerFactory.getLogger(ViewResource.class);
 
   private final AnalyticsView _view;
   private final AnalyticsViewManager _viewManager;
@@ -53,17 +62,30 @@ public class ViewResource {
   }
 
   @PUT
-  @Path("pause")
-  public Response pauseView() {
-    _viewClient.pause();
-    return Response.ok().build();
+  @Path("pauseOrResume")
+  public Response pauseOrResumeView(@FormParam("state") String state) {
+   
+    state = StringUtils.stripToNull(state);
+    Response response = Response.status(Status.BAD_REQUEST).build();
+    if (state != null) {
+      ViewClientState currentState = _viewClient.getState();
+      state = state.toUpperCase();
+      if ("PAUSE".equals(state) || "P".equals(state)) {
+        if (currentState != ViewClientState.TERMINATED) {
+          _viewClient.pause();
+          response = Response.ok().build();
+        } 
+      } else if ("RESUME".equals(state) || "R".equals(state)) {
+        if (currentState != ViewClientState.TERMINATED) {
+          _viewClient.resume();
+          response = Response.ok().build();
+        } 
+      } else {
+        s_logger.warn("client {} requesting for invalid view client state change to {}", _viewId, state);
+        response = Response.status(Status.BAD_REQUEST).build();
+      }
+    }
+    return response;
   }
-  
-  @PUT
-  @Path("resume")
-  public Response resumeView() {
-    _viewClient.resume();
-    return Response.ok().build();
-  }
-    
+       
 }
