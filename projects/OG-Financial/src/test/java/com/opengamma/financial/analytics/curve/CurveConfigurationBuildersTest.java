6c3859f1ba5c9abe8245ae655a8310ff642b3f53
Elaine McLeod <elaine@opengamma.com>
2013-06-11 19:35:41 +0100

    [PLAT-3552] Testing fudge builders for curve configuration types

--- projects/OG-Financial/src/test/java/com/opengamma/financial/analytics/curve/CurveConfigurationBuildersTest.java
+++ projects/OG-Financial/src/test/java/com/opengamma/financial/analytics/curve/CurveConfigurationBuildersTest.java
@@ -5,10 +5,45 @@
  */
 package com.opengamma.financial.analytics.curve;
 
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.testng.annotations.Test;
+
+import com.opengamma.financial.analytics.fudgemsg.AnalyticsTestBase;
+import com.opengamma.id.UniqueId;
+
 /**
  * 
  */
-public class CurveConfigurationBuildersTest {
+public class CurveConfigurationBuildersTest extends AnalyticsTestBase {
 
+  @Test
+  public void testDiscountingCurveTypeConfiguration() {
+    final String name = "USD Discounting";
+    final String code = "USD";
+    final DiscountingCurveTypeConfiguration config = new DiscountingCurveTypeConfiguration(name, code);
+    config.setUniqueId(UniqueId.of(UniqueId.EXTERNAL_SCHEME.getName(), "123"));
+    assertEquals(config, cycleObject(DiscountingCurveTypeConfiguration.class, config));
+  }
+
+  @Test
+  public void testIndexCurveTypeConfiguration() {
+    final String name = "USD Forward3M";
+    final String conventionName = "USD 3m Libor";
+    final String indexType = "Ibor";
+    final IndexCurveTypeConfiguration config = new IndexCurveTypeConfiguration(name, conventionName, indexType);
+    config.setUniqueId(UniqueId.of(UniqueId.EXTERNAL_SCHEME.getName(), "234"));
+    assertEquals(config, cycleObject(IndexCurveTypeConfiguration.class, config));
+  }
+
+  @Test
+  public void testIssuerCurveTypeConfiguration() {
+    final String name = "OG Bond Curve";
+    final String issuerName = "OG";
+    final String underlyingCode = "USD";
+    final IssuerCurveTypeConfiguration config = new IssuerCurveTypeConfiguration(name, issuerName, underlyingCode);
+    config.setUniqueId(UniqueId.of(UniqueId.EXTERNAL_SCHEME.getName(), "345"));
+    assertEquals(config, cycleObject(IssuerCurveTypeConfiguration.class, config));
+  }
 
 }
