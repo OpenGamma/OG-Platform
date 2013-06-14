d392df89d5576d61b297eae52243ba89f9aa128c
Ian Turner <ian@opengamma.com>
2013-06-14 15:16:09 +0100

    [PLAT-3575] Scenario selection fixed markup, removed unnecessary block elements

--- projects/OG-Web/web-engine/prototype/scripts/og/views/config_blocks/og.views.config_blocks.scenario.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/config_blocks/og.views.config_blocks.scenario.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * Copyright 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
  * Please see distribution for license.
  */
 $.register_module({
@@ -8,22 +8,15 @@ $.register_module({
     obj: function () {
         var module = this, Block = og.common.util.ui.Block;
         var Scenario = function (config) {
-            var block = this, id = og.common.id('scenario'), data_index = config.index, form = config.form,
-                value = config.value, data = config.data, $rules, $rule, extras = {
-                    id: id, class_value: (data && data[0])
-                };
-            var processor = function (data) {
-                if (!$('#' + id).length) return;
-                var indices = data_index.split('.'), last = indices.pop(), result = {};
-            };
-            form.Block.call(block, {extras: extras, processor: processor}); // assign a Block instance to this (block)
-            block.on('form:load', function () {
-                
+            var block = this, id = og.common.id('scenario'),
+                form = config.form,
+                extras = {id : id};
+            form.Block.call(block, {
+                extras: extras,
+                module: 'og.views.forms.view-definition-scenario_tash'
             });
         };
-        Scenario.prototype = new Block(null, { // inherit Block prototype
-            module: 'og.views.forms.view-definition-scenario_tash'
-        });
+        Scenario.prototype = new Block(); // inherit Block prototype
         return Scenario;
     }
 });
