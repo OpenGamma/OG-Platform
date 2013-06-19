a44c4c00ee5dd3fab3bbb4a813bf9dd584a126cd
Yomi Ayodele <yomi@opengamma.com>
2013-06-19 17:54:21 +0100

    [PLAT-3494] Moved responsibility of creating ValueRequirement for security type to View calculation worker

--- projects/OG-Integration/src/main/java/com/opengamma/integration/viewer/status/ViewStatusReporterTool.java
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/viewer/status/ViewStatusReporterTool.java
@@ -8,24 +8,15 @@ package com.opengamma.integration.viewer.status;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
 
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.SetMultimap;
-import com.google.common.collect.Sets;
-import com.google.common.collect.TreeMultimap;
 import com.opengamma.OpenGammaRuntimeException;
 import com.opengamma.component.tool.AbstractTool;
-import com.opengamma.engine.view.helper.AvailableOutput;
-import com.opengamma.engine.view.helper.AvailableOutputs;
-import com.opengamma.engine.view.helper.AvailableOutputsProvider;
 import com.opengamma.financial.tool.ToolContext;
 import com.opengamma.id.UniqueId;
 import com.opengamma.integration.viewer.status.ViewStatusReporterOption.ResultFormat;
@@ -73,18 +64,7 @@ public class ViewStatusReporterTool extends AbstractTool<ToolContext> {
   
   private void generateViewStatusReport(final UniqueId portfolioId, UserPrincipal user, ResultFormat resultFormat, AggregateType aggregateType) {
     
-    Map<String, Collection<String>> valueRequirementBySecType = scanValueRequirementBySecType(portfolioId);
-    
-    if (s_logger.isDebugEnabled()) {
-      StringBuilder strBuf = new StringBuilder();
-      for (String securityType : Sets.newTreeSet(valueRequirementBySecType.keySet())) {
-        Set<String> valueNames = Sets.newTreeSet(valueRequirementBySecType.get(securityType));
-        strBuf.append(String.format("%s\t%s\n", StringUtils.rightPad(securityType, 40), valueNames.toString()));
-      }
-      s_logger.debug("\n{}\n", strBuf.toString());
-    }
-    
-    ViewStatusCalculationWorker calculationWorker = new ViewStatusCalculationWorker(valueRequirementBySecType, getToolContext(), portfolioId, user);
+    ViewStatusCalculationWorker calculationWorker = new ViewStatusCalculationWorker(getToolContext(), portfolioId, user);
     ViewStatusResultAggregator resultAggregator = calculationWorker.run();
     
     ViewStatusResultProducer resultProducer = new ViewStatusResultProducer();
@@ -101,25 +81,6 @@ public class ViewStatusReporterTool extends AbstractTool<ToolContext> {
 
   private File getFileName(ResultFormat resultFormat) {
     return new File(FileUtils.getUserDirectory(), "view-status" + "." + resultFormat.getExtension());
-  }
-
-  private Map<String, Collection<String>> scanValueRequirementBySecType(UniqueId portfolioId) {
-    ToolContext toolContext = getToolContext();
-    AvailableOutputsProvider availableOutputsProvider = toolContext.getAvaliableOutputsProvider();
-    if (availableOutputsProvider == null) {
-      throw new OpenGammaRuntimeException("AvailableOutputsProvider missing from ToolContext");
-    }
-    final SetMultimap<String, String> valueNamesBySecurityType = TreeMultimap.create();
-    
-    AvailableOutputs portfolioOutputs = availableOutputsProvider.getPortfolioOutputs(portfolioId, null);
-    Set<String> securityTypes = portfolioOutputs.getSecurityTypes();
-    for (String securityType : securityTypes) {
-      Set<AvailableOutput> positionOutputs = portfolioOutputs.getPositionOutputs(securityType);
-      for (AvailableOutput availableOutput : positionOutputs) {
-        valueNamesBySecurityType.put(securityType, availableOutput.getValueName());
-      }        
-    }
-    return valueNamesBySecurityType.asMap();
   }
 
   private UniqueId findPortfolioId(final String portfolioName) {
