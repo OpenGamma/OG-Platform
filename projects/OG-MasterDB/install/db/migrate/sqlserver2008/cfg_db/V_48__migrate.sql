d8cd66c1555f252dc084bb41903a52b060a7bba5
Stuart Wakefield <stuart.wakefield@opengamma.com>
2013-06-26 17:25:04 -0400

    [PLAT-3703] Fix bug in sql syntax

--- projects/OG-MasterDB/install/db/migrate/sqlserver2008/cfg_db/V_48__migrate.sql
+++ projects/OG-MasterDB/install/db/migrate/sqlserver2008/cfg_db/V_48__migrate.sql
@@ -4,38 +4,38 @@ BEGIN TRAN;
   
   IF EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_oid')
     DROP INDEX ix_cfg_config_oid ON cfg_config;
-  go
+
   IF EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_ver_from_instant')
     DROP INDEX ix_cfg_config_ver_from_instant ON cfg_config;
-  go
+
   IF EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_ver_from_instant')
     DROP INDEX ix_cfg_config_corr_from_instant ON cfg_config;
-  go
+
   IF EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_corr_from_instant')
     DROP INDEX ix_cfg_config_corr_from_instant ON cfg_config;
-  go
+
   IF EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_corr_to_instant')
     DROP INDEX ix_cfg_config_corr_to_instant ON cfg_config;
-  go
+
   IF EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_name')
     DROP INDEX ix_cfg_config_name ON cfg_config;
-  go
+
 
   IF NOT EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_oid_instants')
     CREATE INDEX ix_cfg_config_oid_instants ON cfg_config(oid, ver_from_instant, corr_from_instant);
-  go
+
   IF NOT EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_ver_instants')
     CREATE INDEX ix_cfg_config_ver_instants ON cfg_config(ver_from_instant, ver_to_instant);
-  go
+
   IF NOT EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_corr_instants')
     CREATE INDEX ix_cfg_config_corr_instants ON cfg_config(corr_from_instant, corr_to_instant);
-  go
+
   IF NOT EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_name_type')
     CREATE INDEX ix_cfg_config_name_type ON cfg_config(name, config_type);
-  go
+
   IF NOT EXISTS (SELECT name FROM sysindexes WHERE name = 'ix_cfg_config_type_dates')
     CREATE INDEX ix_cfg_config_type_dates ON cfg_config(config_type, ver_from_instant, corr_from_instant, ver_to_instant, corr_to_instant);
-  go
+
 
 
 COMMIT;
