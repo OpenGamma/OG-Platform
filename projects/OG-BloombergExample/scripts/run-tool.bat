a834451c5971bcedc8833f4b8962079e04384415
Stephen Colebourne <stephen@opengamma.com>
2013-07-02 12:02:12 +0100

    [PLAT-3406] Maven - Remove old scripts
    Now in scripts jar

--- projects/OG-BloombergExample/scripts/run-tool.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if not exist "%~dp0\..\bin\runtool.exe" goto noexe
-"%~dp0\..\bin\runtool.exe" -elevate -p%PROJECT%.jar %*
-goto done
-
-:noexe
-call "%~dp0\run-tool-deprecated.bat" %*
-
-:done
