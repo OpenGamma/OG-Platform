1e2ce1d2b1b24ece72f328876d8c8d0823b468a3
Elaine McLeod <elaine@opengamma.com>
2013-07-05 19:33:37 +0100

    [PLAT-3844] Adding value requirements and properties required for curve constructions using the Hull-White one factor model

---
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/curve/CurveCalculationPropertyNamesAndValues.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.model.curve;
+
+/**
+ *
+ */
+public class CurveCalculationPropertyNamesAndValues {
+
+  /** The property indicating the name of a set of Hull-White parameters */
+  public static final String PROPERTY_HULL_WHITE_PARAMETERS = "HullWhiteOneFactorParameters";
+
+  /** The mean reversion property */
+  public static final String PROPERTY_HW_MEAN_REVERSION = "HullWhiteOneFactorMeanReversion";
+
+  /** The volatilities property */
+  public static final String PROPERTY_HW_VOLATILITIES = "HullWhiteOneFactorVolatilities";
+
+  /** The volatility times property */
+  public static final String PROPERTY_HW_TIMES = "HullWhiteOneFactorTimes";
+}
