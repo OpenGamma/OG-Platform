ce08ac2efc0308269a0fd3a8a8c7007d41521a11
Elaine McLeod <elaine@opengamma.com>
2013-07-10 15:46:36 +0100

    [PLAT-3881] If the instrument for which PV01 is being calculated is a swap where there are only known payments to be made (i.e. all of the floating payments are fixed), return 0 for sensitivities to the forward curve.

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/fixedincome/InterestRateInstrumentPV01Function.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/fixedincome/InterestRateInstrumentPV01Function.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
- * 
+ *
  * Please see distribution for license.
  */
 package com.opengamma.financial.analytics.model.fixedincome;
@@ -13,6 +13,11 @@ import com.opengamma.OpenGammaRuntimeException;
 import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
 import com.opengamma.analytics.financial.interestrate.PV01Calculator;
 import com.opengamma.analytics.financial.interestrate.YieldCurveBundle;
+import com.opengamma.analytics.financial.interestrate.annuity.derivative.Annuity;
+import com.opengamma.analytics.financial.interestrate.payments.derivative.CouponFixed;
+import com.opengamma.analytics.financial.interestrate.payments.derivative.Payment;
+import com.opengamma.analytics.financial.interestrate.payments.derivative.PaymentFixed;
+import com.opengamma.analytics.financial.interestrate.swap.derivative.Swap;
 import com.opengamma.engine.ComputationTarget;
 import com.opengamma.engine.function.FunctionInputs;
 import com.opengamma.engine.value.ComputedValue;
@@ -35,9 +40,33 @@ public class InterestRateInstrumentPV01Function extends InterestRateInstrumentCu
       final ValueSpecification resultSpec) {
     final Map<String, Double> pv01 = CALCULATOR.visit(derivative, curves);
     if (!pv01.containsKey(curveName)) {
+      if (derivative instanceof Swap) {
+        @SuppressWarnings("unchecked")
+        final Swap<Payment, Payment> swap = (Swap<Payment, Payment>) derivative;
+        final Annuity<Payment> firstLeg = swap.getFirstLeg();
+        final Annuity<Payment> secondLeg = swap.getSecondLeg();
+        if ((firstLeg.getPayments().length <= 1 && secondLeg.getPayments().length == 1) || (firstLeg.getPayments().length == 1 && secondLeg.getPayments().length <= 1)) {
+          boolean firstPaymentFixed = true;
+          if (firstLeg.getNumberOfPayments() > 0) {
+            final Payment lastPayment = firstLeg.getNthPayment(0);
+            firstPaymentFixed = lastPayment instanceof CouponFixed || lastPayment instanceof PaymentFixed;
+          }
+          boolean secondPaymentFixed = true;
+          if (secondLeg.getPayments().length > 0) {
+            final Payment lastPayment = secondLeg.getNthPayment(0);
+            secondPaymentFixed = lastPayment instanceof CouponFixed || lastPayment instanceof PaymentFixed;
+          }
+          if (firstPaymentFixed && secondPaymentFixed) {
+            // This will happen in the case where the final floating payment has fixed and the PV01 to the forward curve is requested.
+            // In this case, it is reasonable to return 0.
+            return Collections.singleton(new ComputedValue(resultSpec, 0.));
+          }
+        }
+      }
       throw new OpenGammaRuntimeException("Could not get PV01 for curve named " + curveName + "; should never happen");
     }
     return Collections.singleton(new ComputedValue(resultSpec, pv01.get(curveName)));
   }
+
 
 }
