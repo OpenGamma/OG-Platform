f1495f0c7a70fd985ca9abc0b30ba600e5a5aa73
Ian Turner <ian@opengamma.com>
2013-07-11 16:18:21 +0100

    [GUI-326] reverted back to scenario script only

--- projects/OG-Web/web-engine/prototype/scripts/og/views/config_blocks/og.views.config_blocks.scenario.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/config_blocks/og.views.config_blocks.scenario.js
@@ -6,44 +6,19 @@ $.register_module({
     name: 'og.views.config_blocks.Scenario',
     dependencies: ['og.common.util.ui.Form'],
     obj: function () {
-        var Block = og.common.util.ui.Block, data, constructor = this;
+        var Block = og.common.util.ui.Block;
         var Scenario = function (config) {
-            var block = this, id = og.common.id('scenario'), form = config.form,
-                index = config.index, extras = {id : id},
-                template = '<select class="og-scenario-select" id="'
-                + index + '"><option value="">Please select...</option></select>';
-            data = config.data;
+            var block = this, id = og.common.id('scenario'), form = config.form, data = config.data,
+                index = config.index, extras = {id : id};
             form.Block.call(block, {
                 extras: extras,
                 module: 'og.views.forms.view-definition-scenario_tash',
                 children: [
-                    new form.Block({content: template})
+                    new og.common.util.ui.Dropdown({ form: form, resource: 'configs', index: index,
+                        placeholder: 'Please select....',  value: data, fields: [0, 1],
+                        rest_options : {type: 'ScenarioDslScript' }
+                        })
                 ]
-            });
-            form.on('form:load', function () {
-                constructor.content();
-            });
-        };
-        constructor.content = function () {
-            og.api.rest.configs.get({type: 'ScenarioDefinition'}).pipe(function (result) {
-                $('.og-scenario-select').append('<optgroup label="Scenario Definition"></optgroup>');
-                result.data.data.map(function (datum) { // search results
-                    var fields = datum.split('|');
-                    $('.og-scenario-select optgroup[label="Scenario Definition"]').append(
-                        $('<option></option>').val(fields[0]).html(fields[1])
-                            .attr('selected', fields[0] === data)
-                    );
-                });
-            });
-            og.api.rest.configs.get({type: 'ScenarioDslScript'}).pipe(function (result) {
-                $('.og-scenario-select').append('<optgroup label="Scenario DSL Script"></optgroup>');
-                result.data.data.map(function (datum) { // search results
-                    var fields = datum.split('|');
-                    $('.og-scenario-select optgroup[label="Scenario DSL Script"]').append(
-                        $('<option></option>').val(fields[0]).html(fields[1])
-                            .attr('selected', fields[0] === data)
-                    );
-                });
             });
         };
         Scenario.prototype = new Block(); // inherit Block prototype
