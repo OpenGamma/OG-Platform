fd6b22dda86a9ad1cde2ecc0746e82890d16c526
Ian Turner <ian@opengamma.com>
2013-07-11 15:23:52 +0100

    [GUI-326] combined dropdown for scenario script and definitions

--- projects/OG-Web/web-engine/prototype/scripts/og/views/config_blocks/og.views.config_blocks.scenario.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/views/config_blocks/og.views.config_blocks.scenario.js
@@ -9,20 +9,37 @@ $.register_module({
         var Block = og.common.util.ui.Block;
         var Scenario = function (config) {
             var block = this, id = og.common.id('scenario'), form = config.form, data = config.data,
-                index = config.index, extras = {id : id};
+                index = config.index, extras = {id : id},
+                template = '<select class="og-scenario-select" id="'
+                + index + '"><option value="">Please select...</option></select>';
             form.Block.call(block, {
                 extras: extras,
                 module: 'og.views.forms.view-definition-scenario_tash',
                 children: [
-                    new og.common.util.ui.Dropdown({ form: form, resource: 'configs', index: index,
-                            placeholder: 'Please select....',  value: data, fields: [0, 1],
-                            rest_options : {type: 'ScenarioDefinition' }
-                        }),
-                    new og.common.util.ui.Dropdown({ form: form, resource: 'configs', index: index,
-                            placeholder: 'Please select....',  value: data, fields: [0, 1],
-                            rest_options : {type: 'ScenarioDslScript' }
-                        })
+                    new form.Block({content: template})
                 ]
+            });
+            form.on('form:load', function () {
+                og.api.rest.configs.get({type: 'ScenarioDefinition'}).pipe(function (result) {
+                    $('.og-scenario-select').append('<optgroup label="Scenario Definition"></optgroup>');
+                    result.data.data.map(function (datum) { // search results
+                        var fields = datum.split('|');
+                        $('.og-scenario-select optgroup[label="Scenario Definition"]').append(
+                            $('<option></option>').val(fields[0]).html(fields[1])
+                                .attr('selected', fields[0] === data)
+                        );
+                    });
+                });
+                og.api.rest.configs.get({type: 'ScenarioDslScript'}).pipe(function (result) {
+                    $('.og-scenario-select').append('<optgroup label="Scenario DSL Script"></optgroup>');
+                    result.data.data.map(function (datum) { // search results
+                        var fields = datum.split('|');
+                        $('.og-scenario-select optgroup[label="Scenario DSL Script"]').append(
+                            $('<option></option>').val(fields[0]).html(fields[1])
+                                .attr('selected', fields[0] === data)
+                        );
+                    });
+                });
             });
         };
         Scenario.prototype = new Block(); // inherit Block prototype
