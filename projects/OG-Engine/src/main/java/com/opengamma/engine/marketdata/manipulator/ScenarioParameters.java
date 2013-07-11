4ebfc55154311d614ae9bd07dfa7447b6329ac53
Ian Turner <ian@opengamma.com>
2013-07-11 18:18:47 +0100

    [PLAT-3892] added scenario parameters to view config and fudge builder

---
+++ projects/OG-Engine/src/main/java/com/opengamma/engine/marketdata/manipulator/ScenarioParameters.java
@@ -0,0 +1,16 @@
+package com.opengamma.engine.marketdata.manipulator;
+
+import java.util.Map;
+
+/**
+ * Config object for storing parameters required to build a scenario. This object stores a Groovy script which is
+ * executed to populate the parameter values. The script should be of the form:
+ * <pre>
+ * aString = "FOO"
+ * aList = [1, 2, 3]
+ * aMap = [key1: "val1", key2: "val2"]
+ * </pre>
+ */
+public interface ScenarioParameters {
+  Map<String, Object> getParameters();
+}
