fca9cf0d17af594f6c4560d98e3d597ede140c8d
Stephen Colebourne <stephen@opengamma.com>
2013-07-13 16:02:24 +0100

    PLAT-3917 - Replace Scannotation by Reflections - try to fix class loader

--- projects/OG-Util/src/main/java/com/opengamma/util/fudgemsg/OpenGammaFudgeContext.java
+++ projects/OG-Util/src/main/java/com/opengamma/util/fudgemsg/OpenGammaFudgeContext.java
@@ -5,9 +5,18 @@
  */
 package com.opengamma.util.fudgemsg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.fudgemsg.AnnotationReflector;
 import org.fudgemsg.FudgeContext;
 import org.fudgemsg.FudgeTypeDictionary;
+import org.reflections.Configuration;
+import org.reflections.scanners.FieldAnnotationsScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 
 import com.opengamma.timeseries.date.localdate.ImmutableLocalDateDoubleTimeSeries;
 import com.opengamma.timeseries.precise.zdt.ImmutableZonedDateTimeDoubleTimeSeries;
@@ -35,6 +44,61 @@ public final class OpenGammaFudgeContext {
     return ContextHolder.INSTANCE;
   }
 
+  private static final String DEFAULT_ANNOTATION_REFLECTOR_FILTER =
+      "-java., " +
+      "-javax., " +
+      "-sun., " +
+      "-sunw., " +
+      "-com.sun., " +
+      "-org.springframework., " +
+      "-org.eclipse., " +
+      "-org.apache., " +
+      "-org.antlr., " +
+      "-org.hibernate., " +
+      "-org.fudgemsg., " +
+      "-org.threeten., " +
+      "-org.reflections., " +
+      "-org.joda., " +
+      "-cern.clhep., " +
+      "-cern.colt., " +
+      "-cern.jet.math., " +
+      "-ch.qos.logback., " +
+      "-com.codahale.metrics., " +
+      "-com.mongodb., " +
+      "-com.sleepycat., " +
+      "-com.yahoo.platform.yui., " +
+      "-de.odysseus.el., " +
+      "-freemarker., " +
+      "-groovy., " +
+      "-groovyjar, " +
+      "-it.unimi.dsi.fastutil., " +
+      "-jargs.gnu., " +
+      "-javassist., " +
+      "-jsr166y., " +
+      "-net.sf.ehcache., " +
+      "-org.bson., " +
+      "-org.codehaus.groovy., " +
+      "-org.cometd., " +
+      "-com.google.common., " +
+      "-org.hsqldb., " +
+      "-com.jolbox., " +
+      "-edu.emory.mathcs., " +
+      "-info.ganglia., " +
+      "-org.aopalliance., " +
+      "-org.dom4j., " +
+      "-org.mozilla.javascript., " +
+      "-org.mozilla.classfile., " +
+      "-org.objectweb.asm., " +
+      "-org.osgi., " +
+      "-org.postgresql., " +
+      "-org.quartz., " +
+      "-org.slf4j., " +
+      "-org.w3c.dom, " +
+      "-org.xml.sax., " +
+      "-org.jcsp., " +
+      "-org.json., " +
+      "-redis.";
+
   /**
    * Avoid double-checked-locking using the Initialization-on-demand holder idiom.
    */
@@ -44,7 +108,23 @@ public final class OpenGammaFudgeContext {
       FudgeContext fudgeContext = new FudgeContext();
       ExtendedFudgeBuilderFactory.init(fudgeContext.getObjectDictionary());
       InnerClassFudgeBuilderFactory.init(fudgeContext.getObjectDictionary());
-      AnnotationReflector reflector = AnnotationReflector.getDefaultReflector();
+      
+      // hack to try to get a better classpath
+      List<ClassLoader> loaders = new ArrayList<>();
+      loaders.add(OpenGammaFudgeContext.class.getClassLoader());
+      try {
+        loaders.add(Thread.currentThread().getContextClassLoader());
+      } catch (Exception ex) {
+        // ignore
+      }
+      Configuration config = new ConfigurationBuilder()
+        .setUrls(ClasspathHelper.forManifest(ClasspathHelper.forJavaClassPath()))
+        .setScanners(new TypeAnnotationsScanner(), new FieldAnnotationsScanner())
+        .filterInputsBy(FilterBuilder.parse(DEFAULT_ANNOTATION_REFLECTOR_FILTER))
+        .addClassLoaders(loaders)
+        .useParallelExecutor();
+      AnnotationReflector reflector = new AnnotationReflector(config);
+      
       fudgeContext.getObjectDictionary().addAllAnnotatedBuilders(reflector);
       fudgeContext.getTypeDictionary().addAllAnnotatedSecondaryTypes(reflector);
       
