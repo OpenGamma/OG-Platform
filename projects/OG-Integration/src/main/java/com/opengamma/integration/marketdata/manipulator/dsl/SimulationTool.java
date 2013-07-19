69fd10a17185429ec4c26dd5782234bb0a09e845
Chris Kent <chris@opengamma.com>
2013-07-19 17:20:41 +0100

    PLAT-4004 - unfinished SimulationTool changes

--- projects/OG-Integration/src/main/java/com/opengamma/integration/marketdata/manipulator/dsl/SimulationTool.java
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/marketdata/manipulator/dsl/SimulationTool.java
@@ -5,18 +5,20 @@
  */
 package com.opengamma.integration.marketdata.manipulator.dsl;
 
+import java.io.File;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.io.FileUtils;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.opengamma.OpenGammaRuntimeException;
 import com.opengamma.component.tool.AbstractTool;
 import com.opengamma.core.config.ConfigSource;
 import com.opengamma.core.config.impl.ConfigItem;
-import com.opengamma.engine.marketdata.spec.LiveMarketDataSpecification;
 import com.opengamma.engine.marketdata.spec.MarketDataSpecification;
 import com.opengamma.engine.view.ViewDefinition;
 import com.opengamma.engine.view.ViewProcessor;
@@ -27,20 +29,23 @@ import com.opengamma.id.VersionCorrection;
 import com.opengamma.scripts.Scriptable;
 
 /**
- * Tool for running simulations. It's probably easier to use the approach in {@code RunSimulation} instead.
- * @deprecated This can probably be deleted
+ * Tool for running simulations defined in Groovy DSL scripts.
  */
 @Scriptable
 public class SimulationTool extends AbstractTool<ToolContext> {
 
   /** Command line option for view definition name. */
   private static final String VIEW_DEF_NAME_OPTION = "v";
-  /** Command line option for the class name of an implementation of SimulationSupplier. */
-  private static final String SIMULATION_SUPPLIER_CLASS_OPTION = "s";
   /** Command line option for whether to execute in batch mode. */
   private static final String BATCH_MODE_OPTION = "b";
   /** Command line option for the class name of an implementation of ViewResultListener. */
   private static final String RESULT_LISTENER_CLASS_OPTION = "r";
+  /** Command line option for the location of the Groovy script that defines the simulation. */
+  private static final String SIMULATION_SCRIPT_OPTION = "s";
+  /** Command line option for the location of the Groovy script that defines the simulation parameters. */
+  private static final String PARAMETER_SCRIPT_OPTION = "p";
+  /** Command line option for the names of the market data sources used for running the view. */
+  private static final String MARKET_DATA_OPTION = "m";
 
   public static void main(final String[] args) {
     new SimulationTool().initAndRun(args, ToolContext.class);
@@ -51,8 +56,7 @@ public class SimulationTool extends AbstractTool<ToolContext> {
   protected void doRun() throws Exception {
     ViewProcessor viewProcessor = getToolContext().getViewProcessor();
     ConfigSource configSource = getToolContext().getConfigSource();
-    String portfolioName = getCommandLine().getOptionValue('p');
-    String supplierClassName = getCommandLine().getOptionValue('s');
+    String viewDefName = getCommandLine().getOptionValue('v');
     boolean batchMode = getCommandLine().hasOption('b');
     ViewResultListener listener;
     if (getCommandLine().hasOption('v')) {
@@ -61,16 +65,33 @@ public class SimulationTool extends AbstractTool<ToolContext> {
     } else {
       listener = null;
     }
-    // TODO option for market data
-    List<MarketDataSpecification> marketDataSpecs =
-        ImmutableList.<MarketDataSpecification>of(new LiveMarketDataSpecification("Simulated live market data"));
-    SimulationSupplier supplier = instantiate(supplierClassName, SimulationSupplier.class);
-    Simulation simulation = supplier.get();
+    String[] marketDataSpecNames = getCommandLine().getOptionValues('m');
+    List<MarketDataSpecification> marketDataSpecs = Lists.newArrayListWithCapacity(marketDataSpecNames.length);
+    for (String marketDataSpecName : marketDataSpecNames) {
+      // TODO need some format like
+      //   live:name
+      //   fixedhistorical:date, time series rating
+      //   latesthistorical:time series rating
+      //   snapshot:name
+      //marketDataSpecs.add();
+    }
+    /*List<MarketDataSpecification> marketDataSpecs =
+        ImmutableList.<MarketDataSpecification>of(new LiveMarketDataSpecification("Simulated live market data"));*/
+    Map<String, Object> paramValues;
+    if (getCommandLine().hasOption('p')) {
+      String paramScript = getCommandLine().getOptionValue('p');
+      ScenarioDslParameters params = new ScenarioDslParameters(FileUtils.readFileToString(new File(paramScript)));
+      paramValues = params.getParameters();
+    } else {
+      paramValues = null;
+    }
+    String simulationScript = getCommandLine().getOptionValue('s');
+    Simulation simulation = SimulationUtils.createSimulationFromDsl(simulationScript, paramValues);
     VersionCorrection viewDefVersionCorrection = VersionCorrection.LATEST;
     Collection<ConfigItem<ViewDefinition>> viewDefs =
-        configSource.get(ViewDefinition.class, portfolioName, viewDefVersionCorrection);
+        configSource.get(ViewDefinition.class, viewDefName, viewDefVersionCorrection);
     if (viewDefs.isEmpty()) {
-      throw new IllegalStateException("View definition " + portfolioName + " not found");
+      throw new IllegalStateException("View definition " + viewDefName + " not found");
     }
     ConfigItem<ViewDefinition> viewDef = viewDefs.iterator().next();
     UniqueId viewDefId = viewDef.getUniqueId();
@@ -104,10 +125,19 @@ public class SimulationTool extends AbstractTool<ToolContext> {
     viewDefNameOption.setArgName("viewdefname");
     options.addOption(viewDefNameOption);
 
-    Option simulationSupplierOption = new Option(SIMULATION_SUPPLIER_CLASS_OPTION, true, "Simulation supplier class");
-    simulationSupplierOption.setRequired(true);
-    simulationSupplierOption.setArgName("supplierclass");
-    options.addOption(simulationSupplierOption);
+    Option marketDataOption = new Option(MARKET_DATA_OPTION, true, "Market data source names");
+    marketDataOption.setRequired(true);
+    marketDataOption.setArgName("marketdata");
+    options.addOption(marketDataOption);
+
+    Option simulationScriptOption = new Option(SIMULATION_SCRIPT_OPTION, true, "Simulation script location");
+    simulationScriptOption.setRequired(true);
+    simulationScriptOption.setArgName("simulationscript");
+    options.addOption(simulationScriptOption);
+
+    Option paramScriptOption = new Option(PARAMETER_SCRIPT_OPTION, true, "Simulation parameters script location");
+    paramScriptOption.setArgName("simulationparameters");
+    options.addOption(paramScriptOption);
 
     Option batchModeOption = new Option(BATCH_MODE_OPTION, false, "Run in batch mode");
     batchModeOption.setArgName("batchmode");
