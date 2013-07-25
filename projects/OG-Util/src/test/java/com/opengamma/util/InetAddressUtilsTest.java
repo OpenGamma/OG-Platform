45aa559bf7933a4c2538a6f62dbb7cef075d23eb
Stephen Colebourne <stephen@opengamma.com>
2013-07-25 17:35:10 +0100

    PLAT-4078 - Enhance utils tests

--- projects/OG-Util/src/test/java/com/opengamma/util/InetAddressUtilsTest.java
+++ projects/OG-Util/src/test/java/com/opengamma/util/InetAddressUtilsTest.java
@@ -5,7 +5,11 @@
  */
 package com.opengamma.util;
 
+import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 
 import org.testng.annotations.Test;
 
@@ -17,6 +21,18 @@ import com.opengamma.util.test.TestGroup;
 @Test(groups = TestGroup.UNIT)
 public class InetAddressUtilsTest {
 
+  @SuppressWarnings("unchecked")
+  public void test_constructor() throws Exception {
+    Constructor<?>[] cons = InetAddressUtils.class.getDeclaredConstructors();
+    assertEquals(1, cons.length);
+    assertEquals(0, cons[0].getParameterTypes().length);
+    assertEquals(true, Modifier.isPrivate(cons[0].getModifiers()));
+    Constructor<InetAddressUtils> con = (Constructor<InetAddressUtils>) cons[0];
+    con.setAccessible(true);
+    con.newInstance();
+  }
+
+  //-------------------------------------------------------------------------
   public void test_strip() {
     assertNotNull(InetAddressUtils.getLocalHostName());
   }
