3a90fde286121bf1e6d165db7f5cf0c36351e61f
Yuki Iwashita <yuki@opengamma.com>
2013-07-30 11:11:38 +0100

    [PLAT-4059] Greeks for binomial trees

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/option/pricing/tree/TrigeorgisLatticeSpecification.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/option/pricing/tree/TrigeorgisLatticeSpecification.java
@@ -20,4 +20,9 @@ public class TrigeorgisLatticeSpecification extends LatticeSpecification {
 
     return new double[] {upFactor, downFactor, upProbability, 1. - upProbability };
   }
+
+  @Override
+  public double getTheta(final double spot, final double volatility, final double interestRate, final double dt, final double[] greeksTmp) {
+    return 0.5 * (greeksTmp[3] - greeksTmp[0]) / dt;
+  }
 }
