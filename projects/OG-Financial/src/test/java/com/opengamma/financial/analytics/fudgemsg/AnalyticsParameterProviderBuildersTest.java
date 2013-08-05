fc90ae62021064c02835e1deba4838935fd1e263
Elaine McLeod <elaine@opengamma.com>
2013-08-05 22:23:19 +0100

    [PLAT-4196] Adding a fudge builder for forward curve providers

--- projects/OG-Financial/src/test/java/com/opengamma/financial/analytics/fudgemsg/AnalyticsParameterProviderBuildersTest.java
+++ projects/OG-Financial/src/test/java/com/opengamma/financial/analytics/fudgemsg/AnalyticsParameterProviderBuildersTest.java
@@ -28,7 +28,9 @@ import com.opengamma.analytics.financial.provider.curve.CurveBuildingBlockBundle
 import com.opengamma.analytics.financial.provider.description.inflation.InflationProviderDiscount;
 import com.opengamma.analytics.financial.provider.description.interestrate.HullWhiteOneFactorProviderDiscount;
 import com.opengamma.analytics.financial.provider.description.interestrate.MulticurveProviderDiscount;
+import com.opengamma.analytics.financial.provider.description.interestrate.MulticurveProviderForward;
 import com.opengamma.analytics.math.curve.ConstantDoublesCurve;
+import com.opengamma.analytics.math.curve.DoublesCurve;
 import com.opengamma.analytics.math.matrix.DoubleMatrix2D;
 import com.opengamma.financial.convention.businessday.BusinessDayConventionFactory;
 import com.opengamma.financial.convention.daycount.DayCountFactory;
@@ -103,6 +105,35 @@ public class AnalyticsParameterProviderBuildersTest extends AnalyticsTestBase {
     overnight.put(new IndexON("NAME2", Currency.EUR, DayCountFactory.INSTANCE.getDayCount("Act/360"), 0), new YieldCurve("F", ConstantDoublesCurve.from(0.006, "f")));
     final MulticurveProviderDiscount provider = new MulticurveProviderDiscount(discounting, ibor, overnight, matrix);
     assertEquals(provider, cycleObject(MulticurveProviderDiscount.class, provider));
+  }
+
+  @Test
+  public void testMulticurveProviderForward() {
+    final Map<Currency, Integer> map = new LinkedHashMap<>();
+    final Currency[] currencies = new Currency[] {Currency.AUD, Currency.CAD, Currency.CHF, Currency.FRF, Currency.DEM, Currency.USD, Currency.GBP, Currency.EUR, Currency.HKD, Currency.DKK};
+    final double[][] fxRates = new double[10][10];
+    for (int i = 0; i < 10; i++) {
+      map.put(currencies[i], i);
+      for (int j = 0; j < 10; j++) {
+        fxRates[i][j] = Math.random();
+      }
+    }
+    final FXMatrix matrix = new FXMatrix(map, fxRates);
+    final Map<Currency, YieldAndDiscountCurve> discounting = new LinkedHashMap<>();
+    discounting.put(Currency.USD, new YieldCurve("A", ConstantDoublesCurve.from(0.06, "a")));
+    discounting.put(Currency.EUR, new DiscountCurve("B", ConstantDoublesCurve.from(0.99, "b")));
+    final Map<IborIndex, DoublesCurve> ibor = new LinkedHashMap<>();
+    ibor.put(new IborIndex(Currency.USD, Period.ofMonths(3), 0, DayCountFactory.INSTANCE.getDayCount("Act/360"),
+        BusinessDayConventionFactory.INSTANCE.getBusinessDayConvention("Following"), false, "F"),
+        ConstantDoublesCurve.from(0.03, "c"));
+    ibor.put(new IborIndex(Currency.EUR, Period.ofMonths(6), 1, DayCountFactory.INSTANCE.getDayCount("Act/360"),
+        BusinessDayConventionFactory.INSTANCE.getBusinessDayConvention("Following"), false, "D"),
+        ConstantDoublesCurve.from(0.03, "d"));
+    final Map<IndexON, YieldAndDiscountCurve> overnight = new LinkedHashMap<>();
+    overnight.put(new IndexON("NAME1", Currency.USD, DayCountFactory.INSTANCE.getDayCount("Act/360"), 1), new YieldCurve("E", ConstantDoublesCurve.from(0.003, "e")));
+    overnight.put(new IndexON("NAME2", Currency.EUR, DayCountFactory.INSTANCE.getDayCount("Act/360"), 0), new YieldCurve("F", ConstantDoublesCurve.from(0.006, "f")));
+    final MulticurveProviderForward provider = new MulticurveProviderForward(discounting, ibor, overnight, matrix);
+    assertEquals(provider, cycleObject(MulticurveProviderForward.class, provider));
   }
 
   @Test
