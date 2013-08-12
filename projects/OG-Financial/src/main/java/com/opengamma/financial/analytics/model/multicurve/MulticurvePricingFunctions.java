651e9cd943df3ae9a826d93e7e920ed4023c3499
Elaine McLeod <elaine@opengamma.com>
2013-08-12 18:56:39 +0100

    [PLAT-4272] Adding currency exposures for FX forwards and cross-currency swaps and adding all new functions to the repository

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/multicurve/MulticurvePricingFunctions.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/multicurve/MulticurvePricingFunctions.java
@@ -10,6 +10,8 @@ import java.util.List;
 import com.opengamma.engine.function.config.AbstractFunctionConfigurationBean;
 import com.opengamma.engine.function.config.FunctionConfiguration;
 import com.opengamma.engine.function.config.FunctionConfigurationSource;
+import com.opengamma.financial.analytics.model.multicurve.discounting.DiscountingCurrencyExposureFunction;
+import com.opengamma.financial.analytics.model.multicurve.discounting.DiscountingFXPVFunction;
 import com.opengamma.financial.analytics.model.multicurve.discounting.DiscountingPV01Function;
 import com.opengamma.financial.analytics.model.multicurve.discounting.DiscountingPVFunction;
 import com.opengamma.financial.analytics.model.multicurve.discounting.DiscountingParRateFunction;
@@ -30,6 +32,8 @@ public class MulticurvePricingFunctions extends AbstractFunctionConfigurationBea
 
   @Override
   protected void addAllConfigurations(final List<FunctionConfiguration> functions) {
+    functions.add(functionConfiguration(DiscountingCurrencyExposureFunction.class));
+    functions.add(functionConfiguration(DiscountingFXPVFunction.class));
     functions.add(functionConfiguration(DiscountingPVFunction.class));
     functions.add(functionConfiguration(DiscountingParRateFunction.class));
     functions.add(functionConfiguration(DiscountingPV01Function.class));
