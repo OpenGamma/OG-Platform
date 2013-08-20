baffd71d307bd0da22ba96de2850173458d17916
Yuki Iwashita <yuki@opengamma.com>
2013-08-20 12:25:05 +0100

    [PLAT-4375] Curve derivative for DoubleQuadratic

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/interpolation/DoubleQuadraticInterpolator1D.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/interpolation/DoubleQuadraticInterpolator1D.java
@@ -64,6 +64,39 @@ public class DoubleQuadraticInterpolator1D extends Interpolator1D {
   }
 
   @Override
+  public double firstDerivative(final Interpolator1DDataBundle data, final Double value) {
+    Validate.notNull(value, "value");
+    Validate.notNull(data, "data bundle");
+    Validate.isTrue(data instanceof Interpolator1DDoubleQuadraticDataBundle);
+    final Interpolator1DDoubleQuadraticDataBundle quadraticData = (Interpolator1DDoubleQuadraticDataBundle) data;
+    final int low = data.getLowerBoundIndex(value);
+    final int high = low + 1;
+    final int n = data.size() - 1;
+    final double[] xData = data.getKeys();
+    final double[] yData = data.getValues();
+    if (low == n) {
+      return 0.;
+    } else if (low == 0) {
+      final RealPolynomialFunction1D quadraticFirstDerivative = quadraticData.getQuadraticFirstDerivative(0);
+      final double x = value - xData[1];
+      return quadraticFirstDerivative.evaluate(x);
+    } else if (high == n) {
+      final RealPolynomialFunction1D quadraticFirstDerivative = quadraticData.getQuadraticFirstDerivative(n - 2);
+      final double x = value - xData[n - 1];
+      return quadraticFirstDerivative.evaluate(x);
+    }
+    final RealPolynomialFunction1D quadratic1 = quadraticData.getQuadratic(low - 1);
+    final RealPolynomialFunction1D quadratic2 = quadraticData.getQuadratic(high - 1);
+    final RealPolynomialFunction1D quadratic1FirstDerivative = quadraticData.getQuadraticFirstDerivative(low - 1);
+    final RealPolynomialFunction1D quadratic2FirstDerivative = quadraticData.getQuadraticFirstDerivative(high - 1);
+    final double w = _weightFunction.getWeight((xData[high] - value) / (xData[high] - xData[low]));
+    //final double w = (xData[high] - value) / (xData[high] - xData[low]);
+    final double res = w * quadratic1FirstDerivative.evaluate(value - xData[low]) + (1 - w) * quadratic2FirstDerivative.evaluate(value - xData[high]) +
+        (quadratic2.evaluate(value - xData[high]) - quadratic1.evaluate(value - xData[low])) / (xData[high] - xData[low]);
+    return res;
+  }
+
+  @Override
   public double[] getNodeSensitivitiesForValue(final Interpolator1DDataBundle data, final Double value) {
     Validate.notNull(data, "data");
     Validate.isTrue(data instanceof Interpolator1DDoubleQuadraticDataBundle);
