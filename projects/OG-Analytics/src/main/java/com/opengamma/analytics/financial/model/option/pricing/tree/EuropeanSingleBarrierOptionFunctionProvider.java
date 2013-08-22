1f119e02513d40cdc0197efa671176c134c7aca7
Yuki Iwashita <yuki@opengamma.com>
2013-08-22 15:32:23 +0100

    [PLAT-4297] Test barrier option pricing method

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/option/pricing/tree/EuropeanSingleBarrierOptionFunctionProvider.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/option/pricing/tree/EuropeanSingleBarrierOptionFunctionProvider.java
@@ -5,37 +5,10 @@
  */
 package com.opengamma.analytics.financial.model.option.pricing.tree;
 
-import org.apache.commons.lang.NotImplementedException;
-
 /**
  * 
  */
-public class EuropeanSingleBarrierOptionFunctionProvider extends OptionFunctionProvider1D {
-  /**
-   * Use these strings to specify barrier option type. 
-   * DownAndIn and UpAndIn MUST be computed via in-out parity if the option is European
-   */
-  public static enum BarrierTypes {
-    /**
-     * Down-and-out option
-     */
-    DownAndOut,
-    /**
-     * Up-and-out option
-     */
-    UpAndOut,
-    /**
-     * Down-and-in option, not implemented
-     */
-    DownAndIn,
-    /**
-     * Up-and-in option, not implemented
-     */
-    UpAndIn
-  }
-
-  private double _barrier;
-  private CrossBarrierChecker _checker;
+public class EuropeanSingleBarrierOptionFunctionProvider extends BarrierOptionFunctionProvider {
 
   /**
    * @param strike Strike price
@@ -45,22 +18,7 @@ public class EuropeanSingleBarrierOptionFunctionProvider extends OptionFunctionP
    * @param typeName {@link BarrierTypes}, DownAndOut or UpAndOut
    */
   public EuropeanSingleBarrierOptionFunctionProvider(final double strike, final int steps, final boolean isCall, final double barrier, final BarrierTypes typeName) {
-    super(strike, steps, isCall);
-    _barrier = barrier;
-
-    switch (typeName) {
-      case DownAndOut:
-        _checker = new CrossLowerBarrier();
-        break;
-      case UpAndOut:
-        _checker = new CrossUpperBarrier();
-        break;
-      case DownAndIn:
-        throw new NotImplementedException("Down-and-in should be computed by using down-and-out and in-out parity");
-      case UpAndIn:
-        throw new NotImplementedException("Up-and-in should be computed by using up-and-out and in-out parity");
-    }
-
+    super(strike, steps, isCall, barrier, typeName);
   }
 
   @Override
@@ -72,7 +30,7 @@ public class EuropeanSingleBarrierOptionFunctionProvider extends OptionFunctionP
     final double[] values = new double[nStepsP];
     double priceTmp = assetPrice;
     for (int i = 0; i < nStepsP; ++i) {
-      values[i] = _checker.checkOut(priceTmp) ? 0. : Math.max(sign * (priceTmp - strike), 0);
+      values[i] = getChecker().checkOut(priceTmp) ? 0. : Math.max(sign * (priceTmp - strike), 0);
       priceTmp *= upOverDown;
     }
     return values;
@@ -86,27 +44,9 @@ public class EuropeanSingleBarrierOptionFunctionProvider extends OptionFunctionP
     final double[] res = new double[nStepsP];
     double assetPrice = baseAssetPrice * Math.pow(downFactor, steps);
     for (int j = 0; j < nStepsP; ++j) {
-      res[j] = _checker.checkOut(assetPrice) ? 0. : discount * (upProbability * values[j + 1] + downProbability * values[j]);
+      res[j] = getChecker().checkOut(assetPrice + sumCashDiv) ? 0. : discount * (upProbability * values[j + 1] + downProbability * values[j]);
       assetPrice *= upOverDown;
     }
     return res;
-  }
-
-  private abstract class CrossBarrierChecker {
-    public abstract boolean checkOut(final double priceTmp);
-  }
-
-  private class CrossLowerBarrier extends CrossBarrierChecker {
-    @Override
-    public boolean checkOut(final double priceTmp) {
-      return priceTmp <= _barrier;
-    }
-  }
-
-  private class CrossUpperBarrier extends CrossBarrierChecker {
-    @Override
-    public boolean checkOut(final double priceTmp) {
-      return priceTmp >= _barrier;
-    }
   }
 }
