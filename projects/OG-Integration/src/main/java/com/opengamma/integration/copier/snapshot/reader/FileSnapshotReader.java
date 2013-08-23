f3fcacc9a7e871748f9ae42a57bef0d9e7a4bcaf
Ian Turner <ian@opengamma.com>
2013-08-23 18:06:11 +0100

    [PLAT-3804] File snapshot readers and writers

---
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/copier/snapshot/reader/FileSnapshotReader.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.integration.copier.snapshot.reader;
+
+import java.util.Map;
+
+import com.opengamma.core.marketdatasnapshot.CurveKey;
+import com.opengamma.core.marketdatasnapshot.CurveSnapshot;
+import com.opengamma.core.marketdatasnapshot.StructuredMarketDataSnapshot;
+import com.opengamma.core.marketdatasnapshot.VolatilitySurfaceKey;
+import com.opengamma.core.marketdatasnapshot.VolatilitySurfaceSnapshot;
+import com.opengamma.core.marketdatasnapshot.YieldCurveKey;
+import com.opengamma.core.marketdatasnapshot.YieldCurveSnapshot;
+
+public class FileSnapshotReader implements SnapshotReader{
+
+  public FileSnapshotReader(String filename) {
+  }
+
+  @Override
+  public Map<CurveKey, CurveSnapshot> readCurves() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public StructuredMarketDataSnapshot readGlobalValues() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public Map<VolatilitySurfaceKey, VolatilitySurfaceSnapshot> readVolatilitySurfaces() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public Map<YieldCurveKey, YieldCurveSnapshot> readYieldCurves() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public String[] getCurrentPath() {
+    return new String[0];  //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public void close() {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public String getName() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public String getBasisViewName() {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
+}
---
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/copier/snapshot/writer/FileSnapshotWriter.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.integration.copier.snapshot.writer;
+
+import com.opengamma.master.marketdatasnapshot.MarketDataSnapshotMaster;
+
+public class FileSnapshotWriter implements SnapshotWriter{
+
+  public FileSnapshotWriter(String filename, MarketDataSnapshotMaster marketDataSnapshotMaster) {
+  }
+
+  @Override
+  public void close() {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+}
