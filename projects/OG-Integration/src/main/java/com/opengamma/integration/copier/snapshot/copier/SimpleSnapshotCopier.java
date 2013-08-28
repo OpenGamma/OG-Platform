7581e790a1dccc4806952352bea2349fecf19a4b
Ian Turner <ian@opengamma.com>
2013-08-28 11:42:52 +0100

    [PLAT-3804] Simple snapshot copier with all copy methods

--- projects/OG-Integration/src/main/java/com/opengamma/integration/copier/snapshot/copier/SimpleSnapshotCopier.java
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/copier/snapshot/copier/SimpleSnapshotCopier.java
@@ -28,7 +28,6 @@ public class SimpleSnapshotCopier implements SnapshotCopier {
     _structure = null;
   }
 
-
   public SimpleSnapshotCopier(String[] structure) {
     _structure = structure;
   }
@@ -39,67 +38,13 @@ public class SimpleSnapshotCopier implements SnapshotCopier {
   }
 
   public void copy(SnapshotReader snapshotReader, SnapshotWriter snapshotWriter, SnapshotCopierVisitor visitor) {
-
     ArgumentChecker.notNull(snapshotWriter, "snapshotWriter");
     ArgumentChecker.notNull(snapshotReader, "snapshotReader");
-
     snapshotWriter.writeName(snapshotReader.getName());
-    
-    ObjectsPair<ManageablePosition, ManageableSecurity[]> next;
-
-    //while (true) {
-    //
-    //  // Read in next row, checking for errors and EOF
-    //  try {
-    //    next = snapshotReader.readNext();
-    //  } catch (Exception e) {
-    //    // skip to next row on uncaught exception while parsing row
-    //    s_logger.error("Unable to parse row", e);
-    //    continue;
-    //  }
-    //  if (next == null) {
-    //    // stop loading on EOF
-    //    break;
-    //  }
-    //
-    //  // Is position and security data is available for the current row?
-    //  ManageablePosition position = next.getFirst();
-    //  ManageableSecurity[] securities = next.getSecond();
-    //
-    //  // Is position and security data available for the current row?
-    //  if (position != null && securities != null) {
-    //
-    //    // Set current path
-    //    String[] path;
-    //    if (_structure == null) {
-    //      path = snapshotReader.getCurrentPath();
-    //    } else {
-    //      path = new String[_structure.length];
-    //      for (int i = 0; i < _structure.length; i++) {
-    //        path[i] = position.getAttributes().get(_structure[i]);
-    //      }
-    //    }
-    //    snapshotWriter.setPath(path);
-    //
-    //    // Write position and security data
-    //    ObjectsPair<ManageablePosition, ManageableSecurity[]> written =
-    //        snapshotWriter.writePosition(position, securities);
-    //
-    //    if (visitor != null && written != null) {
-    //      visitor.info(StringUtils.arrayToDelimitedString(path, "/"), written.getFirst(), written.getSecond());
-    //    }
-    //  } else {
-    //    if (visitor != null) {
-    //      if (position == null) {
-    //        visitor.error("Could not load position");
-    //      }
-    //      if (securities == null) {
-    //        visitor.error("Could not load security(ies)");
-    //      }
-    //    }
-    //  }
-    //
-    //}
-
+    snapshotWriter.writeBasisViewName(snapshotReader.getBasisViewName());
+    snapshotWriter.writeCurves(snapshotReader.readCurves());
+    snapshotWriter.writeYieldCurves(snapshotReader.readYieldCurves());
+    snapshotWriter.writeGlobalValues(snapshotReader.readGlobalValues());
+    snapshotWriter.writeVoliatilitySurface(snapshotReader.readVolatilitySurfaces());
   }
 }
