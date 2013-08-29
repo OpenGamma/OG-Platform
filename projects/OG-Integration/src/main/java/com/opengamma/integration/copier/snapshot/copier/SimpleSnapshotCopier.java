77e96dfa797eeb0e3eba16fdd4a484153de628c4
Ian Turner <ian@opengamma.com>
2013-08-29 12:18:28 +0100

    [PLAT-3804] removed unnecessary structure element from snapshot copier

--- projects/OG-Integration/src/main/java/com/opengamma/integration/copier/snapshot/copier/SimpleSnapshotCopier.java
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/copier/snapshot/copier/SimpleSnapshotCopier.java
@@ -5,32 +5,14 @@
  */
 package com.opengamma.integration.copier.snapshot.copier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.opengamma.integration.copier.snapshot.reader.SnapshotReader;
 import com.opengamma.integration.copier.snapshot.writer.SnapshotWriter;
-import com.opengamma.master.position.ManageablePosition;
-import com.opengamma.master.security.ManageableSecurity;
 import com.opengamma.util.ArgumentChecker;
-import com.opengamma.util.tuple.ObjectsPair;
 
 /**
  * A simple snapshot copier that copies positions from readers to the specified writer.
  */
 public class SimpleSnapshotCopier implements SnapshotCopier {
-
-  private static final Logger s_logger = LoggerFactory.getLogger(SimpleSnapshotCopier.class);
-
-  private String[] _structure;
-
-  public SimpleSnapshotCopier() {
-    _structure = null;
-  }
-
-  public SimpleSnapshotCopier(String[] structure) {
-    _structure = structure;
-  }
 
   @Override
   public void copy(SnapshotReader snapshotReader, SnapshotWriter snapshotWriter) {
