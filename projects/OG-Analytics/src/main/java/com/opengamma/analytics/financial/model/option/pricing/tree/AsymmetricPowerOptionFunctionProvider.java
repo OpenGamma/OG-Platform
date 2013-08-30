86b3f4f2d75a60f57faddc9cb50d43edfd8c9b97
Yuki Iwashita <yuki@opengamma.com>
2013-08-30 10:19:14 +0100

    [PLAT-4329] Asymmetric Power Option

---
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/option/pricing/tree/AsymmetricPowerOptionFunctionProvider.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.analytics.financial.model.option.pricing.tree;
+
+import com.google.common.primitives.Doubles;
+import com.opengamma.util.ArgumentChecker;
+
+/**
+ * Payoff of asymmetric power option is max( S^i - K , 0 ) for call and max( K - S^i , 0 ) for put with i > 0
+ */
+public class AsymmetricPowerOptionFunctionProvider extends OptionFunctionProvider1D {
+
+  private double _power;
+
+  /**
+   * @param strike Strike price, K
+   * @param steps Number of steps
+   * @param isCall True if call, false if put
+   * @param power Power, i
+   */
+  public AsymmetricPowerOptionFunctionProvider(final double strike, final int steps, final boolean isCall, final double power) {
+    super(strike, steps, isCall);
+    ArgumentChecker.isTrue(power > 0., "power should be positive");
+    ArgumentChecker.isTrue(Doubles.isFinite(power), "power should be finite");
+    _power = power;
+  }
+
+  @Override
+  public double[] getPayoffAtExpiry(final double assetPrice, final double upOverDown) {
+    final double strike = getStrike();
+    final int nStepsP = getNumberOfSteps() + 1;
+    final double sign = getSign();
+
+    final double[] values = new double[nStepsP];
+    double priceTmp = assetPrice;
+    for (int i = 0; i < nStepsP; ++i) {
+      values[i] = Math.max(sign * (Math.pow(priceTmp, _power) - strike), 0.);
+      priceTmp *= upOverDown;
+    }
+    return values;
+  }
+
+  @Override
+  public double[] getNextOptionValues(final double discount, final double upProbability, final double downProbability, final double[] values, final double baseAssetPrice, final double sumCashDiv,
+      final double downFactor, final double upOverDown, final int steps) {
+    final int nStepsP = steps + 1;
+
+    final double[] res = new double[nStepsP];
+    for (int j = 0; j < nStepsP; ++j) {
+      res[j] = discount * (upProbability * values[j + 1] + downProbability * values[j]);
+    }
+    return res;
+  }
+
+  /**
+   * Access power
+   * @return _power
+   */
+  public double getPower() {
+    return _power;
+  }
+}
