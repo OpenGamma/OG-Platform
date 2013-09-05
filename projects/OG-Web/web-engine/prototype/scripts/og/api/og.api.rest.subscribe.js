4c5766e9be3dca9135c5fd30f33ca42008e22aad
Chris Kent <chris@opengamma.com>
2013-09-05 14:56:18 +0100

    PLAT-4546 - Revert unwanted changes

--- projects/OG-Web/web-engine/prototype/scripts/og/api/og.api.rest.subscribe.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/api/og.api.rest.subscribe.js
@@ -6,33 +6,20 @@ $.register_module({
     name: 'og.api.rest.subscribe',
     dependencies: ['og.api.common', 'og.api.rest', 'og.common.routes'],
     obj: function () {
-        var common = og.api.common;
-        var api = og.api.rest;
-        var subscribe;
-        var RESUBSCRIBE = 10000;
-        var warn = og.dev.warn;
-        subscribe = api.handshake.get.partial({handler: function (result) {
+        var common = og.api.common, api = og.api.rest, str = common.str, check = common.check,
+            subscribe, RESUBSCRIBE = 10000 /* 10s */, warn = og.dev.warn;
+        return subscribe = api.handshake.get.partial({handler: function (result) {
             var listen, fire_updates;
-            if (result.error) {
-                warn(api.name + ': handshake failed\n', result.message);
-                return setTimeout(subscribe, RESUBSCRIBE);
-            }
+            if (result.error)
+                return warn(api.name + ': handshake failed\n', result.message), setTimeout(subscribe, RESUBSCRIBE);
             api.id = result.data['clientId'];
-            fire_updates = function (reset, result) {
+            (fire_updates = function (reset, result) {
                 var current = og.common.routes.current();
-                if (reset && api.disconnected) {
-                    api.disconnected = false;
-                    api.fire('reconnect');
-                }
-                // throw out stale registrations
-                api.registrations = api.registrations.filter(function (reg) { return !common.request_expired(reg, current); });
-                // fire all updates if connection is reset
-                if (reset) {
-                    api.registrations = api.registrations.filter(
-                        function (reg) {return reg.update($.extend({reset: true}, reg)) && false; }
-                    );
-                    return api.registrations;
-                }
+                if (reset && api.disconnected) (api.disconnected = false), api.fire('reconnect');
+                api.registrations = api.registrations // throw out stale registrations
+                    .filter(function (reg) {return !common.request_expired(reg, current);});
+                if (reset) return api.registrations = api.registrations // fire all updates if connection is reset
+                    .filter(function (reg) {return reg.update($.extend({reset: true}, reg)) && false;});
                 result.data.updates.filter(function (update) {
                     var simple = typeof update === 'string', promise, request;
                     if (!simple && (promise = (request = api.outstanding_requests[update.id]) && request.promise)) {
@@ -42,42 +29,26 @@ $.register_module({
                         delete api.outstanding_requests[promise.id];
                     }
                     return simple;
-                }).forEach(
-                    function (update) {
-                        var lcv, reg;
-                        var len = api.registrations.length;
-                        for (lcv = 0; lcv < len; lcv += 1) {
-                            reg = api.registrations[lcv];
-                            if (reg && reg.url === update) {
-                                api.registrations[lcv] = null;
-                                reg.update(reg);
-                            }
-                        }
-                        api.registrations = api.registrations.filter(Boolean);
-                    }
-                );
-            };
-            fire_updates(true, null); // there are no registrations when subscribe() is called unless the connection's been reset
-            listen = function () {
+                }).forEach(function (update) {
+                    var lcv, len = api.registrations.length, reg;
+                    for (lcv = 0; lcv < len; lcv += 1)
+                        if ((reg = api.registrations[lcv]) && reg.url === update)
+                            (api.registrations[lcv] = null), reg.update(reg);
+                    api.registrations = api.registrations.filter(Boolean);
+                });
+            })(true, null); // there are no registrations when subscribe() is called unless the connection's been reset
+            (listen = function () {
                 api.updates.get({handler: function (result) {
                     if (result.error) {
-                        if (!api.disconnected) {
-                            api.disconnected = true;
-                            api.fire('disconnect');
-                            api.id = null;
-                        }
+                        if (!api.disconnected) (api.disconnected = true), api.fire('disconnect'), (api.id = null);
                         warn(api.name + ': subscription failed\n', result.message);
                         return setTimeout(subscribe, RESUBSCRIBE);
                     }
-                    if (!result.data || !result.data.updates.length) {
-                        return setTimeout(listen, common.INSTANT);
-                    }
+                    if (!result.data || !result.data.updates.length) return setTimeout(listen, common.INSTANT);
                     fire_updates(false, result);
                     setTimeout(listen, common.INSTANT);
                 }});
-            };
-            listen();
+            })();
         }});
-        return subscribe;
     }
 });
