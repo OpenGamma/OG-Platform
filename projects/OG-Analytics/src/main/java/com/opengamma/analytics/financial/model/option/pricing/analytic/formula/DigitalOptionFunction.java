8d97685567c3897e52b9a56dc0455fa93e5ec656
Yuki Iwashita <yuki@opengamma.com>
2013-09-18 13:29:46 +0100

    [PLAT-4677] Digital Option Greeks

---
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/option/pricing/analytic/formula/DigitalOptionFunction.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.analytics.financial.model.option.pricing.analytic.formula;
+
+import com.opengamma.analytics.math.statistics.distribution.NormalDistribution;
+import com.opengamma.analytics.math.statistics.distribution.ProbabilityDistribution;
+import com.opengamma.util.ArgumentChecker;
+
+/**
+ * Compute price and Greeks of digital (cash-or-nothing) option
+ * Cash-or-nothing call option pays 0 if S <= K and K if S > K, whereas cash-or-nothing put option pays 1. if S < K and 0 if S >= K, 
+ * where S is asset price at expiry.
+ */
+public class DigitalOptionFunction {
+  private static final ProbabilityDistribution<Double> NORMAL = new NormalDistribution(0, 1);
+
+  /**
+   * Compute <b>spot</b> price of cash-or-nothing option
+   * @param spot The spot 
+   * @param strike The strike
+   * @param timeToExpiry The time to expiry
+   * @param lognormalVol The log-normal volatility
+   * @param interestRate The interest rate
+   * @param costOfCarry The cost-of-carry
+   * @param isCall True for calls, false for puts
+   * @return The option price
+   */
+  public double price(final double spot, final double strike, final double timeToExpiry, final double lognormalVol, final double interestRate, final double costOfCarry, final boolean isCall) {
+    ArgumentChecker.isTrue(spot > 0.0, "negative/NaN spot; have {}", spot);
+    ArgumentChecker.isTrue(strike > 0.0, "negative/NaN strike; have {}", strike);
+    ArgumentChecker.isTrue(timeToExpiry > 0.0, "negative/NaN timeToExpiry; have {}", timeToExpiry);
+    ArgumentChecker.isTrue(lognormalVol > 0.0, "negative/NaN lognormalVol; have {}", lognormalVol);
+    ArgumentChecker.isFalse(Double.isNaN(interestRate), "interestRate is NaN");
+    ArgumentChecker.isFalse(Double.isNaN(costOfCarry), "costOfCarry is NaN");
+
+    final double d = (Math.log(spot / strike) + (costOfCarry - 0.5 * lognormalVol * lognormalVol) * timeToExpiry) / lognormalVol / Math.sqrt(timeToExpiry);
+    final double sign = isCall ? 1. : -1.;
+    return Math.exp(-interestRate * timeToExpiry) * NORMAL.getCDF(sign * d);
+  }
+
+  /**
+   * Compute delta of cash-or-nothing option
+   * @param spot The spot 
+   * @param strike The strike
+   * @param timeToExpiry The time to expiry
+   * @param lognormalVol The log-normal volatility
+   * @param interestRate The interest rate
+   * @param costOfCarry The cost-of-carry
+   * @param isCall True for calls, false for puts
+   * @return The option price
+   */
+  public double delta(final double spot, final double strike, final double timeToExpiry, final double lognormalVol, final double interestRate, final double costOfCarry, final boolean isCall) {
+    ArgumentChecker.isTrue(spot > 0.0, "negative/NaN spot; have {}", spot);
+    ArgumentChecker.isTrue(strike > 0.0, "negative/NaN strike; have {}", strike);
+    ArgumentChecker.isTrue(timeToExpiry > 0.0, "negative/NaN timeToExpiry; have {}", timeToExpiry);
+    ArgumentChecker.isTrue(lognormalVol > 0.0, "negative/NaN lognormalVol; have {}", lognormalVol);
+    ArgumentChecker.isFalse(Double.isNaN(interestRate), "interestRate is NaN");
+    ArgumentChecker.isFalse(Double.isNaN(costOfCarry), "costOfCarry is NaN");
+
+    final double sigmaRootT = lognormalVol * Math.sqrt(timeToExpiry);
+    final double d = (Math.log(spot / strike) + (costOfCarry - 0.5 * lognormalVol * lognormalVol) * timeToExpiry) / sigmaRootT;
+    final double sign = isCall ? 1. : -1.;
+    return Math.exp(-interestRate * timeToExpiry) * (sign * NORMAL.getPDF(d) / spot / sigmaRootT);
+  }
+
+  /**
+   * Compute gamma of cash-or-nothing option
+   * @param spot The spot 
+   * @param strike The strike
+   * @param timeToExpiry The time to expiry
+   * @param lognormalVol The log-normal volatility
+   * @param interestRate The interest rate
+   * @param costOfCarry The cost-of-carry
+   * @param isCall True for calls, false for puts
+   * @return The option price
+   */
+  public double gamma(final double spot, final double strike, final double timeToExpiry, final double lognormalVol, final double interestRate, final double costOfCarry, final boolean isCall) {
+    ArgumentChecker.isTrue(spot > 0.0, "negative/NaN spot; have {}", spot);
+    ArgumentChecker.isTrue(strike > 0.0, "negative/NaN strike; have {}", strike);
+    ArgumentChecker.isTrue(timeToExpiry > 0.0, "negative/NaN timeToExpiry; have {}", timeToExpiry);
+    ArgumentChecker.isTrue(lognormalVol > 0.0, "negative/NaN lognormalVol; have {}", lognormalVol);
+    ArgumentChecker.isFalse(Double.isNaN(interestRate), "interestRate is NaN");
+    ArgumentChecker.isFalse(Double.isNaN(costOfCarry), "costOfCarry is NaN");
+
+    final double sigmaRootT = lognormalVol * Math.sqrt(timeToExpiry);
+    final double d = (Math.log(spot / strike) + (costOfCarry - 0.5 * lognormalVol * lognormalVol) * timeToExpiry) / sigmaRootT;
+    final double sign = isCall ? 1. : -1.;
+    return -sign * (Math.exp(-interestRate * timeToExpiry) * NORMAL.getPDF(d) * (1. + d / sigmaRootT) / spot / spot / sigmaRootT);
+  }
+
+  /**
+   * Compute theta price of cash-or-nothing option
+   * @param spot The spot 
+   * @param strike The strike
+   * @param timeToExpiry The time to expiry
+   * @param lognormalVol The log-normal volatility
+   * @param interestRate The interest rate
+   * @param costOfCarry The cost-of-carry
+   * @param isCall True for calls, false for puts
+   * @return The option price
+   */
+  public double theta(final double spot, final double strike, final double timeToExpiry, final double lognormalVol, final double interestRate, final double costOfCarry, final boolean isCall) {
+    ArgumentChecker.isTrue(spot > 0.0, "negative/NaN spot; have {}", spot);
+    ArgumentChecker.isTrue(strike > 0.0, "negative/NaN strike; have {}", strike);
+    ArgumentChecker.isTrue(timeToExpiry > 0.0, "negative/NaN timeToExpiry; have {}", timeToExpiry);
+    ArgumentChecker.isTrue(lognormalVol > 0.0, "negative/NaN lognormalVol; have {}", lognormalVol);
+    ArgumentChecker.isFalse(Double.isNaN(interestRate), "interestRate is NaN");
+    ArgumentChecker.isFalse(Double.isNaN(costOfCarry), "costOfCarry is NaN");
+
+    final double d = (Math.log(spot / strike) + (costOfCarry - 0.5 * lognormalVol * lognormalVol) * timeToExpiry) / lognormalVol / Math.sqrt(timeToExpiry);
+    final double sign = isCall ? 1. : -1.;
+    final double div = 0.5 * (-Math.log(spot / strike) / Math.pow(timeToExpiry, 1.5) + (costOfCarry - 0.5 * lognormalVol * lognormalVol) / Math.pow(timeToExpiry, 0.5)) / lognormalVol;
+    return interestRate * Math.exp(-interestRate * timeToExpiry) * NORMAL.getCDF(sign * d) - sign * Math.exp(-interestRate * timeToExpiry) * NORMAL.getPDF(d) * div;
+  }
+}
---
+++ projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/model/option/pricing/analytic/formula/DigitalOptionFunctionTest.java
@@ -0,0 +1,246 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.analytics.financial.model.option.pricing.analytic.formula;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.testng.annotations.Test;
+import org.threeten.bp.Period;
+import org.threeten.bp.ZonedDateTime;
+
+import com.opengamma.analytics.financial.forex.definition.ForexDefinition;
+import com.opengamma.analytics.financial.forex.definition.ForexOptionDigitalDefinition;
+import com.opengamma.analytics.financial.forex.derivative.ForexOptionDigital;
+import com.opengamma.analytics.financial.forex.method.TestsDataSetsForex;
+import com.opengamma.analytics.financial.interestrate.YieldCurveBundle;
+import com.opengamma.analytics.financial.model.volatility.surface.SmileDeltaTermStructureParametersStrikeInterpolation;
+import com.opengamma.analytics.financial.schedule.ScheduleCalculator;
+import com.opengamma.analytics.math.statistics.distribution.NormalDistribution;
+import com.opengamma.analytics.math.statistics.distribution.ProbabilityDistribution;
+import com.opengamma.analytics.util.time.TimeCalculator;
+import com.opengamma.financial.convention.businessday.BusinessDayConvention;
+import com.opengamma.financial.convention.businessday.BusinessDayConventionFactory;
+import com.opengamma.financial.convention.calendar.Calendar;
+import com.opengamma.financial.convention.calendar.MondayToFridayCalendar;
+import com.opengamma.util.money.Currency;
+import com.opengamma.util.time.DateUtils;
+import com.opengamma.util.tuple.Triple;
+
+/**
+ * 
+ */
+@SuppressWarnings("deprecation")
+public class DigitalOptionFunctionTest {
+
+  private static final DigitalOptionFunction _function = new DigitalOptionFunction();
+  private static final double SPOT = 105.;
+  private static final double[] STRIKES = new double[] {97., 105., 105.1, 114. };
+  private static final double TIME = 4.2;
+  private static final double[] INTERESTS = new double[] {-0.01, 0.017, 0.05, 0.1 };
+  private static final double[] VOLS = new double[] {0.05, 0.1, 0.5 };
+  private static final double[] DIVIDENDS = new double[] {0.005, 0.024, 0.05 };
+
+  private static final ZonedDateTime REFERENCE_DATE = DateUtils.getUTCDate(2011, 6, 13);
+  private static final Calendar CALENDAR = new MondayToFridayCalendar("A");
+  private static final BusinessDayConvention BUSINESS_DAY = BusinessDayConventionFactory.INSTANCE.getBusinessDayConvention("Modified Following");
+  private static final int SETTLEMENT_DAYS = 2;
+  private static final Currency EUR = Currency.EUR;
+  private static final Currency USD = Currency.USD;
+  private static final YieldCurveBundle CURVES = TestsDataSetsForex.createCurvesForex();
+  private static final String[] CURVES_NAME = TestsDataSetsForex.curveNames();
+  private static final SmileDeltaTermStructureParametersStrikeInterpolation SMILE_TERM = TestsDataSetsForex.smile5points(REFERENCE_DATE);
+  private static final ProbabilityDistribution<Double> NORMAL = new NormalDistribution(0, 1);
+
+  /**
+   * Tests the present value against an explicit computation. The amount is paid in the domestic currency.
+   */
+  @Test
+  public void presentValueDomestic() {
+    final double strike = 1.45;
+    final boolean isCall = true;
+    final boolean isLong = true;
+    final double notional = 100000000;
+    final ZonedDateTime payDate = ScheduleCalculator.getAdjustedDate(REFERENCE_DATE, Period.ofMonths(9), BUSINESS_DAY, CALENDAR);
+    final ZonedDateTime expDate = ScheduleCalculator.getAdjustedDate(payDate, -SETTLEMENT_DAYS, CALENDAR);
+    final double timeToExpiry = TimeCalculator.getTimeBetween(REFERENCE_DATE, expDate);
+    final ForexDefinition forexUnderlyingDefinition = new ForexDefinition(EUR, USD, payDate, notional, strike);
+    final ForexOptionDigitalDefinition forexOptionDefinition = new ForexOptionDigitalDefinition(forexUnderlyingDefinition, expDate, isCall, isLong);
+    final ForexOptionDigital forexOption = forexOptionDefinition.toDerivative(REFERENCE_DATE, CURVES_NAME);
+    final double dfDomestic = CURVES.getCurve(CURVES_NAME[1]).getDiscountFactor(forexOption.getUnderlyingForex().getPaymentTime());
+    final double dfForeign = CURVES.getCurve(CURVES_NAME[0]).getDiscountFactor(forexOption.getUnderlyingForex().getPaymentTime());
+    final double rDomestic = CURVES.getCurve(CURVES_NAME[1]).getInterestRate(forexOption.getUnderlyingForex().getPaymentTime());
+    final double rForeign = CURVES.getCurve(CURVES_NAME[0]).getInterestRate(forexOption.getUnderlyingForex().getPaymentTime());
+    final double forward = SPOT * dfForeign / dfDomestic;
+    final double volatility = SMILE_TERM.getVolatility(new Triple<>(timeToExpiry, forward, forward));
+    final double sigmaRootT = volatility * Math.sqrt(forexOption.getExpirationTime());
+    final double dM = Math.log(forward / strike) / sigmaRootT - 0.5 * sigmaRootT;
+    final int omega = isCall ? 1 : -1;
+
+    final double pvExpected = Math.abs(forexOption.getUnderlyingForex().getPaymentCurrency1().getAmount()) * dfDomestic * NORMAL.getCDF(omega * dM) * (isLong ? 1.0 : -1.0);
+    final double price = Math.abs(forexOption.getUnderlyingForex().getPaymentCurrency1().getAmount()) *
+        _function.price(SPOT, strike, forexOption.getUnderlyingForex().getPaymentTime(), volatility, rDomestic, rDomestic - rForeign, isCall);
+    assertEquals(price, pvExpected, pvExpected * 1.e-14);
+  }
+
+  /**
+   * 
+   */
+  @Test
+  public void greeksTest() {
+    final boolean[] tfSet = new boolean[] {true, false };
+    final double eps = 1.e-6;
+    for (final boolean isCall : tfSet) {
+      for (final double strike : STRIKES) {
+        for (final double interest : INTERESTS) {
+          for (final double vol : VOLS) {
+            for (final double dividend : DIVIDENDS) {
+              final double delta = _function.delta(SPOT, strike, TIME, vol, interest, interest - dividend, isCall);
+              final double gamma = _function.gamma(SPOT, strike, TIME, vol, interest, interest - dividend, isCall);
+              final double theta = _function.theta(SPOT, strike, TIME, vol, interest, interest - dividend, isCall);
+              final double upSpot = _function.price(SPOT + eps, strike, TIME, vol, interest, interest - dividend, isCall);
+              final double downSpot = _function.price(SPOT - eps, strike, TIME, vol, interest, interest - dividend, isCall);
+              final double upSpotDelta = _function.delta(SPOT + eps, strike, TIME, vol, interest, interest - dividend, isCall);
+              final double downSpotDelta = _function.delta(SPOT - eps, strike, TIME, vol, interest, interest - dividend, isCall);
+              final double upTime = _function.price(SPOT, strike, TIME + eps, vol, interest, interest - dividend, isCall);
+              final double downTime = _function.price(SPOT, strike, TIME - eps, vol, interest, interest - dividend, isCall);
+              assertEquals(delta, 0.5 * (upSpot - downSpot) / eps, eps);
+              assertEquals(gamma, 0.5 * (upSpotDelta - downSpotDelta) / eps, eps);
+              assertEquals(theta, -0.5 * (upTime - downTime) / eps, eps);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeSpotPriceTest() {
+    _function.price(-SPOT, STRIKES[0], TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeStrikePriceTest() {
+    _function.price(SPOT, -STRIKES[0], TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeTimePriceTest() {
+    _function.price(SPOT, STRIKES[0], -TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeVolPriceTest() {
+    _function.price(SPOT, STRIKES[0], TIME, -VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeSpotdeltaTest() {
+    _function.delta(-SPOT, STRIKES[0], TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeStrikedeltaTest() {
+    _function.delta(SPOT, -STRIKES[0], TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeTimedeltaTest() {
+    _function.delta(SPOT, STRIKES[0], -TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeVoldeltaTest() {
+    _function.delta(SPOT, STRIKES[0], TIME, -VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeSpotgammaTest() {
+    _function.gamma(-SPOT, STRIKES[0], TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeStrikegammaTest() {
+    _function.gamma(SPOT, -STRIKES[0], TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeTimegammaTest() {
+    _function.gamma(SPOT, STRIKES[0], -TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeVolgammaTest() {
+    _function.gamma(SPOT, STRIKES[0], TIME, -VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeSpotthetaTest() {
+    _function.theta(-SPOT, STRIKES[0], TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeStrikethetaTest() {
+    _function.theta(SPOT, -STRIKES[0], TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeTimethetaTest() {
+    _function.theta(SPOT, STRIKES[0], -TIME, VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+
+  /**
+   * 
+   */
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void negativeVolthetaTest() {
+    _function.theta(SPOT, STRIKES[0], TIME, -VOLS[1], INTERESTS[1], INTERESTS[1] - DIVIDENDS[1], true);
+  }
+}
