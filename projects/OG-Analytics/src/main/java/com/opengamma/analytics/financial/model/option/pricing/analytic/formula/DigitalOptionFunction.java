f440e9c4da6c341a75a702761485038810f9aabe
Yuki Iwashita <yuki@opengamma.com>
2013-09-18 21:23:20 +0100

    [PLAT-4677] Digital Option Greeks
    Greeks added, ForexOptionDigitalBlackMethod fixed

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/option/pricing/analytic/formula/DigitalOptionFunction.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/model/option/pricing/analytic/formula/DigitalOptionFunction.java
@@ -118,4 +118,53 @@ public class DigitalOptionFunction {
     final double div = 0.5 * (-Math.log(spot / strike) / Math.pow(timeToExpiry, 1.5) + (costOfCarry - 0.5 * lognormalVol * lognormalVol) / Math.pow(timeToExpiry, 0.5)) / lognormalVol;
     return interestRate * Math.exp(-interestRate * timeToExpiry) * NORMAL.getCDF(sign * d) - sign * Math.exp(-interestRate * timeToExpiry) * NORMAL.getPDF(d) * div;
   }
+
+  /**
+   * Compute driftless (forward) theta price of cash-or-nothing option
+   * @param forward The forward
+   * @param strike The strike
+   * @param timeToExpiry The time to expiry
+   * @param lognormalVol The log-normal volatility
+   * @param isCall True for calls, false for puts
+   * @return The option price
+   */
+  public static double driftlessTheta(final double forward, final double strike, final double timeToExpiry, final double lognormalVol, final boolean isCall) {
+    ArgumentChecker.isTrue(forward > 0.0, "negative/NaN forward; have {}", forward);
+    ArgumentChecker.isTrue(strike > 0.0, "negative/NaN strike; have {}", strike);
+    ArgumentChecker.isTrue(timeToExpiry > 0.0, "negative/NaN timeToExpiry; have {}", timeToExpiry);
+    ArgumentChecker.isTrue(lognormalVol > 0.0, "negative/NaN lognormalVol; have {}", lognormalVol);
+
+    final double sigmaRootT = lognormalVol * Math.sqrt(timeToExpiry);
+    final double d = Math.log(forward / strike) / sigmaRootT - 0.5 * sigmaRootT;
+    final double sign = isCall ? 1. : -1.;
+    final double div = 0.5 * (-Math.log(forward / strike) / Math.pow(timeToExpiry, 1.5) / lognormalVol - 0.5 * lognormalVol / Math.pow(timeToExpiry, 0.5));
+    return sign * NORMAL.getPDF(d) * div;
+  }
+
+  /**
+   * Compute vega price of cash-or-nothing option
+   * @param spot The spot
+   * @param strike The strike
+   * @param timeToExpiry The time to expiry
+   * @param lognormalVol The log-normal volatility
+   * @param interestRate The interest rate
+   * @param costOfCarry The cost-of-carry
+   * @param isCall True for calls, false for puts
+   * @return The option price
+   */
+  public static double vega(final double spot, final double strike, final double timeToExpiry, final double lognormalVol, final double interestRate,
+      final double costOfCarry, final boolean isCall) {
+    ArgumentChecker.isTrue(spot > 0.0, "negative/NaN spot; have {}", spot);
+    ArgumentChecker.isTrue(strike > 0.0, "negative/NaN strike; have {}", strike);
+    ArgumentChecker.isTrue(timeToExpiry > 0.0, "negative/NaN timeToExpiry; have {}", timeToExpiry);
+    ArgumentChecker.isTrue(lognormalVol > 0.0, "negative/NaN lognormalVol; have {}", lognormalVol);
+    ArgumentChecker.isFalse(Double.isNaN(interestRate), "interestRate is NaN");
+    ArgumentChecker.isFalse(Double.isNaN(costOfCarry), "costOfCarry is NaN");
+
+    final double rootT = Math.sqrt(timeToExpiry);
+    final double d = (Math.log(spot / strike) + (costOfCarry - 0.5 * lognormalVol * lognormalVol) * timeToExpiry) / lognormalVol / rootT;
+    final double sign = isCall ? 1. : -1.;
+    final double div = -(Math.log(spot / strike) + costOfCarry * timeToExpiry) / lognormalVol / lognormalVol / rootT - 0.5 * rootT;
+    return sign * Math.exp(-interestRate * timeToExpiry) * NORMAL.getPDF(d) * div;
+  }
 }
