dcf93d9a64e246621c60b730967478d5c919de63
Yuki Iwashita <yuki@opengamma.com>
2013-09-20 17:05:31 +0100

    [PLAT-4662] time-varying vol in trinomial tree

--- projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/model/option/pricing/tree/SupershareOptionFunctionProviderTest.java
+++ projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/model/option/pricing/tree/SupershareOptionFunctionProviderTest.java
@@ -340,10 +340,13 @@ public class SupershareOptionFunctionProviderTest {
     final LatticeSpecification lattice1 = new TimeVaryingLatticeSpecification();
     final double[] time_set = new double[] {0.5, 1.2 };
     final int steps = 977;
-
     final double[] vol = new double[steps];
     final double[] rate = new double[steps];
     final double[] dividend = new double[steps];
+    final int stepsTri = 4817;
+    final double[] volTri = new double[stepsTri];
+    final double[] rateTri = new double[stepsTri];
+    final double[] dividendTri = new double[stepsTri];
     final double constA = 0.01;
     final double constB = 0.001;
     final double constC = 0.1;
@@ -357,6 +360,11 @@ public class SupershareOptionFunctionProviderTest {
           rate[i] = constA + constB * i * time / steps;
           vol[i] = constC + constD * Math.sin(i * time / steps);
           dividend[i] = 0.005;
+        }
+        for (int i = 0; i < stepsTri; ++i) {
+          rateTri[i] = constA + constB * i * time / steps;
+          volTri[i] = constC + constD * Math.sin(i * time / steps);
+          dividendTri[i] = 0.005;
         }
         final double rateRef = constA + 0.5 * constB * time;
         final double volRef = Math.sqrt(constC * constC + 0.5 * constD * constD + 2. * constC * constD / time * (1. - Math.cos(time)) - constD * constD * 0.25 / time * Math.sin(2. * time));
@@ -372,6 +380,16 @@ public class SupershareOptionFunctionProviderTest {
         assertEquals(resGreeks.get(Greek.DELTA), resGreeksConst.get(Greek.DELTA), Math.max(Math.abs(resGreeksConst.get(Greek.DELTA)), 1.) * 1.e-2);
         assertEquals(resGreeks.get(Greek.GAMMA), resGreeksConst.get(Greek.GAMMA), Math.max(Math.abs(resGreeksConst.get(Greek.GAMMA)), 1.) * 1.e-2);
         assertEquals(resGreeks.get(Greek.THETA), resGreeksConst.get(Greek.THETA), Math.max(Math.abs(resGreeksConst.get(Greek.THETA)), 1.));
+
+        //        final OptionFunctionProvider1D functionTri = new SupershareOptionFunctionProvider(time, lowerBound, upperBound, stepsTri);
+        //        final double resPriceTrinomial = _modelTrinomial.getPrice(functionTri, SPOT, volTri, rateTri, dividendTri);
+        //        assertEquals(resPriceTrinomial, resPriceConst, Math.max(Math.abs(resPriceConst), 1.) * 1.e-2);
+        //        final GreekResultCollection resGreeksTrinomial = _modelTrinomial.getGreeks(functionTri, SPOT, volTri, rateTri, dividendTri);
+        //        System.out.println(resPriceConst + "\t" + price(SPOT, lowerBound, upperBound, TIME, volRef, rateRef, rateRef - dividend[0]) + "\t" + resPriceTrinomial);
+        //        assertEquals(resGreeksTrinomial.get(Greek.FAIR_PRICE), resGreeksConst.get(Greek.FAIR_PRICE), Math.max(Math.abs(resGreeksConst.get(Greek.FAIR_PRICE)), 1.) * 1.e-2);
+        //        assertEquals(resGreeksTrinomial.get(Greek.DELTA), resGreeksConst.get(Greek.DELTA), Math.max(Math.abs(resGreeksConst.get(Greek.DELTA)), 1.) * 1.e-2);
+        //        assertEquals(resGreeksTrinomial.get(Greek.GAMMA), resGreeksConst.get(Greek.GAMMA), Math.max(Math.abs(resGreeksConst.get(Greek.GAMMA)), 1.) * 1.e-2);
+        //        assertEquals(resGreeksTrinomial.get(Greek.THETA), resGreeksConst.get(Greek.THETA), Math.max(Math.abs(resGreeksConst.get(Greek.THETA)), 0.1));
       }
     }
   }
