fc55b42ef911a3ac4ac6e332c01111c2fba08122
Yuki Iwashita <yuki@opengamma.com>
2013-09-20 12:32:04 +0100

    [PLAT-4698] Fix value theta

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/forex/method/ForexOptionDigitalBlackMethod.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/forex/method/ForexOptionDigitalBlackMethod.java
@@ -659,7 +659,7 @@ public final class ForexOptionDigitalBlackMethod implements ForexPricingMethod {
     final boolean isCall = optionForex.isCall();
     final double volatility = FXVolatilityUtils.getVolatility(smile, foreignCcy, domesticCcy, expiry, forward, forward);
     final double sign = (optionForex.isLong() ? 1.0 : -1.0);
-    final double theta = DigitalOptionFunction.theta(spot, strike, expiry, volatility, rDomestic, rForeign - rDomestic, isCall) * sign
+    final double theta = DigitalOptionFunction.theta(spot, strike, expiry, volatility, rDomestic, rDomestic - rForeign, isCall) * sign
         * Math.abs(optionForex.getUnderlyingForex().getPaymentCurrency1().getAmount());
     return CurrencyAmount.of(optionForex.getUnderlyingForex().getCurrency2(), theta);
   }
