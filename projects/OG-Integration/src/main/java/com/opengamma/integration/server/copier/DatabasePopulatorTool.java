300cb7c05e0c7f3545ac24b4ea3b3a98969c1716
Yomi Ayodele <yomi@opengamma.com>
2013-09-24 11:22:37 +0100

    [LAV-31] Tools and supporting classes to create and populate standalone database from an opengamma serverURL

---
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/server/copier/DatabasePopulatorTool.java
@@ -0,0 +1,222 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.integration.server.copier;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Sets;
+import com.opengamma.component.tool.AbstractTool;
+import com.opengamma.core.config.impl.ConfigItem;
+import com.opengamma.core.position.Portfolio;
+import com.opengamma.core.position.Position;
+import com.opengamma.core.position.PositionSource;
+import com.opengamma.core.security.SecuritySource;
+import com.opengamma.engine.function.config.FunctionConfigurationDefinition;
+import com.opengamma.engine.function.config.FunctionConfigurationSource;
+import com.opengamma.engine.view.compilation.PortfolioCompiler;
+import com.opengamma.financial.aggregation.PortfolioAggregator;
+import com.opengamma.financial.portfolio.save.SavePortfolio;
+import com.opengamma.financial.tool.ToolContext;
+import com.opengamma.id.ObjectId;
+import com.opengamma.id.UniqueId;
+import com.opengamma.id.VersionCorrection;
+import com.opengamma.integration.tool.config.ConfigLoader;
+import com.opengamma.integration.tool.config.ConfigSaver;
+import com.opengamma.master.config.ConfigMaster;
+import com.opengamma.master.config.ConfigMasterUtils;
+import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesInfoDocument;
+import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesInfoSearchRequest;
+import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesMaster;
+import com.opengamma.master.historicaltimeseries.ManageableHistoricalTimeSeries;
+import com.opengamma.master.historicaltimeseries.impl.HistoricalTimeSeriesInfoSearchIterator;
+import com.opengamma.master.marketdatasnapshot.MarketDataSnapshotDocument;
+import com.opengamma.master.marketdatasnapshot.MarketDataSnapshotMaster;
+import com.opengamma.master.marketdatasnapshot.MarketDataSnapshotSearchRequest;
+import com.opengamma.master.marketdatasnapshot.impl.MarketDataSnapshotSearchIterator;
+import com.opengamma.master.portfolio.PortfolioDocument;
+import com.opengamma.master.portfolio.PortfolioMaster;
+import com.opengamma.master.portfolio.PortfolioSearchRequest;
+import com.opengamma.master.portfolio.impl.PortfolioSearchIterator;
+import com.opengamma.master.position.PositionMaster;
+import com.opengamma.master.security.SecurityDocument;
+import com.opengamma.master.security.SecurityMaster;
+import com.opengamma.master.security.SecuritySearchRequest;
+import com.opengamma.master.security.impl.SecuritySearchIterator;
+import com.opengamma.util.ArgumentChecker;
+
+/**
+ * 
+ */
+/* package */ class DatabasePopulatorTool extends AbstractTool<ToolContext> {
+  
+  private static final Logger s_logger = LoggerFactory.getLogger(DatabasePopulatorTool.class);
+  /**
+   * Demo function configuration object name.
+   */
+  public static final String DEMO_FUNCTION = "DEMO_FUNCTIONS";
+  
+  private final String _serverUrl;
+  private final ExecutorService _executorService = Executors.newCachedThreadPool();
+  
+  public DatabasePopulatorTool(final String serverUrl) {
+    ArgumentChecker.notNull(serverUrl, "serverUrl");
+    _serverUrl = serverUrl;
+  }
+
+  @Override
+  protected void doRun() throws Exception {
+    ToolContext toolContext = getToolContext();
+    loadSecurity(toolContext.getSecurityMaster());
+    loadPortfolio(toolContext.getPortfolioMaster(), toolContext.getPositionMaster(), toolContext.getSecurityMaster(), toolContext.getSecuritySource());
+    loadConfig(toolContext.getConfigMaster(), toolContext.getPortfolioMaster());
+    loadHistoricalTimeSeries(toolContext.getHistoricalTimeSeriesMaster());
+    loadSnapshot(toolContext.getMarketDataSnapshotMaster());
+    loadFunctionConfiguration(toolContext.getConfigMaster());
+  }
+  
+  private void loadFunctionConfiguration(final ConfigMaster configMaster) {
+    AbstractTool<ToolContext> remoteServerTool = new AbstractTool<ToolContext>() {
+
+      @Override
+      protected void doRun() throws Exception {
+        FunctionConfigurationSource functionConfigSource = getToolContext().getFunctionConfigSource();
+        FunctionConfigurationDefinition definition = FunctionConfigurationDefinition.of(DEMO_FUNCTION, functionConfigSource);
+        final ConfigItem<FunctionConfigurationDefinition> config = ConfigItem.of(definition, DEMO_FUNCTION, FunctionConfigurationDefinition.class);
+        ConfigMasterUtils.storeByName(getToolContext().getConfigMaster(), config);
+      }
+    };
+    String[] args = {"-c", _serverUrl};
+    remoteServerTool.initAndRun(args, ToolContext.class);
+  }
+
+  private void loadSecurity(final SecurityMaster demoSecurityMaster) {
+    s_logger.info("loading securities");
+    AbstractTool<ToolContext> remoteServerTool = new AbstractTool<ToolContext>() {
+
+      @Override
+      protected void doRun() throws Exception {
+        SecurityMaster remotesecurityMaster = getToolContext().getSecurityMaster();
+        
+        PortfolioMaster remotePortfolioMaster = getToolContext().getPortfolioMaster();
+        PositionSource remotePositionSource = getToolContext().getPositionSource();
+        
+        final Set<UniqueId> portfoliosecurities = Sets.newHashSet();
+        PortfolioSearchRequest request = new PortfolioSearchRequest();
+        request.setDepth(0);
+        for (PortfolioDocument portfolioDocument : PortfolioSearchIterator.iterable(remotePortfolioMaster, request)) {
+          Portfolio portfolio = remotePositionSource.getPortfolio(portfolioDocument.getUniqueId(), VersionCorrection.LATEST);
+          Portfolio resolvePortfolio = PortfolioCompiler.resolvePortfolio(portfolio, _executorService, getToolContext().getSecuritySource());
+          List<Position> positions = PortfolioAggregator.flatten(resolvePortfolio);
+          for (Position position : positions) {
+            if (position.getSecurity() != null) {
+              portfoliosecurities.add(position.getSecurity().getUniqueId());
+            }
+          }
+        }
+          
+        for (SecurityDocument securityDocument : SecuritySearchIterator.iterable(remotesecurityMaster, new SecuritySearchRequest())) {
+          if (portfoliosecurities.contains(securityDocument.getUniqueId())) {
+            securityDocument.setUniqueId(null);
+            demoSecurityMaster.add(securityDocument);
+          }
+        }
+      }
+    };
+    String[] args = {"-c", _serverUrl};
+    remoteServerTool.initAndRun(args, ToolContext.class);
+  }
+  
+  private void loadPortfolio(final PortfolioMaster demoPortfolioMaster, final PositionMaster demoPositionMaster,
+      final SecurityMaster demoSecurityMaster, final SecuritySource demoSecuritySource) {
+    s_logger.info("loading portfolios");
+    AbstractTool<ToolContext> remoteServerTool = new AbstractTool<ToolContext>() {
+
+      @Override
+      protected void doRun() throws Exception {
+        PortfolioMaster remotePortfolioMaster = getToolContext().getPortfolioMaster();
+        PositionSource remotePositionSource = getToolContext().getPositionSource();
+        
+        PortfolioSearchRequest request = new PortfolioSearchRequest();
+        request.setDepth(0);
+        for (PortfolioDocument portfolioDocument : PortfolioSearchIterator.iterable(remotePortfolioMaster, request)) {
+          Portfolio portfolio = remotePositionSource.getPortfolio(portfolioDocument.getUniqueId(), VersionCorrection.LATEST);
+          SavePortfolio savePortfolio = new SavePortfolio(_executorService, demoPortfolioMaster, demoPositionMaster);
+          savePortfolio.savePortfolio(portfolio, true);
+        }
+      }
+    };
+    String[] args = {"-c", _serverUrl };
+    remoteServerTool.initAndRun(args, ToolContext.class);
+  }
+  
+  private void loadConfig(final ConfigMaster configMaster, final PortfolioMaster portfolioMaster) {
+    s_logger.info("loading configs");
+    AbstractTool<ToolContext> remoteServerTool = new AbstractTool<ToolContext>() {
+
+      @Override
+      protected void doRun() throws Exception {
+        final ConfigMaster remoteConfigMaster = getToolContext().getConfigMaster();
+        final PortfolioMaster remotePortfolioMaster = getToolContext().getPortfolioMaster();
+        ConfigSaver configSaver = new ConfigSaver(remoteConfigMaster, remotePortfolioMaster, new ArrayList<String>(), new ArrayList<String>(), true, true);
+        ByteArrayOutputStream byteArrayOutput = new ByteArrayOutputStream();
+        PrintStream outputStream = new PrintStream(byteArrayOutput);
+        configSaver.saveConfigs(outputStream);
+        ConfigLoader configLoader = new ConfigLoader(configMaster, portfolioMaster, true, true, true);
+        configLoader.loadConfig(new ByteArrayInputStream(byteArrayOutput.toByteArray()));            
+      }
+    };
+    String[] args = {"-c", _serverUrl };
+    remoteServerTool.initAndRun(args, ToolContext.class);
+  }
+  
+  private void loadHistoricalTimeSeries(final HistoricalTimeSeriesMaster htsMaster) {
+    s_logger.info("loading timeseries");
+    AbstractTool<ToolContext> remoteServerTool = new AbstractTool<ToolContext>() {
+
+      @Override
+      protected void doRun() throws Exception {
+        final HistoricalTimeSeriesMaster remoteHtsMaster = getToolContext().getHistoricalTimeSeriesMaster();
+        HistoricalTimeSeriesInfoSearchRequest request = new HistoricalTimeSeriesInfoSearchRequest();
+        for (HistoricalTimeSeriesInfoDocument historicalTimeSeriesInfoDocument : HistoricalTimeSeriesInfoSearchIterator.iterable(remoteHtsMaster, request)) {
+          ObjectId timeSeriesObjectId = historicalTimeSeriesInfoDocument.getInfo().getTimeSeriesObjectId();
+          ManageableHistoricalTimeSeries timeSeries = remoteHtsMaster.getTimeSeries(timeSeriesObjectId, VersionCorrection.LATEST);            
+          HistoricalTimeSeriesInfoDocument addedDoc = htsMaster.add(historicalTimeSeriesInfoDocument);
+          htsMaster.updateTimeSeriesDataPoints(addedDoc.getInfo().getTimeSeriesObjectId(), timeSeries.getTimeSeries());
+        }
+      }
+    };
+    String[] args = {"-c", _serverUrl };
+    remoteServerTool.initAndRun(args, ToolContext.class);
+  }
+
+  private void loadSnapshot(final MarketDataSnapshotMaster marketDataSnapshotMaster) {
+    s_logger.info("loading market data snapshots");
+    AbstractTool<ToolContext> remoteServerTool = new AbstractTool<ToolContext>() {
+
+      @Override
+      protected void doRun() throws Exception {
+        MarketDataSnapshotMaster remoteSnapshotMaster = getToolContext().getMarketDataSnapshotMaster();
+        MarketDataSnapshotSearchRequest request = new MarketDataSnapshotSearchRequest();
+        for (MarketDataSnapshotDocument snapshotDocument : MarketDataSnapshotSearchIterator.iterable(remoteSnapshotMaster, request)) {
+          marketDataSnapshotMaster.add(snapshotDocument);
+        }
+      }
+    };
+    String[] args = {"-c", _serverUrl };
+    remoteServerTool.initAndRun(args, ToolContext.class);
+  }
+
+}
