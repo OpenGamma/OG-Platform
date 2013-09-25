63f3c25398d7c2f66af82428b64f926501b068c2
Ian Turner <ian@opengamma.com>
2013-09-25 17:24:24 +0100

    [PLAT-4742] rest api with correct login/logout put methods

--- projects/OG-Web/web-engine/prototype/scripts/og/api/og.api.rest.user.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/api/og.api.rest.user.js
@@ -3,10 +3,10 @@
  * Please see distribution for license.
  */
 $.register_module({
-    name: 'og.api.rest.regions',
+    name: 'og.api.rest.user',
     dependencies: ['og.api.common', 'og.api.rest'],
     obj: function () {
-        var common = og.api.common, api = og.api.rest;
+        var common = og.api.common, api = og.api.rest, check = common.check;
         return { // all requests that begin with /user
             root: 'user',
             get: common.not_implemented_get,
@@ -15,13 +15,29 @@ $.register_module({
             login: {
                 root: 'user/login',
                 get: common.not_implemented_put,
-                put: console.log('in'),
+                put: function (config) {
+                    config = config || {};
+                    var root = this.root, method = root.split('/'), data = {}, meta;
+                    meta = check({
+                        bundle: {method: root + '#put', config: config}
+                    });
+                    meta.type = 'PUT';
+                    return api.request(method, {data: data, meta: meta});
+                },
                 del: common.not_implemented_del
             },
             logout: {
                 root: 'user/logout',
-                get: common.not_implemented_put,
-                put: console.log('out'),
+                get: common.not_implemented_get,
+                put: function (config) {
+                    config = config || {};
+                    var root = this.root, method = root.split('/'), data = {}, meta;
+                    meta = check({
+                        bundle: {method: root + '#put', config: config}
+                    });
+                    meta.type = 'PUT';
+                    return api.request(method, {data: data, meta: meta});
+                },
                 del: common.not_implemented_del
             }
         };
