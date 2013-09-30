600fabd886ccefd53a54488362be353043a62803
Jonathan Senior <jonathan@opengamma.com>
2013-09-30 16:52:46 +0100

    [PLAT-4774] Truncate tiny values to zero

--- projects/OG-MasterDB/src/test/java/com/opengamma/masterdb/batch/DbBatchWriterTest.java
+++ projects/OG-MasterDB/src/test/java/com/opengamma/masterdb/batch/DbBatchWriterTest.java
@@ -29,6 +29,7 @@ import org.threeten.bp.Instant;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.opengamma.DataNotFoundException;
@@ -55,6 +56,7 @@ import com.opengamma.engine.value.ValueRequirement;
 import com.opengamma.engine.value.ValueSpecification;
 import com.opengamma.engine.view.AggregatedExecutionLog;
 import com.opengamma.engine.view.ViewComputationResultModel;
+import com.opengamma.engine.view.ViewResultEntry;
 import com.opengamma.engine.view.cycle.ViewCycleMetadata;
 import com.opengamma.engine.view.impl.InMemoryViewComputationResultModel;
 import com.opengamma.financial.security.equity.EquitySecurity;
@@ -543,4 +545,21 @@ public class DbBatchWriterTest extends AbstractDbBatchTest {
     result.addValue("config_1", cvr);
     _batchMaster.addJobResults(run.getObjectId(), result);
   }
+  
+  @Test
+  public void truncateSmallValueToZero() {
+    final UniqueId marketDataUid = _cycleMetadataStub.getMarketDataSnapshotId();
+    _batchMaster.createMarketData(marketDataUid);
+    final RiskRun run = _batchMaster.startRiskRun(_cycleMetadataStub, Maps.<String, String>newHashMap(), RunCreationMode.AUTO, SnapshotMode.PREPARED);
+    final InMemoryViewComputationResultModel result = new InMemoryViewComputationResultModel();
+    final ComputedValueResult cvr = new ComputedValueResult(_specification, 1e-323, AggregatedExecutionLog.EMPTY, "someComputeNode", null, InvocationResult.SUCCESS);
+    //cvr.setRequirements(newHashSet(_requirement));
+    result.addValue("config_1", cvr);
+    _batchMaster.addJobResults(run.getObjectId(), result);
+    
+    List<ViewResultEntry> resultEntries = _batchMaster.getBatchValues(run.getObjectId(), PagingRequest.ALL).getFirst();
+    ViewResultEntry resultEntry = Iterables.getOnlyElement(resultEntries);
+    assertEquals(0d, resultEntry.getComputedValue().getValue());
+  }
+  
 }
