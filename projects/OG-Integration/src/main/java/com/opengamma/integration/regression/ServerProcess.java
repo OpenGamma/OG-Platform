9becd97fc927bd890e7d075b0c548fc5f89dcda9
Chris Kent <chris@opengamma.com>
2013-10-02 10:28:40 +0100

    PLAT-4708 - Split ServerRunner into ServerProcess and ViewRunner

---
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/regression/ServerProcess.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.integration.regression;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import com.opengamma.OpenGammaRuntimeException;
+import com.opengamma.component.OpenGammaComponentServer;
+
+/**
+ *
+ */
+public final class ServerProcess implements AutoCloseable {
+
+  private final Process _process;
+  
+  private ServerProcess(Process process) {
+    _process = process;
+  }
+
+  // TODO catch and throw OGRE?
+  public static ServerProcess start() {
+    // TODO is it possible to scan the view defs and run all of them?
+    // need to be able to choose the snapshot for each view def. if there's only one for each base view def then
+    // it should be possible
+    // TODO how does the web resource figure out the base view for a snapshot?
+    String projectName = "examples-simulated";
+    String version = "2.2.0-SNAPSHOT";
+    String configFile = "classpath:fullstack/fullstack-examplessimulated-bin.properties";
+    String serverJar = projectName + "-" + version + ".jar";
+    String classpath = "config:lib/" + serverJar;
+    String logbackConfig = "-Dlogback.configurationFile=com/opengamma/util/warn-logback.xml";
+
+    // after 'mvn install' the server is in
+    // $PROJECT_DIR/server/target/server-dir
+    // TODO ATM this is running from the same location but that won't be the case forever
+    // do I want to switch the branch underneath the running installation? seems dodgy but probably OK for testing
+    ProcessBuilder processBuilder = new ProcessBuilder("java",
+                                                       logbackConfig, // TODO why isn't this getting picked up?
+                                                       "-cp",
+                                                       classpath,
+                                                       "-Xmx2g",
+                                                       "-XX:MaxPermSize=256M",
+                                                       "com.opengamma.component.OpenGammaComponentServer",
+                                                       configFile);
+    Process process;
+    try {
+      process = processBuilder.start();
+    } catch (IOException e) {
+      throw new OpenGammaRuntimeException("Failed to start server process", e);
+    }
+    BlockingQueue<Boolean> startupQueue = new ArrayBlockingQueue<>(1);
+    consumeStream(process.getInputStream(), OpenGammaComponentServer.STARTUP_COMPLETE_MESSAGE, startupQueue, true, System.out);
+    consumeStream(process.getErrorStream(), OpenGammaComponentServer.STARTUP_FAILED_MESSAGE, startupQueue, false, System.err);
+    Boolean startupSuccess;
+    try {
+      startupSuccess = startupQueue.take();
+    } catch (InterruptedException e) {
+      // not going to happen
+      throw new OpenGammaRuntimeException("unexpected exception", e);
+    }
+
+    if (!startupSuccess) {
+      throw new OpenGammaRuntimeException("startup failed, aborting");
+    }
+    return new ServerProcess(process);
+  }
+
+  /**
+   * Starts a thread which consumes a stream line by line and puts a value onto a queue when it encounters
+   * a line starting with a particular value. If an exception occurs a value of false it put onto the queue.
+   * Every line read from the stream is written to a {@link PrintStream}.
+   * @param stream The stream to consume
+   * @param value The trigger value - the stream line must <em>startWith</em> this string
+   * @param queue The queue
+   * @param signalValue The value to put onto the queue when line is encountered in the stream
+   * @param output Every line read from the stream is written to this print stream
+   */
+  private static void consumeStream(final InputStream stream,
+                                    final String value,
+                                    final BlockingQueue<Boolean> queue,
+                                    final Boolean signalValue,
+                                    final PrintStream output) {
+    Thread thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+          String nextLine;
+          while ((nextLine = reader.readLine()) != null) {
+            output.println(nextLine);
+            if (nextLine.startsWith(value)) {
+              queue.put(signalValue);
+            }
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+          try {
+            queue.put(false);
+          } catch (InterruptedException e1) {
+            // not going to happen
+          }
+        } catch (InterruptedException e) {
+          // not going to happen
+        }
+      }
+    });
+    thread.setDaemon(true);
+    thread.start();
+  }
+
+  @Override
+  public void close() {
+    _process.destroy();
+  }
+}
--- projects/OG-Integration/src/main/java/com/opengamma/integration/regression/ViewRunner.java
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/regression/ViewRunner.java
@@ -5,15 +5,8 @@
  */
 package com.opengamma.integration.regression;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.util.EnumSet;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -22,7 +15,6 @@ import org.threeten.bp.Instant;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.opengamma.OpenGammaRuntimeException;
-import com.opengamma.component.OpenGammaComponentServer;
 import com.opengamma.core.position.PositionSource;
 import com.opengamma.engine.marketdata.spec.MarketDataSpecification;
 import com.opengamma.engine.marketdata.spec.UserMarketDataSpecification;
@@ -53,57 +45,26 @@ import com.opengamma.util.ArgumentChecker;
 /**
  *
  */
-public final class ServerRunner {
+/* package */ class ViewRunner {
 
-  private ServerRunner() {
-  }
+
 
   public static void main(String[] args) throws Exception {
-    CalculationResults results1 = ServerRunner.run();
-    CalculationResults results2 = ServerRunner.run();
+    Instant valuationTime = Instant.now();
+    CalculationResults results1 = ViewRunner.run(valuationTime);
+    CalculationResults results2 = ViewRunner.run(valuationTime);
     CalculationDifference.Result result = CalculationDifference.compare(results1, results2, 0.01d);
     System.out.println(result);
   }
 
-  public static CalculationResults run() throws IOException, InterruptedException {
-    // TODO is it possible to scan the view defs and run all of them?
-    // need to be able to choose the snapshot for each view def. if there's only one for each base view def then
-    // it should be possible
-    // TODO how does the web resource figure out the base view for a snapshot?
-    String projectName = "examples-simulated";
-    String version = "2.2.0-SNAPSHOT";
-    String configFile = "classpath:fullstack/fullstack-examplessimulated-bin.properties";
-    String serverJar = projectName + "-" + version + ".jar";
-    String classpath = "config:lib/" + serverJar;
-    String logbackConfig = "-Dlogback.configurationFile=com/opengamma/util/warn-logback.xml";
-    String viewDefName = "AUD Swaps (3m / 6m basis) (1)";
-    String snapshotTime = "2013-09-27T12:17:45.587Z";
-    String snapshotName = viewDefName + "/" + snapshotTime;
+  public static CalculationResults run(Instant valuationTime) {
     int serverHttpPort = 8080;
-    Instant valuationTime = Instant.now();
-
-    // after 'mvn install' the server is in
-    // $PROJECT_DIR/server/target/server-dir
-    // TODO ATM this is running from the same location but that won't be the case forever
-    // do I want to switch the branch underneath the running installation? seems dodgy but probably OK for testing
-    ProcessBuilder processBuilder = new ProcessBuilder("java",
-                                                       logbackConfig, // TODO why isn't this getting picked up?
-                                                       "-cp",
-                                                       classpath,
-                                                       "-Xmx2g",
-                                                       "-XX:MaxPermSize=256M",
-                                                       "com.opengamma.component.OpenGammaComponentServer",
-                                                       configFile);
-    Process process = processBuilder.start();
-    BlockingQueue<Boolean> startupQueue = new ArrayBlockingQueue<>(1);
-    consumeStream(process.getInputStream(), OpenGammaComponentServer.STARTUP_COMPLETE_MESSAGE, startupQueue, true, System.out);
-    consumeStream(process.getErrorStream(), OpenGammaComponentServer.STARTUP_FAILED_MESSAGE, startupQueue, false, System.err);
-    Boolean startupSuccess = startupQueue.take();
-
-    if (!startupSuccess) {
-      throw new OpenGammaRuntimeException("startup failed, aborting");
-    }
-    try (RemoteServer server = RemoteServer.create("http://localhost:" + serverHttpPort)) {
+    // TODO get server details (port. any others? URL?) from the server process
+    try (ServerProcess serverProcess = ServerProcess.start();
+         RemoteServer server = RemoteServer.create("http://localhost:" + serverHttpPort)) {
+      String viewDefName = "AUD Swaps (3m / 6m basis) (1)";
+      String snapshotTime = "2013-09-27T12:17:45.587Z";
+      String snapshotName = viewDefName + "/" + snapshotTime;
       // look up the snapshot by name to get the ID
       MarketDataSnapshotSearchRequest snapshotSearchRequest = new MarketDataSnapshotSearchRequest();
       snapshotSearchRequest.setName(snapshotName);
@@ -127,7 +88,8 @@ public final class ServerRunner {
               .setResolverVersionCorrection(VersionCorrection.LATEST)
               .create();
       EnumSet<ViewExecutionFlags> flags = ExecutionFlags.triggersEnabled().get();
-      ArbitraryViewCycleExecutionSequence sequence = new ArbitraryViewCycleExecutionSequence(ImmutableList.of(cycleOptions));
+      ArbitraryViewCycleExecutionSequence sequence = new ArbitraryViewCycleExecutionSequence(ImmutableList.of(
+          cycleOptions));
       ViewExecutionOptions executionOptions = ExecutionOptions.of(sequence, cycleOptions, flags);
 
       ViewProcessor viewProcessor = server.getViewProcessor();
@@ -142,51 +104,11 @@ public final class ServerRunner {
       viewClient.waitForCompletion();
       System.out.println("view client completed");
       latch.await();
-      process.destroy();
       return listener.getResults();
+    } catch (InterruptedException e) {
+      // not going to happen but need this to satisfy the compiler
+      throw new OpenGammaRuntimeException("unexpected exception", e);
     }
-  }
-
-  /**
-   * Starts a thread which consumes a stream line by line and puts a value onto a queue when it encounters
-   * a line starting with a particular value. If an exception occurs a value of false it put onto the queue.
-   * Every line read from the stream is written to a {@link PrintStream}.
-   * @param stream The stream to consume
-   * @param value The trigger value - the stream line must <em>startWith</em> this string
-   * @param queue The queue
-   * @param signalValue The value to put onto the queue when line is encountered in the stream
-   * @param output Every line read from the stream is written to this print stream
-   */
-  private static void consumeStream(final InputStream stream,
-                                    final String value,
-                                    final BlockingQueue<Boolean> queue,
-                                    final Boolean signalValue,
-                                    final PrintStream output) {
-    Thread thread = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
-          String nextLine;
-          while ((nextLine = reader.readLine()) != null) {
-            output.println(nextLine);
-            if (nextLine.startsWith(value)) {
-              queue.put(signalValue);
-            }
-          }
-        } catch (IOException e) {
-          e.printStackTrace();
-          try {
-            queue.put(false);
-          } catch (InterruptedException e1) {
-            // not going to happen
-          }
-        } catch (InterruptedException e) {
-          // not going to happen
-        }
-      }
-    });
-    thread.setDaemon(true);
-    thread.start();
   }
 }
 
