cfab82d2ee469a67be35aa0af1de4677d027c611
Julian Howarth <julian@opengamma.com>
2013-10-03 12:36:51 +0100

    PLAT-4381 - MDC logging
    Unit testing

--- projects/OG-Util/src/test/java/com/opengamma/util/MdcAwareThreadPoolExecutorTest.java
+++ projects/OG-Util/src/test/java/com/opengamma/util/MdcAwareThreadPoolExecutorTest.java
@@ -19,7 +19,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.MDC;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.opengamma.util.test.TestGroup;
@@ -27,11 +26,12 @@ import com.opengamma.util.test.TestGroup;
 
 /**
  * Tests that MdcAwareThreadPoolExecutorTest correctly segregates MDC
- * information from different threads.
+ * information from different threads. Prints output to standard out
+ * as it's otherwise difficult to check that everything is created by
+ * the expected thread.
  */
 @Test(groups = TestGroup.UNIT)
 public class MdcAwareThreadPoolExecutorTest {
-
 
   private ExecutorService createMdcAwareService(int threads) {
     return new MdcAwareThreadPoolExecutor(threads,
@@ -42,7 +42,7 @@ public class MdcAwareThreadPoolExecutorTest {
                                               new NamedThreadPoolFactory("mdcAware"));
   }
 
-  private static ExecutorService createStandardService(int threads) {
+  private ExecutorService createStandardService(int threads) {
     return new ThreadPoolExecutor(threads,
                                               threads,
                                               60,
@@ -51,67 +51,71 @@ public class MdcAwareThreadPoolExecutorTest {
                                               new NamedThreadPoolFactory("standard"));
   }
 
-  private Runnable createCheckJob(final Map expectedContext, final AtomicInteger checkCount) {
-
-    System.out.println("Creating check job on thread - " +  Thread.currentThread().getName() + " MDC: " + MDC.getCopyOfContextMap());
-    return new Runnable() {
-      @Override
-      public void run() {
-
-        System.out.println("Running on thread-" + Thread.currentThread().getName() + " == " + MDC.getCopyOfContextMap()) ;
-        try {
-          assertThat(MDC.getCopyOfContextMap(), is(expectedContext));
-          checkCount.incrementAndGet();
-        } catch (Throwable e) {
-          System.out.println(e);
-        }
-      }
-    };
-  }
-
+  @Test(enabled = false)
   public void testMdcIdLostWhenUsingStandardPool() throws InterruptedException {
+
+    // This is the case we want our new code to cope with. i.e. a thread pool losing
+    // the MDC information. Unfortunately, the test below fails as the MDC data
+    // *is* being magically passed across the threads
     final ExecutorService service = createStandardService(3);
     final AtomicInteger checkCount = new AtomicInteger(0);
-    //new Thread(new Runnable() {
-    //  @Override
-    //  public void run() {
-    //    MDC.put("some_key", "somevalue");
-    //    MDC.put("name", Thread.currentThread().getName());
-    //    s_service.execute(createCheckJob(null, checkCount));
-    //  }
-    //}, "starter").start();
+    createStarterThreads(3, service, checkCount, false);
 
-    createStarterThreads(3, service, checkCount);
-
-
-    Thread.sleep(1000);
+    // Wait for the threads to complete
+    Thread.sleep(200);
 
     assertThat(checkCount.get(), is(3));
   }
 
+  @Test
   public void testMdcIdLoggedForSingleThread() throws InterruptedException {
 
     final ExecutorService service = createMdcAwareService(1);
     final AtomicInteger checkCount = new AtomicInteger(0);
-    createStarterThreads(1, service, checkCount);
+    createStarterThreads(1, service, checkCount, true);
 
-    Thread.sleep(1000);
+    Thread.sleep(200);
 
     assertThat(checkCount.get(), is(1));
   }
 
+  @Test
   public void testCorrectIdLoggedForMultipleThreads() throws InterruptedException {
 
     final ExecutorService service = createMdcAwareService(3);
     final AtomicInteger checkCount = new AtomicInteger(0);
-    createStarterThreads(3, service, checkCount);
+    createStarterThreads(3, service, checkCount, true);
 
-    Thread.sleep(1000);
+    Thread.sleep(200);
 
     assertThat(checkCount.get(), is(3));
   }
 
-  private void createStarterThreads(int qty, final ExecutorService service, final AtomicInteger checkCount) {
+  @Test
+  public void testCorrectIdLoggedForMultipleThreadsWithPoolReuse() throws InterruptedException {
+
+    final ExecutorService service = createMdcAwareService(3);
+    final AtomicInteger checkCount = new AtomicInteger(0);
+
+    createStarterThreads(3, service, checkCount, true);
+    Thread.sleep(200);
+
+    createStarterThreads(3, service, checkCount, true);
+    Thread.sleep(200);
+
+    createStarterThreads(3, service, checkCount, true);
+    Thread.sleep(200);
+
+    createStarterThreads(3, service, checkCount, true);
+    Thread.sleep(200);
+
+    assertThat(checkCount.get(), is(12));
+  }
+
+  private void createStarterThreads(int qty,
+                                    final ExecutorService service,
+                                    final AtomicInteger checkCount,
+                                    final boolean mdcPropagationExpected) {
 
     Set<Thread> threads = new HashSet<>();
     for (int i = 0; i < qty; i++) {
@@ -125,21 +129,33 @@ public class MdcAwareThreadPoolExecutorTest {
           contextMap.put("name", Thread.currentThread().getName());
           System.out.println("Putting MDC values: " + contextMap);
           MDC.setContextMap(contextMap);
-          service.execute(createCheckJob(contextMap, checkCount));
+          Map<String, String> expectedContext = mdcPropagationExpected ? contextMap : null;
+          service.execute(createCheckJob(expectedContext, checkCount));
         }
       }, "starter" + i));
     }
-    System.out.println("Threads created");
+    System.out.println("Threads created - MDC contains: " + MDC.getCopyOfContextMap());
     for (Thread thread : threads) {
       thread.start();
     }
-    System.out.println("Threads started");
+    System.out.println("Threads started - MDC contains: " + MDC.getCopyOfContextMap());
   }
 
-// setup mdc aware executors pool
-  // start pair of threads with different MDC contexts
+  private Runnable createCheckJob(final Map expectedContext, final AtomicInteger checkCount) {
 
-  // execute simultaneous jobs using the executors pool and verify MDC information for each is as expected
+    System.out.println("Creating check job on thread - " +  Thread.currentThread().getName() + " MDC: " + MDC.getCopyOfContextMap());
+    return new Runnable() {
+      @Override
+      public void run() {
 
-
+        System.out.println("Running on thread-" + Thread.currentThread().getName() + " => " + MDC.getCopyOfContextMap()) ;
+        try {
+          assertThat(MDC.getCopyOfContextMap(), is(expectedContext));
+          checkCount.incrementAndGet();
+        } catch (Throwable e) {
+          System.out.println(e);
+        }
+      }
+    };
+  }
 }
