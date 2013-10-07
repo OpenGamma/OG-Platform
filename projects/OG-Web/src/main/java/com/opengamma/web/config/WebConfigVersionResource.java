1ed5ee474c3d03489a708948259f7abacd4d1421
Yomi Ayodele <yomi@opengamma.com>
2013-10-07 15:36:41 +0100

    [PLAT-4684] Move parseXML and createXML to AbstractWebResource so it can be shared by all WebResources

--- projects/OG-Web/src/main/java/com/opengamma/web/config/WebConfigVersionResource.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/config/WebConfigVersionResource.java
@@ -50,7 +50,7 @@ public class WebConfigVersionResource extends AbstractWebConfigResource {
   public String getHTML() {
     FlexiBean out = createRootData();
     ConfigDocument doc = data().getVersioned();
-    out.put("configXml", createBeanXML(doc));
+    out.put("configXml", createBeanXML(doc.getConfig().getValue()));
     return getFreemarker().build(HTML_DIR + "configversion.ftl", out);
   }
 
@@ -68,7 +68,7 @@ public class WebConfigVersionResource extends AbstractWebConfigResource {
     if (jsonConfig != null) {
       out.put("configJSON", jsonConfig);
     } else {
-      out.put("configXML", StringEscapeUtils.escapeJavaScript(createBeanXML(doc)));
+      out.put("configXML", StringEscapeUtils.escapeJavaScript(createBeanXML(doc.getConfig().getValue())));
     }
     out.put("type", data().getTypeMap().inverse().get(doc.getType()));
     String json = getFreemarker().build(JSON_DIR + "config.ftl", out);
@@ -102,6 +102,7 @@ public class WebConfigVersionResource extends AbstractWebConfigResource {
     out.put("configDoc", versionedConfig);
     out.put("config", versionedConfig.getConfig().getValue());
     out.put("configDescription", getConfigTypesProvider().getDescription(versionedConfig.getConfig().getType()));
+    out.put("configXml", createXML(versionedConfig.getConfig().getValue()));
     out.put("deleted", !latestDoc.isLatest());
     return out;
   }
