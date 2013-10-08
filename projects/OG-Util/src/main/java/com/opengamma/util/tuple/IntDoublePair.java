bbc7c57b26721ea06ea8fe0fc269f0f72572f923
Stephen Colebourne <stephen@opengamma.com>
2013-10-08 15:50:27 +0100

    PLAT-4839 - Pair hierarchy as Joda-Beans or Joda-Convert types

--- projects/OG-Util/src/main/java/com/opengamma/util/tuple/IntDoublePair.java
+++ projects/OG-Util/src/main/java/com/opengamma/util/tuple/IntDoublePair.java
@@ -5,6 +5,12 @@
  */
 package com.opengamma.util.tuple;
 
+import org.apache.commons.lang.StringUtils;
+import org.joda.convert.FromString;
+import org.joda.convert.ToString;
+
+import com.opengamma.util.ArgumentChecker;
+
 import it.unimi.dsi.fastutil.ints.Int2DoubleMap;
 
 /**
@@ -26,7 +32,23 @@ public class IntDoublePair extends Pair<Integer, Double> implements Int2DoubleMa
   public final double second; // CSIGNORE
 
   /**
-   * Creates a pair from the specified values.
+   * Obtains an {@code IntDoublePair} from a {@code Pair}.
+   * 
+   * @param pair  the pair to convert, not null
+   * @return a pair formed by extracting values from the pair, not null
+   */
+  public static IntDoublePair of(final Pair<Integer, Double> pair) {
+    if (pair instanceof IntDoublePair) {
+      return (IntDoublePair) pair;
+    }
+    ArgumentChecker.notNull(pair, "pair");
+    ArgumentChecker.notNull(pair.getFirst(), "pair.first");
+    ArgumentChecker.notNull(pair.getSecond(), "pair.second");
+    return new IntDoublePair(pair.getFirst(), pair.getSecond());
+  }
+
+  /**
+   * Obtains an {@code IntDoublePair} from two values.
    * 
    * @param first  the first element
    * @param second  the second element
@@ -36,11 +58,45 @@ public class IntDoublePair extends Pair<Integer, Double> implements Int2DoubleMa
     return new IntDoublePair(first, second);
   }
 
+  //-------------------------------------------------------------------------
   /**
-   * Constructor.
+   * Parses an {@code IntDoublePair} from the standard string format.
+   * <p>
+   * The standard format is '[$first, $second]'. Spaces around the values are trimmed.
+   * 
+   * @param pairStr  the text to parse, not null
+   * @return the parsed pair, not null
+   */
+  @FromString
+  public static IntDoublePair parse(final String pairStr) {
+    ArgumentChecker.notNull(pairStr, "pairStr");
+    if (pairStr.length() < 5) {
+      throw new IllegalArgumentException("Invalid pair format, too short: " + pairStr);
+    }
+    if (pairStr.charAt(0) != '[') {
+      throw new IllegalArgumentException("Invalid pair format, must start with [: " + pairStr);
+    }
+    if (pairStr.charAt(pairStr.length() - 1) != ']') {
+      throw new IllegalArgumentException("Invalid pair format, must end with ]: " + pairStr);
+    }
+    String[] split = StringUtils.split(pairStr.substring(1, pairStr.length() - 1), ',');
+    if (split.length != 2) {
+      throw new IllegalArgumentException("Invalid pair format, must have two values: " + pairStr);
+    }
+    int first = Integer.parseInt(split[0].trim());
+    double second = Double.parseDouble(split[1].trim());
+    return new IntDoublePair(first, second);
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Constructs a pair.
+   * 
    * @param first  the first element
    * @param second  the second element
+   * @deprecated Use public factory of(int,double)
    */
+  @Deprecated
   public IntDoublePair(final int first, final double second) {
     this.first = first;
     this.second = second;
@@ -109,6 +165,24 @@ public class IntDoublePair extends Pair<Integer, Double> implements Int2DoubleMa
     // see Map.Entry API specification
     final long d = Double.doubleToLongBits(second);
     return first ^ ((int) (d ^ (d >>> 32)));
+  }
+
+  /**
+   * Gets the pair using a standard string format.
+   * <p>
+   * The standard format is '[$first, $second]'. Spaces around the values are trimmed.
+   * 
+   * @return the pair as a string, not null
+   */
+  @Override
+  @ToString
+  public String toString() {
+    return new StringBuilder()
+        .append("[")
+        .append(first)
+        .append(", ")
+        .append(second)
+        .append("]").toString();
   }
 
 }
--- projects/OG-Util/src/test/java/com/opengamma/util/tuple/LongDoublePairTest.java
+++ projects/OG-Util/src/test/java/com/opengamma/util/tuple/LongDoublePairTest.java
@@ -13,62 +13,62 @@ import org.testng.annotations.Test;
 import com.opengamma.util.test.TestGroup;
 
 /**
- * Test IntDoublePair.
+ * Test LongDoublePair.
  */
 @Test(groups = TestGroup.UNIT)
-public class IntDoublePairTest {
+public class LongDoublePairTest {
 
-  public void test_IntDoublePair_of() {
-    IntDoublePair test = IntDoublePair.of(2, 2.5d);
-    assertEquals(Integer.valueOf(2), test.getFirst());
+  public void test_LongDoublePair_of() {
+    LongDoublePair test = LongDoublePair.of(2L, 2.5d);
+    assertEquals(Long.valueOf(2), test.getFirst());
     assertEquals(Double.valueOf(2.5d), test.getSecond());
-    assertEquals(2, test.getFirstInt());
+    assertEquals(2, test.getFirstLong());
     assertEquals(2.5d, test.getSecondDouble(), 0.00001d);
-    assertEquals(Integer.valueOf(2), test.getKey());
+    assertEquals(Long.valueOf(2), test.getKey());
     assertEquals(Double.valueOf(2.5d), test.getValue());
-    assertEquals(2, test.getIntKey());
+    assertEquals(2, test.getLongKey());
     assertEquals(2.5d, test.getDoubleValue(), 0.00001d);
     assertEquals("[2, 2.5]", test.toString());
   }
 
-  public void test_IntDoublePair_parse1() {
-    IntDoublePair test = IntDoublePair.parse("[2, 2.5]");
-    assertEquals(Integer.valueOf(2), test.getFirst());
+  public void test_LongDoublePair_parse1() {
+    LongDoublePair test = LongDoublePair.parse("[2, 2.5]");
+    assertEquals(Long.valueOf(2), test.getFirst());
     assertEquals(Double.valueOf(2.5d), test.getSecond());
   }
 
-  public void test_IntDoublePair_parse2() {
-    IntDoublePair test = IntDoublePair.parse("[2,2.5]");
-    assertEquals(Integer.valueOf(2), test.getFirst());
+  public void test_LongDoublePair_parse2() {
+    LongDoublePair test = LongDoublePair.parse("[2,2.5]");
+    assertEquals(Long.valueOf(2), test.getFirst());
     assertEquals(Double.valueOf(2.5d), test.getSecond());
   }
 
   //-------------------------------------------------------------------------
   @Test(expectedExceptions = UnsupportedOperationException.class)
   public void testSetValue() {
-    IntDoublePair pair = IntDoublePair.of(2, -0.3d);
+    LongDoublePair pair = LongDoublePair.of(2L, -0.3d);
     pair.setValue(Double.valueOf(1.2d));
   }
 
   @Test(expectedExceptions = UnsupportedOperationException.class)
   public void testSetValue_null() {
-    IntDoublePair pair = IntDoublePair.of(2, -0.3d);
+    LongDoublePair pair = LongDoublePair.of(2L, -0.3d);
     pair.setValue(null);
   }
 
   @Test(expectedExceptions = UnsupportedOperationException.class)
   public void testSetValue_primitives() {
-    IntDoublePair pair = IntDoublePair.of(2, -0.3d);
+    LongDoublePair pair = LongDoublePair.of(2L, -0.3d);
     pair.setValue(1.2d);
   }
 
   //-------------------------------------------------------------------------
   public void compareTo() {
-    IntDoublePair ab = IntDoublePair.of(1, 1.7d);
-    IntDoublePair ac = IntDoublePair.of(1, 1.9d);
-    IntDoublePair ba = IntDoublePair.of(2, 1.5d);
+    LongDoublePair ab = LongDoublePair.of(1L, 1.7d);
+    LongDoublePair ac = LongDoublePair.of(1L, 1.9d);
+    LongDoublePair ba = LongDoublePair.of(2L, 1.5d);
 
-    FirstThenSecondPairComparator<Integer, Double> comparator = new FirstThenSecondPairComparator<Integer, Double>();
+    FirstThenSecondPairComparator<Long, Double> comparator = new FirstThenSecondPairComparator<Long, Double>();
     assertTrue(comparator.compare(ab, ab) == 0);
     assertTrue(comparator.compare(ac, ab) > 0);
     assertTrue(comparator.compare(ba, ab) > 0);
@@ -83,10 +83,10 @@ public class IntDoublePairTest {
   }
 
   public void testEquals() {
-    IntDoublePair a = IntDoublePair.of(1, 2.0);
-    IntDoublePair b = IntDoublePair.of(1, 3.0);
-    IntDoublePair c = IntDoublePair.of(2, 2.0);
-    IntDoublePair d = IntDoublePair.of(2, 3.0);
+    LongDoublePair a = LongDoublePair.of(1L, 2.0);
+    LongDoublePair b = LongDoublePair.of(1L, 3.0);
+    LongDoublePair c = LongDoublePair.of(2L, 2.0);
+    LongDoublePair d = LongDoublePair.of(2L, 3.0);
     assertEquals(true, a.equals(a));
     assertEquals(false, a.equals(b));
     assertEquals(false, a.equals(c));
@@ -109,15 +109,15 @@ public class IntDoublePairTest {
   }
 
   public void testEquals_toObjectVersion() {
-    IntDoublePair a = IntDoublePair.of(1, 1.7d);
-    Pair<Integer, Double> b = Pair.of(Integer.valueOf(1), Double.valueOf(1.7d));
+    LongDoublePair a = LongDoublePair.of(1L, 1.7d);
+    Pair<Long, Double> b = Pair.of(Long.valueOf(1), Double.valueOf(1.7d));
     assertEquals(true, a.equals(b));
     assertEquals(true, b.equals(a));
   }
 
   public void testEquals_toObjectVersion_null() {
-    IntDoublePair b = IntDoublePair.of(1, 1.7d);
-    Pair<Integer, Double> a = Pair.of(null, Double.valueOf(1.9d));
+    LongDoublePair b = LongDoublePair.of(1L, 1.7d);
+    Pair<Long, Double> a = Pair.of(null, Double.valueOf(1.9d));
     assertEquals(true, a.equals(a));
     assertEquals(false, a.equals(b));
     assertEquals(false, b.equals(a));
@@ -125,15 +125,15 @@ public class IntDoublePairTest {
   }
 
   public void testHashCode() {
-    IntDoublePair a = IntDoublePair.of(1, 1.7d);
-    Pair<Integer, Double> b = Pair.of(Integer.valueOf(1), Double.valueOf(1.7d));
+    LongDoublePair a = LongDoublePair.of(1L, 1.7d);
+    Pair<Long, Double> b = Pair.of(Long.valueOf(1), Double.valueOf(1.7d));
     assertEquals(a.hashCode(), b.hashCode());
   }
 
   public void testHashCode_value() {
-    IntDoublePair a = IntDoublePair.of(1, 2.0);
+    LongDoublePair a = LongDoublePair.of(1L, 2.0);
     assertEquals(a.hashCode(), a.hashCode());
-    assertEquals(Integer.valueOf(1).hashCode() ^ Double.valueOf(2.0).hashCode(), a.hashCode());
+    assertEquals(Long.valueOf(1).hashCode() ^ Double.valueOf(2.0).hashCode(), a.hashCode());
     // can't test for different hash codes as they might not be different
   }
 
