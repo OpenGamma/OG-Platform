abab441978f20ddbda07bab2fe4b57fa05662960
Chris Kent <chris@opengamma.com>
2013-10-08 10:26:41 +0100

    PLAT-4380 - Handle all FudgeDateTime accuracies when building Expiry

--- projects/OG-Util/src/main/java/com/opengamma/util/time/ExpiryFudgeBuilder.java
+++ projects/OG-Util/src/main/java/com/opengamma/util/time/ExpiryFudgeBuilder.java
@@ -77,6 +77,10 @@ public final class ExpiryFudgeBuilder extends AbstractFudgeBuilder implements Fu
 
   protected static Expiry dateTimeToExpiry(final FudgeDateTime datetime, final String timezone) {
     switch (datetime.getAccuracy()) {
+      case NANOSECOND:
+      case MILLISECOND:
+      case MICROSECOND:
+      case SECOND:
       case MINUTE:
         return new Expiry(ZonedDateTime.ofInstant(datetime.toInstant(), ZoneId.of(timezone)), ExpiryAccuracy.MIN_HOUR_DAY_MONTH_YEAR);
       case HOUR:
