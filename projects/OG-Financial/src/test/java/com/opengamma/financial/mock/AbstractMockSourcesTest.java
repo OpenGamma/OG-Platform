7c613575034a0ed85e882cd905f624cb0fd55585
stuartw <stuart.wakefield@gmail.com>
2013-10-18 13:08:31 +0100

    [PLAT-4906] Allow sources to be instantiated and populated for financial tests

---
+++ projects/OG-Financial/src/test/java/com/opengamma/financial/mock/AbstractMockSourcesTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.mock;
+
+
+import org.springframework.util.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.opengamma.core.holiday.HolidaySource;
+import com.opengamma.core.region.RegionSource;
+import com.opengamma.engine.function.FunctionExecutionContext;
+import com.opengamma.financial.OpenGammaExecutionContext;
+import com.opengamma.financial.convention.ConventionBundleSource;
+import com.opengamma.util.test.TestGroup;
+
+/**
+ * Test harness which instantiates InMemory sources with sample data.
+ */
+public class AbstractMockSourcesTest {
+
+  protected FunctionExecutionContext _executionContext;
+  protected RegionSource _regionSource;
+  protected HolidaySource _holidaySource;
+  protected ConventionBundleSource _conventionBundleSource;
+
+  @BeforeSuite
+  protected void initMocks() {
+    _executionContext = MockSources.isdaMocks();
+    _regionSource = OpenGammaExecutionContext.getRegionSource(_executionContext);
+    _holidaySource = OpenGammaExecutionContext.getHolidaySource(_executionContext);
+    _conventionBundleSource = OpenGammaExecutionContext.getConventionBundleSource(_executionContext);
+  }
+
+  @Test(enabled = true, groups = TestGroup.UNIT)
+  public void testSourcesInited() {
+    Assert.notNull(_regionSource);
+    Assert.notNull(_holidaySource);
+    Assert.notNull(_conventionBundleSource);
+  }
+
+
+}
