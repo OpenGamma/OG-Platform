46d1f8ab02768a158231e8628e880c5075602e24
Stephen Colebourne <stephen@opengamma.com>
2013-11-01 10:35:27 +0000

    Add missing license

--- projects/OG-Master/src/main/java/com/opengamma/master/portfolio/impl/DynamicDelegatingPortfolioMaster.java
+++ projects/OG-Master/src/main/java/com/opengamma/master/portfolio/impl/DynamicDelegatingPortfolioMaster.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.master.portfolio.impl;
 
 import static com.google.common.collect.Maps.newHashMap;
