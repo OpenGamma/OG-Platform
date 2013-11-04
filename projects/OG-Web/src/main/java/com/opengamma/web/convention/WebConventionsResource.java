8685e0dd143c0f10de9ba9e522d58df5b9c2769f
Stephen Colebourne <stephen@opengamma.com>
2013-11-04 17:35:41 +0000

    Checkstyle

--- projects/OG-Web/src/main/java/com/opengamma/web/convention/WebConventionsResource.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/convention/WebConventionsResource.java
@@ -8,7 +8,6 @@ package com.opengamma.web.convention;
 import java.net.URI;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -20,7 +19,6 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
