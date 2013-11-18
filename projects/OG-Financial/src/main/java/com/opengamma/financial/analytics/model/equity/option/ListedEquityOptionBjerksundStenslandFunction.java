f9f7add5fe6b0c387f09540e7c7129de86adf309
Casey Clements <casey@opengamma.com>
2013-11-18 14:53:40 -0500

    Revert "Revert "PLAT-5120 Create ListedEquityOptionBjerksundStenslandFunction family, BjS with option price as vol requirement""
    This reverts commit e7ad56abceb4f3487004352c456ade6c5eeca659.

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/equity/option/ListedEquityOptionBjerksundStenslandFunction.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/equity/option/ListedEquityOptionBjerksundStenslandFunction.java
@@ -8,22 +8,24 @@ package com.opengamma.financial.analytics.model.equity.option;
 import com.opengamma.core.security.Security;
 import com.opengamma.engine.ComputationTarget;
 import com.opengamma.engine.function.FunctionCompilationContext;
+import com.opengamma.engine.value.ValueRequirementNames;
 import com.opengamma.financial.analytics.model.CalculationPropertyNamesAndValues;
 import com.opengamma.financial.analytics.model.SecurityExerciseTypeVisitor;
 import com.opengamma.financial.security.FinancialSecurity;
 import com.opengamma.financial.security.option.AmericanExerciseType;
 
 /**
- * Base class for the {@link CalculationPropertyNamesAndValues#BJERKSUND_STENSLAND_METHOD}<p>
- * This form requires a {@link ValueRequirementNames#BLACK_VOLATILITY_SURFACE}. 
- * See {@link ListedEquityOptionBjerksundStenslandFunction} for a simpler version that instead requires a market price. 
- */
-public abstract class EquityOptionBjerksundStenslandFunction extends EquityOptionFunction {
+* Base class for the {@link CalculationPropertyNamesAndValues#BJERKSUND_STENSLAND_METHOD}.<p>
+* This form requires that a market price is available for the option.
+* When this isn't available, pricing can be done via {@link ValueRequirementNames#BLACK_VOLATILITY_SURFACE}. 
+* See {@link EquityOptionBjerksundStenslandFunction} 
+*/
+public abstract class ListedEquityOptionBjerksundStenslandFunction extends ListedEquityOptionFunction {
 
   /**
    * @param valueRequirementNames The value requirement names
    */
-  public EquityOptionBjerksundStenslandFunction(final String... valueRequirementNames) {
+  public ListedEquityOptionBjerksundStenslandFunction(final String... valueRequirementNames) {
     super(valueRequirementNames);
   }
 
@@ -32,10 +34,10 @@ public abstract class EquityOptionBjerksundStenslandFunction extends EquityOptio
     final Security security = target.getSecurity();
     return ((FinancialSecurity) security).accept(new SecurityExerciseTypeVisitor()) instanceof AmericanExerciseType;
   }
-
+  
   @Override
   protected String getCalculationMethod() {
-    return CalculationPropertyNamesAndValues.BJERKSUND_STENSLAND_METHOD;
+    return CalculationPropertyNamesAndValues.BJERKSUND_STENSLAND_LISTED_METHOD;
   }
 
   @Override
