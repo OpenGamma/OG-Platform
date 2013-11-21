d78521f4e2e2e5069e2ec26afb61e355e3a0cd16
stuartw <stuart.wakefield@gmail.com>
2013-11-21 10:21:45 +0000

    [PLAT-5072] Sign conventions for CDS

--- projects/OG-Financial/src/main/java/com/opengamma/financial/security/NotionalVisitor.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/security/NotionalVisitor.java
@@ -148,14 +148,14 @@ public class NotionalVisitor extends FinancialSecurityVisitorAdapter<CurrencyAmo
   @Override
   public CurrencyAmount visitStandardVanillaCDSSecurity(final StandardVanillaCDSSecurity security) {
     final InterestRateNotional notional = security.getNotional();
-    final int sign = security.isBuy() ? 1 : -1;
+    final int sign = security.isBuy() ? -1 : 1;
     return CurrencyAmount.of(notional.getCurrency(), sign * notional.getAmount());
   }
 
   @Override
   public CurrencyAmount visitLegacyVanillaCDSSecurity(final LegacyVanillaCDSSecurity security) {
     final InterestRateNotional notional = security.getNotional();
-    final int sign = security.isBuy() ? 1 : -1;
+    final int sign = security.isBuy() ? -1 : 1;
     return CurrencyAmount.of(notional.getCurrency(), sign * notional.getAmount());
   }
 
@@ -227,7 +227,8 @@ public class NotionalVisitor extends FinancialSecurityVisitorAdapter<CurrencyAmo
   @Override
   public CurrencyAmount visitCreditDefaultSwapIndexSecurity(final CreditDefaultSwapIndexSecurity security) {
     final InterestRateNotional notional = security.getNotional();
-    return CurrencyAmount.of(notional.getCurrency(), notional.getAmount());
+    final int sign = security.isBuy() ? -1 : 1;
+    return CurrencyAmount.of(notional.getCurrency(), sign * notional.getAmount());
   }
 
   @Override
