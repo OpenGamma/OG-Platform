c8c886afd48dcd0df511cb5eb1c9c93ba516c719
Casey Clements <casey@opengamma.com>
2013-11-25 18:58:24 -0500

    PLAT-5204 Relax restrictions on deliberable bond basket in BondFutureSecurityConverter

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/conversion/BondFutureSecurityConverter.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/conversion/BondFutureSecurityConverter.java
@@ -5,8 +5,11 @@
  */
 package com.opengamma.financial.analytics.conversion;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.threeten.bp.ZonedDateTime;
 
 import com.opengamma.OpenGammaRuntimeException;
@@ -29,6 +32,7 @@ public class BondFutureSecurityConverter extends FinancialSecurityVisitorAdapter
   /** Converter for the bonds in the deliverable basket */
   private final BondSecurityConverter _bondConverter;
 
+  private static final Logger s_logger = LoggerFactory.getLogger(BondFutureSecurityConverter.class);
   /**
    * @param securitySource The security source, not null
    * @param bondConverter The bond converter, not null
@@ -40,7 +44,6 @@ public class BondFutureSecurityConverter extends FinancialSecurityVisitorAdapter
     _bondConverter = bondConverter;
   }
 
-  //FIXME CASE - BondFutureDefinition needs a reference price. Without a trade, where will it come from?
   @Override
   public BondFutureDefinition visitBondFutureSecurity(final BondFutureSecurity bondFuture) {
     ArgumentChecker.notNull(bondFuture, "security");
@@ -50,17 +53,31 @@ public class BondFutureSecurityConverter extends FinancialSecurityVisitorAdapter
     final double notional = bondFuture.getUnitAmount();
     final List<BondFutureDeliverable> basket = bondFuture.getBasket();
     final int n = basket.size();
-    final BondFixedSecurityDefinition[] deliverables = new BondFixedSecurityDefinition[n];
-    final double[] conversionFactor = new double[n];
+    final ArrayList<BondFixedSecurityDefinition> deliverablesList = new ArrayList<>();
+    final ArrayList<Double> conversionFactorList = new ArrayList<>();
+    
+
     for (int i = 0; i < n; i++) {
       final BondFutureDeliverable deliverable = basket.get(i);
       final BondSecurity bondSecurity = (BondSecurity) _securitySource.getSingle(deliverable.getIdentifiers());
       if (bondSecurity == null) {
-        throw new OpenGammaRuntimeException("No security found with identifiers " + deliverable.getIdentifiers() + " in bond future deliverable basket");
+        s_logger.debug("Unable to create underlying bond with identifiers " + deliverable.getIdentifiers() + " in bond future deliverable basket");
+      } else {
+        deliverablesList.add((BondFixedSecurityDefinition) bondSecurity.accept(_bondConverter));
+        conversionFactorList.add(deliverable.getConversionFactor());
       }
-      deliverables[i] = (BondFixedSecurityDefinition) bondSecurity.accept(_bondConverter);
-      conversionFactor[i] = deliverable.getConversionFactor();
     }
+    final int m = deliverablesList.size();
+    if (m == 0) {
+      throw new OpenGammaRuntimeException("Unable to satisfy a single identifier in the bond future deliverable basket for " + bondFuture.toString());
+    }
+    final BondFixedSecurityDefinition[] deliverables = new BondFixedSecurityDefinition[m];
+    final double[] conversionFactor = new double[m];
+    for (int j = 0; j < m; j++) {
+      deliverables[j] = deliverablesList.get(j);
+      conversionFactor[j] = conversionFactorList.get(j);
+    }
+    
     return new BondFutureDefinition(tradingLastDate, noticeFirstDate, noticeLastDate, notional, deliverables, conversionFactor);
   }
 }
