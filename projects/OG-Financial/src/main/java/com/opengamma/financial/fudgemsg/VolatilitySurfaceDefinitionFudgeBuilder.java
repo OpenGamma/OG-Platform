74b1d5283a96a2b54de09d0ed08d27b858163848
Jim Moores <jim.moores@gmail.com>
2013-11-27 17:44:04 +0000

    PLAT-5223 Fixed fudge builder to create arrays of correct type.

--- projects/OG-Financial/src/main/java/com/opengamma/financial/fudgemsg/VolatilitySurfaceDefinitionFudgeBuilder.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/fudgemsg/VolatilitySurfaceDefinitionFudgeBuilder.java
@@ -5,6 +5,7 @@
  */
 package com.opengamma.financial.fudgemsg;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -72,18 +73,24 @@ public class VolatilitySurfaceDefinitionFudgeBuilder implements FudgeBuilder<Vol
     }
     final String name = message.getString(NAME_FIELD);
     final List<FudgeField> xsFields = message.getAllByName(XS_FIELD);
-    final List<Object> xs = new ArrayList<>();
+    final Object firstX = deserializer.fieldValueToObject(xsFields.get(0));
+    Object[] xs = (Object[]) Array.newInstance(firstX.getClass(), xsFields.size());
+    int i = 0;
     for (final FudgeField xField : xsFields) {
       final Object x = deserializer.fieldValueToObject(xField);
-      xs.add(x);
+      xs[i] = x;
+      i++;
     }
     final List<FudgeField> ysFields = message.getAllByName(YS_FIELD);
-    final List<Object> ys = new ArrayList<>();
+    final Object firstY = deserializer.fieldValueToObject(ysFields.get(0));
+    Object[] ys = (Object[]) Array.newInstance(firstY.getClass(), ysFields.size());
+    int j = 0;
     for (final FudgeField yField : ysFields) {
       final Object y = deserializer.fieldValueToObject(yField);
-      ys.add(y);
+      ys[j] = y;
+      j++;
     }
-    return new VolatilitySurfaceDefinition<>(name, target, xs.toArray(), ys.toArray());
+    return new VolatilitySurfaceDefinition<>(name, target, xs, ys);
   }
 
 }
