70ccc451303cab126042138cdd5e46ba8146678b
Marc Henrard <marc@opengamma.com>
2013-11-29 10:38:02 +0000

    [PLAT-5224] Changed the bondFixed constructor to include the number of coupon per year.

--- projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/instrument/bond/BondFixedTransactionDefinitionTest.java
+++ projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/instrument/bond/BondFixedTransactionDefinitionTest.java
@@ -17,8 +17,10 @@ import com.opengamma.financial.convention.businessday.BusinessDayConvention;
 import com.opengamma.financial.convention.businessday.BusinessDayConventionFactory;
 import com.opengamma.financial.convention.calendar.Calendar;
 import com.opengamma.financial.convention.calendar.MondayToFridayCalendar;
+import com.opengamma.financial.convention.daycount.ActualActualICMA;
 import com.opengamma.financial.convention.daycount.DayCount;
 import com.opengamma.financial.convention.daycount.DayCountFactory;
+import com.opengamma.financial.convention.yield.SimpleYieldConvention;
 import com.opengamma.financial.convention.yield.YieldConvention;
 import com.opengamma.financial.convention.yield.YieldConventionFactory;
 import com.opengamma.util.money.Currency;
@@ -50,7 +52,7 @@ public class BondFixedTransactionDefinitionTest {
   // to derivatives: common
   private static final String CREDIT_CURVE_NAME = "Credit";
   private static final String REPO_CURVE_NAME = "Repo";
-  private static final String[] CURVES_NAME = {CREDIT_CURVE_NAME, REPO_CURVE_NAME};
+  private static final String[] CURVES_NAME = {CREDIT_CURVE_NAME, REPO_CURVE_NAME };
   private static final ZonedDateTime REFERENCE_DATE_1 = DateUtils.getUTCDate(2011, 8, 17);
 
   @Test(expectedExceptions = IllegalArgumentException.class)
@@ -97,4 +99,42 @@ public class BondFixedTransactionDefinitionTest {
     assertEquals("Bond transaction: toDerivative", bondSecurityPurchase.getCouponPerYear(), bondTransaction.getBondTransaction().getCouponPerYear());
     assertEquals("Bond transaction: toDerivative", bondSecurityPurchase.getYieldConvention(), bondTransaction.getBondTransaction().getYieldConvention());
   }
+
+  // MDLZ 5 3/8 12/11/14 - ISIN: XS0417033007 - Check the long first coupon and accrued
+
+  private static final Currency GBP = Currency.GBP;
+
+  private static final ActualActualICMA DAY_COUNT_ACTACTICMA = new ActualActualICMA();
+  private static final BusinessDayConvention FOLLOWING = BusinessDayConventionFactory.INSTANCE.getBusinessDayConvention("Following");
+  private static final String ISSUER_MON = " MONDELEZ INTERNATIONAL";
+  private static final YieldConvention US_STREET = SimpleYieldConvention.US_STREET;
+  private static final int SETTLEMENT_DAYS_MON = 3;
+  private static final Period PAYMENT_TENOR_MON = Period.ofMonths(6);
+  //  private static final int COUPON_PER_YEAR_MON = 2;
+  private static final ZonedDateTime BOND_MATURITY_MON = DateUtils.getUTCDate(2014, 12, 11);
+  private static final ZonedDateTime BOND_START_MON = DateUtils.getUTCDate(2009, 3, 11);
+  private static final ZonedDateTime BOND_FIRSTCPN_MON = DateUtils.getUTCDate(2009, 12, 11);
+  private static final double RATE_MON = 0.05375;
+  private static final BondFixedSecurityDefinition BOND_MON_SECURITY_DEFINITION = BondFixedSecurityDefinition.from(GBP, BOND_START_MON, BOND_FIRSTCPN_MON, BOND_MATURITY_MON, PAYMENT_TENOR_MON,
+      RATE_MON, SETTLEMENT_DAYS_MON, CALENDAR, DAY_COUNT_ACTACTICMA, FOLLOWING, US_STREET, IS_EOM, ISSUER_MON);
+
+  private static final double TOLERANCE_ACCRUED = 1.0E-2;
+
+  @Test
+  public void longFirstCouponMon() {
+    final double notional = 1000000;
+    final ZonedDateTime settle090410 = DateUtils.getUTCDate(2009, 04, 10);
+    final double accrual090410 = 4429.95;
+    BondFixedSecurity bond090410 = BOND_MON_SECURITY_DEFINITION.toDerivative(settle090410, settle090410);
+    assertEquals("Bond transaction: accrued", accrual090410, bond090410.getAccruedInterest() * notional, TOLERANCE_ACCRUED);
+    final ZonedDateTime settle091010 = DateUtils.getUTCDate(2009, 10, 10);
+    final double accrual091010 = 31354.97;
+    BondFixedSecurity bond091010 = BOND_MON_SECURITY_DEFINITION.toDerivative(settle091010, settle091010);
+    assertEquals("Bond transaction: accrued", accrual091010, bond091010.getAccruedInterest() * notional, TOLERANCE_ACCRUED);
+    final ZonedDateTime settle101010 = DateUtils.getUTCDate(2010, 10, 10);
+    final double accrual101010 = 17769.81;
+    BondFixedSecurity bond101010 = BOND_MON_SECURITY_DEFINITION.toDerivative(settle101010, settle101010);
+    assertEquals("Bond transaction: accrued", accrual101010, bond101010.getAccruedInterest() * notional, TOLERANCE_ACCRUED);
+  }
+
 }
