a209ac6332c8f1c0cbb188e9e211ab6ab2fb0a25
Stephen Colebourne <stephen@opengamma.com>
2013-12-05 17:19:32 +0000

    PLAT-5081 - Do not run performance test

--- projects/OG-MasterDB/src/test/java/com/opengamma/masterdb/security/SecuritiesJodaBeanXmlPerformanceTest.java
+++ projects/OG-MasterDB/src/test/java/com/opengamma/masterdb/security/SecuritiesJodaBeanXmlPerformanceTest.java
@@ -5,27 +5,14 @@
  */
 package com.opengamma.masterdb.security;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.fudgemsg.FudgeContext;
-import org.fudgemsg.FudgeMsg;
-import org.fudgemsg.mapping.FudgeDeserializer;
-import org.fudgemsg.mapping.FudgeSerializer;
-import org.fudgemsg.wire.FudgeDataInputStreamReader;
-import org.fudgemsg.wire.FudgeDataOutputStreamWriter;
-import org.fudgemsg.wire.FudgeMsgReader;
-import org.fudgemsg.wire.FudgeMsgWriter;
-import org.joda.beans.ser.JodaBeanSer;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.threeten.bp.Duration;
 
 import com.opengamma.master.security.ManageableSecurity;
-import com.opengamma.util.fudgemsg.OpenGammaFudgeContext;
 import com.opengamma.util.test.TestGroup;
 
 /**
@@ -41,116 +28,112 @@ public class SecuritiesJodaBeanXmlPerformanceTest extends SecurityTestCase {
 
   @AfterClass
   public void tearDown() {
-    System.out.println("Average: " + (((double) total) / number));
+    if (total > 0 && number > 0) {
+      System.out.println("Average: " + (((double) total) / number));
+    }
   }
 
   @Override
   protected <T extends ManageableSecurity> void assertSecurity(Class<T> securityClass, T security) {
-    count = 0;
-    
-    // Joda write
-    String jodaXml = null;
-//    byte[] packedJoda2 = null;
-    for (int i = 0; i < 1000; i++) {
-      jodaXml = JodaBeanSer.PRETTY.xmlWriter().write(security);
-//      packedJoda2 = ZipUtils.deflateString(jodaXml);
-    }
-    if (jodaXml.contains("\"CommodityNotional")) {
-      System.out.println(jodaXml);
-    }
-    long start = System.nanoTime();
-    for (int i = 0; i < 1000; i++) {
-      jodaXml = JodaBeanSer.COMPACT.xmlWriter().write(security);
-//      packedJoda2 = ZipUtils.deflateString(jodaXml);
-      if (jodaXml.length() > 0) {
-        count++;
-      }
-    }
-    long end = System.nanoTime();
-    long diffJodaWrite = (end - start) / 1_000_000;
-    if (jodaXml.contains("\"CommodityNotional")) {
-      System.out.println(jodaXml);
-    }
-    
-    // Joda read
-    Object obj = null;
-    start = System.nanoTime();
-    for (int i = 0; i < 1000; i++) {
-//      String unpacked = ZipUtils.inflateString(packedJoda2);
-      String unpacked = jodaXml;
-      obj = JodaBeanSer.COMPACT.xmlReader().read(unpacked);
-      if (obj instanceof ManageableSecurity) {
-        count++;
-      }
-    }
-    end = System.nanoTime();
-    long diffJodaRead = (end - start) / 1_000_000;
-    total += diffJodaRead;
-    number++;
-    
-    // Fudge write
-    byte[] fudgeBytes = null;
-    start = System.nanoTime();
-    for (int i = 0; i < 1000; i++) {
-      FudgeContext fc = OpenGammaFudgeContext.getInstance();
-      FudgeSerializer serializer = new FudgeSerializer(fc);
-      FudgeMsg msg = serializer.objectToFudgeMsg(security);
-      ByteArrayOutputStream baos = new ByteArrayOutputStream(512);
-      @SuppressWarnings("resource")
-      final FudgeMsgWriter fmwr = new FudgeMsgWriter(new FudgeDataOutputStreamWriter(fc, baos));
-      fmwr.writeMessage(msg);
-      fmwr.flush();
-      try {
-        baos.close();
-      } catch (IOException ex) {
-        ex.printStackTrace();
-      }
-      fudgeBytes = baos.toByteArray();
-      if (fudgeBytes.length > 0) {
-        count++;
-      }
-    }
-    end = System.nanoTime();
-    long diffFudgeWrite = (end - start) / 1_000_000;
-    
-    // Fudge read
-    obj = null;
-    start = System.nanoTime();
-    for (int i = 0; i < 1000; i++) {
-      FudgeContext fc = OpenGammaFudgeContext.getInstance();
-      FudgeMsg msg = null;
-      try (ByteArrayInputStream bais = new ByteArrayInputStream(fudgeBytes)) {
-        final FudgeMsgReader fmr = new FudgeMsgReader(new FudgeDataInputStreamReader(fc, bais));
-        msg = fmr.nextMessage();
-        fmr.close();
-      } catch (IOException ex) {
-        throw new RuntimeException(ex);
-      }
-      FudgeDeserializer deserializer = new FudgeDeserializer(fc);
-      obj = deserializer.fudgeMsgToObject(msg);
-      if (obj instanceof ManageableSecurity) {
-        count++;
-      }
-    }
-    end = System.nanoTime();
-    long diffFudgeRead = (end - start) / 1_000_000;
-    
-//    // Fudge XML write
-//    FudgeContext fc = OpenGammaFudgeContext.getInstance();
-//    FudgeSerializer serializer = new FudgeSerializer(fc);
-//    FudgeMsg msg = serializer.objectToFudgeMsg(security);
-//    StringWriter strWr = new StringWriter();
-//    FudgeXMLStreamWriter xmlStreamWriter = new FudgeXMLStreamWriter(OpenGammaFudgeContext.getInstance(), strWr);
-//    FudgeMsgWriter fudgeMsgWriter = new FudgeMsgWriter(xmlStreamWriter);
-//    fudgeMsgWriter.writeMessage(msg);
-//    fudgeMsgWriter.close();
-//    String fudgeXml = strWr.toString();
-    
-    // output
-    System.out.println(securityClass.getSimpleName() + " JodaWrite:" + diffJodaWrite + " JodaRead:" + diffJodaRead +
-        " FudgeWrite:" + diffFudgeWrite + " FudgeRead:" + diffFudgeRead);
-//    System.out.println(securityClass.getSimpleName() + " JodaXML:" + jodaXml.length() + " JodaXMLZlib:" +
-//        packedJoda2.length + " Fudge:" + fudgeBytes.length + " FudgeXML:" + fudgeXml.length() + (count > 0 ? "" : " no count"));
+//    count = 0;
+//    
+//    // Joda write
+//    String jodaXml = null;
+////    byte[] packedJoda2 = null;
+//    for (int i = 0; i < 1000; i++) {
+//      jodaXml = JodaBeanSer.PRETTY.xmlWriter().write(security);
+////      packedJoda2 = ZipUtils.deflateString(jodaXml);
+//    }
+//    long start = System.nanoTime();
+//    for (int i = 0; i < 1000; i++) {
+//      jodaXml = JodaBeanSer.COMPACT.xmlWriter().write(security);
+////      packedJoda2 = ZipUtils.deflateString(jodaXml);
+//      if (jodaXml.length() > 0) {
+//        count++;
+//      }
+//    }
+//    long end = System.nanoTime();
+//    long diffJodaWrite = (end - start) / 1_000_000;
+//    
+//    // Joda read
+//    Object obj = null;
+//    start = System.nanoTime();
+//    for (int i = 0; i < 1000; i++) {
+////      String unpacked = ZipUtils.inflateString(packedJoda2);
+//      String unpacked = jodaXml;
+//      obj = JodaBeanSer.COMPACT.xmlReader().read(unpacked);
+//      if (obj instanceof ManageableSecurity) {
+//        count++;
+//      }
+//    }
+//    end = System.nanoTime();
+//    long diffJodaRead = (end - start) / 1_000_000;
+//    total += diffJodaRead;
+//    number++;
+//    
+//    // Fudge write
+//    byte[] fudgeBytes = null;
+//    start = System.nanoTime();
+//    for (int i = 0; i < 1000; i++) {
+//      FudgeContext fc = OpenGammaFudgeContext.getInstance();
+//      FudgeSerializer serializer = new FudgeSerializer(fc);
+//      FudgeMsg msg = serializer.objectToFudgeMsg(security);
+//      ByteArrayOutputStream baos = new ByteArrayOutputStream(512);
+//      @SuppressWarnings("resource")
+//      final FudgeMsgWriter fmwr = new FudgeMsgWriter(new FudgeDataOutputStreamWriter(fc, baos));
+//      fmwr.writeMessage(msg);
+//      fmwr.flush();
+//      try {
+//        baos.close();
+//      } catch (IOException ex) {
+//        ex.printStackTrace();
+//      }
+//      fudgeBytes = baos.toByteArray();
+//      if (fudgeBytes.length > 0) {
+//        count++;
+//      }
+//    }
+//    end = System.nanoTime();
+//    long diffFudgeWrite = (end - start) / 1_000_000;
+//    
+//    // Fudge read
+//    obj = null;
+//    start = System.nanoTime();
+//    for (int i = 0; i < 1000; i++) {
+//      FudgeContext fc = OpenGammaFudgeContext.getInstance();
+//      FudgeMsg msg = null;
+//      try (ByteArrayInputStream bais = new ByteArrayInputStream(fudgeBytes)) {
+//        final FudgeMsgReader fmr = new FudgeMsgReader(new FudgeDataInputStreamReader(fc, bais));
+//        msg = fmr.nextMessage();
+//        fmr.close();
+//      } catch (IOException ex) {
+//        throw new RuntimeException(ex);
+//      }
+//      FudgeDeserializer deserializer = new FudgeDeserializer(fc);
+//      obj = deserializer.fudgeMsgToObject(msg);
+//      if (obj instanceof ManageableSecurity) {
+//        count++;
+//      }
+//    }
+//    end = System.nanoTime();
+//    long diffFudgeRead = (end - start) / 1_000_000;
+//    
+////    // Fudge XML write
+////    FudgeContext fc = OpenGammaFudgeContext.getInstance();
+////    FudgeSerializer serializer = new FudgeSerializer(fc);
+////    FudgeMsg msg = serializer.objectToFudgeMsg(security);
+////    StringWriter strWr = new StringWriter();
+////    FudgeXMLStreamWriter xmlStreamWriter = new FudgeXMLStreamWriter(OpenGammaFudgeContext.getInstance(), strWr);
+////    FudgeMsgWriter fudgeMsgWriter = new FudgeMsgWriter(xmlStreamWriter);
+////    fudgeMsgWriter.writeMessage(msg);
+////    fudgeMsgWriter.close();
+////    String fudgeXml = strWr.toString();
+//    
+//    // output
+//    System.out.println(securityClass.getSimpleName() + " JodaWrite:" + diffJodaWrite + " JodaRead:" + diffJodaRead +
+//        " FudgeWrite:" + diffFudgeWrite + " FudgeRead:" + diffFudgeRead);
+////    System.out.println(securityClass.getSimpleName() + " JodaXML:" + jodaXml.length() + " JodaXMLZlib:" +
+////        packedJoda2.length + " Fudge:" + fudgeBytes.length + " FudgeXML:" + fudgeXml.length() + (count > 0 ? "" : " no count"));
   }
 
 }
