6701c62e32551e6e24339bae5e4c4a9dcc9a13c4
Andrew Griffin <andrew@opengamma.com>
2013-12-18 11:44:54 +0000

    [PLAT-5359, XLS-613] Helper methods for working with objects that might have lifecycles

---
+++ projects/OG-Util/src/test/java/com/opengamma/util/spring/LifecycleUtilsTest.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.util.spring;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.mockito.Mockito;
+import org.springframework.context.Lifecycle;
+import org.testng.annotations.Test;
+
+/**
+ * Tests the {@link LifecycleUtils} class
+ */
+@Test
+public class LifecycleUtilsTest {
+
+  public void testStart() {
+    // No-op; not a Lifecycle
+    LifecycleUtils.start("Foo");
+    // Call start on implementing object
+    final Lifecycle mock = Mockito.mock(Lifecycle.class);
+    LifecycleUtils.start(mock);
+    Mockito.verify(mock).start();
+    Mockito.verifyNoMoreInteractions(mock);
+  }
+
+  public void testIsRunning() {
+    // Default return value; not a Lifecycle
+    assertFalse(LifecycleUtils.isRunning("Foo", false));
+    assertTrue(LifecycleUtils.isRunning("Foo", true));
+    // Call start on implementing object
+    final Lifecycle mock = Mockito.mock(Lifecycle.class);
+    Mockito.when(mock.isRunning()).thenReturn(true);
+    assertTrue(LifecycleUtils.isRunning(mock, false));
+  }
+
+  public void testStop() {
+    // No-op; not a Lifecycle
+    LifecycleUtils.stop("Foo");
+    // Call start on implementing object
+    final Lifecycle mock = Mockito.mock(Lifecycle.class);
+    LifecycleUtils.stop(mock);
+    Mockito.verify(mock).stop();
+    Mockito.verifyNoMoreInteractions(mock);
+  }
+
+}
