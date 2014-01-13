9d898d5cb8eeb383577ecaab5af691c90ce9a518
Yuki Iwashita <yuki@opengamma.com>
2014-01-13 16:10:18 +0000

    [PLAT-5526] Added ImpliedVolatilityCalculator for RGW model

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/equity/option/ListedEquityOptionRollGeskeWhaleyImpliedVolFunction.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/equity/option/ListedEquityOptionRollGeskeWhaleyImpliedVolFunction.java
@@ -11,19 +11,9 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.opengamma.OpenGammaRuntimeException;
-import com.opengamma.analytics.financial.equity.EquityOptionBlackPresentValueCalculator;
+import com.opengamma.analytics.financial.equity.EquityOptionRollGeskeWhaleyImpliedVolatilityCalculator;
 import com.opengamma.analytics.financial.equity.StaticReplicationDataBundle;
-import com.opengamma.analytics.financial.equity.option.EquityIndexFutureOption;
-import com.opengamma.analytics.financial.equity.option.EquityIndexOption;
-import com.opengamma.analytics.financial.equity.option.EquityOption;
-import com.opengamma.analytics.financial.equity.variance.pricing.AffineDividends;
 import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
-import com.opengamma.analytics.financial.model.interestrate.curve.ForwardCurve;
-import com.opengamma.analytics.financial.model.interestrate.curve.ForwardCurveAffineDividends;
-import com.opengamma.analytics.financial.model.option.pricing.analytic.BjerksundStenslandModel;
-import com.opengamma.analytics.financial.model.option.pricing.analytic.RollGeskeWhaleyModel;
-import com.opengamma.analytics.financial.model.volatility.BlackFormulaRepository;
 import com.opengamma.core.value.MarketDataRequirementNames;
 import com.opengamma.engine.ComputationTargetSpecification;
 import com.opengamma.engine.function.FunctionInputs;
@@ -32,7 +22,6 @@ import com.opengamma.engine.value.ValueProperties;
 import com.opengamma.engine.value.ValueRequirement;
 import com.opengamma.engine.value.ValueRequirementNames;
 import com.opengamma.engine.value.ValueSpecification;
-import com.opengamma.financial.analytics.model.volatility.surface.black.EquityBlackVolatilitySurfaceFromSinglePriceFunction;
 
 /**
  *
@@ -42,12 +31,10 @@ public class ListedEquityOptionRollGeskeWhaleyImpliedVolFunction extends ListedE
   private static final Logger s_logger = LoggerFactory.getLogger(ListedEquityOptionRollGeskeWhaleyImpliedVolFunction.class);
 
   /**
-   * The Black present value calculator
-   * The model is chosen to be consistent with {@link EquityBlackVolatilitySurfaceFromSinglePriceFunction}
+   * Implied volatility calculator
    */
-  private static final EquityOptionBlackPresentValueCalculator s_pvCalculator = EquityOptionBlackPresentValueCalculator.getInstance();
-//  private static final EqyOptRollGeskeWhaleyPresentValueCalculator s_pvCalculator = EqyOptRollGeskeWhaleyPresentValueCalculator.getInstance();
-
+  private static final EquityOptionRollGeskeWhaleyImpliedVolatilityCalculator s_volCalculator =  EquityOptionRollGeskeWhaleyImpliedVolatilityCalculator.getInstance();
+  
   /** Default constructor */
   public ListedEquityOptionRollGeskeWhaleyImpliedVolFunction() {
     super(ValueRequirementNames.IMPLIED_VOLATILITY);
@@ -56,8 +43,7 @@ public class ListedEquityOptionRollGeskeWhaleyImpliedVolFunction extends ListedE
   @Override
   protected Set<ComputedValue> computeValues(final InstrumentDerivative derivative, final StaticReplicationDataBundle market, final FunctionInputs inputs,
       final Set<ValueRequirement> desiredValues, final ComputationTargetSpecification targetSpec, final ValueProperties resultProperties) {
-
-    // Get market price
+    
     Double marketPrice = null;
     final ComputedValue mktPriceObj = inputs.getComputedValue(MarketDataRequirementNames.MARKET_VALUE);
     if (mktPriceObj == null) {
@@ -65,108 +51,15 @@ public class ListedEquityOptionRollGeskeWhaleyImpliedVolFunction extends ListedE
     } else {
       marketPrice = (Double) mktPriceObj.getValue();
     }
-    // Get details of option for impliedVol Call
-    // If market price is not available. compute from model instead
-    final double optionPrice;
-    final double strike;
-    final double timeToExpiry;
-    final boolean isCall;
-    if (derivative instanceof EquityOption) {
-      final EquityOption option = (EquityOption) derivative;
-      strike = option.getStrike();
-      timeToExpiry = option.getTimeToExpiry();
-      isCall = option.isCall();
-      if (marketPrice == null) {
-        optionPrice = derivative.accept(s_pvCalculator, market) / option.getUnitAmount();
-      } else {
-        optionPrice = marketPrice;
-      }
-    } else if (derivative instanceof EquityIndexOption) {
-      final EquityIndexOption option = (EquityIndexOption) derivative;
-      strike = option.getStrike();
-      timeToExpiry = option.getTimeToExpiry();
-      isCall = option.isCall();
-      if (marketPrice == null) {
-        optionPrice = derivative.accept(s_pvCalculator, market) / option.getUnitAmount();
-      } else {
-        optionPrice = marketPrice;
-      }
-    } else if (derivative instanceof EquityIndexFutureOption) {
-      final EquityIndexFutureOption option = (EquityIndexFutureOption) derivative;
-      strike = option.getStrike();
-      timeToExpiry = option.getExpiry();
-      isCall = option.isCall();
-      if (marketPrice == null) {
-        optionPrice =  derivative.accept(s_pvCalculator, market) / option.getPointValue();
-      } else {
-        optionPrice = marketPrice;
-      }
-
-    } else {
-      throw new OpenGammaRuntimeException("Unexpected InstrumentDerivative type");
-    }
-
-    final double spot = market.getForwardCurve().getSpot();
-    final double discountRate = market.getDiscountCurve().getInterestRate(timeToExpiry);
 
     Double impliedVol = null;
-    if (isCall) {
-      final RollGeskeWhaleyModel model = new RollGeskeWhaleyModel();
-      final ForwardCurve fCurve = market.getForwardCurve();
-      double[] divTime = null;
-      double[] divAmount = null;
-      if (fCurve instanceof ForwardCurveAffineDividends) {
-        final AffineDividends div = ((ForwardCurveAffineDividends) market.getForwardCurve()).getDividends();
-        divTime = div.getTau();
-        divAmount = div.getAlpha();
-      } else {
-        divTime = new double[] {0. };
-        divAmount = new double[] {0. };
-      }
-
-      try {
-        impliedVol = model.impliedVolatility(optionPrice, spot, strike, discountRate, timeToExpiry, divAmount, divTime);
-      } catch (final IllegalArgumentException e) {
-        s_logger.info(MarketDataRequirementNames.IMPLIED_VOLATILITY + " undefined" + targetSpec);
-        impliedVol = market.getVolatilitySurface().getVolatility(timeToExpiry, strike);
-      }
-    } else {
-      final double volatility = market.getVolatilitySurface().getVolatility(timeToExpiry, strike);
-      if (derivative instanceof EquityOption) {
-        final BjerksundStenslandModel model = new BjerksundStenslandModel();
-        double costOfCarry = discountRate;
-        double modSpot = spot;
-
-        final ForwardCurve fCurve = market.getForwardCurve();
-        if (fCurve instanceof ForwardCurveAffineDividends) {
-          final AffineDividends div = ((ForwardCurveAffineDividends) fCurve).getDividends();
-          final int number = div.getNumberOfDividends();
-          int i = 0;
-          while (i < number && div.getTau(i) < timeToExpiry) {
-            modSpot = modSpot * (1. - div.getBeta(i)) - div.getAlpha(i) * market.getDiscountCurve().getDiscountFactor(div.getTau(i));
-            ++i;
-          }
-        } else {
-          costOfCarry = Math.log(fCurve.getForward(timeToExpiry) / spot) / timeToExpiry;
-        }
-
-        try {
-          if (timeToExpiry < 7. / 365.) {
-            final double fwd = optionPrice / market.getDiscountCurve().getDiscountFactor(timeToExpiry);
-            impliedVol = BlackFormulaRepository.impliedVolatility(fwd, fCurve.getForward(timeToExpiry), strike, timeToExpiry, false);
-          } else {
-            impliedVol = model.impliedVolatility(optionPrice, modSpot, strike, discountRate, costOfCarry, timeToExpiry, false, Math.min(volatility * 1.5, 0.2));
-          }
-        } catch (final IllegalArgumentException e) {
-          s_logger.info(MarketDataRequirementNames.IMPLIED_VOLATILITY + " undefined" + targetSpec);
-          impliedVol = volatility;
-        }
-      } else {
-        impliedVol = volatility;
-      }
+    try {
+      impliedVol = s_volCalculator.getRollGeskeWhaleyImpliedVol(derivative, market, marketPrice); 
+    } catch (final IllegalArgumentException e) {
+      s_logger.info(MarketDataRequirementNames.IMPLIED_VOLATILITY + " undefined" + targetSpec);
+      impliedVol = derivative.accept(s_volCalculator, market);
     }
-
-
+ 
     final ValueSpecification resultSpec = new ValueSpecification(getValueRequirementNames()[0], targetSpec, resultProperties);
     return Collections.singleton(new ComputedValue(resultSpec, impliedVol));
   }
