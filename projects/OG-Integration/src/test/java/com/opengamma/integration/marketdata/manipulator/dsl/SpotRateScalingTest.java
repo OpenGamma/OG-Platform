25ed833d48c2f040a7fe480637a75a31e526d8fc
Chris Kent <cjkent@hotmail.com>
2014-01-25 11:45:54 +0000

    PLAT-5698 - Fix tests

--- projects/OG-Integration/src/test/java/com/opengamma/integration/marketdata/manipulator/dsl/SpotRateScalingTest.java
+++ projects/OG-Integration/src/test/java/com/opengamma/integration/marketdata/manipulator/dsl/SpotRateScalingTest.java
@@ -24,8 +24,14 @@ import com.opengamma.util.test.TestGroup;
 public class SpotRateScalingTest {
 
   private static final double DELTA = 0.00000001;
-  private static final SpotRateScaling SCALING = new SpotRateScaling(2d, ImmutableSet.of(CurrencyPair.parse("EUR/USD"),
+
+  /* Scaling of 0.1 means +10%, i.e. multiply by 1.1 */
+  private static final SpotRateScaling UP_10 = new SpotRateScaling(0.1d, ImmutableSet.of(CurrencyPair.parse("EUR/USD"),
                                                                                          CurrencyPair.parse("CHF/JPY")));
+
+  /* Scaling of -0.2 means -20%, i.e. multiply by 0.8 */
+  private static final SpotRateScaling DOWN_20 = new SpotRateScaling(-0.2, ImmutableSet.of(CurrencyPair.parse("EUR/USD"),
+                                                                                           CurrencyPair.parse("CHF/JPY")));
 
   private static ValueSpecification valueSpec(String currencyPairStr) {
     ValueProperties properties = ValueProperties.with(ValuePropertyNames.FUNCTION, "foo").get();
@@ -36,14 +42,18 @@ public class SpotRateScalingTest {
 
   @Test
   public void normalPair() {
-    assertEquals(8d, SCALING.execute(4d, valueSpec("EUR/USD"), new FunctionExecutionContext()), DELTA);
-    assertEquals(10d, SCALING.execute(5d, valueSpec("CHF/JPY"), new FunctionExecutionContext()), DELTA);
+    assertEquals(4.4, UP_10.execute(4.0, valueSpec("EUR/USD"), new FunctionExecutionContext()), DELTA);
+    assertEquals(5.5, UP_10.execute(5.0, valueSpec("CHF/JPY"), new FunctionExecutionContext()), DELTA);
+    assertEquals(3.2, DOWN_20.execute(4.0, valueSpec("EUR/USD"), new FunctionExecutionContext()), DELTA);
+    assertEquals(4.0, DOWN_20.execute(5.0, valueSpec("CHF/JPY"), new FunctionExecutionContext()), DELTA);
   }
 
   @Test
   public void inversePair() {
-    assertEquals(2d, SCALING.execute(4d, valueSpec("USD/EUR"), new FunctionExecutionContext()), DELTA);
-    assertEquals(3d, SCALING.execute(6d, valueSpec("JPY/CHF"), new FunctionExecutionContext()), DELTA);
+    assertEquals(0.22727272727272727, UP_10.execute(0.25, valueSpec("USD/EUR"), new FunctionExecutionContext()), DELTA);
+    assertEquals(0.18181818181818182, UP_10.execute(0.2, valueSpec("JPY/CHF"), new FunctionExecutionContext()), DELTA);
+    assertEquals(0.3125, DOWN_20.execute(0.25, valueSpec("USD/EUR"), new FunctionExecutionContext()), DELTA);
+    assertEquals(0.25, DOWN_20.execute(0.2, valueSpec("JPY/CHF"), new FunctionExecutionContext()), DELTA);
   }
 
   @Test(expectedExceptions = IllegalArgumentException.class)
@@ -52,11 +62,11 @@ public class SpotRateScalingTest {
     ComputationTargetSpecification targetSpec = new ComputationTargetSpecification(ComputationTargetType.CURRENCY,
                                                                                    Currency.GBP.getUniqueId());
     ValueSpecification valueSpec = new ValueSpecification("SpotRate", targetSpec, properties);
-    SCALING.execute(2d, valueSpec, new FunctionExecutionContext());
+    UP_10.execute(2d, valueSpec, new FunctionExecutionContext());
   }
 
   @Test(expectedExceptions = IllegalArgumentException.class)
   public void unexpectedCurrencyPair() {
-    SCALING.execute(2d, valueSpec("GBP/USD"), new FunctionExecutionContext());
+    UP_10.execute(2d, valueSpec("GBP/USD"), new FunctionExecutionContext());
   }
 }
