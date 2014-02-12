0462eeacca62aac73f3e5a1df452346187b00e93
Stephen Colebourne <stephen@opengamma.com>
2014-02-12 17:09:09 +0000

    PLAT-5000 - Enable JDK 8 compatibility
    Remove Map interface from Map2
    Fix Mockito issue
    Reviewed-by: cjkent
    
    Conflicts:
    	RELEASE-NOTES.md

--- projects/OG-Util/src/test/java/com/opengamma/util/map/HashMap2Test.java
+++ projects/OG-Util/src/test/java/com/opengamma/util/map/HashMap2Test.java
@@ -12,7 +12,6 @@ import static org.testng.Assert.assertTrue;
 import org.testng.annotations.Test;
 
 import com.opengamma.util.test.TestGroup;
-import com.opengamma.util.tuple.ObjectsPair;
 
 /**
  * Tests the {@link HashMap2} implementation.
@@ -23,24 +22,18 @@ public class HashMap2Test {
   private void testBasicOperations(final Map2<String, String, String> map) {
     assertTrue(map.isEmpty());
     assertEquals(map.size(), 0);
-    assertEquals(map.put(ObjectsPair.of("A", "B"), "Foo"), null);
     assertEquals(map.put("B", "A", "Bar"), null);
+    assertEquals(map.put("A", "B", "Foo"), null);
     assertFalse(map.isEmpty());
     assertEquals(map.size(), 2);
-    assertEquals(map.get(ObjectsPair.of("A", "B")), "Foo");
-    assertEquals(map.get(ObjectsPair.of("B", "A")), "Bar");
     assertEquals(map.get("A", "B"), "Foo");
     assertEquals(map.get("B", "A"), "Bar");
     assertEquals(map.get("X", "Y"), null);
-    assertEquals(map.get(ObjectsPair.of("X", "Y")), null);
-    assertTrue(map.containsKey(ObjectsPair.of("B", "A")));
     assertTrue(map.containsKey("A", "B"));
     assertFalse(map.containsKey("X", "Y"));
-    assertFalse(map.containsKey(ObjectsPair.of("X", "Y")));
     map.clear();
     assertTrue(map.isEmpty());
     assertEquals(map.size(), 0);
-    assertFalse(map.containsKey(ObjectsPair.of("B", "A")));
     assertFalse(map.containsKey("A", "B"));
   }
 
@@ -53,12 +46,7 @@ public class HashMap2Test {
   }
 
   private void testRemove(final Map2<String, String, String> map) {
-    map.put(ObjectsPair.of("A", "B"), "Foo");
-    map.put("B", "A", "Bar");
-    assertEquals(map.remove(ObjectsPair.of("B", "A")), "Bar");
-    assertEquals(map.size(), 1);
-    assertEquals(map.remove(ObjectsPair.of("B", "A")), null);
-    assertEquals(map.remove(ObjectsPair.of("X", "Y")), null);
+    map.put("A", "B", "Foo");
     assertEquals(map.size(), 1);
     assertEquals(map.remove("A", "B"), "Foo");
     assertEquals(map.size(), 0);
@@ -75,8 +63,8 @@ public class HashMap2Test {
 
   private void testPutIfAbsent(final Map2<String, String, String> map) {
     assertEquals(map.put("A", "B", "Foo"), null);
-    assertEquals(map.put(ObjectsPair.of("B", "A"), "Bar"), null);
-    assertEquals(map.put(ObjectsPair.of("A", "B"), "Cow"), "Foo");
+    assertEquals(map.put("B", "A", "Bar"), null);
+    assertEquals(map.put("A", "B", "Cow"), "Foo");
     assertEquals(map.put("B", "A", "Dog"), "Bar");
     assertEquals(map.putIfAbsent("A", "B", "Foo"), "Cow");
     assertEquals(map.putIfAbsent("B", "A", "Bar"), "Dog");
