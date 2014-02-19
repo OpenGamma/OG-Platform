18f58ae3a5aa155f2cdd1118bf803874f384f2c4
Daniel Kwiecinski <daniel@opengamma.com>
2014-02-19 11:05:28 +0000

    [PLAT-5918] Create tool for upgrading/creating appropriate schema version for examples

---
+++ projects/OG-Component/src/main/java/com/opengamma/component/tool/DbUpgradeOrCreateTool.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.component.tool;
+
+import java.io.File;
+
+import com.opengamma.scripts.Scriptable;
+import com.opengamma.util.db.tool.DbToolContext;
+import com.opengamma.util.db.tool.DbUpgradeOperation;
+
+/**
+ * Tool for upgrading database objects to the latest version using the installation scripts.
+ */
+@Scriptable
+public class DbUpgradeOrCreateTool extends AbstractDbTool<DbToolContext> {
+
+  //-------------------------------------------------------------------------
+  @Override
+  protected void doRun(boolean write, File outputFile) throws Exception {
+    DbUpgradeOperation upgradeOp = new DbUpgradeOperation(getDbToolContext(), write, outputFile);
+    upgradeOp.execute();
+    if (!upgradeOp.isUpgradeRequired()) {
+      System.out.println("Database up-to-date");
+    }
+  }
+  
+  //-------------------------------------------------------------------------
+  /**
+   * Main method to run the tool.
+   * 
+   * @param args  the arguments, not null
+   */
+  public static void main(String[] args) { //CSIGNORE
+    boolean success = new DbUpgradeOrCreateTool().initAndRun(args, DbToolContext.class);
+    System.exit(success ? 0 : 1);
+  }
+
+}
