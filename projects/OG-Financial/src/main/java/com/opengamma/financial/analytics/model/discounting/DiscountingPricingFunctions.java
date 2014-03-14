8651857bf9a51baa4eefba5475db1bcec2a9d3b1
stuartw <stuart.wakefield@gmail.com>
2014-03-14 11:24:28 +0000

    [PLAT-6164] Add DiscountingInterpolatedParRateFunction

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/discounting/DiscountingPricingFunctions.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/discounting/DiscountingPricingFunctions.java
@@ -42,6 +42,7 @@ public class DiscountingPricingFunctions extends AbstractFunctionConfigurationBe
     functions.add(functionConfiguration(DiscountingInterpolatedPVFunction.class));
     functions.add(functionConfiguration(DiscountingInterpolatedAllPV01Function.class));
     functions.add(functionConfiguration(DiscountingInterpolatedPV01Function.class));
+    functions.add(functionConfiguration(DiscountingInterpolatedParRateFunction.class));
 
     functions.add(functionConfiguration(DiscountingInflationBCSFunction.class));
     functions.add(functionConfiguration(DiscountingInflationPVFunction.class));
