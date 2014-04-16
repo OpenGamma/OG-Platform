11c4a1be281613f1c647d5253f4fe540bce39e12
Elaine McLeod <emcleod@elaine-lx-2.hq.opengamma.com>
2014-04-16 13:09:43 +0100

    [PLAT-6395] Correcting error in code that gets the currency of a bill

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/curve/CurveNodeCurrencyVisitor.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/curve/CurveNodeCurrencyVisitor.java
@@ -141,7 +141,7 @@ public class CurveNodeCurrencyVisitor implements CurveNodeVisitor<Set<Currency>>
       throw new OpenGammaRuntimeException("Config source was null");
     }
     final CurveNodeIdMapper idMapper = _configSource.getSingle(CurveNodeIdMapper.class, node.getCurveNodeIdMapperName(), VersionCorrection.LATEST);
-    final Security security = _securitySource.getSingle(idMapper.getBondNodeId(null, node.getMaturityTenor()).toBundle()); // curve date is not relevant for bonds
+    final Security security = _securitySource.getSingle(idMapper.getBillNodeId(null, node.getMaturityTenor()).toBundle()); // curve date is not relevant for bills
     if (security instanceof BillSecurity) {
       return Collections.singleton(((BillSecurity) security).getCurrency());
     }
