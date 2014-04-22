1d34def6cc853ec5dabd766aac1abf748491255f
Yomi Ayodele <yomi@opengamma.com>
2014-04-22 14:10:07 +0100

    [PLAT-6404] Add unit test to check permissions on BloombergBpipePermissionCheckProvider Reviewed-By:Stephen

---
+++ projects/OG-Bloomberg/src/test/java/com/opengamma/bbg/permission/BloombergBpipePermissionCheckProviderTest.java
@@ -0,0 +1,110 @@
+/**
+ * Copyright (C) 2009 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.bbg.permission;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertTrue;
+
+import java.util.Map;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.opengamma.bbg.BloombergConnector;
+import com.opengamma.bbg.test.BloombergTestUtils;
+import com.opengamma.core.id.ExternalSchemes;
+import com.opengamma.provider.permission.PermissionCheckProviderRequest;
+import com.opengamma.provider.permission.PermissionCheckProviderResult;
+import com.opengamma.util.test.TestGroup;
+
+/**
+ * Test.
+ */
+@Test(groups = TestGroup.INTEGRATION)
+public class BloombergBpipePermissionCheckProviderTest {
+
+  private BloombergBpipePermissionCheckProvider _provider;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    BloombergConnector connector = BloombergTestUtils.getBloombergBipeConnector();
+    BloombergBpipePermissionCheckProvider provider = new BloombergBpipePermissionCheckProvider(connector);
+    
+    provider.start();
+    _provider = provider;
+  }
+
+  @AfterMethod
+  public void tearDown() throws Exception {
+    if (_provider != null) {
+      _provider.stop();
+    }
+    _provider = null;
+  }
+
+  //-------------------------------------------------------------------------
+  @Test(enabled = false)
+  public void isPermitted() {
+    PermissionCheckProviderRequest request = PermissionCheckProviderRequest.createGet(ExternalSchemes.bloombergEMRSUserId("og:yomi"), "10.0.2.91",
+        "27749", "35009", "39491", "40066", "41095", "46707", "1234");
+
+    PermissionCheckProviderResult resultHolder = _provider.isPermitted(request);
+    assertNotNull(resultHolder);
+    assertNotNull(resultHolder.getPermissionCheckResult());
+    Map<String, Boolean> checkPermissionResult = resultHolder.getPermissionCheckResult();
+    assertEquals(7, checkPermissionResult.size());
+    assertTrue(checkPermissionResult.get("27749"));
+    assertTrue(checkPermissionResult.get("35009"));
+    assertTrue(checkPermissionResult.get("39491"));
+    assertTrue(checkPermissionResult.get("40066"));
+    assertTrue(checkPermissionResult.get("41095"));
+    assertTrue(checkPermissionResult.get("46707"));
+    assertFalse(checkPermissionResult.get("1234"));
+  }
+
+  @Test(enabled = false)
+  public void notPermittedAfterEntitlementRevoked() {
+    PermissionCheckProviderRequest request = PermissionCheckProviderRequest.createGet(ExternalSchemes.bloombergEMRSUserId("og:yomi"), "10.0.2.91",
+        "27749", "35009", "39491", "40066", "41095", "46707", "1234");
+
+    PermissionCheckProviderResult resultHolder = _provider.isPermitted(request);
+    assertNotNull(resultHolder);
+    assertNotNull(resultHolder.getPermissionCheckResult());
+    Map<String, Boolean> checkPermissionResult = resultHolder.getPermissionCheckResult();
+    assertEquals(7, checkPermissionResult.size());
+    assertTrue(checkPermissionResult.get("27749"));
+    assertTrue(checkPermissionResult.get("35009"));
+    assertTrue(checkPermissionResult.get("39491"));
+    assertTrue(checkPermissionResult.get("40066"));
+    assertTrue(checkPermissionResult.get("41095"));
+    assertTrue(checkPermissionResult.get("46707"));
+    assertFalse(checkPermissionResult.get("1234"));
+
+    //sleep for a bit to allow bloomberg logon on another PC
+    try {
+      Thread.sleep(120000);
+    } catch (InterruptedException ex) {
+      ex.printStackTrace();
+    }
+
+    resultHolder = _provider.isPermitted(request);
+    assertNotNull(resultHolder);
+    assertNotNull(resultHolder.getPermissionCheckResult());
+    checkPermissionResult = resultHolder.getPermissionCheckResult();
+    assertEquals(7, checkPermissionResult.size());
+    assertFalse(checkPermissionResult.get("27749"));
+    assertFalse(checkPermissionResult.get("35009"));
+    assertFalse(checkPermissionResult.get("39491"));
+    assertFalse(checkPermissionResult.get("40066"));
+    assertFalse(checkPermissionResult.get("41095"));
+    assertFalse(checkPermissionResult.get("46707"));
+    assertFalse(checkPermissionResult.get("1234"));
+  }
+
+}
