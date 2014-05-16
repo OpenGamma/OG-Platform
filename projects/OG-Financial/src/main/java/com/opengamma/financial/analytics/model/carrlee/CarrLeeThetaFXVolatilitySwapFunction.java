71ce4cd94869db5ef0330a94d788112c0ad95fe7
Yuki Iwashita <yuki@opengamma.com>
2014-05-16 19:00:31 +0100

    [PLAT-6509] Integration of Carr-Lee Greek calculators
    Reviewed by Vince.

---
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/carrlee/CarrLeeThetaFXVolatilitySwapFunction.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.financial.analytics.model.carrlee;
+
+import static com.opengamma.engine.value.ValueRequirementNames.THETA;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.threeten.bp.Instant;
+
+import com.google.common.collect.Iterables;
+import com.opengamma.analytics.financial.forex.method.FXMatrix;
+import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
+import com.opengamma.analytics.financial.interestrate.InstrumentDerivativeVisitor;
+import com.opengamma.analytics.financial.provider.description.volatilityswap.CarrLeeFXData;
+import com.opengamma.analytics.financial.volatilityswap.CarrLeeFXVolatilitySwapVegaCalculator;
+import com.opengamma.engine.ComputationTarget;
+import com.opengamma.engine.function.CompiledFunctionDefinition;
+import com.opengamma.engine.function.FunctionCompilationContext;
+import com.opengamma.engine.function.FunctionExecutionContext;
+import com.opengamma.engine.function.FunctionInputs;
+import com.opengamma.engine.value.ComputedValue;
+import com.opengamma.engine.value.ValueProperties;
+import com.opengamma.engine.value.ValueRequirement;
+import com.opengamma.engine.value.ValueRequirementNames;
+import com.opengamma.engine.value.ValueSpecification;
+
+/**
+ * 
+ */
+public class CarrLeeThetaFXVolatilitySwapFunction extends CarrLeeFXVolatilitySwapFunction {
+
+  /** The theta calculator */
+  private static final InstrumentDerivativeVisitor<CarrLeeFXData, Double> CALCULATOR = new CarrLeeFXVolatilitySwapVegaCalculator();
+
+  /**
+   * Sets the value requirement to {@link ValueRequirementNames#THETA}.
+   */
+  public CarrLeeThetaFXVolatilitySwapFunction() {
+    super(THETA);
+  }
+
+  @Override
+  public CompiledFunctionDefinition compile(final FunctionCompilationContext context, final Instant atInstant) {
+    return new CarrLeeFXVolatilitySwapCompiledFunction(getTargetToDefinitionConverter(context), getDefinitionToDerivativeConverter(context), false) {
+
+      @SuppressWarnings("synthetic-access")
+      @Override
+      protected Set<ComputedValue> getValues(final FunctionExecutionContext executionContext, final FunctionInputs inputs, final ComputationTarget target,
+          final Set<ValueRequirement> desiredValues, final InstrumentDerivative derivative, final FXMatrix fxMatrix) {
+        final CarrLeeFXData data = getCarrLeeData(executionContext, inputs, target, fxMatrix);
+        final Double theta = derivative.accept(CALCULATOR, data);
+        final ValueProperties properties = Iterables.getOnlyElement(desiredValues).getConstraints().copy().get();
+        final ValueSpecification spec = new ValueSpecification(THETA, target.toSpecification(), properties);
+        return Collections.singleton(new ComputedValue(spec, theta));
+      }
+
+    };
+  }
+}
