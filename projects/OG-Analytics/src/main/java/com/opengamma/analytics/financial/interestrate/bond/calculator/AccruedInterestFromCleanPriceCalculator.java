b319c71a9ae235feb36d65f506d924d3dcd2872b
Vince Lam <vincent@opengamma.com>
2014-06-04 13:58:10 +0100

    [PLAT-6554] Fix scaling accrued interest to 100 par value and whether to return the nominal or real amount

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/interestrate/bond/calculator/AccruedInterestFromCleanPriceCalculator.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/interestrate/bond/calculator/AccruedInterestFromCleanPriceCalculator.java
@@ -5,10 +5,13 @@
  */
 package com.opengamma.analytics.financial.interestrate.bond.calculator;
 
+import static com.opengamma.financial.convention.yield.SimpleYieldConvention.INDEX_LINKED_FLOAT;
+
 import com.opengamma.analytics.financial.interestrate.InstrumentDerivativeVisitorAdapter;
 import com.opengamma.analytics.financial.interestrate.bond.definition.BondCapitalIndexedSecurity;
 import com.opengamma.analytics.financial.interestrate.bond.definition.BondCapitalIndexedTransaction;
 import com.opengamma.analytics.financial.interestrate.bond.definition.BondFixedSecurity;
+import com.opengamma.analytics.financial.interestrate.bond.definition.BondFixedTransaction;
 import com.opengamma.analytics.financial.interestrate.bond.provider.BondCapitalIndexedSecurityDiscountingMethod;
 import com.opengamma.analytics.financial.interestrate.bond.provider.BondSecurityDiscountingMethod;
 import com.opengamma.util.ArgumentChecker;
@@ -38,24 +41,47 @@ public final class AccruedInterestFromCleanPriceCalculator extends InstrumentDer
   }
 
   /**
-   * The method used for different instruments.
+   * The method used for discounting the bond cashflows.
    */
   private static final BondSecurityDiscountingMethod METHOD_BOND_SECURITY = BondSecurityDiscountingMethod.getInstance();
+  /**
+   * The method used for discounting the inflation bond cashflows.
+   */
   private static final BondCapitalIndexedSecurityDiscountingMethod METHOD_INFLATION_BOND_SECURITY = BondCapitalIndexedSecurityDiscountingMethod.getInstance();
 
   @Override
-  public Double visitBondFixedSecurity(final BondFixedSecurity bond, final Double price) {
-    return METHOD_BOND_SECURITY.accruedInterestFromCleanPrice(bond, price);
+  public Double visitBondFixedSecurity(final BondFixedSecurity bond, final Double cleanPrice) {
+    ArgumentChecker.notNull(bond, "bond");
+    ArgumentChecker.notNull(cleanPrice, "cleanPrice");
+    return METHOD_BOND_SECURITY.accruedInterestFromCleanPrice(bond, cleanPrice) * 100;
+  }
+  
+  @Override
+  public Double visitBondFixedTransaction(BondFixedTransaction bond, Double cleanPrice) {
+    return visitBondFixedSecurity(bond.getBondTransaction(), cleanPrice);
+  }
+  
+  @Override
+  public Double visitBondCapitalIndexedSecurity(BondCapitalIndexedSecurity<?> bond, Double cleanRealPrice) {
+    ArgumentChecker.notNull(bond, "bond");
+    ArgumentChecker.notNull(cleanRealPrice, "yield");
+    double accruedInterest = METHOD_INFLATION_BOND_SECURITY.accruedInterestFromCleanRealPrice(bond, cleanRealPrice) * 100;
+    if (bond.getYieldConvention().equals(INDEX_LINKED_FLOAT)) {
+      return accruedInterest * bond.getIndexRatio();
+    } else {
+      return accruedInterest;
+    }
   }
 
   @Override
-  public Double visitBondCapitalIndexedTransaction(final BondCapitalIndexedTransaction bond, final Double cleanPrice) {
+  public Double visitBondCapitalIndexedTransaction(final BondCapitalIndexedTransaction<?> bond, final Double cleanPrice) {
     ArgumentChecker.notNull(bond, "bond");
     ArgumentChecker.notNull(cleanPrice, "yield");
-    ArgumentChecker.notNull(bond.getBondStandard() instanceof BondCapitalIndexedSecurity<?>, "the bond should be a BondCapitalIndexedSecurity");
-
+    if (!(bond.getBondStandard() instanceof BondCapitalIndexedSecurity<?>)) {
+      throw new IllegalArgumentException("Bond should be a BondCapitalIndexedSecurity");
+    }
     final BondCapitalIndexedSecurity<?> bondSecurity = (BondCapitalIndexedSecurity<?>) bond.getBondStandard();
-    return METHOD_INFLATION_BOND_SECURITY.accruedInterestFromCleanRealPrice(bondSecurity, cleanPrice);
+    return visitBondCapitalIndexedSecurity(bondSecurity, cleanPrice);
   }
 
 }
