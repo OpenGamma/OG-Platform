5d384534f4e54cde8485f7c8a6e3f58fd1bd65f7
stuartw <stuart.wakefield@opengamma.com>
2014-06-06 12:03:47 +0100

    PLAT-6568 Remove superceeded function

--- projects/OG-Financial/src/main/java/com/opengamma/financial/convention/FloatingIndexFactory.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/convention/FloatingIndexFactory.java
@@ -5,8 +5,6 @@
  */
 package com.opengamma.financial.convention;
 
-import java.util.Iterator;
-
 import org.joda.convert.FromString;
 
 /**
@@ -48,18 +46,6 @@ public final class FloatingIndexFactory extends AbstractNamedInstanceFactory<Flo
       // add alternative name that replaces - with _ (for backwards compatibility)
       addInstance(index, index.getIsdaName().replace("-", "_"));
     }
-  }
-
-  /**
-   * Iterates over the available conventions. No particular ordering is specified and conventions may
-   * exist in the system not provided by this factory that aren't included as part of this enumeration.
-   *
-   * @return the available conventions, not null
-   * @deprecated use {@link #instanceMap()}
-   */
-  @Deprecated
-  public Iterator<FloatingIndex> enumerateAvailableDayCounts() {
-    return instanceMap().values().iterator();
   }
 
 }
