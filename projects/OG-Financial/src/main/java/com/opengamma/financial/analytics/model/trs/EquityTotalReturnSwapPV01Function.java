9cf57204e992addeabd0cf9e8f879d46c501addc
Yuki Iwashita <yuki@opengamma.com>
2014-06-09 16:18:49 +0100

    [PLAT-6585] PV01 calculator replaced

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/trs/EquityTotalReturnSwapPV01Function.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/model/trs/EquityTotalReturnSwapPV01Function.java
@@ -23,13 +23,10 @@ import org.threeten.bp.Instant;
 
 import com.google.common.collect.Iterables;
 import com.opengamma.analytics.financial.equity.EquityTrsDataBundle;
-import com.opengamma.analytics.financial.equity.trs.EquityTotalReturnSwap;
 import com.opengamma.analytics.financial.forex.method.FXMatrix;
 import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
-import com.opengamma.analytics.financial.interestrate.InstrumentDerivativeVisitor;
 import com.opengamma.analytics.financial.provider.calculator.discounting.PV01CurveParametersCalculator;
-import com.opengamma.analytics.financial.provider.calculator.discounting.PresentValueCurveSensitivityDiscountingCalculator;
-import com.opengamma.analytics.financial.provider.description.interestrate.MulticurveProviderInterface;
+import com.opengamma.analytics.financial.provider.calculator.equity.PresentValueCurveSensitivityEquityDiscountingCalculator;
 import com.opengamma.analytics.util.amount.ReferenceAmount;
 import com.opengamma.engine.ComputationTarget;
 import com.opengamma.engine.function.CompiledFunctionDefinition;
@@ -49,9 +46,9 @@ import com.opengamma.util.tuple.Pair;
  */
 public class EquityTotalReturnSwapPV01Function extends EquityTotalReturnSwapFunction {
   /** The calculator */
-  private static final InstrumentDerivativeVisitor<MulticurveProviderInterface, ReferenceAmount<Pair<String, Currency>>> CALCULATOR =
-      new PV01CurveParametersCalculator<>(PresentValueCurveSensitivityDiscountingCalculator.getInstance());
-
+  private static final PV01CurveParametersCalculator<EquityTrsDataBundle> CALCULATOR =
+      new PV01CurveParametersCalculator<>(PresentValueCurveSensitivityEquityDiscountingCalculator.getInstance());
+      
   /**
    * Sets the value requirement to {@link ValueRequirementNames#PV01}.
    */
@@ -70,7 +67,7 @@ public class EquityTotalReturnSwapPV01Function extends EquityTotalReturnSwapFunc
         final ValueProperties properties = Iterables.getOnlyElement(desiredValues).getConstraints().copy().get();
         final EquityTrsDataBundle data = getDataBundle(inputs, fxMatrix);
         final String desiredCurveName = properties.getStrictValue(CURVE);
-        final ReferenceAmount<Pair<String, Currency>> pv01 = ((EquityTotalReturnSwap) derivative).getFundingLeg().accept(CALCULATOR, data.getCurves());
+        final ReferenceAmount<Pair<String, Currency>> pv01 = derivative.accept(CALCULATOR, data);
         final Set<ComputedValue> results = new HashSet<>();
         boolean curveNameFound = false;
         for (final Map.Entry<Pair<String, Currency>, Double> entry : pv01.getMap().entrySet()) {
