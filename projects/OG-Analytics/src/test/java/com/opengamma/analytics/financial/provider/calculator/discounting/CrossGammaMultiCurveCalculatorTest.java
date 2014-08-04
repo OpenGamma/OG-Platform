1613432bff2efd62fa3ab290c233f3e8638a1c8a
Marc Henrard <marc@opengamma.com>
2014-08-04 13:12:27 +0100

    [PLAT-420] Added cross-gamma cross-curves method in cross-gamma calculator.

--- projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/provider/calculator/discounting/CrossGammaMultiCurveCalculatorTest.java
+++ projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/provider/calculator/discounting/CrossGammaMultiCurveCalculatorTest.java
@@ -47,7 +47,7 @@ public class CrossGammaMultiCurveCalculatorTest {
 
   private static final Calendar TYO = new MondayToFridayCalendar("TYO");
   private static final ZonedDateTime CALIBRATION_DATE = DateUtils.getUTCDate(2014, 8, 2);
-  
+
   private static final GeneratorSwapFixedIborMaster GENERATOR_IRS_MASTER = GeneratorSwapFixedIborMaster.getInstance();
   private static final GeneratorSwapIborIborMaster GENERATOR_BS_MASTER = GeneratorSwapIborIborMaster.getInstance();
   /** Instrument description */
@@ -55,12 +55,12 @@ public class CrossGammaMultiCurveCalculatorTest {
   private static final GeneratorSwapIborIbor JPYLIBOR3MLIBOR6M = GENERATOR_BS_MASTER.getGenerator("JPYLIBOR3MLIBOR6M", TYO);
   private static final IborIndex JPYLIBOR6M = JPY6MLIBOR6M.getIborIndex();
   private static final Period SWAP_TENOR = Period.ofYears(10);
-  private static final ZonedDateTime SETTLEMENT_DATE = 
+  private static final ZonedDateTime SETTLEMENT_DATE =
       ScheduleCalculator.getAdjustedDate(CALIBRATION_DATE, JPYLIBOR6M.getSpotLag(), TYO);
   private static final double NOTIONAL = 100000000; //100m
   private static final double RATE_FIXED = 0.025;
   private static final double SPREAD_BS = 0.0005;
-  private static final SwapFixedIborDefinition SWAP_FIXED_IBOR_DEFINITION = 
+  private static final SwapFixedIborDefinition SWAP_FIXED_IBOR_DEFINITION =
       SwapFixedIborDefinition.from(SETTLEMENT_DATE, SWAP_TENOR, JPY6MLIBOR6M, NOTIONAL, RATE_FIXED, true);
   private static final SwapIborIborDefinition SWAP_IBOR_IBOR_DEFINITION =
       JPYLIBOR3MLIBOR6M.generateInstrument(CALIBRATION_DATE, SPREAD_BS, NOTIONAL, new GeneratorAttributeIR(SWAP_TENOR));
@@ -68,33 +68,65 @@ public class CrossGammaMultiCurveCalculatorTest {
   private static final PresentValueDiscountingCalculator PVDC = PresentValueDiscountingCalculator.getInstance();
   private static final PresentValueCurveSensitivityDiscountingCalculator PVCSDC = PresentValueCurveSensitivityDiscountingCalculator.getInstance();
   private static final CrossGammaMultiCurveCalculator CGC = new CrossGammaMultiCurveCalculator(PVCSDC);
+  /** Curves */
+  private static final Pair<MulticurveProviderDiscount, CurveBuildingBlockBundle> MULTICURVE_PAIR = AnalysisMarketDataJPYSets.getMulticurveJPY();
+  private static final MulticurveProviderDiscount MULTICURVE = MULTICURVE_PAIR.getFirst();
   /** Constants */
   private static final double SHIFT = 1.0E-4;
   private static final double TOLERANCE_PV_GAMMA = 2.0E+0;
   private static final double TOLERANCE_PV_GAMMA_RELATIF = 6.0E-4;
+
+  @SuppressWarnings("unused")
+  @Test
+  public void crossGammaCrossCurveIrs() {
+    final SwapFixedCoupon<Coupon> swap = SWAP_FIXED_IBOR_DEFINITION.toDerivative(CALIBRATION_DATE);
+    HashMap<String, DoubleMatrix2D> gammaIntraCurve = CGC.calculateCrossGammaIntraCurve(swap, MULTICURVE);
+    DoubleMatrix2D gammaCrossCurve = CGC.calculateCrossGammaCrossCurve(swap, MULTICURVE);
+    // Checking that overlap is coherent
+    Set<String> names = MULTICURVE.getAllNames();
+    int submatrixsize = 0;
+    for (String name : names) {
+      DoubleMatrix2D gamma = gammaIntraCurve.get(name);
+      YieldAndDiscountCurve curve = MULTICURVE.getCurve(name);
+      ArgumentChecker.isTrue(curve instanceof YieldCurve, "curve should be YieldCurve");
+      YieldCurve yieldCurve = (YieldCurve) curve;
+      ArgumentChecker.isTrue(yieldCurve.getCurve() instanceof InterpolatedDoublesCurve, "Yield curve should be based on InterpolatedDoublesCurve");
+      InterpolatedDoublesCurve interpolatedCurve = (InterpolatedDoublesCurve) yieldCurve.getCurve();
+      int nbNode = interpolatedCurve.getXDataAsPrimitive().length;
+      for (int i = 0; i < nbNode; i++) {
+        for (int j = 0; j < nbNode; j++) {
+          double ic = gamma.getEntry(i, j);
+          double cc = gammaCrossCurve.getEntry(submatrixsize + i, submatrixsize + j);
+          assertTrue("CrossGammaMultiCurveCalculator - cross-gamma cross-curves - " + i + " - " + j + " / " + ic + " - " + cc,
+              (Math.abs(ic / cc - 1) < TOLERANCE_PV_GAMMA_RELATIF) || // If relative difference is small enough
+                  (Math.abs(ic - cc) < TOLERANCE_PV_GAMMA)); // If absolute difference is small enough
+        }
+      }
+      submatrixsize += nbNode;
+    }
+    int t = 0;
+  }
 
   @Test
   public void crossGammaIntraCurveIrs() {
     final SwapFixedCoupon<Coupon> swap = SWAP_FIXED_IBOR_DEFINITION.toDerivative(CALIBRATION_DATE);
     crossGammaIntraCurve(swap);
   }
-  
+
   @Test
   public void crossGammaIntraCurveBs() {
     final Swap<Coupon, Coupon> swap = SWAP_IBOR_IBOR_DEFINITION.toDerivative(CALIBRATION_DATE);
     crossGammaIntraCurve(swap);
   }
- 
-  private void crossGammaIntraCurve(Swap<?,?> swap) {
-    Pair<MulticurveProviderDiscount, CurveBuildingBlockBundle> multicurvePair = AnalysisMarketDataJPYSets.getMulticurveJPY();
-    MulticurveProviderDiscount multicurve = multicurvePair.getFirst();
-    HashMap<String, DoubleMatrix2D> gammaMap = CGC.calculateCrossGammaIntraCurve(swap, multicurve);
-    Set<String> names = multicurve.getAllNames();
-    for(String name: names) { // Start curves
-      Set<Currency> ccys = multicurve.getCurrencies();
+
+  private void crossGammaIntraCurve(Swap<?, ?> swap) {
+    HashMap<String, DoubleMatrix2D> gammaMap = CGC.calculateCrossGammaIntraCurve(swap, MULTICURVE);
+    Set<String> names = MULTICURVE.getAllNames();
+    for (String name : names) { // Start curves
+      Set<Currency> ccys = MULTICURVE.getCurrencies();
       ArgumentChecker.isTrue(ccys.size() == 1, "only one currency allowed for multi-curve gamma");
       Currency ccy = ccys.iterator().next();
-      YieldAndDiscountCurve curve = multicurve.getCurve(name);
+      YieldAndDiscountCurve curve = MULTICURVE.getCurve(name);
       ArgumentChecker.isTrue(curve instanceof YieldCurve, "curve should be YieldCurve");
       YieldCurve yieldCurve = (YieldCurve) curve;
       ArgumentChecker.isTrue(yieldCurve.getCurve() instanceof InterpolatedDoublesCurve, "Yield curve should be based on InterpolatedDoublesCurve");
@@ -115,18 +147,18 @@ public class CrossGammaMultiCurveCalculatorTest {
               final YieldAndDiscountCurve curveBumped = new YieldCurve(name,
                   new InterpolatedDoublesCurve(x, yieldBumpedPP, interpolatedCurve.getInterpolator(), true));
               MulticurveProviderDiscount providerBumped = new MulticurveProviderDiscount();
-              for (Currency loopccy : multicurve.getCurrencies()) {
-                if (loopccy.equals(multicurve.getCurrencyForName(name))) {
+              for (Currency loopccy : MULTICURVE.getCurrencies()) {
+                if (loopccy.equals(MULTICURVE.getCurrencyForName(name))) {
                   providerBumped.setCurve(loopccy, curveBumped);
                 } else {
-                  providerBumped.setCurve(loopccy, multicurve.getCurve(loopccy));
+                  providerBumped.setCurve(loopccy, MULTICURVE.getCurve(loopccy));
                 }
               }
-              for (IborIndex loopibor : multicurve.getIndexesIbor()) {
-                if (loopibor.equals(multicurve.getIborIndexForName(name))) {
+              for (IborIndex loopibor : MULTICURVE.getIndexesIbor()) {
+                if (loopibor.equals(MULTICURVE.getIborIndexForName(name))) {
                   providerBumped.setCurve(loopibor, curveBumped);
                 } else {
-                  providerBumped.setCurve(loopibor, multicurve.getCurve(loopibor));
+                  providerBumped.setCurve(loopibor, MULTICURVE.getCurve(loopibor));
                 }
               }
               pv[pmi][pmj] = swap.accept(PVDC, providerBumped).getAmount(ccy);
@@ -146,5 +178,5 @@ public class CrossGammaMultiCurveCalculatorTest {
       } // End assert
     } // End curves
   }
-  
+
 }
