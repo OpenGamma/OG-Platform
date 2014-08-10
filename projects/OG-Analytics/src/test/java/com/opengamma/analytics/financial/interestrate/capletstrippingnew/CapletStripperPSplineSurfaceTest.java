3878e381cb6ee0780cb62524826c04bb06d33f55
Richard White <richard@opengamma.com>
2014-08-10 20:38:50 +0100

    Can't get PSpline caplet stripping to work

--- projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/interestrate/capletstrippingnew/CapletStripperPSplineSurfaceTest.java
+++ projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/interestrate/capletstrippingnew/CapletStripperPSplineSurfaceTest.java
@@ -6,7 +6,9 @@ import org.testng.annotations.Test;
 
 import com.opengamma.analytics.financial.interestrate.capletstripping.BasisSplineVolatilitySurfaceProvider;
 import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurface;
+import com.opengamma.analytics.math.FunctionUtils;
 import com.opengamma.analytics.math.interpolation.PenaltyMatrixGenerator;
+import com.opengamma.analytics.math.matrix.AssertMatrix;
 import com.opengamma.analytics.math.matrix.DoubleMatrix1D;
 import com.opengamma.analytics.math.matrix.DoubleMatrix2D;
 import com.opengamma.analytics.math.matrix.MatrixAlgebra;
@@ -40,47 +42,103 @@ public class CapletStripperPSplineSurfaceTest extends SingleStrikeSetup {
 
   @Test
   public void debugTest() {
-    final int tSize = 10;
-    final int kSize = 10;
-    final int tDegree = 2;
-    final int kDegree = 2;
-    final double tLambda = 1;
+    final MultiCapFloorPricer pricer = new MultiCapFloorPricer(getAllCaps(), getYieldCurves());
+  
+    final int tSize = 3;
+    final int kSize = 3;
+    final int tDegree = 1;
+    final int kDegree = 1;
+    final double kMax = 0.12;
+    final double tMax = 10.0;
+    final int tKnots = tSize + tDegree - 1;
+    final int kKnots = kSize + kDegree - 1;
+    final double tLambda = 100;
     final double kLambda = 100;
 
-    final VolatilitySurfaceProvider vsp = new BasisSplineVolatilitySurfaceProvider(0.0, 0.12, kSize, kDegree, 0.0, 10.0, tSize, tDegree);
+    final VolatilitySurfaceProvider vsp = new BasisSplineVolatilitySurfaceProvider(0.0, kMax, kSize, kDegree, 0.0, tMax, tSize, tDegree);
     final int n = vsp.getNumModelParameters();
     final DoubleMatrix1D x = new DoubleMatrix1D(n);
-    Arrays.fill(x.getData(), 0, 11, 1.0);
-    Arrays.fill(x.getData(), 11, 22, 0.5);
-    //  x.getData()[80] = 1.0;
-    final VolatilitySurface vs = vsp.getVolSurface(x);
+    for (int i = 0; i < kKnots; i++) {
+      double k = i / (kKnots - 1.0);
+    for (int j = 0; j < tKnots; j++) {
+        double t = j / (tKnots - 1.0);
+        double w =  0.7;// 0.2 + 0.4 * k + 0.7 * t + t * k + 0.1*t*t;
+        x.getData()[i * tKnots + j] = w;
+      }
+//    x.getData()[i * tKnots + 0] = 1.0;
+//    x.getData()[i * tKnots + 5] = 1.0;
+//    x.getData()[i * tKnots + 8] = 1.0;
+    }
     final int nStrikePoints = 101;
     final int nExpPoints = 101;
     final double[] strikes = new double[nStrikePoints];
     final double[] times = new double[nExpPoints];
-
-    final double arse = vs.getVolatility(7.8, 0.05);
-
+    VolatilitySurface vs = vsp.getVolSurface(x);
+    
     for (int i = 0; i < nStrikePoints; i++) {
-      strikes[i] = 0.0 + 0.12 * i / (nStrikePoints - 1.0);
+      strikes[i] = -0.05 + 0.22 * i / (nStrikePoints - 1.0);
     }
-    //    System.out.println();
-    //    for (int j = 0; j < nExpPoints; j++) {
-    //      times[j] = 0 + 10.0 * j / (nExpPoints - 1.0);
-    //      System.out.print("\t" + times[j]);
-    //    }
-    //
-    //    for (int i = 0; i < nStrikePoints; i++) {
-    //      System.out.print("\n" + strikes[i]);
-    //      for (int j = 0; j < nExpPoints; j++) {
-    //        final Double vol = vs.getVolatility(times[j], strikes[i]);
-    //        System.out.print("\t" + vol);
-    //      }
-    //    }
-    //    System.out.println();
+//    System.out.println();
+//    for (int j = 0; j < nExpPoints; j++) {
+//      times[j] = -5 + 20.0 * j / (nExpPoints - 1.0);
+//      System.out.print("\t" + times[j]);
+//    }
+//
+//    for (int i = 0; i < nStrikePoints; i++) {
+//      System.out.print("\n" + strikes[i]);
+//      for (int j = 0; j < nExpPoints; j++) {
+//        final Double vol = vs.getVolatility(times[j], strikes[i]);
+//        System.out.print("\t" + vol);
+//      }
+//    }
+//    System.out.println("\n");
 
-    final DoubleMatrix2D penalty = PenaltyMatrixGenerator.getPenaltyMatrix(new int[] {tSize + tDegree - 1, kSize + tDegree - 1 }, new int[] {2, 2 }, new double[] {tLambda, kLambda });
-    final double p = MA.getInnerProduct(x, MA.multiply(penalty, x));
-    System.out.println(penalty);
+    
+    DiscreteVolatilityFunctionProvider dvfp = new DiscreateVolatilityFunctionProviderFromVolSurface(vsp);
+    DiscreteVolatilityFunction func = dvfp.from(pricer.getExpiryStrikeArray());
+    System.out.println(func.evaluate(x));
+    DoubleMatrix2D jac = func.evaluateJacobian(x);
+    DoubleMatrix2D jacFD = func.evaluateJacobianViaFD(x);
+    AssertMatrix.assertEqualsMatrix(jac, jacFD, 1e-9);
+    
+
+    final DoubleMatrix2D penalty = PenaltyMatrixGenerator.getPenaltyMatrix(new int[] {kKnots, tKnots }, new int[] {1, 1 }, new double[] {kLambda, tLambda });
+  //  final double p = MA.getInnerProduct(x, MA.multiply(penalty, x));
+//   System.out.println(penalty);
+ //   System.out.println(p);
+    CapletStrippingImp imp = new CapletStrippingImp(pricer, dvfp);
+    final double[] vols = getAllCapVols();
+    final int nVols = vols.length;
+    final double[] errors = new double[nVols];
+    Arrays.fill(errors, 1e-4); //1bps
+    CapletStrippingResult res = imp.solveForCapVols(vols, errors, x, penalty, new PositiveOrZero());
+    System.out.println(res);
+    
+    DoubleMatrix1D xFit = res.getFitParameters();
+    final double p = MA.getInnerProduct(xFit, MA.multiply(penalty, xFit));
+    System.out.println(p);
+    
+
+
+  
+vs = vsp.getVolSurface(xFit);
+  for (int i = 0; i < nStrikePoints; i++) {
+    strikes[i] = 0.0 + 0.12 * i / (nStrikePoints - 1.0);
+  }
+  System.out.println();
+  for (int j = 0; j < nExpPoints; j++) {
+    times[j] = 0 + 10.0 * j / (nExpPoints - 1.0);
+    System.out.print("\t" + times[j]);
+  }
+
+  for (int i = 0; i < nStrikePoints; i++) {
+    System.out.print("\n" + strikes[i]);
+    for (int j = 0; j < nExpPoints; j++) {
+      final Double vol = vs.getVolatility(times[j], strikes[i]);
+      System.out.print("\t" + vol);
+    }
+  }
+  System.out.println("\n");
+
   }
 }
