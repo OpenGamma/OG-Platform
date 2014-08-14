6bfb9d127ba40acf32e59901c4db302e60cfa256
Vince Lam <vincent@opengamma.com>
2014-08-14 12:57:32 +0100

    PLAT-6692 Added missing coupon types to CouponTenorVisitor

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/interestrate/CouponTenorVisitor.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/interestrate/CouponTenorVisitor.java
@@ -7,16 +7,29 @@ package com.opengamma.analytics.financial.interestrate;
 
 import com.opengamma.analytics.financial.instrument.InstrumentDefinitionVisitor;
 import com.opengamma.analytics.financial.instrument.InstrumentDefinitionVisitorAdapter;
+import com.opengamma.analytics.financial.instrument.payment.CouponFixedAccruedCompoundingDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponFixedCompoundingDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponFixedDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborAverageFixingDatesCompoundingDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborAverageFixingDatesCompoundingFlatSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborAverageFixingDatesDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborAverageIndexDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborCompoundingDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborCompoundingFlatSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborCompoundingSimpleSpreadDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborCompoundingSpreadDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborGearingDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborRatchetDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponONArithmeticAverageDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponONArithmeticAverageSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponONArithmeticAverageSpreadSimplifiedDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponONCompoundedDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponONDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponONSimplifiedDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponONSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponONSpreadSimplifiedDefinition;
 import com.opengamma.util.time.Tenor;
 
 /**
@@ -58,9 +71,49 @@ public final class CouponTenorVisitor extends InstrumentDefinitionVisitorAdapter
   public Tenor visitCouponIborCompoundingFlatSpreadDefinition(final CouponIborCompoundingFlatSpreadDefinition definition) {
     return Tenor.of(definition.getIndex().getTenor());
   }
+  
+  @Override
+  public Tenor visitCouponIborCompoundingSimpleSpreadDefinition(CouponIborCompoundingSimpleSpreadDefinition payment) {
+    return Tenor.of(payment.getIndex().getTenor());
+  }
+  
+  @Override
+  public Tenor visitCouponIborRatchetDefinition(CouponIborRatchetDefinition payment) {
+    return Tenor.of(payment.getIndex().getTenor());
+  }
+  
+  @Override
+  public Tenor visitCouponIborAverageDefinition(CouponIborAverageIndexDefinition payment) {
+    return null;
+  }
+  
+  @Override
+  public Tenor visitCouponIborAverageCompoundingDefinition(CouponIborAverageFixingDatesCompoundingDefinition payment) {
+    return Tenor.of(payment.getIndex().getTenor());
+  }
+  
+  @Override
+  public Tenor visitCouponIborAverageFixingDatesDefinition(CouponIborAverageFixingDatesDefinition payment) {
+    return Tenor.of(payment.getIndex().getTenor());
+  }
+  
+  @Override
+  public Tenor visitCouponIborAverageFlatCompoundingSpreadDefinition(CouponIborAverageFixingDatesCompoundingFlatSpreadDefinition payment) {
+    return Tenor.of(payment.getIndex().getTenor());
+  }
 
   @Override
   public Tenor visitCouponOISDefinition(final CouponONDefinition definition) {
+    return Tenor.ON;
+  }
+  
+  @Override
+  public Tenor visitCouponOISSimplifiedDefinition(CouponONSimplifiedDefinition payment) {
+    return Tenor.ON;
+  }
+  
+  @Override
+  public Tenor visitCouponArithmeticAverageONDefinition(CouponONArithmeticAverageDefinition payment) {
     return Tenor.ON;
   }
 
@@ -68,9 +121,24 @@ public final class CouponTenorVisitor extends InstrumentDefinitionVisitorAdapter
   public Tenor visitCouponArithmeticAverageONSpreadDefinition(final CouponONArithmeticAverageSpreadDefinition definition) {
     return Tenor.ON;
   }
+  
+  @Override
+  public Tenor visitCouponArithmeticAverageONSpreadSimplifiedDefinition(CouponONArithmeticAverageSpreadSimplifiedDefinition payment) {
+    return Tenor.ON;
+  }
 
   @Override
   public Tenor visitCouponONSpreadDefinition(final CouponONSpreadDefinition definition) {
+    return Tenor.ON;
+  }
+  
+  @Override
+  public Tenor visitCouponONSpreadSimplifiedDefinition(CouponONSpreadSimplifiedDefinition payment) {
+    return Tenor.ON;
+  }
+  
+  @Override
+  public Tenor visitCouponONCompoundedDefinition(CouponONCompoundedDefinition payment) {
     return Tenor.ON;
   }
 
@@ -82,5 +150,15 @@ public final class CouponTenorVisitor extends InstrumentDefinitionVisitorAdapter
   @Override
   public Tenor visitCouponIborCompoundingSpreadDefinition(CouponIborCompoundingSpreadDefinition payment) {
     return Tenor.of(payment.getIndex().getTenor());
+  }
+  
+  @Override
+  public Tenor visitCouponFixedAccruedCompoundingDefinition(CouponFixedAccruedCompoundingDefinition payment) {
+    return null;
+  }
+  
+  @Override
+  public Tenor visitCouponFixedCompoundingDefinition(CouponFixedCompoundingDefinition payment) {
+    return null;
   }
 }
