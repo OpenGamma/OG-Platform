3df3886d6b52d6cee0e3c7ad644a4976445e5a91
Vince Lam <vincent@opengamma.com>
2014-08-14 16:51:11 +0100

    PLAT-6692 Renamed expandIndexTenors to formatIndexTenors

--- projects/OG-Web/src/main/java/com/opengamma/web/analytics/formatting/FloatingSwapLegDetailsFormatter.java
+++ projects/OG-Web/src/main/java/com/opengamma/web/analytics/formatting/FloatingSwapLegDetailsFormatter.java
@@ -120,7 +120,7 @@ import com.opengamma.util.time.Tenor;
       values[i][13] = _caFormatter.formatCell(value.getNotionals()[i], valueSpec, null);
       values[i][14] = _basisPointFormatter.formatCell(value.getSpreads()[i], valueSpec, null);
       values[i][15] = value.getGearings()[i];
-      values[i][16] = expandTenors(value.getIndexTenors()[i]);
+      values[i][16] = formatIndexTenors(value.getIndexTenors()[i]);
       values[i][17] = value.getDiscountedPaymentAmounts()[i] == null ? "-" : _caFormatter.formatCell(value.getDiscountedPaymentAmounts()[i], valueSpec, null);
       values[i][18] = value.getDiscountedProjectedAmounts()[i] == null ? "-" : _caFormatter.formatCell(value.getDiscountedProjectedAmounts()[i], valueSpec, null);
     }
@@ -136,7 +136,7 @@ import com.opengamma.util.time.Tenor;
    * @param indexTenors the index tenors to format.
    * @return the formatted index tenors.
    */
-  private String expandTenors(Set<Tenor> indexTenors) {
+  private String formatIndexTenors(Set<Tenor> indexTenors) {
     if (indexTenors == null || indexTenors.isEmpty()) {
       return "-";
     } else {
