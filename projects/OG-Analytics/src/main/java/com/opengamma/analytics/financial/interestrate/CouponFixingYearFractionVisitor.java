a13a871c3f76c5d36e9dfd3a9db616e9ea062024
Vince Lam <vincent@opengamma.com>
2014-08-20 21:09:17 +0100

    PLAT-6692 Added missing coupon visitors, removed duplicated visitors

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/interestrate/CouponFixingYearFractionVisitor.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/interestrate/CouponFixingYearFractionVisitor.java
@@ -7,13 +7,19 @@ package com.opengamma.analytics.financial.interestrate;
 
 import com.opengamma.analytics.financial.instrument.InstrumentDefinitionVisitorAdapter;
 import com.opengamma.analytics.financial.instrument.payment.CouponFixedDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborAverageFixingDatesCompoundingDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborAverageIndexDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborCompoundingDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborCompoundingFlatSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborCompoundingSimpleSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponIborCompoundingSpreadDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborGearingDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborRatchetDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponIborSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponONArithmeticAverageDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponONArithmeticAverageSpreadDefinition;
+import com.opengamma.analytics.financial.instrument.payment.CouponONArithmeticAverageSpreadSimplifiedDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponONDefinition;
 
 /**
@@ -81,5 +87,41 @@ public class CouponFixingYearFractionVisitor extends InstrumentDefinitionVisitor
   public Double visitCouponFixedDefinition(final CouponFixedDefinition payment) {
     return null;
   }
+  
+  @Override
+  public Double visitCouponIborAverageDefinition(CouponIborAverageIndexDefinition payment) {
+    return payment.getFixingPeriodAccrualFactor1();
+  }
+  
+  @Override
+  public Double visitCouponIborCompoundingSimpleSpreadDefinition(CouponIborCompoundingSimpleSpreadDefinition payment) {
+    double total = 0.0;
+    for (double fraction : payment.getFixingSubperiodAccrualFactors()) {
+      total += fraction;
+    }
+    return total;
+  }
 
+  @Override
+  public Double visitCouponIborCompoundingSpreadDefinition(CouponIborCompoundingSpreadDefinition payment) {
+    double total = 0.0;
+    for (double fraction : payment.getFixingPeriodAccrualFactors()) {
+      total += fraction;
+    }
+    return total;
+  }
+  
+  @Override
+  public Double visitCouponArithmeticAverageONDefinition(CouponONArithmeticAverageDefinition payment) {
+    double total = 0.0;
+    for (double fraction : payment.getFixingPeriodAccrualFactors()) {
+      total += fraction;
+    }
+    return total;
+  }
+  
+  @Override
+  public Double visitCouponArithmeticAverageONSpreadSimplifiedDefinition(CouponONArithmeticAverageSpreadSimplifiedDefinition payment) {
+    return null;
+  }
 }
