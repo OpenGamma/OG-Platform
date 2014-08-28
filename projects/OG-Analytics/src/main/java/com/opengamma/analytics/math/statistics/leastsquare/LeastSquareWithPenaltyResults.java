d469daf03a2c4302bbc0bbdb7b93dc8d8d14237f
Richard White <richard@opengamma.com>
2014-08-28 18:11:30 +0100

    [PLAT-6640] remove final

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/statistics/leastsquare/LeastSquareWithPenaltyResults.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/statistics/leastsquare/LeastSquareWithPenaltyResults.java
@@ -9,17 +9,19 @@ import com.opengamma.analytics.math.matrix.DoubleMatrix1D;
 import com.opengamma.analytics.math.matrix.DoubleMatrix2D;
 
 /**
- * 
+ * Hold for results of {@link NonLinearLeastSquareWithPenalty}
  */
 public class LeastSquareWithPenaltyResults extends LeastSquareResults {
   private final double _penalty;
 
-  public LeastSquareWithPenaltyResults(final double chiSq, final double penalty, final DoubleMatrix1D parameters, final DoubleMatrix2D covariance) {
+  public LeastSquareWithPenaltyResults(double chiSq, double penalty, DoubleMatrix1D parameters,
+      DoubleMatrix2D covariance) {
     super(chiSq, parameters, covariance);
     _penalty = penalty;
   }
 
-  public LeastSquareWithPenaltyResults(final double chiSq, final double penalty, final DoubleMatrix1D parameters, final DoubleMatrix2D covariance, final DoubleMatrix2D inverseJacobian) {
+  public LeastSquareWithPenaltyResults(double chiSq, double penalty, DoubleMatrix1D parameters,
+      DoubleMatrix2D covariance, DoubleMatrix2D inverseJacobian) {
     super(chiSq, parameters, covariance, inverseJacobian);
     _penalty = penalty;
   }
