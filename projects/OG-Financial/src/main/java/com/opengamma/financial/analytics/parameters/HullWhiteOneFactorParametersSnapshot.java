d5b3a7ed66caeee57046e766efeb154402537ed0
Julian Howarth <julian@opengamma.com>
2014-09-10 15:44:50 +0100

    REQS-147 - Futures convexity adjustment calculation
    Enhance basic data structures to support convexity
    adjustment during curve construction

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/parameters/HullWhiteOneFactorParametersSnapshot.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/parameters/HullWhiteOneFactorParametersSnapshot.java
@@ -22,6 +22,8 @@ import org.joda.beans.impl.direct.DirectMetaProperty;
 import org.joda.beans.impl.direct.DirectMetaPropertyMap;
 
 import com.opengamma.core.marketdatasnapshot.NamedSnapshot;
+import com.opengamma.financial.convention.businessday.BusinessDayConvention;
+import com.opengamma.id.ExternalId;
 import com.opengamma.id.UniqueId;
 import com.opengamma.util.money.Currency;
 
@@ -55,6 +57,15 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
    */
   @PropertyDefinition
   private final double _meanReversion;
+
+  @PropertyDefinition(validate = "notNull")
+  private final BusinessDayConvention _businessDayConvention;
+
+  @PropertyDefinition
+  private final boolean _endOfMonth;
+
+  @PropertyDefinition(validate = "notNull")
+  private final ExternalId _region;
 
   /**
    * Term structure of volatilities to be used.
@@ -96,11 +107,16 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
   protected HullWhiteOneFactorParametersSnapshot(HullWhiteOneFactorParametersSnapshot.Builder builder) {
     JodaBeanUtils.notNull(builder._name, "name");
     JodaBeanUtils.notNull(builder._currency, "currency");
+    JodaBeanUtils.notNull(builder._businessDayConvention, "businessDayConvention");
+    JodaBeanUtils.notNull(builder._region, "region");
     JodaBeanUtils.notNull(builder._volatilityTable, "volatilityTable");
     this._uniqueId = builder._uniqueId;
     this._name = builder._name;
     this._currency = builder._currency;
     this._meanReversion = builder._meanReversion;
+    this._businessDayConvention = builder._businessDayConvention;
+    this._endOfMonth = builder._endOfMonth;
+    this._region = builder._region;
     this._volatilityTable = builder._volatilityTable;
   }
 
@@ -160,6 +176,33 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
 
   //-----------------------------------------------------------------------
   /**
+   * Gets the businessDayConvention.
+   * @return the value of the property, not null
+   */
+  public BusinessDayConvention getBusinessDayConvention() {
+    return _businessDayConvention;
+  }
+
+  //-----------------------------------------------------------------------
+  /**
+   * Gets the endOfMonth.
+   * @return the value of the property
+   */
+  public boolean isEndOfMonth() {
+    return _endOfMonth;
+  }
+
+  //-----------------------------------------------------------------------
+  /**
+   * Gets the region.
+   * @return the value of the property, not null
+   */
+  public ExternalId getRegion() {
+    return _region;
+  }
+
+  //-----------------------------------------------------------------------
+  /**
    * Gets term structure of volatilities to be used.
    * @return the value of the property, not null
    */
@@ -187,6 +230,9 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
           JodaBeanUtils.equal(getName(), other.getName()) &&
           JodaBeanUtils.equal(getCurrency(), other.getCurrency()) &&
           JodaBeanUtils.equal(getMeanReversion(), other.getMeanReversion()) &&
+          JodaBeanUtils.equal(getBusinessDayConvention(), other.getBusinessDayConvention()) &&
+          (isEndOfMonth() == other.isEndOfMonth()) &&
+          JodaBeanUtils.equal(getRegion(), other.getRegion()) &&
           JodaBeanUtils.equal(getVolatilityTable(), other.getVolatilityTable());
     }
     return false;
@@ -199,13 +245,16 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
     hash += hash * 31 + JodaBeanUtils.hashCode(getName());
     hash += hash * 31 + JodaBeanUtils.hashCode(getCurrency());
     hash += hash * 31 + JodaBeanUtils.hashCode(getMeanReversion());
+    hash += hash * 31 + JodaBeanUtils.hashCode(getBusinessDayConvention());
+    hash += hash * 31 + JodaBeanUtils.hashCode(isEndOfMonth());
+    hash += hash * 31 + JodaBeanUtils.hashCode(getRegion());
     hash += hash * 31 + JodaBeanUtils.hashCode(getVolatilityTable());
     return hash;
   }
 
   @Override
   public String toString() {
-    StringBuilder buf = new StringBuilder(192);
+    StringBuilder buf = new StringBuilder(288);
     buf.append("HullWhiteOneFactorParametersSnapshot{");
     int len = buf.length();
     toString(buf);
@@ -221,6 +270,9 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
     buf.append("name").append('=').append(JodaBeanUtils.toString(getName())).append(',').append(' ');
     buf.append("currency").append('=').append(JodaBeanUtils.toString(getCurrency())).append(',').append(' ');
     buf.append("meanReversion").append('=').append(JodaBeanUtils.toString(getMeanReversion())).append(',').append(' ');
+    buf.append("businessDayConvention").append('=').append(JodaBeanUtils.toString(getBusinessDayConvention())).append(',').append(' ');
+    buf.append("endOfMonth").append('=').append(JodaBeanUtils.toString(isEndOfMonth())).append(',').append(' ');
+    buf.append("region").append('=').append(JodaBeanUtils.toString(getRegion())).append(',').append(' ');
     buf.append("volatilityTable").append('=').append(JodaBeanUtils.toString(getVolatilityTable())).append(',').append(' ');
   }
 
@@ -255,6 +307,21 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
     private final MetaProperty<Double> _meanReversion = DirectMetaProperty.ofImmutable(
         this, "meanReversion", HullWhiteOneFactorParametersSnapshot.class, Double.TYPE);
     /**
+     * The meta-property for the {@code businessDayConvention} property.
+     */
+    private final MetaProperty<BusinessDayConvention> _businessDayConvention = DirectMetaProperty.ofImmutable(
+        this, "businessDayConvention", HullWhiteOneFactorParametersSnapshot.class, BusinessDayConvention.class);
+    /**
+     * The meta-property for the {@code endOfMonth} property.
+     */
+    private final MetaProperty<Boolean> _endOfMonth = DirectMetaProperty.ofImmutable(
+        this, "endOfMonth", HullWhiteOneFactorParametersSnapshot.class, Boolean.TYPE);
+    /**
+     * The meta-property for the {@code region} property.
+     */
+    private final MetaProperty<ExternalId> _region = DirectMetaProperty.ofImmutable(
+        this, "region", HullWhiteOneFactorParametersSnapshot.class, ExternalId.class);
+    /**
      * The meta-property for the {@code volatilityTable} property.
      */
     private final MetaProperty<HullWhiteOneFactorVolatilityTable> _volatilityTable = DirectMetaProperty.ofImmutable(
@@ -268,6 +335,9 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
         "name",
         "currency",
         "meanReversion",
+        "businessDayConvention",
+        "endOfMonth",
+        "region",
         "volatilityTable");
 
     /**
@@ -287,6 +357,12 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
           return _currency;
         case -2016560896:  // meanReversion
           return _meanReversion;
+        case -1002835891:  // businessDayConvention
+          return _businessDayConvention;
+        case -1344100978:  // endOfMonth
+          return _endOfMonth;
+        case -934795532:  // region
+          return _region;
         case -2122778967:  // volatilityTable
           return _volatilityTable;
       }
@@ -342,6 +418,30 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
     }
 
     /**
+     * The meta-property for the {@code businessDayConvention} property.
+     * @return the meta-property, not null
+     */
+    public final MetaProperty<BusinessDayConvention> businessDayConvention() {
+      return _businessDayConvention;
+    }
+
+    /**
+     * The meta-property for the {@code endOfMonth} property.
+     * @return the meta-property, not null
+     */
+    public final MetaProperty<Boolean> endOfMonth() {
+      return _endOfMonth;
+    }
+
+    /**
+     * The meta-property for the {@code region} property.
+     * @return the meta-property, not null
+     */
+    public final MetaProperty<ExternalId> region() {
+      return _region;
+    }
+
+    /**
      * The meta-property for the {@code volatilityTable} property.
      * @return the meta-property, not null
      */
@@ -361,6 +461,12 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
           return ((HullWhiteOneFactorParametersSnapshot) bean).getCurrency();
         case -2016560896:  // meanReversion
           return ((HullWhiteOneFactorParametersSnapshot) bean).getMeanReversion();
+        case -1002835891:  // businessDayConvention
+          return ((HullWhiteOneFactorParametersSnapshot) bean).getBusinessDayConvention();
+        case -1344100978:  // endOfMonth
+          return ((HullWhiteOneFactorParametersSnapshot) bean).isEndOfMonth();
+        case -934795532:  // region
+          return ((HullWhiteOneFactorParametersSnapshot) bean).getRegion();
         case -2122778967:  // volatilityTable
           return ((HullWhiteOneFactorParametersSnapshot) bean).getVolatilityTable();
       }
@@ -388,6 +494,9 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
     private String _name;
     private Currency _currency;
     private double _meanReversion;
+    private BusinessDayConvention _businessDayConvention;
+    private boolean _endOfMonth;
+    private ExternalId _region;
     private HullWhiteOneFactorVolatilityTable _volatilityTable;
 
     /**
@@ -405,6 +514,9 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
       this._name = beanToCopy.getName();
       this._currency = beanToCopy.getCurrency();
       this._meanReversion = beanToCopy.getMeanReversion();
+      this._businessDayConvention = beanToCopy.getBusinessDayConvention();
+      this._endOfMonth = beanToCopy.isEndOfMonth();
+      this._region = beanToCopy.getRegion();
       this._volatilityTable = beanToCopy.getVolatilityTable();
     }
 
@@ -420,6 +532,12 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
           return _currency;
         case -2016560896:  // meanReversion
           return _meanReversion;
+        case -1002835891:  // businessDayConvention
+          return _businessDayConvention;
+        case -1344100978:  // endOfMonth
+          return _endOfMonth;
+        case -934795532:  // region
+          return _region;
         case -2122778967:  // volatilityTable
           return _volatilityTable;
         default:
@@ -441,6 +559,15 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
           break;
         case -2016560896:  // meanReversion
           this._meanReversion = (Double) newValue;
+          break;
+        case -1002835891:  // businessDayConvention
+          this._businessDayConvention = (BusinessDayConvention) newValue;
+          break;
+        case -1344100978:  // endOfMonth
+          this._endOfMonth = (Boolean) newValue;
+          break;
+        case -934795532:  // region
+          this._region = (ExternalId) newValue;
           break;
         case -2122778967:  // volatilityTable
           this._volatilityTable = (HullWhiteOneFactorVolatilityTable) newValue;
@@ -524,6 +651,38 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
     }
 
     /**
+     * Sets the {@code businessDayConvention} property in the builder.
+     * @param businessDayConvention  the new value, not null
+     * @return this, for chaining, not null
+     */
+    public Builder businessDayConvention(BusinessDayConvention businessDayConvention) {
+      JodaBeanUtils.notNull(businessDayConvention, "businessDayConvention");
+      this._businessDayConvention = businessDayConvention;
+      return this;
+    }
+
+    /**
+     * Sets the {@code endOfMonth} property in the builder.
+     * @param endOfMonth  the new value
+     * @return this, for chaining, not null
+     */
+    public Builder endOfMonth(boolean endOfMonth) {
+      this._endOfMonth = endOfMonth;
+      return this;
+    }
+
+    /**
+     * Sets the {@code region} property in the builder.
+     * @param region  the new value, not null
+     * @return this, for chaining, not null
+     */
+    public Builder region(ExternalId region) {
+      JodaBeanUtils.notNull(region, "region");
+      this._region = region;
+      return this;
+    }
+
+    /**
      * Sets the {@code volatilityTable} property in the builder.
      * @param volatilityTable  the new value, not null
      * @return this, for chaining, not null
@@ -537,7 +696,7 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
     //-----------------------------------------------------------------------
     @Override
     public String toString() {
-      StringBuilder buf = new StringBuilder(192);
+      StringBuilder buf = new StringBuilder(288);
       buf.append("HullWhiteOneFactorParametersSnapshot.Builder{");
       int len = buf.length();
       toString(buf);
@@ -553,6 +712,9 @@ public class HullWhiteOneFactorParametersSnapshot implements NamedSnapshot, Immu
       buf.append("name").append('=').append(JodaBeanUtils.toString(_name)).append(',').append(' ');
       buf.append("currency").append('=').append(JodaBeanUtils.toString(_currency)).append(',').append(' ');
       buf.append("meanReversion").append('=').append(JodaBeanUtils.toString(_meanReversion)).append(',').append(' ');
+      buf.append("businessDayConvention").append('=').append(JodaBeanUtils.toString(_businessDayConvention)).append(',').append(' ');
+      buf.append("endOfMonth").append('=').append(JodaBeanUtils.toString(_endOfMonth)).append(',').append(' ');
+      buf.append("region").append('=').append(JodaBeanUtils.toString(_region)).append(',').append(' ');
       buf.append("volatilityTable").append('=').append(JodaBeanUtils.toString(_volatilityTable)).append(',').append(' ');
     }
 
