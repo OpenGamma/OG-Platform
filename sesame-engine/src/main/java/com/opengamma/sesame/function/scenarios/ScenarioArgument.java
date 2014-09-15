3f8b2734793acfb8c5c969a137f201d0c1c8e5aa
Chris Kent <cjkent@hotmail.com>
2014-09-15 08:42:48 +0100

    PLT-418/419 - Javadoc

--- sesame-engine/src/main/java/com/opengamma/sesame/function/scenarios/ScenarioArgument.java
+++ sesame-engine/src/main/java/com/opengamma/sesame/function/scenarios/ScenarioArgument.java
@@ -6,22 +6,18 @@
 package com.opengamma.sesame.function.scenarios;
 
 /**
- *
- * TODO explain why hashCode() and equals() are important for scenario args
- *
- * TODO why is that useful? why not key args by their own type and have decorators specify the arg type they want?
- * useful for:
- *   ScenarioArguments.getDecoratorTypes() - for creating view config for scenarios - self-describing scenarios
- *   FilteredScenarioArguments.forFunctions() - arg pruning for better caching
- * could still key by arg, not decorator type
- * used to work without this because scenario args were a map of decorator class to arg instance
- * if arg type were used for lookup instead of fn instance it would allow any old fn to look up args
- * that might break caching because the logic behind argument pruning wouldn't be safe
+ * Interface for classes that define a transformation applied to data when running a scenario.
+ * <p>
+ * This allows a scenario to be self-describing. The arguments describe the transformations to apply to
+ * the data and also specify which functions must be added to the configuration to perform the transformations.
+ * <p>
+ * Scenario argument and function types exist in pairs. An argument's {@link #getFunctionType() function type}
+ * should return the argument's type from {@link ScenarioFunction#getArgumentType()}.
  */
 public interface ScenarioArgument<T extends ScenarioFunction<?>> {
 
   /**
-   * @return  the function type that uses this type of arguments
+   * @return  the type of the function that uses this type of argument
    */
   Class<T> getFunctionType();
 }
