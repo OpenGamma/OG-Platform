842904d7a3bd9cc39bf592f87518ddab162b7196
Ian Turner <ian@opengamma.com>
2014-09-24 16:07:51 +0100

    REQS-529 - remote bond example test, updated config and snapshot

---
+++ sesame-examples/example-app/src/main/java/com/opengamma/solutions/RemoteViewBondUtils.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.solutions;
+
+import static com.opengamma.sesame.config.ConfigBuilder.argument;
+import static com.opengamma.sesame.config.ConfigBuilder.arguments;
+import static com.opengamma.sesame.config.ConfigBuilder.column;
+import static com.opengamma.sesame.config.ConfigBuilder.config;
+import static com.opengamma.sesame.config.ConfigBuilder.function;
+import static com.opengamma.sesame.config.ConfigBuilder.implementations;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalTime;
+import org.threeten.bp.OffsetTime;
+import org.threeten.bp.Period;
+import org.threeten.bp.ZoneOffset;
+import org.threeten.bp.ZonedDateTime;
+
+import com.google.common.collect.ImmutableList;
+import com.opengamma.core.id.ExternalSchemes;
+import com.opengamma.core.link.ConfigLink;
+import com.opengamma.core.position.Counterparty;
+import com.opengamma.core.position.impl.SimpleCounterparty;
+import com.opengamma.core.position.impl.SimpleTrade;
+import com.opengamma.financial.analytics.curve.exposure.ExposureFunctions;
+import com.opengamma.financial.convention.daycount.DayCount;
+import com.opengamma.financial.convention.daycount.DayCounts;
+import com.opengamma.financial.convention.frequency.Frequency;
+import com.opengamma.financial.convention.frequency.PeriodFrequency;
+import com.opengamma.financial.convention.yield.SimpleYieldConvention;
+import com.opengamma.financial.convention.yield.YieldConvention;
+import com.opengamma.financial.currency.CurrencyMatrix;
+import com.opengamma.financial.security.bond.BondSecurity;
+import com.opengamma.financial.security.bond.GovernmentBondSecurity;
+import com.opengamma.id.ExternalId;
+import com.opengamma.id.ExternalIdBundle;
+import com.opengamma.sesame.ConfigDbMarketExposureSelectorFn;
+import com.opengamma.sesame.DefaultCurveNodeConverterFn;
+import com.opengamma.sesame.DefaultDiscountingIssuerProviderBundleFn;
+import com.opengamma.sesame.DefaultHistoricalTimeSeriesFn;
+import com.opengamma.sesame.ExposureFunctionsIssuerProviderFn;
+import com.opengamma.sesame.IssuerProviderBundleFn;
+import com.opengamma.sesame.IssuerProviderFn;
+import com.opengamma.sesame.RootFinderConfiguration;
+import com.opengamma.sesame.bond.BondCalculatorFactory;
+import com.opengamma.sesame.bond.BondFn;
+import com.opengamma.sesame.bond.DefaultBondFn;
+import com.opengamma.sesame.bond.DiscountingBondCalculatorFactory;
+import com.opengamma.sesame.component.RetrievalPeriod;
+import com.opengamma.sesame.component.StringSet;
+import com.opengamma.sesame.config.ViewColumn;
+import com.opengamma.sesame.marketdata.DefaultHistoricalMarketDataFn;
+import com.opengamma.sesame.marketdata.DefaultMarketDataFn;
+import com.opengamma.sesame.trade.BondTrade;
+import com.opengamma.util.ArgumentChecker;
+import com.opengamma.util.money.Currency;
+import com.opengamma.util.time.DateUtils;
+import com.opengamma.util.time.Expiry;
+
+/**
+ * Utility class for remote views
+ */
+public final class RemoteViewBondUtils {
+
+  private RemoteViewBondUtils() { /* private constructor */ }
+
+  /**
+   * Utility for creating a fra specific view column
+   * @param output output name, not null
+   * @param exposureConfig exposure function config, not null
+   * @param currencyMatrixLink currency matrix config, not null
+   */
+  public static ViewColumn createBondViewColumn(String output,
+                                                ConfigLink<ExposureFunctions> exposureConfig,
+                                                ConfigLink<CurrencyMatrix> currencyMatrixLink) {
+    ArgumentChecker.notNull(output, "output");
+    ArgumentChecker.notNull(exposureConfig, "exposureConfig");
+    ArgumentChecker.notNull(currencyMatrixLink, "currencyMatrixLink");
+
+    return
+        column(output,
+               config(
+                   arguments(
+                       function(ConfigDbMarketExposureSelectorFn.class,
+                                argument("exposureConfig", exposureConfig)),
+                       function(
+                           RootFinderConfiguration.class,
+                           argument("rootFinderAbsoluteTolerance", 1e-10),
+                           argument("rootFinderRelativeTolerance", 1e-10),
+                           argument("rootFinderMaxIterations", 1000)),
+                       function(DefaultCurveNodeConverterFn.class,
+                                argument("timeSeriesDuration", RetrievalPeriod.of(Period.ofYears(1)))),
+                       function(DefaultHistoricalMarketDataFn.class,
+                                argument("dataSource", "BLOOMBERG"),
+                                argument("currencyMatrix", currencyMatrixLink)),
+                       function(DefaultMarketDataFn.class,
+                                argument("dataSource", "BLOOMBERG"),
+                                argument("currencyMatrix", currencyMatrixLink)),
+                       function(
+                           DefaultHistoricalTimeSeriesFn.class,
+                           argument("resolutionKey", "DEFAULT_TSS"),
+                           argument("htsRetrievalPeriod", RetrievalPeriod.of((Period.ofYears(1))))),
+                       function(
+                           DefaultDiscountingIssuerProviderBundleFn.class,
+                           argument("impliedCurveNames", StringSet.of()))),
+                   implementations(
+                       BondFn.class, DefaultBondFn.class,
+                       BondCalculatorFactory.class, DiscountingBondCalculatorFactory.class,
+                       IssuerProviderBundleFn.class, DefaultDiscountingIssuerProviderBundleFn.class,
+                       IssuerProviderFn.class, ExposureFunctionsIssuerProviderFn.class)));
+  }
+
+
+  /** List of Bond inputs */
+  public static final List<Object> BOND_INPUTS = ImmutableList.<Object>of(createGovernmentBondTrade());
+
+
+  private static BondTrade createGovernmentBondTrade() {
+    Counterparty counterparty = new SimpleCounterparty(ExternalId.of(Counterparty.DEFAULT_SCHEME, "COUNTERPARTY"));
+    BigDecimal tradeQuantity = BigDecimal.valueOf(10000);
+    LocalDate tradeDate = LocalDate.of(2014, 7, 23);
+    OffsetTime tradeTime = OffsetTime.of(LocalTime.of(0, 0), ZoneOffset.UTC);
+    SimpleTrade trade = new SimpleTrade(createGovernmentBondSecurity(), tradeQuantity, counterparty, tradeDate, tradeTime);
+    trade.setPremium(0.0);
+    trade.setPremiumDate(tradeDate);
+    trade.setPremiumCurrency(Currency.USD);
+    return new BondTrade(trade);
+  }
+
+  private static BondSecurity createGovernmentBondSecurity() {
+
+    String issuerName = "US TREASURY N/B";
+    String issuerDomicile = "US";
+    String issuerType = "Sovereign";
+    Currency currency = Currency.USD;
+    YieldConvention yieldConvention = SimpleYieldConvention.US_STREET;
+    DayCount dayCountConvention = DayCounts.ACT_ACT_ICMA;
+
+    Period couponPeriod = Period.parse("P6M");
+    String couponType = "Fixed";
+    double couponRate = 5.0;
+    Frequency couponFrequency = PeriodFrequency.of(couponPeriod);
+
+    ZonedDateTime maturityDate = DateUtils.getUTCDate(2014, 8, 15);
+    ZonedDateTime firstCouponDate = DateUtils.getUTCDate(2012, 2, 15);
+    ZonedDateTime interestAccrualDate = DateUtils.getUTCDate(2011, 8, 15);
+    ZonedDateTime settlementDate = DateUtils.getUTCDate(2014, 8, 17);
+    Expiry lastTradeDate = new Expiry(maturityDate);
+
+    double issuancePrice = 100;
+    double totalAmountIssued = 32980000000.0;
+    double minimumAmount = 100;
+    double minimumIncrement = 100;
+    double parAmount = 100;
+    double redemptionValue = 100;
+
+    GovernmentBondSecurity bond =
+        new GovernmentBondSecurity(issuerName, issuerType, issuerDomicile, issuerType, currency, yieldConvention,
+                                   lastTradeDate, couponType, couponRate, couponFrequency, dayCountConvention,
+                                   interestAccrualDate, settlementDate, firstCouponDate, issuancePrice,
+                                   totalAmountIssued, minimumAmount, minimumIncrement, parAmount, redemptionValue);
+
+    ExternalId isinId = ExternalSchemes.isinSecurityId("US912828RB87");
+    ExternalId bloombergId = ExternalSchemes.bloombergTickerSecurityId("T 0.5 08/15/14 Govt");
+    bond.setExternalIdBundle(ExternalIdBundle.of(isinId, bloombergId));
+    return bond;
+  }
+
+}
---
+++ sesame-examples/example-app/src/test/java/com/opengamma/solutions/RemoteBondTest.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.solutions;
+
+import static com.opengamma.sesame.config.ConfigBuilder.configureView;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+
+import java.net.URI;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.opengamma.component.tool.ToolContextUtils;
+import com.opengamma.core.link.ConfigLink;
+import com.opengamma.engine.marketdata.spec.UserMarketDataSpecification;
+import com.opengamma.financial.analytics.curve.exposure.ExposureFunctions;
+import com.opengamma.financial.currency.CurrencyMatrix;
+import com.opengamma.financial.tool.ToolContext;
+import com.opengamma.id.UniqueId;
+import com.opengamma.integration.tool.IntegrationToolContext;
+import com.opengamma.master.security.SecurityMaster;
+import com.opengamma.sesame.OutputNames;
+import com.opengamma.sesame.config.ViewConfig;
+import com.opengamma.sesame.engine.Results;
+import com.opengamma.sesame.server.FunctionServer;
+import com.opengamma.sesame.server.FunctionServerRequest;
+import com.opengamma.sesame.server.IndividualCycleOptions;
+import com.opengamma.sesame.server.RemoteFunctionServer;
+import com.opengamma.util.money.Currency;
+import com.opengamma.util.money.MultipleCurrencyAmount;
+import com.opengamma.util.result.Result;
+import com.opengamma.util.test.TestGroup;
+import com.opengamma.util.time.DateUtils;
+
+/**
+ * Integration tests run against a remote server
+ * Input: Vanilla Interest Rate Swaps, Snapshot Market Data
+ * Output: Present Value
+ */
+
+@Test(groups = TestGroup.INTEGRATION, enabled = true)
+public class RemoteBondTest {
+
+  private static final String URL = "http://localhost:8080/jax";
+  private FunctionServer _functionServer;
+  private IndividualCycleOptions _cycleOptions;
+  private ConfigLink<ExposureFunctions> _exposureConfig;
+  private ConfigLink<CurrencyMatrix> _currencyMatrixLink;
+  private Results _bondResults;
+
+  SecurityMaster _securityMaster;
+
+
+  private static final double STD_TOLERANCE_PV = 1.0E-3;
+
+  @BeforeClass
+  public void setUp() {
+
+    ToolContext tc = ToolContextUtils.getToolContext(URL, IntegrationToolContext.class);
+    _securityMaster = tc.getSecurityMaster();
+
+    _functionServer = new RemoteFunctionServer(URI.create(URL));
+    _cycleOptions = IndividualCycleOptions.builder()
+        .valuationTime(DateUtils.getUTCDate(2014, 1, 22))
+        .marketDataSpec(UserMarketDataSpecification.of(UniqueId.of("DbSnp", "1039")))
+        .build();
+
+    _exposureConfig = ConfigLink.resolvable("USD Bond Exposure Functions", ExposureFunctions.class);
+    _currencyMatrixLink = ConfigLink.resolvable("BloombergLiveData", CurrencyMatrix.class);
+
+
+
+    FunctionServerRequest<IndividualCycleOptions> bondRequest =
+        FunctionServerRequest.<IndividualCycleOptions>builder()
+            .viewConfig(createViewConfig())
+            .inputs(RemoteViewBondUtils.BOND_INPUTS)
+            .cycleOptions(_cycleOptions)
+            .build();
+
+    _bondResults = _functionServer.executeSingleCycle(bondRequest);
+
+  }
+
+  private ViewConfig createViewConfig() {
+
+    return
+        configureView(
+            "Bond Remote view",
+            RemoteViewBondUtils.createBondViewColumn(OutputNames.PRESENT_VALUE_MARKET_CLEAN,
+                                                     _exposureConfig,
+                                                     _currencyMatrixLink)
+        );
+  }
+
+  @Test(enabled = true)
+  public void testBondPV() {
+    Result result = _bondResults.get(0, 0).getResult();
+    assertThat(result.isSuccess(), is(true));
+    assertThat(result.getValue(), is(instanceOf(MultipleCurrencyAmount.class)));
+    MultipleCurrencyAmount mca = (MultipleCurrencyAmount) result.getValue();
+    assertThat(mca.getCurrencyAmount(Currency.USD).getAmount(), is(closeTo(9999.290864101724, STD_TOLERANCE_PV)));
+
+  }
+
+}
