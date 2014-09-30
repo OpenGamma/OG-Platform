6d63c8cf918105326029677daf0a4b88d9104064
Julian Howarth <julian@opengamma.com>
2014-09-30 13:04:54 +0100

    PLT-451 - Remove use of convention and security source
    Replaced with links so caching is applied correctly
    Reformatted to match standards

--- projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/curve/RollDateSwapNodeConverter.java
+++ projects/OG-Financial/src/main/java/com/opengamma/financial/analytics/curve/RollDateSwapNodeConverter.java
@@ -10,6 +10,7 @@ import org.threeten.bp.ZonedDateTime;
 import com.opengamma.analytics.financial.instrument.InstrumentDefinition;
 import com.opengamma.core.convention.ConventionSource;
 import com.opengamma.core.holiday.HolidaySource;
+import com.opengamma.core.link.ConventionLink;
 import com.opengamma.core.marketdatasnapshot.SnapshotDataBundle;
 import com.opengamma.core.region.RegionSource;
 import com.opengamma.core.security.SecuritySource;
@@ -25,10 +26,6 @@ import com.opengamma.util.ArgumentChecker;
  *
  */
 public class RollDateSwapNodeConverter extends CurveNodeVisitorAdapter<InstrumentDefinition<?>> {
-  /** The security source */
-  private final SecuritySource _securitySource;
-  /** The convention source */
-  private final ConventionSource _conventionSource;
   /** The holiday source */
   private final HolidaySource _holidaySource;
   /** The region source */
@@ -41,41 +38,55 @@ public class RollDateSwapNodeConverter extends CurveNodeVisitorAdapter<Instrumen
   private final ZonedDateTime _valuationTime;
 
   /**
-   * @param securitySource The security source, not null
-   * @param conventionSource The convention source, not null
    * @param holidaySource The holiday source, not null
    * @param regionSource The region source, not null
    * @param marketData The market data, not null
    * @param dataId The id of the market data, not null
    * @param valuationTime The valuation time, not null
    */
-  public RollDateSwapNodeConverter(final SecuritySource securitySource, final ConventionSource conventionSource, final HolidaySource holidaySource, final RegionSource regionSource,
-      final SnapshotDataBundle marketData, final ExternalId dataId, final ZonedDateTime valuationTime) {
-    ArgumentChecker.notNull(securitySource, "security source");
-    ArgumentChecker.notNull(conventionSource, "convention source");
-    ArgumentChecker.notNull(holidaySource, "holiday source");
-    ArgumentChecker.notNull(regionSource, "region source");
-    ArgumentChecker.notNull(marketData, "market data");
-    ArgumentChecker.notNull(dataId, "data id");
-    ArgumentChecker.notNull(valuationTime, "valuation time");
-    _securitySource = securitySource;
-    _conventionSource = conventionSource;
-    _holidaySource = holidaySource;
-    _regionSource = regionSource;
-    _marketData = marketData;
-    _dataId = dataId;
-    _valuationTime = valuationTime;
+  public RollDateSwapNodeConverter(HolidaySource holidaySource,
+                                   RegionSource regionSource,
+                                   SnapshotDataBundle marketData,
+                                   ExternalId dataId,
+                                   ZonedDateTime valuationTime) {
+    _holidaySource = ArgumentChecker.notNull(holidaySource, "holidaySource");
+    _regionSource = ArgumentChecker.notNull(regionSource, "regionSource");
+    _marketData = ArgumentChecker.notNull(marketData, "marketData");
+    _dataId = ArgumentChecker.notNull(dataId, "dataId");
+    _valuationTime = ArgumentChecker.notNull(valuationTime, "valuationTime");
+  }
+
+  /**
+   * @param securitySource The security source, not required
+   * @param conventionSource The convention source, not required
+   * @param holidaySource The holiday source, not null
+   * @param regionSource The region source, not null
+   * @param marketData The market data, not null
+   * @param dataId The id of the market data, not null
+   * @param valuationTime The valuation time, not null
+   * @deprecated use constructor without securitySource and conventionSource
+   */
+  public RollDateSwapNodeConverter(SecuritySource securitySource, ConventionSource conventionSource,
+                                   HolidaySource holidaySource, RegionSource regionSource,
+      SnapshotDataBundle marketData, ExternalId dataId, ZonedDateTime valuationTime) {
+    this(holidaySource, regionSource, marketData, dataId, valuationTime);
   }
 
   @Override
-  public InstrumentDefinition<?> visitRollDateSwapNode(final RollDateSwapNode rollDateSwapNode) {
-    final RollDateSwapConvention swapConvention = _conventionSource.getSingle(rollDateSwapNode.getRollDateSwapConvention(), RollDateSwapConvention.class);
-    final FinancialConvention payLegConvention = _conventionSource.getSingle(swapConvention.getPayLegConvention(), FinancialConvention.class);
-    final FinancialConvention receiveLegConvention = _conventionSource.getSingle(swapConvention.getReceiveLegConvention(), FinancialConvention.class);
-    final RollDateAdjuster adjuster = RollDateAdjusterFactory.getAdjuster(swapConvention.getRollDateConvention().getValue());
-    final ZonedDateTime unadjustedStartDate = _valuationTime.plus(rollDateSwapNode.getStartTenor().getPeriod());
-    return NodeConverterUtils.getSwapRollDateDefinition(payLegConvention, receiveLegConvention, unadjustedStartDate, rollDateSwapNode.getRollDateStartNumber(),
-        rollDateSwapNode.getRollDateEndNumber(), adjuster, _securitySource, _regionSource, _holidaySource, _conventionSource, _marketData, _dataId, _valuationTime);
+  public InstrumentDefinition<?> visitRollDateSwapNode(RollDateSwapNode rollDateSwapNode) {
+
+    RollDateSwapConvention swapConvention =
+        ConventionLink.resolvable(rollDateSwapNode.getRollDateSwapConvention(), RollDateSwapConvention.class).resolve();
+    FinancialConvention payLegConvention =
+        ConventionLink.resolvable(swapConvention.getPayLegConvention(), FinancialConvention.class).resolve();
+    FinancialConvention receiveLegConvention =
+        ConventionLink.resolvable(swapConvention.getReceiveLegConvention(), FinancialConvention.class).resolve();
+    RollDateAdjuster adjuster = RollDateAdjusterFactory.getAdjuster(swapConvention.getRollDateConvention().getValue());
+    ZonedDateTime unadjustedStartDate = _valuationTime.plus(rollDateSwapNode.getStartTenor().getPeriod());
+    return NodeConverterUtils.getSwapRollDateDefinition(
+        payLegConvention, receiveLegConvention, unadjustedStartDate, rollDateSwapNode.getRollDateStartNumber(),
+        rollDateSwapNode.getRollDateEndNumber(), adjuster, _regionSource, _holidaySource, _marketData,
+        _dataId, _valuationTime);
   }
   
 }
