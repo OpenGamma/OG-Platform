3b71d61b813979096682620ed566d472c007f76c
Marc Henrard <marc@opengamma.com>
2014-10-20 11:21:16 +0100

    [REQS-490] Updated MarketQuote calculator to use ParameterProviderInterface

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/provider/calculator/discounting/MarketQuoteDiscountingCalculator.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/provider/calculator/discounting/MarketQuoteDiscountingCalculator.java
@@ -5,16 +5,27 @@
  */
 package com.opengamma.analytics.financial.provider.calculator.discounting;
 
+import com.opengamma.analytics.financial.forex.derivative.Forex;
+import com.opengamma.analytics.financial.forex.provider.ForexDiscountingMethod;
 import com.opengamma.analytics.financial.interestrate.InstrumentDerivativeVisitorAdapter;
+import com.opengamma.analytics.financial.interestrate.cash.derivative.Cash;
+import com.opengamma.analytics.financial.interestrate.cash.derivative.DepositIbor;
+import com.opengamma.analytics.financial.interestrate.cash.provider.CashDiscountingMethod;
+import com.opengamma.analytics.financial.interestrate.cash.provider.DepositIborDiscountingMethod;
+import com.opengamma.analytics.financial.interestrate.fra.derivative.ForwardRateAgreement;
+import com.opengamma.analytics.financial.interestrate.fra.provider.ForwardRateAgreementDiscountingProviderMethod;
 import com.opengamma.analytics.financial.interestrate.future.derivative.InterestRateFutureSecurity;
 import com.opengamma.analytics.financial.interestrate.future.derivative.InterestRateFutureTransaction;
 import com.opengamma.analytics.financial.interestrate.future.provider.InterestRateFutureSecurityDiscountingMethod;
-import com.opengamma.analytics.financial.provider.description.interestrate.MulticurveProviderInterface;
+import com.opengamma.analytics.financial.interestrate.swap.derivative.SwapFixedCoupon;
+import com.opengamma.analytics.financial.interestrate.swap.provider.SwapFixedCouponDiscountingMethod;
+import com.opengamma.analytics.financial.provider.description.interestrate.ParameterProviderInterface;
 
 /**
  * Calculate the market quote of instruments dependent of a Hull-White one factor provider.
  */
-public final class MarketQuoteDiscountingCalculator extends InstrumentDerivativeVisitorAdapter<MulticurveProviderInterface, Double> {
+public final class MarketQuoteDiscountingCalculator 
+  extends InstrumentDerivativeVisitorAdapter<ParameterProviderInterface, Double> {
 
   /**
    * The unique instance of the calculator.
@@ -35,23 +46,71 @@ public final class MarketQuoteDiscountingCalculator extends InstrumentDerivative
     return INSTANCE;
   }
 
+  /** Pricing methods and calculators. */
+  private static final PresentValueDiscountingCalculator PVC = PresentValueDiscountingCalculator.getInstance();
+  private static final CashDiscountingMethod METHOD_DEPO = CashDiscountingMethod.getInstance();
+  private static final DepositIborDiscountingMethod METHOD_IBOR = DepositIborDiscountingMethod.getInstance();
+  private static final ForwardRateAgreementDiscountingProviderMethod METHOD_FRA = ForwardRateAgreementDiscountingProviderMethod.getInstance();
+  private static final SwapFixedCouponDiscountingMethod METHOD_SWAP = SwapFixedCouponDiscountingMethod.getInstance();
+  private static final InterestRateFutureSecurityDiscountingMethod METHOD_IR_FUT = InterestRateFutureSecurityDiscountingMethod.getInstance();
+  private static final ForexDiscountingMethod METHOD_FOREX = ForexDiscountingMethod.getInstance();
+
+  //     -----     Deposit     ------
+
+  @Override
+  public Double visitCash(final Cash deposit, final ParameterProviderInterface multicurves) {
+    return METHOD_DEPO.parRate(deposit, multicurves.getMulticurveProvider());
+  }
+
+  @Override
+  public Double visitDepositIbor(final DepositIbor deposit, final ParameterProviderInterface multicurves) {
+    return METHOD_IBOR.parRate(deposit, multicurves.getMulticurveProvider());
+  }
+
+  //     -----     Payment/Coupon     ------
+
+  @Override
+  public Double visitForwardRateAgreement(final ForwardRateAgreement fra, final ParameterProviderInterface multicurves) {
+    return METHOD_FRA.parRate(fra, multicurves.getMulticurveProvider());
+  }
+
   /**
    * Pricing methods.
    */
-  private static final InterestRateFutureSecurityDiscountingMethod METHOD_IR_FUT = InterestRateFutureSecurityDiscountingMethod.getInstance();
-
-  // TODO: Add FRA, IRS, FX forward, FX swap
+  @Override
+  public Double visitFixedCouponSwap(final SwapFixedCoupon<?> swap, final ParameterProviderInterface multicurves) {
+    //TODO: check currency
+    final double pvSecond = swap.getSecondLeg().accept(PVC, multicurves).getAmount(swap.getSecondLeg().getCurrency()) 
+        * Math.signum(swap.getSecondLeg().getNthPayment(0).getNotional());
+    final double pvbp = METHOD_SWAP.presentValueBasisPoint(swap, multicurves.getMulticurveProvider());
+    return pvSecond / pvbp;
+  }
+  
+  // TODO: add generic swap? margin on first leg to get a pv=0
 
   //     -----     Futures     -----
 
   @Override
-  public Double visitInterestRateFutureSecurity(final InterestRateFutureSecurity futures, final MulticurveProviderInterface multicurves) {
+  public Double visitInterestRateFutureSecurity(final InterestRateFutureSecurity futures, final ParameterProviderInterface multicurves) {
     return METHOD_IR_FUT.price(futures, multicurves);
   }
 
   @Override
-  public Double visitInterestRateFutureTransaction(final InterestRateFutureTransaction futures, final MulticurveProviderInterface multicurves) {
+  public Double visitInterestRateFutureTransaction(final InterestRateFutureTransaction futures, final ParameterProviderInterface multicurves) {
     return METHOD_IR_FUT.price(futures.getUnderlyingSecurity(), multicurves);
+  }
+
+  //     -----     Forex     ------
+
+  /**
+   * Computes the forward forex rate.
+   * @param forex The forex instrument.
+   * @param multicurves The multicurves provider.
+   * @return The forward forex rate.
+   */
+  @Override
+  public Double visitForex(final Forex forex, final ParameterProviderInterface multicurves) {
+    return METHOD_FOREX.forwardForexRate(forex, multicurves.getMulticurveProvider());
   }
 
 }
