8b664a2ba105794328166d26dcc90d7b0c7ded11
Chris Kent <chris@opengamma.com>
2014-10-28 11:29:32 +0000

    PLT-479 - Runner to create and execute views for scenarios

--- sesame-engine/src/main/java/com/opengamma/sesame/engine/CycleArguments.java
+++ sesame-engine/src/main/java/com/opengamma/sesame/engine/CycleArguments.java
@@ -5,6 +5,7 @@
  */
 package com.opengamma.sesame.engine;
 
+import java.util.Collections;
 import java.util.Map;
 
 import org.threeten.bp.ZonedDateTime;
@@ -12,13 +13,13 @@ import org.threeten.bp.ZonedDateTime;
 import com.google.common.collect.ImmutableMap;
 import com.opengamma.id.VersionCorrection;
 import com.opengamma.sesame.config.FunctionArguments;
+import com.opengamma.sesame.function.scenarios.ScenarioArgument;
+import com.opengamma.sesame.function.scenarios.ScenarioDefinition;
 import com.opengamma.sesame.marketdata.CycleMarketDataFactory;
 import com.opengamma.util.ArgumentChecker;
-import com.opengamma.util.tuple.Pair;
-import com.opengamma.util.tuple.Pairs;
 
 /**
- * TODO this will probably need to be a joda bean for serialization
+ * Arguments used to drive the calculations in a single calculation cycle.
  */
 public final class CycleArguments {
 
@@ -49,18 +50,21 @@ public final class CycleArguments {
     FULL
   }
 
-  // TODO function arguments for the output functions
-  // TODO portfolio version correction
+  // TODO sort out function arguments
 
   private final ZonedDateTime _valuationTime;
   private final CycleMarketDataFactory _cycleMarketDataFactory;
   private final VersionCorrection _configVersionCorrection;
-  private final Map<Pair<Integer, Integer>, TraceType> _traceCells;
+  private final Map<Cell, TraceType> _traceCells;
   private final Map<String, TraceType> _traceOutputs;
   private final FunctionArguments _functionArguments;
   private final boolean _captureInputs;
+  private final ScenarioDefinition _scenarioDefinition;
 
-  // TODO use a Cell class instead of Pair<Integer, Integer>
+  /**
+   * @deprecated this will be removed, use {@link #builder}
+   */
+  @Deprecated
   public CycleArguments(ZonedDateTime valuationTime,
                         VersionCorrection configVersionCorrection,
                         CycleMarketDataFactory cycleMarketDataFactory) {
@@ -70,6 +74,10 @@ public final class CycleArguments {
          FunctionArguments.EMPTY);
   }
 
+  /**
+   * @deprecated this will be removed, use {@link #builder}
+   */
+  @Deprecated
   public CycleArguments(ZonedDateTime valuationTime,
                         VersionCorrection configVersionCorrection,
                         CycleMarketDataFactory cycleMarketDataFactory,
@@ -78,46 +86,80 @@ public final class CycleArguments {
          configVersionCorrection,
          cycleMarketDataFactory,
          functionArguments,
-         ImmutableMap.<Pair<Integer, Integer>, TraceType>of(),
+         ImmutableMap.<Cell, TraceType>of(),
          ImmutableMap.<String, TraceType>of());
   }
 
+  /**
+   * @deprecated this will be removed, use {@link #builder}
+   */
+  @Deprecated
   public CycleArguments(ZonedDateTime valuationTime,
                         VersionCorrection configVersionCorrection,
                         CycleMarketDataFactory cycleMarketDataFactory,
                         FunctionArguments functionArguments,
-                        Map<Pair<Integer, Integer>, TraceType> traceCells,
+                        Map<Cell, TraceType> traceCells,
                         Map<String, TraceType> traceOutputs) {
-    this(valuationTime, configVersionCorrection, cycleMarketDataFactory, functionArguments,
-         traceCells, traceOutputs, false);
+    this(valuationTime,
+         configVersionCorrection,
+         cycleMarketDataFactory,
+         functionArguments,
+         traceCells,
+         traceOutputs,
+         false,
+         ScenarioDefinition.EMPTY);
   }
 
+  /**
+   * @deprecated this will be removed, use {@link #builder}
+   */
+  @Deprecated
   public CycleArguments(ZonedDateTime valuationTime,
                         VersionCorrection configVersionCorrection,
                         CycleMarketDataFactory cycleMarketDataFactory,
                         boolean captureInputs) {
-    this(valuationTime, configVersionCorrection, cycleMarketDataFactory, FunctionArguments.EMPTY,
-         ImmutableMap.<Pair<Integer, Integer>, TraceType>of(),
-         ImmutableMap.<String, TraceType>of(), captureInputs);
+    this(valuationTime,
+         configVersionCorrection,
+         cycleMarketDataFactory,
+         FunctionArguments.EMPTY,
+         ImmutableMap.<Cell, TraceType>of(),
+         ImmutableMap.<String, TraceType>of(),
+         captureInputs,
+         ScenarioDefinition.EMPTY);
   }
 
+  /**
+   * @deprecated this will be removed, use {@link #builder}
+   */
+  @Deprecated
   public CycleArguments(ZonedDateTime valuationTime,
                         VersionCorrection configVersionCorrection,
                         CycleMarketDataFactory cycleMarketDataFactory,
                         FunctionArguments functionArguments,
                         boolean captureInputs) {
-    this(valuationTime, configVersionCorrection, cycleMarketDataFactory, functionArguments,
-         ImmutableMap.<Pair<Integer, Integer>, TraceType>of(),
-         ImmutableMap.<String, TraceType>of(), captureInputs);
+    this(valuationTime,
+         configVersionCorrection,
+         cycleMarketDataFactory,
+         functionArguments,
+         ImmutableMap.<Cell, TraceType>of(),
+         ImmutableMap.<String, TraceType>of(),
+         captureInputs,
+         ScenarioDefinition.EMPTY);
   }
 
+  /**
+   * @deprecated this will become private, use {@link #builder}
+   */
+  @Deprecated
   public CycleArguments(ZonedDateTime valuationTime,
                         VersionCorrection configVersionCorrection,
                         CycleMarketDataFactory cycleMarketDataFactory,
                         FunctionArguments functionArguments,
-                        Map<Pair<Integer, Integer>, TraceType> traceCells,
+                        Map<Cell, TraceType> traceCells,
                         Map<String, TraceType> traceOutputs,
-                        boolean captureInputs) {
+                        boolean captureInputs,
+                        ScenarioDefinition scenarioDefinition) {
+    _scenarioDefinition = ArgumentChecker.notNull(scenarioDefinition, "scenarioDefinition");
     _functionArguments = ArgumentChecker.notNull(functionArguments, "functionArguments");
     _configVersionCorrection = ArgumentChecker.notNull(configVersionCorrection, "configVersionCorrection");
     _valuationTime = ArgumentChecker.notNull(valuationTime, "valuationTime");
@@ -149,14 +191,199 @@ public final class CycleArguments {
     return _functionArguments;
   }
 
+  ScenarioDefinition getScenarioDefinition() {
+    return _scenarioDefinition;
+  }
+
   TraceType traceType(int rowIndex, int colIndex) {
-    Pair<Integer, Integer> cell = Pairs.of(rowIndex, colIndex);
+    Cell cell = new Cell(rowIndex, colIndex);
     return _traceCells.containsKey(cell) ?
       _traceCells.get(cell) :
       TraceType.NONE;
   }
 
-  public boolean isCaptureInputs() {
+  boolean isCaptureInputs() {
     return _captureInputs;
+  }
+
+  /**
+   * Creates a builder for building up cycle arguments.
+   *
+   * @param marketDataFactory the market data factory for the cycle
+   * @return a builder for building up cycle arguments
+   */
+  public static Builder builder(CycleMarketDataFactory marketDataFactory) {
+    return new Builder(marketDataFactory);
+  }
+
+  /**
+   * Represents a single cell in the grid of {@link Results}.
+   */
+  public static class Cell {
+
+    private final int _row;
+    private final int _col;
+
+    /**
+     * @param row the index of the cell's row (zero based)
+     * @param col the index of the cell's column (zero based)
+     */
+    public Cell(int row, int col) {
+      _row = ArgumentChecker.notNegative(row, "row");
+      _col = ArgumentChecker.notNegative(col, "col");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Cell cell = (Cell) o;
+      return _row == cell._row && _col == cell._col;
+    }
+
+    @Override
+    public int hashCode() {
+      int result = _row;
+      result = 31 * result + _col;
+      return result;
+    }
+  }
+
+  /**
+   * Builder for constructing instances of {@link CycleArguments}.
+   */
+  public static final class Builder {
+
+    private final CycleMarketDataFactory _cycleMarketDataFactory;
+
+    private ZonedDateTime _valuationTime = ZonedDateTime.now();
+    private Map<Cell, TraceType> _traceCells = Collections.emptyMap();
+    private Map<String, TraceType> _traceOutputs = Collections.emptyMap();
+    // TODO this is correct, enable it
+    //private Map<Class<?>, FunctionArguments> _functionArguments = Collections.emptyMap();
+    private FunctionArguments _functionArguments = FunctionArguments.EMPTY;
+    private boolean _captureInputs = false;
+    private VersionCorrection _versionCorrection = VersionCorrection.LATEST;
+    private ScenarioDefinition _scenarioDefinition = ScenarioDefinition.EMPTY;
+
+    private Builder(CycleMarketDataFactory cycleMarketDataFactory) {
+      _cycleMarketDataFactory = ArgumentChecker.notNull(cycleMarketDataFactory, "cycleMarketDataFactory");
+    }
+
+    /**
+     * @return an instance of {@code CycleArguments} containing the data in this builder
+     */
+    public CycleArguments build() {
+      return new CycleArguments(_valuationTime,
+                                _versionCorrection,
+                                _cycleMarketDataFactory,
+                                _functionArguments,
+                                _traceCells,
+                                _traceOutputs,
+                                _captureInputs,
+                                _scenarioDefinition);
+    }
+
+    /**
+     * Sets the cells for which tracing of function calls and values will be enabled
+     *
+     * @param cells the cells for which tracing will be enabled
+     * @return this builder
+     */
+    public Builder traceCells(Map<Cell, TraceType> cells) {
+      _traceCells = ArgumentChecker.notNull(cells, "cells");
+      return this;
+    }
+
+    /**
+     * Sets the non-portfolio outputs for which tracing of function calls and values will be enabled
+     *
+     * @param outputs the outputs for which tracing will be enabled
+     * @return this builder
+     */
+    public Builder traceOutputs(Map<String, TraceType> outputs) {
+      _traceOutputs = ArgumentChecker.notNull(outputs, "outputs");
+      return this;
+    }
+
+    /**
+     * Sets the function arguments for the cycle.
+     *
+     * @param arguments the function arguments for the cycle
+     * @return this builder
+     */
+    public Builder functionArguments(FunctionArguments arguments) {
+      _functionArguments = ArgumentChecker.notNull(arguments, "arguments");
+      return this;
+    }
+
+    /**
+     * Sets the valuation time for the calculations in the cycle
+     *
+     * @param valuationTime the valuation time for the calculations in the cycle
+     * @return this builder
+     */
+    public Builder valuationTime(ZonedDateTime valuationTime) {
+      _valuationTime = ArgumentChecker.notNull(valuationTime, "valuationTime");
+      return this;
+    }
+
+    // TODO this is the correct version
+    // TODO but what if we want to provide different arguments for different columns?
+    /* public Builder functionArguments(Map<Class<?>, FunctionArguments> arguments) {
+      _functionArguments = ArgumentChecker.notNull(arguments, "arguments");
+      return this;
+    }*/
+
+    /**
+     * Sets the version correction used to load from the data store
+     *
+     * @param versionCorrection the version correction used to load from the data store
+     * @return this builder
+     */
+    public Builder versionCorrection(VersionCorrection versionCorrection) {
+      _versionCorrection = ArgumentChecker.notNull(versionCorrection, "versionCorrection");
+      return this;
+    }
+
+    /**
+     * Sets whether all inputs to the calculations should be captured in addition to the results.
+     * <p>
+     * This includes all market data and any data loaded from a data store.
+     *
+     * @param capture true if inputs should be captured and returned with the results
+     * @return this builder
+     */
+    public Builder captureInputs(boolean capture) {
+      _captureInputs = capture;
+      return this;
+    }
+
+    /**
+     * Merges {@code scenarioDefinition} with the builder's scenario definition.
+     *
+     * @param scenarioDefinition a scenario definition
+     * @return this builder
+     */
+    public Builder scenarioDefinition(ScenarioDefinition scenarioDefinition) {
+      ArgumentChecker.notNull(scenarioDefinition, "scenarioDefinition");
+      _scenarioDefinition = _scenarioDefinition.mergedWith(scenarioDefinition);
+      return this;
+    }
+
+    /**
+     * Adds scenario arguments to this builder's scenario definition.
+     *
+     * @param args the scenario arguments
+     * @return this builder
+     */
+    public Builder scenarioArguments(ScenarioArgument<?, ?>... args) {
+      _scenarioDefinition = _scenarioDefinition.with(args);
+      return this;
+    }
   }
 }
