152f6c6d3a28fee2b9042a8660f4ce2c9e02963e
Yuki Iwashita <yuki@opengamma.com>
2014-11-06 14:24:17 +0000

    [PLAT-6849] Added consistency test

--- projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/instrument/annuity/FixedAnnuityDefinitionBuilderTest.java
+++ projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/instrument/annuity/FixedAnnuityDefinitionBuilderTest.java
@@ -20,16 +20,34 @@ import com.opengamma.analytics.financial.datasets.CalendarUSD;
 import com.opengamma.analytics.financial.instrument.NotionalProvider;
 import com.opengamma.analytics.financial.instrument.VariableNotionalProvider;
 import com.opengamma.analytics.financial.instrument.annuity.AbstractAnnuityDefinitionBuilder.CouponStub;
+import com.opengamma.analytics.financial.instrument.index.GeneratorAttributeIR;
+import com.opengamma.analytics.financial.instrument.index.GeneratorLegFixed;
+import com.opengamma.analytics.financial.instrument.index.GeneratorLegIbor;
+import com.opengamma.analytics.financial.instrument.index.GeneratorLegIborMaster;
 import com.opengamma.analytics.financial.instrument.index.GeneratorSwapFixedIbor;
 import com.opengamma.analytics.financial.instrument.index.GeneratorSwapFixedIborMaster;
 import com.opengamma.analytics.financial.instrument.index.IborIndex;
 import com.opengamma.analytics.financial.instrument.index.IndexIborMaster;
 import com.opengamma.analytics.financial.instrument.payment.CouponDefinition;
 import com.opengamma.analytics.financial.instrument.payment.CouponFixedDefinition;
+import com.opengamma.analytics.financial.instrument.payment.PaymentDefinition;
+import com.opengamma.analytics.financial.instrument.swap.SwapCouponFixedCouponDefinition;
+import com.opengamma.analytics.financial.instrument.swap.SwapDefinition;
+import com.opengamma.analytics.financial.interestrate.datasets.StandardDataSetsMulticurveUSD;
+import com.opengamma.analytics.financial.interestrate.payments.derivative.Payment;
+import com.opengamma.analytics.financial.interestrate.swap.derivative.Swap;
+import com.opengamma.analytics.financial.interestrate.swap.provider.SwapInstrumentsDataSet;
+import com.opengamma.analytics.financial.provider.calculator.discounting.PresentValueDiscountingCalculator;
+import com.opengamma.analytics.financial.provider.description.interestrate.MulticurveProviderDiscount;
 import com.opengamma.analytics.financial.schedule.ScheduleCalculator;
 import com.opengamma.financial.convention.StubType;
+import com.opengamma.financial.convention.businessday.BusinessDayConvention;
 import com.opengamma.financial.convention.calendar.Calendar;
+import com.opengamma.financial.convention.daycount.DayCount;
+import com.opengamma.financial.convention.daycount.DayCounts;
 import com.opengamma.financial.convention.rolldate.RollConvention;
+import com.opengamma.timeseries.precise.zdt.ImmutableZonedDateTimeDoubleTimeSeries;
+import com.opengamma.timeseries.precise.zdt.ZonedDateTimeDoubleTimeSeries;
 import com.opengamma.util.money.Currency;
 import com.opengamma.util.time.DateUtils;
 
@@ -236,5 +254,63 @@ public class FixedAnnuityDefinitionBuilderTest {
     CouponFixedDefinition cpnL = targetAnnuity.getNthPayment(refPosition);
     assertEquals(message, cpnL.getAccrualStartDate().toLocalDate(), expectedStartDate);
     assertEquals(message, cpnL.getAccrualEndDate().toLocalDate(), expectedEndDate);
+  }
+
+  /**
+   * An example showing relation to GeneratorLeg
+   * See {@link SwapInstrumentsDataSet}. 
+   */
+  @Test
+  public void ConsistencyGeneratorTest() {
+    MulticurveProviderDiscount mCurvesOIS = StandardDataSetsMulticurveUSD.getCurvesUSDOisL1L3L6().getFirst();
+    PresentValueDiscountingCalculator pvCalculator = PresentValueDiscountingCalculator.getInstance();
+    DayCount dc = DayCounts.THIRTY_U_360;
+    StubType stubType = StubType.SHORT_START;
+    BusinessDayConvention bbc = USDLIBOR3M.getBusinessDayConvention();
+    int offset = 2;
+    ZonedDateTime valDate = DateUtils.getUTCDate(2014, 1, 22);
+    ZonedDateTime tradeDate = DateUtils.getUTCDate(2014, 9, 10);
+    Period period = Period.ofMonths(22);
+    double fixedRate = 0.01;
+    ZonedDateTimeDoubleTimeSeries tsLibor = ImmutableZonedDateTimeDoubleTimeSeries.ofUTC(
+        new ZonedDateTime[] {DateUtils.getUTCDate(2013, 12, 10), DateUtils.getUTCDate(2013, 12, 12) },
+        new double[] {0.0024185, 0.0024285 });
+    ZonedDateTimeDoubleTimeSeries[] ts = new ZonedDateTimeDoubleTimeSeries[] {tsLibor, tsLibor };
+
+    GeneratorLegFixed fixedGnr = new GeneratorLegFixed("LEG_USDFixed1Y", USD, offset,
+        P6M, dc, bbc, 0, true, stubType, false, NYC);
+    GeneratorLegIbor liborGnr = GeneratorLegIborMaster.getInstance().getGenerator("USDLIBOR3M", NYC);
+    GeneratorAttributeIR att = new GeneratorAttributeIR(period);
+    PaymentDefinition[] payments = fixedGnr.generateInstrument(tradeDate, fixedRate, NOTIONAL_1, att)
+        .getPayments();
+    CouponFixedDefinition[] cpnFixedDefinition = new CouponFixedDefinition[payments.length];
+    for (int n = 0; n < payments.length; n++) {
+      cpnFixedDefinition[n] = (CouponFixedDefinition) payments[n];
+    }
+    AnnuityCouponFixedDefinition fixedLegGnr = new AnnuityCouponFixedDefinition(cpnFixedDefinition, NYC);
+    AnnuityDefinition<? extends CouponDefinition> liborLegGnr = (AnnuityDefinition<? extends CouponDefinition>)
+        liborGnr.generateInstrument(tradeDate, 0.0, -NOTIONAL_1, att);
+    SwapCouponFixedCouponDefinition swapDfnGnr = new SwapCouponFixedCouponDefinition(fixedLegGnr, liborLegGnr);
+    Swap<? extends Payment, ? extends Payment> swapGnr = swapDfnGnr.toDerivative(valDate, ts);
+
+    AdjustedDateParameters adjDatePram = new AdjustedDateParameters(NYC, USDLIBOR3M.getBusinessDayConvention());
+    OffsetAdjustedDateParameters offsetAdjParam = new OffsetAdjustedDateParameters(-2, OffsetType.BUSINESS, NYC, bbc);
+    CouponStub stub = new CouponStub(stubType);
+    // start date adjustment is absent in the annuity builder
+    LocalDate spot = ScheduleCalculator.getAdjustedDate(tradeDate, offset, NYC).toLocalDate();
+
+    AnnuityDefinition<?> liborLegBld = new FloatingAnnuityDefinitionBuilder()
+        .notional(NOTIONAL_PROV_1).payer(true).startDate(spot).endDate(spot.plus(period)).index(USDLIBOR3M)
+        .accrualPeriodFrequency(P3M).rollDateAdjuster(RollConvention.NONE.getRollDateAdjuster(0))
+        .resetDateAdjustmentParameters(adjDatePram).accrualPeriodParameters(adjDatePram).currency(USD).spread(0.0)
+        .dayCount(USDLIBOR3M.getDayCount()).fixingDateAdjustmentParameters(offsetAdjParam).startStub(stub).build();
+    AnnuityDefinition<?> fixedLegBld = new FixedAnnuityDefinitionBuilder()
+        .payer(false).notional(NOTIONAL_PROV_1).startDate(spot).endDate(spot.plusMonths(22))
+        .accrualPeriodFrequency(P6M).rollDateAdjuster(RollConvention.NONE.getRollDateAdjuster(0))
+        .accrualPeriodParameters(adjDatePram).dayCount(dc).currency(USD).startStub(stub).rate(fixedRate).build();
+    SwapDefinition swapDfnBld = new SwapDefinition(fixedLegBld, liborLegBld);
+    Swap<? extends Payment, ? extends Payment> swapBld = swapDfnBld.toDerivative(valDate, ts);
+    double ref = swapGnr.accept(pvCalculator, mCurvesOIS).getAmount(USD);
+    assertEquals(ref, swapBld.accept(pvCalculator, mCurvesOIS).getAmount(USD), Math.abs(ref) * 1.0e-10);
   }
 }
