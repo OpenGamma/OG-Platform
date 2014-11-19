a5d3a49d6d292d52fddcff43fced1b9c3f48d517
Mathieu Lorentz <mathieu@opengamma.com>
2014-11-19 10:55:11 -0500

    [REQS-712] Light-weight holiday source proposal
    Per review, renaming class to use Store suffix, removing support for retrieving holidays per currency

--- projects/OG-Master/src/main/java/com/opengamma/master/holiday/impl/SimpleInMemoryHolidayStore.java
+++ projects/OG-Master/src/main/java/com/opengamma/master/holiday/impl/SimpleInMemoryHolidayStore.java
@@ -11,10 +11,8 @@ import java.util.Map;
 
 import org.threeten.bp.LocalDate;
 
-import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
 import com.opengamma.core.holiday.Holiday;
 import com.opengamma.core.holiday.HolidaySource;
 import com.opengamma.core.holiday.HolidayType;
@@ -28,19 +26,14 @@ import com.opengamma.util.money.Currency;
 /**
  * 
  */
-public class SimpleInMemoryHolidaySource implements HolidaySource {
+public class SimpleInMemoryHolidayStore implements HolidaySource {
 
   private Map<UniqueId, Holiday> _calendarHolidays = Maps.newHashMap();
-  private Multimap<Currency, Holiday> _currencyHolidays = HashMultimap.create();
  
   public void putAll(Map<UniqueId, Holiday> calendarHolidays) {
     _calendarHolidays.putAll(calendarHolidays);
   }
   
-  public void putAll(Multimap<Currency, Holiday> currencyHolidays) {
-    _currencyHolidays.putAll(currencyHolidays);
-  }
-
   @Override
   public Holiday get(UniqueId uniqueId) {
     return _calendarHolidays.get(uniqueId);
@@ -85,19 +78,12 @@ public class SimpleInMemoryHolidaySource implements HolidaySource {
 
   @Override
   public Collection<Holiday> get(Currency currency) {
-    return _currencyHolidays.get(currency);
+    throw new UnsupportedOperationException("Cannot check if date is a holiday without a calendar or region id");
   }
 
   @Override
   public boolean isHoliday(LocalDate dateToCheck, Currency currency) {
-    Collection<Holiday> holidays = get(currency);
-    
-    for (Holiday holiday : holidays) {
-      if (holiday.getHolidayDates().contains(dateToCheck)) {
-        return true;
-      }
-    }
-    return false;
+    throw new UnsupportedOperationException("Cannot check if date is a holiday without a calendar or region id");
   }
 
   @Override
