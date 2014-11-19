cc2837dba3ca3d9a68db0e48b31403e74daa5009
Chris Kent <chris@opengamma.com>
2014-11-19 15:41:28 +0000

    PLT-515 - Remove redundant FXMatrix parameter from IssuerProviderFn

--- sesame-function/src/main/java/com/opengamma/sesame/IssuerProviderFn.java
+++ sesame-function/src/main/java/com/opengamma/sesame/IssuerProviderFn.java
@@ -25,7 +25,7 @@ public interface IssuerProviderFn {
    * @param fxMatrix the FX matrix to include inside the multicurve bundle.
    * @return the multicurve bundle for curves by issuer.
    * 
-   * @deprecated use {@link #createBundle(Environment, Trade, FXMatrix)} with the original trade.
+   * @deprecated use {@link #getMulticurveBundle(Environment, Trade)} with the original trade.
    */
   @Deprecated
   Result<IssuerProviderBundle> createBundle(Environment env, FinancialSecurity security, FXMatrix fxMatrix);
@@ -37,6 +37,20 @@ public interface IssuerProviderFn {
    * @param trade the trade to return the multicurve bundle for.
    * @param fxMatrix the FX matrix to include inside the multicurve bundle.
    * @return the multicurve bundle for curves by issuer.
+   * @deprecated use {@link #getMulticurveBundle(Environment, Trade)}. The FX matrix argument isn't used in
+   *   any implementation if this method and all the callers create an empty matrix just to satisfy the
+   *   method signature. Also this method can't be sensibly implemented for the case where pre-calibrated
+   *   curves are provided by the caller.
    */
+  @Deprecated
   Result<IssuerProviderBundle> createBundle(Environment env, Trade trade, FXMatrix fxMatrix);
+
+  /**
+   * Returns the multicurve bundle for curves by issuer for a trade.
+   *
+   * @param env the environment to return the multicurve bundle for.
+   * @param trade the trade to return the multicurve bundle for.
+   * @return the multicurve bundle for curves by issuer.
+   */
+  Result<IssuerProviderBundle> getMulticurveBundle(Environment env, Trade trade);
 }
