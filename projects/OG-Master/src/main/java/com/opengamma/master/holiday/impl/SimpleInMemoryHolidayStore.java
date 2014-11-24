28560f6f6f97dd72150385394d2de351b5ce67ee
stuartw <stuart.wakefield@opengamma.com>
2014-11-24 16:05:03 +0000

    PLAT-6867 Simplify holiday store by removing uniqueid

--- projects/OG-Master/src/main/java/com/opengamma/master/holiday/impl/SimpleInMemoryHolidayStore.java
+++ projects/OG-Master/src/main/java/com/opengamma/master/holiday/impl/SimpleInMemoryHolidayStore.java
@@ -14,6 +14,7 @@ import java.util.Set;
 import org.threeten.bp.LocalDate;
 
 import com.google.common.collect.Maps;
+import com.opengamma.DataNotFoundException;
 import com.opengamma.core.holiday.Holiday;
 import com.opengamma.core.holiday.HolidaySource;
 import com.opengamma.core.holiday.HolidayType;
@@ -23,48 +24,65 @@ import com.opengamma.id.ObjectId;
 import com.opengamma.id.UniqueId;
 import com.opengamma.id.VersionCorrection;
 import com.opengamma.util.money.Currency;
-import com.opengamma.util.tuple.Pair;
-import com.opengamma.util.tuple.Pairs;
 
 /**
  * Simple implementation of a container of holidays per calendar relying on a map. The putAll method can be used to
  * add calendars and their corresponding holidays to the store. The other methods are part of the {@link HolidaySource}
- * interface and allow to check whether a date is a holiday for a calendar or region.
- * 
- * The methods to get a collection of holidays for a currency or to check with a date is a holiday for a currency 
+ * interface and allow to check whether a date is a holiday for a calendar.
+ *
+ * The methods to get a collection of holidays for a currency or to check with a date is a holiday for a currency
  * (rather than a calendar) are not implemented.
+ *
+ * {@link HolidayType} is ignored, only one holiday series is stored (or queried per id) - the holiday type information
+ * on both the insertion (and query) is ignored).
  */
 public class SimpleInMemoryHolidayStore implements HolidaySource {
 
-  private Map<Pair<UniqueId, HolidayType>, Holiday> _calendarHolidays = Maps.newHashMap();
- 
+  private Map<ExternalId, Holiday> _calendarHolidays = Maps.newHashMap();
+
   /**
    * @param calendarHolidays a map from a calendar or region id to a holiday that will be added to the store
    */
-  public void putAll(Map<UniqueId, Holiday> calendarHolidays) {
-    for (Entry<UniqueId, Holiday> entry : calendarHolidays.entrySet()) {
-      Holiday holiday = entry.getValue();
-      
-      _calendarHolidays.put(Pairs.of(entry.getKey(), holiday.getType()), holiday);
+  public void putAll(Map<ExternalId, Holiday> calendarHolidays) {
+    for (Entry<ExternalId, Holiday> entry : calendarHolidays.entrySet()) {
+      _calendarHolidays.put(entry.getKey(), entry.getValue());
     }
   }
-  
+
+  /**
+   * Get the holiday series, null if not found. Ignores version of uniqueId
+   * @param uniqueId  the unique identifier to search for, not null
+   * @return the series, null if not found
+   */
   @Override
   public Holiday get(UniqueId uniqueId) {
-    return _calendarHolidays.get(uniqueId);
+    Holiday holiday = _calendarHolidays.get(ExternalId.of(uniqueId.getScheme(), uniqueId.getValue()));
+    if (holiday == null) {
+      throw new DataNotFoundException("Holiday with id " + uniqueId + " not found");
+    }
+    return holiday;
   }
 
+  /**
+   * Get the holiday series, null if not found. Ignores version of uniqueId
+   * @param objectId the objectid of the series
+   * @param versionCorrection version correction, which is ignored
+   * @return the series, null if not found
+   */
   @Override
   public Holiday get(ObjectId objectId, VersionCorrection versionCorrection) {
-    return _calendarHolidays.get(UniqueId.of(objectId, versionCorrection.getVersionAsOfString()));
+    Holiday holiday = _calendarHolidays.get(ExternalId.of(objectId.getScheme(), objectId.getValue()));
+    if (holiday == null) {
+      throw new DataNotFoundException("Holiday with id " + objectId + " not found");
+    }
+    return holiday;
   }
 
   @Override
   public Map<UniqueId, Holiday> get(Collection<UniqueId> uniqueIds) {
     Map<UniqueId, Holiday> holidays = Maps.newHashMap();
-    
     for (UniqueId id : uniqueIds) {
-      holidays.put(id, _calendarHolidays.get(id));
+      holidays.put(id, get(id));
     }
     return holidays;
   }
@@ -74,7 +92,7 @@ public class SimpleInMemoryHolidayStore implements HolidaySource {
     Map<ObjectId, Holiday> holidays = Maps.newHashMap();
     
     for (ObjectId obj : objectIds) {
-      holidays.put(obj, _calendarHolidays.get(UniqueId.of(obj, versionCorrection.getVersionAsOfString())));
+      holidays.put(obj, _calendarHolidays.get(ExternalId.of(obj.getScheme(), obj.getValue())));
     }
     return holidays;
   }
@@ -82,12 +100,9 @@ public class SimpleInMemoryHolidayStore implements HolidaySource {
   @Override
   public Set<Holiday> get(HolidayType holidayType, ExternalIdBundle regionOrExchangeIds) {
     Set<Holiday> holidays = new HashSet<>();
-    
     for (ExternalId id : regionOrExchangeIds.getExternalIds()) {
-      Pair<ExternalId, HolidayType> key = Pairs.of(id, holidayType);
-      
-      if (_calendarHolidays.containsKey(key)) {
-        holidays.add(_calendarHolidays.get(key));
+      if (_calendarHolidays.containsKey(id)) {
+        holidays.add(_calendarHolidays.get(id));
       }
     }
     return holidays;
@@ -119,12 +134,12 @@ public class SimpleInMemoryHolidayStore implements HolidaySource {
 
   @Override
   public boolean isHoliday(LocalDate dateToCheck, HolidayType holidayType, ExternalId regionOrExchangeId) {
-    return isWeekend(dateToCheck) || isHoliday(dateToCheck, Pairs.of(regionOrExchangeId, holidayType));
+    return isWeekend(dateToCheck) || isHoliday(dateToCheck, regionOrExchangeId);
   }
   
-  private boolean isHoliday(LocalDate dateToCheck, Pair<ExternalId, HolidayType> key) {
-    if (_calendarHolidays.containsKey(key)) {
-      return _calendarHolidays.get(key).getHolidayDates().contains(dateToCheck);
+  private boolean isHoliday(LocalDate dateToCheck, ExternalId id) {
+    if (_calendarHolidays.containsKey(id)) {
+      return _calendarHolidays.get(id).getHolidayDates().contains(dateToCheck);
     } else {
       return false;
     }
