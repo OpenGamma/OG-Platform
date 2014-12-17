2d91dc1a713bc6bbf0aea969ea986ba6c14bb4a7
Yuki Iwashita <yuki@opengamma.com>
2014-12-17 18:10:37 +0000

    [PLAT-6877] Fix first derivative for time square interpolation

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/interpolation/TimeSquareInterpolator1D.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/interpolation/TimeSquareInterpolator1D.java
@@ -15,6 +15,7 @@ import com.opengamma.util.ArgumentChecker;
 /**
  * A one-dimensional interpolator.
  * The interpolation is linear on x y^2. The interpolator is used for interpolation on integrated variance for options.
+ * All values of x, y must be positive. 
  */
 public class TimeSquareInterpolator1D extends Interpolator1D {
   private static final long serialVersionUID = 1L;
@@ -44,14 +45,23 @@ public class TimeSquareInterpolator1D extends Interpolator1D {
     Validate.notNull(value, "Value to be interpolated must not be null");
     ArgumentChecker.isTrue(value > 0, "Value should be stricly positive");
     Validate.notNull(data, "Data bundle must not be null");
-    final InterpolationBoundedValues boundedValues = data.getBoundedValues(value);
-    final double x1 = boundedValues.getLowerBoundKey();
-    final double y1 = boundedValues.getLowerBoundValue();
+    double x1;
+    double y1;
+    double x2;
+    double y2;
     if (data.getLowerBoundIndex(value) == data.size() - 1) {
-      return 0.;
+      int index = data.size() - 2;
+      x1 = data.getKeys()[index];
+      y1 = data.getValues()[index];
+      x2 = data.getKeys()[index + 1];
+      y2 = data.getValues()[index + 1];
+    } else {
+      InterpolationBoundedValues boundedValues = data.getBoundedValues(value);
+      x1 = boundedValues.getLowerBoundKey();
+      y1 = boundedValues.getLowerBoundValue();
+      x2 = boundedValues.getHigherBoundKey();
+      y2 = boundedValues.getHigherBoundValue();
     }
-    final double x2 = boundedValues.getHigherBoundKey();
-    final double y2 = boundedValues.getHigherBoundValue();
     final double w = (x2 - value) / (x2 - x1);
     final double xy21 = x1 * y1 * y1;
     final double xy22 = x2 * y2 * y2;
@@ -93,11 +103,21 @@ public class TimeSquareInterpolator1D extends Interpolator1D {
 
   @Override
   public Interpolator1DDataBundle getDataBundle(final double[] x, final double[] y) {
+    ArgumentChecker.notNull(y, "y");
+    int nY = y.length;
+    for (int i = 0; i < nY; ++i) {
+      ArgumentChecker.isTrue(y[i] > 0.0, "All values in y must be positive");
+    }
     return new ArrayInterpolator1DDataBundle(x, y);
   }
 
   @Override
   public Interpolator1DDataBundle getDataBundleFromSortedArrays(final double[] x, final double[] y) {
+    ArgumentChecker.notNull(y, "y");
+    int nY = y.length;
+    for (int i = 0; i < nY; ++i) {
+      ArgumentChecker.isTrue(y[i] > 0.0, "All values in y must be positive");
+    }
     return new ArrayInterpolator1DDataBundle(x, y, true);
   }
 
