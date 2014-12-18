022b939ae3f1aa251546d9fca8833f062241b2ac
Yuki Iwashita <yuki@opengamma.com>
2014-12-18 16:21:20 +0000

    [PLAT-6878] Square linear interpolation

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/interpolation/SquareLinearInterpolator1D.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/interpolation/SquareLinearInterpolator1D.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
  * 
  * Please see distribution for license.
  */
@@ -13,37 +13,34 @@ import com.opengamma.analytics.math.interpolation.data.Interpolator1DDataBundle;
 import com.opengamma.util.ArgumentChecker;
 
 /**
- * A one-dimensional interpolator.
- * The interpolation is linear on x y^2. The interpolator is used for interpolation on integrated variance for options.
+ * The interpolation is linear on y^2. The interpolator is used for interpolation on variance for options.
  * All values of y must be positive. 
  */
-public class TimeSquareInterpolator1D extends Interpolator1D {
+public class SquareLinearInterpolator1D extends Interpolator1D {
   private static final long serialVersionUID = 1L;
 
   @Override
   public Double interpolate(final Interpolator1DDataBundle data, final Double value) {
     Validate.notNull(value, "Value to be interpolated must not be null");
-    ArgumentChecker.isTrue(value > 0, "Value should be stricly positive");
     Validate.notNull(data, "Data bundle must not be null");
-    final InterpolationBoundedValues boundedValues = data.getBoundedValues(value);
-    final double x1 = boundedValues.getLowerBoundKey();
-    final double y1 = boundedValues.getLowerBoundValue();
+    InterpolationBoundedValues boundedValues = data.getBoundedValues(value);
+    double x1 = boundedValues.getLowerBoundKey();
+    double y1 = boundedValues.getLowerBoundValue();
     if (boundedValues.getLowerBoundIndex() == data.size() - 1) {
       return y1;
     }
-    final double x2 = boundedValues.getHigherBoundKey();
-    final double y2 = boundedValues.getHigherBoundValue();
-    final double w = (x2 - value) / (x2 - x1);
-    final double xy21 = x1 * y1 * y1;
-    final double xy22 = x2 * y2 * y2;
-    final double xy2 = w * xy21 + (1 - w) * xy22;
-    return Math.sqrt(xy2 / value);
+    double x2 = boundedValues.getHigherBoundKey();
+    double y2 = boundedValues.getHigherBoundValue();
+    double w = (x2 - value) / (x2 - x1);
+    double y21 = y1 * y1;
+    double y22 = y2 * y2;
+    double ySq = w * y21 + (1.0 - w) * y22;
+    return Math.sqrt(ySq);
   }
 
   @Override
   public double firstDerivative(final Interpolator1DDataBundle data, final Double value) {
     Validate.notNull(value, "Value to be interpolated must not be null");
-    ArgumentChecker.isTrue(value > 0, "Value should be stricly positive");
     Validate.notNull(data, "Data bundle must not be null");
     int lowerIndex = data.getLowerBoundIndex(value);
     int index;
@@ -56,42 +53,37 @@ public class TimeSquareInterpolator1D extends Interpolator1D {
     double y1 = data.getValues()[index];
     double x2 = data.getKeys()[index + 1];
     double y2 = data.getValues()[index + 1];
-    final double w = (x2 - value) / (x2 - x1);
-    final double xy21 = x1 * y1 * y1;
-    final double xy22 = x2 * y2 * y2;
-    final double xy2 = w * xy21 + (1 - w) * xy22;
-    return 0.5 * (-Math.sqrt(xy2 / value) + (-xy21 + xy22) / (x2 - x1) / Math.sqrt(xy2 / value)) / value;
+    double w = (x2 - value) / (x2 - x1);
+    double y21 = y1 * y1;
+    double y22 = y2 * y2;
+    double ySq = w * y21 + (1.0 - w) * y22;
+    return 0.5 * (y22 - y21) / (x2 - x1) / Math.sqrt(ySq);
   }
 
   @Override
   public double[] getNodeSensitivitiesForValue(final Interpolator1DDataBundle data, final Double value) {
     Validate.notNull(value, "Value to be interpolated must not be null");
     Validate.notNull(data, "Data bundle must not be null");
-    final int n = data.size();
-    final double[] resultSensitivity = new double[n];
-    final InterpolationBoundedValues boundedValues = data.getBoundedValues(value);
-    final double x1 = boundedValues.getLowerBoundKey();
-    final double y1 = boundedValues.getLowerBoundValue();
-    final int index = boundedValues.getLowerBoundIndex();
+    int n = data.size();
+    double[] resultSensitivity = new double[n];
+    InterpolationBoundedValues boundedValues = data.getBoundedValues(value);
+    double x1 = boundedValues.getLowerBoundKey();
+    double y1 = boundedValues.getLowerBoundValue();
+    int index = boundedValues.getLowerBoundIndex();
     if (index == n - 1) {
       resultSensitivity[n - 1] = 1.0;
       return resultSensitivity;
     }
-    final double x2 = boundedValues.getHigherBoundKey();
-    final double y2 = boundedValues.getHigherBoundValue();
-    final double w = (x2 - value) / (x2 - x1);
-    final double xy21 = x1 * y1 * y1;
-    final double xy22 = x2 * y2 * y2;
-    final double xy2 = w * xy21 + (1 - w) * xy22;
-    final double resultValue = Math.sqrt(xy2 / value);
-    final double resultValueBar = 1.0;
-    final double xy2Bar = 0.5 / resultValue / value * resultValueBar;
-    final double xy21Bar = w * xy2Bar;
-    final double xy22Bar = (1 - w) * xy2Bar;
-    final double y2Bar = 2 * x2 * y2 * xy22Bar;
-    final double y1Bar = 2 * x1 * y1 * xy21Bar;
-    resultSensitivity[index] = y1Bar;
-    resultSensitivity[index + 1] = y2Bar;
+    double x2 = boundedValues.getHigherBoundKey();
+    double y2 = boundedValues.getHigherBoundValue();
+    double w2 = (x2 - value) / (x2 - x1);
+    double w1 = 1.0 - w2;
+    double y21 = y1 * y1;
+    double y22 = y2 * y2;
+    double ySq = w2 * y21 + w1 * y22;
+    double resultValue = Math.sqrt(ySq);
+    resultSensitivity[index] = w2 * y1 / resultValue;
+    resultSensitivity[index + 1] = w1 * y2 / resultValue;
     return resultSensitivity;
   }
 
