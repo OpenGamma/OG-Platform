15f6c2fc548b8e8ae6c84a539f794f159018f6d0
marc-opengamma <marc@opengamma.com>
2015-01-22 15:17:18 +0000

    [PLAT-6888] Extended the TS difference operator to accept flexible lag.

---
+++ projects/OG-Analytics/src/test/java/com/opengamma/analytics/financial/timeseries/util/TimeSeriesDifferenceOperatorTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.analytics.financial.timeseries.util;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.testng.annotations.Test;
+import org.threeten.bp.LocalDate;
+
+import com.opengamma.timeseries.date.DateDoubleTimeSeries;
+import com.opengamma.timeseries.date.localdate.LocalDateDoubleTimeSeries;
+
+/**
+ * Tests {@link TimeSeriesDifferenceOperator}
+ */
+public class TimeSeriesDifferenceOperatorTest {
+  
+  private static final LocalDateDoubleTimeSeries TS_1 = TimeSeriesDataSet.timeSeriesGbpLibor3M2014Jan(LocalDate.of(2014, 2, 1));
+  private static final int NB_DATA_1 = TS_1.size();
+  
+  private static final TimeSeriesDifferenceOperator OP_DIF_1 = new TimeSeriesDifferenceOperator();
+  private static final TimeSeriesDifferenceOperator OP_DIF_2 = new TimeSeriesDifferenceOperator(2);
+  
+  private static final double TOLERANCE_DIFF = 1.0E-10;
+  
+  /**
+   * Test the difference operator for a standard lag of 1 element.
+   */
+  @Test
+  public void difference1() {
+    DateDoubleTimeSeries<?> tsDiff1 = OP_DIF_1.evaluate(TS_1);
+    assertEquals(NB_DATA_1 - 1, tsDiff1.size());
+    for (int i = 1; i < NB_DATA_1; i++) {
+      LocalDate dateTs = TS_1.getTimeAtIndex(i);
+      LocalDate dateDiff = (LocalDate) tsDiff1.getTimeAtIndex(i - 1);
+      assertEquals(dateTs, dateDiff);
+      double diffComputed = tsDiff1.getValueAtIndex(i - 1);
+      double diffExpected = TS_1.getValueAtIndex(i) - TS_1.getValueAtIndex(i - 1);
+      assertEquals(diffExpected, diffComputed, TOLERANCE_DIFF);
+    }
+  }
+  
+  /** Tests the difference operator for a lag of 2 elements. */
+  @Test
+  public void difference2() {
+    DateDoubleTimeSeries<?> tsDiff = OP_DIF_2.evaluate(TS_1);
+    assertEquals(NB_DATA_1 - 2, tsDiff.size());
+    for (int i = 2; i < NB_DATA_1; i++) {
+      LocalDate dateTs = TS_1.getTimeAtIndex(i);
+      LocalDate dateDiff = (LocalDate) tsDiff.getTimeAtIndex(i - 2);
+      assertEquals(dateTs, dateDiff);
+      double diffComputed = tsDiff.getValueAtIndex(i - 2);
+      double diffExpected = TS_1.getValueAtIndex(i) - TS_1.getValueAtIndex(i - 2);
+      assertEquals(diffExpected, diffComputed, TOLERANCE_DIFF);
+    }
+  }
+  
+}
