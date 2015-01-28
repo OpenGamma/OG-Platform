80c424f50d7c1a6d4e20ed95d8f034427c84b1a4
Chris Kent <chris@opengamma.com>
2015-01-28 16:19:54 +0000

    PLT-603 - Fix Javadoc

--- sesame-engine/src/main/java/com/opengamma/sesame/marketdata/scenarios/PerturbationTarget.java
+++ sesame-engine/src/main/java/com/opengamma/sesame/marketdata/scenarios/PerturbationTarget.java
@@ -6,21 +6,22 @@
 package com.opengamma.sesame.marketdata.scenarios;
 
 /**
- * The type of the data affected by a {@link Perturbation}. It can raw input data used for building market data
- * or the market data itself.
+ * The type of the data affected by a {@link Perturbation}. A perturbation can be applied to the raw input data
+ * used for building market data or to the market data itself.
  */
 public enum PerturbationTarget {
 
   /**
-   * The perturbation transforms raw input data used to build market data, for example the quotes used
+   * The perturbation transforms raw input data used to build market data, for example by shifting the quotes used
    * when calibrating a curve.
    * <p>
-   * For this target type, the market data type of the filter is not the same as the data type of the perturbation.
+   * For this target type, the market data type of the filter is not necessarily the same as the
+   * data type transformed by the perturbation.
    */
   INPUT,
 
   /**
-   * The perturbation build market data, for example applying a parallel shift to a curve.
+   * The perturbation transforms built market data, for example by applying a parallel shift to a curve.
    * <p>
    * For this target type, the market data type of the filter is the same as the data type of the perturbation.
    */
