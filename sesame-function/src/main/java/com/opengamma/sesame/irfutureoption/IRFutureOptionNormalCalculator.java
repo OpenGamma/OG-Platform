33f45b11b70c326e963f1409b26a3777bfe15550
Ian Turner <ian@opengamma.com>
2015-02-12 16:54:43 +0000

    LNTN-255 normal model working for stir future options

--- sesame-function/src/main/java/com/opengamma/sesame/irfutureoption/IRFutureOptionNormalCalculator.java
+++ sesame-function/src/main/java/com/opengamma/sesame/irfutureoption/IRFutureOptionNormalCalculator.java
@@ -2,38 +2,27 @@ package com.opengamma.sesame.irfutureoption;
 
 import java.util.Map;
 
-import org.threeten.bp.ZonedDateTime;
-
-import com.opengamma.analytics.financial.instrument.InstrumentDefinition;
-import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
 import com.opengamma.analytics.financial.interestrate.future.calculator.FuturesPriceMulticurveCalculator;
 import com.opengamma.analytics.financial.interestrate.future.calculator.FuturesPriceNormalSTIRFuturesCalculator;
-import com.opengamma.analytics.financial.provider.calculator.discounting.PV01CurveParametersCalculator;
+import com.opengamma.analytics.financial.interestrate.future.derivative.FuturesTransaction;
+import com.opengamma.analytics.financial.interestrate.future.derivative.InterestRateFutureOptionSecurity;
 import com.opengamma.analytics.financial.provider.calculator.normalstirfutures.PositionDeltaNormalSTIRFutureOptionCalculator;
 import com.opengamma.analytics.financial.provider.calculator.normalstirfutures.PositionGammaNormalSTIRFutureOptionCalculator;
 import com.opengamma.analytics.financial.provider.calculator.normalstirfutures.PositionThetaNormalSTIRFutureOptionCalculator;
 import com.opengamma.analytics.financial.provider.calculator.normalstirfutures.PositionVegaNormalSTIRFutureOptionCalculator;
 import com.opengamma.analytics.financial.provider.calculator.normalstirfutures.PresentValueCurveSensitivityNormalSTIRFuturesCalculator;
 import com.opengamma.analytics.financial.provider.calculator.normalstirfutures.PresentValueNormalSTIRFuturesCalculator;
-import com.opengamma.analytics.financial.provider.description.interestrate.BlackSTIRFuturesProviderInterface;
 import com.opengamma.analytics.financial.provider.description.interestrate.NormalSTIRFuturesExpSimpleMoneynessProviderDiscount;
 import com.opengamma.analytics.financial.provider.description.interestrate.NormalSTIRFuturesProviderInterface;
 import com.opengamma.analytics.financial.provider.sensitivity.multicurve.MultipleCurrencyMulticurveSensitivity;
 import com.opengamma.analytics.financial.provider.sensitivity.multicurve.MultipleCurrencyParameterSensitivity;
 import com.opengamma.analytics.financial.provider.sensitivity.parameter.ParameterSensitivityParameterCalculator;
-import com.opengamma.analytics.util.amount.ReferenceAmount;
-import com.opengamma.financial.analytics.conversion.FixedIncomeConverterDataProvider;
-import com.opengamma.financial.analytics.conversion.InterestRateFutureOptionTradeConverter;
 import com.opengamma.financial.analytics.model.fixedincome.BucketedCurveSensitivities;
-import com.opengamma.financial.analytics.timeseries.HistoricalTimeSeriesBundle;
 import com.opengamma.sesame.CurveMatrixLabeller;
 import com.opengamma.sesame.ZeroIRDeltaBucketingUtils;
-import com.opengamma.sesame.trade.IRFutureOptionTrade;
 import com.opengamma.util.ArgumentChecker;
-import com.opengamma.util.money.Currency;
 import com.opengamma.util.money.MultipleCurrencyAmount;
 import com.opengamma.util.result.Result;
-import com.opengamma.util.tuple.Pair;
 
 /**
  * Interest rate future option Normal calculator.
@@ -48,8 +37,6 @@ public class IRFutureOptionNormalCalculator implements IRFutureOptionCalculator 
       PresentValueCurveSensitivityNormalSTIRFuturesCalculator.getInstance();
   private static final ParameterSensitivityParameterCalculator<NormalSTIRFuturesProviderInterface> PSSFC =
       new ParameterSensitivityParameterCalculator<>(PV01_CALC);
-  //private static final PV01CurveParametersCalculator<NormalSTIRFuturesProviderInterface> PV01_CALC =
-  //    new PV01CurveParametersCalculator<>(PVSC);
   private static final PositionDeltaNormalSTIRFutureOptionCalculator DELTA_CALC =
       PositionDeltaNormalSTIRFutureOptionCalculator.getInstance();
   private static final PositionGammaNormalSTIRFutureOptionCalculator GAMMA_CALC =
@@ -58,75 +45,58 @@ public class IRFutureOptionNormalCalculator implements IRFutureOptionCalculator 
       PositionThetaNormalSTIRFutureOptionCalculator.getInstance();
   private static final PositionVegaNormalSTIRFutureOptionCalculator VEGA_CALC =
       PositionVegaNormalSTIRFutureOptionCalculator.getInstance();
-  private static final FuturesPriceMulticurveCalculator FPC =
-      FuturesPriceMulticurveCalculator.getInstance();
 
-  private final InstrumentDerivative _derivative;
+  private final FuturesTransaction<InterestRateFutureOptionSecurity> _derivative;
   private final Map<String, CurveMatrixLabeller> _curveLabellers;
-  private final NormalSTIRFuturesExpSimpleMoneynessProviderDiscount _normalMulticurves;
+  private final NormalSTIRFuturesExpSimpleMoneynessProviderDiscount _normalMulticurve;
 
 
-  public IRFutureOptionNormalCalculator(IRFutureOptionTrade trade,
-                                        InterestRateFutureOptionTradeConverter converter,
-                                        NormalSTIRFuturesExpSimpleMoneynessProviderDiscount normalMulticurves,
-                                        ZonedDateTime valTime,
-                                        FixedIncomeConverterDataProvider definitionToDerivativeConverter,
-                                        HistoricalTimeSeriesBundle fixings,
+  public IRFutureOptionNormalCalculator(FuturesTransaction<InterestRateFutureOptionSecurity> derivative,
+                                        NormalSTIRFuturesExpSimpleMoneynessProviderDiscount normalMulticurve,
                                         Map<String, CurveMatrixLabeller> curveLabellers) {
-    ArgumentChecker.notNull(trade, "trade");
-    ArgumentChecker.notNull(converter, "converter");
-    ArgumentChecker.notNull(valTime, "valTime");
-    ArgumentChecker.notNull(definitionToDerivativeConverter, "definitionToDerivativeConverter");
-    ArgumentChecker.notNull(fixings, "fixings");
-
-    _normalMulticurves = ArgumentChecker.notNull(normalMulticurves, "normalMulticurves");
+    _normalMulticurve = ArgumentChecker.notNull(normalMulticurve, "normalMulticurves");
     _curveLabellers = ArgumentChecker.notNull(curveLabellers, "curveLabellers");
-    _derivative = IRFutureOptionFnUtils.createDerivative(trade,
-                                                         converter,
-                                                         valTime,
-                                                         definitionToDerivativeConverter,
-                                                         fixings);
+    _derivative = ArgumentChecker.notNull(derivative, "derivative");
   }
-
 
   @Override
   public Result<MultipleCurrencyAmount> calculatePV() {
-    return Result.success(_derivative.accept(PV_CALC, _normalMulticurves));
+    return Result.success(_derivative.accept(PV_CALC, _normalMulticurve));
   }
 
   @Override
   public Result<MultipleCurrencyMulticurveSensitivity> calculatePV01() {
-    return Result.success(_derivative.accept(PV01_CALC, _normalMulticurves));
+    return Result.success(_derivative.accept(PV01_CALC, _normalMulticurve));
   }
 
   @Override
   public Result<Double> calculateModelPrice() {
-    return Result.success(_derivative.accept(PRICE_CALC, _normalMulticurves));
+    return Result.success(_derivative.accept(PRICE_CALC, _normalMulticurve));
   }
 
   @Override
   public Result<Double> calculateDelta() {
-    return Result.success(_derivative.accept(DELTA_CALC, _normalMulticurves));
+    return Result.success(_derivative.accept(DELTA_CALC, _normalMulticurve));
   }
 
   @Override
   public Result<Double> calculateGamma() {
-    return Result.success(_derivative.accept(GAMMA_CALC, _normalMulticurves));
+    return Result.success(_derivative.accept(GAMMA_CALC, _normalMulticurve));
   }
 
   @Override
   public Result<Double> calculateVega() {
-    return Result.success(_derivative.accept(VEGA_CALC, _normalMulticurves));
+    return Result.success(_derivative.accept(VEGA_CALC, _normalMulticurve));
   }
 
   @Override
   public Result<Double> calculateTheta() {
-    return Result.success(_derivative.accept(THETA_CALC, _normalMulticurves));
+    return Result.success(_derivative.accept(THETA_CALC, _normalMulticurve));
   }
 
   @Override
   public Result<BucketedCurveSensitivities> calculateBucketedZeroIRDelta() {
-    MultipleCurrencyParameterSensitivity sensitivities = PSSFC.calculateSensitivity(_derivative, _normalMulticurves);
+    MultipleCurrencyParameterSensitivity sensitivities = PSSFC.calculateSensitivity(_derivative, _normalMulticurve);
     BucketedCurveSensitivities bucketedCurveSensitivities =
         ZeroIRDeltaBucketingUtils.getBucketedCurveSensitivities(sensitivities, _curveLabellers);
 
--- sesame-function/src/main/java/com/opengamma/sesame/irfutureoption/IRFutureOptionNormalCalculatorFactory.java
+++ sesame-function/src/main/java/com/opengamma/sesame/irfutureoption/IRFutureOptionNormalCalculatorFactory.java
@@ -1,20 +1,17 @@
 package com.opengamma.sesame.irfutureoption;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import org.threeten.bp.Period;
-
-import com.opengamma.analytics.financial.instrument.index.IborIndex;
+import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
+import com.opengamma.analytics.financial.interestrate.future.derivative.FuturesTransaction;
+import com.opengamma.analytics.financial.interestrate.future.derivative.InterestRateFutureOptionSecurity;
+import com.opengamma.analytics.financial.interestrate.future.derivative.InterestRateFutureSecurity;
+import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurface;
 import com.opengamma.analytics.financial.provider.description.interestrate.NormalSTIRFuturesExpSimpleMoneynessProviderDiscount;
-import com.opengamma.analytics.math.interpolation.GridInterpolator2D;
-import com.opengamma.analytics.math.surface.InterpolatedDoublesSurface;
 import com.opengamma.financial.analytics.conversion.FixedIncomeConverterDataProvider;
 import com.opengamma.financial.analytics.conversion.InterestRateFutureOptionTradeConverter;
 import com.opengamma.financial.analytics.timeseries.HistoricalTimeSeriesBundle;
-import com.opengamma.financial.convention.businessday.ModifiedFollowingBusinessDayConvention;
-import com.opengamma.financial.convention.daycount.DayCounts;
 import com.opengamma.financial.security.option.IRFutureOptionSecurity;
 import com.opengamma.sesame.CurveLabellingFn;
 import com.opengamma.sesame.CurveMatrixLabeller;
@@ -22,10 +19,10 @@ import com.opengamma.sesame.DiscountingMulticurveCombinerFn;
 import com.opengamma.sesame.Environment;
 import com.opengamma.sesame.FixingsFn;
 import com.opengamma.sesame.MulticurveBundle;
+import com.opengamma.sesame.marketdata.VolatilitySurfaceId;
 import com.opengamma.sesame.trade.IRFutureOptionTrade;
 import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.result.Result;
-import com.opengamma.util.tuple.DoublesPair;
 
 /**
  * Normal calculator for interest rate future options.
@@ -38,7 +35,7 @@ public class IRFutureOptionNormalCalculatorFactory implements IRFutureOptionCalc
   private final CurveLabellingFn _curveLabellingFn;
   private final DiscountingMulticurveCombinerFn _multicurveFn;
   private final boolean _moneynessOnPrice;
-  private final GridInterpolator2D _interpolator;
+  private final String _volSurfaceName;
 
   public IRFutureOptionNormalCalculatorFactory(InterestRateFutureOptionTradeConverter converter,
                                                FixedIncomeConverterDataProvider definitionToDerivativeConverter,
@@ -46,73 +43,60 @@ public class IRFutureOptionNormalCalculatorFactory implements IRFutureOptionCalc
                                                FixingsFn fixingsFn,
                                                CurveLabellingFn curveLabellingFn,
                                                boolean moneynessOnPrice,
-                                               GridInterpolator2D interpolator) {
+                                               String volSurfaceName) {
     _converter = ArgumentChecker.notNull(converter, "converter");
     _definitionToDerivativeConverter =
         ArgumentChecker.notNull(definitionToDerivativeConverter, "definitionToDerivativeConverter");
     _fixingsFn = ArgumentChecker.notNull(fixingsFn, "fixingsFn");
     _curveLabellingFn = ArgumentChecker.notNull(curveLabellingFn, "curveLabellingFn");
-    //TODO is it right to wire this in here?
     _multicurveFn = ArgumentChecker.notNull(multicurveFn, "multicurveFn");
     _moneynessOnPrice = ArgumentChecker.notNull(moneynessOnPrice, "moneynessOnPrice");
-    _interpolator = ArgumentChecker.notNull(interpolator, "interpolator");
+    _volSurfaceName = ArgumentChecker.notNull(volSurfaceName, "volSurfaceName");
+
   }
 
   @Override
   public Result<IRFutureOptionCalculator> createCalculator(Environment env, IRFutureOptionTrade trade) {
 
     IRFutureOptionSecurity security = trade.getSecurity();
+    Result<MulticurveBundle> multicurveBundle = _multicurveFn.getMulticurveBundle(env, trade);
+    Result<VolatilitySurface> surfaceResult =
+        env.getMarketDataBundle().get(VolatilitySurfaceId.of(_volSurfaceName), VolatilitySurface.class);
     Result<HistoricalTimeSeriesBundle> fixingsResult = _fixingsFn.getFixingsForSecurity(env, security);
 
-    Result<MulticurveBundle> multicurveBundle = _multicurveFn.getMulticurveBundle(env, trade);
-
-    if (!multicurveBundle.isSuccess()) {
-      return Result.failure(multicurveBundle);
+    if (Result.anyFailures(multicurveBundle, surfaceResult, fixingsResult)) {
+      return Result.failure(multicurveBundle, surfaceResult, fixingsResult);
     }
 
+    FuturesTransaction<InterestRateFutureOptionSecurity> derivative =
+        IRFutureOptionFnUtils.createDerivative(trade,
+                                               _converter,
+                                               env.getValuationTime(),
+                                               _definitionToDerivativeConverter,
+                                               fixingsResult.getValue());
+
+    VolatilitySurface volSurface = surfaceResult.getValue();
     MulticurveBundle bundle = multicurveBundle.getValue();
-    NormalSTIRFuturesExpSimpleMoneynessProviderDiscount normalMulticurve = createNormalMulticurve(security, bundle);
+
+    InterestRateFutureSecurity underlyingFuture = derivative.getUnderlyingSecurity().getUnderlyingFuture();
+    NormalSTIRFuturesExpSimpleMoneynessProviderDiscount normalMulticurve =
+        new NormalSTIRFuturesExpSimpleMoneynessProviderDiscount(bundle.getMulticurveProvider(),
+                                                                volSurface.getSurface(),
+                                                                underlyingFuture.getIborIndex(),
+                                                                _moneynessOnPrice);
+
     Set<String> curveNames = normalMulticurve.getMulticurveProvider().getAllCurveNames();
     Result<Map<String, CurveMatrixLabeller>> curveLabellers = _curveLabellingFn.getCurveLabellers(curveNames);
 
-
-    if (Result.anyFailures(fixingsResult, curveLabellers)) {
-     return Result.failure(fixingsResult, curveLabellers);
-    } else {
-
-
-      IRFutureOptionCalculator calculator = new IRFutureOptionNormalCalculator(trade,
-                                                                               _converter,
-                                                                               normalMulticurve,
-                                                                               env.getValuationTime(),
-                                                                               _definitionToDerivativeConverter,
-                                                                               fixingsResult.getValue(),
-                                                                               curveLabellers.getValue());
-      return Result.success(calculator);
+    if (Result.anyFailures(curveLabellers)) {
+     return Result.failure(curveLabellers);
     }
 
-  }
-
-  private NormalSTIRFuturesExpSimpleMoneynessProviderDiscount createNormalMulticurve(IRFutureOptionSecurity security,
-                                                                                     MulticurveBundle bundle) {
-
-
-    //TODO where does this come from?
-    Map<DoublesPair, Double> vols = new HashMap<DoublesPair, Double>();
-    InterpolatedDoublesSurface surface = new InterpolatedDoublesSurface(vols, _interpolator);
-    //TODO where does this come from?
-    IborIndex index = new IborIndex(security.getCurrency(),
-                                    Period.ofMonths(3),
-                                    0,
-                                    DayCounts.ACT_360,
-                                    new ModifiedFollowingBusinessDayConvention(),
-                                    false,
-                                    "");
-
-    return new NormalSTIRFuturesExpSimpleMoneynessProviderDiscount(bundle.getMulticurveProvider(),
-                                                                   surface,
-                                                                   index,
-                                                                   _moneynessOnPrice);
+    IRFutureOptionCalculator calculator = new IRFutureOptionNormalCalculator(derivative,
+                                                                             normalMulticurve,
+                                                                             curveLabellers.getValue());
+    return Result.success(calculator);
 
   }
+
 }
