6c740d7ac95f8297149ea328b33df0d7611085b3
Ian Turner <ian@opengamma.com>
2015-02-13 10:40:50 +0000

    LNTN-255 add javadocs

--- sesame-function/src/main/java/com/opengamma/sesame/irfutureoption/IRFutureOptionNormalCalculatorFactory.java
+++ sesame-function/src/main/java/com/opengamma/sesame/irfutureoption/IRFutureOptionNormalCalculatorFactory.java
@@ -1,9 +1,13 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.sesame.irfutureoption;
 
 import java.util.Map;
 import java.util.Set;
 
-import com.opengamma.analytics.financial.interestrate.InstrumentDerivative;
 import com.opengamma.analytics.financial.interestrate.future.derivative.FuturesTransaction;
 import com.opengamma.analytics.financial.interestrate.future.derivative.InterestRateFutureOptionSecurity;
 import com.opengamma.analytics.financial.interestrate.future.derivative.InterestRateFutureSecurity;
