45491bd1ad3ef9911f88ba160cd97315dff23dd2
Ian Turner <ian@opengamma.com>
2015-03-18 18:32:07 +0000

    REQS-15 delete examples

--- examples/examples-bloomberg/scripts/load-timeseries.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-`dirname $0`/run-tool.sh com.opengamma.integration.tool.marketdata.TimeSeriesLoaderTool $@ -c config/toolcontext/toolcontext-examplesbloomberg-bin.properties -l com/opengamma/util/warn-logback.xml
--- examples/examples-simulated/scripts/config-import-export-tool.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-`dirname $0`/run-tool.sh com.opengamma.integration.tool.config.ConfigImportExportTool -c classpath:toolcontext/toolcontext-examplessimulated.properties -l com/opengamma/util/info-logback.xml
