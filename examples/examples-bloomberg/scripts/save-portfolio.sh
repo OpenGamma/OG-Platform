582f6857434f3f8df62b3fa73b43260b9094c4cc
Ian Turner <ian@opengamma.com>
2015-03-18 18:32:07 +0000

    REQS-15 delete examples

--- examples/examples-bloomberg/scripts/save-portfolio.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-`dirname $0`/run-tool.sh com.opengamma.integration.tool.portfolio.PortfolioSaverTool $@ -c classpath:toolcontext/toolcontext-examplesbloomberg-bin.properties -l com/opengamma/util/warn-logback.xml
