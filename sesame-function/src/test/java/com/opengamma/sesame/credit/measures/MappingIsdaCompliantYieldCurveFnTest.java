4343823f2ca65d46e3537dcd45b1bfdc4487338a
Ian Turner <ian@opengamma.com>
2015-03-30 19:33:50 +0100

    PLT-697 add zero rate tests and docs on mapping

--- sesame-function/src/test/java/com/opengamma/sesame/credit/measures/MappingIsdaCompliantYieldCurveFnTest.java
+++ sesame-function/src/test/java/com/opengamma/sesame/credit/measures/MappingIsdaCompliantYieldCurveFnTest.java
@@ -18,6 +18,7 @@ import org.threeten.bp.ZonedDateTime;
 import com.google.common.collect.ImmutableMap;
 import com.opengamma.analytics.financial.model.interestrate.curve.DiscountCurve;
 import com.opengamma.analytics.financial.model.interestrate.curve.YieldAndDiscountCurve;
+import com.opengamma.analytics.financial.model.interestrate.curve.YieldCurve;
 import com.opengamma.analytics.financial.provider.curve.CurveBuildingBlockBundle;
 import com.opengamma.analytics.financial.provider.description.interestrate.MulticurveProviderDiscount;
 import com.opengamma.analytics.math.curve.InterpolatedDoublesCurve;
@@ -94,10 +95,8 @@ public class MappingIsdaCompliantYieldCurveFnTest {
 
   }
 
-  @BeforeMethod
-  private MarketDataEnvironment getSuppliedData() {
+  private MarketDataEnvironment getSuppliedData(MulticurveBundle multicurveBundle) {
 
-    MulticurveBundle multicurveBundle = getMulticurveBundle();
     MulticurveId multicurveId = MulticurveId.of("Curve Bundle");
     MarketDataEnvironment suppliedData = new MarketDataEnvironmentBuilder()
         .add(multicurveId, multicurveBundle)
@@ -106,43 +105,66 @@ public class MappingIsdaCompliantYieldCurveFnTest {
   return suppliedData;
   }
 
+
   @Test
-  public void testStandardCdsPV() {
+  public void testDiscountingStandardCdsPV() {
 
     final StandardCDSSecurity security = CreditPricingSampleData.createStandardCDSSecurity();
     double tolerance = security.getNotional().getAmount() * BP;
-    Result<CurrencyAmount> postResult = _functionRunner.runFunction(ARGS, getSuppliedData(), new Function<Environment, Result<CurrencyAmount>>() {
+    Result<CurrencyAmount> result = _functionRunner.runFunction(ARGS, getSuppliedData(getDiscountCurveBundle()), new Function<Environment, Result<CurrencyAmount>>() {
       @Override
       public Result<CurrencyAmount> apply(Environment env) {
         return _pvFunction.priceStandardCds(env, security);
       }
     });
 
-    assertThat(postResult.isSuccess(), is(true));
-    CurrencyAmount postCa = postResult.getValue();
-    assertThat(postCa.getCurrency(), is(Currency.USD));
-    assertThat(postCa.getAmount(), is(closeTo(CreditPvFnTest.SINGLE_NAME_EXPECTED_PV, tolerance)));
+    assertThat(result.isSuccess(), is(true));
+    CurrencyAmount amount = result.getValue();
+    assertThat(amount.getCurrency(), is(Currency.USD));
+    assertThat(amount.getAmount(), is(closeTo(CreditPvFnTest.SINGLE_NAME_EXPECTED_PV, tolerance)));
 
-    assertThat(postCa.getAmount(), is(closeTo(SINGLE_NAME_EXPECTED_PV, STD_TOLERANCE_PV)));
+    assertThat(amount.getAmount(), is(closeTo(SINGLE_NAME_EXPECTED_PV, STD_TOLERANCE_PV)));
 
   }
 
   @Test
-  public void testIndexCdsPV() {
+  public void testYieldCurveStandardCdsPV() {
+
+    final StandardCDSSecurity security = CreditPricingSampleData.createStandardCDSSecurity();
+    double tolerance = security.getNotional().getAmount() * BP;
+    Result<CurrencyAmount> result = _functionRunner.runFunction(ARGS, getSuppliedData(getYieldCurveBundle()), new Function<Environment, Result<CurrencyAmount>>() {
+      @Override
+      public Result<CurrencyAmount> apply(Environment env) {
+        return _pvFunction.priceStandardCds(env, security);
+      }
+    });
+
+    assertThat(result.isSuccess(), is(true));
+    CurrencyAmount amount = result.getValue();
+    assertThat(amount.getCurrency(), is(Currency.USD));
+    assertThat(amount.getAmount(), is(closeTo(CreditPvFnTest.SINGLE_NAME_EXPECTED_PV, tolerance)));
+
+    assertThat(amount.getAmount(), is(closeTo(SINGLE_NAME_EXPECTED_PV, STD_TOLERANCE_PV)));
+
+  }
+
+
+  @Test
+  public void testDiscountingIndexCdsPV() {
 
     final IndexCDSSecurity security = CreditPricingSampleData.createIndexCDSSecurity();
     double tolerance = security.getNotional().getAmount() * BP;
-    Result<CurrencyAmount> postResult = _functionRunner.runFunction(ARGS, getSuppliedData(), new Function<Environment, Result<CurrencyAmount>>() {
+    Result<CurrencyAmount> result = _functionRunner.runFunction(ARGS, getSuppliedData(getDiscountCurveBundle()), new Function<Environment, Result<CurrencyAmount>>() {
       @Override
       public Result<CurrencyAmount> apply(Environment env) {
         return _pvFunction.priceIndexCds(env, security);
       }
     });
 
-    assertThat(postResult.isSuccess(), is(true));
-    CurrencyAmount postCa = postResult.getValue();
-    assertThat(postCa.getCurrency(), is(Currency.USD));
-    assertThat(postCa.getAmount(), is(closeTo(CreditPvFnTest.INDEX_EXPECTED_PV, tolerance)));
+    assertThat(result.isSuccess(), is(true));
+    CurrencyAmount amount = result.getValue();
+    assertThat(amount.getCurrency(), is(Currency.USD));
+    assertThat(amount.getAmount(), is(closeTo(CreditPvFnTest.INDEX_EXPECTED_PV, tolerance)));
 
   }
 
@@ -157,6 +179,15 @@ public class MappingIsdaCompliantYieldCurveFnTest {
     }
   }
 
+  private static final double[] ZERO_RATES = new double[] {6.85141288906939E-4, 6.927004450275529E-4,
+      7.052829705582995E-4, 7.229431397828344E-4, 7.47375407385545E-4, 7.834380855687433E-4,
+      8.347416042149961E-4, 9.038326597250597E-4, 0.0013722796143422762, 0.002176305350143962,
+      0.0032811647971705956, 0.00460755549100302, 0.006040682945580618, 0.00750878742104914,
+      0.008941312543172845, 0.010292758463102044, 0.012637449883912839, 0.015245511253328924,
+      0.01770642792742225, 0.018777178300023893, 0.01925642557322497, 0.01971509345664727,
+      0.019728349624112827};
+
+
   private static final double[] DISCOUNT_FACTOR = new double[] {0.999829198540859, 0.9996527611741278,
       0.9994706948329838, 0.9992773181206462, 0.999064682720019, 0.9988244611565431, 0.9985396977323168,
       0.9981914957448704, 0.9958878795040178, 0.9913266485465384, 0.9837191754362143, 0.9727087439132421,
@@ -164,11 +195,24 @@ public class MappingIsdaCompliantYieldCurveFnTest {
       0.7954480462459721, 0.7016145412095421, 0.6251659631564156, 0.5609548680303263, 0.45423350825036723,
       0.3726483241596532 };
 
-  private static MulticurveBundle getMulticurveBundle() {
+  private static MulticurveBundle getDiscountCurveBundle() {
 
     String curveName = "USD DISC";
     InterpolatedDoublesCurve rawCurve = InterpolatedDoublesCurve.from(TIME, DISCOUNT_FACTOR, s_interpolator, curveName);
     YieldAndDiscountCurve yc = new DiscountCurve(curveName, rawCurve);
+    MulticurveProviderDiscount multicurveProvider = new MulticurveProviderDiscount();
+    CurveBuildingBlockBundle curveBuildingBlockBundle = new CurveBuildingBlockBundle();
+    multicurveProvider.setCurve(Currency.USD, yc);
+    MulticurveBundle bundle = new MulticurveBundle(multicurveProvider, curveBuildingBlockBundle);
+    return bundle;
+
+  }
+
+  private static MulticurveBundle getYieldCurveBundle() {
+
+    String curveName = "USD DISC";
+    InterpolatedDoublesCurve rawCurve = InterpolatedDoublesCurve.from(TIME, ZERO_RATES, s_interpolator, curveName);
+    YieldAndDiscountCurve yc = new YieldCurve(curveName, rawCurve);
     MulticurveProviderDiscount multicurveProvider = new MulticurveProviderDiscount();
     CurveBuildingBlockBundle curveBuildingBlockBundle = new CurveBuildingBlockBundle();
     multicurveProvider.setCurve(Currency.USD, yc);
