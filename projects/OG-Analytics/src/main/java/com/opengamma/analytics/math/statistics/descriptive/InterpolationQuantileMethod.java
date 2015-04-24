572982df8dd90c22f56d02a64cde5cbb79441b4c
marc-opengamma <marc@opengamma.com>
2015-04-24 11:17:20 +0100

    [PLT-865] Created quantile calculator hierarchy. Implemented 4 versions: discrete (nearest and index above) and interpolation (midway and sample points).

---
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/math/statistics/descriptive/InterpolationQuantileMethod.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.analytics.math.statistics.descriptive;
+
+import com.opengamma.util.ArgumentChecker;
+
+/**
+ * Implementation of a quantile estimator.
+ * The quantile is linearly interpolated between two sample values. The probability dimension 
+ * <i>p<subscript>i</subscript> on which the interpolation take place (X axis) varies between actual
+ * implementation of the abstract class. For each probability <i>p<subscript>i</subscript></i>, the cumulative 
+ * distribution value is the sample value with same index. 
+ * The index used above are the Java index plus 1.
+ * <p> Reference: Value-At-Risk, OpenGamma Documentation 31, Version 0.1, April 2015.
+ */
+public abstract class InterpolationQuantileMethod extends QuantileCalculationMethod {
+
+  @Override
+  public double quantileFromSorted(double level, double[] sortedSample) {
+    ArgumentChecker.isTrue(level > 0, "Quantile should be above 0.");
+    ArgumentChecker.isTrue(level < 1, "Quantile should be below 1.");
+    int sampleSize = sortedSample.length;
+    double adjustedLevel = level * sampleSize + indexCorrection();
+    int lowerIndex = (int) Math.floor(adjustedLevel);
+    int upperIndex = (int) Math.ceil(adjustedLevel);
+    double lowerWeight = upperIndex - adjustedLevel;
+    double upperWeight = 1.0d - lowerWeight;
+    return lowerWeight * sortedSample[lowerIndex - 1] + upperWeight * sortedSample[upperIndex - 1];
+  }
+  
+  /**
+   * Internal method returning the index correction for the specific implementation.
+   * @return The correction.
+   */
+  abstract double indexCorrection();
+
+}
