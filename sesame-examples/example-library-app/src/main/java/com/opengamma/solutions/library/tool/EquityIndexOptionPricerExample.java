d3ce36b9c8dc9f4b117306daa86d52af74e8e8e8
Ian Turner <ian@opengamma.com>
2015-05-15 16:46:30 +0100

    PLT-896 example pricer

---
+++ sesame-examples/example-library-app/src/main/java/com/opengamma/solutions/library/tool/EquityIndexOptionPricerExample.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.solutions.library.tool;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.threeten.bp.ZonedDateTime;
+
+import com.google.common.collect.Sets;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.opengamma.sesame.engine.Results;
+import com.opengamma.solutions.library.engine.EngineModule;
+import com.opengamma.solutions.library.storage.DataLoadModule;
+import com.opengamma.solutions.library.storage.InMemoryStorageModule;
+import com.opengamma.solutions.library.storage.SourcesModule;
+import com.opengamma.solutions.util.ViewUtils;
+import com.opengamma.util.time.DateUtils;
+
+/**
+ * Equity Index Option example
+ */
+public class EquityIndexOptionPricerExample {
+
+  private static final Logger s_logger = LoggerFactory.getLogger(EquityIndexOptionPricerExample.class);
+  private static final String DISCOUNTING_CURVES = "data/curves/discounting-curves.csv";
+  private static final String FORWARD_CURVES = "data/curves/forward-curves.csv";
+  private static final String VOLATILITY_SURFACES = ""; //TODO add file
+  private static final String TRADES = ""; //TODO add file
+  private static final ZonedDateTime VALUATION_TIME = DateUtils.getUTCDate(2014, 7, 22);
+
+  /**
+   * Entry point to running the Equity Index Option Pricer.
+   * Log PV results to the console
+   * @param args, no args are need to run this tool
+   */
+  public static void main(String[] args) throws IOException {
+
+    Set<Module> modules = Sets.newHashSet();
+    modules.add(new InMemoryStorageModule());
+    modules.add(new SourcesModule());
+    modules.add(new EngineModule());
+    Injector injector = Guice.createInjector(modules);
+
+    EquityIndexOptionPricer pricer = injector.getInstance(EquityIndexOptionPricer.class);
+    Results results = pricer.price(VALUATION_TIME,
+                                   TRADES,
+                                   DISCOUNTING_CURVES,
+                                   FORWARD_CURVES,
+                                   VOLATILITY_SURFACES);
+    s_logger.info("Got results:\n" + ViewUtils.format(results));
+
+  }
+
+}
