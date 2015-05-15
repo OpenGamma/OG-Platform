553dfd9b764f88fc7604be2e3b7cc16ed8908b73
Ian Turner <ian@opengamma.com>
2015-05-15 16:49:14 +0100

    PLT-896 curve and option utils

---
+++ sesame-examples/example-data/src/main/java/com/opengamma/solutions/util/CurveUtils.java
@@ -0,0 +1,203 @@
+/**
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.solutions.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import com.bethecoder.table.AsciiTableInstance;
+import com.google.common.collect.Lists;
+import com.opengamma.analytics.financial.model.interestrate.curve.ForwardCurve;
+import com.opengamma.analytics.financial.model.interestrate.curve.YieldAndDiscountCurve;
+import com.opengamma.analytics.financial.model.interestrate.curve.YieldCurve;
+import com.opengamma.analytics.math.curve.InterpolatedDoublesCurve;
+import com.opengamma.analytics.math.interpolation.CombinedInterpolatorExtrapolatorFactory;
+import com.opengamma.analytics.math.interpolation.Interpolator1D;
+import com.opengamma.analytics.math.interpolation.Interpolator1DFactory;
+import com.opengamma.financial.analytics.DoubleLabelledMatrix1D;
+import com.opengamma.financial.analytics.model.fixedincome.BucketedCurveSensitivities;
+import com.opengamma.financial.security.FinancialSecurity;
+import com.opengamma.sesame.engine.ResultItem;
+import com.opengamma.sesame.engine.ResultRow;
+import com.opengamma.sesame.engine.Results;
+import com.opengamma.sesame.trade.TradeWrapper;
+import com.opengamma.util.ArgumentChecker;
+import com.opengamma.util.money.MultipleCurrencyAmount;
+import com.opengamma.util.result.Result;
+import com.opengamma.util.time.Tenor;
+import com.opengamma.util.tuple.Pair;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+/**
+ * Utility class for views
+ */
+public final class CurveUtils {
+
+  private CurveUtils() { /* private constructor */ }
+
+  private static final Logger s_logger = LoggerFactory.getLogger(CurveUtils.class);
+
+  /**
+   * Reads and parses curve data into in memory raw representation
+   * <p/>
+   * The format of the file is simple csv with a header row:
+   * <p/>
+   * Name,Tenor,Value
+   * USD-Federal Funds,1D,0.05918
+   * USD-Federal Funds,1W,0.06263
+   * USD-Federal Funds,2W,0.06415
+   *
+   * @param file the data to be parsed
+   * @return the parsed curve data by curve name
+   */
+  public static Map<String, CurveRawData> parseCurves(String file) throws IOException {
+
+    s_logger.info("Creating curves from {}.", file);
+    Map<String, CurveRawData> curveData = new HashMap<>();
+
+    Reader curveReader = new BufferedReader(
+        new InputStreamReader(
+            new ClassPathResource(file).getInputStream()
+        )
+    );
+
+    try {
+      CSVReader csvReader = new CSVReader(curveReader);
+      String[] line;
+      csvReader.readNext(); // skip headers
+      while ((line = csvReader.readNext()) != null) {
+        String name = line[0];
+
+        String tenor = line[1];
+        if (tenor.endsWith("W")) {
+          int weeks = Integer.parseInt(tenor.substring(0, tenor.length() - 1));
+          tenor = (weeks * 7) + "D";
+        } else if (tenor.endsWith("M")) {
+          int months = Integer.parseInt(tenor.substring(0, tenor.length() - 1));
+          tenor = (months / 12 * 365) + "D";
+        } else if (tenor.endsWith("Y")) {
+          int years = Integer.parseInt(tenor.substring(0, tenor.length() - 1));
+          tenor = (years * 365) + "D";
+        }
+
+        String quote = line[2];
+        double quoteValue;
+        try {
+          quoteValue = Double.parseDouble(quote);
+        } catch (NumberFormatException e) {
+          s_logger.warn("Invalid quote {} for {} at {}", quote, name, tenor);
+          continue;
+        }
+        if (!curveData.containsKey(name)) {
+          curveData.put(name, new CurveRawData());
+        }
+        curveData.get(name).add(Tenor.parse("P" + tenor), quoteValue);
+      }
+    } catch (IOException e) {
+      s_logger.error("Failed to parse curve data ", e);
+    }
+    return curveData;
+  }
+
+
+  public static ForwardCurve createForwardCurve(CurveRawData data) {
+    List<Tenor> tenors = Lists.newArrayList(data.getCurvePoint());
+    double[] maturities = new double[tenors.size()];
+    double[] prices = new double[tenors.size()];
+
+    for (int i = 0; i < tenors.size(); i++) {
+      Tenor tenor = tenors.get(i);
+      maturities[i] = tenor.getPeriod().getDays() / 365d;
+      prices[i] = data.getValue(tenor);
+    }
+
+    Interpolator1D interpolator = CombinedInterpolatorExtrapolatorFactory.getInterpolator(
+        Interpolator1DFactory.LINEAR,
+        Interpolator1DFactory.FLAT_EXTRAPOLATOR,
+        Interpolator1DFactory.FLAT_EXTRAPOLATOR);
+
+    return new ForwardCurve(InterpolatedDoublesCurve.from(maturities, prices, interpolator));
+  }
+
+  public static YieldAndDiscountCurve createYieldCurve(String name, CurveRawData data) {
+    List<Tenor> tenors = Lists.newArrayList(data.getCurvePoint());
+    double[] ttm = new double[tenors.size()];
+    double[] rates = new double[tenors.size()];
+
+    for (int i = 0; i < tenors.size(); i++) {
+      Tenor tenor = tenors.get(i);
+      ttm[i] = tenor.getPeriod().getDays() / 365d;
+      rates[i] = data.getValue(tenor) / 100d;
+    }
+
+    Interpolator1D interpolator = CombinedInterpolatorExtrapolatorFactory.getInterpolator(
+        Interpolator1DFactory.LINEAR,
+        Interpolator1DFactory.FLAT_EXTRAPOLATOR,
+        Interpolator1DFactory.FLAT_EXTRAPOLATOR);
+
+    InterpolatedDoublesCurve interpolatedCurve = InterpolatedDoublesCurve.from(ttm, rates, interpolator);
+    return new YieldCurve(name, interpolatedCurve);
+  }
+
+
+  /**
+   * Helper class to store raw curve data
+   */
+  static class CurveRawData {
+    private TreeMap<Tenor, Double> _doublesCurve = new TreeMap<>();
+
+    /**
+     * Add a curve point
+     *
+     * @param curvePoint tenor identifying this point
+     * @param curveValue value at that point as a zero rate
+     */
+    public void add(Tenor curvePoint, Double curveValue) {
+      ArgumentChecker.notNull(curvePoint, "curvePoint");
+      ArgumentChecker.notNull(curveValue, "curveValue");
+      _doublesCurve.put(curvePoint, curveValue);
+    }
+
+    /**
+     * @return points available on the curve
+     */
+    public Collection<Tenor> getCurvePoint() {
+      return _doublesCurve.keySet();
+    }
+
+    /**
+     * @return all of the raw data points in the curve
+     */
+    public Collection<Double> getCurveValue() {
+      return _doublesCurve.values();
+    }
+
+    /**
+     * @param tenor point on the curve
+     * @return rate of the specified point
+     */
+    public Double getValue(Tenor tenor) {
+      return _doublesCurve.get(tenor);
+    }
+
+  }
+
+
+}
---
+++ sesame-examples/example-data/src/main/java/com/opengamma/solutions/util/EquityIndexOptionViewUtils.java
@@ -0,0 +1,233 @@
+/**
+ * Copyright (C) 2014 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.solutions.util;
+
+import static com.opengamma.sesame.config.ConfigBuilder.argument;
+import static com.opengamma.sesame.config.ConfigBuilder.arguments;
+import static com.opengamma.sesame.config.ConfigBuilder.column;
+import static com.opengamma.sesame.config.ConfigBuilder.config;
+import static com.opengamma.sesame.config.ConfigBuilder.configureView;
+import static com.opengamma.sesame.config.ConfigBuilder.function;
+import static com.opengamma.sesame.config.ConfigBuilder.implementations;
+import static com.opengamma.sesame.config.ConfigBuilder.output;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalTime;
+import org.threeten.bp.OffsetTime;
+import org.threeten.bp.ZoneOffset;
+import org.threeten.bp.ZonedDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.opengamma.analytics.financial.model.interestrate.curve.ForwardCurve;
+import com.opengamma.analytics.financial.model.interestrate.curve.YieldAndDiscountCurve;
+import com.opengamma.analytics.financial.model.interestrate.curve.YieldCurve;
+import com.opengamma.analytics.financial.model.volatility.surface.VolatilitySurface;
+import com.opengamma.analytics.financial.provider.curve.CurveBuildingBlockBundle;
+import com.opengamma.analytics.financial.provider.description.interestrate.MulticurveProviderDiscount;
+import com.opengamma.analytics.math.curve.ConstantDoublesCurve;
+import com.opengamma.analytics.math.curve.InterpolatedDoublesCurve;
+import com.opengamma.analytics.math.interpolation.CombinedInterpolatorExtrapolatorFactory;
+import com.opengamma.analytics.math.interpolation.GridInterpolator2D;
+import com.opengamma.analytics.math.interpolation.Interpolator1D;
+import com.opengamma.analytics.math.interpolation.Interpolator1DFactory;
+import com.opengamma.analytics.math.surface.InterpolatedDoublesSurface;
+import com.opengamma.core.id.ExternalSchemes;
+import com.opengamma.core.link.ConfigLink;
+import com.opengamma.core.position.Counterparty;
+import com.opengamma.core.position.impl.SimpleCounterparty;
+import com.opengamma.core.position.impl.SimpleTrade;
+import com.opengamma.financial.analytics.curve.exposure.CurrencyExposureFunction;
+import com.opengamma.financial.analytics.curve.exposure.ExposureFunctions;
+import com.opengamma.financial.convention.businessday.BusinessDayConventions;
+import com.opengamma.financial.convention.daycount.DayCount;
+import com.opengamma.financial.convention.daycount.DayCounts;
+import com.opengamma.financial.convention.frequency.SimpleFrequency;
+import com.opengamma.financial.currency.CurrencyMatrix;
+import com.opengamma.financial.security.credit.StandardCDSSecurity;
+import com.opengamma.financial.security.fra.ForwardRateAgreementSecurity;
+import com.opengamma.financial.security.option.EquityIndexOptionSecurity;
+import com.opengamma.financial.security.option.ExerciseType;
+import com.opengamma.financial.security.option.OptionType;
+import com.opengamma.id.ExternalId;
+import com.opengamma.sesame.CurveSelector;
+import com.opengamma.sesame.CurveSelectorMulticurveBundleFn;
+import com.opengamma.sesame.DefaultForwardCurveFn;
+import com.opengamma.sesame.DiscountingMulticurveCombinerFn;
+import com.opengamma.sesame.ForwardCurveFn;
+import com.opengamma.sesame.MarketExposureSelector;
+import com.opengamma.sesame.MulticurveBundle;
+import com.opengamma.sesame.OutputNames;
+import com.opengamma.sesame.UnderlyingForwardCurveFn;
+import com.opengamma.sesame.config.FunctionModelConfig;
+import com.opengamma.sesame.config.ViewColumn;
+import com.opengamma.sesame.config.ViewConfig;
+import com.opengamma.sesame.equity.StaticReplicationDataBundleFn;
+import com.opengamma.sesame.equity.StrikeDataBundleFn;
+import com.opengamma.sesame.equityindexoptions.DefaultEquityIndexOptionFn;
+import com.opengamma.sesame.equityindexoptions.EquityIndexOptionFn;
+import com.opengamma.sesame.fra.DiscountingFRACalculatorFactory;
+import com.opengamma.sesame.fra.DiscountingFRAFn;
+import com.opengamma.sesame.fra.FRACalculatorFactory;
+import com.opengamma.sesame.fra.FRAFn;
+import com.opengamma.sesame.marketdata.DefaultHistoricalMarketDataFn;
+import com.opengamma.sesame.marketdata.DefaultMarketDataFn;
+import com.opengamma.sesame.marketdata.ForwardCurveId;
+import com.opengamma.sesame.marketdata.MarketDataEnvironmentBuilder;
+import com.opengamma.sesame.marketdata.MulticurveId;
+import com.opengamma.sesame.marketdata.RawId;
+import com.opengamma.sesame.marketdata.VolatilitySurfaceId;
+import com.opengamma.sesame.trade.EquityIndexOptionTrade;
+import com.opengamma.util.ArgumentChecker;
+import com.opengamma.util.money.Currency;
+import com.opengamma.util.time.DateUtils;
+import com.opengamma.util.time.Expiry;
+import com.opengamma.util.time.Tenor;
+import com.opengamma.util.tuple.Pair;
+import com.opengamma.util.tuple.Pairs;
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+/**
+ * Utility class for Equity Index Options views
+ */
+public final class EquityIndexOptionViewUtils {
+
+  private EquityIndexOptionViewUtils() { /* private constructor */ }
+  /**
+   * Utility for creating a Equity Index Options specific view column
+   */
+  public static ViewConfig createViewConfig() {
+
+    return
+        configureView(
+            "Equity Index Options View",
+            config(
+                arguments(
+                    function(
+                        MarketExposureSelector.class,
+                        argument("exposureFunctions", ConfigLink.resolved(createExposureFunction())))),
+                implementations(
+                    EquityIndexOptionFn.class, DefaultEquityIndexOptionFn.class,
+                    StaticReplicationDataBundleFn.class, StrikeDataBundleFn.class,
+                    CurveSelector.class, MarketExposureSelector.class,
+                    ForwardCurveFn.class, DefaultForwardCurveFn.class,
+                    DiscountingMulticurveCombinerFn.class, CurveSelectorMulticurveBundleFn.class)),
+            column(OutputNames.PRESENT_VALUE, output(OutputNames.PRESENT_VALUE, EquityIndexOptionTrade.class)),
+            column(OutputNames.DELTA, output(OutputNames.DELTA, EquityIndexOptionTrade.class)),
+            column(OutputNames.GAMMA, output(OutputNames.GAMMA, EquityIndexOptionTrade.class)),
+            column(OutputNames.VEGA, output(OutputNames.VEGA, EquityIndexOptionTrade.class)),
+            column(OutputNames.PV01, output(OutputNames.PV01, EquityIndexOptionTrade.class)),
+            column(OutputNames.BUCKETED_PV01, output(OutputNames.BUCKETED_PV01, EquityIndexOptionTrade.class))
+        );
+  }
+
+  // TODO create from discounting input
+  private static ExposureFunctions createExposureFunction() {
+    List<String> exposureFunctions =  ImmutableList.of(CurrencyExposureFunction.NAME);
+    Map<ExternalId, String> idsToNames = new HashMap<>();
+    idsToNames.put(ExternalId.of("CurrencyISO", "JPY"), "MultiCurve");
+    return new ExposureFunctions("Exposure", exposureFunctions, idsToNames);
+  }
+
+  public static List<Object> parseTrades(String tradeFile) {
+    return INPUTS;
+  }
+
+  public static void parseDiscountingCurves(MarketDataEnvironmentBuilder builder, String discountingCurves) throws IOException {
+    String bundleName = "MultiCurve";
+    MulticurveProviderDiscount multicurve = new MulticurveProviderDiscount();
+    Map<String, CurveUtils.CurveRawData> curves = CurveUtils.parseCurves(discountingCurves);
+    for(Map.Entry<String, CurveUtils.CurveRawData> curve : curves.entrySet()) {
+      YieldAndDiscountCurve yieldCurve = CurveUtils.createYieldCurve(curve.getKey() + " Discounting", curve.getValue());
+      multicurve.setCurve(Currency.of(curve.getKey()), yieldCurve);
+
+    }
+    MulticurveBundle bundle = new MulticurveBundle(multicurve, new CurveBuildingBlockBundle());
+    builder.add(MulticurveId.of(bundleName), bundle);
+  }
+
+  public static void parseVolatilitySurfaces(MarketDataEnvironmentBuilder builder, String volatilitySurfaces) {
+    builder.add(VolatilitySurfaceId.of("CALL_NK225"), getVolatilitySurface());
+  }
+
+  public static void parseForwardCurves(MarketDataEnvironmentBuilder builder, String forwardCurves) throws IOException {
+    Map<String, CurveUtils.CurveRawData> curves = CurveUtils.parseCurves(forwardCurves);
+    for(Map.Entry<String, CurveUtils.CurveRawData> curve : curves.entrySet()) {
+      builder.add(ForwardCurveId.of(curve.getKey()), CurveUtils.createForwardCurve(curve.getValue()));
+    }
+  }
+
+
+  //TODO REMOVE ALL STATIC DATA BELOW.....
+  private static final ZonedDateTime VALUATION_TIME = DateUtils.getUTCDate(2014, 7, 22);
+  private static final List<Object> INPUTS = ImmutableList.<Object>of(createOptionTrade());
+  private static EquityIndexOptionTrade createOptionTrade() {
+    Counterparty counterparty = new SimpleCounterparty(ExternalId.of(Counterparty.DEFAULT_SCHEME, "COUNTERPARTY"));
+    BigDecimal tradeQuantity = BigDecimal.valueOf(1);
+    LocalDate tradeDate = LocalDate.of(2000, 1, 1);
+    OffsetTime tradeTime = OffsetTime.of(LocalTime.of(0, 0), ZoneOffset.UTC);
+    SimpleTrade trade = new SimpleTrade(createEquityIndexOptionSecurity(), tradeQuantity, counterparty, tradeDate, tradeTime);
+    trade.setPremium(0.0);
+    trade.setPremiumCurrency(Currency.JPY);
+    return new EquityIndexOptionTrade(trade);
+  }
+
+  private static EquityIndexOptionSecurity createEquityIndexOptionSecurity() {
+    OptionType optionType = OptionType.CALL;
+    double strike = 10500;
+    Currency currency = Currency.JPY;
+    ExternalId underlyingId = ExternalId.of(ExternalSchemes.BLOOMBERG_TICKER, "NK225");
+    ExerciseType exerciseType = ExerciseType.of("European");
+    Expiry expiry = new Expiry(VALUATION_TIME.plusMonths(2));
+    double pointValue = 1;
+    String exchange = "XJPY";
+    EquityIndexOptionSecurity security = new EquityIndexOptionSecurity(optionType,
+                                                                       strike,
+                                                                       currency,
+                                                                       underlyingId,
+                                                                       exerciseType,
+                                                                       expiry,
+                                                                       pointValue,
+                                                                       exchange);
+    security.setName(underlyingId.getValue() + " " + optionType.toString() + " Option " + expiry.getExpiry().toString());
+    return security;
+  }
+
+  private static VolatilitySurface getVolatilitySurface() {
+    Interpolator1D linearFlat =
+        CombinedInterpolatorExtrapolatorFactory.getInterpolator(Interpolator1DFactory.LINEAR,
+                                                                Interpolator1DFactory.FLAT_EXTRAPOLATOR,
+                                                                Interpolator1DFactory.FLAT_EXTRAPOLATOR);
+    GridInterpolator2D interpolator2D = new GridInterpolator2D(linearFlat, linearFlat);
+    InterpolatedDoublesSurface surface = InterpolatedDoublesSurface.from(
+        new double[] {31d/356, 59d/365, 94d/365, 31d/356, 59d/365, 94d/365},
+        new double[] {4000, 10500, 12750, 4500, 10750, 13000},
+        new double[] {258.68/100, 78.7594/100, 39.8789/100, 239.8297/100, 76.0916/100, 38.2484/100},
+        interpolator2D
+    );
+    return new VolatilitySurface(surface);
+  }
+
+}
