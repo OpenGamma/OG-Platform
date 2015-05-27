d74e58681018231c8dbc2f4d5246c7a8862a09e5
Steve McCarthy <stephenm@opengamma.com>
2015-05-27 14:47:52 -0400

    [PLT-889] Add REST endpoint to facilitate excel

--- sesame-web/src/main/java/com/opengamma/sesame/web/curves/SwapPricingResource.java
+++ sesame-web/src/main/java/com/opengamma/sesame/web/curves/SwapPricingResource.java
@@ -4,12 +4,6 @@ package com.opengamma.sesame.web.curves;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.gson.Gson;
-import com.opengamma.analytics.financial.instrument.index.IborIndex;
-import com.opengamma.analytics.financial.instrument.index.IndexON;
-import com.opengamma.analytics.financial.model.interestrate.curve.DiscountCurve;
-import com.opengamma.analytics.financial.model.interestrate.curve.YieldAndDiscountCurve;
-import com.opengamma.analytics.financial.model.interestrate.curve.YieldCurve;
-import com.opengamma.analytics.financial.provider.description.interestrate.MulticurveProviderDiscount;
 import com.opengamma.core.link.ConfigLink;
 import com.opengamma.engine.marketdata.spec.MarketDataSpecification;
 import com.opengamma.engine.marketdata.spec.MarketDataSpecificationParser;
@@ -19,7 +13,6 @@ import com.opengamma.sesame.CurveSelector;
 import com.opengamma.sesame.CurveSelectorMulticurveBundleFn;
 import com.opengamma.sesame.DiscountingMulticurveCombinerFn;
 import com.opengamma.sesame.MarketExposureSelector;
-import com.opengamma.sesame.MulticurveBundle;
 import com.opengamma.sesame.OutputNames;
 import com.opengamma.sesame.config.ViewColumn;
 import com.opengamma.sesame.config.ViewConfig;
@@ -45,23 +38,17 @@ import com.opengamma.sesame.marketdata.builders.MarketDataEnvironmentFactory;
 import com.opengamma.sesame.marketdata.scenarios.SingleScenarioDefinition;
 import com.opengamma.sesame.trade.InterestRateSwapTrade;
 import com.opengamma.util.ArgumentChecker;
-import com.opengamma.util.money.Currency;
-import org.apache.commons.lang.ArrayUtils;
 import org.joda.beans.Bean;
 import org.joda.beans.ser.JodaBeanSer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.threeten.bp.ZonedDateTime;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import static com.opengamma.sesame.config.ConfigBuilder.argument;
@@ -91,60 +78,12 @@ public class SwapPricingResource  {
   }
 
   /**
-   * Rest endpoint to obtain curve bundle data
-   * @param bundle the name of the bundle
-   * @param spec the market data specification in the format {type}:{name/id}
-   * @return a json representation of the bundle, split into discounting, forward ON and forward Ibor Curves
+   *
+   * @param bundle Curve Construction Config
+   * @param spec Data source
+   * @param input Swap in Body of POST
+   * @return PV
    */
-  @GET
-  @Produces("application/json")
-  public String buildCurveBundle(@PathParam("bundle") String bundle,
-                                 @PathParam("spec") String spec) {
-
-    MarketDataSpecification marketDataSpec = MarketDataSpecificationParser.parse(spec);
-    ZonedDateTime valuationTime = ZonedDateTime.now();
-    MarketDataEnvironment suppliedData = MarketDataEnvironmentBuilder.empty();
-    MulticurveId multicurveId = MulticurveId.of(bundle);
-    SingleValueRequirement requirement = SingleValueRequirement.of(multicurveId);
-    Set<MarketDataRequirement> requirements = ImmutableSet.<MarketDataRequirement>of(requirement);
-    SingleScenarioDefinition perturbations = SingleScenarioDefinition.base();
-    MarketDataEnvironment marketData = _environmentFactory.build(suppliedData,
-        requirements,
-        perturbations,
-        marketDataSpec,
-        valuationTime);
-    MulticurveBundle curveBundle = (MulticurveBundle) marketData.getData().get(requirement);
-    MulticurveProviderDiscount multicurveProvider = curveBundle.getMulticurveProvider();
-
-    Map<Currency, YieldAndDiscountCurve> discountingCurves = multicurveProvider.getDiscountingCurves();
-    Map<IndexON, YieldAndDiscountCurve> forwardONCurves = multicurveProvider.getForwardONCurves();
-    Map<IborIndex, YieldAndDiscountCurve> forwardIborCurves = multicurveProvider.getForwardIborCurves();
-
-    Map<String, Map<String, Object>> discounting = new HashMap<>();
-    Map<String, Map<String, Object>> forwardON = new HashMap<>();
-    Map<String, Map<String, Object>> forwardIbor = new HashMap<>();
-
-    for (Map.Entry<Currency, YieldAndDiscountCurve> entry : discountingCurves.entrySet()) {
-      discounting.put(entry.getKey().getCode(), getCurveData(entry.getValue()));
-    }
-
-    for (Map.Entry<IndexON, YieldAndDiscountCurve> entry : forwardONCurves.entrySet()) {
-      forwardON.put(entry.getKey().toString(), getCurveData(entry.getValue()));
-    }
-
-    for (Map.Entry<IborIndex, YieldAndDiscountCurve> entry : forwardIborCurves.entrySet()) {
-      forwardIbor.put(entry.getKey().toString(), getCurveData(entry.getValue()));
-    }
-
-    Map<String, Map> curves = new HashMap<>();
-    curves.put("discounting", discounting);
-    curves.put("forwardOn", forwardON);
-    curves.put("forwardIbor", forwardIbor);
-
-    Gson gson = new Gson();
-    return gson.toJson(curves);
-  }
-
   @POST
   public String priceSwap(@PathParam("bundle") String bundle,
                           @PathParam("spec") String spec, String input){
@@ -172,7 +111,6 @@ public class SwapPricingResource  {
 
     ViewRunner viewRunner = new RemoteViewRunner(URI.create("http://localhost:8080/jax"));
 
-    MarketDataEnvironment marketDataEnvironment = MarketDataEnvironmentBuilder.empty();
     MarketDataSpecification marketDataSpec = MarketDataSpecificationParser.parse(spec);
     ZonedDateTime valuationTime = ZonedDateTime.now();
     MarketDataEnvironment suppliedData = MarketDataEnvironmentBuilder.empty();
@@ -203,24 +141,6 @@ public class SwapPricingResource  {
 
   }
 
-//  ****Commented out because Matrix1D is a pain to JSON-ify
-//  private ViewConfig createViewConfig(ConfigLink<ExposureFunctions> exposureConfig, ConfigLink<CurrencyMatrix> currencyMatrixLink) {
-//    return
-//        configureView(
-//            "IRS Remote view",
-//            createInterestRateSwapViewColumn(OutputNames.PRESENT_VALUE,
-//                exposureConfig,
-//                currencyMatrixLink),
-//            createInterestRateSwapViewColumn(OutputNames.BUCKETED_PV01,
-//                exposureConfig,
-//                currencyMatrixLink),
-//            createInterestRateSwapViewColumn(OutputNames.PAY_LEG_CASH_FLOWS,
-//                exposureConfig,
-//                currencyMatrixLink),
-//            createInterestRateSwapViewColumn(OutputNames.RECEIVE_LEG_CASH_FLOWS,
-//                exposureConfig,
-//                currencyMatrixLink));
-//  }
 
   private ViewConfig createViewConfig(ConfigLink<ExposureFunctions> exposureConfig, ConfigLink<CurrencyMatrix> currencyMatrixLink) {
     return
@@ -260,34 +180,5 @@ public class SwapPricingResource  {
                     InterestRateSwapCalculatorFactory.class, DiscountingInterestRateSwapCalculatorFactory.class)));
   }
 
-  private Map<String, Object> getCurveData(YieldAndDiscountCurve curve) {
-    Double[] tenors;
-    Double[] rates;
-    Map<String, Object> data = new HashMap<>();
-
-    try {
-      if (curve instanceof YieldCurve) {
-        tenors = ((YieldCurve) curve).getCurve().getXData();
-        rates = ((YieldCurve) curve).getCurve().getYData();
-      } else if (curve instanceof DiscountCurve) {
-        tenors = ((DiscountCurve) curve).getCurve().getXData();
-        rates = ((DiscountCurve) curve).getCurve().getYData();
-      } else {
-        tenors = new Double[0];
-        rates = new Double[0];
-        s_logger.error(curve.getName() + " is in instance of " + curve.getClass() + " CurveBundleResource only " +
-            "supports YieldCurve and DiscountCurve instances of YieldAndDiscountCurve");
-      }
-    } catch (UnsupportedOperationException e) {
-      throw new IllegalArgumentException("Curve type: " + curve.getClass().getName() + ", does not support " +
-          "the ability to get X or Y data", e);
-    }
-
-    data.put("name", curve.getName());
-    data.put("x", ArrayUtils.toPrimitive(tenors));
-    data.put("y", ArrayUtils.toPrimitive(rates));
-
-    return data;
-  }
 
 }
